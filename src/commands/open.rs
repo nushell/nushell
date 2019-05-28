420b840cd62cdd840f9a10ed3caca50f12c532ab
Jonathan Turner <joturner@microsoft.com>
2019-05-28 16:00:00 +1200

    Add missing files

---
+++ src/commands/open.rs
@@ -0,0 +1,20 @@
+use crate::errors::ShellError;
+use crate::object::{Primitive, Value};
+use crate::prelude::*;
+use std::path::PathBuf;
+
+pub fn open(args: CommandArgs) -> Result<OutputStream, ShellError> {
+    let cwd = args.env.lock().unwrap().cwd().to_path_buf();
+    let mut full_path = PathBuf::from(cwd);
+    match &args.args[0] {
+        Value::Primitive(Primitive::String(s)) => full_path.push(s),
+        _ => {}
+    }
+
+    let contents = std::fs::read_to_string(&full_path).unwrap();
+
+    let mut stream = VecDeque::new();
+    stream.push_back(ReturnValue::Value(Value::Primitive(Primitive::String(contents))));
+
+    Ok(stream.boxed())
+}
