71b893561bac57b083ce5661f36fa193a4732412
Jonathan Turner <jonathan.d.turner@gmail.com>
2019-08-04 19:12:22 +1200

    WIP moving to multi-shells

--- src/env/environment.rs
+++ src/env/environment.rs
@@ -1,3 +1,7 @@
+use crate::commands::command::CallInfo;
+use crate::object::dir_entry_dict;
+use crate::prelude::*;
+use crate::shell::shell::Shell;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug, Clone)]
@@ -14,5 +18,120 @@ impl Environment {
 
     pub fn path(&self) -> &Path {
         self.path.as_path()
+    }
+}
+
+impl Shell for Environment {
+    fn ls(&self, call_info: CallInfo, _input: InputStream) -> Result<OutputStream, ShellError> {
+        let cwd = self.path.clone();
+        let mut full_path = PathBuf::from(&cwd);
+        match &call_info.args.nth(0) {
+            Some(Tagged { item: value, .. }) => full_path.push(Path::new(&value.as_string()?)),
+            _ => {}
+        }
+
+        let entries = glob::glob(&full_path.to_string_lossy());
+
+        if entries.is_err() {
+            return Err(ShellError::string("Invalid pattern."));
+        }
+
+        let mut shell_entries = VecDeque::new();
+        let entries: Vec<_> = entries.unwrap().collect();
+
+        // If this is a single entry, try to display the contents of the entry if it's a directory
+        if entries.len() == 1 {
+            if let Ok(entry) = &entries[0] {
+                if entry.is_dir() {
+                    let entries = std::fs::read_dir(&full_path);
+
+                    let entries = match entries {
+                        Err(e) => {
+                            if let Some(s) = call_info.args.nth(0) {
+                                return Err(ShellError::labeled_error(
+                                    e.to_string(),
+                                    e.to_string(),
+                                    s.span(),
+                                ));
+                            } else {
+                                return Err(ShellError::maybe_labeled_error(
+                                    e.to_string(),
+                                    e.to_string(),
+                                    call_info.name_span,
+                                ));
+                            }
+                        }
+                        Ok(o) => o,
+                    };
+                    for entry in entries {
+                        let entry = entry?;
+                        let filepath = entry.path();
+                        let filename = filepath.strip_prefix(&cwd).unwrap();
+                        let value =
+                            dir_entry_dict(filename, &entry.metadata()?, call_info.name_span)?;
+                        shell_entries.push_back(ReturnSuccess::value(value))
+                    }
+                    return Ok(shell_entries.to_output_stream());
+                }
+            }
+        }
+
+        // Enumerate the entries from the glob and add each
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let filename = entry.strip_prefix(&cwd).unwrap();
+                let metadata = std::fs::metadata(&entry)?;
+                let value = dir_entry_dict(filename, &metadata, call_info.name_span)?;
+                shell_entries.push_back(ReturnSuccess::value(value))
+            }
+        }
+
+        Ok(shell_entries.to_output_stream())
+    }
+
+    fn cd(&self, call_info: CallInfo, _input: InputStream) -> Result<OutputStream, ShellError> {
+        let path = match call_info.args.nth(0) {
+            None => match dirs::home_dir() {
+                Some(o) => o,
+                _ => {
+                    return Err(ShellError::maybe_labeled_error(
+                        "Can not change to home directory",
+                        "can not go to home",
+                        call_info.name_span,
+                    ))
+                }
+            },
+            Some(v) => {
+                let target = v.as_string()?;
+                match dunce::canonicalize(self.path.join(target).as_path()) {
+                    Ok(p) => p,
+                    Err(_) => {
+                        return Err(ShellError::labeled_error(
+                            "Can not change to directory",
+                            "directory not found",
+                            v.span().clone(),
+                        ));
+                    }
+                }
+            }
+        };
+
+        let mut stream = VecDeque::new();
+        match std::env::set_current_dir(&path) {
+            Ok(_) => {}
+            Err(_) => {
+                if call_info.args.len() > 0 {
+                    return Err(ShellError::labeled_error(
+                        "Can not change to directory",
+                        "directory not found",
+                        call_info.args.nth(0).unwrap().span().clone(),
+                    ));
+                } else {
+                    return Err(ShellError::string("Can not change to directory"));
+                }
+            }
+        }
+        stream.push_back(ReturnSuccess::change_cwd(path));
+        Ok(stream.into())
     }
 }
--- src/shell.rs
+++ src/shell.rs
@@ -1,4 +1,5 @@
 crate mod completer;
 crate mod helper;
+crate mod shell;
 
 crate use helper::Helper;
