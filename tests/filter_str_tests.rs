cee2f5207ed409456ccb460971aaf3b82df0f6bb
Andrés N. Robalino <andres@androbtech.com>
2019-08-11 12:46:14 -0500

    Str plugin: replace & find-replace.

--- tests/filter_str_tests.rs
+++ tests/filter_str_tests.rs
@@ -11,24 +11,27 @@ fn can_only_apply_one() {
         "open caco3_plastics.csv | first 1 | str origin --downcase --upcase"
     );
 
-    assert!(output.contains("Usage: str field [--downcase|--upcase|--to-int]"));
+    assert!(
+        output.contains("Usage: str field [--downcase|--upcase|--to-int|--replace|--find-replace]")
+    );
 }
 
 #[test]
 fn acts_without_passing_field() {
-    Playground::setup_for("plugin_str_test_without_passing_field")
-        .with_files(vec![FileWithContent(
+    Playground::setup_for("plugin_str_acts_without_passing_field_test").with_files(vec![
+        FileWithContent(
             "sample.yml",
             r#"
                 environment:
                   global:
                     PROJECT_NAME: nushell
             "#,
-        )]);
+        ),
+    ]);
 
     nu!(
         output,
-        cwd("tests/fixtures/nuplayground/plugin_str_test_without_passing_field"),
+        cwd("tests/fixtures/nuplayground/plugin_str_acts_without_passing_field_test"),
         "open sample.yml | get environment.global.PROJECT_NAME | str --upcase | echo $it"
     );
 
@@ -37,18 +40,17 @@ fn acts_without_passing_field() {
 
 #[test]
 fn downcases() {
-    Playground::setup_for("plugin_str_test_downcases")
-        .with_files(vec![FileWithContent(
-            "sample.toml",
-            r#"
-                [dependency]
-                name = "LIGHT"
-            "#,
-        )]);
+    Playground::setup_for("plugin_str_downcases_test").with_files(vec![FileWithContent(
+        "sample.toml",
+        r#"
+            [dependency]
+            name = "LIGHT"
+        "#,
+    )]);
 
     nu!(
         output,
-        cwd("tests/fixtures/nuplayground/plugin_str_test_downcases"),
+        cwd("tests/fixtures/nuplayground/plugin_str_downcases_test"),
         "open sample.toml | str dependency.name --downcase | get dependency.name | echo $it"
     );
 
@@ -57,18 +59,17 @@ fn downcases() {
 
 #[test]
 fn upcases() {
-    Playground::setup_for("plugin_str_test_upcases")
-        .with_files(vec![FileWithContent(
-            "sample.toml",
-            r#"
-                [package]
-                name = "nushell"
-            "#,
-        )]);
+    Playground::setup_for("plugin_str_upcases_test").with_files(vec![FileWithContent(
+        "sample.toml",
+        r#"
+            [package]
+            name = "nushell"
+        "#,
+    )]);
 
     nu!(
         output,
-        cwd("tests/fixtures/nuplayground/plugin_str_test_upcases"),
+        cwd("tests/fixtures/nuplayground/plugin_str_upcases_test"),
         "open sample.toml | str package.name --upcase | get package.name | echo $it"
     );
 
@@ -84,4 +85,63 @@ fn converts_to_int() {
     );
 
     assert_eq!(output, "2509000000");
+}
+
+#[test]
+fn replaces() {
+    Playground::setup_for("plugin_str_replaces_test").with_files(vec![FileWithContent(
+        "sample.toml",
+        r#"
+            [package]
+            name = "nushell"
+        "#,
+    )]);
+
+    nu!(
+        output,
+        cwd("tests/fixtures/nuplayground/plugin_str_replaces_test"),
+        "open sample.toml | str package.name --replace wykittenshell  | get package.name | echo $it"
+    );
+
+    assert_eq!(output, "wykittenshell");
+}
+
+#[test]
+fn find_and_replaces() {
+    Playground::setup_for("plugin_str_find_and_replaces_test").with_files(vec![FileWithContent(
+        "sample.toml",
+        r#"
+            [fortune.teller]
+            phone = "1-800 KATZ"
+        "#,
+    )]);
+
+    nu!(
+        output,
+        cwd("tests/fixtures/nuplayground/plugin_str_find_and_replaces_test"),
+        "open sample.toml | str fortune.teller.phone --find-replace KATZ \"5289\" | get fortune.teller.phone | echo $it"
+    );
+
+    assert_eq!(output, "1-800 5289");
+}
+
+#[test]
+fn find_and_replaces_without_passing_field() {
+    Playground::setup_for("plugin_str_find_and_replaces_without_passing_field_test").with_files(
+        vec![FileWithContent(
+            "sample.toml",
+            r#"
+                [fortune.teller]
+                phone = "1-800 KATZ"
+            "#,
+        )],
+    );
+
+    nu!(
+        output,
+        cwd("tests/fixtures/nuplayground/plugin_str_find_and_replaces_without_passing_field_test"),
+        "open sample.toml | get fortune.teller.phone | str --find-replace KATZ \"5289\" | echo $it"
+    );
+
+    assert_eq!(output, "1-800 5289");
 }
