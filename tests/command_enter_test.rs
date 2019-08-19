51ec48f357c38eff76a6db7a0a28d0a04fb1d413
Jonathan Turner <jonathan.d.turner@gmail.com>
2019-08-20 04:40:47 +1200

    Update enter test

--- tests/command_enter_test.rs
+++ tests/command_enter_test.rs
@@ -45,7 +45,6 @@ fn knows_the_filesystems_entered() {
             n
             mv andres.nu ../expected/andres.nu
             exit
-            n
             cd ..
             rm red_pill --recursive
             exit
