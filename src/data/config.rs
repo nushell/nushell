a16eb25487b130dcc37b504f77b7d52365afdcbe
Akshat Agarwal <51334444+akshat2654@users.noreply.github.com>
2019-10-08 13:24:43 +0530

    Update config.rs

--- src/data/config.rs
+++ src/data/config.rs
@@ -52,7 +52,7 @@ pub fn user_data() -> Result<PathBuf, ShellError> {
 
 pub fn app_path(app_data_type: AppDataType, display: &str) -> Result<PathBuf, ShellError> {
     let path = app_root(app_data_type, &APP_INFO)
-        .map_err(|err| ShellError::string(&format!("Couldn't open {} path:\n{}", display, err)))?;
+        .map_err(|err| ShellError::labeled_error(&format!("Couldn't open {} path:\n{}", display, err)))?;
 
     Ok(path)
 }
@@ -75,10 +75,10 @@ pub fn read(
     let tag = tag.into();
     let contents = fs::read_to_string(filename)
         .map(|v| v.tagged(tag))
-        .map_err(|err| ShellError::string(&format!("Couldn't read config file:\n{}", err)))?;
+        .map_err(|err| ShellError::labeled_error(&format!("Couldn't read config file:\n{}", err)))?;
 
     let parsed: toml::Value = toml::from_str(&contents)
-        .map_err(|err| ShellError::string(&format!("Couldn't parse config file:\n{}", err)))?;
+        .map_err(|err| ShellError::labeled_error(&format!("Couldn't parse config file:\n{}", err)))?;
 
     let value = convert_toml_value_to_nu_value(&parsed, tag);
     let tag = value.tag();
