f0ca0312f3ef0061033c888b56de2a64fa530365
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2019-10-11 19:06:24 +0200

    Adds racer, formats shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,25 @@
 let
- moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  moz_overlay = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  nightly = ((nixpkgs.rustChannelOf { date = "2019-09-01"; channel = "nightly"; }).rust.override { extensions = [ "rust-src" "rls-preview" "clippy-preview" "rust-analysis" "rustfmt-preview" ];});
-in
-with nixpkgs;
+
+  nightly = ((nixpkgs.rustChannelOf {
+    date = "2019-09-01";
+    channel = "nightly";
+  }).rust.override {
+    extensions = [
+      "rust-src"
+      "rls-preview"
+      "clippy-preview"
+      "rust-analysis"
+      "rustfmt-preview"
+    ];
+  });
+
+in with nixpkgs;
 stdenv.mkDerivation {
   name = "nushell-rust";
-  buildInputs = [ nightly openssl_1_1 pkg-config ];
+  buildInputs = [ nightly openssl_1_1 pkg-config rustracer ];
+  RUST_SRC_PATH = "${nightly}/lib/rustlib/src/rust/src";
 }
