ecebe1314a2a46789062beaa5d0a4398b0d47174
Sebastian Jung <git.sebastian@gmx.de>
2019-12-03 20:56:39 +0100

    update to new crates structure

--- src/commands/range.rs
+++ src/commands/range.rs
@@ -1,7 +1,8 @@
 use crate::commands::WholeStreamCommand;
 use crate::context::CommandRegistry;
-use crate::errors::ShellError;
 use crate::prelude::*;
+use nu_errors::ShellError;
+use nu_protocol::{Signature, SyntaxShape};
 use nu_source::Tagged;
 
 #[derive(Deserialize)]
