219b7e64cdaabad9e95067b0e5abcbd4e396b9c1
Matteo Bertini <matteo@naufraghi.net>
2019-12-06 19:07:53 +0100

    Use shellexpand to expand ~ in external commands
    Add tests for ~tilde expansion:
    
    - test that "~" is expanded (no more "~" in output)
    - ensure that "1~1" is not expanded to "1/home/user1" as it was
      before
    
    Fixes #972
    
    Note: the first test does not check the literal expansion because
    the path on Windows is expanded as a Linux path, but the correct
    expansion may come for free once `shellexpand` will use the `dirs`
    crate too (https://github.com/netvl/shellexpand/issues/3).

--- tests/external_tests.rs
+++ tests/external_tests.rs
@@ -1,5 +1,7 @@
 mod helpers;
 
+use helpers::Playground;
+
 #[test]
 fn external_command() {
     let actual = nu!(
@@ -8,4 +10,33 @@ fn external_command() {
     );
 
     assert!(actual.contains("1"));
+}
+
+#[test]
+fn spawn_external_process_with_home_in_arguments() {
+    Playground::setup("echo_tilde", |dirs, _| {
+        let actual = nu!(
+            cwd: dirs.test(),
+            r#"
+                sh -c "echo ~"
+            "#
+        );
+
+        assert!(
+            !actual.contains("~"),
+            format!("'{}' should not contain ~", actual)
+        );
+    })
+}
+
+#[test]
+fn spawn_external_process_with_tilde_in_arguments() {
+    let actual = nu!(
+        cwd: "tests/fixtures",
+        r#"
+            sh -c "echo 1~1"
+        "#
+    );
+
+    assert_eq!(actual, "1~1");
 }
