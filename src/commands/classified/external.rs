2ab5803f0031ed6afe6f85cfecf7757c92ccb064
Koenraad Verheyden <mail@koenraadverheyden.com>
2020-01-13 17:41:18 +0100

    $it: add conversion from Path for external commands (#1210)
    * $it: add conversion from Path for external commands (#1203)
    
    * Replace PathBuf::to_str with to_string_lossy

--- src/commands/classified/external.rs
+++ src/commands/classified/external.rs
@@ -88,6 +88,10 @@ async fn run_with_iterator_arg(
                 value: UntaggedValue::Primitive(Primitive::Line(s)),
                 ..
             } => Ok(s.clone()),
+            Value {
+                value: UntaggedValue::Primitive(Primitive::Path(p)),
+                ..
+            } => Ok(p.to_string_lossy().to_string()),
             _ => {
                 let arg = args.iter().find(|arg| arg.contains("$it"));
                 if let Some(arg) = arg {
