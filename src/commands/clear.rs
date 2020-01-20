90360c2cb5e0b1f606ed56c1f5c4f18fb98a90f4
Sean Hellum <seanhellum45@gmail.com>
2020-01-20 00:45:37 +0000

    update

--- src/commands/clear.rs
+++ src/commands/clear.rs
@@ -1,5 +1,9 @@
 use crate::commands::WholeStreamCommand;
-use nu_protocol::Signature;
+use crate::prelude::*;
+use indexmap::IndexMap;
+use nu_errors::ShellError;
+use nu_protocol::{Dictionary, Signature, UntaggedValue};
+
 pub struct Clear;
 
 impl WholeStreamCommand for Clear {
@@ -12,4 +16,24 @@ impl WholeStreamCommand for Clear {
     fn usage(&self) -> &str {
         "clears the terminal"
     }
+    fn run(
+        &self,
+        args: CommandArgs,
+        registry: &CommandRegistry,
+    ) -> Result<OutputStream, ShellError> {
+        clear(args, registry)
+    }
+}
+pub fn clear(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputStream, ShellError> {
+    let args = args.evaluate_once(registry)?;
+    let tag = args.call_info.name_tag.clone();
+
+    let mut indexmap = IndexMap::new();
+    indexmap.insert(
+        "clear".to_string(),
+        UntaggedValue::string(clap::crate_version!()).into_value(&tag),
+    );
+
+    let value = UntaggedValue::Row(Dictionary::from(indexmap)).into_value(&tag);
+    Ok(OutputStream::one(value))
 }
