cb5d963d62fff7881599918b09c0f8b28a3c9b30
Amanita-Muscaria <nope>
2020-01-27 15:35:36 -0600

    Add a name_only flag to dir_entry_dict
    Add name_only flag to indicate if the caller only cares about filenames
    or wants the whole path

--- src/data/files.rs
+++ src/data/files.rs
@@ -8,10 +8,23 @@ pub(crate) fn dir_entry_dict(
     tag: impl Into<Tag>,
     full: bool,
     with_symlink_targets: bool,
-    short_name: bool,
+    name_only: bool,
 ) -> Result<Value, ShellError> {
-    let mut dict = TaggedDictBuilder::new(tag);
-    dict.insert_untagged("name", UntaggedValue::string(filename.to_string_lossy()));
+    let tag = tag.into();
+    let mut dict = TaggedDictBuilder::new(&tag);
+
+    let name = if name_only {
+        filename.file_name().and_then(|s| s.to_str())
+    } else {
+        filename.to_str()
+    }
+    .ok_or(ShellError::labeled_error(
+        format!("Invalid File name: {:}", filename.to_string_lossy()),
+        "Invalid File Name",
+        tag,
+    ))?;
+
+    dict.insert_untagged("name", UntaggedValue::string(name));
 
     if metadata.is_dir() {
         dict.insert_untagged("type", UntaggedValue::string("Dir"));
