eaa027bca3fffbb5c6e4e496d2acac596aa8aaa3
Sandorex <rzhw3h@gmail.com>
2020-02-01 18:12:24 +0100

    Added quiet option, supports variable number of ids

--- src/commands/kill.rs
+++ src/commands/kill.rs
@@ -3,14 +3,16 @@ use crate::prelude::*;
 use nu_errors::ShellError;
 use nu_protocol::{Signature, SyntaxShape};
 use nu_source::Tagged;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 pub struct Kill;
 
 #[derive(Deserialize)]
 pub struct KillArgs {
-    pub pid: Tagged<i64>,
+    pub pid: Tagged<u64>,
+    pub rest: Vec<Tagged<u64>>,
     pub force: Tagged<bool>,
+    pub quiet: Tagged<bool>,
 }
 
 impl WholeStreamCommand for Kill {
@@ -20,8 +22,14 @@ impl WholeStreamCommand for Kill {
 
     fn signature(&self) -> Signature {
         Signature::build("kill")
-            .required("pid", SyntaxShape::Int, "id of the process to be killed")
+            .required(
+                "pid",
+                SyntaxShape::Int,
+                "process id of process that is to be killed",
+            )
+            .rest(SyntaxShape::Int, "rest of processes to kill")
             .switch("force", "forcefully kill the process")
+            .switch("quiet", "won't print anything to the console")
     }
 
     fn usage(&self) -> &str {
@@ -38,10 +46,15 @@ impl WholeStreamCommand for Kill {
 }
 
 fn kill(
-    KillArgs { pid, force }: KillArgs,
+    KillArgs {
+        pid,
+        rest,
+        force,
+        quiet,
+    }: KillArgs,
     _context: RunnableContext,
 ) -> Result<OutputStream, ShellError> {
-    if cfg!(windows) {
+    let mut cmd = if cfg!(windows) {
         let mut cmd = Command::new("cmd");
 
         cmd.arg("/C");
@@ -51,26 +64,45 @@ fn kill(
             cmd.arg("/F");
         }
 
-        // TODO try to avoid repeating this code
+        cmd.arg("/PID");
+        cmd.arg(pid.item().to_string());
 
-        // '/PID' must prefix every PID
-        cmd.arg("/PID")
-            .arg(pid.item().to_string())
-            .status()
-            .expect("failed to execute process");
+        // each pid must written as `/PID 0` otherwise
+        // taskkill will act as `killall` unix command
+        for id in &rest {
+            cmd.arg("/PID");
+            cmd.arg(id.item().to_string());
+        }
+
+        cmd
     } else if cfg!(unix) {
         let mut cmd = Command::new("/bin/sh");
 
-        cmd.arg("-c").arg("kill");
+        cmd.arg("-c");
+        cmd.arg("kill");
 
         if *force {
             cmd.arg("-9");
         }
 
-        cmd.arg(pid.item().to_string())
-            .status()
-            .expect("failed to execute process");
+        cmd.arg(pid.item().to_string());
+
+        cmd.args(rest.iter().map(move |id| id.item().to_string()));
+
+        cmd
+    } else {
+        // rust complains if else branch doesn't exist
+        unreachable!("unsupported platform");
+    };
+
+    // pipe everything to null
+    if *quiet {
+        cmd.stdin(Stdio::null())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null());
     }
+
+    cmd.status().expect("failed to execute shell command");
 
     Ok(OutputStream::empty())
 }
