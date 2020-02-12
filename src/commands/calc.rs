2047947d08491101ee89aaba82f87499cd03819b
Shaurya Shubham <shauryashubham1234567890@gmail.com>
2020-02-12 14:45:18 +0530

    Improved error message in calc

--- src/commands/calc.rs
+++ src/commands/calc.rs
@@ -54,7 +54,7 @@ pub fn parse(math_expression: &str, tag: impl Into<Tag>) -> Result<Value, String
     match num {
         Ok(num) => {
             if num == f64::INFINITY || num == f64::NEG_INFINITY {
-                return Err(String::from("Cannot divide by zero"));
+                return Err(String::from("cannot represent result"));
             }
             Ok(UntaggedValue::from(Primitive::from(num)).into_value(tag))
         }
