c1bec3b44349fef6ab08ba165e0d1fa5ddd1db9b
Shaurya Shubham <shauryashubham1234567890@gmail.com>
2020-02-12 19:08:04 +0530

    Return error on a divide by zero (#1376)
    Return error on a divide by zero

--- src/commands/calc.rs
+++ src/commands/calc.rs
@@ -49,9 +49,15 @@ fn calc(input: Value, args: &RawCommandArgs) -> Result<OutputStream, ShellError>
 }
 
 pub fn parse(math_expression: &str, tag: impl Into<Tag>) -> Result<Value, String> {
+    use std::f64;
     let num = meval::eval_str(math_expression);
     match num {
-        Ok(num) => Ok(UntaggedValue::from(Primitive::from(num)).into_value(tag)),
+        Ok(num) => {
+            if num == f64::INFINITY || num == f64::NEG_INFINITY {
+                return Err(String::from("cannot represent result"));
+            }
+            Ok(UntaggedValue::from(Primitive::from(num)).into_value(tag))
+        }
         Err(error) => Err(error.to_string()),
     }
 }
