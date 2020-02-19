55c5f3a441d52b3ebafd57a7e92e8826ab205c71
Paul Woolcock <pwoolcock@tenable.com>
2020-02-18 23:01:20 -0500

    Implements --get, --set, and --remove for `env`

--- src/commands/env.rs
+++ src/commands/env.rs
@@ -2,12 +2,23 @@ use crate::cli::History;
 use crate::data::config;
 use crate::prelude::*;
 use nu_errors::ShellError;
-use nu_protocol::{Dictionary, Signature, TaggedDictBuilder, UntaggedValue, Value};
+use nu_protocol::{
+    Dictionary, Primitive, ReturnSuccess, Signature, SyntaxShape, TaggedDictBuilder, UntaggedValue,
+    Value,
+};
+use nu_source::Tagged;
 
 use crate::commands::WholeStreamCommand;
 use indexmap::IndexMap;
 
 pub struct Env;
+
+#[derive(Deserialize)]
+pub struct EnvArgs {
+    set: Option<(Tagged<String>, Value)>,
+    get: Option<Tagged<String>>,
+    remove: Option<Tagged<String>>,
+}
 
 impl WholeStreamCommand for Env {
     fn name(&self) -> &str {
@@ -16,6 +27,24 @@ impl WholeStreamCommand for Env {
 
     fn signature(&self) -> Signature {
         Signature::build("env")
+            .named(
+                "set",
+                SyntaxShape::Any,
+                "set a value in the environment, eg) --set [key value]",
+                Some('s'),
+            )
+            .named(
+                "get",
+                SyntaxShape::String,
+                "get a value from the environment",
+                Some('g'),
+            )
+            .named(
+                "remove",
+                SyntaxShape::String,
+                "remove a value from the environment",
+                Some('d'),
+            )
     }
 
     fn usage(&self) -> &str {
@@ -27,7 +56,7 @@ impl WholeStreamCommand for Env {
         args: CommandArgs,
         registry: &CommandRegistry,
     ) -> Result<OutputStream, ShellError> {
-        env(args, registry)
+        args.process(registry, env)?.run()
     }
 }
 
@@ -76,16 +105,44 @@ pub fn get_environment(tag: Tag) -> Result<Value, Box<dyn std::error::Error>> {
     Ok(UntaggedValue::Row(Dictionary::from(indexmap)).into_value(&tag))
 }
 
-pub fn env(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputStream, ShellError> {
-    let args = args.evaluate_once(registry)?;
+pub fn env(
+    EnvArgs { get, set, remove }: EnvArgs,
+    RunnableContext { name, .. }: RunnableContext,
+) -> Result<OutputStream, ShellError> {
+    if let Some(key) = get {
+        match std::env::var(&key.item) {
+            Ok(val) => Ok(OutputStream::one(ReturnSuccess::value(val))),
+            Err(err) => Err(ShellError::labeled_error(
+                "Variable doesn't exist",
+                err.to_string(),
+                &key.tag,
+            )),
+        }
+    } else if let Some((key, value)) = set {
+        match value.value {
+            UntaggedValue::Primitive(Primitive::String(ref s)) => {
+                std::env::set_var(&key.item, s);
+            }
+            _ => {
+                return Err(ShellError::labeled_error(
+                    "Expected a string",
+                    "requires string input",
+                    &key.tag,
+                ));
+            }
+        }
+        Ok(OutputStream::empty())
+    } else if let Some(key) = remove {
+        std::env::remove_var(&key.item);
+        Ok(OutputStream::empty())
+    } else {
+        let mut env_out = VecDeque::new();
 
-    let mut env_out = VecDeque::new();
-    let tag = args.call_info.name_tag.clone();
+        let value = get_environment(name)?;
+        env_out.push_back(value);
 
-    let value = get_environment(tag)?;
-    env_out.push_back(value);
+        let env_out = futures::stream::iter(env_out);
 
-    let env_out = futures::stream::iter(env_out);
-
-    Ok(env_out.to_output_stream())
+        Ok(env_out.to_output_stream())
+    }
 }
