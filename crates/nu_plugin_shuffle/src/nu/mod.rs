38fc39de288b9b02dddd2e946716df5298bebd85
Falco Hirschenberger <falco.hirschenberger@gmail.com>
2020-03-04 14:50:28 +0100

    Add `--num` parameter to limit the numer of returned elements

--- crates/nu_plugin_shuffle/src/nu/mod.rs
+++ crates/nu_plugin_shuffle/src/nu/mod.rs
@@ -1,6 +1,8 @@
 use nu_errors::ShellError;
 use nu_plugin::Plugin;
-use nu_protocol::{ReturnValue, Signature, Value};
+use nu_protocol::{
+    CallInfo, ReturnSuccess, ReturnValue, Signature, SyntaxShape, UntaggedValue, Value,
+};
 
 use rand::seq::SliceRandom;
 use rand::thread_rng;
@@ -8,11 +10,21 @@ use rand::thread_rng;
 #[derive(Default)]
 pub struct Shuffle {
     values: Vec<ReturnValue>,
+    limit: Option<u64>,
 }
 
 impl Shuffle {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    pub fn setup(&mut self, call_info: CallInfo) -> ReturnValue {
+        self.limit = if let Some(value) = call_info.args.get("num") {
+            Some(value.as_u64()?)
+        } else {
+            None
+        };
+        ReturnSuccess::value(UntaggedValue::nothing().into_untagged_value())
     }
 }
 
@@ -20,6 +32,12 @@ impl Plugin for Shuffle {
     fn config(&mut self) -> Result<Signature, ShellError> {
         Ok(Signature::build("shuffle")
             .desc("Shuffle input randomly")
+            .named(
+                "num",
+                SyntaxShape::Int,
+                "Limit output to `num` number of values",
+                Some('n'),
+            )
             .filter())
     }
 
@@ -30,7 +48,13 @@ impl Plugin for Shuffle {
 
     fn end_filter(&mut self) -> Result<Vec<ReturnValue>, ShellError> {
         let mut rng = thread_rng();
-        self.values.shuffle(&mut rng);
-        Ok(self.values.clone())
+        if let Some(n) = self.limit {
+            println!("Limited");
+            let (shuffled, _) = self.values.partial_shuffle(&mut rng, n as usize);
+            Ok(shuffled.to_vec())
+        } else {
+            self.values.shuffle(&mut rng);
+            Ok(self.values.clone())
+        }
     }
 }
