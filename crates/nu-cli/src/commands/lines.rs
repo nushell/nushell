45609934ef53e1cae8934e179f69f70074338515
Jonathan Turner <jonathan.d.turner@gmail.com>
2020-03-06 19:52:27 +1300

    Finish fixing lines

--- crates/nu-cli/src/commands/lines.rs
+++ crates/nu-cli/src/commands/lines.rs
@@ -53,7 +53,8 @@ fn lines(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputStream, 
                     let mut st = leftover_string.clone() + &st;
                     leftover.clear();
 
-                    let mut lines: Vec<String> = st.lines().filter(|s| s.trim() != "").map(|x| x.to_string()).collect();
+                    let mut lines: Vec<String> = st.lines().map(|x| x.to_string()).collect();
+
                     if !ends_with_line_ending(&st) {
                         if let Some(last) = lines.pop() {
                             leftover_string = last;
@@ -71,7 +72,7 @@ fn lines(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputStream, 
                     let mut st = leftover_string.clone() + &st;
                     leftover.clear();
 
-                    let mut lines: Vec<String> = st.lines().filter(|s| s.trim() != "").map(|x| x.to_string()).collect();
+                    let mut lines: Vec<String> = st.lines().map(|x| x.to_string()).collect();
                     if !ends_with_line_ending(&st) {
                         if let Some(last) = lines.pop() {
                             leftover_string = last;
@@ -106,48 +107,11 @@ fn lines(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputStream, 
                 }
             }
         }
-        yield futures::stream::iter(vec![ReturnSuccess::value(UntaggedValue::string(leftover_string).into_untagged_value())]);
+        if !leftover_string.is_empty() {
+            yield futures::stream::iter(vec![ReturnSuccess::value(UntaggedValue::string(leftover_string).into_untagged_value())]);
+        }
     }
     .flatten();
-
-    // let stream = input
-    //     .values
-    //     .map(move |v| {
-    //         if let Ok(s) = v.as_string() {
-    //             let split_result: Vec<_> = s.lines().filter(|s| s.trim() != "").collect();
-
-    //             trace!("split result = {:?}", split_result);
-
-    //             let result = split_result
-    //                 .into_iter()
-    //                 .map(|s| {
-    //                     ReturnSuccess::value(
-    //                         UntaggedValue::Primitive(Primitive::Line(s.into()))
-    //                             .into_untagged_value(),
-    //                     )
-    //                 })
-    //                 .collect::<Vec<_>>();
-
-    //             futures::stream::iter(result)
-    //         } else if let Value {
-    //             value: UntaggedValue::Primitive(Primitive::Binary(_)),
-    //             ..
-    //         } = v
-    //         {
-    //             futures::stream::iter(vec![ReturnSuccess::value(v)])
-    //         } else {
-    //             let value_span = v.tag.span;
-
-    //             futures::stream::iter(vec![Err(ShellError::labeled_error_with_secondary(
-    //                 "Expected a string from pipeline",
-    //                 "requires string input",
-    //                 name_span,
-    //                 "value originates from here",
-    //                 value_span,
-    //             ))])
-    //         }
-    //     })
-    //     .flatten();
 
     Ok(stream.to_output_stream())
 }
