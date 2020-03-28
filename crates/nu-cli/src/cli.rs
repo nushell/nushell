fb7d35f7a97e20a19d35f28cfa93fa13c5979424
Sam Hedin <sam.hedin@gmail.com>
2020-03-28 02:41:41 +0100

    Move whole_stream_command(Headers) to more appropriate section

--- crates/nu-cli/src/cli.rs
+++ crates/nu-cli/src/cli.rs
@@ -276,7 +276,6 @@ pub fn create_default_context(
             whole_stream_command(Autoview),
             whole_stream_command(Table),
             // Text manipulation
-            whole_stream_command(Headers),
             whole_stream_command(SplitColumn),
             whole_stream_command(SplitRow),
             whole_stream_command(Lines),
@@ -312,6 +311,7 @@ pub fn create_default_context(
             whole_stream_command(Shuffle),
             whole_stream_command(Wrap),
             whole_stream_command(Pivot),
+            whole_stream_command(Headers),
             // Data processing
             whole_stream_command(Histogram),
             whole_stream_command(Sum),
