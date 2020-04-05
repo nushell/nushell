5c092ca404c7e9596924c3bea17fecc8d17bb73e
Jonathan Turner <jonathan.d.turner@gmail.com>
2020-04-05 12:21:49 +1200

    Turn back on some completions

--- crates/nu-cli/src/shell/completer.rs
+++ crates/nu-cli/src/shell/completer.rs
@@ -27,7 +27,7 @@ impl NuCompleter {
         // // smarter completions
         // let shapes = nu_parser::pipeline_shapes(line, expand_context);
 
-        // let commands: Vec<String> = self.commands.names();
+        let commands: Vec<String> = self.commands.names();
 
         let line_chars: Vec<_> = line[..pos].chars().collect();
 
@@ -39,59 +39,59 @@ impl NuCompleter {
             replace_pos -= 1;
         }
 
-        // let mut completions;
+        let mut completions;
 
         // // See if we're a flag
         // if pos > 0 && replace_pos < line_chars.len() && line_chars[replace_pos] == '-' {
         //     completions = self.get_matching_arguments(&line_chars, line, replace_pos, pos);
         // } else {
-        //     completions = self.file_completer.complete(line, pos, context)?.1;
+        completions = self.file_completer.complete(line, pos, context)?.1;
 
-        //     for completion in &mut completions {
-        //         if completion.replacement.contains("\\ ") {
-        //             completion.replacement = completion.replacement.replace("\\ ", " ");
-        //         }
-        //         if completion.replacement.contains("\\(") {
-        //             completion.replacement = completion.replacement.replace("\\(", "(");
-        //         }
+        for completion in &mut completions {
+            if completion.replacement.contains("\\ ") {
+                completion.replacement = completion.replacement.replace("\\ ", " ");
+            }
+            if completion.replacement.contains("\\(") {
+                completion.replacement = completion.replacement.replace("\\(", "(");
+            }
 
-        //         if completion.replacement.contains(' ') || completion.replacement.contains('(') {
-        //             if !completion.replacement.starts_with('\"') {
-        //                 completion.replacement = format!("\"{}", completion.replacement);
-        //             }
-        //             if !completion.replacement.ends_with('\"') {
-        //                 completion.replacement = format!("{}\"", completion.replacement);
-        //             }
-        //         }
-        //     }
+            if completion.replacement.contains(' ') || completion.replacement.contains('(') {
+                if !completion.replacement.starts_with('\"') {
+                    completion.replacement = format!("\"{}", completion.replacement);
+                }
+                if !completion.replacement.ends_with('\"') {
+                    completion.replacement = format!("{}\"", completion.replacement);
+                }
+            }
+        }
         // };
 
-        // for command in commands.iter() {
-        //     let mut pos = replace_pos;
-        //     let mut matched = true;
-        //     if pos < line_chars.len() {
-        //         for chr in command.chars() {
-        //             if line_chars[pos] != chr {
-        //                 matched = false;
-        //                 break;
-        //             }
-        //             pos += 1;
-        //             if pos == line_chars.len() {
-        //                 break;
-        //             }
-        //         }
-        //     }
+        for command in commands.iter() {
+            let mut pos = replace_pos;
+            let mut matched = true;
+            if pos < line_chars.len() {
+                for chr in command.chars() {
+                    if line_chars[pos] != chr {
+                        matched = false;
+                        break;
+                    }
+                    pos += 1;
+                    if pos == line_chars.len() {
+                        break;
+                    }
+                }
+            }
 
-        //     if matched {
-        //         completions.push(rustyline::completion::Pair {
-        //             display: command.clone(),
-        //             replacement: command.clone(),
-        //         });
-        //     }
-        // }
+            if matched {
+                completions.push(rustyline::completion::Pair {
+                    display: command.clone(),
+                    replacement: command.clone(),
+                });
+            }
+        }
 
-        // Ok((replace_pos, completions))
-        Ok((replace_pos, vec![]))
+        Ok((replace_pos, completions))
+        // Ok((replace_pos, vec![]))
     }
 
     // fn get_matching_arguments(
