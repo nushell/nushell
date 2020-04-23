b207b3cf600b7d82347a73edd6929a15a66c8699
Kevin Del Castillo <quebin31@gmail.com>
2020-04-22 22:41:18 -0500

    feat: pub export of expand_ndots

--- crates/nu-parser/src/lib.rs
+++ crates/nu-parser/src/lib.rs
@@ -8,5 +8,6 @@ mod signature;
 pub use crate::files::Files;
 pub use crate::lite_parse::{lite_parse, LiteBlock};
 pub use crate::parse::{classify_block, garbage};
+pub use crate::path::expand_ndots;
 pub use crate::shapes::shapes;
 pub use crate::signature::{Signature, SignatureRegistry};
