eb91edd22aff54f443d4ee419ea4c2ac7a3b4959
rimathia <rimathia@users.noreply.github.com>
2020-04-26 14:45:48 +0200

    make trim apply to strings contained in tables (also at deeper nesting levels), not just top-level strings

--- crates/nu-cli/src/commands/trim.rs
+++ crates/nu-cli/src/commands/trim.rs
@@ -2,7 +2,7 @@ use crate::commands::WholeStreamCommand;
 
 use crate::prelude::*;
 use nu_errors::ShellError;
-use nu_protocol::{ReturnSuccess, Signature, UntaggedValue};
+use nu_protocol::{Dictionary, Primitive, ReturnSuccess, Signature, UntaggedValue, Value};
 
 pub struct Trim;
 
@@ -28,12 +28,47 @@ impl WholeStreamCommand for Trim {
     }
 }
 
+fn trim_primitive(p: &mut Primitive) {
+    match p {
+        Primitive::String(s) | Primitive::Line(s) => *p = Primitive::String(s.trim().to_string()),
+        Primitive::Nothing
+        | Primitive::Int(_)
+        | Primitive::Decimal(_)
+        | Primitive::Bytes(_)
+        | Primitive::ColumnPath(_)
+        | Primitive::Pattern(_)
+        | Primitive::Boolean(_)
+        | Primitive::Date(_)
+        | Primitive::Duration(_)
+        | Primitive::Range(_)
+        | Primitive::Path(_)
+        | Primitive::Binary(_)
+        | Primitive::BeginningOfStream
+        | Primitive::EndOfStream => (),
+    }
+}
+
+fn trim_row(d: &mut Dictionary) {
+    for (_, mut value) in d.entries.iter_mut() {
+        trim_value(&mut value);
+    }
+}
+
+fn trim_value(v: &mut Value) {
+    match &mut v.value {
+        UntaggedValue::Primitive(p) => trim_primitive(p),
+        UntaggedValue::Row(row) => trim_row(row),
+        _ => (),
+    };
+}
+
 fn trim(args: CommandArgs, _registry: &CommandRegistry) -> Result<OutputStream, ShellError> {
     Ok(args
         .input
-        .map(move |v| {
-            let string = String::extract(&v)?;
-            ReturnSuccess::value(UntaggedValue::string(string.trim()).into_value(v.tag()))
+        .map(|v| {
+            let mut trimmed = v.clone();
+            trim_value(&mut trimmed);
+            ReturnSuccess::value(trimmed)
         })
         .to_output_stream())
 }
