dae91206b319fdda17d1a33f0713987f2c9c89f6
Elton <eltonp3103@gmail.com>
2020-05-04 10:14:24 -0400

    working model for macos is done

--- crates/nu_plugin_start/src/nu/mod.rs
+++ crates/nu_plugin_start/src/nu/mod.rs
@@ -1,7 +1,6 @@
 use nu_errors::ShellError;
 use nu_plugin::Plugin;
 use nu_protocol::{CallInfo, Signature, SyntaxShape, Value};
-use std::process::{Command, Stdio};
 
 use crate::start::Start;
 
@@ -15,34 +14,13 @@ impl Plugin for Start {
                 SyntaxShape::String,
                 "Specifies the application used for opening the files/directories/urls",
                 Some('a'),
-            )
-            .switch(
-                "wait",
-                "Blocks until used applications are closed",
-                Some('w'),
-            )
-            .switch(
-                "background",
-                "Does not bring the application to the foreground",
-                Some('b'),
             ))
     }
 
     fn sink(&mut self, call_info: CallInfo, _input: Vec<Value>) {
-        let filenames = match call_info.args.positional {
-            Some(values) => values
-                .iter()
-                .map(|val| val.as_string())
-                .collect::<Result<Vec<String>, ShellError>>()
-                .unwrap_or(vec![]),
-            None => vec![],
+        match Start::exec(&call_info) {
+            Err(msg) => println!("{}", msg),
+            _ => {}
         };
-
-        Command::new("open")
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .args(&filenames)
-            .spawn()
-            .unwrap();
     }
 }
--- crates/nu_plugin_start/src/start.rs
+++ crates/nu_plugin_start/src/start.rs
@@ -1,1 +1,39 @@
+use nu_protocol::CallInfo;
+use std::process::{Command, Stdio};
+
 pub struct Start;
+
+impl Start {
+    fn filenames(call_info: &CallInfo) -> Vec<String> {
+        match &call_info.args.positional {
+            Some(values) => values
+                .iter()
+                .map(|val| val.as_string())
+                .collect::<Result<Vec<String>, _>>()
+                .unwrap_or(vec![]),
+            None => vec![],
+        }
+    }
+
+    #[cfg(target_os = "macos")]
+    pub fn exec(call_info: &CallInfo) -> Result<(), &str> {
+        // let mut command;
+        let mut args = vec![];
+
+        if let Some(app) = call_info.args.get("application") {
+            match app.as_string() {
+                Ok(name) => args.append(&mut vec![String::from("-a"), name]),
+                Err(_) => return Err("Application name not found"),
+            }
+        }
+
+        args.append(&mut Self::filenames(&call_info));
+        Command::new("open")
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .args(&args)
+            .spawn();
+
+        Ok(())
+    }
+}
