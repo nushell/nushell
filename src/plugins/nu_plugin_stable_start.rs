0c1e439acc8667812fc4540da77e6531b7f3578a
Elton <eltonp3103@gmail.com>
2020-05-09 11:32:09 -0400

    add to stable and fix clippy lint

--- src/plugins/nu_plugin_stable_start.rs
+++ src/plugins/nu_plugin_stable_start.rs
@@ -2,5 +2,8 @@ use nu_plugin::serve_plugin;
 use nu_plugin_start::Start;
 
 fn main() {
-    serve_plugin(&mut Start::new());
+    serve_plugin(&mut Start {
+        filenames: vec![],
+        application: None,
+    });
 }
