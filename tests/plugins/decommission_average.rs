48ee20782fdc73bf4b28a68e7c0de531842a6568
Andrés N. Robalino <andres@androbtech.com>
2020-05-29 03:10:15 -0500

    Ensure `end_filter` plugin lifecycle stage gets called.

---
+++ tests/plugins/decommission_average.rs
@@ -0,0 +1,26 @@
+use nu_test_support::{nu, pipeline};
+
+#[test]
+fn can_average_numbers() {
+    let actual = nu!(
+        cwd: "tests/fixtures/formats", pipeline(
+        r#"
+             open sgml_description.json
+             | get glossary.GlossDiv.GlossList.GlossEntry.Sections
+             | average
+             | echo $it
+         "#
+    ));
+
+    assert_eq!(actual.out, "101.5")
+}
+
+#[test]
+fn can_average_bytes() {
+    let actual = nu!(
+        cwd: "tests/fixtures/formats",
+        "ls | sort-by name | skip 1 | first 2 | get size | average | format \"{$it}\" | echo $it"
+    );
+
+    assert_eq!(actual.out, "1.6 KB");
+}
