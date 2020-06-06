ac9c1286092672d49ea1b7db3c186f47a1c32781
Joseph T Lyons <JosephTLyons@gmail.com>
2020-06-06 01:13:05 -0400

    Revert row.rs code

--- crates/nu-cli/src/commands/split/row.rs
+++ crates/nu-cli/src/commands/split/row.rs
@@ -2,7 +2,7 @@ use crate::commands::WholeStreamCommand;
 use crate::prelude::*;
 use log::trace;
 use nu_errors::ShellError;
-use nu_protocol::{Primitive, Signature, SyntaxShape, UntaggedValue};
+use nu_protocol::{Primitive, ReturnSuccess, Signature, SyntaxShape, UntaggedValue};
 use nu_source::Tagged;
 
 #[derive(Deserialize)]
@@ -35,45 +35,41 @@ impl WholeStreamCommand for SubCommand {
         args: CommandArgs,
         registry: &CommandRegistry,
     ) -> Result<OutputStream, ShellError> {
-        split_row(args, registry).await
+        split_row(args, registry)
     }
 }
 
-async fn split_row(
-    args: CommandArgs,
-    registry: &CommandRegistry,
-) -> Result<OutputStream, ShellError> {
+fn split_row(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputStream, ShellError> {
     let registry = registry.clone();
-    let name = args.call_info.name_tag.clone();
-    let (SplitRowArgs { separator }, mut input) = args.process(&registry).await?;
+    let stream = async_stream! {
+        let name = args.call_info.name_tag.clone();
+        let (SplitRowArgs { separator }, mut input) = args.process(&registry).await?;
+        while let Some(v) = input.next().await {
+            if let Ok(s) = v.as_string() {
+                let splitter = separator.item.replace("\\n", "\n");
+                trace!("splitting with {:?}", splitter);
+                let split_result: Vec<_> = s.split(&splitter).filter(|s| s.trim() != "").collect();
 
-    let mut values_vec_deque = VecDeque::new();
+                trace!("split result = {:?}", split_result);
 
-    while let Some(v) = input.next().await {
-        if let Ok(s) = v.as_string() {
-            let splitter = separator.item.replace("\\n", "\n");
-            trace!("splitting with {:?}", splitter);
-            let split_result: Vec<_> = s.split(&splitter).filter(|s| s.trim() != "").collect();
-
-            trace!("split result = {:?}", split_result);
-
-            for s in split_result {
-                values_vec_deque.push_back(
-                    UntaggedValue::Primitive(Primitive::String(s.into())).into_value(&v.tag),
-                );
+                for s in split_result {
+                    yield ReturnSuccess::value(
+                        UntaggedValue::Primitive(Primitive::String(s.into())).into_value(&v.tag),
+                    );
+                }
+            } else {
+                yield Err(ShellError::labeled_error_with_secondary(
+                    "Expected a string from pipeline",
+                    "requires string input",
+                    name.span,
+                    "value originates from here",
+                    v.tag.span,
+                ));
             }
-        } else {
-            return Err(ShellError::labeled_error_with_secondary(
-                "Expected a string from pipeline",
-                "requires string input",
-                name.span,
-                "value originates from here",
-                v.tag.span,
-            ));
         }
-    }
+    };
 
-    Ok(futures::stream::iter(values_vec_deque).to_output_stream())
+    Ok(stream.to_output_stream())
 }
 
 #[cfg(test)]
