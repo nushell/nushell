52154d4a3abdcc1f7b52299aa26a39b16ffabd47
Joseph T Lyons <JosephTLyons@gmail.com>
2020-06-13 11:39:48 -0400

    Remove async_stream from some commands

--- crates/nu-cli/src/commands/str_/command.rs
+++ crates/nu-cli/src/commands/str_/command.rs
@@ -28,14 +28,11 @@ impl WholeStreamCommand for Command {
         registry: &CommandRegistry,
     ) -> Result<OutputStream, ShellError> {
         let registry = registry.clone();
-        let stream = async_stream! {
-            yield Ok(ReturnSuccess::Value(
-                UntaggedValue::string(crate::commands::help::get_help(&Command, &registry))
-                    .into_value(Tag::unknown()),
-            ));
-        };
 
-        Ok(stream.to_output_stream())
+        Ok(OutputStream::one(ReturnSuccess::value(
+            UntaggedValue::string(crate::commands::help::get_help(&Command, &registry))
+                .into_value(Tag::unknown()),
+        )))
     }
 }
 
