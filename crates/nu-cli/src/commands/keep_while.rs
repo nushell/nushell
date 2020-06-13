d7b1480ad04e5deb43728a69b3bc399a63188044
Jonathan Turner <jonathandturner@users.noreply.github.com>
2020-06-13 16:54:35 -0700

    Another batch of removing async_stream (#1981)

--- crates/nu-cli/src/commands/keep_while.rs
+++ crates/nu-cli/src/commands/keep_while.rs
@@ -3,9 +3,7 @@ use crate::evaluate::evaluate_baseline_expr;
 use crate::prelude::*;
 use log::trace;
 use nu_errors::ShellError;
-use nu_protocol::{
-    hir::ClassifiedCommand, ReturnSuccess, Signature, SyntaxShape, UntaggedValue, Value,
-};
+use nu_protocol::{hir::ClassifiedCommand, Signature, SyntaxShape, UntaggedValue, Value};
 
 pub struct KeepWhile;
 
@@ -34,80 +32,81 @@ impl WholeStreamCommand for KeepWhile {
         args: CommandArgs,
         registry: &CommandRegistry,
     ) -> Result<OutputStream, ShellError> {
-        let registry = registry.clone();
-        let scope = args.call_info.scope.clone();
-        let stream = async_stream! {
-            let mut call_info = args.evaluate_once(&registry).await?;
+        let registry = Arc::new(registry.clone());
+        let scope = Arc::new(args.call_info.scope.clone());
+        let call_info = args.evaluate_once(&registry).await?;
 
-            let block = call_info.args.expect_nth(0)?.clone();
+        let block = call_info.args.expect_nth(0)?.clone();
 
-            let condition = match block {
-                Value {
-                    value: UntaggedValue::Block(block),
-                    tag,
-                } => {
-                    if block.block.len() != 1 {
-                        yield Err(ShellError::labeled_error(
-                            "Expected a condition",
-                            "expected a condition",
-                            tag,
-                        ));
-                        return;
-                    }
-                    match block.block[0].list.get(0) {
-                        Some(item) => match item {
-                            ClassifiedCommand::Expr(expr) => expr.clone(),
-                            _ => {
-                                yield Err(ShellError::labeled_error(
-                                    "Expected a condition",
-                                    "expected a condition",
-                                    tag,
-                                ));
-                                return;
-                            }
-                        },
-                        None => {
-                            yield Err(ShellError::labeled_error(
-                                "Expected a condition",
-                                "expected a condition",
-                                tag,
-                            ));
-                            return;
-                        }
-                    }
-                }
-                Value { tag, .. } => {
-                    yield Err(ShellError::labeled_error(
+        let condition = Arc::new(match block {
+            Value {
+                value: UntaggedValue::Block(block),
+                tag,
+            } => {
+                if block.block.len() != 1 {
+                    return Err(ShellError::labeled_error(
                         "Expected a condition",
                         "expected a condition",
                         tag,
                     ));
-                    return;
                 }
-            };
-
-            while let Some(item) = call_info.input.next().await {
-                let condition = condition.clone();
-                trace!("ITEM = {:?}", item);
-                let result =
-                    evaluate_baseline_expr(&*condition, &registry, &item, &scope.vars, &scope.env)
-                        .await;
-                trace!("RESULT = {:?}", result);
-
-                let return_value = match result {
-                    Ok(ref v) if v.is_true() => true,
-                    _ => false,
-                };
-
-                if return_value {
-                    yield ReturnSuccess::value(item);
-                } else {
-                    break;
+                match block.block[0].list.get(0) {
+                    Some(item) => match item {
+                        ClassifiedCommand::Expr(expr) => expr.clone(),
+                        _ => {
+                            return Err(ShellError::labeled_error(
+                                "Expected a condition",
+                                "expected a condition",
+                                tag,
+                            ));
+                        }
+                    },
+                    None => {
+                        return Err(ShellError::labeled_error(
+                            "Expected a condition",
+                            "expected a condition",
+                            tag,
+                        ));
+                    }
                 }
             }
-        };
+            Value { tag, .. } => {
+                return Err(ShellError::labeled_error(
+                    "Expected a condition",
+                    "expected a condition",
+                    tag,
+                ));
+            }
+        });
 
-        Ok(stream.to_output_stream())
+        Ok(call_info
+            .input
+            .take_while(move |item| {
+                let condition = condition.clone();
+                let registry = registry.clone();
+                let scope = scope.clone();
+                let item = item.clone();
+
+                trace!("ITEM = {:?}", item);
+
+                async move {
+                    let result = evaluate_baseline_expr(
+                        &*condition,
+                        &registry,
+                        &item,
+                        &scope.vars,
+                        &scope.env,
+                    )
+                    .await;
+                    trace!("RESULT = {:?}", result);
+
+                    match result {
+                        Ok(ref v) if v.is_true() => true,
+                        _ => false,
+                    }
+                }
+            })
+            .to_output_stream())
     }
 }
 
--- crates/nu-cli/src/stream/interruptible.rs
+++ crates/nu-cli/src/stream/interruptible.rs
@@ -4,17 +4,17 @@ use std::sync::atomic::{AtomicBool, Ordering};
 
 pub struct InterruptibleStream<V> {
     inner: BoxStream<'static, V>,
-    ctrl_c: Arc<AtomicBool>,
+    interrupt_signal: Arc<AtomicBool>,
 }
 
 impl<V> InterruptibleStream<V> {
-    pub fn new<S>(inner: S, ctrl_c: Arc<AtomicBool>) -> InterruptibleStream<V>
+    pub fn new<S>(inner: S, interrupt_signal: Arc<AtomicBool>) -> InterruptibleStream<V>
     where
         S: Stream<Item = V> + Send + 'static,
     {
         InterruptibleStream {
             inner: inner.boxed(),
-            ctrl_c,
+            interrupt_signal,
         }
     }
 }
@@ -26,7 +26,7 @@ impl<V> Stream for InterruptibleStream<V> {
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> core::task::Poll<Option<Self::Item>> {
-        if self.ctrl_c.load(Ordering::SeqCst) {
+        if self.interrupt_signal.load(Ordering::SeqCst) {
             Poll::Ready(None)
         } else {
             Stream::poll_next(std::pin::Pin::new(&mut self.inner), cx)
