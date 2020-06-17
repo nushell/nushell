233d864f47732fa21f94491617a8c780809df035
Sam Hedin <sam.hedin@gmail.com>
2020-06-17 14:06:10 +0200

    Restore overwritten vals code

--- crates/nu-cli/src/env/environment.rs
+++ crates/nu-cli/src/env/environment.rs
@@ -64,12 +64,12 @@ impl Environment {
             self.remove_env(&k);
         });
 
-        // self.direnv
-        //     .overwritten_values_to_restore()?
-        //     .iter()
-        //     .for_each(|(k, v)| {
-        //         self.add_env(&k, &v.to_string_lossy(), true);
-        //     });
+        self.direnv
+            .overwritten_values_to_restore()?
+            .iter()
+            .for_each(|(k, v)| {
+                self.add_env(&k, &v.to_string_lossy(), true);
+            });
 
         self.direnv.env_vars_to_add()?.iter().for_each(|(k, v)| {
             // std::env::set_var(k, v);
