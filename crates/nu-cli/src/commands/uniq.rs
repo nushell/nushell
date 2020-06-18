5fc0a641cf5c4eea8b7a6ebba23d7d57ec1ed27f
Christoph Siedentop <christoph@siedentop.name>
2020-06-18 00:01:24 -0700

    uniq: Add counting option (WIP!)
    Usage:
    
    fetch https://raw.githubusercontent.com/timbray/topfew/master/test/data/access-1k | lines | wrap item | uniq | sort-by count | last 10

--- crates/nu-cli/src/commands/uniq.rs
+++ crates/nu-cli/src/commands/uniq.rs
@@ -1,9 +1,11 @@
 use crate::commands::WholeStreamCommand;
 use crate::context::CommandRegistry;
 use crate::prelude::*;
-use indexmap::set::IndexSet;
+use indexmap::map::IndexMap;
 use nu_errors::ShellError;
-use nu_protocol::{ReturnSuccess, Signature};
+use nu_protocol::Signature;
+
+use num_bigint::ToBigUint;
 
 pub struct Uniq;
 
@@ -32,15 +34,33 @@ impl WholeStreamCommand for Uniq {
 
 async fn uniq(args: CommandArgs, _registry: &CommandRegistry) -> Result<OutputStream, ShellError> {
     let input = args.input;
-    let uniq_values: IndexSet<_> = input.collect().await;
+    let uniq_values = {
+        let mut counter = IndexMap::<nu_protocol::Value, usize>::new();
+        for line in input.into_vec().await {
+            // TODO: Is there a way to collect and await at the end of the loop? (input.map() failed)
+            *counter.entry(line).or_insert(0) += 1;
+        }
+        counter
+    };
 
-    let mut values_vec_deque = VecDeque::new();
+    let mut values_vec_deque = VecDeque::<nu_protocol::Value>::new();
 
-    for item in uniq_values
-        .iter()
-        .map(|row| ReturnSuccess::value(row.clone()))
-    {
-        values_vec_deque.push_back(item);
+    for item in uniq_values {
+        use nu_protocol::{UntaggedValue, Value};
+        let value = match item.0.value {
+            UntaggedValue::Row(mut row) => {
+                row.entries.insert(
+                    "count".to_string(),
+                    UntaggedValue::int(item.1.to_biguint().unwrap()).into_untagged_value(),
+                );
+                Value {
+                    value: UntaggedValue::Row(row),
+                    tag: item.0.tag,
+                }
+            }
+            _ => panic!("Could not match: {:#?}", item),
+        };
+        values_vec_deque.push_back(value);
     }
 
     Ok(futures::stream::iter(values_vec_deque).to_output_stream())
