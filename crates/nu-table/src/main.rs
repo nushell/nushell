60278ff7aadbf48946b0a36ffa8622bd837811e5
Jonathan Turner <jonathan.d.turner@gmail.com>
2020-06-21 08:15:24 +1200

    Fix carriage returns in cells

--- crates/nu-table/src/main.rs
+++ crates/nu-table/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
         vec![
             StyledString::new("Test me".to_owned(), TextStyle::default_header()),
             StyledString::new(
-                "Long column \n name with carriage returns on on ononoohoh honasdf am\n checkit"
+                "Long column \n name with carriage returns and a lot of text\n check it out"
                     .to_owned(),
                 TextStyle::default_header(),
             ),
