a6d4501b7d8ee3cdabd4382f0b0c21319208339e
Darren Schroeder <fdncred@hotmail.com>
2020-06-24 16:32:10 -0500

    Made starship usage configurable

--- crates/nu-cli/src/cli.rs
+++ crates/nu-cli/src/cli.rs
@@ -4,13 +4,11 @@ use crate::commands::plugin::JsonRpc;
 use crate::commands::plugin::{PluginCommand, PluginSink};
 use crate::commands::whole_stream_command;
 use crate::context::Context;
-#[cfg(not(feature = "starship-prompt"))]
 use crate::git::current_branch;
 use crate::path::canonicalize;
 use crate::prelude::*;
 use crate::EnvironmentSyncer;
 use futures_codec::FramedRead;
-
 use nu_errors::ShellError;
 use nu_protocol::hir::{ClassifiedCommand, Expression, InternalCommand, Literal, NamedArguments};
 use nu_protocol::{Primitive, ReturnSuccess, Signature, UntaggedValue, Value};
@@ -584,6 +582,15 @@ pub async fn cli(
 
         rl.set_helper(Some(crate::shell::Helper::new(context.clone())));
 
+        let config = config::config(Tag::unknown())?;
+        let use_starship = match config.get("use_starship") {
+            Some(b) => match b.as_bool() {
+                Ok(b) => b,
+                _ => false,
+            },
+            _ => false,
+        };
+
         let edit_mode = config::config(Tag::unknown())?
             .get("edit_mode")
             .map(|s| match s.value.expect_string() {
@@ -621,8 +628,7 @@ pub async fn cli(
         rl.set_completion_type(completion_mode);
 
         let colored_prompt = {
-            #[cfg(feature = "starship-prompt")]
-            {
+            if use_starship {
                 std::env::set_var("STARSHIP_SHELL", "");
                 std::env::set_var("PWD", &cwd);
                 let mut starship_context =
@@ -638,9 +644,7 @@ pub async fn cli(
                     _ => {}
                 };
                 starship::print::get_prompt(starship_context)
-            }
-            #[cfg(not(feature = "starship-prompt"))]
-            {
+            } else {
                 format!(
                     "\x1b[32m{}{}\x1b[m> ",
                     cwd,
