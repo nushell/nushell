03b4759a4d53010599c63798ef14b948dea5cc6e
Joseph T Lyons <JosephTLyons@gmail.com>
2020-06-26 00:22:46 -0400

    Add bias flag to `random bool`

--- crates/nu-cli/src/commands/random/bool.rs
+++ crates/nu-cli/src/commands/random/bool.rs
@@ -1,10 +1,16 @@
 use crate::commands::WholeStreamCommand;
 use crate::prelude::*;
 use nu_errors::ShellError;
-use nu_protocol::{ReturnSuccess, Signature, UntaggedValue};
+use nu_protocol::{ReturnSuccess, Signature, SyntaxShape, UntaggedValue};
+use nu_source::Tagged;
 use rand::prelude::{thread_rng, Rng};
 
 pub struct SubCommand;
+
+#[derive(Deserialize)]
+pub struct BoolArgs {
+    bias: Option<Tagged<f64>>,
+}
 
 #[async_trait]
 impl WholeStreamCommand for SubCommand {
@@ -13,7 +19,12 @@ impl WholeStreamCommand for SubCommand {
     }
 
     fn signature(&self) -> Signature {
-        Signature::build("random bool")
+        Signature::build("random bool").named(
+            "bias",
+            SyntaxShape::Number,
+            "Adjusts the probability of a \"true\" outcome",
+            Some('b'),
+        )
     }
 
     fn usage(&self) -> &str {
@@ -29,20 +40,45 @@ impl WholeStreamCommand for SubCommand {
     }
 
     fn examples(&self) -> Vec<Example> {
-        vec![Example {
-            description: "Generate a random boolean value",
-            example: "random bool",
-            result: None,
-        }]
+        vec![
+            Example {
+                description: "Generate a random boolean value",
+                example: "random bool",
+                result: None,
+            },
+            Example {
+                description: "Generate a random boolean value with a 75% chance of \"true\"",
+                example: "random bool --bias 0.75",
+                result: None,
+            },
+        ]
     }
 }
 
-    _args: CommandArgs,
-    _registry: &CommandRegistry,
 pub async fn bool_command(
+    args: CommandArgs,
+    registry: &CommandRegistry,
 ) -> Result<OutputStream, ShellError> {
+    let (BoolArgs { bias }, _) = args.process(&registry).await?;
+
+    let mut probability = 0.5;
+
+    if let Some(prob) = bias {
+        probability = *prob as f64;
+
+        let probability_is_valid = 0.0 <= probability && probability <= 1.0;
+
+        if !probability_is_valid {
+            return Err(ShellError::labeled_error(
+                "The probability is invalid",
+                "invalid probability",
+                prob.span(),
+            ));
+        }
+    }
+
     let mut rng = thread_rng();
-    let bool_result: bool = rng.gen_bool(0.5);
+    let bool_result: bool = rng.gen_bool(probability);
     let bool_untagged_value = UntaggedValue::boolean(bool_result);
 
     Ok(OutputStream::one(ReturnSuccess::value(bool_untagged_value)))
