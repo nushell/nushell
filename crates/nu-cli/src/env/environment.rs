8773f6c9fa61ccdf5407d52fee6faf43a29d0047
Sam Hedin <sam.hedin@gmail.com>
2020-06-30 22:40:34 +0200

    Infinite loop debug

--- crates/nu-cli/src/env/environment.rs
+++ crates/nu-cli/src/env/environment.rs
@@ -60,9 +60,9 @@ impl Environment {
     }
 
     pub fn autoenv(&mut self) -> Result<(), ShellError> {
-        for (k, v) in self.autoenv.env_vars_to_add()? {
-            std::env::set_var(&k, OsString::from(v.to_string_lossy().to_string()));
-        }
+        // for (k, v) in self.autoenv.env_vars_to_add()? {
+        //     std::env::set_var(&k, OsString::from(v.to_string_lossy().to_string()));
+        // }
 
         let cleanup = self.autoenv.cleanup_after_dir_exit()?;
 
