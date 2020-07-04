b746419cb1847f2d5faf7eeae610158cfffe297e
Arash Outadi <arash.out@gmail.com>
2020-07-04 15:04:10 -0700

    add a nicer error message

--- crates/nu-cli/src/shell/filesystem_shell.rs
+++ crates/nu-cli/src/shell/filesystem_shell.rs
@@ -12,9 +12,11 @@ use crate::shell::completer::NuCompleter;
 use crate::shell::shell::Shell;
 use crate::utils::FileStructure;
 
+use log::debug;
 use rustyline::completion::FilenameCompleter;
 use rustyline::hint::{Hinter, HistoryHinter};
 use std::collections::HashMap;
+use std::io::{Error, ErrorKind};
 use std::path::{Path, PathBuf};
 
 #[cfg(unix)]
@@ -587,25 +589,28 @@ impl Shell for FilesystemShell {
                                 .map(|val| val.is_true())
                                 .unwrap_or(false);
                             result = if _trash.item || (rm_always_trash && !_permanent.item) {
-                                trash::remove(&f).map_err(|_| f.to_string_lossy())
+                                trash::remove(&f).map_err(|e: trash::Error| {
+                                    Error::new(ErrorKind::Other, format!("{:?}", e))
+                                })
                             } else if metadata.is_file() {
-                                std::fs::remove_file(&f).map_err(|_| f.to_string_lossy())
+                                std::fs::remove_file(&f)
                             } else {
-                                std::fs::remove_dir_all(&f).map_err(|_| f.to_string_lossy())
+                                std::fs::remove_dir_all(&f)
                             };
                         }
                         #[cfg(not(feature = "trash-support"))]
                         {
                             result = if metadata.is_file() {
-                                std::fs::remove_file(&f).map_err(|_| f.to_string_lossy())
+                                std::fs::remove_file(&f)
                             } else {
-                                std::fs::remove_dir_all(&f).map_err(|_| f.to_string_lossy())
+                                std::fs::remove_dir_all(&f)
                             };
                         }
 
                         if let Err(e) = result {
+                            debug!("{:?}", e.kind());
                             let msg = format!("Could not delete {:}", e);
-                            Err(ShellError::labeled_error(msg, e, tag))
+                            Err(ShellError::labeled_error(msg, e.to_string(), tag))
                         } else {
                             let val = format!("deleted {:}", f.to_string_lossy()).into();
                             Ok(ReturnSuccess::Value(val))
