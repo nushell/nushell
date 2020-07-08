600828bf7cf5cb8a76e14a189e952f2fc7fd5b33
Arash Outadi <aoutadi@infoblox.com>
2020-07-08 12:32:41 -0700

    Remove colour codes

--- docs/generate_summary.sh
+++ docs/generate_summary.sh
@@ -1,11 +1,11 @@
-nu -c "help commands | get name | to md | save _names.md"
+nu -c "help commands | get name | to md | trim | save _names.md"
 
 path="docs/summaries.md"
 echo "" > $path
 while IFS= read -r command; do
     echo "Appending usage for $command"
-    # Keep newlines when using command substition
-    RESULTX="$(nu -c "$command --help"; echo x)"
+    # Keep newlines when using command substition and sed removes colour codes
+    RESULTX="$(nu -c "$command --help" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"; echo x)"
     echo -e "# $command \n" >> $path
     echo -e "${RESULTX%x}" >> $path
 done < "_names.md"
