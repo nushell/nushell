0f688d7da7481c78b54b280661930e0912e1b7b4
Joseph T. Lyons <JosephTLyons@users.noreply.github.com>
2020-07-16 13:39:51 -0400

    Use '?' for error propagation, remove `match` (#2194)

--- crates/nu-cli/src/commands/str_/from.rs
+++ crates/nu-cli/src/commands/str_/from.rs
@@ -100,18 +100,10 @@ async fn operate(
             } else {
                 let mut ret = v;
                 for path in &column_paths {
-                    let swapping = ret.swap_data_by_column_path(
+                    ret = ret.swap_data_by_column_path(
                         path,
                         Box::new(move |old| action(old, old.tag(), digits, group_digits)),
-                    );
-                    match swapping {
-                        Ok(new_value) => {
-                            ret = new_value;
-                        }
-                        Err(err) => {
-                            return Err(err);
-                        }
-                    }
+                    )?;
                 }
 
                 ReturnSuccess::value(ret)
