ed962f243e8f02f3dbb0eec862f741bde2455e4b
Joseph T Lyons <JosephTLyons@gmail.com>
2020-08-02 15:25:59 -0400

    Use `ok_or_else()`

--- crates/nu-errors/src/lib.rs
+++ crates/nu-errors/src/lib.rs
@@ -893,7 +893,7 @@ macro_rules! ranged_int {
 
         impl CoerceInto<$ty> for nu_source::Tagged<BigInt> {
             fn coerce_into(self, operation: impl Into<String>) -> Result<$ty, ShellError> {
-                self.$op().ok_or(ShellError::range_error(
+                self.$op().ok_or_else(||ShellError::range_error(
                     $ty::to_expected_range(),
                     &self.item.spanned(self.tag.span),
                     operation.into(),
@@ -903,7 +903,7 @@ macro_rules! ranged_int {
 
         impl CoerceInto<$ty> for nu_source::Tagged<&BigInt> {
             fn coerce_into(self, operation: impl Into<String>) -> Result<$ty, ShellError> {
-                self.$op().ok_or(ShellError::range_error(
+                self.$op().ok_or_else(||ShellError::range_error(
                     $ty::to_expected_range(),
                     &self.item.spanned(self.tag.span),
                     operation.into(),
@@ -932,7 +932,7 @@ macro_rules! ranged_decimal {
 
         impl CoerceInto<$ty> for nu_source::Tagged<BigDecimal> {
             fn coerce_into(self, operation: impl Into<String>) -> Result<$ty, ShellError> {
-                self.$op().ok_or(ShellError::range_error(
+                self.$op().ok_or_else(||ShellError::range_error(
                     $ty::to_expected_range(),
                     &self.item.spanned(self.tag.span),
                     operation.into(),
@@ -942,7 +942,7 @@ macro_rules! ranged_decimal {
 
         impl CoerceInto<$ty> for nu_source::Tagged<&BigDecimal> {
             fn coerce_into(self, operation: impl Into<String>) -> Result<$ty, ShellError> {
-                self.$op().ok_or(ShellError::range_error(
+                self.$op().ok_or_else(||ShellError::range_error(
                     $ty::to_expected_range(),
                     &self.item.spanned(self.tag.span),
                     operation.into(),
