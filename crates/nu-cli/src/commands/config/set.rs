48850becd8270bb653a6681de8e4eec00f4c157a
Jonathan Turner <jonathandturner@users.noreply.github.com>
2020-08-22 17:06:19 +1200

    Add some minor fixes (#2391)

--- crates/nu-cli/src/commands/config/set.rs
+++ crates/nu-cli/src/commands/config/set.rs
@@ -39,8 +39,8 @@ impl WholeStreamCommand for SubCommand {
 
     fn examples(&self) -> Vec<Example> {
         vec![Example {
-            description: "Set completion_mode to circular",
-            example: "config set [completion_mode circular]",
+            description: "Set nonzero_exit_errors to true",
+            example: "config set nonzero_exit_errors $true",
             result: None,
         }]
     }
