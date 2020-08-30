65c07c6700ba9135f8519740cac58d6064579271
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2020-08-30 13:25:38 -0500

    cleaned up some comments

--- crates/nu-cli/src/commands/table/options.rs
+++ crates/nu-cli/src/commands/table/options.rs
@@ -6,35 +6,10 @@ use nu_table::TextStyle;
 use std::fmt::Debug;
 
 pub trait ConfigExtensions: Debug + Send {
-    // fn header_alignment(&self) -> nu_table::Alignment;
-    // fn header_color(&self) -> Option<ansi_term::Color>;
-    // fn header_bold(&self) -> bool;
     fn table_mode(&self) -> nu_table::Theme;
     fn disabled_indexes(&self) -> bool;
-    // fn text_color(&self) -> Option<ansi_term::Color>;
-    // fn line_color(&self) -> Option<ansi_term::Color>;
     fn header_style(&self) -> TextStyle;
 }
-
-// pub fn header_alignment(config: &NuConfig) -> nu_table::Alignment {
-//     let vars = config.vars.lock();
-
-//     let alignment = vars.get("header_align");
-
-//     if alignment.is_none() {
-//         return nu_table::Alignment::Center;
-//     }
-
-//     alignment.map_or(nu_table::Alignment::Left, |a| {
-//         a.as_string().map_or(nu_table::Alignment::Center, |a| {
-//             match a.to_lowercase().as_str() {
-//                 "center" | "c" => nu_table::Alignment::Center,
-//                 "right" | "r" => nu_table::Alignment::Right,
-//                 _ => nu_table::Alignment::Center,
-//             }
-//         })
-//     })
-// }
 
 pub fn header_alignment_from_value(align_value: Option<&Value>) -> nu_table::Alignment {
     match align_value {
@@ -52,50 +27,6 @@ pub fn header_alignment_from_value(align_value: Option<&Value>) -> nu_table::Ali
     }
 }
 
-// pub fn get_color_for_config_key(config: &NuConfig, key: &str) -> Option<TextStyle> {
-//     let vars = config.vars.lock();
-
-//     Some(match vars.get(key) {
-//         Some(c) => match c.as_string() {
-//             Ok(color) => match color.to_lowercase().as_str() {
-//                 "g" | "green" => TextStyle::new().fg(ansi_term::Color::Green),
-//                 "r" | "red" => TextStyle::new().fg(ansi_term::Color::Red),
-//                 "u" | "blue" => TextStyle::new().fg(ansi_term::Color::Blue),
-//                 "b" | "black" => TextStyle::new().fg(ansi_term::Color::Black),
-//                 "y" | "yellow" => TextStyle::new().fg(ansi_term::Color::Yellow),
-//                 "p" | "purple" => TextStyle::new().fg(ansi_term::Color::Purple),
-//                 "c" | "cyan" => TextStyle::new().fg(ansi_term::Color::Cyan),
-//                 "w" | "white" => TextStyle::new().fg(ansi_term::Color::White),
-//                 _ => TextStyle::new().fg(ansi_term::Color::Green),
-//             },
-//             _ => TextStyle::new().fg(ansi_term::Color::Green),
-//         },
-//         _ => TextStyle::new().fg(ansi_term::Color::Green),
-//     })
-// }
-
-// pub fn get_color_for_config_key(config: &NuConfig, key: &str) -> Option<ansi_term::Color> {
-//     let vars = config.vars.lock();
-
-//     Some(match vars.get(key) {
-//         Some(c) => match c.as_string() {
-//             Ok(color) => match color.to_lowercase().as_str() {
-//                 "g" | "green" => ansi_term::Color::Green,
-//                 "r" | "red" => ansi_term::Color::Red,
-//                 "u" | "blue" => ansi_term::Color::Blue,
-//                 "b" | "black" => ansi_term::Color::Black,
-//                 "y" | "yellow" => ansi_term::Color::Yellow,
-//                 "p" | "purple" => ansi_term::Color::Purple,
-//                 "c" | "cyan" => ansi_term::Color::Cyan,
-//                 "w" | "white" => ansi_term::Color::White,
-//                 _ => ansi_term::Color::Green,
-//             },
-//             _ => ansi_term::Color::Green,
-//         },
-//         _ => ansi_term::Color::Green,
-//     })
-// }
-
 pub fn get_color_from_key_and_subkey(config: &NuConfig, key: &str, subkey: &str) -> Value {
     let vars = config.vars.lock();
 
@@ -112,14 +43,6 @@ pub fn get_color_from_key_and_subkey(config: &NuConfig, key: &str, subkey: &str)
 
     v
 }
-
-// pub fn header_bold(config: &NuConfig) -> bool {
-//     let vars = config.vars.lock();
-
-//     vars.get("header_bold")
-//         .map(|x| x.as_bool().unwrap_or(true))
-//         .unwrap_or(true)
-// }
 
 pub fn header_bold_from_value(bold_value: Option<&Value>) -> bool {
     bold_value
@@ -148,23 +71,8 @@ pub fn disabled_indexes(config: &NuConfig) -> bool {
 }
 
 impl ConfigExtensions for NuConfig {
-    // fn header_alignment(&self) -> nu_table::Alignment {
-    //     header_alignment(self)
-    // }
-
-    // fn header_color(&self) -> Option<ansi_term::Color> {
-    //     get_color_for_config_key(self, "header_color")
-    // }
-
-    // fn text_color(&self) -> Option<ansi_term::Color> {
-    //     get_color_for_config_key(self, "text_color")
-    // }
-
-    // fn line_color(&self) -> Option<ansi_term::Color> {
-    //     get_color_for_config_key(self, "line_color")
-    // }
-
     fn header_style(&self) -> TextStyle {
+        // FIXME: I agree, this is the long way around, please suggest and alternative.
         let head_color = get_color_from_key_and_subkey(self, "color_config", "header_color");
         let head_color_style = lookup_ansi_color_style(
             head_color
@@ -185,10 +93,6 @@ impl ConfigExtensions for NuConfig {
                 .foreground
                 .unwrap_or(ansi_term::Color::Green))
     }
-
-    // fn header_bold(&self) -> bool {
-    //     header_bold(self)
-    // }
 
     fn table_mode(&self) -> nu_table::Theme {
         table_mode(self)
