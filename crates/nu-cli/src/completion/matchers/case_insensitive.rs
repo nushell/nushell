12103018a93a835d86d14a6d4c9ef79270d9bd16
rezural <69941255+rezural@users.noreply.github.com>
2020-09-09 19:22:14 +1000

    Update crates/nu-cli/src/completion/matchers/case_insensitive.rs
    Co-authored-by: Jason Gedge <jason.gedge@shopify.com>

--- crates/nu-cli/src/completion/matchers/case_insensitive.rs
+++ crates/nu-cli/src/completion/matchers/case_insensitive.rs
@@ -7,6 +7,6 @@ impl matchers::Matcher for Matcher {
         let from_u_substring: UniCase<&str> =
             UniCase::new(from[0..std::cmp::min(from.len(), partial.len())].into());
         let partial_u: UniCase<&str> = UniCase::new(partial.into());
-        return from_u_substring == partial_u;
+        from_u_substring == partial_u
     }
 }
