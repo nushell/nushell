889ef8b87f2b4f71133be4d2b7a73b52113014c7
gorogoroumaru <zokutyou2@gmail.com>
2020-09-12 09:53:33 +0900

    applied requested change for avg.rs

--- crates/nu-cli/src/commands/math/avg.rs
+++ crates/nu-cli/src/commands/math/avg.rs
@@ -70,16 +70,6 @@ fn to_byte(value: &Value) -> Option<Value> {
     }
 }
 
-fn to_duration(value: &Value) -> Option<Value> {
-    match &value.value {
-        UntaggedValue::Primitive(Primitive::Int(duration)) => Some(
-            UntaggedValue::Primitive(Primitive::Duration((*duration).clone()))
-                .into_untagged_value(),
-        ),
-        _ => None,
-    }
-}
-
 pub fn average(values: &[Value], name: &Tag) -> Result<Value, ShellError> {
     let sum = reducer_for(Reduce::Summation);
 
@@ -96,13 +86,6 @@ pub fn average(values: &[Value], name: &Tag) -> Result<Value, ShellError> {
         })?
         .is_filesize();
 
-    let is_duration = values
-        .get(0)
-        .ok_or_else(|| {
-            ShellError::unexpected("Cannot perform aggregate math operation on empty data")
-        })?
-        .is_duration();
-
     let total = if are_bytes {
         to_byte(&sum(
             UntaggedValue::int(0).into_untagged_value(),
@@ -114,28 +97,6 @@ pub fn average(values: &[Value], name: &Tag) -> Result<Value, ShellError> {
                         value: UntaggedValue::Primitive(Primitive::Filesize(num)),
                         ..
                     } => UntaggedValue::int(*num as usize).into_untagged_value(),
-                    other => other.clone(),
-                })
-                .collect::<Vec<_>>(),
-        )?)
-        .ok_or_else(|| {
-            ShellError::labeled_error(
-                "could not convert to big decimal",
-                "could not convert to big decimal",
-                &name.span,
-            )
-        })
-    } else if is_duration {
-        to_duration(&sum(
-            UntaggedValue::int(0).into_untagged_value(),
-            values
-                .to_vec()
-                .iter()
-                .map(|v| match v {
-                    Value {
-                        value: UntaggedValue::Primitive(Primitive::Duration(duration)),
-                        ..
-                    } => UntaggedValue::int((*duration).clone()).into_untagged_value(),
                     other => other.clone(),
                 })
                 .collect::<Vec<_>>(),
