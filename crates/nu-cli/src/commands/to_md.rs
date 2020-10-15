bf2363947b7c00b3b217781708c160f33b7e3e3e
Joseph T. Lyons <JosephTLyons@users.noreply.github.com>
2020-10-14 23:20:55 -0400

    Add pretty flag to `to md` (#2640)
    * First draft for adding a `pretty` flag to `to md`
    
    * rustfmt
    
    * Fix Clippy warnings
    
    * rustfmt
    
    * Using Clippy suggestion broken code, reverting and putting in a statement to ignore clippy warning
    
    * Add test for `to md -p`

--- crates/nu-cli/src/commands/to_md.rs
+++ crates/nu-cli/src/commands/to_md.rs
@@ -7,6 +7,11 @@ use nu_protocol::{ReturnSuccess, Signature, UntaggedValue, Value};
 
 pub struct ToMarkdown;
 
+#[derive(Deserialize)]
+pub struct ToMarkdownArgs {
+    pretty: bool,
+}
+
 #[async_trait]
 impl WholeStreamCommand for ToMarkdown {
     fn name(&self) -> &str {
@@ -14,7 +19,11 @@ impl WholeStreamCommand for ToMarkdown {
     }
 
     fn signature(&self) -> Signature {
-        Signature::build("to md")
+        Signature::build("to md").switch(
+            "pretty",
+            "Formats the Markdown table to vertically align items",
+            Some('p'),
+        )
     }
 
     fn usage(&self) -> &str {
@@ -28,27 +37,84 @@ impl WholeStreamCommand for ToMarkdown {
     ) -> Result<OutputStream, ShellError> {
         to_md(args, registry).await
     }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![
+            Example {
+                description: "Outputs an unformatted md string representing the contents of ls",
+                example: "ls | to md",
+                result: None,
+            },
+            Example {
+                description: "Outputs a formatted md string representing the contents of ls",
+                example: "ls | to md -p",
+                result: None,
+            },
+        ]
+    }
 }
 
 async fn to_md(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputStream, ShellError> {
     let registry = registry.clone();
-    let args = args.evaluate_once(&registry).await?;
-    let name_tag = args.name_tag();
-    let input: Vec<Value> = args.input.collect().await;
+    let name_tag = args.call_info.name_tag.clone();
+    let (ToMarkdownArgs { pretty }, input) = args.process(&registry).await?;
+    let input: Vec<Value> = input.collect().await;
     let headers = nu_protocol::merge_descriptors(&input);
     let mut output_string = String::new();
 
+    let mut column_length_vector: Vec<usize> = Vec::new();
+
+    if pretty {
+        if !headers.is_empty() && (headers.len() > 1 || headers[0] != "") {
+            for header in &headers {
+                let htmlescape_header_string = &htmlescape::encode_minimal(&header);
+                column_length_vector.push(htmlescape_header_string.len());
+            }
+        }
+
+        for row in &input {
+            if let UntaggedValue::Row(row) = row.value.clone() {
+                for i in 0..headers.len() {
+                    let data = row.get_data(&headers[i]);
+                    let new_column_length = format_leaf(data.borrow()).plain_string(100_000).len();
+
+                    if column_length_vector[i] < new_column_length {
+                        column_length_vector[i] = new_column_length;
+                    }
+                }
+            }
+        }
+    }
+
     if !headers.is_empty() && (headers.len() > 1 || headers[0] != "") {
         output_string.push_str("|");
-        for header in &headers {
-            output_string.push_str(&htmlescape::encode_minimal(&header));
+
+        for i in 0..headers.len() {
+            let htmlescape_string = htmlescape::encode_minimal(&headers[i]);
+            let final_string = if pretty {
+                get_padded_string(htmlescape_string, column_length_vector[i], ' ')
+            } else {
+                htmlescape_string
+            };
+
+            output_string.push_str(&final_string);
             output_string.push_str("|");
         }
+
         output_string.push_str("\n|");
-        for _ in &headers {
-            output_string.push_str("-");
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..headers.len() {
+            let final_string = if pretty {
+                "-".repeat(column_length_vector[i])
+            } else {
+                String::from("-")
+            };
+
+            output_string.push_str(final_string.as_str());
             output_string.push_str("|");
         }
+
         output_string.push_str("\n");
     }
 
@@ -56,11 +122,20 @@ async fn to_md(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputSt
         match row.value {
             UntaggedValue::Row(row) => {
                 output_string.push_str("|");
-                for header in &headers {
-                    let data = row.get_data(header);
-                    output_string.push_str(&format_leaf(data.borrow()).plain_string(100_000));
+
+                for i in 0..headers.len() {
+                    let data = row.get_data(&headers[i]);
+                    let leaf_string = format_leaf(data.borrow()).plain_string(100_000);
+                    let final_string = if pretty {
+                        get_padded_string(leaf_string, column_length_vector[i], ' ')
+                    } else {
+                        leaf_string
+                    };
+
+                    output_string.push_str(&final_string);
                     output_string.push_str("|");
                 }
+
                 output_string.push_str("\n");
             }
             p => {
@@ -75,6 +150,11 @@ async fn to_md(args: CommandArgs, registry: &CommandRegistry) -> Result<OutputSt
     Ok(OutputStream::one(ReturnSuccess::value(
         UntaggedValue::string(output_string).into_value(name_tag),
     )))
+}
+
+fn get_padded_string(text: String, desired_length: usize, character: char) -> String {
+    let padding_length = desired_length - text.len();
+    return format!("{}{}", text, character.to_string().repeat(padding_length));
 }
 
 #[cfg(test)]
