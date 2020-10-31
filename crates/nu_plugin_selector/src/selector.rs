75a50b27fc0b655526ec432af5df3af8dec9275a
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2020-10-31 20:17:44 +0000

    wip

--- crates/nu_plugin_selector/src/selector.rs
+++ crates/nu_plugin_selector/src/selector.rs
@@ -1,9 +1,10 @@
 use nu_errors::ShellError;
 use nu_protocol::{TaggedDictBuilder, UntaggedValue, Value};
 use nu_source::{Tag, Tagged};
-use bigdecimal::{BigDecimal, FromPrimitive};
-use sxd_document::parser;
-use sxd_xpath::{Context, Factory};
+use nipper::Document;
+// use bigdecimal::{BigDecimal, FromPrimitive};
+// use sxd_document::parser;
+// use sxd_xpath::{Context, Factory};
 
 pub struct Selector {
     pub query: String,
@@ -35,91 +36,140 @@ fn execute_selector_query(
     tag: impl Into<Tag>,
 ) -> Result<Vec<Value>, ShellError> {
     let tag = tag.into();
-    let selector = build_selector(&query_string)?;
 
-    let package = parser::parse(&input_string);
+    println!("Test1");
+    // let html = r#"<div name="foo" value="bar"></div>"#;
+    // let document = Document::from(html);
+    // println!("{}", document.html());
 
-    if package.is_err() {
-        return Err(ShellError::labeled_error(
-            "invalid xml document",
-            "invalid xml document",
-            tag.span,
-        ));
-    }
+    // println!("Test2");
+    // let mut input = document.select(r#"div[name="foo"]"#);
+    // println!("{}", input.html());
+    // input.set_attr("id", "input");
+    // input.remove_attr("name");
+    // println!("{}", input.attr("value").unwrap());
 
-    let package = package.expect("invalid xml document");
+    // println!("{}", input.html());
+    // println!("Test3");
+    // input.replace_with_html(r#"<a href="https://wisburg.com">wisburg</a><h2>xxx</h2>"#);
+    // println!("{}", input.html());
+    // println!("{}", document.html());
 
-    let document = package.as_document();
-    let context = Context::new();
+    let document = Document::from(
+        r#"                <div class="loginContent">
+    <div class="loginContentbg">
+        <div class="el-dialog__wrapper login-dialog">
+            <div role="dialog" aria-modal="true" aria-label="dialog"
+                class="el-dialog el-dialog--center">
+                <!---->
+            </div>
+        </div>
+        <div class="el-dialog__wrapper login-dialog">
+            <div role="dialog" aria-modal="true" aria-label="dialog"
+                class="el-dialog el-dialog--center">
+                <!---->
+                <!---->
+            </div>
+        </div>
+    </div>
+</div>"#,
+    );
 
-    // leaving this here for augmentation at some point
-    // build_variables(&arguments, &mut context);
-    // build_namespaces(&arguments, &mut context);
+    let mut div = document.select("div.loginContent");
+    println!("{}", div.is("div"));
 
-    let res = selector.evaluate(&context, document.root());
+    println!("|{}|", div.text().trim());
 
-    // Some xpath statements can be long, so let's truncate it with ellipsis
-    let mut key = query_string.clone();
-    if query_string.len() >= 20 {
-        key.truncate(17);
-        key += "...";
-    } else {
-        key = query_string;
-    };
+    div.remove();
 
-    match res {
-        Ok(r) => {
-            let rows: Vec<Value> = match r {
-                sxd_xpath::Value::Nodeset(ns) => ns
-                    .into_iter()
-                    .map(|a| {
-                        let mut row = TaggedDictBuilder::new(Tag::unknown());
-                        row.insert_value(&key, UntaggedValue::string(a.string_value()));
-                        row.into_value()
-                    })
-                    .collect::<Vec<Value>>(),
-                sxd_xpath::Value::Boolean(b) => {
-                    let mut row = TaggedDictBuilder::new(Tag::unknown());
-                    row.insert_value(&key, UntaggedValue::boolean(b));
-                    vec![row.into_value()]
-                }
-                sxd_xpath::Value::Number(n) => {
-                    let mut row = TaggedDictBuilder::new(Tag::unknown());
-                    row.insert_value(
-                        &key,
-                        UntaggedValue::decimal(BigDecimal::from_f64(n).expect("error with f64"))
-                            .into_untagged_value(),
-                    );
+    println!("{}", document.html());
 
-                    vec![row.into_value()]
-                }
-                sxd_xpath::Value::String(s) => {
-                    let mut row = TaggedDictBuilder::new(Tag::unknown());
-                    row.insert_value(&key, UntaggedValue::string(s));
-                    vec![row.into_value()]
-                }
-            };
+    let ret = vec![];
+    Ok(ret)
+    // let selector = build_selector(&query_string)?;
+    // let package = parser::parse(&input_string);
 
-            Ok(rows)
-        }
-        Err(_) => Err(ShellError::labeled_error(
-            "selector query error",
-            "selector query error",
-            tag,
-        )),
-    }
+    // if package.is_err() {
+    //     return Err(ShellError::labeled_error(
+    //         "invalid xml document",
+    //         "invalid xml document",
+    //         tag.span,
+    //     ));
+    // }
+
+    // let package = package.expect("invalid xml document");
+
+    // let document = package.as_document();
+    // let context = Context::new();
+
+    // // leaving this here for augmentation at some point
+    // // build_variables(&arguments, &mut context);
+    // // build_namespaces(&arguments, &mut context);
+
+    // let res = selector.evaluate(&context, document.root());
+
+    // // Some xpath statements can be long, so let's truncate it with ellipsis
+    // let mut key = query_string.clone();
+    // if query_string.len() >= 20 {
+    //     key.truncate(17);
+    //     key += "...";
+    // } else {
+    //     key = query_string;
+    // };
+
+    // match res {
+    //     Ok(r) => {
+    //         let rows: Vec<Value> = match r {
+    //             sxd_xpath::Value::Nodeset(ns) => ns
+    //                 .into_iter()
+    //                 .map(|a| {
+    //                     let mut row = TaggedDictBuilder::new(Tag::unknown());
+    //                     row.insert_value(&key, UntaggedValue::string(a.string_value()));
+    //                     row.into_value()
+    //                 })
+    //                 .collect::<Vec<Value>>(),
+    //             sxd_xpath::Value::Boolean(b) => {
+    //                 let mut row = TaggedDictBuilder::new(Tag::unknown());
+    //                 row.insert_value(&key, UntaggedValue::boolean(b));
+    //                 vec![row.into_value()]
+    //             }
+    //             sxd_xpath::Value::Number(n) => {
+    //                 let mut row = TaggedDictBuilder::new(Tag::unknown());
+    //                 row.insert_value(
+    //                     &key,
+    //                     UntaggedValue::decimal(BigDecimal::from_f64(n).expect("error with f64"))
+    //                         .into_untagged_value(),
+    //                 );
+
+    //                 vec![row.into_value()]
+    //             }
+    //             sxd_xpath::Value::String(s) => {
+    //                 let mut row = TaggedDictBuilder::new(Tag::unknown());
+    //                 row.insert_value(&key, UntaggedValue::string(s));
+    //                 vec![row.into_value()]
+    //             }
+    //         };
+
+    //         Ok(rows)
+    //     }
+    //     Err(_) => Err(ShellError::labeled_error(
+    //         "selector query error",
+    //         "selector query error",
+    //         tag,
+    //     )),
+    // }
 }
 
-fn build_selector(selector_str: &str) -> Result<sxd_xpath::XPath, ShellError> {
-    let factory = Factory::new();
+// fn build_selector(selector_str: &str) -> Result<sxd_xpath::XPath, ShellError> {
+//     let factory = Factory::new();
 
-    match factory.build(selector_str) {
-        Ok(selector) => selector.ok_or_else(|| ShellError::untagged_runtime_error("invalid selector query")),
-        Err(_) => Err(ShellError::untagged_runtime_error(
-            "expected valid selector query",
-        )),
-    }
-}
+//     match factory.build(selector_str) {
+//         Ok(selector) => selector.ok_or_else(|| ShellError::untagged_runtime_error("invalid selector query")),
+//         Err(_) => Err(ShellError::untagged_runtime_error(
+//             "expected valid selector query",
+//         )),
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
