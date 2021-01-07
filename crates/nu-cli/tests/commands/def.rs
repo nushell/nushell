eb3c2c9e76767f4b500a313973a0213a7fee1032
Leonhard Kipp <leonhard.kipp@web.de>
2021-01-07 18:14:51 +0100

    Add comments to next LiteCommand (#2846)
    This commit applied adds comments preceding a command to the LiteCommands new
    field `comments`.
    
    This can be usefull for example when defining a function with `def`. Nushell
    could pick up the comments and display them when the user types `help my_def_func`.
    
    Example
    ```shell
    def my_echo [arg] { echo $arg }
    ```
    The LiteCommand def will now contain the comments `My echo` and `It's much
    better :)`.
    
    The comment is not associated with the next command if there is a (or multiple) newline
    between them.
    Example
    ```shell
    
    echo 42
    ```
    
    This new functionality is similar to DocStrings. One might introduce a special
    notation for such DocStrings, so that the parser can differentiate better
    between discardable comments and usefull documentation.

---
+++ crates/nu-cli/tests/commands/def.rs
@@ -0,0 +1,19 @@
+use nu_test_support::nu;
+use nu_test_support::playground::Playground;
+use std::fs;
+#[test]
+fn def_with_comment() {
+    Playground::setup("def_with_comment", |dirs, _| {
+        let data = r#"
+#My echo
+def e [arg] {echo $arg}
+            "#;
+        fs::write(dirs.root().join("def_test"), data).expect("Unable to write file");
+        let actual = nu!(
+            cwd: dirs.root(),
+            "source def_test; help e | to json"
+        );
+
+        assert!(actual.out.contains("My echo\\n\\n"));
+    });
+}
