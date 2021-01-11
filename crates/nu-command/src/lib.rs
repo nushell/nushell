95b4be2145cf7b6d13ddc1d2c47765fe83bad004
Michael Angerman <stormasm@gmail.com>
2021-01-11 14:28:51 -0800

    remove mod completion from lib.rs which was not being used along with quickcheck macros

--- crates/nu-command/src/lib.rs
+++ crates/nu-command/src/lib.rs
@@ -6,16 +6,7 @@ extern crate indexmap;
 
 #[macro_use]
 mod prelude;
-
-#[cfg(test)]
-extern crate quickcheck;
-#[cfg(test)]
-#[macro_use(quickcheck)]
-extern crate quickcheck_macros;
-
 pub mod commands;
-#[cfg(feature = "rustyline-support")]
-mod completion;
 mod futures;
 pub mod maybe_print_errors;
 pub mod script;
