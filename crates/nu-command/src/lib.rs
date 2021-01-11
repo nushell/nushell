183e76ed338737bd3a9573c9e4384f44431ec8be
Michael Angerman <stormasm@gmail.com>
2021-01-11 07:20:23 -0800

    move commands, futures.rs, script.rs, utils

---
+++ crates/nu-command/src/lib.rs
@@ -0,0 +1,51 @@
+#![recursion_limit = "2048"]
+
+#[cfg(test)]
+#[macro_use]
+extern crate indexmap;
+
+#[macro_use]
+mod prelude;
+
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
+mod commands;
+#[cfg(feature = "rustyline-support")]
+mod completion;
+//mod env;
+//mod format;
+mod futures;
+//#[cfg(feature = "rustyline-support")]
+//mod git;
+//#[cfg(feature = "rustyline-support")]
+//mod keybinding;
+//mod line_editor;
+pub mod script;
+//mod shell;
+//pub mod types;
+pub mod utils;
+
+//#[cfg(test)]
+//mod examples;
+
+//#[cfg(feature = "rustyline-support")]
+//pub use crate::cli::cli;
+
+//pub use crate::cli::{parse_and_eval, register_plugins, run_script_file};
+
+//pub use crate::commands::default_context::create_default_context;
+//pub use crate::env::environment_syncer::EnvironmentSyncer;
+pub use nu_data::config;
+pub use nu_data::dict::TaggedListBuilder;
+pub use nu_data::primitive;
+pub use nu_data::value;
+pub use nu_stream::{InputStream, InterruptibleStream, OutputStream};
+pub use nu_value_ext::ValueExt;
+pub use num_traits::cast::ToPrimitive;
+
+// TODO: Temporary redirect
+pub use nu_protocol::{did_you_mean, TaggedDictBuilder};
---
+++ crates/nu-command/src/prelude.rs
@@ -0,0 +1,109 @@
+#[macro_export]
+macro_rules! return_err {
+    ($expr:expr) => {
+        match $expr {
+            Err(_) => return,
+            Ok(expr) => expr,
+        };
+    };
+}
+
+#[macro_export]
+macro_rules! stream {
+    ($($expr:expr),*) => {{
+        let mut v = VecDeque::new();
+
+        $(
+            v.push_back($expr);
+        )*
+
+        v
+    }}
+}
+
+#[macro_export]
+macro_rules! trace_out_stream {
+    (target: $target:tt, $desc:tt = $expr:expr) => {{
+        if log::log_enabled!(target: $target, log::Level::Trace) {
+            use futures::stream::StreamExt;
+
+            let objects = $expr.inspect(move |o| {
+                trace!(
+                    target: $target,
+                    "{} = {}",
+                    $desc,
+                    match o {
+                        Err(err) => format!("{:?}", err),
+                        Ok(value) => value.display(),
+                    }
+                );
+            });
+
+            nu_stream::OutputStream::new(objects)
+        } else {
+            $expr
+        }
+    }};
+}
+
+pub(crate) use nu_protocol::{out, outln, row};
+
+//pub(crate) use crate::cli::maybe_print_errors;
+pub(crate) use crate::commands::command::RunnableContext;
+pub(crate) use async_trait::async_trait;
+pub(crate) use bigdecimal::BigDecimal;
+pub(crate) use futures::{Stream, StreamExt};
+pub(crate) use indexmap::{indexmap, IndexMap};
+pub(crate) use itertools::Itertools;
+pub(crate) use nu_data::config;
+pub(crate) use nu_data::value;
+pub(crate) use nu_engine::EvaluationContext;
+pub(crate) use nu_engine::Example;
+pub(crate) use nu_engine::Host;
+pub(crate) use nu_engine::RawCommandArgs;
+pub(crate) use nu_engine::ShellManager;
+pub(crate) use nu_engine::{get_help, CommandArgs, Scope, WholeStreamCommand};
+pub(crate) use nu_errors::ShellError;
+pub(crate) use nu_parser::ParserScope;
+pub(crate) use nu_source::{AnchorLocation, PrettyDebug, Span, SpannedItem, Tag, TaggedItem, Text};
+pub(crate) use nu_stream::ToInputStream;
+pub(crate) use nu_stream::{InputStream, Interruptible, OutputStream};
+pub(crate) use nu_value_ext::ValueExt;
+pub(crate) use num_bigint::BigInt;
+pub(crate) use num_traits::cast::ToPrimitive;
+pub(crate) use serde::Deserialize;
+pub(crate) use std::collections::VecDeque;
+pub(crate) use std::future::Future;
+pub(crate) use std::sync::atomic::{AtomicBool, Ordering};
+pub(crate) use std::sync::Arc;
+
+pub trait FromInputStream {
+    fn from_input_stream(self) -> OutputStream;
+}
+
+impl<T> FromInputStream for T
+where
+    T: Stream<Item = nu_protocol::Value> + Send + 'static,
+{
+    fn from_input_stream(self) -> OutputStream {
+        OutputStream {
+            values: self.map(nu_protocol::ReturnSuccess::value).boxed(),
+        }
+    }
+}
+
+pub trait ToOutputStream {
+    fn to_output_stream(self) -> OutputStream;
+}
+
+impl<T, U> ToOutputStream for T
+where
+    T: Stream<Item = U> + Send + 'static,
+    U: Into<nu_protocol::ReturnValue>,
+{
+    fn to_output_stream(self) -> OutputStream {
+        OutputStream {
+            values: self.map(|item| item.into()).boxed(),
+        }
+    }
+}
