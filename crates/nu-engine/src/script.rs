e823109d90c3dfbec2222a38a03c6fc8e72a8c9d
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-04-01 23:32:16 +0200

    Make the compiler happy :)

--- crates/nu-engine/src/script.rs
+++ crates/nu-engine/src/script.rs
@@ -11,7 +11,6 @@ use nu_protocol::{
 };
 use nu_protocol::{Primitive, UntaggedValue, Value};
 use nu_stream::{InputStream, ToInputStream};
-use std::error::Error;
 
 use crate::EvaluationContext;
 use log::{debug, trace};
