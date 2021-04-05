c067c229a823ffdf9e159f2d8f1ee96a27c0d345
kubouch <kubouch@gmail.com>
2021-04-05 17:43:19 +0300

    Add structured path joining
    Structured path is implicitly joined at every patch subcommand call.

--- crates/nu-command/src/commands/path/mod.rs
+++ crates/nu-command/src/commands/path/mod.rs
@@ -9,7 +9,7 @@ mod r#type;
 
 use crate::prelude::*;
 use nu_errors::ShellError;
-use nu_protocol::{ColumnPath, MaybeOwned, Primitive, ReturnSuccess, ShellTypeName, UntaggedValue, Value};
+use nu_protocol::{ColumnPath, Dictionary, MaybeOwned, Primitive, ReturnSuccess, ShellTypeName, UntaggedValue, Value};
 use nu_source::Span;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
@@ -33,75 +33,61 @@ trait PathSubcommandArguments {
     fn get_column_paths(&self) -> &Vec<ColumnPath>;
 }
 
-fn into_path(v: &Value, span: Span) -> Result<PathBuf, ShellError> {
-    match &v.value {
-        UntaggedValue::Primitive(Primitive::FilePath(path_buf)) => Ok(path_buf.into()),
-        UntaggedValue::Primitive(Primitive::String(s)) => Ok(s.into()),
-        UntaggedValue::Row(entries) => {
-            if entries.length() == 0 {
-                return Err(ShellError::untagged_runtime_error("Empty rows are not allowed"));
-            }
+fn join_path(entries: &Dictionary) -> Result<PathBuf, ShellError> {
+    if entries.length() == 0 {
+        return Err(ShellError::untagged_runtime_error("Empty rows are not allowed"));
+    }
 
-            //TODO: check out `get` error for wrong column:
-            //   There isn't a column named 'name'
-            //   Perhaps you meant 'parent'? Columns available: parent, stem, extension
-            for col in entries.keys() {
-                if !ALLOWED_COLUMNS.contains(&col.as_str()) {
-                    let msg = format!("Column '{}' is not allowed", col);
-                    return Err(ShellError::untagged_runtime_error(msg));
-                }
-            }
-
-            // At this point, the row is known to have >0 columns, all of them allowed
-            let mut result = PathBuf::new();
-
-            // #[cfg(windows)]
-            if let MaybeOwned::Borrowed(val) = entries.get_data("prefix") {
-                let s = val.as_string()?;
-                if !s.is_empty() {
-                    result.push(&s);
-                }
-            };
-
-            if let MaybeOwned::Borrowed(val) = entries.get_data("parent") {
-                let p = val.as_filepath()?;
-                if !p.as_os_str().is_empty() {
-                    result.push(p);
-                }
-            };
-
-            let mut basename = String::new();
-
-            if let MaybeOwned::Borrowed(val) = entries.get_data("stem") {
-                let s = val.as_string()?;
-                if !s.is_empty() {
-                    basename.push_str(&s);
-                }
-            };
-
-            if let MaybeOwned::Borrowed(val) = entries.get_data("extension") {
-                let s = val.as_string()?;
-                if !s.is_empty() {
-                    basename.push('.');
-                    basename.push_str(&s);
-                }
-            };
-
-            result.push(basename);
-
-            Ok(result)
-        }
-        other => {
-            let got = format!("got {}", other.type_name());
-            Err(ShellError::labeled_error_with_secondary(
-                "value is not string or path",
-                got,
-                span,
-                "originates from here".to_string(),
-                v.tag().span,
-            ))
+    //TODO: check out `get` error for wrong column:
+    //   There isn't a column named 'name'
+    //   Perhaps you meant 'parent'? Columns available: parent, stem, extension
+    for col in entries.keys() {
+        if !ALLOWED_COLUMNS.contains(&col.as_str()) {
+            let msg = format!("Column '{}' is not allowed", col);
+            return Err(ShellError::untagged_runtime_error(msg));
         }
     }
+
+    // At this point, the row is known to have >0 columns, all of them allowed
+    let mut result = PathBuf::new();
+
+    #[cfg(windows)]
+    if let MaybeOwned::Borrowed(val) = entries.get_data("prefix") {
+        let s = val.as_string()?;
+        if !s.is_empty() {
+            result.push(&s);
+        }
+    };
+
+    if let MaybeOwned::Borrowed(val) = entries.get_data("parent") {
+        let p = val.as_filepath()?;
+        if !p.as_os_str().is_empty() {
+            result.push(p);
+        }
+    };
+
+    let mut basename = String::new();
+
+    if let MaybeOwned::Borrowed(val) = entries.get_data("stem") {
+        let s = val.as_string()?;
+        if !s.is_empty() {
+            basename.push_str(&s);
+        }
+    };
+
+    if let MaybeOwned::Borrowed(val) = entries.get_data("extension") {
+        let s = val.as_string()?;
+        if !s.is_empty() {
+            basename.push('.');
+            basename.push_str(&s);
+        }
+    };
+
+    if !basename.is_empty() {
+        result.push(basename);
+    }
+
+    Ok(result)
 }
 
 fn handle_value<F, T>(action: &F, v: &Value, span: Span, args: Arc<T>) -> Result<Value, ShellError>
@@ -109,28 +95,29 @@ where
     T: PathSubcommandArguments + Send + 'static,
     F: Fn(&Path, Tag, &T) -> Result<Value, ShellError> + Send + 'static,
 {
-    let pb = into_path(v, span)?;
-    action(&pb, v.tag(), &args)
-    // match &v.value {
-    //     UntaggedValue::Primitive(Primitive::FilePath(buf)) => {
-    //         // action(buf, &args).into_value(v.tag())
-    //         action(buf, v.tag(), &args)
-    //     }
-    //     UntaggedValue::Primitive(Primitive::String(s)) => {
-    //         // action(s.as_ref(), &args).into_value(v.tag())
-    //         action(s.as_ref(), v.tag(), &args)
-    //     }
-    //     other => {
-    //         let got = format!("got {}", other.type_name());
-    //         Err(ShellError::labeled_error_with_secondary(
-    //             "value is not string or path",
-    //             got,
-    //             span,
-    //             "originates from here".to_string(),
-    //             v.tag().span,
-    //         ))
-    //     }
-    // }
+    match &v.value {
+        UntaggedValue::Primitive(Primitive::FilePath(buf)) => {
+            action(buf, v.tag(), &args)
+        }
+        UntaggedValue::Primitive(Primitive::String(s)) => {
+            action(s.as_ref(), v.tag(), &args)
+        }
+        UntaggedValue::Row(entries) => {
+            // implicit join makes all subcommands understand the structured path
+            let path_buf = join_path(entries)?;
+            action(&path_buf, v.tag(), &args)
+        },
+        other => {
+            let got = format!("got {}", other.type_name());
+            Err(ShellError::labeled_error_with_secondary(
+                "value is not string, path or a table with relevant columns",
+                got,
+                span,
+                "originates from here".to_string(),
+                v.tag().span,
+            ))
+        }
+    }
 }
 
 fn operate<F, T>(
