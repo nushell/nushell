2880109f31f0ea02d90e9fba57c09a93e102a3cd
Andrés N. Robalino <andres@androbtech.com>
2021-04-08 13:51:12 -0500

    Runnable contexts move. (#3283)
    * Engine extract first steps.
    
    * Don't depend on ShellManager.

--- crates/nu-engine/src/lib.rs
+++ crates/nu-engine/src/lib.rs
@@ -14,7 +14,6 @@ mod from_value;
 mod maybe_text_codec;
 pub mod plugin;
 mod print;
-mod runnable_context;
 pub mod script;
 pub mod shell;
 mod whole_stream_command;
@@ -24,6 +23,7 @@ pub use crate::basic_shell_manager::basic_shell_manager;
 pub use crate::call_info::UnevaluatedCallInfo;
 pub use crate::command_args::{
     CommandArgs, EvaluatedCommandArgs, EvaluatedWholeStreamCommandArgs, RawCommandArgs,
+    RunnableContext, RunnableContextWithoutInput,
 };
 pub use crate::config_holder::ConfigHolder;
 pub use crate::documentation::{generate_docs, get_brief_help, get_documentation, get_full_help};
@@ -40,7 +40,6 @@ pub use crate::filesystem::path;
 pub use crate::from_value::FromValue;
 pub use crate::maybe_text_codec::{BufCodecReader, MaybeTextCodec, StringOrBinary};
 pub use crate::print::maybe_print_errors;
-pub use crate::runnable_context::RunnableContext;
 pub use crate::shell::painter::Painter;
 pub use crate::shell::palette::{DefaultPalette, Palette};
 pub use crate::shell::shell_manager::ShellManager;
