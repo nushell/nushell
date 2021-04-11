dbecbdccd4eaeca9bea33fac1bc8d9ea3000c3d8
Jakub Žádník <kubouch@gmail.com>
2021-04-12 02:53:31 +0300

    Fix type error when Value is viewed as file path (#3305)
    I believe this should be "path", not "string".

--- crates/nu-protocol/src/value.rs
+++ crates/nu-protocol/src/value.rs
@@ -324,7 +324,7 @@ impl Value {
     pub fn as_filepath(&self) -> Result<PathBuf, ShellError> {
         match &self.value {
             UntaggedValue::Primitive(Primitive::FilePath(path)) => Ok(path.clone()),
-            _ => Err(ShellError::type_error("string", self.spanned_type_name())),
+            _ => Err(ShellError::type_error("path", self.spanned_type_name())),
         }
     }
 
