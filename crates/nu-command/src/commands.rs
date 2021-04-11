4cabeb76965b33e578f7f1b8c6cabbea5c964dd3
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-04-11 12:03:16 +0200

    Fix rebase stuff

--- crates/nu-command/src/commands.rs
+++ crates/nu-command/src/commands.rs
@@ -212,7 +212,8 @@ pub use help::Help;
 pub use histogram::Histogram;
 pub use history::History;
 pub use insert::Command as Insert;
-pub use into_int::IntoInt;
+pub use into::Into;
+pub use into::IntoInt;
 pub use keep::{Keep, KeepUntil, KeepWhile};
 pub use last::Last;
 pub use length::Length;
@@ -305,6 +306,7 @@ mod tests {
             whole_stream_command(Append),
             whole_stream_command(GroupBy),
             whole_stream_command(Insert),
+            whole_stream_command(Into),
             whole_stream_command(Move),
             whole_stream_command(Update),
             whole_stream_command(Empty),
