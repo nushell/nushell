1a60c5a3192a4a0a277b0e643d508985e82df2fa
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-04-18 01:12:25 +0200

    Remove try_all

--- crates/nu-command/src/commands/all.rs
+++ crates/nu-command/src/commands/all.rs
@@ -3,7 +3,6 @@ use nu_engine::evaluate_baseline_expr;
 use nu_engine::WholeStreamCommand;
 use nu_errors::ShellError;
 use nu_protocol::{hir::CapturedBlock, hir::ClassifiedCommand, Signature, SyntaxShape};
-use std::error::Error;
 
 pub struct Command;
 
@@ -88,50 +87,20 @@ fn all(args: CommandArgs) -> Result<OutputStream, ShellError> {
     // invocations are independent of each other!
     scope.enter_scope();
     scope.add_vars(&all_args.predicate.captured.entries);
-    //`try_all` is like `all`, but with short-circuiting for Err
-    let result: Result<bool, ShellError> = args.input.try_all(|row| {
+    let result = args.input.all(|row| {
         //$it gets overwritten each invocation
         scope.add_var("$it", row);
 
-        let condition = evaluate_baseline_expr(&*condition, &ctx)?;
-
-        condition.as_bool()
+        let condition = evaluate_baseline_expr(&*condition, &ctx);
+        match condition {
+            Ok(cond) => cond.as_bool().unwrap_or(false),
+            Err(_) => false,
+        }
     });
     scope.exit_scope();
 
-    Ok(OutputStream::one(result?))
+    Ok(OutputStream::one(result))
 }
-
-trait TryAllExt: Iterator {
-    /// Tests if every element of the iterator matches a predicate.
-    ///
-    /// `try_all()` takes a closure that returns `Ok(true)`, `Ok(false)` or Err(E). It applies
-    /// this closure to each element of the iterator, and if they all return
-    /// `Ok(true)`, then so does `try_all()`. If any of them return `Ok(false)`, it
-    /// returns `Ok(false)`. If the closure returns Err(E), `try_all` returns Err(E) immediatly
-    /// (short-circuiting).
-    ///
-    /// `try_all()` is short-circuiting; in other words, it will stop processing
-    /// as soon as it finds a `Ok(false)` or `Err(E)`
-    ///
-    /// An empty iterator returns `Ok(true)`.
-    fn try_all<F, E>(&mut self, mut f: F) -> Result<bool, E>
-    where
-        Self: Sized,
-        F: FnMut(Self::Item) -> Result<bool, E>,
-        E: Error,
-    {
-        for item in self {
-            //if f fails, we return failure
-            if !f(item)? {
-                return Ok(false);
-            }
-        }
-        Ok(true)
-    }
-}
-
-impl<I: Iterator> TryAllExt for I {}
 
 #[cfg(test)]
 mod tests {
