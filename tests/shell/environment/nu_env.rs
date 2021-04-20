74f924e65f04133a0b86c2b163af429929cbaaad
JT <jonathan.d.turner@gmail.com>
2021-04-20 17:47:51 +1200

    Bump to 0.30

--- tests/shell/environment/nu_env.rs
+++ tests/shell/environment/nu_env.rs
@@ -72,7 +72,7 @@ fn picks_up_env_keys_when_entering_trusted_directory_indirectly() {
         sandbox.with_files(vec![FileWithContent(
             ".nu-env",
             r#"[env]
-                nu-version = "0.29.2" "#,
+                nu-version = "0.30.0" "#,
         )]);
 
         let expected = "0.29.2";
