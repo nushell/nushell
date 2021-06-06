57a009b8e620727fe5361335b4cb23705f73455e
Pirmin Kalberer <pka@sourcepole.ch>
2021-06-06 19:25:03 +0200

    Respect territory in locale (e.g. de_CH) for byte formatting (#3560)

--- crates/nu-data/src/base/shape.rs
+++ crates/nu-data/src/base/shape.rs
@@ -202,11 +202,12 @@ impl InlineShape {
                     // Since get_locale() and Locale::from_name() don't always return the same items
                     // we need to try and parse it to match. For instance, a valid locale is de_DE
                     // however Locale::from_name() wants only de so we split and parse it out.
+                    let locale_string = locale_string.replace("_", "-"); // en_AU -> en-AU
                     let locale = match Locale::from_name(&locale_string) {
                         Ok(loc) => loc,
                         _ => {
                             let all = num_format::Locale::available_names();
-                            let locale_prefix = &locale_string.split('_').collect::<Vec<&str>>();
+                            let locale_prefix = &locale_string.split('-').collect::<Vec<&str>>();
                             if all.contains(&locale_prefix[0]) {
                                 // eprintln!("Found alternate: {}", &locale_prefix[0]);
                                 Locale::from_name(locale_prefix[0]).unwrap_or(Locale::en)
