735424318c062beb19fda908c4da51b70617e3e1
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-06-07 20:56:47 +0100

    Column selector using FullColumnPath

--- crates/nu-command/src/commands/dataframe/with_column.rs
+++ crates/nu-command/src/commands/dataframe/with_column.rs
@@ -5,6 +5,7 @@ use nu_protocol::{
     dataframe::{NuDataFrame, PolarsData},
     Signature, SyntaxShape, UntaggedValue, Value,
 };
+use nu_source::Tagged;
 
 use super::utils::parse_polars_error;
 pub struct DataFrame;
@@ -19,11 +20,14 @@ impl WholeStreamCommand for DataFrame {
     }
 
     fn signature(&self) -> Signature {
-        Signature::build("pls with-column").required(
-            "series",
-            SyntaxShape::Any,
-            "series to be added",
-        )
+        Signature::build("pls with-column")
+            .required("series", SyntaxShape::Any, "series to be added")
+            .named(
+                "name",
+                SyntaxShape::String,
+                "optional column name",
+                Some('n'),
+            )
     }
 
     fn run(&self, args: CommandArgs) -> Result<OutputStream, ShellError> {
@@ -42,9 +46,10 @@ impl WholeStreamCommand for DataFrame {
 fn command(args: CommandArgs) -> Result<OutputStream, ShellError> {
     let tag = args.call_info.name_tag.clone();
     let mut args = args.evaluate_once()?;
+    let name: Option<Tagged<String>> = args.get_flag("name")?;
     let value: Value = args.req(0)?;
 
-    let series = match value.value {
+    let mut series = match value.value {
         UntaggedValue::DataFrame(PolarsData::Series(series)) => Ok(series),
         _ => Err(ShellError::labeled_error(
             "Incorrect type",
@@ -53,11 +58,17 @@ fn command(args: CommandArgs) -> Result<OutputStream, ShellError> {
         )),
     }?;
 
+    let series = if let Some(name) = name {
+        series.as_mut().rename(name.item.as_ref()).clone()
+    } else {
+        series.as_ref().clone()
+    };
+
     let mut df = NuDataFrame::try_from_stream(&mut args.input, &tag.span)?;
 
     let res = df
         .as_mut()
-        .with_column(series.series())
+        .with_column(series)
         .map_err(|e| parse_polars_error::<&str>(&e, &tag.span, None))?;
 
     Ok(OutputStream::one(NuDataFrame::dataframe_to_value(
