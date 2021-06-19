51c685aa9999a5a2e49d1f848e338a4ce4286404
Eli Flanagan <eli@typedspace.com>
2021-06-18 23:01:18 -0400

    port string case commands to engine-p (#3649)
    Port snake_case and friends to engine-p syntax.
    
    Part of #3390.

--- crates/nu-command/src/commands/strings/str_/case/kebab_case.rs
+++ crates/nu-command/src/commands/strings/str_/case/kebab_case.rs
@@ -23,7 +23,7 @@ impl WholeStreamCommand for SubCommand {
         "converts a string to kebab-case"
     }
 
-    fn run_with_actions(&self, args: CommandArgs) -> Result<ActionStream, ShellError> {
+    fn run(&self, args: CommandArgs) -> Result<OutputStream, ShellError> {
         operate(args, &to_kebab_case)
     }
 
