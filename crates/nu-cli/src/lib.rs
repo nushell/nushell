3ce88f082ebfa0822d892738cf91ffd6ff9ddffc
Andrés N. Robalino <andres@androbtech.com>
2021-06-30 16:24:49 -0500

    nu-cli: Remove crates not needed.

--- crates/nu-cli/src/lib.rs
+++ crates/nu-cli/src/lib.rs
@@ -1,9 +1,3 @@
-#[cfg(test)]
-extern crate quickcheck;
-#[cfg(test)]
-#[macro_use(quickcheck)]
-extern crate quickcheck_macros;
-
 pub mod app;
 mod cli;
 #[cfg(feature = "rustyline-support")]
@@ -18,16 +12,4 @@ pub use crate::cli::cli;
 pub use crate::app::App;
 pub use crate::cli::{parse_and_eval, register_plugins, run_script_file};
 
-pub use nu_command::{
-    commands::NuSignature as Nu, commands::Version as NuVersion, create_default_context,
-};
-pub use nu_data::config;
-pub use nu_data::dict::TaggedListBuilder;
-pub use nu_data::primitive;
-pub use nu_data::value;
-pub use nu_stream::{ActionStream, InputStream, InterruptibleStream};
-pub use nu_value_ext::ValueExt;
-pub use num_traits::cast::ToPrimitive;
-
-// TODO: Temporary redirect
-pub use nu_protocol::{did_you_mean, TaggedDictBuilder};
+pub use nu_command::create_default_context;
