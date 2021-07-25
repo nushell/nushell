d54d7cc4316c485a13940e14ed7ce66783e3d9c3
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-07-25 21:36:09 +0100

    append dataframes (#3839)

--- crates/nu-protocol/src/dataframe/mod.rs
+++ crates/nu-protocol/src/dataframe/mod.rs
@@ -1,8 +1,10 @@
 pub mod nu_dataframe;
 pub mod nu_groupby;
+pub mod operations;
 
 pub use nu_dataframe::{Column, NuDataFrame};
 pub use nu_groupby::NuGroupBy;
+pub use operations::Axis;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Hash, Serialize, Deserialize)]
