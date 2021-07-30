ea04cadf9db78af520079b49e74d4d93c74a8064
Lily Mara <lilymara@fastmail.com>
2021-07-30 10:21:03 -0700

    Support equals sign in shorthand environment variable values
    Some environment variables, such as `RUST_LOG` include equals signs. Nushell
    should support this in the shorthand environment variable syntax so that
    developers using these variables can control them easily. We accomplish this by
    swapping `std::str::split` for `std::str::splitn`, which ensures that we only
    consider the first equals sign in the string instead of all of them, which we
    did previously.
    
    Closes #3867

--- crates/nu-parser/src/parse.rs
+++ crates/nu-parser/src/parse.rs
@@ -1997,7 +1997,7 @@ fn expand_shorthand_forms(
         if lite_pipeline.commands[0].parts[0].contains('=')
             && !lite_pipeline.commands[0].parts[0].starts_with('$')
         {
-            let assignment: Vec<_> = lite_pipeline.commands[0].parts[0].split('=').collect();
+            let assignment: Vec<_> = lite_pipeline.commands[0].parts[0].splitn(2, '=').collect();
             if assignment.len() != 2 {
                 (
                     lite_pipeline.clone(),
