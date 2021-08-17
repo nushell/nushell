6938689e30c02f93c3c741c789ac2f16bea6d927
kubouch <kubouch@gmail.com>
2021-08-17 19:36:58 +0300

    Canonicalize also ~ and ndots; Unify path joining
    Also, add doc comments in nu_path::expansions.

--- crates/nu-path/src/expansions.rs
+++ crates/nu-path/src/expansions.rs
@@ -4,41 +4,8 @@ use std::path::{Path, PathBuf};
 use super::dots::{expand_dots, expand_ndots};
 use super::tilde::expand_tilde;
 
-// Trace a relative path back to its root starting from current directory.
-// Returns error if not possible.
-// If path is absolute, just return it.
-// fn absolutize(path: impl AsRef<Path>) -> io::Result<PathBuf> {
-//     if path.as_ref().is_absolute() {
-//         Ok(path.as_ref().into())
-//     } else {
-//         Ok(std::env::current_dir()?.join(path))
-//     }
-// }
-
-// Trace a relative path back to its root starting from a custom directory.
-// Returns error if not possible.
-// If path is absolute, just return it.
-// fn absolutize_with<P, Q>(path: P, relative_to: Q) -> io::Result<PathBuf>
-// where
-//     P: AsRef<Path>,
-//     Q: AsRef<Path>,
-// {
-//     if path.as_ref().is_absolute() {
-//         Ok(path.as_ref().into())
-//     } else {
-//         if relative_to.as_ref().is_absolute() {
-//             Ok(relative_to.as_ref().join(path))
-//         } else {
-//             Ok(absolutize(relative_to)?.join(path))
-//         }
-//     }
-// }
-
-pub fn canonicalize(path: impl AsRef<Path>) -> io::Result<PathBuf> {
-    dunce::canonicalize(path)
-}
-
-pub fn canonicalize_with<P, Q>(path: P, relative_to: Q) -> io::Result<PathBuf>
+// Join a path relative to another path. Paths starting with tilde are considered as absolute.
+fn join_path_relative<P, Q>(path: P, relative_to: Q) -> PathBuf
 where
     P: AsRef<Path>,
     Q: AsRef<Path>,
@@ -46,49 +13,60 @@ where
     let path = path.as_ref();
     let relative_to = relative_to.as_ref();
 
-    let path = if path == Path::new(".") {
+    if path == Path::new(".") {
         // Joining a Path with '.' appends a '.' at the end, making the prompt
         // more ugly - so we don't do anything, which should result in an equal
         // path on all supported systems.
-        relative_to.to_owned()
+        relative_to.into()
     } else if path.starts_with("~") {
-        // TODO: No need for this branch
-        expand_tilde(path)
+        // do not end up with "/some/path/~"
+        path.into()
     } else {
         relative_to.join(path)
-    };
+    }
+}
+
+/// Resolve all symbolic links and all components (tilde, ., .., ...+) and return the path in its
+/// absolute form.
+///
+/// Fails under the same conditions as std::fs::canonicalize().
+pub fn canonicalize(path: impl AsRef<Path>) -> io::Result<PathBuf> {
+    let path = expand_tilde(path);
+    let path = expand_ndots(path);
+
+    dunce::canonicalize(path)
+}
+
+/// Same as canonicalize() but the input path is composed of two parts
+pub fn canonicalize_with<P, Q>(path: P, relative_to: Q) -> io::Result<PathBuf>
+where
+    P: AsRef<Path>,
+    Q: AsRef<Path>,
+{
+    let path = join_path_relative(path, relative_to);
 
     canonicalize(path)
 }
 
-// Expands ~ to home and shortens paths by removing unecessary ".." and "."
-// where possible. Also expands "...+" appropriately.
-// Does not convert to absolute form nor does it resolve symlinks.
+/// Resolve path components (tilde, ., .., ...+) if possible.
+///
+/// The function works in a "best effort" mode: It does not fail but rather returns the unexpanded
+/// version if the expansion is not possible.
+///
+/// Does not convert to absolute form nor does it resolve symlinks.
 pub fn expand_path(path: impl AsRef<Path>) -> PathBuf {
     let path = expand_tilde(path);
     let path = expand_ndots(path);
     expand_dots(path)
 }
 
+/// Same as expand_path() but the input path is composed of two parts
 pub fn expand_path_with<P, Q>(path: P, relative_to: Q) -> PathBuf
 where
     P: AsRef<Path>,
     Q: AsRef<Path>,
 {
-    let path = path.as_ref();
-    let relative_to = relative_to.as_ref();
-
-    let path = if path == Path::new(".") {
-        // Joining a Path with '.' appends a '.' at the end, making the prompt
-        // more ugly - so we don't do anything, which should result in an equal
-        // path on all supported systems.
-        relative_to.into()
-    } else if path.starts_with("~") {
-        // TODO: No need for this branch
-        expand_tilde(path)
-    } else {
-        relative_to.join(path)
-    };
+    let path = join_path_relative(path, relative_to);
 
     expand_path(path)
 }
