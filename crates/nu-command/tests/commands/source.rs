25be068b3f38e9724d22519fad93f11c9b13badb
kubouch <kubouch@gmail.com>
2021-08-19 09:38:13 +0300

    Test sourcing also ASCII and single-quoted paths

--- crates/nu-command/tests/commands/source.rs
+++ crates/nu-command/tests/commands/source.rs
@@ -2,11 +2,11 @@ use nu_test_support::fs::Stub::FileWithContent;
 use nu_test_support::nu;
 use nu_test_support::playground::Playground;
 
-fn try_source_foo_with_quotes_in(testdir: &str) {
-    Playground::setup("source_test_1", |dirs, sandbox| {
+fn try_source_foo_with_double_quotes_in(testdir: &str, playdir: &str) {
+    Playground::setup(playdir, |dirs, sandbox| {
         let testdir = String::from(testdir);
         let mut foo_file = testdir.clone();
-        foo_file.push_str("/🚒.nu");
+        foo_file.push_str("/foo.nu");
 
         sandbox.mkdir(&testdir);
         sandbox.with_files(vec![FileWithContent(&foo_file, "echo foo")]);
@@ -19,11 +19,28 @@ fn try_source_foo_with_quotes_in(testdir: &str) {
     });
 }
 
-fn try_source_foo_without_quotes_in(testdir: &str) {
-    Playground::setup("source_test_1", |dirs, sandbox| {
+fn try_source_foo_with_single_quotes_in(testdir: &str, playdir: &str) {
+    Playground::setup(playdir, |dirs, sandbox| {
         let testdir = String::from(testdir);
         let mut foo_file = testdir.clone();
-        foo_file.push_str("/🚒.nu");
+        foo_file.push_str("/foo.nu");
+
+        sandbox.mkdir(&testdir);
+        sandbox.with_files(vec![FileWithContent(&foo_file, "echo foo")]);
+
+        let cmd = String::from("source ") + r#"'"# + &foo_file + r#"'"#;
+
+        let actual = nu!(cwd: dirs.test(), &cmd);
+
+        assert_eq!(actual.out, "foo");
+    });
+}
+
+fn try_source_foo_without_quotes_in(testdir: &str, playdir: &str) {
+    Playground::setup(playdir, |dirs, sandbox| {
+        let testdir = String::from(testdir);
+        let mut foo_file = testdir.clone();
+        foo_file.push_str("/foo.nu");
 
         sandbox.mkdir(&testdir);
         sandbox.with_files(vec![FileWithContent(&foo_file, "echo foo")]);
@@ -38,32 +55,37 @@ fn try_source_foo_without_quotes_in(testdir: &str) {
 
 #[test]
 fn sources_unicode_file_in_normal_dir() {
-    try_source_foo_with_quotes_in("foo");
-    try_source_foo_without_quotes_in("foo");
+    try_source_foo_with_single_quotes_in("foo", "source_test_1");
+    try_source_foo_with_double_quotes_in("foo", "source_test_2");
+    try_source_foo_without_quotes_in("foo", "source_test_3");
 }
 
 #[test]
 fn sources_unicode_file_in_unicode_dir_without_spaces_1() {
-    try_source_foo_with_quotes_in("🚒");
-    try_source_foo_without_quotes_in("🚒");
+    try_source_foo_with_single_quotes_in("🚒", "source_test_4");
+    try_source_foo_with_double_quotes_in("🚒", "source_test_5");
+    try_source_foo_without_quotes_in("🚒", "source_test_6");
 }
 
 #[cfg(not(windows))] // ':' is not allowed in Windows paths
 #[test]
 fn sources_unicode_file_in_unicode_dir_without_spaces_2() {
-    try_source_foo_with_quotes_in(":fire_engine:");
-    try_source_foo_without_quotes_in(":fire_engine:");
+    try_source_foo_with_single_quotes_in(":fire_engine:", "source_test_7");
+    try_source_foo_with_double_quotes_in(":fire_engine:", "source_test_8");
+    try_source_foo_without_quotes_in(":fire_engine:", "source_test_9");
 }
 
 #[test]
 fn sources_unicode_file_in_unicode_dir_with_spaces_1() {
-    try_source_foo_with_quotes_in("e-$ èрт🚒♞中片-j");
+    try_source_foo_with_single_quotes_in("e-$ èрт🚒♞中片-j", "source_test_8");
+    try_source_foo_with_double_quotes_in("e-$ èрт🚒♞中片-j", "source_test_9");
 }
 
 #[cfg(not(windows))] // ':' is not allowed in Windows paths
 #[test]
 fn sources_unicode_file_in_unicode_dir_with_spaces_2() {
-    try_source_foo_with_quotes_in("e-$ èрт:fire_engine:♞中片-j");
+    try_source_foo_with_single_quotes_in("e-$ èрт:fire_engine:♞中片-j", "source_test_10");
+    try_source_foo_with_double_quotes_in("e-$ èрт:fire_engine:♞中片-j", "source_test_11");
 }
 
 #[ignore]
