aa06a71e1ffd7e7babf46a886ebdd1744817d04d
Jakub Žádník <kubouch@gmail.com>
2021-09-29 21:56:59 +0300

    Move new commands to the new structure

--- crates/nu-command/src/core_commands/mod.rs
+++ crates/nu-command/src/core_commands/mod.rs
@@ -1,6 +1,8 @@
 mod alias;
 mod def;
 mod do_;
+mod export_def;
+mod hide;
 mod if_;
 mod let_;
 mod module;
@@ -9,6 +11,8 @@ mod use_;
 pub use alias::Alias;
 pub use def::Def;
 pub use do_::Do;
+pub use export_def::ExportDef;
+pub use hide::Hide;
 pub use if_::If;
 pub use let_::Let;
 pub use module::Module;
