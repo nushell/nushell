db3e9efc4bc2dd21531f69c54d650003f9e1bc46
JT <jonathan.d.turner@gmail.com>
2021-10-01 18:20:25 +1300

    fix warnings

--- crates/nu-protocol/src/value/stream.rs
+++ crates/nu-protocol/src/value/stream.rs
@@ -38,19 +38,21 @@ impl Iterator for ValueStream {
 }
 
 impl Serialize for ValueStream {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    fn serialize<S>(&self, _serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
+        // FIXME: implement these
         todo!()
     }
 }
 
 impl<'de> Deserialize<'de> for ValueStream {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    fn deserialize<D>(_deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::Deserializer<'de>,
     {
+        // FIXME: implement these
         todo!()
     }
 }
