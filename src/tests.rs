4dacfaa44a863e242d2bc74eba3ae9d9701476b1
Jakub Žádník <kubouch@gmail.com>
2021-10-04 20:08:24 +0300

    Add import pattern support to 'hide'

--- src/tests.rs
+++ src/tests.rs
@@ -442,6 +442,46 @@ fn hide_twice_not_allowed() -> TestResult {
 }
 
 #[test]
+fn hides_import_1() -> TestResult {
+    fail_test(
+        r#"module spam { export def foo [] { "foo" } }; use spam; hide spam.foo; foo"#,
+        "not found"
+    )
+}
+
+#[test]
+fn hides_import_2() -> TestResult {
+    fail_test(
+        r#"module spam { export def foo [] { "foo" } }; use spam; hide spam.*; foo"#,
+        "not found"
+    )
+}
+
+#[test]
+fn hides_import_3() -> TestResult {
+    fail_test(
+        r#"module spam { export def foo [] { "foo" } }; use spam; hide spam.[foo]; foo"#,
+        "not found"
+    )
+}
+
+#[test]
+fn hides_import_4() -> TestResult {
+    fail_test(
+        r#"module spam { export def foo [] { "foo" } }; use spam.foo; hide foo; foo"#,
+        "not found"
+    )
+}
+
+#[test]
+fn hides_import_5() -> TestResult {
+    fail_test(
+        r#"module spam { export def foo [] { "foo" } }; use spam.*; hide foo; foo"#,
+        "not found"
+    )
+}
+
+#[test]
 fn def_twice_should_fail() -> TestResult {
     fail_test(
         r#"def foo [] { "foo" }; def foo [] { "bar" }"#,
