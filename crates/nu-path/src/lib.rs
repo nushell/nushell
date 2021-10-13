a760e46c1cc2d0f238dd04ba758f5bec5ce36cb9
JT <jonathan.d.turner@gmail.com>
2021-10-13 16:57:05 +1300

    Add config file loading

--- crates/nu-path/src/lib.rs
+++ crates/nu-path/src/lib.rs
@@ -1,8 +1,10 @@
 mod dots;
 mod expansions;
+mod helpers;
 mod tilde;
 mod util;
 
 pub use expansions::{canonicalize, canonicalize_with, expand_path, expand_path_with};
+pub use helpers::{config_dir, home_dir};
 pub use tilde::expand_tilde;
 pub use util::trim_trailing_slash;
