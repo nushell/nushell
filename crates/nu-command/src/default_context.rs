e756a9ea04943a21e1ec688b7316eb0396e05704
onthebridgetonowhere <71919805+onthebridgetonowhere@users.noreply.github.com>
2021-11-11 20:45:39 +0100

    Port str indexof (#327)
    * Port str indexof
    
    * Fix clippy warning
    
    Co-authored-by: Stefan Stanciulescu <contact@stefanstanciulescu.com>

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -104,6 +104,7 @@ pub fn create_default_context() -> EngineState {
             StrContains,
             StrDowncase,
             StrEndswith,
+            StrIndexOf,
             StrFindReplace,
             StrKebabCase,
             StrPascalCase,
--- crates/nu-command/src/strings/str_/mod.rs
+++ crates/nu-command/src/strings/str_/mod.rs
@@ -5,6 +5,7 @@ mod contains;
 mod downcase;
 mod ends_with;
 mod find_replace;
+mod index_of;
 
 pub use capitalize::SubCommand as StrCapitalize;
 pub use case::*;
@@ -13,3 +14,4 @@ pub use contains::SubCommand as StrContains;
 pub use downcase::SubCommand as StrDowncase;
 pub use ends_with::SubCommand as StrEndswith;
 pub use find_replace::SubCommand as StrFindReplace;
+pub use index_of::SubCommand as StrIndexOf;
