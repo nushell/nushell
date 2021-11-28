e07ce574231317623fef21d7100f2d99e0c4beea
Andrew <dev@scorpil.com>
2021-11-28 22:15:32 +0100

    Port over the sleep command from nushell (#371)
    * Port over the sleep command from nushell
    
    * Fix clippy warning
    
    * Remove unused variable

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -116,6 +116,7 @@ pub fn create_default_context() -> EngineState {
             Select,
             Shuffle,
             Size,
+            Sleep,
             Source,
             Split,
             SplitChars,
--- crates/nu-command/src/platform/mod.rs
+++ crates/nu-command/src/platform/mod.rs
@@ -1,3 +1,5 @@
 mod clear;
+mod sleep;
 
 pub use clear::Clear;
+pub use sleep::Sleep;
