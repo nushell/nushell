82aa84706eac6a004021ece0b1339e8462205aac
Jae-Heon Ji <32578710+jaeheonji@users.noreply.github.com>
2021-12-05 02:14:24 +0900

    feat(random): add random-dice (#428)

--- crates/nu-command/src/random/mod.rs
+++ crates/nu-command/src/random/mod.rs
@@ -2,8 +2,10 @@ mod bool;
 mod chars;
 mod command;
 mod decimal;
+mod dice;
 
 pub use self::bool::SubCommand as Bool;
 pub use self::chars::SubCommand as Chars;
 pub use self::decimal::SubCommand as Decimal;
+pub use self::dice::SubCommand as Dice;
 pub use command::RandomCommand as Random;
