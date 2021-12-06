c8b9913718b953c4081eee02845558ead19b436d
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2021-12-06 11:28:11 -0600

    introducing `gstat`, a new command to get the git status (#443)
    * wip - preliminary checking
    
    * updated to latest pluging
    
    * i think it's all working now, except bare words
    
    * clippy

---
+++ crates/nu_plugin_gstat/src/nu/mod.rs
@@ -0,0 +1,28 @@
+use crate::GStat;
+use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
+use nu_protocol::{Signature, Span, Spanned, SyntaxShape, Value};
+
+impl Plugin for GStat {
+    fn signature(&self) -> Vec<Signature> {
+        vec![Signature::build("gstat")
+            .desc("Get the git status of a repo")
+            .optional("path", SyntaxShape::String, "path to repo")]
+    }
+
+    fn run(
+        &mut self,
+        name: &str,
+        call: &EvaluatedCall,
+        input: &Value,
+    ) -> Result<Value, LabeledError> {
+        if name != "gstat" {
+            return Ok(Value::Nothing {
+                span: Span::unknown(),
+            });
+        }
+
+        let repo_path: Option<Spanned<String>> = call.opt(0)?;
+        // eprintln!("input value: {:#?}", &input);
+        self.gstat(input, repo_path, &call.head)
+    }
+}
