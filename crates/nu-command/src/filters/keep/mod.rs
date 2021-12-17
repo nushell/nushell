342584e5f88c9c6536c55229e5847053df141caf
Arthur Targaryen <arthur.targaryen@protonmail.com>
2021-12-17 01:57:02 +0100

    Port `keep`, `keep while` and `keep until` commands (#384)
    * Add `KeepUntil` sub-command
    
    * Add `KeepWhile` sub-command
    
    * Add `Keep` command
    
    * Fix error type

---
+++ crates/nu-command/src/filters/keep/mod.rs
@@ -0,0 +1,7 @@
+mod command;
+mod until;
+mod while_;
+
+pub use command::Keep;
+pub use until::KeepUntil;
+pub use while_::KeepWhile;
---
+++ crates/nu-command/src/filters/keep/while_.rs
@@ -0,0 +1,90 @@
+use nu_engine::eval_block;
+use nu_protocol::{
+    ast::Call,
+    engine::{Command, EngineState, Stack},
+    Category, Example, IntoInterruptiblePipelineData, PipelineData, ShellError, Signature, Span,
+    SyntaxShape, Value,
+};
+
+#[derive(Clone)]
+pub struct KeepWhile;
+
+impl Command for KeepWhile {
+    fn name(&self) -> &str {
+        "keep while"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build(self.name())
+            .required(
+                "predicate",
+                SyntaxShape::RowCondition,
+                "the predicate that kept element must not match",
+            )
+            .category(Category::Filters)
+    }
+
+    fn usage(&self) -> &str {
+        "Keep elements of the input while a predicate is true."
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![Example {
+            description: "Keep while the element is negative",
+            example: "echo [-1 -2 9 1] | keep while $it < 0",
+            result: Some(Value::List {
+                vals: vec![Value::from(-1), Value::from(-2)],
+                span: Span::unknown(),
+            }),
+        }]
+    }
+
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
+        input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        let span = call.head;
+
+        let predicate = &call.positional[0];
+        let block_id = predicate
+            .as_row_condition_block()
+            .ok_or_else(|| ShellError::TypeMismatch("expected row condition".to_owned(), span))?;
+
+        let block = engine_state.get_block(block_id).clone();
+        let var_id = block.signature.get_positional(0).and_then(|arg| arg.var_id);
+
+        let mut stack = stack.collect_captures(&block.captures);
+
+        let ctrlc = engine_state.ctrlc.clone();
+        let engine_state = engine_state.clone();
+
+        Ok(input
+            .into_iter()
+            .take_while(move |value| {
+                if let Some(var_id) = var_id {
+                    stack.add_var(var_id, value.clone());
+                }
+
+                eval_block(&engine_state, &mut stack, &block, PipelineData::new(span))
+                    .map_or(false, |pipeline_data| {
+                        pipeline_data.into_value(span).is_true()
+                    })
+            })
+            .into_pipeline_data(ctrlc))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_examples() {
+        use crate::test_examples;
+
+        test_examples(KeepWhile)
+    }
+}
