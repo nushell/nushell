057bfff0cb2a9bdad9f4e64aeec448a3e094622f
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-01-20 21:31:33 -0600

    add `term size` command (#792)
    * add `term-size` command
    
    * Update term_size.rs
    
    Co-authored-by: JT <547158+jntrnr@users.noreply.github.com>

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -172,6 +172,7 @@ pub fn create_default_context(cwd: impl AsRef<Path>) -> EngineState {
             Input,
             Kill,
             Sleep,
+            TermSize,
         };
 
         // Date
--- crates/nu-command/src/platform/mod.rs
+++ crates/nu-command/src/platform/mod.rs
@@ -3,9 +3,11 @@ mod clear;
 mod input;
 mod kill;
 mod sleep;
+mod term_size;
 
 pub use ansi::{Ansi, AnsiGradient, AnsiStrip};
 pub use clear::Clear;
 pub use input::Input;
 pub use kill::Kill;
 pub use sleep::Sleep;
+pub use term_size::TermSize;
