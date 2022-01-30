67cb720f24912fdffddc9edbfa2241217d1adf14
Julian Aichholz <39018167+rusty-jules@users.noreply.github.com>
2022-01-30 13:41:05 -0800

    Port update cells command (#891)
    * Port update cells command
    
    Clean up, nicer match statements in UpdateCellsIterator
    
    Return columns flag into HashSet errors
    
    Add FIXME: for update cell behavior on nested lists
    
    * Fix: process cells for Record when no columns are specified
    
    * Fix: address clippy lints for unwrap and into_iter
    
    * Fix: don't step into lists and don't bind $it var

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -97,6 +97,7 @@ pub fn create_default_context(cwd: impl AsRef<Path>) -> EngineState {
             Transpose,
             Uniq,
             Update,
+            UpdateCells,
             Where,
             Wrap,
             Zip,
