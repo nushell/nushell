342847fec62b7d73b01052061d2d7d5794ad2f9d
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-02-11 21:31:44 -0600

    changed example description

--- crates/nu-command/src/filters/each_group.rs
+++ crates/nu-command/src/filters/each_group.rs
@@ -43,7 +43,7 @@ impl Command for EachGroup {
 
         vec![Example {
             example: "echo [1 2 3 4] | each group 2 { $it.0 + $it.1 }",
-            description: "Multiplies elements in list",
+            description: "Echo the sum of each pair",
             result: Some(Value::List {
                 vals: stream_test_1,
                 span: Span::test_data(),
