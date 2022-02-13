560be6e73e61f1ad558d8d889c84fb78eb808fd8
Justin Ma <hustcer@outlook.com>
2022-02-13 10:30:37 +0800

    feat: mark str to-datetime as deprecated command (#4448)

---
+++ crates/nu-command/src/deprecated/str_datetime.rs
@@ -0,0 +1,36 @@
+use nu_protocol::{
+    ast::Call,
+    engine::{Command, EngineState, Stack},
+    Category, PipelineData, Signature,
+};
+
+#[derive(Clone)]
+pub struct StrDatetimeDeprecated;
+
+impl Command for StrDatetimeDeprecated {
+    fn name(&self) -> &str {
+        "str to-datetime"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build(self.name()).category(Category::Deprecated)
+    }
+
+    fn usage(&self) -> &str {
+        "Deprecated command"
+    }
+
+    fn run(
+        &self,
+        _engine_state: &EngineState,
+        _stack: &mut Stack,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<nu_protocol::PipelineData, nu_protocol::ShellError> {
+        Err(nu_protocol::ShellError::DeprecatedCommand(
+            self.name().to_string(),
+            "into datetime".to_string(),
+            call.head,
+        ))
+    }
+}
