8f5b857fcf3ba4f563421cd2b90a0e5bd51492dc
Genna Wingert <wingertbe@gmail.com>
2022-02-17 18:49:54 +0100

    Fix ignore to run side effects of previous command (#4510)
    Co-authored-by: Genna Wingert <wingertge@gmail.com>

--- crates/nu-command/src/core_commands/ignore.rs
+++ crates/nu-command/src/core_commands/ignore.rs
@@ -23,8 +23,9 @@ impl Command for Ignore {
         _engine_state: &EngineState,
         _stack: &mut Stack,
         call: &Call,
-        _input: PipelineData,
+        input: PipelineData,
     ) -> Result<nu_protocol::PipelineData, nu_protocol::ShellError> {
+        input.into_value(call.head);
         Ok(PipelineData::new(call.head))
     }
 
