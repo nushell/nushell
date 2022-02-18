a5f9ad2a43d4ae944cf3ffc9b1e8776a6c8a3b40
Justin Ma <hustcer@outlook.com>
2022-02-18 21:06:52 +0800

    Add or update examples for some commands (#4521)
    * chore: add or update examples for some commands
    
    * chore: code formatting

--- crates/nu-command/tests/format_conversions/html.rs
+++ crates/nu-command/tests/format_conversions/html.rs
@@ -76,16 +76,16 @@ fn test_no_color_flag() {
 }
 
 #[test]
-fn test_html_color_where_flag_dark_false() {
+fn test_html_color_cd_flag_dark_false() {
     let actual = nu!(
         cwd: ".", pipeline(
             r#"
-                where --help | to html --html-color
+                cd --help | to html --html-color
             "#
         )
     );
     assert_eq!(
         actual.out,
-        r"<html><style>body { background-color:white;color:black; }</style><body>Filter values based on a condition.<br><br>Usage:<br>  &gt; where &lt;cond&gt; <br><br>Flags:<br>  -h, --help<br>      Display this help message<br><br>Parameters:<br>  cond: condition<br><br></body></html>"
+        r"<html><style>body { background-color:white;color:black; }</style><body>Change directory.<br><br>Usage:<br>  &gt; cd (path) <br><br>Flags:<br>  -h, --help<br>      Display this help message<br><br>Parameters:<br>  (optional) path: the path to change to<br><br></body></html>"
     );
 }
