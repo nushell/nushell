e7fbf950abdfc93a26e840e2219d249b2cc095e4
Joseph T Lyons <JosephTLyons@gmail.com>
2022-02-20 16:53:15 -0500

    Change order of examples

--- crates/nu-command/src/formats/to/json.rs
+++ crates/nu-command/src/formats/to/json.rs
@@ -67,13 +67,7 @@ impl Command for ToJson {
         vec![
             Example {
                 description:
-                    "Outputs an unformatted JSON string representing the contents of this table",
-                example: "[1 2 3] | to json -r",
-                result: Some(Value::test_string("[1,2,3]")),
-            },
-            Example {
-                description:
-                    "Outputs a JSON string representing the contents of this table",
+                    "Outputs a JSON string, with default indentation, representing the contents of this table",
                 example: "[a b c] | to json",
                 result: Some(Value::test_string("[\n  \"a\",\n  \"b\",\n  \"c\"\n]")),
             },
@@ -82,6 +76,12 @@ impl Command for ToJson {
                     "Outputs a JSON string, with 4-space indentation, representing the contents of this table",
                 example: "[Joe Bob Sam] | to json -i 4",
                 result: Some(Value::test_string("[\n    \"Joe\",\n    \"Bob\",\n    \"Sam\"\n]")),
+            },
+            Example {
+                description:
+                    "Outputs an unformatted JSON string representing the contents of this table",
+                example: "[1 2 3] | to json -r",
+                result: Some(Value::test_string("[1,2,3]")),
             },
         ]
     }
