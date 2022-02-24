5f5a0163b1c5a9f8590e544569058d44ec359342
Fernando Herrera <fernando.j.herrera@gmail.com>
2022-02-24 07:51:33 +0000

    split find functions

--- crates/nu-command/src/filters/find.rs
+++ crates/nu-command/src/filters/find.rs
@@ -87,118 +87,136 @@ impl Command for Find {
         call: &Call,
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let span = call.head;
-        let ctrlc = engine_state.ctrlc.clone();
-        let engine_state = engine_state.clone();
-        let metadata = input.metadata();
-        let config = stack.get_config()?;
-
-        let redirect_stdout = call.redirect_stdout;
-        let redirect_stderr = call.redirect_stderr;
-
-        match call.get_flag::<CaptureBlock>(&engine_state, stack, "predicate")? {
-            Some(predicate) => {
-                let capture_block = predicate;
-                let block_id = capture_block.block_id;
-
-                if !call.rest::<Value>(&engine_state, stack, 0)?.is_empty() {
-                    return Err(ShellError::IncompatibleParametersSingle(
-                        "expected either a predicate or terms, not both".to_owned(),
-                        span,
-                    ));
-                }
-
-                let block = engine_state.get_block(block_id).clone();
-                let var_id = block.signature.get_positional(0).and_then(|arg| arg.var_id);
-
-                let mut stack = stack.captures_to_stack(&capture_block.captures);
-
-                input.filter(
-                    move |value| {
-                        if let Some(var_id) = var_id {
-                            stack.add_var(var_id, value.clone());
-                        }
-
-                        eval_block(
-                            &engine_state,
-                            &mut stack,
-                            &block,
-                            PipelineData::new_with_metadata(metadata.clone(), span),
-                            redirect_stdout,
-                            redirect_stderr,
-                        )
-                        .map_or(false, |pipeline_data| {
-                            pipeline_data.into_value(span).is_true()
-                        })
-                    },
-                    ctrlc,
-                )
-            }
-            None => {
-                let terms = call.rest::<Value>(&engine_state, stack, 0)?;
-                let lower_terms = terms
-                    .iter()
-                    .map(|v| {
-                        if let Ok(span) = v.span() {
-                            Value::string(v.into_string("", &config).to_lowercase(), span)
-                        } else {
-                            v.clone()
-                        }
-                    })
-                    .collect::<Vec<Value>>();
-
-                let pipe = input.filter(
-                    move |value| {
-                        let lower_value = if let Ok(span) = value.span() {
-                            Value::string(value.into_string("", &config).to_lowercase(), span)
-                        } else {
-                            value.clone()
-                        };
-                        lower_terms.iter().any(|term| match value {
-                            Value::Bool { .. }
-                            | Value::Int { .. }
-                            | Value::Filesize { .. }
-                            | Value::Duration { .. }
-                            | Value::Date { .. }
-                            | Value::Range { .. }
-                            | Value::Float { .. }
-                            | Value::Block { .. }
-                            | Value::Nothing { .. }
-                            | Value::Error { .. } => lower_value
-                                .eq(span, term)
-                                .map_or(false, |value| value.is_true()),
-                            Value::String { .. }
-                            | Value::List { .. }
-                            | Value::CellPath { .. }
-                            | Value::CustomValue { .. } => term
-                                .r#in(span, &lower_value)
-                                .map_or(false, |value| value.is_true()),
-                            Value::Record { vals, .. } => vals.iter().any(|val| {
-                                if let Ok(span) = val.span() {
-                                    let lower_val = Value::string(
-                                        val.into_string("", &config).to_lowercase(),
-                                        Span::test_data(),
-                                    );
-
-                                    term.r#in(span, &lower_val)
-                                        .map_or(false, |value| value.is_true())
-                                } else {
-                                    term.r#in(span, val).map_or(false, |value| value.is_true())
-                                }
-                            }),
-                            Value::Binary { .. } => false,
-                        })
-                    },
-                    ctrlc,
-                )?;
-                match metadata {
-                    Some(m) => {
-                        Ok(pipe.into_pipeline_data_with_metadata(m, engine_state.ctrlc.clone()))
-                    }
-                    None => Ok(pipe),
-                }
-            }
+        match call.get_flag::<CaptureBlock>(engine_state, stack, "predicate")? {
+            Some(predicate) => find_with_predicate(predicate, engine_state, stack, call, input),
+            None => find_with_rest(engine_state, stack, call, input),
         }
+    }
+}
+
+fn find_with_predicate(
+    predicate: CaptureBlock,
+    engine_state: &EngineState,
+    stack: &mut Stack,
+    call: &Call,
+    input: PipelineData,
+) -> Result<PipelineData, ShellError> {
+    let span = call.head;
+    let ctrlc = engine_state.ctrlc.clone();
+    let metadata = input.metadata();
+    let redirect_stdout = call.redirect_stdout;
+    let redirect_stderr = call.redirect_stderr;
+    let engine_state = engine_state.clone();
+
+    let capture_block = predicate;
+    let block_id = capture_block.block_id;
+
+    if !call.rest::<Value>(&engine_state, stack, 0)?.is_empty() {
+        return Err(ShellError::IncompatibleParametersSingle(
+            "expected either a predicate or terms, not both".to_owned(),
+            span,
+        ));
+    }
+
+    let block = engine_state.get_block(block_id).clone();
+    let var_id = block.signature.get_positional(0).and_then(|arg| arg.var_id);
+
+    let mut stack = stack.captures_to_stack(&capture_block.captures);
+
+    input.filter(
+        move |value| {
+            if let Some(var_id) = var_id {
+                stack.add_var(var_id, value.clone());
+            }
+
+            eval_block(
+                &engine_state,
+                &mut stack,
+                &block,
+                PipelineData::new_with_metadata(metadata.clone(), span),
+                redirect_stdout,
+                redirect_stderr,
+            )
+            .map_or(false, |pipeline_data| {
+                pipeline_data.into_value(span).is_true()
+            })
+        },
+        ctrlc,
+    )
+}
+
+fn find_with_rest(
+    engine_state: &EngineState,
+    stack: &mut Stack,
+    call: &Call,
+    input: PipelineData,
+) -> Result<PipelineData, ShellError> {
+    let span = call.head;
+    let ctrlc = engine_state.ctrlc.clone();
+    let metadata = input.metadata();
+    let engine_state = engine_state.clone();
+    let config = stack.get_config()?;
+
+    let terms = call.rest::<Value>(&engine_state, stack, 0)?;
+    let lower_terms = terms
+        .iter()
+        .map(|v| {
+            if let Ok(span) = v.span() {
+                Value::string(v.into_string("", &config).to_lowercase(), span)
+            } else {
+                v.clone()
+            }
+        })
+        .collect::<Vec<Value>>();
+
+    let pipe = input.filter(
+        move |value| {
+            let lower_value = if let Ok(span) = value.span() {
+                Value::string(value.into_string("", &config).to_lowercase(), span)
+            } else {
+                value.clone()
+            };
+            lower_terms.iter().any(|term| match value {
+                Value::Bool { .. }
+                | Value::Int { .. }
+                | Value::Filesize { .. }
+                | Value::Duration { .. }
+                | Value::Date { .. }
+                | Value::Range { .. }
+                | Value::Float { .. }
+                | Value::Block { .. }
+                | Value::Nothing { .. }
+                | Value::Error { .. } => lower_value
+                    .eq(span, term)
+                    .map_or(false, |value| value.is_true()),
+                Value::String { .. }
+                | Value::List { .. }
+                | Value::CellPath { .. }
+                | Value::CustomValue { .. } => term
+                    .r#in(span, &lower_value)
+                    .map_or(false, |value| value.is_true()),
+                Value::Record { vals, .. } => vals.iter().any(|val| {
+                    if let Ok(span) = val.span() {
+                        let lower_val = Value::string(
+                            val.into_string("", &config).to_lowercase(),
+                            Span::test_data(),
+                        );
+
+                        term.r#in(span, &lower_val)
+                            .map_or(false, |value| value.is_true())
+                    } else {
+                        term.r#in(span, val).map_or(false, |value| value.is_true())
+                    }
+                }),
+                Value::Binary { .. } => false,
+            })
+        },
+        ctrlc,
+    )?;
+
+    match metadata {
+        Some(m) => Ok(pipe.into_pipeline_data_with_metadata(m, engine_state.ctrlc.clone())),
+        None => Ok(pipe),
     }
 }
 
