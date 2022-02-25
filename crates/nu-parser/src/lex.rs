688b28c5a18c0cb86aa6b996e63898f2ffd4849f
JT <jonathan.d.turner@gmail.com>
2022-02-25 11:48:38 -0500

    WIP windows doc comments

--- crates/nu-parser/src/lex.rs
+++ crates/nu-parser/src/lex.rs
@@ -52,6 +52,7 @@ fn is_item_terminator(
         && (c == b' '
             || c == b'\t'
             || c == b'\n'
+            || c == b'\r'
             || c == b'|'
             || c == b';'
             || c == b'#'
@@ -111,7 +112,7 @@ pub fn lex_item(
                 break;
             }
             in_comment = true;
-        } else if c == b'\n' {
+        } else if c == b'\n' || c == b'\r' {
             in_comment = false;
             if is_item_terminator(&block_level, c, additional_whitespace, special_tokens) {
                 break;
@@ -258,7 +259,7 @@ pub fn lex(
                 TokenContents::Semicolon,
                 Span::new(span_offset + idx, span_offset + idx + 1),
             ));
-        } else if c == b'\n' {
+        } else if c == b'\n' || c == b'\r' {
             // If the next character is a newline, we're looking at an EOL (end of line) token.
 
             let idx = curr_offset;
@@ -275,7 +276,7 @@ pub fn lex(
             let mut start = curr_offset;
 
             while let Some(input) = input.get(curr_offset) {
-                if *input == b'\n' {
+                if *input == b'\n' || *input == b'\r' {
                     if !skip_comment {
                         output.push(Token::new(
                             TokenContents::Comment,
