90efe604684c0f62fc0271d996548b61b49a6b9a
JT <jonathan.d.turner@gmail.com>
2022-02-25 12:40:59 -0500

    actual fix this time

--- crates/nu-command/tests/commands/use_.rs
+++ crates/nu-command/tests/commands/use_.rs
@@ -32,3 +32,34 @@ fn use_module_file_within_block() {
         assert_eq!(actual.out, "hello world");
     })
 }
+
+#[test]
+fn use_keeps_doc_comments() {
+    Playground::setup("use_doc_comments", |dirs, nu| {
+        let file = AbsolutePath::new(dirs.test().join("spam.nu"));
+
+        nu.with_files(vec![FileWithContent(
+            &file.display_path(),
+            r#"
+                # this is my foo command
+                export def foo [
+                    x:string # this is an x parameter
+                ] {
+                    echo "hello world"
+                }
+            "#,
+        )]);
+
+        let actual = nu!(
+            cwd: dirs.test(), pipeline(
+                r#"
+                    use spam.nu foo;
+                    help foo
+                "#
+            )
+        );
+
+        assert!(actual.out.contains("this is my foo command"));
+        assert!(actual.out.contains("this is an x parameter"));
+    })
+}
--- crates/nu-parser/src/lex.rs
+++ crates/nu-parser/src/lex.rs
@@ -112,7 +112,7 @@ pub fn lex_item(
                 break;
             }
             in_comment = true;
-        } else if c == b'\n' {
+        } else if c == b'\n' || c == b'\r' {
             in_comment = false;
             if is_item_terminator(&block_level, c, additional_whitespace, special_tokens) {
                 break;
@@ -276,7 +276,7 @@ pub fn lex(
             let mut start = curr_offset;
 
             while let Some(input) = input.get(curr_offset) {
-                if *input == b'\n' || *input == b'\r' {
+                if *input == b'\n' {
                     if !skip_comment {
                         output.push(Token::new(
                             TokenContents::Comment,
