ef70c8dbe4bd097421292d6c3ace6b16f4c71c4a
Jonathan Moore <jtm170330@utdallas.edu>
2022-02-27 19:21:46 -0600

    Date parse refactor (#4661)
    * More flexible and DRY datetime parsing
    
    * Update error messages
    
    * cargo fmt
    
    * clippy
    
    * Add DatetimeParseError

--- crates/nu-command/src/date/utils.rs
+++ crates/nu-command/src/date/utils.rs
@@ -1,43 +1,26 @@
-use chrono::{DateTime, FixedOffset};
+use chrono::{DateTime, FixedOffset, Local, LocalResult, TimeZone};
 use nu_protocol::{ShellError, Span, Value};
 
-pub fn unsupported_input_error(span: Span) -> Value {
-    Value::Error {
-        error: ShellError::UnsupportedInput(
-            String::from(
-                "Only dates with timezones are supported. The following formats are allowed \n
-            * %Y-%m-%d %H:%M:%S %z -- 2020-04-12 22:10:57 +02:00 \n
-            * %Y-%m-%d %H:%M:%S%.6f %z -- 2020-04-12 22:10:57.213231 +02:00 \n
-            * rfc3339 -- 2020-04-12T22:10:57+02:00 \n
-            * rfc2822 -- Tue, 1 Jul 2003 10:52:37 +0200",
-            ),
-            span,
-        ),
-    }
-}
-
-pub fn parse_date_from_string(input: String, span: Span) -> Result<DateTime<FixedOffset>, Value> {
-    let datetime = DateTime::parse_from_str(&input, "%Y-%m-%d %H:%M:%S %z"); // "2020-04-12 22:10:57 +02:00";
-    match datetime {
-        Ok(x) => Ok(x),
-        Err(_) => {
-            let datetime = DateTime::parse_from_str(&input, "%Y-%m-%d %H:%M:%S%.6f %z"); // "2020-04-12 22:10:57.213231 +02:00";
-            match datetime {
-                Ok(x) => Ok(x),
-                Err(_) => {
-                    let datetime = DateTime::parse_from_rfc3339(&input); // "2020-04-12T22:10:57+02:00";
-                    match datetime {
-                        Ok(x) => Ok(x),
-                        Err(_) => {
-                            let datetime = DateTime::parse_from_rfc2822(&input); // "Tue, 1 Jul 2003 10:52:37 +0200";
-                            match datetime {
-                                Ok(x) => Ok(x),
-                                Err(_) => Err(unsupported_input_error(span)),
-                            }
-                        }
-                    }
-                }
+pub(crate) fn parse_date_from_string(
+    input: &str,
+    span: Span,
+) -> Result<DateTime<FixedOffset>, Value> {
+    match dtparse::parse(input) {
+        Ok((native_dt, fixed_offset)) => {
+            let offset = match fixed_offset {
+                Some(fo) => fo,
+                None => *(Local::now().offset()),
+            };
+            match offset.from_local_datetime(&native_dt) {
+                LocalResult::Single(d) => Ok(d),
+                LocalResult::Ambiguous(d, _) => Ok(d),
+                LocalResult::None => Err(Value::Error {
+                    error: ShellError::DatetimeParseError(span),
+                }),
             }
         }
+        Err(_) => Err(Value::Error {
+            error: ShellError::DatetimeParseError(span),
+        }),
     }
 }
