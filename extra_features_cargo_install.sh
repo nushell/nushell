4e065d92524305f336650b2453207e85d89e26ee
hustcer <hustcer@outlook.com>
2022-03-05 07:43:25 +0800

    Update build and install scripts

--- extra_features_cargo_install.sh
+++ extra_features_cargo_install.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
 cargo install --path . --features=extra
+
+NU_PLUGINS=(
+    'nu_plugin_inc'
+    'nu_plugin_gstat'
+    'nu_plugin_query'
+    'nu_plugin_example'
+)
+for plugin in "${NU_PLUGINS[@]}"
+do
+    echo ''
+    echo "----------------------------------------------"
+    echo "Install plugin $plugin from local..."
+    echo "----------------------------------------------"
+    cd crates/$plugin && cargo install --path . && cd ../../
+done
+
+# Uncomment the followding lines to UNINSTALL:
+# echo '' && cargo uninstall nu
+# for plugin in "${NU_PLUGINS[@]}"
+# do
+#     cargo uninstall $plugin
+# done
