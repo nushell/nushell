6f4c0af09ba7a494d1f48b085589212cbc8e728f
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-03-04 16:16:34 -0600

    build script for mac and linux

---
+++ build-all-maclin.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo "-------------------------------------------------------------------"
+echo "Building nushell (nu.exe) with --features=extra and all the plugins"
+echo "-------------------------------------------------------------------"
+echo ""
+
+echo "Building nushell.exe"
+cargo build --features=extra
+echo ""
+
+cd crates/nu_plugin_example
+echo "Building nu_plugin_example.exe"
+cargo build
+echo ""
+
+cd ../../crates/nu_plugin_gstat
+echo "Building nu_plugin_gstat.exe"
+cargo build
+echo ""
+
+cd ../../crates/nu_plugin_inc
+echo "Building nu_plugin_inc.exe"
+cargo build
+echo ""
+
+cd ../../crates/nu_plugin_query
+echo "Building nu_plugin_query.exe"
+cargo build
+echo ""
+
+cd ../..
