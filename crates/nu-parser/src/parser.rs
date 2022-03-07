35ff1076f33be77599442820f631215f8e069596
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-03-07 16:39:16 -0600

    add ansi escape (#4772)
    * add ansi escape
    
    * also add the ability to escape parens
    
    * add a few more escapes that could be problematic for the nushell lang

--- crates/nu-parser/src/parser.rs
+++ crates/nu-parser/src/parser.rs
@@ -2168,8 +2168,52 @@ pub fn unescape_string(bytes: &[u8], span: Span) -> (Vec<u8>, Option<ParseError>
                     output.push(b'/');
                     idx += 1;
                 }
+                Some(b'(') => {
+                    output.push(b'(');
+                    idx += 1;
+                }
+                Some(b')') => {
+                    output.push(b')');
+                    idx += 1;
+                }
+                Some(b'{') => {
+                    output.push(b'{');
+                    idx += 1;
+                }
+                Some(b'}') => {
+                    output.push(b'}');
+                    idx += 1;
+                }
+                Some(b'$') => {
+                    output.push(b'$');
+                    idx += 1;
+                }
+                Some(b'^') => {
+                    output.push(b'^');
+                    idx += 1;
+                }
+                Some(b'#') => {
+                    output.push(b'#');
+                    idx += 1;
+                }
+                Some(b'|') => {
+                    output.push(b'|');
+                    idx += 1;
+                }
+                Some(b'~') => {
+                    output.push(b'~');
+                    idx += 1;
+                }
+                Some(b'a') => {
+                    output.push(0x7);
+                    idx += 1;
+                }
                 Some(b'b') => {
                     output.push(0x8);
+                    idx += 1;
+                }
+                Some(b'e') => {
+                    output.push(0x1b);
                     idx += 1;
                 }
                 Some(b'f') => {
