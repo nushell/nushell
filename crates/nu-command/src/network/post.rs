5c81e81647ba9f86599335c6c95be1fbff50f348
Genna Wingert <wingertge@gmail.com>
2022-03-07 15:23:18 +0100

    Add serialization for JSON and form bodies in `post`

--- crates/nu-command/src/network/post.rs
+++ crates/nu-command/src/network/post.rs
@@ -1,6 +1,6 @@
 use base64::encode;
 use nu_engine::CallExt;
-use nu_protocol::ast::Call;
+use nu_protocol::ast::{Call, PathMember};
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::RawStream;
 use reqwest::{blocking::Response, StatusCode};
@@ -98,6 +98,11 @@ impl Command for SubCommand {
                 example: "post -H [my-header-key my-header-value] url.com",
                 result: None,
             },
+            Example {
+                description: "Post content to url.com with a json body",
+                example: "post -t application/json url.com { field: value }",
+                result: None,
+            },
         ]
     }
 }
@@ -113,6 +118,14 @@ struct Arguments {
     content_type: Option<String>,
     content_length: Option<String>,
 }
+
+#[derive(PartialEq, Eq)]
+enum BodyType {
+    Json,
+    Form,
+    Unknown,
+}
+
 fn run_post(
     engine_state: &EngineState,
     stack: &mut Stack,
@@ -127,8 +140,8 @@ fn run_post(
         user: call.get_flag(engine_state, stack, "user")?,
         password: call.get_flag(engine_state, stack, "password")?,
         insecure: call.get_flag(engine_state, stack, "insecure")?,
-        content_type: call.get_flag(engine_state, stack, "content_type")?,
-        content_length: call.get_flag(engine_state, stack, "content_length")?,
+        content_type: call.get_flag(engine_state, stack, "content-type")?,
+        content_length: call.get_flag(engine_state, stack, "content-length")?,
     };
     helper(engine_state, stack, call, args)
 }
@@ -178,6 +191,13 @@ fn helper(
         (Some(user), _) => Some(encode(&format!("{}:", user))),
         _ => None,
     };
+
+    let body_type = match &args.content_type {
+        Some(it) if it == "application/json" => BodyType::Json,
+        Some(it) if it == "application/x-www-form-urlencoded" => BodyType::Form,
+        _ => BodyType::Unknown,
+    };
+
     let mut request = http_client(args.insecure.is_some()).post(location);
     match body {
         Value::Binary { val, .. } => {
@@ -185,6 +205,24 @@ fn helper(
         }
         Value::String { val, .. } => {
             request = request.body(val);
+        }
+        Value::Record { .. } if body_type == BodyType::Json => {
+            let data = value_to_json_value(&body)?;
+            request = request.json(&data);
+        }
+        Value::Record { .. } if body_type == BodyType::Form => {
+            let data = value_to_json_value(&body)?;
+            request = request.form(&data);
+        }
+        Value::List { vals, .. } if body_type == BodyType::Form => {
+            if vals.len() % 2 != 0 {
+                return Err(ShellError::IOError("unsupported body input".into()));
+            }
+            let data = vals
+                .chunks(2)
+                .map(|it| Ok((it[0].as_string()?, it[1].as_string()?)))
+                .collect::<Result<Vec<(String, String)>, ShellError>>()?;
+            request = request.form(&data)
         }
         _ => {
             return Err(ShellError::IOError("unsupported body input".into()));
@@ -342,6 +380,53 @@ fn helper(
             span,
         )),
     }
+}
+
+pub fn value_to_json_value(v: &Value) -> Result<nu_json::Value, ShellError> {
+    Ok(match v {
+        Value::Bool { val, .. } => nu_json::Value::Bool(*val),
+        Value::Filesize { val, .. } => nu_json::Value::I64(*val),
+        Value::Duration { val, .. } => nu_json::Value::I64(*val),
+        Value::Date { val, .. } => nu_json::Value::String(val.to_string()),
+        Value::Float { val, .. } => nu_json::Value::F64(*val),
+        Value::Int { val, .. } => nu_json::Value::I64(*val),
+        Value::Nothing { .. } => nu_json::Value::Null,
+        Value::String { val, .. } => nu_json::Value::String(val.to_string()),
+        Value::CellPath { val, .. } => nu_json::Value::Array(
+            val.members
+                .iter()
+                .map(|x| match &x {
+                    PathMember::String { val, .. } => Ok(nu_json::Value::String(val.clone())),
+                    PathMember::Int { val, .. } => Ok(nu_json::Value::U64(*val as u64)),
+                })
+                .collect::<Result<Vec<nu_json::Value>, ShellError>>()?,
+        ),
+
+        Value::List { vals, .. } => nu_json::Value::Array(json_list(vals)?),
+        Value::Error { error } => return Err(error.clone()),
+        Value::Block { .. } | Value::Range { .. } => nu_json::Value::Null,
+        Value::Binary { val, .. } => {
+            nu_json::Value::Array(val.iter().map(|x| nu_json::Value::U64(*x as u64)).collect())
+        }
+        Value::Record { cols, vals, .. } => {
+            let mut m = nu_json::Map::new();
+            for (k, v) in cols.iter().zip(vals) {
+                m.insert(k.clone(), value_to_json_value(v)?);
+            }
+            nu_json::Value::Object(m)
+        }
+        Value::CustomValue { val, .. } => val.to_json(),
+    })
+}
+
+fn json_list(input: &[Value]) -> Result<Vec<nu_json::Value>, ShellError> {
+    let mut out = vec![];
+
+    for value in input {
+        out.push(value_to_json_value(value)?);
+    }
+
+    Ok(out)
 }
 
 pub struct BufferedReader<R: Read> {
