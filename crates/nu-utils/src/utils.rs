885692955c38f006557f5ef7820873764fee99dc
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-03-16 16:49:40 -0500

    hopefully make work in non-windows environments

--- crates/nu-utils/src/utils.rs
+++ crates/nu-utils/src/utils.rs
@@ -1,24 +1,25 @@
 use std::io::Result;
 
-#[cfg(windows)]
 pub fn enable_vt_processing() -> Result<()> {
-    use crossterm_winapi::{ConsoleMode, Handle};
+    #[cfg(windows)]
+    {
+        use crossterm_winapi::{ConsoleMode, Handle};
 
-    pub const ENABLE_PROCESSED_OUTPUT: u32 = 0x0001;
-    pub const ENABLE_VIRTUAL_TERMINAL_PROCESSING: u32 = 0x0004;
-    // let mask = ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        pub const ENABLE_PROCESSED_OUTPUT: u32 = 0x0001;
+        pub const ENABLE_VIRTUAL_TERMINAL_PROCESSING: u32 = 0x0004;
+        // let mask = ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 
-    let console_mode = ConsoleMode::from(Handle::current_out_handle()?);
-    let old_mode = console_mode.mode()?;
+        let console_mode = ConsoleMode::from(Handle::current_out_handle()?);
+        let old_mode = console_mode.mode()?;
 
-    // researching odd ansi behavior in windows terminal repo revealed that
-    // enable_processed_output and enable_virtual_terminal_processing should be used
-    // also, instead of checking old_mode & mask, just set the mode already
+        // researching odd ansi behavior in windows terminal repo revealed that
+        // enable_processed_output and enable_virtual_terminal_processing should be used
+        // also, instead of checking old_mode & mask, just set the mode already
 
-    // if old_mode & mask == 0 {
-    console_mode
-        .set_mode(old_mode | ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING)?;
-    // }
-
+        // if old_mode & mask == 0 {
+        console_mode
+            .set_mode(old_mode | ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING)?;
+        // }
+    }
     Ok(())
 }
