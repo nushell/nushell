77103172246cc42e800bc190a52597d1119764a1
Hristo Filaretov <hello@hgf.sh>
2022-04-17 12:39:56 +0200

    Add known external tests (#5216)
    * Add known external tests
    
    * Add some documentation to the tests
    
    * Document test_hello example
    
    * Set PWD in run_test

---
+++ examples/test_hello.rs
@@ -0,0 +1,6 @@
+/// This function is only meant to be used as part of the test suite
+/// as a simple, cross-platform executable with known output.
+
+fn main() {
+    println!("test-hello");
+}
---
+++ src/tests/test_known_external.rs
@@ -0,0 +1,35 @@
+use crate::tests::{fail_test, run_test, TestResult};
+
+#[test]
+fn known_external_runs() -> TestResult {
+    run_test(
+        r#"extern "cargo run" [-q, --example: string, ...args]; cargo run -q --example test_hello"#,
+        "test-hello",
+    )
+}
+
+#[test]
+fn known_external_unknown_flag() -> TestResult {
+    fail_test(
+        r#"extern "cargo run" [-q, --example: string, ...args]; cargo run -d"#,
+        "command doesn't have flag",
+    )
+}
+
+/// GitHub issues #5179, #4618
+#[test]
+fn known_external_alias() -> TestResult {
+    run_test(
+        r#"extern "cargo run" [-q, --example: string, ...args]; alias cr = cargo run; cr -q --example test_hello"#,
+        "test-hello",
+    )
+}
+
+/// GitHub issues #5179, #4618
+#[test]
+fn known_external_subcommand_alias() -> TestResult {
+    run_test(
+        r#"extern "cargo run" [-q, --example: string, ...args]; alias c = cargo; c run -q --example test_hello"#,
+        "test-hello",
+    )
+}
