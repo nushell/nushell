b8c96dd741dd225518bf426494d7a977e008981a
Fernando Herrera <fernando.j.herrera@gmail.com>
2022-04-18 12:11:19 +0100

    csv reader

---
+++ crates/nu-command/src/dataframe/lazy/join.rs
@@ -0,0 +1,130 @@
+use super::into_expression::IntoExpression;
+use crate::dataframe::values::NuLazyFrame;
+use nu_engine::CallExt;
+use nu_protocol::{
+    ast::Call,
+    engine::{Command, EngineState, Stack},
+    Category, Example, PipelineData, ShellError, Signature, SyntaxShape, Value,
+};
+use polars::prelude::{Expr, JoinType};
+
+#[derive(Clone)]
+pub struct LazyJoin;
+
+impl Command for LazyJoin {
+    fn name(&self) -> &str {
+        "dfl join"
+    }
+
+    fn usage(&self) -> &str {
+        "Joins a lazy frame with other lazy frame"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build(self.name())
+            .required("other", SyntaxShape::Any, "LazyFrame to join with")
+            .required("left_on", SyntaxShape::Any, "Left columns to join on")
+            .required("right_on", SyntaxShape::Any, "Right columns to join on")
+            .switch(
+                "inner",
+                "inner joing between lazyframes (default)",
+                Some('i'),
+            )
+            .switch("left", "left join between lazyframes", Some('l'))
+            .switch("outer", "outer join between lazyframes", Some('o'))
+            .switch("cross", "cross join between lazyframes", Some('c'))
+            .named(
+                "suffix",
+                SyntaxShape::String,
+                "Suffix to use on columns with same name",
+                Some('s'),
+            )
+            .category(Category::Custom("lazyframe".into()))
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![Example {
+            description: "",
+            example: "",
+            result: None,
+        }]
+    }
+
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
+        input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        let left = call.has_flag("left");
+        let outer = call.has_flag("outer");
+        let cross = call.has_flag("cross");
+
+        let how = if left {
+            JoinType::Left
+        } else if outer {
+            JoinType::Outer
+        } else if cross {
+            JoinType::Cross
+        } else {
+            JoinType::Inner
+        };
+
+        let other: Value = call.req(engine_state, stack, 0)?;
+        let other = NuLazyFrame::try_from_value(other)?.into_polars();
+
+        let left_on: Value = call.req(engine_state, stack, 1)?;
+        let left_on = left_on.into_expressions()?;
+
+        let right_on: Value = call.req(engine_state, stack, 2)?;
+        let right_on = right_on.into_expressions()?;
+
+        if left_on.len() != right_on.len() {
+            let right_on: Value = call.req(engine_state, stack, 2)?;
+            return Err(ShellError::IncompatibleParametersSingle(
+                "The right column list has a different size to the left column list".into(),
+                right_on.span()?,
+            ));
+        }
+
+        // Checking that both list of expressions are made out of col expressions or strings
+        for (index, list) in vec![(1, &left_on), (2, &left_on)] {
+            if list.iter().any(|expr| !matches!(expr, Expr::Column(..))) {
+                let value: Value = call.req(engine_state, stack, index)?;
+                return Err(ShellError::IncompatibleParametersSingle(
+                    "Expected only a string, col expressions or list of strings".into(),
+                    value.span()?,
+                ));
+            }
+        }
+
+        let suffix: Option<String> = call.get_flag(engine_state, stack, "suffix")?;
+        let suffix = suffix.unwrap_or("_x".into());
+
+        let lazy = NuLazyFrame::try_from_pipeline(input, call.head)?.into_polars();
+        let lazy: NuLazyFrame = lazy
+            .join_builder()
+            .with(other)
+            .left_on(left_on)
+            .right_on(right_on)
+            .how(how)
+            .force_parallel(true)
+            .suffix(suffix)
+            .finish()
+            .into();
+
+        Ok(PipelineData::Value(lazy.into_value(call.head), None))
+    }
+}
+
+//#[cfg(test)]
+//mod test {
+//    use super::super::super::test_dataframe::test_dataframe;
+//    use super::*;
+//
+//    #[test]
+//    fn test_examples() {
+//        test_dataframe(vec![Box::new(LazyJoin {})])
+//    }
+//}
--- crates/nu-command/src/dataframe/lazy/mod.rs
+++ crates/nu-command/src/dataframe/lazy/mod.rs
@@ -6,10 +6,12 @@ mod command;
 mod fetch;
 mod filter;
 mod groupby;
+mod join;
 mod reverse;
 mod sort_by_expr;
 mod to_lazy;
 mod with_column;
+mod open_csv;
 
 use nu_protocol::engine::StateWorkingSet;
 
@@ -19,10 +21,12 @@ use crate::dataframe::lazy::command::LazyDataframe;
 use crate::dataframe::lazy::fetch::LazyFetch;
 use crate::dataframe::lazy::filter::LazyFilter;
 use crate::dataframe::lazy::groupby::ToLazyGroupBy;
+use crate::dataframe::lazy::join::LazyJoin;
 use crate::dataframe::lazy::reverse::LazyReverse;
 use crate::dataframe::lazy::sort_by_expr::LazySortBy;
 use crate::dataframe::lazy::to_lazy::ToLazyFrame;
 use crate::dataframe::lazy::with_column::LazyWithColumn;
+use crate::dataframe::lazy::open_csv::LazyOpenCSV;
 
 pub fn add_lazy_decls(working_set: &mut StateWorkingSet) {
     macro_rules! bind_command {
@@ -41,7 +45,9 @@ pub fn add_lazy_decls(working_set: &mut StateWorkingSet) {
         LazyDataframe,
         LazyFetch,
         LazyFilter,
+        LazyJoin,
         LazyReverse,
+        LazyOpenCSV,
         LazySortBy,
         LazyWithColumn,
         ToLazyFrame,
