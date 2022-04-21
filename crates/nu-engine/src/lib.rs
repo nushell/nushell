ebdd39f199663788e36515499076b6554d300e27
Reilly Wood <reilly.wood@icloud.com>
2022-04-20 22:21:00 -0700

    Delete unused documentation code+test

--- crates/nu-engine/src/lib.rs
+++ crates/nu-engine/src/lib.rs
@@ -7,7 +7,7 @@ mod glob_from;
 
 pub use call_ext::CallExt;
 pub use column::get_columns;
-pub use documentation::{generate_docs, get_brief_help, get_documentation, get_full_help};
+pub use documentation::get_full_help;
 pub use env::*;
 pub use eval::{
     eval_block, eval_call, eval_expression, eval_expression_with_input, eval_operator,
