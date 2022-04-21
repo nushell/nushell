34b9ea4ae09ed4a74b9cfde93bca1414d24f8fe0
Richard Keller <rdk.keller@gmail.com>
2022-04-21 23:30:12 +0200

    Add unit test for MatchAlgorithm

--- crates/nu-cli/src/completions/completion_options.rs
+++ crates/nu-cli/src/completions/completion_options.rs
@@ -5,18 +5,25 @@ pub enum SortBy {
     None,
 }
 
+/// Describes how suggestions should be matched.
 #[derive(Copy, Clone)]
 pub enum MatchAlgorithm {
+    /// Only show suggestions which begin with the given input
+    ///
+    /// Example:
+    /// "git switch" is matched by "git sw"
     Prefix,
 }
 
 impl MatchAlgorithm {
+    /// Returns whether the `needle` search text matches the given `haystack`.
     pub fn matches_str(&self, haystack: &str, needle: &str) -> bool {
         match *self {
             MatchAlgorithm::Prefix => haystack.starts_with(needle),
         }
     }
 
+    /// Returns whether the `needle` search text matches the given `haystack`.
     pub fn matches_u8(&self, haystack: &[u8], needle: &[u8]) -> bool {
         match *self {
             MatchAlgorithm::Prefix => haystack.starts_with(needle),
@@ -40,5 +47,23 @@ impl Default for CompletionOptions {
             sort_by: SortBy::Ascending,
             match_algorithm: MatchAlgorithm::Prefix,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::MatchAlgorithm;
+
+    #[test]
+    fn match_algorithm_prefix() {
+        let algorithm = MatchAlgorithm::Prefix;
+
+        assert!(algorithm.matches_str("example text", ""));
+        assert!(algorithm.matches_str("example text", "examp"));
+        assert!(!algorithm.matches_str("example text", "text"));
+
+        assert!(algorithm.matches_u8(&[1, 2, 3], &[]));
+        assert!(algorithm.matches_u8(&[1, 2, 3], &[1, 2]));
+        assert!(!algorithm.matches_u8(&[1, 2, 3], &[2, 3]));
     }
 }
