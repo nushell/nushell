4586131a85eb2c458d5bfba5021d433c5064d103
Reilly Wood <reilly.wood@icloud.com>
2022-04-22 17:16:26 -0700

    cargo fmt

--- crates/nu-command/src/query/mod.rs
+++ crates/nu-command/src/query/mod.rs
@@ -1,9 +1,9 @@
-mod query_;
 mod db;
 mod json;
+mod query_;
 mod xml;
 
-pub use query_::Query;
 pub use db::SubCommand as QueryDb;
 pub use json::SubCommand as QueryJson;
+pub use query_::Query;
 pub use xml::SubCommand as QueryXml;
