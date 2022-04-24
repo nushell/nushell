48525731eef393efbdd8e13302bffb4754888c79
Jakub Žádník <kubouch@gmail.com>
2022-04-24 19:00:27 +0300

    Add overlay remove tests

--- tests/overlays/mod.rs
+++ tests/overlays/mod.rs
@@ -40,3 +40,35 @@ fn add_overlay_scoped() {
 
     assert!(actual.err.contains("External command"));
 }
+
+#[test]
+fn remove_overlay() {
+    let actual = nu!(
+        cwd: "tests/overlays", pipeline(
+        r#"
+            module spam { export def foo [] { "foo" } };
+            overlay add spam;
+            overlay remove spam;
+            foo
+        "#
+    ));
+
+    assert!(actual.err.contains("External command"));
+}
+
+#[test]
+fn remove_overlay_scoped() {
+    let actual = nu!(
+        cwd: "tests/overlays", pipeline(
+        r#"
+            module spam { export def foo [] { "foo" } };
+            overlay add spam;
+            do {
+                overlay remove spam
+            };
+            foo
+        "#
+    ));
+
+    assert_eq!(actual.out, "foo");
+}
