80d57d70cd48fa0fa40181eaa77fcddea4b3af75
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-05-01 07:44:29 -0500

    a little database cleanup (#5394)

--- crates/nu-command/src/database/mod.rs
+++ crates/nu-command/src/database/mod.rs
@@ -6,8 +6,8 @@ pub use commands::add_commands_decls;
 pub use expressions::add_expression_decls;
 use nu_protocol::engine::StateWorkingSet;
 pub use values::{
-    convert_sqlite_row_to_nu_value, convert_sqlite_value_to_nu_value, open_and_read_sqlite_db,
-    open_connection_in_memory, read_sqlite_db, SQLiteDatabase,
+    convert_sqlite_row_to_nu_value, convert_sqlite_value_to_nu_value, open_connection_in_memory,
+    SQLiteDatabase,
 };
 
 pub fn add_database_decls(working_set: &mut StateWorkingSet) {
