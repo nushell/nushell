3248a44cf812c68a0fe7adf941aeb2899bfec866
pwygab <88221256+merelymyself@users.noreply.github.com>
2022-05-05 23:56:27 +0800

    Update deparse.rs

--- crates/nu-parser/src/deparse.rs
+++ crates/nu-parser/src/deparse.rs
@@ -1,24 +1,21 @@
-fn parse_input(input: &str) -> String {
+pub fn escape_quote_string(input: &str) -> String {
+    let mut output = String::with_capacity(input.len() + 2);
+    output.push('"');
     let mut output = String::with_capacity(input.len());
+    let is_flag = input.starts_with('-');
+    let mut flag_tripped = false;
     for c in input.chars() {
         if c == '"' || c == '\\' {
             output.push('\\');
         }
         output.push(c);
+        if (c == ' ' || c == '=') && is_flag && !flag_tripped {
+            output.push('"');
+            flag_tripped = true;
+        }
+    }
+    if is_flag {
+        output.push('"');
     }
     output
-}
-
-pub fn escape_quote_string(input: &str) -> String {
-    if input.starts_with('-') {
-        parse_input(input)
-    } else {
-        let mut output = String::with_capacity(input.len() + 2);
-        output.push('"');
-
-        output.push_str(&parse_input(input));
-
-        output.push('"');
-        output
-    }
 }
