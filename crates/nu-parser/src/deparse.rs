8b5838ff49d68dea4f86cd0c9c1a8efd63b0c1f9
pwygab <88221256+merelymyself@users.noreply.github.com>
2022-05-05 16:02:35 +0800

    Fixing the flag issue

--- crates/nu-parser/src/deparse.rs
+++ crates/nu-parser/src/deparse.rs
@@ -1,14 +1,18 @@
 pub fn escape_quote_string(input: &str) -> String {
-    let mut output = String::with_capacity(input.len() + 2);
-    output.push('"');
+    if input.starts_with('-') {
+        String::from(input)
+    } else {
+        let mut output = String::with_capacity(input.len() + 2);
+        output.push('"');
 
-    for c in input.chars() {
-        if c == '"' || c == '\\' {
-            output.push('\\');
+        for c in input.chars() {
+            if c == '"' || c == '\\' {
+                output.push('\\');
+            }
+            output.push(c);
         }
-        output.push(c);
-    }
 
-    output.push('"');
-    output
+        output.push('"');
+        output
+    }
 }
