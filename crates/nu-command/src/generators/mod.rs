3ca44c9992d49574234933f3d339658cf77504ba
WindSoilder <WindSoilder@outlook.com>
2022-05-06 17:33:57 +0800

    add seq char command

--- crates/nu-command/src/generators/mod.rs
+++ crates/nu-command/src/generators/mod.rs
@@ -1,9 +1,9 @@
 mod cal;
 mod seq;
-mod seq_date;
 mod seq_char;
+mod seq_date;
 
 pub use cal::Cal;
 pub use seq::Seq;
-pub use seq_date::SeqDate;
 pub use seq_char::SeqChar;
+pub use seq_date::SeqDate;
