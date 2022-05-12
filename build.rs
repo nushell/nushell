20cb9304783f6eaceb3d3dbb80b3bc07de6cb325
Reilly Wood <reilly.wood@icloud.com>
2022-05-11 21:50:44 -0700

    Skip embedding Windows resource (slow) during tests

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
-// #[cfg(windows)]
-// fn main() {
-//     embed_resource::compile_for("assets/nushell.rc", &["nu"])
-// }
+// Skip embedding resource during tests because it's fairly slow and not needed
+#[cfg(all(windows, not(test)))]
+fn main() {
+    embed_resource::compile_for("assets/nushell.rc", &["nu"])
+}
 
-// #[cfg(not(windows))]
+#[cfg(any(not(windows), test))]
 fn main() {}
