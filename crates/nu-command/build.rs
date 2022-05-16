d90b25c6338e0458b9b0acd6f80179eed62f68be
Reilly Wood <26268125+rgwood@users.noreply.github.com>
2022-05-15 21:57:25 -0400

    Look up git commit hash ourselves, drop libgit2 dependency (#5548)

--- crates/nu-command/build.rs
+++ crates/nu-command/build.rs
@@ -1,3 +1,18 @@
+use std::process::Command;
+
 fn main() -> shadow_rs::SdResult<()> {
+    // Look up the current Git commit ourselves instead of relying on shadow_rs,
+    // because shadow_rs does it in a really slow-to-compile way (it builds libgit2)
+    let hash = get_git_hash().expect("failed to get latest git commit hash");
+    println!("cargo:rustc-env=NU_COMMIT_HASH={}", hash);
+
     shadow_rs::new()
+}
+
+fn get_git_hash() -> Result<String, std::io::Error> {
+    let out = Command::new("git").args(["rev-parse", "HEAD"]).output()?;
+    Ok(String::from_utf8(out.stdout)
+        .expect("could not convert stdout to string")
+        .trim()
+        .to_string())
 }
