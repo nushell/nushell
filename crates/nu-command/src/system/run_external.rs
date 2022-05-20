a9eea8188849a8f4daec580aaadb049570c78ba4
WindSoilder <WindSoilder@outlook.com>
2022-05-20 16:06:31 +0800

    only set cwd for child process if cwd exists, and avoid showing error when pwd is not exists

--- crates/nu-command/src/system/run_external.rs
+++ crates/nu-command/src/system/run_external.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::{Command as CommandSys, Stdio};
 use std::sync::atomic::Ordering;
 use std::sync::mpsc;
@@ -339,7 +339,11 @@ impl ExternalCommand {
                 self.create_command(d)?
             };
 
-            process.current_dir(d);
+            // not trying to set current directory if cwd is not exists
+            // or else it will cause process running failed.
+            if Path::new(&d).exists() {
+                process.current_dir(d);
+            }
             process
         } else {
             return Err(ShellError::GenericError(
