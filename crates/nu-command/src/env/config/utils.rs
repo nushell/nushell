5342099d4a6719137424598402a4617e01968099
Frank Zhang <v-frankz@microsoft.com>
2022-05-21 20:05:05 -0700

    Format code

--- crates/nu-command/src/env/config/utils.rs
+++ crates/nu-command/src/env/config/utils.rs
@@ -5,9 +5,9 @@ pub(crate) fn get_editor(engine_state: &EngineState, stack: &mut Stack) -> Strin
     let env_vars = stack.get_env_vars(engine_state);
     if !config.buffer_editor.is_empty() {
         config.buffer_editor.clone()
-    } else if let Some(value) = env_vars.get("EDITOR"){
+    } else if let Some(value) = env_vars.get("EDITOR") {
         value.as_string().expect("Unknown type")
-    } else if let Some(value) = env_vars.get("VISUAL"){
+    } else if let Some(value) = env_vars.get("VISUAL") {
         value.as_string().expect("Unknown type")
     } else if cfg!(target_os = "windows") {
         "notepad".to_string()
