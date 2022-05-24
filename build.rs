235c882b07b2db9e2450ac7aa5962659d5988d50
Reilly Wood <reilly.wood@icloud.com>
2022-05-24 15:39:16 -0700

    Change `embed-resource` dep to slimmer `winres`

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
 #[cfg(windows)]
 fn main() {
-    embed_resource::compile_for("assets/nushell.rc", &["nu"])
+    let mut res = winres::WindowsResource::new();
+    res.set("ProductName", "Nushell");
+    res.set("FileDescription", "Nushell");
+    res.set("LegalCopyright", "Copyright (C) 2022");
+    res.set_icon("assets/nu_logo.ico");
+    res.compile()
+        .expect("Failed to run the Windows resource compiler (rc.exe)");
 }
 
 #[cfg(not(windows))]
