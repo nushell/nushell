72cd7f74a705a2cfae982c01d8c4f0ed9ae088a5
Jakub Žádník <kubouch@gmail.com>
2022-05-26 17:12:26 +0300

    Add 'overlay new' command

--- crates/nu-command/src/core_commands/overlay/mod.rs
+++ crates/nu-command/src/core_commands/overlay/mod.rs
@@ -1,9 +1,11 @@
 mod add;
 mod command;
 mod list;
+mod new;
 mod remove;
 
 pub use add::OverlayAdd;
 pub use command::Overlay;
 pub use list::OverlayList;
+pub use new::OverlayNew;
 pub use remove::OverlayRemove;
