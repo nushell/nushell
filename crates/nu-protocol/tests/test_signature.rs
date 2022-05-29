46eb34b35d61ad572026f1d708b9250f0cdb40fe
Clements <37786996+mjclements@users.noreply.github.com>
2022-05-29 09:14:15 -0400

    Differentiate internal signature from external signature w.r.t. help (#5667)
    * Differentiate internal signature from external signature w.r.t. help
    
    * Add in the --help flag to default externs in default config
    
    * Remove unusued build_extern
    
    Co-authored-by: mjclements <clements.michael.james@gmail.com>

--- crates/nu-protocol/tests/test_signature.rs
+++ crates/nu-protocol/tests/test_signature.rs
@@ -31,13 +31,10 @@ fn test_signature_chained() {
 
     assert_eq!(signature.required_positional.len(), 1);
     assert_eq!(signature.optional_positional.len(), 1);
-    assert_eq!(signature.named.len(), 4); // The 3 above + help
+    assert_eq!(signature.named.len(), 3);
     assert!(signature.rest_positional.is_some());
-    assert_eq!(signature.get_shorts(), vec!['h', 'r', 'n']);
-    assert_eq!(
-        signature.get_names(),
-        vec!["help", "req-named", "named", "switch"]
-    );
+    assert_eq!(signature.get_shorts(), vec!['r', 'n']);
+    assert_eq!(signature.get_names(), vec!["req-named", "named", "switch"]);
     assert_eq!(signature.num_positionals(), 2);
 
     assert_eq!(
