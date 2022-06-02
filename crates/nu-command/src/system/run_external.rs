0e185663447f304b9e91a776c4e16e81c123d947
WindSoilder <WindSoilder@outlook.com>
2022-06-02 16:25:34 +0800

    while run external, convert list argument to str

--- crates/nu-command/src/system/run_external.rs
+++ crates/nu-command/src/system/run_external.rs
@@ -69,20 +69,30 @@ impl Command for External {
                 })
         }
 
-        let args = args
-            .into_iter()
-            .flat_map(|arg| match arg {
-                Value::List { vals, .. } => vals
-                    .into_iter()
-                    .map(value_as_spanned)
-                    .collect::<Vec<Result<Spanned<String>, ShellError>>>(),
-                val => vec![value_as_spanned(val)],
-            })
-            .collect::<Result<Vec<Spanned<String>>, ShellError>>()?;
+        let mut spanned_args = vec![];
+        for one_arg in args {
+            match one_arg {
+                Value::List { vals, span } => {
+                    // turn all the strings in the array into params.
+                    // Example: one_arg may be something like ["ls" "-a"]
+                    // convert it to "ls -a"
+                    let mut sub_args = vec![];
+                    for v in vals {
+                        sub_args.push(v.as_string()?)
+                    }
+                    let arg_as_string = sub_args.join(" ");
+                    spanned_args.push(Spanned {
+                        item: arg_as_string,
+                        span,
+                    })
+                }
+                val => spanned_args.push(value_as_spanned(val)?),
+            }
+        }
 
         let command = ExternalCommand {
             name,
-            args,
+            args: spanned_args,
             redirect_stdout,
             redirect_stderr,
             env_vars: env_vars_str,
