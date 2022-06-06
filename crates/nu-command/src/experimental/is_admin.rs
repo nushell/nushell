b8d253cbd7e831689595e2817f55975ad9e14ad1
pwygab <88221256+merelymyself@users.noreply.github.com>
2022-06-06 19:55:23 +0800

    Attempts to add a command that checks if nushell is running with admin priveleges (#5712)
    * attempts to add is-admin command
    
    * fmt and clippy
    
    * fmt
    
    * Update is_admin.rs
    
    * typos
    
    * typo in example

---
+++ crates/nu-command/src/experimental/is_admin.rs
@@ -0,0 +1,48 @@
+use is_root::is_root;
+use nu_protocol::ast::Call;
+use nu_protocol::engine::{Command, EngineState, Stack};
+use nu_protocol::{Category, Example, IntoPipelineData, PipelineData, Signature, Span, Value};
+
+#[derive(Clone)]
+pub struct IsAdmin;
+
+impl Command for IsAdmin {
+    fn name(&self) -> &str {
+        "is-admin"
+    }
+
+    fn usage(&self) -> &str {
+        "Check if nushell is running with administrator or root privileges"
+    }
+
+    fn signature(&self) -> nu_protocol::Signature {
+        Signature::build("is-admin").category(Category::Core)
+    }
+
+    fn run(
+        &self,
+        _engine_state: &EngineState,
+        _stack: &mut Stack,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<nu_protocol::PipelineData, nu_protocol::ShellError> {
+        Ok(Value::Bool {
+            val: is_root(),
+            span: call.head,
+        }
+        .into_pipeline_data())
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![
+            Example {
+                description: "Echo 'iamroot' if nushell is running with admin/root privileges, and 'iamnotroot' if not.",
+                example: r#"if is-admin { echo "iamroot" } else { echo "iamnotroot" }"#,
+                result: Some(Value::String {
+                    val: "iamnotroot".to_string(),
+                    span: Span::test_data(),
+                }),
+            },
+        ]
+    }
+}
