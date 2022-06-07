3c965a3fe0a6e7e3bfbbc8c645a8ab27804e0ff7
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-06-07 14:16:55 -0500

    make `to text` work more intuitively

--- crates/nu-command/src/formats/to/text.rs
+++ crates/nu-command/src/formats/to/text.rs
@@ -1,7 +1,9 @@
-use nu_protocol::ast::Call;
-use nu_protocol::engine::{Command, EngineState, Stack};
+use chrono_humanize::HumanTime;
 use nu_protocol::{
-    Category, Example, IntoPipelineData, PipelineData, ShellError, Signature, Value,
+    ast::Call,
+    engine::{Command, EngineState, Stack},
+    format_duration, format_filesize_from_conf, Category, Config, Example, IntoPipelineData,
+    PipelineData, ShellError, Signature, Value,
 };
 
 #[derive(Clone)]
@@ -36,7 +38,8 @@ impl Command for ToText {
             "\n"
         };
 
-        let collected_input = input.collect_string(line_ending, config)?;
+        let collected_input = local_into_string(input.into_value(span), line_ending, config);
+
         Ok(Value::String {
             val: collected_input,
             span,
@@ -66,6 +69,44 @@ impl Command for ToText {
 
     fn search_terms(&self) -> Vec<&str> {
         vec!["text", "convert"]
+    }
+}
+
+fn local_into_string(value: Value, separator: &str, config: &Config) -> String {
+    match value {
+        Value::Bool { val, .. } => val.to_string(),
+        Value::Int { val, .. } => val.to_string(),
+        Value::Float { val, .. } => val.to_string(),
+        Value::Filesize { val, .. } => format_filesize_from_conf(val, config),
+        Value::Duration { val, .. } => format_duration(val),
+        Value::Date { val, .. } => {
+            format!("{} ({})", val.to_rfc2822(), HumanTime::from(val))
+        }
+        Value::Range { val, .. } => {
+            format!(
+                "{}..{}",
+                local_into_string(val.from, ", ", config),
+                local_into_string(val.to, ", ", config)
+            )
+        }
+        Value::String { val, .. } => val,
+        Value::List { vals: val, .. } => val
+            .iter()
+            .map(|x| local_into_string(x.clone(), ", ", config))
+            .collect::<Vec<_>>()
+            .join(separator),
+        Value::Record { cols, vals, .. } => cols
+            .iter()
+            .zip(vals.iter())
+            .map(|(x, y)| format!("{}: {}", x, local_into_string(y.clone(), ", ", config)))
+            .collect::<Vec<_>>()
+            .join(separator),
+        Value::Block { val, .. } => format!("<Block {}>", val),
+        Value::Nothing { .. } => String::new(),
+        Value::Error { error } => format!("{:?}", error),
+        Value::Binary { val, .. } => format!("{:?}", val),
+        Value::CellPath { val, .. } => val.into_string(),
+        Value::CustomValue { val, .. } => val.value_string(),
     }
 }
 
