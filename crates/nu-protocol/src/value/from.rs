d24aae1f4570aed0fb74bdda828816666b3dbf48
Fernando Elizalde <fernando.j.herrera@gmail.com>
2022-06-13 14:01:24 -0500

    filesize conversion

--- crates/nu-protocol/src/value/from.rs
+++ crates/nu-protocol/src/value/from.rs
@@ -16,6 +16,8 @@ impl Value {
     pub fn as_i64(&self) -> Result<i64, ShellError> {
         match self {
             Value::Int { val, .. } => Ok(*val),
+            Value::Filesize { val, .. } => Ok(*val),
+            Value::Duration { val, .. } => Ok(*val),
             x => Err(ShellError::CantConvert(
                 "i64".into(),
                 x.get_type().to_string(),
