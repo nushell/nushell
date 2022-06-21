34d7cda1dc4fa9b79e7b9daef53b899fb7559bc8
merelymyself <poonsters6@gmail.com>
2022-06-21 14:30:32 +0800

    fmt

--- crates/nu-command/tests/commands/transpose.rs
+++ crates/nu-command/tests/commands/transpose.rs
@@ -3,11 +3,11 @@ use nu_test_support::{nu, pipeline};
 #[test]
 fn row() {
     let actual = nu!(
-        cwd: dirs.test(), pipeline(
-        r#"
+    cwd: dirs.test(), pipeline(
+    r#"
         [[key value]; [foo 1] [foo 2]] | transpose -r | debug
             "#
-        ));
+    ));
 
     assert_eq!(actual.out.matches("foo").collect(), ["foo"]);
 }
