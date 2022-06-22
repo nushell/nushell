99f6d39737e03ba96f68a74808f15f1c8c485d51
WindSoilder <WindSoilder@outlook.com>
2022-06-22 10:32:39 +0800

    fmt code

--- crates/nu-command/src/network/mod.rs
+++ crates/nu-command/src/network/mod.rs
@@ -1,9 +1,9 @@
 mod fetch;
+mod port;
 mod post;
 mod url;
-mod port;
 
 pub use self::url::*;
 pub use fetch::SubCommand as Fetch;
-pub use post::SubCommand as Post;
 pub use port::SubCommand as Port;
+pub use post::SubCommand as Post;
--- crates/nu-command/src/network/port.rs
+++ crates/nu-command/src/network/port.rs
@@ -18,7 +18,11 @@ impl Command for SubCommand {
 
     fn signature(&self) -> Signature {
         Signature::build("post")
-            .optional("start", SyntaxShape::Int, "The start port to scan (inclusive)")
+            .optional(
+                "start",
+                SyntaxShape::Int,
+                "The start port to scan (inclusive)",
+            )
             .optional("end", SyntaxShape::Int, "The end port to scan (inclusive)")
             .category(Category::Network)
     }
