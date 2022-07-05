658fbcf7969655a538d1547872f13b7fdfaedd60
nibon7 <nibon7@163.com>
2022-07-05 12:08:28 +0800

    Add -n option for echo
    Related #5724
    
    Signed-off-by: nibon7 <nibon7@163.com>

--- crates/nu-command/src/core_commands/echo.rs
+++ crates/nu-command/src/core_commands/echo.rs
@@ -2,7 +2,8 @@ use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::{
-    Category, Example, ListStream, PipelineData, ShellError, Signature, SyntaxShape, Value,
+    Category, Example, ListStream, PipelineData, ShellError, Signature, Span, SyntaxShape, Type,
+    Value,
 };
 
 #[derive(Clone)]
@@ -20,6 +21,7 @@ impl Command for Echo {
     fn signature(&self) -> Signature {
         Signature::build("echo")
             .rest("rest", SyntaxShape::Any, "the values to echo")
+            .switch("no-newline", "Remove trailing newline", Some('n'))
             .category(Category::Core)
     }
 
@@ -34,17 +36,27 @@ impl Command for Echo {
         call: &Call,
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
+        let no_newline = call.has_flag("no-newline");
         call.rest(engine_state, stack, 0).map(|to_be_echoed| {
             let n = to_be_echoed.len();
             match n.cmp(&1usize) {
                 //  More than one value is converted in a stream of values
                 std::cmp::Ordering::Greater => PipelineData::ListStream(
-                    ListStream::from_stream(to_be_echoed.into_iter(), engine_state.ctrlc.clone()),
+                    ListStream::from_stream(
+                        to_be_echoed
+                            .into_iter()
+                            .map(move |it| remove_newline(it, no_newline)),
+                        engine_state.ctrlc.clone(),
+                    ),
                     None,
                 ),
 
                 //  But a single value can be forwarded as it is
-                std::cmp::Ordering::Equal => PipelineData::Value(to_be_echoed[0].clone(), None),
+                std::cmp::Ordering::Equal => {
+                    let value = remove_newline(to_be_echoed[0].clone(), no_newline);
+
+                    PipelineData::Value(value, None)
+                }
 
                 //  When there are no elements, we echo the empty string
                 std::cmp::Ordering::Less => PipelineData::Value(
@@ -70,7 +82,64 @@ impl Command for Echo {
                 example: "echo $nu",
                 result: None,
             },
+            Example {
+                description: "Remove trailing newline from a single string",
+                example: "echo -n 'hello\n\n'",
+                result: Some(Value::test_string("hello\n")),
+            },
+            Example {
+                description: "Remove trailing newline from multi strings",
+                example: "echo -n 'hello\n' 'hello\n\n'",
+                result: Some(Value::List {
+                    vals: vec![Value::test_string("hello"), Value::test_string("hello\n")],
+                    span: Span::test_data(),
+                }),
+            },
+            Example {
+                description: "Remove trailing newline from a string list",
+                example: "echo -n ['hello\n', 'hello\n\n', 'hello\n\n\n']",
+                result: Some(Value::List {
+                    vals: vec![
+                        Value::test_string("hello"),
+                        Value::test_string("hello\n"),
+                        Value::test_string("hello\n\n"),
+                    ],
+                    span: Span::test_data(),
+                }),
+            },
         ]
+    }
+}
+
+fn remove_newline(value: Value, no_newline: bool) -> Value {
+    if !no_newline {
+        return value;
+    }
+
+    match value {
+        Value::List { vals, span } if vals.iter().all(|it| it.get_type() == Type::String) => {
+            let vals = vals
+                .into_iter()
+                .map(|it| {
+                    Value::string(
+                        strip_suffix(unsafe { it.as_string().unwrap_unchecked() }, "\n"),
+                        unsafe { it.span().unwrap_unchecked() },
+                    )
+                })
+                .collect();
+
+            Value::List { vals, span }
+        }
+        Value::String { val, span } => Value::string(strip_suffix(val, "\n"), span),
+        _ => value,
+    }
+}
+
+#[inline]
+fn strip_suffix(s: String, suffix: &str) -> String {
+    match s.strip_suffix(suffix) {
+        Some(s) => s.to_string(),
+        _ => s,
     }
 }
 
