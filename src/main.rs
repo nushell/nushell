895bf10f8531ba587dcc935b0f9091513c8afa59
Jakub Žádník <kubouch@gmail.com>
2022-07-10 14:29:32 +0300

    Revert "make module imports in scripts used for relative path. (#5913)"
    This reverts commit 6dde231dde97f932b67f12ebf7308f1b60378194.

--- src/main.rs
+++ src/main.rs
@@ -206,14 +206,6 @@ fn main() -> Result<()> {
                     is_perf_true(),
                 );
                 // only want to load config and env if relative argument is provided.
-                config_files::read_config_file(
-                    &mut engine_state,
-                    &mut stack,
-                    binary_args.env_file,
-                    is_perf_true(),
-                    true,
-                    false,
-                );
                 if binary_args.config_file.is_some() {
                     config_files::read_config_file(
                         &mut engine_state,
@@ -221,7 +213,15 @@ fn main() -> Result<()> {
                         binary_args.config_file,
                         is_perf_true(),
                         false,
-                        false,
+                    );
+                }
+                if binary_args.env_file.is_some() {
+                    config_files::read_config_file(
+                        &mut engine_state,
+                        &mut stack,
+                        binary_args.env_file,
+                        is_perf_true(),
+                        true,
                     );
                 }
 
@@ -251,14 +251,6 @@ fn main() -> Result<()> {
                     is_perf_true(),
                 );
                 // only want to load config and env if relative argument is provided.
-                config_files::read_config_file(
-                    &mut engine_state,
-                    &mut stack,
-                    binary_args.env_file,
-                    is_perf_true(),
-                    true,
-                    false,
-                );
                 if binary_args.config_file.is_some() {
                     config_files::read_config_file(
                         &mut engine_state,
@@ -266,7 +258,15 @@ fn main() -> Result<()> {
                         binary_args.config_file,
                         is_perf_true(),
                         false,
-                        false,
+                    );
+                }
+                if binary_args.env_file.is_some() {
+                    config_files::read_config_file(
+                        &mut engine_state,
+                        &mut stack,
+                        binary_args.env_file,
+                        is_perf_true(),
+                        true,
                     );
                 }
 
@@ -323,15 +323,8 @@ fn setup_config(
         info!("read_config_file {}:{}:{}", file!(), line!(), column!());
     }
 
-    config_files::read_config_file(engine_state, stack, env_file, is_perf_true(), true, true);
-    config_files::read_config_file(
-        engine_state,
-        stack,
-        config_file,
-        is_perf_true(),
-        false,
-        true,
-    );
+    config_files::read_config_file(engine_state, stack, env_file, is_perf_true(), true);
+    config_files::read_config_file(engine_state, stack, config_file, is_perf_true(), false);
 
     if is_login_shell {
         config_files::read_loginshell_file(engine_state, stack, is_perf_true());
