3ca262cdd8a1744a7d88d4794abe355a49a8ee99
Frank <v-frankz@microsoft.com>
2022-07-11 15:23:43 -0700

    Add unit test for explicit glob

--- crates/nu-command/tests/commands/run_external.rs
+++ crates/nu-command/tests/commands/run_external.rs
@@ -1,4 +1,6 @@
 use nu_test_support::{nu, pipeline};
+use nu_test_support::fs::Stub::EmptyFile;
+use nu_test_support::playground::Playground;
 
 #[test]
 fn better_empty_redirection() {
@@ -13,3 +15,24 @@ fn better_empty_redirection() {
 
     assert!(!actual.out.contains('2'));
 }
+
+#[test]
+fn explicit_glob() {
+    Playground::setup("external with explicit glob", |dirs, sandbox| {
+        sandbox.with_files(vec![
+            EmptyFile("D&D_volume_1.txt"),
+            EmptyFile("D&D_volume_2.txt"),
+            EmptyFile("foo.sh"),
+        ]);
+
+        let actual = nu!(
+            cwd: dirs.test(), pipeline(
+            r#"
+                ^ls | glob '*.txt' | length
+            "#
+        ));
+
+        assert_eq!(actual.out, "2");
+    })
+}
+
