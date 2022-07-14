763b72e6f551cce5b6bae4c1a78d2823576e0454
Frank <v-frankz@microsoft.com>
2022-07-14 08:26:17 -0700

    Add unit tests

--- crates/nu-command/tests/commands/run_external.rs
+++ crates/nu-command/tests/commands/run_external.rs
@@ -35,3 +35,85 @@ fn explicit_glob() {
         assert_eq!(actual.out, "2");
     })
 }
+
+#[test]
+fn bare_word_expand_path_glob() {
+    Playground::setup("bare word should do the expansion", |dirs, sandbox| {
+        sandbox.with_files(vec![
+            EmptyFile("D&D_volume_1.txt"),
+            EmptyFile("D&D_volume_2.txt"),
+            EmptyFile("foo.sh"),
+        ]);
+
+        let actual = nu!(
+            cwd: dirs.test(), pipeline(
+            r#"
+                ^ls *.txt
+            "#
+        ));
+
+        assert!(actual.out.contains("D&D_volume_1.txt"));
+        assert!(actual.out.contains("D&D_volume_2.txt"));
+    })
+}
+
+#[test]
+fn backtick_expand_path_glob() {
+    Playground::setup("backtick should do the expansion", |dirs, sandbox| {
+        sandbox.with_files(vec![
+            EmptyFile("D&D_volume_1.txt"),
+            EmptyFile("D&D_volume_2.txt"),
+            EmptyFile("foo.sh"),
+        ]);
+
+        let actual = nu!(
+            cwd: dirs.test(), pipeline(
+            r#"
+                ^ls `*.txt`
+            "#
+        ));
+
+        assert!(actual.out.contains("D&D_volume_1.txt"));
+        assert!(actual.out.contains("D&D_volume_2.txt"));
+    })
+}
+
+#[test]
+fn single_quote_does_not_expand_path_glob() {
+    Playground::setup("single quote do not run the expansion", |dirs, sandbox| {
+        sandbox.with_files(vec![
+            EmptyFile("D&D_volume_1.txt"),
+            EmptyFile("D&D_volume_2.txt"),
+            EmptyFile("foo.sh"),
+        ]);
+
+        let actual = nu!(
+            cwd: dirs.test(), pipeline(
+            r#"
+                ^ls '*.txt'
+            "#
+        ));
+
+        assert!(actual.err.contains("ls: cannot access '*.txt': No such file or directory"));
+    })
+}
+
+#[test]
+fn double_quote_does_not_expand_path_glob() {
+    Playground::setup("double quote do not run the expansion", |dirs, sandbox| {
+        sandbox.with_files(vec![
+            EmptyFile("D&D_volume_1.txt"),
+            EmptyFile("D&D_volume_2.txt"),
+            EmptyFile("foo.sh"),
+        ]);
+
+        let actual = nu!(
+            cwd: dirs.test(), pipeline(
+            r#"
+                ^ls "*.txt"
+            "#
+        ));
+
+        assert!(actual.err.contains("ls: cannot access '*.txt': No such file or directory"));
+    })
+}
