db9b88089e9f8ac69ce1a2b0e9f3aff2b892bae5
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-07-20 10:09:33 -0500

    enable find to be able to highlight some hits (#6086)
    * enable find to be able to highlight some hits
    
    * oops, deps in the wrong place

--- crates/nu-utils/src/lib.rs
+++ crates/nu-utils/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod utils;
 
-pub use utils::{enable_vt_processing, stderr_write_all_and_flush, stdout_write_all_and_flush};
+pub use utils::{
+    enable_vt_processing, get_ls_colors, stderr_write_all_and_flush, stdout_write_all_and_flush,
+};
