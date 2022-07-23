ff912a309d04a20913c694eba60c90b156502ab3
Mathspy <mathspy257@gmail.com>
2022-07-23 08:59:56 -0400

    Added a failing test for describing plugin CustomValues

--- tests/plugins/custom_values.rs
+++ tests/plugins/custom_values.rs
@@ -24,3 +24,14 @@ fn can_get_custom_value_from_plugin_and_pass_it_over() {
         "I used to be a custom value! My data was (abcxyz)"
     );
 }
+
+#[test]
+fn can_get_describe_plugin_custom_values() {
+    let actual = nu_with_plugins!(
+        cwd: "tests",
+        plugin: ("json", "nu_plugin_custom_values"),
+        "custom-value generate | describe"
+    );
+
+    assert_eq!(actual.out, "CoolCustomValue");
+}
