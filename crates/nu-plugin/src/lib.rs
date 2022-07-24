33e7ea09cb8fc8eb7d4be206f94640373223dc11
Mathspy <mathspy257@gmail.com>
2022-07-24 08:54:59 -0400

    Export PluginData from nu_plugin
    So plugins can create their very own serve_plugin with whatever
    CustomValue behavior they may desire

--- crates/nu-plugin/src/lib.rs
+++ crates/nu-plugin/src/lib.rs
@@ -6,5 +6,5 @@ mod serializers;
 mod plugin_capnp;
 
 pub use plugin::{get_signature, serve_plugin, Plugin, PluginDeclaration};
-pub use protocol::{EvaluatedCall, LabeledError};
+pub use protocol::{EvaluatedCall, LabeledError, PluginData};
 pub use serializers::{capnp::CapnpSerializer, json::JsonSerializer, EncodingType};
