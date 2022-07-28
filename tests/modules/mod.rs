1ff3ccce91a28f191c795a8184e0d5e984a09d0f
Jakub Žádník <kubouch@gmail.com>
2022-07-28 16:33:32 +0300

    Add more tests

--- tests/modules/mod.rs
+++ tests/modules/mod.rs
@@ -3,8 +3,8 @@ use nu_test_support::playground::Playground;
 use nu_test_support::{nu, pipeline};
 
 #[test]
-fn use_private_import() {
-    Playground::setup("module_private_import", |dirs, sandbox| {
+fn module_private_import_decl() {
+    Playground::setup("module_private_import_decl", |dirs, sandbox| {
         sandbox
             .with_files(vec![FileWithContentToBeTrimmed(
                 "main.nu",
@@ -19,6 +19,62 @@ fn use_private_import() {
                 r#"
                     def get-foo [] { "foo" }
                     export def foo-helper [] { get-foo }
+                "#,
+            )]);
+
+        let inp = &[r#"use main.nu foo"#, r#"foo"#];
+
+        let actual = nu!(cwd: dirs.test(), pipeline(&inp.join("; ")));
+
+        assert_eq!(actual.out, "foo");
+    })
+}
+
+#[test]
+fn module_private_import_alias() {
+    Playground::setup("module_private_import_alias", |dirs, sandbox| {
+        sandbox
+            .with_files(vec![FileWithContentToBeTrimmed(
+                "main.nu",
+                r#"
+                    use spam.nu foo-helper
+
+                    export def foo [] { foo-helper }
+                "#,
+            )])
+            .with_files(vec![FileWithContentToBeTrimmed(
+                "spam.nu",
+                r#"
+                    export alias foo-helper = "foo"
+                "#,
+            )]);
+
+        let inp = &[r#"use main.nu foo"#, r#"foo"#];
+
+        let actual = nu!(cwd: dirs.test(), pipeline(&inp.join("; ")));
+
+        assert_eq!(actual.out, "foo");
+    })
+}
+
+// TODO -- doesn't work because modules are never evaluated
+#[ignore]
+#[test]
+fn module_private_import_env() {
+    Playground::setup("module_private_import_env", |dirs, sandbox| {
+        sandbox
+            .with_files(vec![FileWithContentToBeTrimmed(
+                "main.nu",
+                r#"
+                    use spam.nu FOO_HELPER
+
+                    export def foo [] { $env.FOO_HELPER }
+                "#,
+            )])
+            .with_files(vec![FileWithContentToBeTrimmed(
+                "spam.nu",
+                r#"
+                    export env FOO_HELPER { "foo" }
                 "#,
             )]);
 
