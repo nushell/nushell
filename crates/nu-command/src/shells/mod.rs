2fb7180fad1b23425009e1fa713e8ba21e21a571
nibon7 <nibon7@163.com>
2022-08-07 20:36:20 +0800

    Refactor shell switching related code
    Signed-off-by: nibon7 <nibon7@163.com>

--- crates/nu-command/src/shells/mod.rs
+++ crates/nu-command/src/shells/mod.rs
@@ -9,10 +9,19 @@ pub use enter::Enter;
 pub use exit::Exit;
 pub use g::GotoShell;
 pub use n::NextShell;
+use nu_engine::current_dir;
+use nu_protocol::ast::Call;
 use nu_protocol::engine::{EngineState, Stack};
-use nu_protocol::Value;
+use nu_protocol::{PipelineData, ShellError, Span, Value};
 pub use p::PrevShell;
 pub use shells_::Shells;
+
+enum SwitchTo {
+    Next,
+    Prev,
+    Last,
+    Nth(usize),
+}
 
 pub fn get_shells(engine_state: &EngineState, stack: &mut Stack, cwd: Value) -> Vec<Value> {
     let shells = stack.get_env_var(engine_state, "NUSHELL_SHELLS");
@@ -42,4 +51,75 @@ fn get_last_shell(engine_state: &EngineState, stack: &mut Stack) -> usize {
     } else {
         0
     }
+}
+
+fn switch_shell(
+    engine_state: &EngineState,
+    stack: &mut Stack,
+    call: &Call,
+    span: Span,
+    switch_to: SwitchTo,
+) -> Result<PipelineData, ShellError> {
+    let cwd = current_dir(engine_state, stack)?;
+    let cwd = Value::String {
+        val: cwd.to_string_lossy().to_string(),
+        span: call.head,
+    };
+
+    let shells = get_shells(engine_state, stack, cwd);
+    let current_shell = get_current_shell(engine_state, stack);
+
+    let new_shell = match switch_to {
+        SwitchTo::Next => {
+            let mut new_shell = current_shell + 1;
+
+            if new_shell == shells.len() {
+                new_shell = 0;
+            }
+
+            new_shell
+        }
+        SwitchTo::Prev => {
+            if current_shell == 0 {
+                shells.len() - 1
+            } else {
+                current_shell - 1
+            }
+        }
+        SwitchTo::Last => get_last_shell(engine_state, stack),
+        SwitchTo::Nth(n) => n,
+    };
+
+    let new_path = shells
+        .get(new_shell)
+        .ok_or(ShellError::NotFound(span))?
+        .to_owned();
+
+    stack.add_env_var(
+        "NUSHELL_SHELLS".into(),
+        Value::List {
+            vals: shells,
+            span: call.head,
+        },
+    );
+
+    stack.add_env_var(
+        "NUSHELL_CURRENT_SHELL".into(),
+        Value::Int {
+            val: new_shell as i64,
+            span: call.head,
+        },
+    );
+
+    stack.add_env_var(
+        "NUSHELL_LAST_SHELL".into(),
+        Value::Int {
+            val: current_shell as i64,
+            span: call.head,
+        },
+    );
+
+    stack.add_env_var("PWD".into(), new_path);
+
+    Ok(PipelineData::new(call.head))
 }
