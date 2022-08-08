6616044a67c2427b53560e2e2837c9dc5d05cc4a
^x3ro <git@x3ro.dev>
2022-08-08 10:24:10 +0200

    Use special env var to override locale during testing

--- crates/nu-test-support/src/locale_override.rs
+++ crates/nu-test-support/src/locale_override.rs
@@ -1,37 +1,41 @@
-use std::sync::Mutex;
+#![cfg(debug_assertions)]
+
+use std::sync::{Arc, Mutex};
 
 use lazy_static::lazy_static;
+use nu_utils::locale::LOCALE_OVERRIDE_ENV_VAR;
 
 lazy_static! {
-    static ref LC_ALL_MUTEX: Mutex<()> = Mutex::new(());
+    static ref LOCALE_OVERRIDE_MUTEX: Arc<Mutex<()>> = Arc::new(Mutex::new(()));
 }
 
 /// Run a closure in a fake locale environment.
 ///
-/// Before the closure is executed, the environmen variable `LC_ALL` is set to
-/// the value provided by `locale_string`. When the closure is done, the
-/// original `LC_ALL` value is restored.
+/// Before the closure is executed, an environment variable whose name is
+/// defined in `nu_utils::locale::LOCALE_OVERRIDE_ENV_VAR` is set to the value
+/// provided by `locale_string`. When the closure is done, the previous value is
+/// restored.
 ///
 /// Environment variables are global values. So when they are changed by one
 /// thread they are changed for all others. To prevent a test from overwriting
-/// the `LC_ALL` environment variable of another test, a mutex is used.
+/// the environment variable of another test, a mutex is used.
 pub fn with_locale_override(locale_string: &str, func: fn()) {
     let result = {
-        let _lock = LC_ALL_MUTEX
+        let _lock = LOCALE_OVERRIDE_MUTEX
             .lock()
-            .expect("Failed to get mutex lock for fake locale");
+            .expect("Failed to get mutex lock for locale override");
 
-        let saved = std::env::var("LC_ALL").ok();
-        std::env::set_var("LC_ALL", locale_string);
+        let saved = std::env::var(LOCALE_OVERRIDE_ENV_VAR).ok();
+        std::env::set_var(LOCALE_OVERRIDE_ENV_VAR, locale_string);
 
         let result = std::panic::catch_unwind(|| {
             func();
         });
 
         if let Some(locale_str) = saved {
-            std::env::set_var("LC_ALL", locale_str);
+            std::env::set_var(LOCALE_OVERRIDE_ENV_VAR, locale_str);
         } else {
-            std::env::remove_var("LC_ALL");
+            std::env::remove_var(LOCALE_OVERRIDE_ENV_VAR);
         }
 
         result
