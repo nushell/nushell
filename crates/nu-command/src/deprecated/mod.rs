cd0a04f02a41ced37c56a31d76f3102181e235ec
Reilly Wood <26268125+rgwood@users.noreply.github.com>
2022-08-08 07:46:59 -0700

    Delete most deprecated commands (#6260)

--- crates/nu-command/src/deprecated/mod.rs
+++ crates/nu-command/src/deprecated/mod.rs
@@ -1,25 +1,13 @@
+mod deprecated_commands;
 mod hash_base64;
-mod keep_;
-mod keep_until;
-mod keep_while;
-mod match_;
-mod nth;
-mod pivot;
 mod str_datetime;
 mod str_decimal;
 mod str_find_replace;
 mod str_int;
-mod unalias;
 
+pub use deprecated_commands::*;
 pub use hash_base64::HashBase64;
-pub use keep_::KeepDeprecated;
-pub use keep_until::KeepUntilDeprecated;
-pub use keep_while::KeepWhileDeprecated;
-pub use match_::MatchDeprecated;
-pub use nth::NthDeprecated;
-pub use pivot::PivotDeprecated;
 pub use str_datetime::StrDatetimeDeprecated;
 pub use str_decimal::StrDecimalDeprecated;
 pub use str_find_replace::StrFindReplaceDeprecated;
 pub use str_int::StrIntDeprecated;
-pub use unalias::UnaliasDeprecated;
