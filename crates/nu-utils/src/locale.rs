f03fdc024ad425dea888eca0f5cd12d3e78f5ea8
^x3ro <git@x3ro.dev>
2022-08-08 10:24:10 +0200

    Allow overriding locale with special env variable (when not in release)

--- crates/nu-utils/src/locale.rs
+++ crates/nu-utils/src/locale.rs
@@ -1,8 +1,10 @@
 use num_format::Locale;
-use sys_locale::get_locale;
+
+#[cfg(debug_assertions)]
+pub const LOCALE_OVERRIDE_ENV_VAR: &str = "NU_TEST_LOCALE_OVERRIDE";
 
 pub fn get_system_locale() -> Locale {
-    let locale_string = get_locale().unwrap_or_else(|| String::from("en-US"));
+    let locale_string = get_system_locale_string().unwrap_or_else(|| String::from("en-US"));
     // Since get_locale() and Locale::from_name() don't always return the same items
     // we need to try and parse it to match. For instance, a valid locale is de_DE
     // however Locale::from_name() wants only de so we split and parse it out.
@@ -22,4 +24,16 @@ pub fn get_system_locale() -> Locale {
             }
         }
     }
+}
+
+#[cfg(debug_assertions)]
+fn get_system_locale_string() -> Option<String> {
+    std::env::var(LOCALE_OVERRIDE_ENV_VAR)
+        .ok()
+        .or(sys_locale::get_locale())
+}
+
+#[cfg(not(debug_assertions))]
+fn get_system_locale_string() -> Option<String> {
+    sys_locale::get_locale()
 }
