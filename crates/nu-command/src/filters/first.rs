2b6fc9302f09b40a57bda14d918bcf5b4c693db2
volucris1 <satunovura11@gmail.com>
2022-08-15 20:52:54 +0700

    Fix #6330

--- crates/nu-command/src/filters/first.rs
+++ crates/nu-command/src/filters/first.rs
@@ -28,16 +28,6 @@ impl Command for First {
         "Show only the first number of rows."
     }
 
-    fn run(
-        &self,
-        engine_state: &EngineState,
-        stack: &mut Stack,
-        call: &Call,
-        input: PipelineData,
-    ) -> Result<nu_protocol::PipelineData, nu_protocol::ShellError> {
-        first_helper(engine_state, stack, call, input)
-    }
-
     fn examples(&self) -> Vec<Example> {
         vec![
             Example {
@@ -55,110 +45,38 @@ impl Command for First {
             },
         ]
     }
-}
 
-fn first_helper(
-    engine_state: &EngineState,
-    stack: &mut Stack,
-    call: &Call,
-    input: PipelineData,
-) -> Result<nu_protocol::PipelineData, nu_protocol::ShellError> {
-    let head = call.head;
-    let rows: Option<i64> = call.opt(engine_state, stack, 0)?;
-    let mut rows_desired: usize = match rows {
-        Some(x) => x as usize,
-        None => 1,
-    };
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
+        input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        let metadata = input.metadata();
+        let span = call.head;
 
-    let ctrlc = engine_state.ctrlc.clone();
-    let metadata = input.metadata();
+        let rows: Option<i64> = call.opt(engine_state, stack, 0)?;
+        let v: Vec<_> = input.into_iter().collect();
 
-    let mut input_peek = input.into_iter().peekable();
-    if input_peek.peek().is_some() {
-        match input_peek
-            .peek()
-            .ok_or_else(|| {
-                ShellError::GenericError(
-                    "Error in first".into(),
-                    "unable to pick on next value".into(),
-                    Some(call.head),
-                    None,
-                    Vec::new(),
-                )
-            })?
-            .get_type()
-        {
-            Type::Binary => {
-                match &mut input_peek.next() {
-                    Some(v) => match &v {
-                        Value::Binary { val, .. } => {
-                            let bytes = val;
-                            if bytes.len() >= rows_desired {
-                                // We only want to see a certain amount of the binary
-                                // so let's grab those parts
-                                let output_bytes = bytes[0..rows_desired].to_vec();
-                                Ok(Value::Binary {
-                                    val: output_bytes,
-                                    span: head,
-                                }
-                                .into_pipeline_data())
-                            } else {
-                                // if we want more rows that the current chunk size (8192)
-                                // we must gradually get bigger chunks while testing
-                                // if it's within the requested rows_desired size
-                                let mut bigger: Vec<u8> = vec![];
-                                bigger.extend(bytes);
-                                while bigger.len() < rows_desired {
-                                    match input_peek.next() {
-                                        Some(Value::Binary { val, .. }) => bigger.extend(val),
-                                        _ => {
-                                            // We're at the end of our data so let's break out of this loop
-                                            // and set the rows_desired to the size of our data
-                                            rows_desired = bigger.len();
-                                            break;
-                                        }
-                                    }
-                                }
-                                let output_bytes = bigger[0..rows_desired].to_vec();
-                                Ok(Value::Binary {
-                                    val: output_bytes,
-                                    span: head,
-                                }
-                                .into_pipeline_data())
-                            }
-                        }
+        if rows.is_some() {
+            let iter = v.into_iter().take(rows.unwrap() as usize);
 
-                        _ => todo!(),
-                    },
-                    None => Ok(input_peek
-                        .into_iter()
-                        .take(rows_desired)
-                        .into_pipeline_data(ctrlc)
-                        .set_metadata(metadata)),
-                }
-            }
-            _ => {
-                if rows_desired == 1 {
-                    match input_peek.next() {
-                        Some(val) => Ok(val.into_pipeline_data()),
-                        None => Err(ShellError::AccessBeyondEndOfStream(head)),
-                    }
-                } else {
-                    Ok(input_peek
-                        .into_iter()
-                        .take(rows_desired)
-                        .into_pipeline_data(ctrlc)
-                        .set_metadata(metadata))
-                }
+            Ok(iter
+                .into_pipeline_data(engine_state.ctrlc.clone())
+                .set_metadata(metadata))
+        } else {
+            let first = v.into_iter().last();
+
+            if let Some(first) = first {
+                Ok(first.into_pipeline_data().set_metadata(metadata))
+            } else {
+                Ok(PipelineData::new(span).set_metadata(metadata))
             }
         }
-    } else {
-        Err(ShellError::UnsupportedInput(
-            String::from("Cannot perform into string on empty input"),
-            head,
-        ))
     }
 }
+
 #[cfg(test)]
 mod test {
     use super::*;
