99c42582fe1458c34a10c8bf1eefcae9a6104ba3
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-08-19 12:55:54 -0500

    add a split words command (#6363)
    * add a split words command
    
    * changed regex

--- crates/nu-command/src/strings/split/mod.rs
+++ crates/nu-command/src/strings/split/mod.rs
@@ -3,9 +3,11 @@ pub mod column;
 pub mod command;
 pub mod list;
 pub mod row;
+pub mod words;
 
 pub use chars::SubCommand as SplitChars;
 pub use column::SubCommand as SplitColumn;
 pub use command::SplitCommand as Split;
 pub use list::SubCommand as SplitList;
 pub use row::SubCommand as SplitRow;
+pub use words::SubCommand as SplitWords;
