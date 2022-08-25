1e8149820117b4221fb0e46f9980809e74e4665e
nibon7 <nibon7@163.com>
2022-08-25 19:21:54 +0800

    Fix the span of "invalid time zone" (#6411)
    Signed-off-by: nibon7 <nibon7@163.com>
    
    Signed-off-by: nibon7 <nibon7@163.com>

--- crates/nu-command/src/date/to_timezone.rs
+++ crates/nu-command/src/date/to_timezone.rs
@@ -126,7 +126,7 @@ fn _to_timezone(dt: DateTime<FixedOffset>, timezone: &Spanned<String>, span: Spa
     match datetime_in_timezone(&dt, timezone.item.as_str()) {
         Ok(dt) => Value::Date { val: dt, span },
         Err(_) => Value::Error {
-            error: ShellError::UnsupportedInput(String::from("invalid time zone"), span),
+            error: ShellError::UnsupportedInput(String::from("invalid time zone"), timezone.span),
         },
     }
 }
