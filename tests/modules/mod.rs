7497862fef792d1a06e161abc2d5a17cbc148136
kubouch <kubouch@gmail.com>
2022-08-27 15:11:33 +0300

    Tests: Use source-env; Remove redundant tests

--- tests/modules/mod.rs
+++ tests/modules/mod.rs
@@ -83,35 +83,6 @@ fn module_private_import_decl_not_public() {
     })
 }
 
-// TODO -- doesn't work because modules are never evaluated
-#[ignore]
-#[test]
-fn module_private_import_env() {
-    Playground::setup("module_private_import_env", |dirs, sandbox| {
-        sandbox
-            .with_files(vec![FileWithContentToBeTrimmed(
-                "main.nu",
-                r#"
-                    use spam.nu FOO_HELPER
-
-                    export def foo [] { $env.FOO_HELPER }
-                "#,
-            )])
-            .with_files(vec![FileWithContentToBeTrimmed(
-                "spam.nu",
-                r#"
-                    export env FOO_HELPER { "foo" }
-                "#,
-            )]);
-
-        let inp = &[r#"use main.nu foo"#, r#"foo"#];
-
-        let actual = nu!(cwd: dirs.test(), pipeline(&inp.join("; ")));
-
-        assert_eq!(actual.out, "foo");
-    })
-}
-
 #[test]
 fn module_public_import_decl() {
     Playground::setup("module_public_import_decl", |dirs, sandbox| {
@@ -156,33 +127,6 @@ fn module_public_import_alias() {
             )]);
 
         let inp = &[r#"use main.nu foo"#, r#"foo"#];
-
-        let actual = nu!(cwd: dirs.test(), pipeline(&inp.join("; ")));
-
-        assert_eq!(actual.out, "foo");
-    })
-}
-
-// TODO -- doesn't work because modules are never evaluated
-#[ignore]
-#[test]
-fn module_public_import_env() {
-    Playground::setup("module_public_import_decl", |dirs, sandbox| {
-        sandbox
-            .with_files(vec![FileWithContentToBeTrimmed(
-                "main.nu",
-                r#"
-                    export use spam.nu FOO
-                "#,
-            )])
-            .with_files(vec![FileWithContentToBeTrimmed(
-                "spam.nu",
-                r#"
-                    export env FOO { "foo" }
-                "#,
-            )]);
-
-        let inp = &[r#"use main.nu FOO"#, r#"$env.FOO"#];
 
         let actual = nu!(cwd: dirs.test(), pipeline(&inp.join("; ")));
 
@@ -347,7 +291,7 @@ fn module_nested_imports_in_dirs_prefixed() {
 }
 
 #[test]
-fn module_eval_export_env() {
+fn source_env_eval_export_env() {
     Playground::setup("module_eval_export_env", |dirs, sandbox| {
         sandbox.with_files(vec![FileWithContentToBeTrimmed(
             "spam.nu",
@@ -356,7 +300,7 @@ fn module_eval_export_env() {
             "#,
         )]);
 
-        let inp = &[r#"source spam.nu"#, r#"$env.FOO"#];
+        let inp = &[r#"source-env spam.nu"#, r#"$env.FOO"#];
 
         let actual = nu!(cwd: dirs.test(), pipeline(&inp.join("; ")));
 
