3ec53e544c0fc1d62c3b39567c12d682e2694741
WindSoilder <WindSoilder@outlook.com>
2022-09-01 06:33:30 +0800

    remove capnp protocol for plugin... (#6421)
    * remove capnp protocol for plugin...
    
    * remove relative doc

--- crates/nu-plugin/src/lib.rs
+++ crates/nu-plugin/src/lib.rs
@@ -2,11 +2,6 @@ mod plugin;
 mod protocol;
 mod serializers;
 
-#[allow(dead_code)]
-mod plugin_capnp;
-
 pub use plugin::{get_signature, serve_plugin, Plugin, PluginDeclaration};
 pub use protocol::{EvaluatedCall, LabeledError, PluginData, PluginResponse};
-pub use serializers::{
-    capnp::CapnpSerializer, json::JsonSerializer, msgpack::MsgPackSerializer, EncodingType,
-};
+pub use serializers::{json::JsonSerializer, msgpack::MsgPackSerializer, EncodingType};
--- crates/nu_plugin_example/src/main.rs
+++ crates/nu_plugin_example/src/main.rs
@@ -4,7 +4,7 @@ use nu_plugin_example::Example;
 fn main() {
     // When defining your plugin, you can select the Serializer that could be
     // used to encode and decode the messages. The available options are
-    // CapnpSerializer and JsonSerializer. Both are defined in the serializer
+    // MsgPackSerializer and JsonSerializer. Both are defined in the serializer
     // folder in nu-plugin.
     serve_plugin(&mut Example {}, MsgPackSerializer {})
 
