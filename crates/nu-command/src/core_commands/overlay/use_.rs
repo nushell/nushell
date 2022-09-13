2d1fbdd04aca3d1d9f2bec7dcbaca4133de12a21
Gaxx <damn.frank2z@gmail.com>
2022-09-13 15:45:31 -0700

    format

--- crates/nu-command/src/core_commands/overlay/use_.rs
+++ crates/nu-command/src/core_commands/overlay/use_.rs
@@ -56,7 +56,7 @@ impl Command for OverlayUse {
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
         let mut name_arg: Spanned<String> = call.req(engine_state, caller_stack, 0)?;
-        name_arg.item = trim_quotes_string(name_arg.item).to_string();
+        name_arg.item = trim_quotes_string(name_arg.item);
 
         let origin_module_id = if let Some(overlay_expr) = call.positional_nth(0) {
             if let Expr::Overlay(module_id) = overlay_expr.expr {
