e43d4a425f5458a64b407773d7fa75b6f7bc23f2
kubouch <kubouch@gmail.com>
2022-09-25 17:49:52 +0200

    Re-enable environment hiding for `hide`
    To not break virtualenv since the overlay update is not merged yet

--- crates/nu-command/src/core_commands/hide.rs
+++ crates/nu-command/src/core_commands/hide.rs
@@ -1,6 +1,9 @@
-use nu_protocol::ast::Call;
+use nu_protocol::ast::{Call, Expr, Expression};
 use nu_protocol::engine::{Command, EngineState, Stack};
-use nu_protocol::{Category, Example, PipelineData, Signature, Span, SyntaxShape, Value};
+use nu_protocol::{
+    did_you_mean, Category, Example, PipelineData, ShellError, Signature, Span, Spanned,
+    SyntaxShape, Value,
+};
 
 #[derive(Clone)]
 pub struct Hide;
@@ -33,11 +36,53 @@ This command is a parser keyword. For details, check:
 
     fn run(
         &self,
-        _engine_state: &EngineState,
-        _stack: &mut Stack,
+        engine_state: &EngineState,
+        stack: &mut Stack,
         call: &Call,
         _input: PipelineData,
     ) -> Result<nu_protocol::PipelineData, nu_protocol::ShellError> {
+        let env_var_name = if let Some(Expression {
+            expr: Expr::ImportPattern(pat),
+            ..
+        }) = call.positional_nth(0)
+        {
+            Spanned {
+                item: String::from_utf8_lossy(&pat.head.name).to_string(),
+                span: pat.head.span,
+            }
+        } else {
+            return Err(ShellError::GenericError(
+                "Unexpected import".into(),
+                "import pattern not supported".into(),
+                Some(call.head),
+                None,
+                Vec::new(),
+            ));
+        };
+
+        if stack
+            .remove_env_var(engine_state, &env_var_name.item)
+            .is_none()
+        {
+            let all_names: Vec<String> = stack
+                .get_env_var_names(engine_state)
+                .iter()
+                .cloned()
+                .collect();
+            if let Some(closest_match) = did_you_mean(&all_names, &env_var_name.item) {
+                return Err(ShellError::DidYouMeanCustom(
+                    format!("Environment variable '{}' not found", env_var_name.item),
+                    closest_match,
+                    env_var_name.span,
+                ));
+            } else {
+                return Err(ShellError::EnvVarNotFoundAtRuntime(
+                    env_var_name.item,
+                    env_var_name.span,
+                ));
+            }
+        }
+
         Ok(PipelineData::new(call.head))
     }
 
