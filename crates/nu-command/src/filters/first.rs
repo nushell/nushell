16741d8330f52eb2011d5e444d030aaae118fa64
merelymyself <poonsters6@gmail.com>
2022-09-26 09:38:27 +0800

    make `first` behave same way as `last`

--- crates/nu-command/src/filters/first.rs
+++ crates/nu-command/src/filters/first.rs
@@ -43,7 +43,10 @@ impl Command for First {
             Example {
                 description: "Return the first item of a list/table",
                 example: "[1 2 3] | first",
-                result: Some(Value::test_int(1)),
+                result: Some(Value::List {
+                    vals: vec![Value::test_int(1)],
+                    span: Span::test_data(),
+                }),
             },
             Example {
                 description: "Return the first 2 items of a list/table",
@@ -145,20 +148,11 @@ fn first_helper(
                         .set_metadata(metadata)),
                 }
             }
-            _ => {
-                if rows_desired == 1 {
-                    match input_peek.next() {
-                        Some(val) => Ok(val.into_pipeline_data()),
-                        None => Err(ShellError::AccessBeyondEndOfStream(head)),
-                    }
-                } else {
-                    Ok(input_peek
-                        .into_iter()
-                        .take(rows_desired)
-                        .into_pipeline_data(ctrlc)
-                        .set_metadata(metadata))
-                }
-            }
+            _ => Ok(input_peek
+                .into_iter()
+                .take(rows_desired)
+                .into_pipeline_data(ctrlc)
+                .set_metadata(metadata)),
         }
     } else {
         Ok(PipelineData::new(head).set_metadata(metadata))
