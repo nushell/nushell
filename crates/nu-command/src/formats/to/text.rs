f50b3be3809a2107516841cbdd379d907491e286
Zephaniah Ong <zealorez@gmail.com>
2022-10-14 13:52:16 +0800

    Remove duplicate function

--- crates/nu-command/src/formats/to/text.rs
+++ crates/nu-command/src/formats/to/text.rs
@@ -70,10 +70,6 @@ impl Command for ToText {
             },
         ]
     }
-
-    fn search_terms(&self) -> Vec<&str> {
-        vec!["convert"]
-    }
 }
 
 fn local_into_string(value: Value, separator: &str, config: &Config) -> String {
