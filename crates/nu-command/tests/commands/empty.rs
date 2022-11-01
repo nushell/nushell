43aec8cdbef19c8e50d2f147a1ff4669ac2e1014
Leon <L_1_L_0@yahoo.com>
2022-11-02 04:36:54 +1000

    Fix `$in` in blocks given to `any` and `all` (#6951)
    * Fix $in in blocks given to `any` and `all` (closes #6917)
    
    * Fix help message typos
    
    * Fix tests ($in doesn't work in examples?!)
    
    * Fix formatting

--- crates/nu-command/tests/commands/empty.rs
+++ crates/nu-command/tests/commands/empty.rs
@@ -5,17 +5,57 @@ fn reports_emptiness() {
     let actual = nu!(
         cwd: ".", pipeline(
         r#"
-            echo [[are_empty];
-                     [([[check]; [[]]      ])]
-                     [([[check]; [""]      ])]
-                     [([[check]; [{}] ])]
-            ]
-            | get are_empty
+            [[] '' {} null]
             | all {
-              is-empty check
+              is-empty
             }
         "#
     ));
 
     assert_eq!(actual.out, "true");
+}
+
+#[test]
+fn reports_nonemptiness() {
+    let actual = nu!(
+        cwd: ".", pipeline(
+        r#"
+            [[1] ' ' {a:1} 0]
+            | any {
+              is-empty
+            }
+        "#
+    ));
+
+    assert_eq!(actual.out, "false");
+}
+
+#[test]
+fn reports_emptiness_by_columns() {
+    let actual = nu!(
+        cwd: ".", pipeline(
+        r#"
+            [{a:1 b:null c:null} {a:2 b:null c:null}]
+            | any {
+              is-empty b c
+            }
+        "#
+    ));
+
+    assert_eq!(actual.out, "true");
+}
+
+#[test]
+fn reports_nonemptiness_by_columns() {
+    let actual = nu!(
+        cwd: ".", pipeline(
+        r#"
+            [{a:1 b:null c:3} {a:null b:5 c:2}]
+            | any {
+              is-empty a b
+            }
+        "#
+    ));
+
+    assert_eq!(actual.out, "false");
 }
