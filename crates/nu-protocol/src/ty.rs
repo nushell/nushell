223826fda6910eba8bea62a0e6284bbc18cf8fdf
Dan Davison <dandavison7@gmail.com>
2022-11-02 20:01:44 -0400

    Start defining subtype relation

--- crates/nu-protocol/src/ty.rs
+++ crates/nu-protocol/src/ty.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
+use strum_macros::EnumIter;
 
 use std::fmt::Display;
 
 use crate::SyntaxShape;
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Hash)]
+#[derive(Clone, Debug, Default, EnumIter, PartialEq, Eq, Serialize, Deserialize, Hash)]
 pub enum Type {
     Int,
     Float,
@@ -18,6 +19,7 @@ pub enum Type {
     Filesize,
     List(Box<Type>),
     Number,
+    #[default]
     Nothing,
     Record(Vec<(String, Type)>),
     Table(Vec<(String, Type)>),
@@ -30,6 +32,21 @@ pub enum Type {
 }
 
 impl Type {
+    pub fn is_subtype(&self, other: &Type) -> bool {
+        match (self, other) {
+            (t, u) if t == u => true,
+            (Type::Float, Type::Number) => true,
+            (Type::Int, Type::Number) => true,
+            (_, Type::Any) => true,
+            (Type::List(t), Type::List(u)) if t.is_subtype(u) => true, // List is covariant
+            _ => false,
+        }
+    }
+
+    pub fn is_numeric(&self) -> bool {
+        matches!(self, Type::Int | Type::Float | Type::Number)
+    }
+
     pub fn to_shape(&self) -> SyntaxShape {
         match self {
             Type::Int => SyntaxShape::Int,
@@ -97,6 +114,47 @@ impl Display for Type {
             Type::Binary => write!(f, "binary"),
             Type::Custom(custom) => write!(f, "{}", custom),
             Type::Signature => write!(f, "signature"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Type;
+    use strum::IntoEnumIterator;
+
+    mod subtype_relation {
+        use super::*;
+
+        #[test]
+        fn test_reflexivity() {
+            for ty in Type::iter() {
+                assert!(ty.is_subtype(&ty));
+            }
+        }
+
+        #[test]
+        fn test_any_is_top_type() {
+            for ty in Type::iter() {
+                assert!(ty.is_subtype(&Type::Any));
+            }
+        }
+
+        #[test]
+        fn test_number_supertype() {
+            assert!(Type::Int.is_subtype(&Type::Number));
+            assert!(Type::Float.is_subtype(&Type::Number));
+        }
+
+        #[test]
+        fn test_list_covariance() {
+            for ty1 in Type::iter() {
+                for ty2 in Type::iter() {
+                    let list_ty1 = Type::List(Box::new(ty1.clone()));
+                    let list_ty2 = Type::List(Box::new(ty2.clone()));
+                    assert_eq!(list_ty1.is_subtype(&list_ty2), ty1.is_subtype(&ty2));
+                }
+            }
         }
     }
 }
