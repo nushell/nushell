0aad7d30df9f96bfa680c2542965a8bf0fbb8dba
Ricardo Monteiro <ricardo.monteiro@getmanta.com>
2022-11-06 15:27:56 +0000

    Type validation for headers command (#6918)
    cargo clippy lints
    
    tests
    
    format

--- crates/nu-command/tests/commands/headers.rs
+++ crates/nu-command/tests/commands/headers.rs
@@ -29,3 +29,87 @@ fn headers_adds_missing_column_name() {
 
     assert_eq!(actual.out, r#"["r1c1","r2c1"]"#)
 }
+
+#[test]
+fn headers_invalid_column_type_empty_record() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            [[a b]; [{}, 2], [3,4] ]
+            | headers"#
+    ));
+
+    assert!(actual
+        .err
+        .contains("needs compatible type: Null, String, Bool, Float, Int"));
+}
+
+#[test]
+fn headers_invalid_column_type_record() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            [[a b]; [1 ($nu.scope)] [2 2]]
+            | headers"#
+    ));
+
+    assert!(actual
+        .err
+        .contains("needs compatible type: Null, String, Bool, Float, Int"));
+}
+
+#[test]
+fn headers_invalid_column_type_array() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            [[a b]; [[f,g], 2], [3,4] ]
+            | headers"#
+    ));
+
+    assert!(actual
+        .err
+        .contains("needs compatible type: Null, String, Bool, Float, Int"));
+}
+
+#[test]
+fn headers_invalid_column_type_range() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            [[a b]; [(1..5), 2], [3,4] ]
+            | headers"#
+    ));
+
+    assert!(actual
+        .err
+        .contains("needs compatible type: Null, String, Bool, Float, Int"));
+}
+
+#[test]
+fn headers_invalid_column_type_duration() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            [[a b]; [((date now) - (date now)), 2], [3,4] ]
+            | headers"#
+    ));
+
+    assert!(actual
+        .err
+        .contains("needs compatible type: Null, String, Bool, Float, Int"));
+}
+
+#[test]
+fn headers_invalid_column_type_binary() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            [[a b]; [("aa" | into binary), 2], [3,4] ]
+            | headers"#
+    ));
+
+    assert!(actual
+        .err
+        .contains("needs compatible type: Null, String, Bool, Float, Int"));
+}
