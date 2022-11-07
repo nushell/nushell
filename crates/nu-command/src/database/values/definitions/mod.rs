a388f76b481815aa31adbe9b33f249f18da619ec
Reilly Wood <reilly.wood@icloud.com>
2022-11-06 16:14:02 -0800

    Remove sqlparser SQLite commands

--- crates/nu-command/src/database/values/definitions/mod.rs
+++ crates/nu-command/src/database/values/definitions/mod.rs
@@ -1,7 +1,3 @@
-use nu_protocol::{ShellError, Span};
-use serde::{Deserialize, Serialize};
-use std::{fmt::Display, path::PathBuf};
-
 pub mod db;
 pub mod db_column;
 pub mod db_constraint;
@@ -10,24 +6,3 @@ pub mod db_index;
 pub mod db_row;
 pub mod db_schema;
 pub mod db_table;
-
-#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
-pub enum ConnectionDb {
-    Path(PathBuf),
-}
-
-impl Display for ConnectionDb {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Path(path) => write!(f, "{}", path.to_str().unwrap_or("")),
-        }
-    }
-}
-
-impl ConnectionDb {
-    pub fn as_path(&self, _span: Span) -> Result<&PathBuf, ShellError> {
-        match self {
-            Self::Path(path) => Ok(path),
-        }
-    }
-}
