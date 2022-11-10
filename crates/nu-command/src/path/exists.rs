457f7889df2efd6509cd07ddec95b680cc1f10ab
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-11-09 18:54:43 -0600

    use path.try_exist() to fix silent errors (#7069)

--- crates/nu-command/src/path/exists.rs
+++ crates/nu-command/src/path/exists.rs
@@ -105,7 +105,10 @@ If you need to distinguish dirs and files, please use `path type`."#
 fn exists(path: &Path, span: Span, args: &Arguments) -> Value {
     let path = expand_path_with(path, &args.pwd);
     Value::Bool {
-        val: path.exists(),
+        val: match path.try_exists() {
+            Ok(exists) => exists,
+            Err(err) => return Value::Error { error: err.into() },
+        },
         span,
     }
 }
