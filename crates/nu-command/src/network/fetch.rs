660662e9391c07b6924f3cd47fefde90ee9c2ae9
Leon <L_1_L_0@yahoo.com>
2022-11-24 02:21:50 +1000

    Fix fetch/post not erroring on 4xx and 5xx statuses

--- crates/nu-command/src/network/fetch.rs
+++ crates/nu-command/src/network/fetch.rs
@@ -450,7 +450,8 @@ fn helper(
         }
     }
 
-    match request.send() {
+    // Explicitly turn 4xx and 5xx statuses into errors.
+    match request.send().and_then(|r| r.error_for_status()) {
         Ok(resp) => match resp.headers().get("content-type") {
             Some(content_type) => {
                 let content_type = content_type.to_str().map_err(|e| {
