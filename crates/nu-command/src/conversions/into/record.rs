f8c61456332e6b033c085e68169df6e9b4ba3a07
WindSoilder <WindSoilder@outlook.com>
2022-11-24 16:04:25 +0800

    implement into record

--- crates/nu-command/src/conversions/into/record.rs
+++ crates/nu-command/src/conversions/into/record.rs
@@ -1,10 +1,8 @@
-use crate::input_handler::{operate, CellPathOnlyArgs};
 use chrono::{DateTime, Datelike, FixedOffset, Timelike};
-use nu_engine::CallExt;
 use nu_protocol::{
-    ast::{Call, CellPath},
+    ast::Call,
     engine::{Command, EngineState, Stack},
-    Category, Example, PipelineData, ShellError, Signature, Span, SyntaxShape, Type, Value,
+    Category, Example, IntoPipelineData, PipelineData, ShellError, Signature, Span, Type, Value,
 };
 
 #[derive(Clone)]
@@ -22,13 +20,9 @@ impl Command for SubCommand {
                 (Type::Number, Type::Record(vec![])),
                 (Type::String, Type::Record(vec![])),
                 (Type::Bool, Type::Record(vec![])),
-                (Type::List(Box::new(Type::Any)), Type::Table(vec![])),
+                (Type::Table(vec![]), Type::Record(vec![])),
+                (Type::List(Box::new(Type::Any)), Type::Record(vec![])),
             ])
-            .rest(
-                "rest",
-                SyntaxShape::CellPath,
-                "for a data structure input, convert data at the given cell paths",
-            )
             .category(Category::Conversions)
     }
 
@@ -43,74 +37,76 @@ impl Command for SubCommand {
     fn run(
         &self,
         engine_state: &EngineState,
-        stack: &mut Stack,
+        _stack: &mut Stack,
         call: &Call,
         input: PipelineData,
     ) -> Result<nu_protocol::PipelineData, nu_protocol::ShellError> {
-        into_record(engine_state, stack, call, input)
+        into_record(engine_state, call, input)
     }
 
     fn examples(&self) -> Vec<Example> {
         let span = Span::test_data();
         vec![
             Example {
-                description: "Convert value to boolean in table",
-                example: "echo [[value]; ['false'] ['1'] [0] [1.0] [true]] | into bool value",
-                result: Some(Value::List {
+                description: "Convert from one row table to record",
+                example: "echo [[value]; [false]] | into record",
+                result: Some(Value::Record {
+                    cols: vec!["value".to_string()],
+                    vals: vec![Value::boolean(false, span)],
+                    span,
+                }),
+            },
+            Example {
+                description: "Convert from list to record",
+                example: "[1 2 3] | into record",
+                result: Some(Value::Record {
+                    cols: vec!["0".to_string(), "1".to_string(), "2".to_string()],
                     vals: vec![
-                        Value::Record {
-                            cols: vec!["value".to_string()],
-                            vals: vec![Value::boolean(false, span)],
-                            span,
-                        },
-                        Value::Record {
-                            cols: vec!["value".to_string()],
-                            vals: vec![Value::boolean(true, span)],
-                            span,
-                        },
-                        Value::Record {
-                            cols: vec!["value".to_string()],
-                            vals: vec![Value::boolean(false, span)],
-                            span,
-                        },
-                        Value::Record {
-                            cols: vec!["value".to_string()],
-                            vals: vec![Value::boolean(true, span)],
-                            span,
-                        },
-                        Value::Record {
-                            cols: vec!["value".to_string()],
-                            vals: vec![Value::boolean(true, span)],
-                            span,
-                        },
+                        Value::Int { val: 1, span },
+                        Value::Int { val: 2, span },
+                        Value::Int { val: 3, span },
                     ],
                     span,
                 }),
             },
             Example {
-                description: "Convert bool to boolean",
-                example: "true | into bool",
-                result: Some(Value::boolean(true, span)),
+                description: "Convert bool to record",
+                example: "true | into record",
+                result: Some(Value::Record {
+                    cols: vec!["value".to_string()],
+                    vals: vec![Value::boolean(true, span)],
+                    span,
+                }),
             },
             Example {
-                description: "convert integer to boolean",
-                example: "1 | into bool",
-                result: Some(Value::boolean(true, span)),
+                description: "convert integer to record",
+                example: "1 | into record",
+                result: Some(Value::Record {
+                    cols: vec!["value".to_string()],
+                    vals: vec![Value::Int { val: 1, span }],
+                    span,
+                }),
             },
             Example {
-                description: "convert decimal to boolean",
-                example: "0.3 | into bool",
-                result: Some(Value::boolean(true, span)),
+                description: "convert decimal to record",
+                example: "0.3 | into record",
+                result: Some(Value::Record {
+                    cols: vec!["value".to_string()],
+                    vals: vec![Value::Float { val: 0.3, span }],
+                    span,
+                }),
             },
             Example {
-                description: "convert decimal string to boolean",
-                example: "'0.0' | into bool",
-                result: Some(Value::boolean(false, span)),
-            },
-            Example {
-                description: "convert string to boolean",
-                example: "'true' | into bool",
-                result: Some(Value::boolean(true, span)),
+                description: "convert string to record",
+                example: "'true' | into record",
+                result: Some(Value::Record {
+                    cols: vec!["value".to_string()],
+                    vals: vec![Value::String {
+                        val: "true".to_string(),
+                        span,
+                    }],
+                    span,
+                }),
             },
         ]
     }
@@ -118,123 +114,87 @@ impl Command for SubCommand {
 
 fn into_record(
     engine_state: &EngineState,
-    stack: &mut Stack,
     call: &Call,
     input: PipelineData,
 ) -> Result<PipelineData, ShellError> {
-    let cell_paths: Vec<CellPath> = call.rest(engine_state, stack, 0)?;
-    let args = CellPathOnlyArgs::from(cell_paths);
-    let ctrlc = engine_state.ctrlc.clone();
-
-    operate(action, args, input, call.head, ctrlc)
-}
-
-fn action(input: &Value, _args: &CellPathOnlyArgs, span: Span) -> Value {
-    eprintln!(
-        "into_record::action: input = {:?}, type = {:?}, columns = {:?}",
-        &input,
-        &input.get_type(),
-        &input.columns()
-    );
-    match input {
+    let input = input.into_value(call.head);
+    let input_type = input.get_type();
+    let res = match input {
         // Value::Binary
         // Value::Block
-        Value::Bool { val, .. } => Value::Record {
+        Value::Bool { val, span } => Value::Record {
             cols: vec!["value".into()],
-            vals: vec![Value::boolean(*val, span)],
+            vals: vec![Value::boolean(val, span)],
             span,
         },
         // Value::CellPath
         // Value::Closure
         // Value::CustomValue
-        Value::Date { val, .. } => parse_date_into_record(Ok(*val), span),
-        Value::Duration { val, .. } => Value::Record {
+        Value::Date { val, span } => parse_date_into_record(Ok(val), span),
+        Value::Duration { val, span } => Value::Record {
             cols: vec!["value".into()],
-            vals: vec![Value::Duration { val: *val, span }],
+            vals: vec![Value::Duration { val: val, span }],
             span,
         },
         // Value::Error
-        Value::Filesize { val, .. } => Value::Record {
+        Value::Filesize { val, span } => Value::Record {
             cols: vec!["value".into()],
-            vals: vec![Value::Filesize { val: *val, span }],
+            vals: vec![Value::Filesize { val: val, span }],
             span,
         },
-        Value::Float { val, .. } => Value::Record {
+        Value::Float { val, span } => Value::Record {
             cols: vec!["value".into()],
-            vals: vec![Value::float(*val, span)],
+            vals: vec![Value::float(val, span)],
             span,
         },
-        // [ 1 2 3 ] | into record will come here
-        Value::Int { val, .. } => Value::Record {
+        Value::Int { val, span } => Value::Record {
             cols: vec!["value".into()],
-            vals: vec![Value::int(*val, span)],
+            vals: vec![Value::int(val, span)],
             span,
         },
-        //TODO: This is making a table instead of a record
-        // Value::List { vals, .. } => Value::Record {
-        //     cols: vec!["value".into()],
-        //     vals: vec![Value::List {
-        //         vals: vals.to_vec(),
-        //         span,
-        //     }],
-        //     span,
-        // },
-        // Value::List { vals, .. } => {
-        //     let mut values = vec![];
-        //     for (idx, val) in vals.iter().enumerate() {
-        //         values.push(Value::Record {
-        //             cols: vec![format!("col{}", idx)],
-        //             vals: vec![*val],
-        //             span,
-        //         })
-        //     }
-        //     values
-        // }
-        Value::List { vals, .. } => {
-            eprintln!("vals: {:?}", &vals);
-            let mut cols = vec![];
-            let mut values = vec![];
-            for (idx, val) in vals.iter().enumerate() {
-                cols.push(format!("col{}", idx));
-                values.push(val.clone());
+        Value::List { mut vals, span } => match input_type {
+            Type::Table(..) if vals.len() == 1 => {
+                let item = vals.pop().expect("already checked 1 item");
+                item
             }
-            Value::Record {
-                cols,
-                vals: values,
-                span,
+            _ => {
+                let mut cols = vec![];
+                let mut values = vec![];
+                for (idx, val) in vals.into_iter().enumerate() {
+                    cols.push(format!("{idx}"));
+                    values.push(val);
+                }
+                Value::Record {
+                    cols,
+                    vals: values,
+                    span,
+                }
             }
-        }
+        },
         // Value::Nothing
-        // Value::Range { val, .. } => Value::Record {
-        //     cols: vec!["value".into()],
-        //     vals: vec![Value::Range {
-        //         val: val.into_range_iter(ctrlc.clone()),
-        //         span,
-        //     }],
-        //     span,
-        // },
-
-        // Tables come here
-        Value::Record { cols, vals, .. } => {
-            eprint!("cols: {:?}, vals: {:?}", &cols, &vals);
-            Value::Record {
-                cols: cols.to_vec(),
-                vals: vals.to_vec(),
-                span,
+        Value::Range { val, span } => {
+            let mut cols = vec![];
+            let mut vals = vec![];
+            for (idx, val) in val.into_range_iter(engine_state.ctrlc.clone())?.enumerate() {
+                cols.push(format!("{idx}"));
+                vals.push(val);
             }
+            Value::Record { cols, vals, span }
         }
-        Value::String { val, .. } => Value::Record {
+        Value::Record { cols, vals, span } => Value::Record { cols, vals, span },
+        Value::String { val, span } => Value::Record {
             cols: vec!["value".into()],
             vals: vec![Value::string(val, span)],
             span,
         },
-        _ => Value::Error {
+        other => Value::Error {
             error: ShellError::UnsupportedInput(
-                "'into bool' does not support this input".into(),
-                span,
+                "'into record' does not support this input".into(),
+                other.span().unwrap_or(call.head),
             ),
         },
-    }
+    };
+    Ok(res.into_pipeline_data())
 }
 
 fn parse_date_into_record(date: Result<DateTime<FixedOffset>, Value>, span: Span) -> Value {
