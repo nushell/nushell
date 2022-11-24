95d344ab7e52a94d47805392fc2652feff793db0
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-11-24 14:10:19 -0600

    refactored duration, removed others

--- crates/nu-command/src/conversions/into/record.rs
+++ crates/nu-command/src/conversions/into/record.rs
@@ -1,10 +1,10 @@
 use chrono::{DateTime, Datelike, FixedOffset, Timelike};
+use nu_protocol::format_duration_as_timeperiod;
 use nu_protocol::{
     ast::Call,
     engine::{Command, EngineState, Stack},
     Category, Example, IntoPipelineData, PipelineData, ShellError, Signature, Span, Type, Value,
 };
-
 #[derive(Clone)]
 pub struct SubCommand;
 
@@ -16,17 +16,10 @@ impl Command for SubCommand {
     fn signature(&self) -> Signature {
         Signature::build("into record")
             .input_output_types(vec![
-                (Type::Bool, Type::Record(vec![])),
-                // (Type::Date, Type::Record(vec![])),
                 (Type::Duration, Type::Record(vec![])),
-                (Type::Filesize, Type::Record(vec![])),
-                (Type::Float, Type::Record(vec![])),
-                (Type::Int, Type::Record(vec![])),
                 (Type::List(Box::new(Type::Any)), Type::Record(vec![])),
                 (Type::Range, Type::Record(vec![])),
                 (Type::Record(vec![]), Type::Record(vec![])),
-                // (Type::Number, Type::Record(vec![])),
-                (Type::String, Type::Record(vec![])),
                 (Type::Table(vec![]), Type::Record(vec![])),
             ])
             .category(Category::Conversions)
@@ -76,45 +69,6 @@ impl Command for SubCommand {
                 }),
             },
             Example {
-                description: "Convert bool to record",
-                example: "true | into record",
-                result: Some(Value::Record {
-                    cols: vec!["value".to_string()],
-                    vals: vec![Value::boolean(true, span)],
-                    span,
-                }),
-            },
-            Example {
-                description: "convert integer to record",
-                example: "1 | into record",
-                result: Some(Value::Record {
-                    cols: vec!["value".to_string()],
-                    vals: vec![Value::Int { val: 1, span }],
-                    span,
-                }),
-            },
-            Example {
-                description: "convert decimal to record",
-                example: "0.3 | into record",
-                result: Some(Value::Record {
-                    cols: vec!["value".to_string()],
-                    vals: vec![Value::Float { val: 0.3, span }],
-                    span,
-                }),
-            },
-            Example {
-                description: "convert string to record",
-                example: "'true' | into record",
-                result: Some(Value::Record {
-                    cols: vec!["value".to_string()],
-                    vals: vec![Value::String {
-                        val: "true".to_string(),
-                        span,
-                    }],
-                    span,
-                }),
-            },
-            Example {
                 description: "Convert from range to record",
                 example: "0..2 | into record",
                 result: Some(Value::Record {
@@ -129,22 +83,25 @@ impl Command for SubCommand {
             },
             Example {
                 description: "convert duration to record",
-                example: "1sec | into record",
+                example: "-500day | into record",
                 result: Some(Value::Record {
-                    cols: vec!["value".to_string()],
-                    vals: vec![Value::Duration {
-                        val: 1000 * 1000 * 1000,
-                        span,
-                    }],
-                    span,
-                }),
-            },
-            Example {
-                description: "convert filesize to record",
-                example: "10b | into record",
-                result: Some(Value::Record {
-                    cols: vec!["value".to_string()],
-                    vals: vec![Value::Filesize { val: 10, span }],
+                    cols: vec![
+                        "yr".into(),
+                        "month".into(),
+                        "wk".into(),
+                        "day".into(),
+                        "sign".into(),
+                    ],
+                    vals: vec![
+                        Value::Int { val: 1, span },
+                        Value::Int { val: 4, span },
+                        Value::Int { val: 2, span },
+                        Value::Int { val: 1, span },
+                        Value::String {
+                            val: "-".into(),
+                            span,
+                        },
+                    ],
                     span,
                 }),
             },
@@ -198,38 +155,8 @@ fn into_record(
     let input = input.into_value(call.head);
     let input_type = input.get_type();
     let res = match input {
-        // Value::Binary
-        // Value::Block
-        Value::Bool { val, span } => Value::Record {
-            cols: vec!["value".into()],
-            vals: vec![Value::boolean(val, span)],
-            span,
-        },
-        // Value::CellPath
-        // Value::Closure
-        // Value::CustomValue
         Value::Date { val, span } => parse_date_into_record(Ok(val), span),
-        Value::Duration { val, span } => Value::Record {
-            cols: vec!["value".into()],
-            vals: vec![Value::Duration { val, span }],
-            span,
-        },
-        // Value::Error
-        Value::Filesize { val, span } => Value::Record {
-            cols: vec!["value".into()],
-            vals: vec![Value::Filesize { val, span }],
-            span,
-        },
-        Value::Float { val, span } => Value::Record {
-            cols: vec!["value".into()],
-            vals: vec![Value::float(val, span)],
-            span,
-        },
-        Value::Int { val, span } => Value::Record {
-            cols: vec!["value".into()],
-            vals: vec![Value::int(val, span)],
-            span,
-        },
+        Value::Duration { val, span } => parse_duration_into_record(val, span),
         Value::List { mut vals, span } => match input_type {
             Type::Table(..) if vals.len() == 1 => vals.pop().expect("already checked 1 item"),
             _ => {
@@ -246,7 +173,6 @@ fn into_record(
                 }
             }
         },
-        // Value::Nothing
         Value::Range { val, span } => {
             let mut cols = vec![];
             let mut vals = vec![];
@@ -257,11 +183,6 @@ fn into_record(
             Value::Record { cols, vals, span }
         }
         Value::Record { cols, vals, span } => Value::Record { cols, vals, span },
-        Value::String { val, span } => Value::Record {
-            cols: vec!["value".into()],
-            vals: vec![Value::string(val, span)],
-            span,
-        },
         other => Value::Error {
             error: ShellError::UnsupportedInput(
                 "'into record' does not support this input".into(),
@@ -318,6 +239,30 @@ fn parse_date_into_record(date: Result<DateTime<FixedOffset>, Value>, span: Span
         }
         Err(e) => e,
     }
+}
+
+fn parse_duration_into_record(duration: i64, span: Span) -> Value {
+    let (sign, periods) = format_duration_as_timeperiod(duration);
+
+    let mut cols = vec![];
+    let mut vals = vec![];
+    for p in periods {
+        let num_with_unit = p.to_text().to_string();
+        let split = num_with_unit.split(' ').collect::<Vec<&str>>();
+        cols.push(split[1].to_string());
+        vals.push(Value::Int {
+            val: split[0].parse::<i64>().unwrap_or(0),
+            span,
+        });
+    }
+
+    cols.push("sign".into());
+    vals.push(Value::String {
+        val: if sign == -1 { "-".into() } else { "+".into() },
+        span,
+    });
+
+    Value::Record { cols, vals, span }
 }
 
 #[cfg(test)]
