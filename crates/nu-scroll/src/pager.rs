3a52ee30f99ea43e72e902eb2af7514af75d08f9
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-11-25 08:50:46 -0600

    fmt

--- crates/nu-scroll/src/pager.rs
+++ crates/nu-scroll/src/pager.rs
@@ -1015,7 +1015,7 @@ pub fn nu_ansi_color_to_tui_color(clr: NuColor) -> Option<tui::style::Color> {
         White => Color::White,
         Fixed(i) => Color::Indexed(i),
         Rgb(r, g, b) => tui::style::Color::Rgb(r, g, b),
-        LightGray => Color::Gray,   // todo: make a PR to add the color
+        LightGray => Color::Gray, // todo: make a PR to add the color
         LightPurple => Color::LightMagenta,
         Purple => Color::Magenta,
         Default => return None,
