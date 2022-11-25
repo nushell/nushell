23b8bcf23c5093007ee892b88b6758cd97d9f1f4
Maxim Zhiburt <zhiburt@gmail.com>
2022-11-25 17:01:53 +0300

    Allow search in welcome screen
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- crates/nu-scroll/src/views/information.rs
+++ crates/nu-scroll/src/views/information.rs
@@ -1,46 +1,59 @@
 use crossterm::event::KeyEvent;
 use nu_protocol::engine::{EngineState, Stack};
-use tui::{
-    layout::Rect,
-    text::{Span, Spans},
-    widgets::Paragraph,
-};
+use nu_table::TextStyle;
+use tui::{layout::Rect, widgets::Paragraph};
 
-use crate::pager::{Frame, Transition, ViewConfig, ViewInfo};
+use crate::{
+    nu_common::NuText,
+    pager::{Frame, Transition, ViewConfig, ViewInfo},
+};
 
 use super::{Layout, View};
 
 #[derive(Debug, Default)]
 pub struct InformationView;
 
+impl InformationView {
+    const MESSAGE: [&'static str; 7] = [
+        "Scroll",
+        "",
+        "Scroll helps you dynamically navigate through your data",
+        "",
+        "type :help<Enter> for help",
+        "type :q<Enter> to exit",
+        "type :try<Enter> to enter a REPL",
+    ];
+}
+
 impl View for InformationView {
-    fn draw(&mut self, f: &mut Frame, area: Rect, _: &ViewConfig, _: &mut Layout) {
-        let message = [
-            "Scroll",
-            "",
-            "Scroll helps you dynamically navigate through your data",
-            "",
-            "type :help<Enter> for help",
-            "type :q<Enter> to exit",
-            "type :try<Enter> to enter a REPL",
-        ];
-        let count_lines = message.len() as u16;
+    fn draw(&mut self, f: &mut Frame, area: Rect, _: &ViewConfig, layout: &mut Layout) {
+        let count_lines = Self::MESSAGE.len() as u16;
 
         if area.height < count_lines {
             return;
         }
 
-        let spans = message
-            .into_iter()
-            .map(|line| Spans::from(vec![Span::raw(line)]))
-            .collect::<Vec<_>>();
+        let centerh = area.height / 2;
+        let centerw = area.width / 2;
 
-        let paragraph = Paragraph::new(spans).alignment(tui::layout::Alignment::Center);
+        let mut y = centerh.saturating_sub(count_lines);
+        for mut line in Self::MESSAGE {
+            let mut line_width = line.len() as u16;
+            if line_width > area.width {
+                line_width = area.width;
+                line = &line[..area.width as usize];
+            }
 
-        let y = (area.height / 2).saturating_sub(count_lines);
-        let area = Rect::new(area.x, y, area.width, count_lines);
+            let x = centerw.saturating_sub(line_width / 2);
+            let area = Rect::new(area.x + x, area.y + y, line_width, 1);
 
-        f.render_widget(paragraph, area);
+            let paragraph = Paragraph::new(line);
+            f.render_widget(paragraph, area);
+
+            layout.push(line, area.x, area.y, area.width, area.height);
+
+            y += 1;
+        }
     }
 
     fn handle_input(
@@ -52,5 +65,12 @@ impl View for InformationView {
         _: KeyEvent,
     ) -> Option<Transition> {
         None
+    }
+
+    fn collect_data(&self) -> Vec<NuText> {
+        Self::MESSAGE
+            .into_iter()
+            .map(|line| (line.to_owned(), TextStyle::default()))
+            .collect::<Vec<_>>()
     }
 }
