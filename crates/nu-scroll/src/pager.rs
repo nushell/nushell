e40ba403cc49766300395f6795eca15096a60416
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-11-25 08:43:37 -0600

    update some color names

--- crates/nu-scroll/src/pager.rs
+++ crates/nu-scroll/src/pager.rs
@@ -1016,8 +1016,8 @@ pub fn nu_ansi_color_to_tui_color(clr: NuColor) -> Option<tui::style::Color> {
         Fixed(i) => Color::Indexed(i),
         Rgb(r, g, b) => tui::style::Color::Rgb(r, g, b),
         LightGray => Color::Gray,   // todo: make a PR to add the color
-        LightPurple => Color::Blue, // todo: make a PR to add the color,
-        Purple => Color::Blue,      // todo: make a PR to add the color,
+        LightPurple => Color::LightMagenta,
+        Purple => Color::Magenta,
         Default => return None,
     };
 
