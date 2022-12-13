362a15ef16c151f080a52783a10736777546682e
Leon <L_1_L_0@yahoo.com>
2022-12-13 20:46:38 +1000

    Make $in work in catch closures

--- crates/nu-command/tests/commands/try_.rs
+++ crates/nu-command/tests/commands/try_.rs
@@ -1,50 +1,51 @@
 use nu_test_support::nu;
-use nu_test_support::playground::Playground;
 
 #[test]
 fn try_succeed() {
-    Playground::setup("try_succeed_test", |dirs, _sandbox| {
-        let output = nu!(
-            cwd: dirs.test(),
-            "try { 345 } catch { echo 'hello' }"
-        );
+    let output = nu!(
+        cwd: ".",
+        "try { 345 } catch { echo 'hello' }"
+    );
 
-        assert!(output.out.contains("345"));
-    })
+    assert!(output.out.contains("345"));
 }
 
 #[test]
 fn try_catch() {
-    Playground::setup("try_catch_test", |dirs, _sandbox| {
-        let output = nu!(
-            cwd: dirs.test(),
-            "try { foobarbaz } catch { echo 'hello' }"
-        );
+    let output = nu!(
+        cwd: ".",
+        "try { foobarbaz } catch { echo 'hello' }"
+    );
 
-        assert!(output.out.contains("hello"));
-    })
+    assert!(output.out.contains("hello"));
 }
 
 #[test]
 fn catch_can_access_error() {
-    Playground::setup("try_catch_test", |dirs, _sandbox| {
-        let output = nu!(
-            cwd: dirs.test(),
-            "try { foobarbaz } catch { |err| $err }"
-        );
+    let output = nu!(
+        cwd: ".",
+        "try { foobarbaz } catch { |err| $err }"
+    );
 
-        assert!(output.err.contains("External command failed"));
-    })
+    assert!(output.err.contains("External command failed"));
+}
+
+#[test]
+fn catch_can_access_error_as_dollar_in() {
+    let output = nu!(
+        cwd: ".",
+        "try { foobarbaz } catch { $in }"
+    );
+
+    assert!(output.err.contains("External command failed"));
 }
 
 #[test]
 fn external_failed_should_be_catched() {
-    Playground::setup("try_catch_test", |dirs, _sandbox| {
-        let output = nu!(
-            cwd: dirs.test(),
-            "try { nu --testbin fail; echo 'success' } catch { echo 'fail' }"
-        );
+    let output = nu!(
+        cwd: ".",
+        "try { nu --testbin fail; echo 'success' } catch { echo 'fail' }"
+    );
 
-        assert!(output.out.contains("fail"));
-    })
+    assert!(output.out.contains("fail"));
 }
