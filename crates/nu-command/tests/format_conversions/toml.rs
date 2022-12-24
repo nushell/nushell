2807526c632bc023f446c05ee8c200d595a8b2f8
NotLebedev <artem-itf@yandex.ru>
2022-12-24 19:11:20 +0300

    Add tests for to toml command

--- crates/nu-command/tests/format_conversions/toml.rs
+++ crates/nu-command/tests/format_conversions/toml.rs
@@ -1,7 +1,67 @@
 use nu_test_support::{nu, pipeline};
 
 #[test]
-fn table_to_toml_text_and_from_toml_text_back_into_table() {
+fn record_map_to_toml() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            {a: 1 b: 2 c: 'qwe'} 
+            | to toml
+            | from toml
+            | $in == {a: 1 b: 2 c: 'qwe'}
+        "#
+    ));
+
+    assert_eq!(actual.out, "true");
+}
+
+#[test]
+fn nested_records_to_toml() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            {a: {a: a b: b} c: 1} 
+            | to toml
+            | from toml
+            | $in == {a: {a: a b: b} c: 1}
+        "#
+    ));
+
+    assert_eq!(actual.out, "true");
+}
+
+#[test]
+fn records_with_tables_to_toml() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            {a: [[a b]; [1 2] [3 4]] b: [[c d e]; [1 2 3]]}
+            | to toml
+            | from toml
+            | $in == {a: [[a b]; [1 2] [3 4]] b: [[c d e]; [1 2 3]]}
+        "#
+    ));
+
+    assert_eq!(actual.out, "true");
+}
+
+#[test]
+fn nested_tables_to_toml() {
+    let actual = nu!(
+    cwd: "tests/fixtures/formats", pipeline(
+        r#"
+            {c: [[f g]; [[[h k]; [1 2] [3 4]] 1]]}
+            | to toml
+            | from toml
+            | $in == {c: [[f g]; [[[h k]; [1 2] [3 4]] 1]]}
+        "#
+    ));
+
+    assert_eq!(actual.out, "true");
+}
+
+#[test]
+fn big_record_to_toml_text_and_from_toml_text_back_into_record() {
     let actual = nu!(
         cwd: "tests/fixtures/formats", pipeline(
         r#"
