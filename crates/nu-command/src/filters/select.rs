34808376523f7e24d9442a123e2a20926b649d14
Reilly Wood <reilly.wood@icloud.com>
2022-12-26 21:09:29 -0800

    fix up merge

--- crates/nu-command/src/filters/select.rs
+++ crates/nu-command/src/filters/select.rs
@@ -94,9 +94,6 @@ fn select(
         match members.get(0) {
             Some(PathMember::Int { val, span, .. }) => {
                 if members.len() > 1 {
-                    if ignore_errors {
-                        return Ok(Value::nothing(call_span).into_pipeline_data());
-                    }
                     return Err(ShellError::GenericError(
                         "Select only allows row numbers for rows".into(),
                         "extra after row number".into(),
--- src/tests/test_table_operations.rs
+++ src/tests/test_table_operations.rs
@@ -284,11 +284,6 @@ fn length_defaulted_columns() -> TestResult {
 }
 
 #[test]
-fn get_fuzzy() -> TestResult {
-    run_test("(ls | get -i foo) == null", "true")
-}
-
-#[test]
 fn get_insensitive() -> TestResult {
     run_test(
         r#"[[name, age]; [a, 1] [b, 2]] | get NAmE | select 0 | get 0"#,
