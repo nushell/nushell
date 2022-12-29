942bc5842df0155568a133df7addc9cb302eea05
WindSoilder <WindSoilder@outlook.com>
2022-12-29 08:07:36 +0800

    fix test

--- crates/nu-command/tests/commands/take/rows.rs
+++ crates/nu-command/tests/commands/take/rows.rs
@@ -51,7 +51,7 @@ fn fails_on_string() {
             "#
     ));
 
-    assert!(actual.err.contains("pipeline_mismatch"));
+    assert!(actual.err.contains("Only supports for specific types."));
 }
 
 #[test]
