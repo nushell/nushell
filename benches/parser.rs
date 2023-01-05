78fb8fe831b167a81018211b1b880b6c276ee9c2
Reilly Wood <reilly.wood@icloud.com>
2023-01-04 20:52:43 -0800

    Get parser benchmarks working

--- benches/parser.rs
+++ benches/parser.rs
@@ -1,18 +1,39 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use nu_parser::parse;
-use nu_protocol::{Value, Span};
+use nu_protocol::{Span, Value};
 use nu_utils::{get_default_config, get_default_env};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut engine_state = nu_command::create_default_context();
     // parsing breaks without PWD set
-    engine_state.add_env_var("PWD".into(),  Value::string(std::env::current_dir().unwrap().to_string_lossy().to_owned(), Span::test_data()));
-    let mut working_set = nu_protocol::engine::StateWorkingSet::new(&engine_state);
-    let default_config = get_default_config().as_bytes();
+    engine_state.add_env_var(
+        "PWD".into(),
+        Value::string(
+            std::env::current_dir()
+                .unwrap()
+                .to_string_lossy()
+                .to_owned(),
+            Span::test_data(),
+        ),
+    );
 
-    c.bench_function("parse config", |b| b.iter(|| {
-        nu_parser::parse(&mut working_set, None, default_config, false, &[]);
-    }));
+    let default_config = get_default_config().as_bytes();
+    c.bench_function("parse config.nu", |b| {
+        b.iter_batched(
+            || nu_protocol::engine::StateWorkingSet::new(&engine_state),
+            |mut working_set| parse(&mut working_set, None, default_config, false, &[]),
+            BatchSize::SmallInput,
+        )
+    });
+
+    let default_env = get_default_env().as_bytes();
+    c.bench_function("parse env.nu", |b| {
+        b.iter_batched(
+            || nu_protocol::engine::StateWorkingSet::new(&engine_state),
+            |mut working_set| parse(&mut working_set, None, default_env, false, &[]),
+            BatchSize::SmallInput,
+        )
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
