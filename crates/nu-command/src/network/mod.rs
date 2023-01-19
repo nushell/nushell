bf9f4e35fc86a3c098047c54ef4cd0c1ebc73e3e
Kazuki-Ya <k.yano402@gmail.com>
2023-01-20 04:46:27 +0900

    rename fetch and post

--- crates/nu-command/src/network/mod.rs
+++ crates/nu-command/src/network/mod.rs
@@ -1,14 +1,8 @@
-mod fetch;
 mod http;
 mod port;
-mod post;
 mod url;
 
 pub use self::http::*;
 pub use self::url::*;
 
 pub use port::SubCommand as Port;
-
-// This is alias fetch and post to http methods for convenience
-pub use fetch::SubCommand as HttpGet;
-pub use post::SubCommand as HttpPost;
