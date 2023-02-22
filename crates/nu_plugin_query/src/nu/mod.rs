0a6cf627dbb98bd0f3b9ed4da3a62cb142aab93a
Yethal <nosuchemail@email.com>
2023-02-22 19:28:23 +0100

    More examples

--- crates/nu_plugin_query/src/nu/mod.rs
+++ crates/nu_plugin_query/src/nu/mod.rs
@@ -76,14 +76,19 @@ pub fn web_examples() -> Vec<PluginExample> {
         description: "Retrieve all <header> elements from phoronix.com website".into(),
         result: None,
     }, PluginExample {
-        example: "http get https://developer.valvesoftware.com/wiki/List_of_TF2_console_commands_and_variables
-    | query web -t [Name Cmd? Default Min Max Flags Description]".into(),
-        description: "Retrieve a html table from Valve website and parse it into a nushell table using table headers as guides".into(),
+        example: "http get https://en.wikipedia.org/wiki/List_of_cities_in_India_by_population
+    | query web -t [Rank City 'Population(2011)[3]' 'Population(2001)' 'State or union territory']".into(),
+        description: "Retrieve a html table from Wikipedia and parse it into a nushell table using table headers as guides".into(),
         result: None
     },
     PluginExample {
-        example: "http get https://www.nushell.sh | query web -q 'h2, h2 + p'".into(),
-        description: "Pass multiple css selectors to extract several elements within single query".into(),
+        example: "http get https://www.nushell.sh | query web -q 'h2, h2 + p' | group 2 | each {rotate --ccw tagline description} | flatten".into(),
+        description: "Pass multiple css selectors to extract several elements within single query, group the query results together and rotate them to create a table".into(),
+        result: None,
+    },
+    PluginExample {
+        example: "http get https://example.org | query web --query a --attribute href".into(),
+        description: "Retrieve a specific html attribute instead of the default text".into(),
         result: None,
     }]
 }
