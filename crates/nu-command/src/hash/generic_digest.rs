dc6581a5c9d56b88bec271b689d70a33d07ab778
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-02-24 11:59:07 -0500

    formatting

--- crates/nu-command/src/hash/generic_digest.rs
+++ crates/nu-command/src/hash/generic_digest.rs
@@ -3,7 +3,9 @@ use nu_engine::CallExt;
 use nu_protocol::ast::{Call, CellPath};
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::Span;
-use nu_protocol::{Category, Example, PipelineData, ShellError, Signature, SyntaxShape, Type, Value};
+use nu_protocol::{
+    Category, Example, PipelineData, ShellError, Signature, SyntaxShape, Type, Value,
+};
 use std::marker::PhantomData;
 
 pub trait HashDigest: digest::Digest + Clone {
