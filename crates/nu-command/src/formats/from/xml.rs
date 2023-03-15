f3eebfd81feefe1afa14c0388332305fb0f2c856
Julian Hofer <julianhofer@gnome.org>
2023-03-15 11:08:28 +0100

    Fix xml docs
    See https://github.com/nushell/nushell.github.io/pull/828

--- crates/nu-command/src/formats/from/xml.rs
+++ crates/nu-command/src/formats/from/xml.rs
@@ -35,10 +35,10 @@ impl Command for FromXml {
     fn extra_usage(&self) -> &str {
         r#"Every XML entry is represented via a record with tag, attribute and content fields.
 To represent different types of entries different values are written to this fields:
-1. Tag entry: {tag: <tag name> attrs: {<attr name>: "<string value>" ...} content: [<entries>]}
-2. Comment entry: {tag: '!' attrs: null content: "<comment string>"}
-3. Processing instruction (PI): {tag: '?<pi name>' attrs: null content: "<pi content string>"}
-4. Text: {tag: null attrs: null content: "<text>"}.
+1. Tag entry: `{tag: <tag name> attrs: {<attr name>: "<string value>" ...} content: [<entries>]}`
+2. Comment entry: `{tag: '!' attrs: null content: "<comment string>"}`
+3. Processing instruction (PI): `{tag: '?<pi name>' attrs: null content: "<pi content string>"}`
+4. Text: `{tag: null attrs: null content: "<text>"}`.
 
 Unlike to xml command all null values are always present and text is never represented via plain
 string. This way content of every tag is always a table and is easier to parse"#
