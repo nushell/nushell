867a2e10dd4c8160caf33a092628b512efc62fbc
amtoine <stevan.antoine@gmail.com>
2023-03-22 19:25:58 +0100

    add a test with start > end that should fail

--- crates/nu-command/tests/commands/error_make.rs
+++ crates/nu-command/tests/commands/error_make.rs
@@ -24,3 +24,16 @@ fn no_span_if_unspanned() {
 
     assert!(!actual.err.contains("unseen"));
 }
+
+#[test]
+fn error_start_bigger_than_end_should_fail() {
+    let actual = nu!(
+        cwd: ".", pipeline(
+        r#"
+        error make {msg: foo label: {text: bar start 456 end 123}}
+        "#
+    ));
+
+    assert!(!actual.err.contains("invalid error format"));
+    assert!(!actual.err.contains("`$.label.start` is stricly bigger than `$.label.end`"));
+}
