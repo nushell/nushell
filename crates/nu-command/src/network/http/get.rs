b4b68afa17502014046bdd4eecac202119148539
Reilly Wood <26268125+rgwood@users.noreply.github.com>
2023-03-24 12:45:55 -0700

    Make HTTP requests cancellable when trying to connect (#8591)
    Closes #8585.
    
    Prior to this change, the `http` commands could get stuck for 30s while
    attempting to make a connection to a remote server. After this change,
    `ctrl+c` works as expected:
    
    
    ![image](https://user-images.githubusercontent.com/26268125/227395505-c2d5b19d-6228-4eac-836f-c0c3426b0c19.png)
    
    To make this work, we perform blocking `ureq` calls in a background
    thread and poll the channel while checking `ctrl+c`.

--- crates/nu-command/src/network/http/get.rs
+++ crates/nu-command/src/network/http/get.rs
@@ -163,6 +163,7 @@ fn helper(
     args: Arguments,
 ) -> Result<PipelineData, ShellError> {
     let span = args.url.span()?;
+    let ctrl_c = engine_state.ctrlc.clone();
     let (requested_url, _) = http_parse_url(call, span, args.url)?;
 
     let client = http_client(args.insecure);
@@ -172,7 +173,7 @@ fn helper(
     request = request_add_authorization_header(args.user, args.password, request);
     request = request_add_custom_headers(args.headers, request)?;
 
-    let response = send_request(request, None, None);
+    let response = send_request(request, None, None, ctrl_c);
 
     let request_flags = RequestFlags {
         raw: args.raw,
