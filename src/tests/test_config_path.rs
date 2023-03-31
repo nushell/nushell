45b6ca7f3ca07e89630f189671c772d7e17c4582
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-03-31 09:05:26 -0500

    create file if it doesn't exist

--- src/tests/test_config_path.rs
+++ src/tests/test_config_path.rs
@@ -5,16 +5,29 @@ fn test_default_config_path() {
     let config_dir = nu_path::config_dir().expect("Could not get config directory");
     let cwd = std::env::current_dir().expect("Could not get current working directory");
 
-    // We use canonicalize here in case the config or env is symlinked since $nu.config-path is returning the canonicalized path in #8653
-    let config_path = std::fs::canonicalize(config_dir.join("nushell").join("config.nu"))
-        .expect("canonicalize config-path failed");
-    let actual = nu!(cwd: &cwd, "$nu.config-path");
-    assert_eq!(actual.out, config_path.to_string_lossy().to_string());
+    let config_path = config_dir.join("nushell").join("config.nu");
+    // Create an empty file for canonicalization
+    if !config_path.exists() {
+        let _ = std::fs::File::create(&config_path);
+    }
 
-    let env_path = std::fs::canonicalize(config_dir.join("nushell").join("env.nu"))
-        .expect("canonicalize of env-path failed");
+    // We use canonicalize here in case the config or env is symlinked since $nu.config-path is returning the canonicalized path in #8653
+    let canon_config_path =
+        std::fs::canonicalize(config_path).expect("canonicalize config-path failed");
+
+    let actual = nu!(cwd: &cwd, "$nu.config-path");
+    assert_eq!(actual.out, canon_config_path.to_string_lossy().to_string());
+
+    let env_path = config_dir.join("nushell").join("env.nu");
+    // Create an empty file for canonicalization
+    if !env_path.exists() {
+        let _ = std::fs::File::create(&env_path);
+    }
+
+    let canon_env_path = std::fs::canonicalize(env_path).expect("canonicalize of env-path failed");
+
     let actual = nu!(cwd: &cwd, "$nu.env-path");
-    assert_eq!(actual.out, env_path.to_string_lossy().to_string());
+    assert_eq!(actual.out, canon_env_path.to_string_lossy().to_string());
 }
 
 #[test]
