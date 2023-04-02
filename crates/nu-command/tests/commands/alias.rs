e1805325d4ec84f7ed00b0bd98a2d165d8b623c1
Harshal Chaudhari <harshal.chaudhary@gmail.com>
2023-04-02 13:57:12 +0100

    tests(alias.rs): fix unnecessary cwd() and pipeline()
    Signed-off-by: Harshal Chaudhari <harshal.chaudhary@gmail.com>

--- crates/nu-command/tests/commands/alias.rs
+++ crates/nu-command/tests/commands/alias.rs
@@ -6,13 +6,7 @@ use nu_test_support::{nu, pipeline};
 #[test]
 fn alias_simple() {
     let actual = nu!(
-        cwd: "tests/fixtures/formats", pipeline(
-        r#"
-            alias bar = use sample_def.nu greet;
-            bar;
-            greet
-        "#
-    ));
+        cwd: "tests/fixtures/formats", " alias bar = use sample_def.nu greet; bar; greet ");
 
     assert_eq!(actual.out, "hello");
 }
@@ -49,47 +43,23 @@ fn alias_hiding_2() {
 #[test]
 fn alias_fails_with_invalid_name() {
     let err_msg = "name can't be a number, a filesize, or contain a hash # or caret ^";
-    let actual = nu!(
-        cwd: ".", pipeline(
-        r#"
-            alias 1234 = echo "test"
-        "#
-    ));
+    let actual = nu!(r#" alias 1234 = echo "test" "#);
+
     assert!(actual.err.contains(err_msg));
 
-    let actual = nu!(
-        cwd: ".", pipeline(
-        r#"
-            alias 5gib = echo "test"
-        "#
-    ));
+    let actual = nu!(r#" alias 5gib = echo "test" "#);
     assert!(actual.err.contains(err_msg));
 
-    let actual = nu!(
-        cwd: ".", pipeline(
-        r#"
-            alias "te#t" = echo "test"
-        "#
-    ));
+    let actual = nu!(r#" alias "te#t" = echo "test" "#);
     assert!(actual.err.contains(err_msg));
 
-    let actual = nu!(
-        cwd: ".", pipeline(
-        r#"
-            alias ^foo = echo "bar"
-        "#
-    ));
+    let actual = nu!(r#" alias ^foo = echo "bar" "#);
     assert!(actual.err.contains(err_msg));
 }
 
 #[test]
 fn cant_alias_keyword() {
-    let actual = nu!(
-        cwd: ".", pipeline(
-        r#"
-            alias ou = let
-        "#
-    ));
+    let actual = nu!(r#" alias ou = let "#);
     assert!(actual.err.contains("cant_alias_keyword"));
 }
 
