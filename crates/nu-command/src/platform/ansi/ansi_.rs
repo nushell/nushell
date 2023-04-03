102740a24ef1ff42577497a135c611d7e8d78dc1
amtoine <stevan.antoine@gmail.com>
2023-04-03 18:31:06 +0200

    mention the non-required escapes in `-e` and `-o`

--- crates/nu-command/src/platform/ansi/ansi_.rs
+++ crates/nu-command/src/platform/ansi/ansi_.rs
@@ -515,12 +515,12 @@ impl Command for AnsiCommand {
             )
             .switch(
                 "escape", // \x1b[
-                "escape sequence without the escape character(s)",
+                r#"escape sequence without the escape character(s) ('\x1b[' is not required)"#,
                 Some('e'),
             )
             .switch(
                 "osc", // \x1b]
-                "operating system command (ocs) escape sequence without the escape character(s)",
+                r#"operating system command (osc) escape sequence without the escape character(s) ('\x1b]' is not required)"#,
                 Some('o'),
             )
             .switch("list", "list available ansi code names", Some('l'))
