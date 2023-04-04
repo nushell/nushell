f1f508a3b7d14c5e969bb3c2f902d9ec8de4a252
amtoine <stevan.antoine@gmail.com>
2023-04-04 18:27:00 +0200

    put the original extra usage but in a structured way

--- crates/nu-command/src/platform/ansi/ansi_.rs
+++ crates/nu-command/src/platform/ansi/ansi_.rs
@@ -533,7 +533,61 @@ impl Command for AnsiCommand {
 
     fn extra_usage(&self) -> &str {
         "An introduction to what ANSI escape sequences are can be found in the
-[*ANSI escape code*](https://en.wikipedia.org/wiki/ANSI_escape_code) Wikipedia page."
+[*ANSI escape code*](https://en.wikipedia.org/wiki/ANSI_escape_code) Wikipedia page.
+
+Escape sequences usual values:
+╭────┬────────────┬────────┬────────┬─────────╮
+│  # │    type    │ normal │ bright │  name   │
+├────┼────────────┼────────┼────────┼─────────┤
+│  0 │ foreground │     30 │     90 │ black   │
+│  1 │ foreground │     31 │     91 │ red     │
+│  2 │ foreground │     32 │     92 │ green   │
+│  3 │ foreground │     33 │     93 │ yellow  │
+│  4 │ foreground │     34 │     94 │ blue    │
+│  5 │ foreground │     35 │     95 │ magenta │
+│  6 │ foreground │     36 │     96 │ cyan    │
+│  7 │ foreground │     37 │     97 │ white   │
+│  8 │ foreground │     39 │        │ default │
+│  9 │ background │     40 │    100 │ black   │
+│ 10 │ background │     41 │    101 │ red     │
+│ 11 │ background │     42 │    102 │ green   │
+│ 12 │ background │     43 │    103 │ yellow  │
+│ 13 │ background │     44 │    104 │ blue    │
+│ 14 │ background │     45 │    105 │ magenta │
+│ 15 │ background │     46 │    106 │ cyan    │
+│ 16 │ background │     47 │    107 │ white   │
+│ 17 │ background │     49 │        │ default │
+╰────┴────────────┴────────┴────────┴─────────╯
+
+Escape sequences attributes:
+╭───┬────┬──────────────┬──────────────────────────────╮
+│ # │ id │ abbreviation │         description          │
+├───┼────┼──────────────┼──────────────────────────────┤
+│ 0 │  0 │              │ reset / normal display       │
+│ 1 │  1 │ b            │ bold or increased intensity  │
+│ 2 │  2 │ d            │ faint or decreased intensity │
+│ 3 │  3 │ i            │ italic on (non-mono font)    │
+│ 4 │  4 │ u            │ underline on                 │
+│ 5 │  5 │ l            │ slow blink on                │
+│ 6 │  6 │              │ fast blink on                │
+│ 7 │  7 │ r            │ reverse video on             │
+│ 8 │  8 │ h            │ nondisplayed (invisible) on  │
+│ 9 │  9 │ s            │ strike-through on            │
+╰───┴────┴──────────────┴──────────────────────────────╯
+
+Operating system commands:
+╭───┬─────┬───────────────────────────────────────╮
+│ # │ id  │              description              │
+├───┼─────┼───────────────────────────────────────┤
+│ 0 │   0 │ Set window title and icon name        │
+│ 1 │   1 │ Set icon name                         │
+│ 2 │   2 │ Set window title                      │
+│ 3 │   4 │ Set/read color palette                │
+│ 4 │   9 │ iTerm2 Grown notifications            │
+│ 5 │  10 │ Set foreground color (x11 color spec) │
+│ 6 │  11 │ Set background color (x11 color spec) │
+│ 7 │ ... │ others                                │
+╰───┴─────┴───────────────────────────────────────╯"
     }
 
     fn examples(&self) -> Vec<Example> {
