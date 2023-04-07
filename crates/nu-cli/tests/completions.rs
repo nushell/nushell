604a8827e34bae041675173ef18471b2ca109c72
Jelle Besseling <jelle@bigbridge.nl>
2023-04-07 15:01:20 +0200

    Add $nu.current-exe variable

--- crates/nu-cli/tests/completions.rs
+++ crates/nu-cli/tests/completions.rs
@@ -524,10 +524,11 @@ fn variables_completions() {
     // Test completions for $nu
     let suggestions = completer.complete("$nu.", 4);
 
-    assert_eq!(12, suggestions.len());
+    assert_eq!(13, suggestions.len());
 
     let expected: Vec<String> = vec![
         "config-path".into(),
+        "current-exe".into(),
         "env-path".into(),
         "history-path".into(),
         "home-path".into(),
