d447234a41343fa127ab0e9661d71be52f1f3ce8
melMass <mel@melmassadian.com>
2023-04-22 14:25:50 +0200

    fix: ✨ remove wip closure stuff
    Those were attempts to support closures for validation
    but it's not working yet. I'm removing it for now

--- crates/nu-command/src/platform/input/list.rs
+++ crates/nu-command/src/platform/input/list.rs
@@ -8,55 +8,37 @@ use nu_protocol::{
     Value,
 };
 
-// TODO:
-// - implement more interact modes
-// - add support for validation / formatting closures
-
 enum InteractMode {
     Single(Option<usize>),
     Multi(Option<Vec<usize>>),
 }
 
 #[derive(Clone)]
-pub struct Interact;
+pub struct InputList;
 
-impl Command for Interact {
+impl Command for InputList {
     fn name(&self) -> &str {
         "input list"
     }
 
     fn signature(&self) -> Signature {
         Signature::build("input list")
-            .input_output_types(vec![
-                (
-                    Type::List(Box::new(Type::Any)),
-                    Type::List(Box::new(Type::Any)),
-                ),
-                // (Type::Table(vec![]), Type::List(Box::new(Type::Any))),
-                // (Type::List(Box::new(Type::Any)), Type::Any),
-                // (Type::String, Type::String),
-            ])
+            .input_output_types(vec![(
+                Type::List(Box::new(Type::Any)),
+                Type::List(Box::new(Type::Any)),
+            )])
             .optional("prompt", SyntaxShape::String, "the prompt to display")
             .switch(
                 "multi",
                 "Use multiple results, you can press a to toggle all options on/off",
                 Some('m'),
             )
-            // record index uszie value string
-            // .optional(
-            //     "validator",
-            //     SyntaxShape::Closure(Some(vec![
-            //         SyntaxShape::List(Box::new(SyntaxShape::Record)),
-            //         SyntaxShape::Int,
-            //     ])),
-            //     "validator for the selection (not implemented yet)",
-            // )
             .allow_variants_without_examples(true)
-            .category(Category::Misc)
+            .category(Category::Platform)
     }
 
     fn usage(&self) -> &str {
-        "Show interactive menus. Abort with esc or q"
+        "Interactive list selection. Abort with esc or q"
     }
 
     fn search_terms(&self) -> Vec<&str> {
@@ -73,57 +55,9 @@ impl Command for Interact {
         let head = call.head;
         let prompt: Option<String> = call.opt(engine_state, stack, 0)?;
 
-        // let capture_block: Closure = call.req(engine_state, stack, 0)?;
-
-        // let block = engine_state.get_block(capture_block.block_id).clone();
-        // let var_id = block.signature.get_positional(0).and_then(|arg| arg.var_id);
-        // let mut stack = stack.captures_to_stack(&capture_block.captures);
-
-        // let redirect_stdout = call.redirect_stdout;
-        // let redirect_stderr = call.redirect_stderr;
-
-        // Lots of great examples here:
-        // https://github.com/mikaelmello/inquire
-
-        // let mut data: PipelineData = PipelineData::Empty;
-        // let validator = |a: &[ListOption<&&str>]| {
-        //     let data: PipelineData = a
-        //         .iter()
-        //         // map the record index uszie value string
-        //         .map(|s| Value::Record {
-        //             cols: vec!["index".to_string(), "value".to_string()],
-        //             vals: vec![
-        //                 Value::Int {
-        //                     val: s.index.to_owned() as i64,
-        //                     span: head,
-        //                 },
-        //                 Value::String {
-        //                     val: s.value.to_owned().to_string(),
-        //                     span: head,
-        //                 },
-        //             ],
-        //             span: head,
-        //         })
-        //         .into_pipeline_data(None);
-        // .collect();
-        //if let Some(var_id) = var_id {
-        //     stack.add_var(var_id, data);
-        // }
-        // eval_block(
-        //     &engine_state,
-        //     &mut stack,
-        //     &block,
-        //     data,
-        //     redirect_stdout,
-        //     redirect_stderr,
-        // );
-        // };
-
         let options: Vec<String> = match input {
             PipelineData::Value(Value::Range { .. }, ..)
             | PipelineData::Value(Value::List { .. }, ..) => {
-                //| PipelineData::ListStream { .. } => {
-                // let mut options = Vec::new();
                 input
                     .into_iter()
                     .map_while(move |x| {
@@ -143,17 +77,8 @@ impl Command for Interact {
                         }
                     })
                     .collect()
-                // for val in vals {
-                //     match val {
-                //         // Value::String { val, .. } => options.push(val),
-                //         // _ => return Err(ShellError::type_error("string", val.type_name())),
-                //         _ => options.push(val.as_string()?),
-                //     }
-                // }
-                // options
             }
-            // PipelineData::Value(Value::String { val, .. }, _) => vec![val],
-            // _ => return Err(ShellError::type_error("string", input.type_name())),
+
             PipelineData::ListStream { .. } => input
                 .into_iter()
                 .map_while(move |x| {
@@ -183,26 +108,11 @@ impl Command for Interact {
                 },
                 _,
             ) => {
-                // if let Some(var) = block.signature.get_positional(0) {
-                //     if let Some(var_id) = &var.var_id {
-                //         stack.add_var(*var_id, x.clone());
-                //     }
-                // }
-
                 println!("cols: {:?}", cols);
 
                 vals.iter()
                     .map(|x| x.as_string().unwrap_or_else(|_| "RECORD".to_string()))
                     .collect()
-
-                // eval_block_with_early_return(
-                //     &engine_state,
-                //     &mut stack,
-                //     &block,
-                //     x.into_pipeline_data(),
-                //     redirect_stdout,
-                //     redirect_stderr,
-                // )
             }
             _ => {
                 return Err(ShellError::TypeMismatch {
@@ -220,7 +130,7 @@ impl Command for Interact {
                     .items(&options)
                     .interact_on_opt(&Term::stderr())
                     .map_err(|_| {
-                        ShellError::IOError("Oopsie, interact is a wip command...".to_owned())
+                        ShellError::IOError("Oopsie, list input is a wip command...".to_owned())
                     })?,
             )
         } else {
@@ -230,7 +140,7 @@ impl Command for Interact {
                     .with_prompt(&prompt)
                     .interact_on_opt(&Term::stderr())
                     .map_err(|_| {
-                        ShellError::IOError("Oopsie, interact is a wip command...".to_owned())
+                        ShellError::IOError("Oopsie, list input is a wip command...".to_owned())
                     })?,
             )
         };
@@ -295,6 +205,6 @@ mod test {
     fn test_examples() {
         use crate::test_examples;
 
-        test_examples(Interact {})
+        test_examples(InputList {})
     }
 }
