63712bb74d42ff629ecf92ae7e86c61c462a684a
Steven Xu <stevenxxiu@gmail.com>
2023-04-28 19:15:24 +1000

    test: remove redundant spaces

--- src/tests/test_commandline.rs
+++ src/tests/test_commandline.rs
@@ -47,14 +47,14 @@ fn commandline_test_replace() -> TestResult {
 fn commandline_test_cursor() -> TestResult {
     run_test(
         "commandline --replace '0👩‍❤️‍👩2'\n\
-        commandline --cursor '1' \n\
+        commandline --cursor '1'\n\
         commandline --insert 'x'\n\
         commandline",
         "0x👩‍❤️‍👩2",
     )?;
     run_test(
         "commandline --replace '0👩‍❤️‍👩2'\n\
-        commandline --cursor '2' \n\
+        commandline --cursor '2'\n\
         commandline --insert 'x'\n\
         commandline",
         "0👩‍❤️‍👩x2",
@@ -65,13 +65,13 @@ fn commandline_test_cursor() -> TestResult {
 fn commandline_test_cursor_show_pos() -> TestResult {
     run_test(
         "commandline --replace '0👩‍❤️‍👩2'\n\
-        commandline --cursor '1' \n\
+        commandline --cursor '1'\n\
         commandline --cursor",
         "1",
     )?;
     run_test(
         "commandline --replace '0👩‍❤️‍👩2'\n\
-        commandline --cursor '2' \n\
+        commandline --cursor '2'\n\
         commandline --cursor",
         "2",
     )
@@ -81,7 +81,7 @@ fn commandline_test_cursor_show_pos() -> TestResult {
 fn commandline_test_cursor_too_small() -> TestResult {
     run_test(
         "commandline --replace '123456'\n\
-        commandline --cursor '-1' \n\
+        commandline --cursor '-1'\n\
         commandline --insert '0'\n\
         commandline",
         "0123456",
@@ -92,7 +92,7 @@ fn commandline_test_cursor_too_small() -> TestResult {
 fn commandline_test_cursor_too_large() -> TestResult {
     run_test(
         "commandline --replace '123456'\n\
-        commandline --cursor '10' \n\
+        commandline --cursor '10'\n\
         commandline --insert '0'\n\
         commandline",
         "1234560",
