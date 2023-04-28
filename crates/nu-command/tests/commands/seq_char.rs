3076378373fc8d5c94ef2fcbe4fc4d031c6baa31
Reilly Wood <26268125+rgwood@users.noreply.github.com>
2023-04-28 04:25:44 -0700

    Slim down tests (#9021)
    This PR just tidies up some tests by removing unused code:
    
    1. If the filesystem is not touched, don't use the filesystem
    playground/sandbox
    2. If the filesystem is not touched, don't specify the `cwd`
    3. If the command is short, don't bother wrapping it in `pipeline()`
    4. If the command doesn't have quotes, don't bother with a `r#"..."#`
    raw string
    
    Part of #8670.

--- crates/nu-command/tests/commands/seq_char.rs
+++ crates/nu-command/tests/commands/seq_char.rs
@@ -1,25 +1,15 @@
-use nu_test_support::{nu, pipeline};
+use nu_test_support::nu;
 
 #[test]
 fn fails_when_first_arg_is_multiple_chars() {
-    let actual = nu!(
-        cwd: ".", pipeline(
-        r#"
-        seq char aa z
-        "#
-    ));
+    let actual = nu!("seq char aa z");
 
     assert!(actual.err.contains("should be 1 character long"));
 }
 
 #[test]
 fn fails_when_second_arg_is_multiple_chars() {
-    let actual = nu!(
-        cwd: ".", pipeline(
-        r#"
-        seq char a zz
-        "#
-    ));
+    let actual = nu!("seq char a zz");
 
     assert!(actual.err.contains("should be 1 character long"));
 }
