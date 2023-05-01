e0a3e0a39735f334a5245a4c8dd05a303f533e71
sam schick <github@schicksw.mozmail.com>
2023-05-01 19:16:46 -0400

    "remove expect"

--- crates/nu-protocol/src/lev_distance.rs
+++ crates/nu-protocol/src/lev_distance.rs
@@ -55,8 +55,7 @@ pub fn lev_distance(a: &str, b: &str, limit: usize) -> Option<usize> {
 
 /// Finds the Levenshtein distance between two strings.
 pub fn levenshtein_distance(a: &str, b: &str) -> usize {
-    lev_distance(a, b, usize::max_value())
-        .expect("It is impossible to exceed the supplied limit since all types involved are usize.")
+    lev_distance(a, b, usize::max_value()).unwrap_or(usize::max_value())
 }
 /// Provides a word similarity score between two words that accounts for substrings being more
 /// meaningful than a typical Levenshtein distance. The lower the score, the closer the match.
