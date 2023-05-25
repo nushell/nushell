b8111ed8cb19b952fa3a829e3d96769c6e024113
amtoine <stevan.antoine@gmail.com>
2023-05-25 19:07:00 +0200

    refactor `std_dir` to use `NU_STDLIB_VIRTUAL_DIR` and `"std"` once

--- crates/nu-std/src/lib.rs
+++ crates/nu-std/src/lib.rs
@@ -12,6 +12,10 @@ pub fn load_standard_library(
     engine_state: &mut nu_protocol::engine::EngineState,
 ) -> Result<(), miette::ErrReport> {
     let (block, delta) = {
+        // Using full virtual path to avoid potential conflicts with user having 'std' directory
+        // in their working directory.
+        let std_dir = PathBuf::from(NU_STDLIB_VIRTUAL_DIR).join("std");
+
         let mut std_files = vec![
             ("mod.nu", include_str!("../std/mod.nu")),
             ("dirs.nu", include_str!("../std/dirs.nu")),
@@ -27,7 +31,7 @@ pub fn load_standard_library(
         let mut std_virt_paths = vec![];
 
         for (name, content) in std_files.drain(..) {
-            let name = PathBuf::from(NU_STDLIB_VIRTUAL_DIR).join("std").join(name);
+            let name = std_dir.join(name);
 
             let file_id =
                 working_set.add_file(name.to_string_lossy().to_string(), content.as_bytes());
@@ -38,12 +42,7 @@ pub fn load_standard_library(
             std_virt_paths.push(virtual_file_id);
         }
 
-        // Using full virtual path to avoid potential conflicts with user having 'std' directory
-        // in their working directory.
-        let std_dir = PathBuf::from(NU_STDLIB_VIRTUAL_DIR)
-            .join("std")
-            .to_string_lossy()
-            .to_string();
+        let std_dir = std_dir.to_string_lossy().to_string();
         let source = format!(
             r#"
 # Define the `std` module
