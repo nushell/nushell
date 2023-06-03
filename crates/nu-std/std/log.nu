nushell/nushell

commit edf627c3d156f551b780e713706de80f513166d6

refactor the "auto-deduction" error in a tool command

crates/nu-std/std/log.nu
@@ -222,6 +222,20 @@ export def debug [
     handle-log $message (log-types | get DEBUG) $format $short
 }
 
+def log-level-deduction-error [
+    span: record<start: int, end: int>
+    log_level: int
+] {
+    error make {
+        msg: $"Cannot deduce level prefix for given log level: ($log_level). Available log levels: ($env.LOG_LEVEL | values). You can configure them by the LOG_LEVEL env variable."
+        label: {
+            text: "Invalid log level for ansi auto-deduction"
+            start: $span.start
+            end: $span.end
+        }
+    }
+}
+
 # Log a message with a specific format and verbosity level, with either configurable or auto-deduced %LEVEL% and %ANSI_START% placeholder extensions
 export def custom [
     message: string, # A message
@@ -244,15 +258,7 @@ export def custom [
 
     let prefix = if ($level_prefix | is-empty) {
         if ($log_level not-in $valid_levels_for_defaulting) {
-            let span = (metadata $log_level).span
-            error make {
-                "msg": $"Cannot deduce level prefix for given log level: ($log_level). Available log levels: ($env.LOG_LEVEL | values). You can configure them by the LOG_LEVEL env variable."
-                label: {
-                    text: "Invalid log level for prefix auto-deduction"
-                    start: $span.start
-                    end: $span.end
-                }
-            }
+            log-level-deduction-error (metadata $log_level).span $log_level
         }
         
         parse-int-level $log_level
@@ -263,15 +269,7 @@ export def custom [
 
     let ansi = if ($ansi | is-empty) {
         if ($log_level not-in $valid_levels_for_defaulting) {
-            let span = (metadata $log_level).span
-            error make {
-                "msg": $"Cannot deduce ansi for given log level: ($log_level)"
-                label: {
-                    text: "Invalid log level for ansi auto-deduction"
-                    start: $span.start
-                    end: $span.end
-                }
-            }
+            log-level-deduction-error (metadata $log_level).span $log_level
         }
 
         (

commit 5f8055632d45f877c4b56c80b2a139094208cc1a

add `type` to the error command to fix the tests
related to https://github.com/nushell/nushell/actions/runs/5163292820/jobs/9301532478
where the output error does not contain the original text.

test bug introduced in edf627c3d.

crates/nu-std/std/log.nu
@@ -223,11 +223,12 @@ export def debug [
 }
 
 def log-level-deduction-error [
+    type: string
     span: record<start: int, end: int>
     log_level: int
 ] {
     error make {
-        msg: $"(ansi red_bold)Cannot deduce level prefix for given log level: ($log_level).(ansi reset)"
+        msg: $"(ansi red_bold)Cannot deduce ($type) for given log level: ($log_level).(ansi reset)"
         label: {
             text: ([
                  "Invalid log level."
@@ -262,7 +263,7 @@ export def custom [
 
     let prefix = if ($level_prefix | is-empty) {
         if ($log_level not-in $valid_levels_for_defaulting) {
-            log-level-deduction-error (metadata $log_level).span $log_level
+            log-level-deduction-error "level prefix" (metadata $log_level).span $log_level
         }
 
         parse-int-level $log_level
@@ -273,7 +274,7 @@ export def custom [
 
     let ansi = if ($ansi | is-empty) {
         if ($log_level not-in $valid_levels_for_defaulting) {
-            log-level-deduction-error (metadata $log_level).span $log_level
+            log-level-deduction-error "ansi" (metadata $log_level).span $log_level
         }
 
         (