e9ea4722aa5759a27e5fb67fb26c8ef46de75aed
JT <547158+jntrnr@users.noreply.github.com>
2023-06-18 12:42:34 +1200

    Update example_test.rs

--- crates/nu-cmd-extra/src/example_test.rs
+++ crates/nu-cmd-extra/src/example_test.rs
@@ -19,7 +19,7 @@ mod test_examples {
         engine::{Command, EngineState, StateWorkingSet},
         Type,
     };
-    use std::collections::HashMap;
+    use std::collections::HashSet;
 
     pub fn test_examples(cmd: impl Command + 'static) {
         let examples = cmd.examples();
