651e42459570d0527909eaa86f0c2aa9e2729df0
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-06-22 19:47:03 -0700

    code cleanup

--- crates/nu-cmd-extra/tests/main.rs
+++ crates/nu-cmd-extra/tests/main.rs
@@ -1,9 +1,1 @@
-//use nu_protocol::engine::EngineState;
-
 mod commands;
-
-/*
-fn create_default_context() -> EngineState {
-    nu_cmd_extra::add_extra_command_context(nu_cmd_lang::create_default_context())
-}
-*/
