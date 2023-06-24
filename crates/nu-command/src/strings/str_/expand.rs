98b1b3b203b7055ee6daebf2434488fc2d41178c
A. Taha Baki <atahabaki@pm.me>
2023-06-24 14:25:30 +0300

    str-expand: all potential errors handled isp

--- crates/nu-command/src/strings/str_/expand.rs
+++ crates/nu-command/src/strings/str_/expand.rs
@@ -133,36 +133,42 @@ fn str_expand(contents: &str, span: Span, value_span: Span) -> Value {
                             Value::List { vals: possibilities.iter().map(|e| Value::string(e,span)).collect::<Vec<Value>>(), span }
                         },
                         Err(e) => match e {
-                            bracoxide::ExpansionError::NumConversionFailed(_) => todo!(),
+                            bracoxide::ExpansionError::NumConversionFailed(s) => Value::Error { error:
+                                Box::new(ShellError::GenericError("Number Conversion Failed".to_owned(), format!("Number conversion failed at {s}."), Some(value_span), Some("Expected number, found text. Range format is `{M..N}`, where M and N are numeric values representing the starting and ending limits.".to_owned()), vec![])) },
                         },
                     }
                 },
-                Err(e) => match e {
-                    ParsingError::NoTokens => todo!(),
-                    ParsingError::OBraExpected(_) => todo!(),
-                    ParsingError::CBraExpected(_) => todo!(),
-                    ParsingError::RangeStartLimitExpected(_) => todo!(),
-                    ParsingError::RangeEndLimitExpected(_) => todo!(),
-                    ParsingError::ExpectedText(_) => todo!(),
-                    ParsingError::InvalidCommaUsage(_) => todo!(),
-                    ParsingError::ExtraCBra(_) => todo!(),
-                    ParsingError::ExtraOBra(_) => todo!(),
-                    ParsingError::NothingInBraces(_) => todo!(),
-                    ParsingError::RangeCantHaveText(_) => todo!(),
-                    ParsingError::ExtraRangeOperator(_) => todo!(),
-                },
+                Err(e) => Value::Error { error: Box::new(
+                    match e {
+                        ParsingError::NoTokens => ShellError::PipelineEmpty { dst_span: value_span },
+                        ParsingError::OBraExpected(s) => ShellError::GenericError("Opening Brace Expected".to_owned(), format!("Opening brace is expected at {s}."), Some(value_span), Some("In brace syntax, we use equal amount of opening (`{`) and closing (`}`). Please, take a look at the examples.".to_owned()), vec![]),
+                        ParsingError::CBraExpected(s) => ShellError::GenericError("Closing Brace Expected".to_owned(), format!("Closing brace is expected at {s}."), Some(value_span), Some("In brace syntax, we use equal amount of opening (`{`) and closing (`}`). Please, see the examples.".to_owned()), vec![]),
+                        ParsingError::RangeStartLimitExpected(s) => ShellError::GenericError("Range Start Expected".to_owned(), format!("Range start limit is missing, expected at {s}."), Some(value_span), Some("In brace syntax, Range is defined like `{X..Y}`, where X and Y are a number. X is the start, Y is the end. Please, inspect the examples for more information.".to_owned()), vec![]),
+                        ParsingError::RangeEndLimitExpected(s) => ShellError::GenericError("Range Start Expected".to_owned(), format!("Range start limit is missing, expected at {s}."), Some(value_span), Some("In brace syntax, Range is defined like `{X..Y}`, where X and Y are a number. X is the start, Y is the end. Please see the examples, for more information.".to_owned()), vec![]),
+                        ParsingError::ExpectedText(s) => ShellError::GenericError("Expected Text".to_owned(), format!("Expected text at {s}."), Some(value_span), Some("Texts are only allowed before opening brace (`{`), after closing brace (`}`), or inside `{}`. Please take a look at the examples.".to_owned()), vec![]),
+                        ParsingError::InvalidCommaUsage(s) => ShellError::GenericError("Invalid Comma Usage".to_owned(), format!("Found comma at {s}. Commas are only valid inside collection (`{{X,Y}}`)."), Some(value_span), Some("To escape comma use backslash `\\,`.".to_owned()), vec![]),
+                        ParsingError::RangeCantHaveText(s) => ShellError::GenericError("Range Can not Have Text".to_owned(), format!("Expecting, brace, number, or range operator, but found text at {s}."), Some(value_span), Some("Please use the format {M..N} for ranges in brace expansion, where M and N are numeric values representing the starting and ending limits of the sequence, respectively.".to_owned()), vec![]),
+                        ParsingError::ExtraRangeOperator(s) => ShellError::GenericError("Extra Range Operator".to_owned(), format!("Found additional, range operator at {s}."), Some(value_span), Some("Please, use the format `{M..N}` where M and N are numeric values representing the starting and ending limits of the range.".to_owned()), vec![]),
+                        ParsingError::ExtraCBra(s) => ShellError::GenericError("Extra Closing Brace".to_owned(), format!("Used extra closing brace at {s}."), Some(value_span), Some("To escape closing brace use backslash, e.g. `\\}`".to_owned()), vec![]),
+                        ParsingError::ExtraOBra(s) => ShellError::GenericError("Extra Opening Brace".to_owned(), format!("Used extra opening brace at {s}."), Some(value_span), Some("To escape opening brace use backslash, e.g. `\\{`".to_owned()), vec![]),
+                        ParsingError::NothingInBraces(s) => ShellError::GenericError("Nothing In Braces".to_owned(), format!("Nothing found inside braces at {s}."), Some(value_span), Some("Please provide valid content within the braces. Additionaly, you can safely remove it, not needed.".to_owned()), vec![]),
+                    }
+                ) }
             }
         },
         Err(e) => match e {
             TokenizationError::EmptyContent => Value::Error {
                 error: Box::new(ShellError::PipelineEmpty { dst_span: value_span }),
             },
-            TokenizationError::FormatNotSupported => Value::Error { 
+            TokenizationError::FormatNotSupported => Value::Error {
                 error: Box::new(
-                    ShellError::DelimiterError {
-                    msg: "Only opening or closing brace is used. Brace Expansion syntax is as follows: `{COL,LEC,TION}`.".to_owned(),
-                    span: value_span
-                })
+                    ShellError::GenericError(
+                        "Format Not Supported".to_owned(),
+                        "Usage of only `{` or `}`. Brace Expansion syntax, needs to have equal amount of opening (`{`) and closing (`}`)".to_owned(),
+                        Some(value_span),
+                        Some("In brace expansion syntax, it is important to have an equal number of opening (`{`) and closing (`}`) braces. Please ensure that you provide a balanced pair of braces in your brace expansion pattern.".to_owned()),
+                        vec![]
+                ))
             },
             TokenizationError::NoBraces => Value::Error {
                 error: Box::new(ShellError::GenericError("No Braces".to_owned(), "At least one `{}` brace expansion expected.".to_owned(), Some(value_span), Some("Please, examine the examples.".to_owned()), vec![]))
