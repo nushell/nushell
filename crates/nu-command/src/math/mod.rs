bd61f55768dce503e2be1acae9eba1a0e4fce496
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-07-10 10:37:49 -0700

    the six arc commands are now compiling

--- crates/nu-command/src/math/mod.rs
+++ crates/nu-command/src/math/mod.rs
@@ -1,10 +1,4 @@
 mod abs;
-mod arccos;
-mod arccosh;
-mod arcsin;
-mod arcsinh;
-mod arctan;
-mod arctanh;
 mod avg;
 mod ceil;
 mod cos;
@@ -58,13 +52,6 @@ pub use sin::SubCommand as MathSin;
 pub use sinh::SubCommand as MathSinH;
 pub use tan::SubCommand as MathTan;
 pub use tanh::SubCommand as MathTanH;
-
-pub use arccos::SubCommand as MathArcCos;
-pub use arccosh::SubCommand as MathArcCosH;
-pub use arcsin::SubCommand as MathArcSin;
-pub use arcsinh::SubCommand as MathArcSinH;
-pub use arctan::SubCommand as MathArcTan;
-pub use arctanh::SubCommand as MathArcTanH;
 
 pub use egamma::SubCommand as MathEulerGamma;
 pub use euler::SubCommand as MathEuler;
