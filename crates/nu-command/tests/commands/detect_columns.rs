1da6263e895351250ecc924a430a8a71c777b528
mengsuenyan <49850817+mengsuenyan@users.noreply.github.com>
2023-07-21 21:25:06 +0800

    fix #9653 the cmd `detect columns` with the flag `-c` (#9667)
    fix `detect columns` with flag `-c, --combine-columns` run failed when
    using some range
    
    - fixes #9653 
    
    fix #9653 the cmd detect columns with the flag -c, --combine-columns run
    failed when using some range.
    
    add unit test for the command `detect columns`
    
    ```text
    Attempt to automatically split text into multiple columns.
    
    Usage:
      > detect columns {flags} 
    
    Flags:
      -h, --help - Display the help message for this command
      -s, --skip <Int> - number of rows to skip before detecting
      -n, --no-headers - don't detect headers
      -c, --combine-columns <Range> - columns to be combined; listed as a range
    
    Signatures:
      <string> | detect columns -> <table>
    
    Examples:
      Splits string across multiple columns
      > 'a b c' | detect columns -n
      ╭───┬─────────┬─────────┬─────────╮
      │ # │ column0 │ column1 │ column2 │
      ├───┼─────────┼─────────┼─────────┤
      │ 0 │ a       │ b       │ c       │
      ╰───┴─────────┴─────────┴─────────╯
    
      Splits a multi-line string into columns with headers detected
      > $'c1 c2 c3 c4 c5(char nl)a b c d e' | detect columns
      ╭───┬────┬────┬────┬────┬────╮
      │ # │ c1 │ c2 │ c3 │ c4 │ c5 │
      ├───┼────┼────┼────┼────┼────┤
      │ 0 │ a  │ b  │ c  │ d  │ e  │
      ╰───┴────┴────┴────┴────┴────╯
    
      
      > $'c1 c2 c3 c4 c5(char nl)a b c d e' | detect columns -c 0..1
      ╭───┬─────┬────┬────┬────╮
      │ # │ c1  │ c3 │ c4 │ c5 │
      ├───┼─────┼────┼────┼────┤
      │ 0 │ a b │ c  │ d  │ e  │
      ╰───┴─────┴────┴────┴────╯
    
      Splits a multi-line string into columns with headers detected
      > $'c1 c2 c3 c4 c5(char nl)a b c d e' | detect columns -c -2..-1
      ╭───┬────┬────┬────┬─────╮
      │ # │ c1 │ c2 │ c3 │ c4  │
      ├───┼────┼────┼────┼─────┤
      │ 0 │ a  │ b  │ c  │ d e │
      ╰───┴────┴────┴────┴─────╯
    
      Splits a multi-line string into columns with headers detected
      > $'c1 c2 c3 c4 c5(char nl)a b c d e' | detect columns -c 2..
      ╭───┬────┬────┬───────╮
      │ # │ c1 │ c2 │  c3   │
      ├───┼────┼────┼───────┤
      │ 0 │ a  │ b  │ c d e │
      ╰───┴────┴────┴───────╯
    
      Parse external ls command and combine columns for datetime
      > ^ls -lh | detect columns --no-headers --skip 1 --combine-columns 5..7
    
    ```

---
+++ crates/nu-command/tests/commands/detect_columns.rs
@@ -0,0 +1,65 @@
+use nu_test_support::{nu, playground::Playground};
+
+#[test]
+fn detect_columns() {
+    let cases = [(
+        "$\"c1 c2 c3 c4 c5(char nl)a b c d e\"",
+        "[[c1,c2,c3,c4,c5]; [a,b,c,d,e]]",
+    )];
+
+    Playground::setup("detect_columns_test_1", |dirs, _| {
+        for case in cases.into_iter() {
+            let out = nu!(
+                cwd: dirs.test(),
+                "({} | detect columns) == {}",
+                case.0,
+                case.1
+            );
+
+            assert_eq!(
+                out.out, "true",
+                "({} | detect columns) == {}",
+                case.0, case.1
+            );
+        }
+    });
+}
+
+#[test]
+fn detect_columns_with_flag_c() {
+    let cases = [
+        (
+            "$\"c1 c2 c3 c4 c5(char nl)a b c d e\"",
+            "[[c1,c3,c4,c5]; ['a b',c,d,e]]",
+            "0..1",
+        ),
+        (
+            "$\"c1 c2 c3 c4 c5(char nl)a b c d e\"",
+            "[[c1,c2,c3,c4]; [a,b,c,'d e']]",
+            "(-2)..(-1)",
+        ),
+        (
+            "$\"c1 c2 c3 c4 c5(char nl)a b c d e\"",
+            "[[c1,c2,c3]; [a,b,'c d e']]",
+            "2..",
+        ),
+    ];
+
+    Playground::setup("detect_columns_test_1", |dirs, _| {
+        for case in cases.into_iter() {
+            let out = nu!(
+                cwd: dirs.test(),
+                "({} | detect columns -c {}) == {}",
+                case.0,
+                case.2,
+                case.1,
+            );
+
+            assert_eq!(
+                out.out, "true",
+                "({} | detect columns -c {}) == {}",
+                case.0, case.2, case.1
+            );
+        }
+    });
+}
