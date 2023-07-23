5a0f9ddec57270a9fdc620d7e15f092fb38ed24b
amtoine <stevan.antoine@gmail.com>
2023-07-23 16:10:32 +0200

    fix the test
    actually, the output of
    ```nushell
    [[date]; ["2022-01-01"] ["2023-01-01"]] | into datetime date
    ```
    is more like
    ```
    ╭───┬──────────────╮
    │ # │     date     │
    ├───┼──────────────┤
    │ 0 │ 2 years ago  │
    │ 1 │ 6 months ago │
    ╰───┴──────────────╯
    ```
    so we should probably more check for the presence of " ago" in the
    output.

--- crates/nu-command/tests/commands/into_datetime.rs
+++ crates/nu-command/tests/commands/into_datetime.rs
@@ -4,6 +4,5 @@ use nu_test_support::nu;
 fn into_datetime_table_column() {
     let actual = nu!(r#"[[date]; ["2022-01-01"] ["2023-01-01"]] | into datetime date"#);
 
-    assert!(actual.out.contains("Sat, 01 Jan 2022"));
-    assert!(actual.out.contains("Sun, 01 Jan 2023"));
+    assert!(actual.out.contains(" ago"));
 }
