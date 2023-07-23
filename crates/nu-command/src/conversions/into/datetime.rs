79359598db68fa0fff9daeb429c70cd89f4ee76a
Antoine Stevan <44101798+amtoine@users.noreply.github.com>
2023-07-23 20:14:51 +0200

    add `table -> table` to `into datetime` (#9775)
    should close https://github.com/nushell/nushell/issues/9774
    
    # Description
    given the help page of `into datetime`, 
    ```
    Parameters:
      ...rest <cellpath>: for a data structure input, convert data at the given cell paths
    ```
    it looks like `into datetime` should accept tables as input :thinking: 
    
    this PR
    - adds the `table -> table` signature to `into datetime`
    - adds a test to make sure the behaviour stays there

--- crates/nu-command/src/conversions/into/datetime.rs
+++ crates/nu-command/src/conversions/into/datetime.rs
@@ -68,7 +68,9 @@ impl Command for SubCommand {
             (Type::Int, Type::Date),
             (Type::String, Type::Date),
             (Type::List(Box::new(Type::String)), Type::List(Box::new(Type::Date))),
+            (Type::Table(vec![]), Type::Table(vec![])),
         ])
+        .allow_variants_without_examples(true)
         .named(
                 "timezone",
                 SyntaxShape::String,
