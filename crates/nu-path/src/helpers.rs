16815851b6ad918aec41a9e7e0e61a4bb969d0e2
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-07-26 00:12:55 +0100

    doc(nu-path): document home_dir/config_dir

--- crates/nu-path/src/helpers.rs
+++ crates/nu-path/src/helpers.rs
@@ -9,10 +9,13 @@ static CONFIG_DIR: Lazy<Option<PathBuf>> = Lazy::new(|| {
     let default = if let Some(mut config_dir) = home_dir() {
         config_dir.push(".config");
         if config_dir.join("nushell").is_dir() {
+            // `~/.config/nushell` exists, so we'll use that
             return Some(config_dir);
         }
+        // `~/.config/nushell` doesn't exist, but we'll use it if the "native" folder doesn't exist either
         Some(config_dir)
     } else {
+        // `~` not found, so we'll return `None` if the "native" folder doesn't exist either
         None
     };
 
@@ -23,17 +26,32 @@ static CONFIG_DIR: Lazy<Option<PathBuf>> = Lazy::new(|| {
             basedirs.config_dir()
         };
         if config_home.join("nushell").is_dir() {
+            // "native" config folder exists, so we'll use that
             return Some(config_home);
         }
     }
 
+    // fresh install, so we'll use the default defined above
     default
 });
 
+/// Returns the home directory of the current user.
+///
+/// Uses the `HOME` environment variable on Linux and macOS, if set, otherwise `getpwuid_r`.
+/// Uses the `USERPROFILE` environment variable on Windows, if set, otherwise `SHGetKnownFolderPath` with `CSIDL_PROFILE`.
 pub fn home_dir() -> Option<PathBuf> {
     HOME_DIR.clone()
 }
 
+/// Returns the path where the `nushell` folder should be located.
+///
+/// If `~/.config/nushell` exists, returns `~/.config`.
+/// Checks the following path based on the OS:
+///   - Linux: `~/.config/`
+///   - macOS: `~/Library/Application Support/`
+///   - Windows: `{FOLDERID_RoamingAppData}`
+/// If the `nushell` folder exists in the path, returns the path.
+/// Otherwise, returns `~/.config'.
 pub fn config_dir() -> Option<PathBuf> {
     CONFIG_DIR.clone()
 }
