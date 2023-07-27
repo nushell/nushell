08d82096de6aa1198bdcf034767dea9624b091fd
WindSoilder <WindSoilder@outlook.com>
2023-07-27 15:44:43 +0800

    fix append signature

--- crates/nu-command/src/filters/append.rs
+++ crates/nu-command/src/filters/append.rs
@@ -22,7 +22,7 @@ impl Command for Append {
                     Type::List(Box::new(Type::Any)),
                 ),
                 (Type::Record(vec![]), Type::Table(vec![])),
-                (Type::Any, Type::List(Box::new(Type::Any))),
+                (Type::String, Type::List(Box::new(Type::Any))),
             ])
             .required("row", SyntaxShape::Any, "the row, list, or table to append")
             .allow_variants_without_examples(true)
