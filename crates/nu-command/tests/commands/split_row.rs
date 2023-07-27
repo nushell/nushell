88fc5cff22346c0df7a04d7befea1151ebfaad92
sholderbach <sholderbach@users.noreply.github.com>
2023-07-27 21:12:13 +0200

    Fix test to check both reported and actual sig

--- crates/nu-command/tests/commands/split_row.rs
+++ crates/nu-command/tests/commands/split_row.rs
@@ -47,15 +47,13 @@ fn to_row() {
         assert!(actual.out.contains('5'));
 
         let actual = nu!(
-            cwd: dirs.test(), pipeline(
             r#"
-                open sample2.txt
-                | lines
-                | str trim
-                | split row -r '\s*,\s*'
-                | describe
+                def foo [a: list<string>] {
+                    $a | describe
+                }
+                foo (["a b", "c d"] | split row " ")
             "#
-        ));
+        );
 
         assert!(actual.out.contains("list<string>"));
     })
