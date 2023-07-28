25c31b4b25f0abebd2e0c3f9d8d3a0225e203d1e
amtoine <stevan.antoine@gmail.com>
2023-07-28 15:54:24 +0200

    add tests

--- crates/nu-command/tests/commands/inspect.rs
+++ crates/nu-command/tests/commands/inspect.rs
@@ -5,3 +5,21 @@ fn inspect_with_empty_pipeline() {
     let actual = nu!("inspect");
     assert!(actual.err.contains("no input value was piped in"));
 }
+
+#[test]
+fn inspect_with_null() {
+    let actual = nu!("null | inspect");
+    assert!(actual.err.contains("no input value was piped in"));
+}
+
+#[test]
+fn inspect_with_empty_list() {
+    let actual = nu!("[] | inspect");
+    assert!(actual.err.is_empty());
+}
+
+#[test]
+fn inspect_with_empty_table() {
+    let actual = nu!("{} | inspect");
+    assert!(actual.err.is_empty());
+}
