1da4f8823ffbad64b4767c9d96fd490e350e9c17
Jack Wright <56345+ayax79@users.noreply.github.com>
2023-07-31 05:34:12 -0700

    Merged overloaded commands (#9860)
    - fixes #9807
    
    # Description
    
    This pull request merges all overloaded dfr commands into one command:
    
    eager:
    dfr first -> eager/first.rs
    dfr last -> eager/last.rs
    dfr into-nu -> eager/to_nu.rs (merged)
    
    lazy:
    dfr min -> expressions/expressions_macro.rs lazy_expressions_macro
    dfr max -> expressions/expressions_macro.rs lazy_expressions_macro
    dfr sum -> expressions/expressions_macro.rs lazy_expressions_macro
    dfr mean -> expressions/expressions_macro.rs lazy_expressions_macro
    dfr std -> expressions/expressions_macro.rs lazy_expressions_macro
    dfr var   -> expressions/expressions_macro.rs lazy_expressions_macro
    
    series:
    dfr n-unique -> series/n_unique.rs
    dfr is-not-null -> series/masks/is_not_null.rs
    dfr is-null -> series/masks/is_null.rs
    
    # User-Facing Changes
    No user facing changes
    
    ---------
    
    Co-authored-by: Jack Wright <jack.wright@disqo.com>

--- crates/nu-cmd-dataframe/src/dataframe/series/n_unique.rs
+++ crates/nu-cmd-dataframe/src/dataframe/series/n_unique.rs
@@ -1,4 +1,4 @@
-use super::super::values::{Column, NuDataFrame};
+use super::super::values::{Column, NuDataFrame, NuExpression};
 use nu_protocol::{
     ast::Call,
     engine::{Command, EngineState, Stack},
@@ -19,26 +19,39 @@ impl Command for NUnique {
 
     fn signature(&self) -> Signature {
         Signature::build(self.name())
-            .input_output_type(
-                Type::Custom("dataframe".into()),
-                Type::Custom("dataframe".into()),
-            )
+            .input_output_types(vec![
+                (
+                    Type::Custom("expression".into()),
+                    Type::Custom("expression".into()),
+                ),
+                (
+                    Type::Custom("dataframe".into()),
+                    Type::Custom("dataframe".into()),
+                ),
+            ])
             .category(Category::Custom("dataframe".into()))
     }
 
     fn examples(&self) -> Vec<Example> {
-        vec![Example {
-            description: "Counts unique values",
-            example: "[1 1 2 2 3 3 4] | dfr into-df | dfr n-unique",
-            result: Some(
-                NuDataFrame::try_from_columns(vec![Column::new(
-                    "count_unique".to_string(),
-                    vec![Value::test_int(4)],
-                )])
-                .expect("simple df for test should not fail")
-                .into_value(Span::test_data()),
-            ),
-        }]
+        vec![
+            Example {
+                description: "Counts unique values",
+                example: "[1 1 2 2 3 3 4] | dfr into-df | dfr n-unique",
+                result: Some(
+                    NuDataFrame::try_from_columns(vec![Column::new(
+                        "count_unique".to_string(),
+                        vec![Value::test_int(4)],
+                    )])
+                    .expect("simple df for test should not fail")
+                    .into_value(Span::test_data()),
+                ),
+            },
+            Example {
+                description: "Creates a is n-unique expression from a column",
+                example: "dfr col a | dfr n-unique",
+                result: None,
+            },
+        ]
     }
 
     fn run(
@@ -48,8 +61,19 @@ impl Command for NUnique {
         call: &Call,
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let df = NuDataFrame::try_from_pipeline(input, call.head)?;
-        command(engine_state, stack, call, df)
+        let value = input.into_value(call.head);
+        if NuDataFrame::can_downcast(&value) {
+            let df = NuDataFrame::try_from_value(value)?;
+            command(engine_state, stack, call, df)
+        } else {
+            let expr = NuExpression::try_from_value(value)?;
+            let expr: NuExpression = expr.into_polars().n_unique().into();
+
+            Ok(PipelineData::Value(
+                NuExpression::into_value(expr, call.head),
+                None,
+            ))
+        }
     }
 }
 
@@ -77,11 +101,24 @@ fn command(
 
 #[cfg(test)]
 mod test {
-    use super::super::super::test_dataframe::test_dataframe;
+    use super::super::super::test_dataframe::{build_test_engine_state, test_dataframe_example};
     use super::*;
+    use crate::dataframe::lazy::aggregate::LazyAggregate;
+    use crate::dataframe::lazy::groupby::ToLazyGroupBy;
 
     #[test]
-    fn test_examples() {
-        test_dataframe(vec![Box::new(NUnique {})])
+    fn test_examples_dataframe() {
+        let mut engine_state = build_test_engine_state(vec![Box::new(NUnique {})]);
+        test_dataframe_example(&mut engine_state, &NUnique.examples()[0]);
+    }
+
+    #[test]
+    fn test_examples_expression() {
+        let mut engine_state = build_test_engine_state(vec![
+            Box::new(NUnique {}),
+            Box::new(LazyAggregate {}),
+            Box::new(ToLazyGroupBy {}),
+        ]);
+        test_dataframe_example(&mut engine_state, &NUnique.examples()[1]);
     }
 }
