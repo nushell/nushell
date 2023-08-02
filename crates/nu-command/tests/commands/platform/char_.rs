6528dcbe0a0dffd37a35c91571664f4196f3dad8
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-08-02 10:23:08 -0500

    typo

--- crates/nu-command/tests/commands/platform/char_.rs
+++ crates/nu-command/tests/commands/platform/char_.rs
@@ -1,7 +1,7 @@
 use nu_test_support::{nu, pipeline};
 
 #[test]
-fn test_ansi_list_outputs_table() {
+fn test_char_list_outputs_table() {
     let actual = nu!(pipeline(
         r#"
             char --list | length
