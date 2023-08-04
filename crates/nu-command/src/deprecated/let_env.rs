052fefe94f76eae062c3edc91310e078d0f2e1a0
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-08-04 13:21:01 -0500

    make params optional

--- crates/nu-command/src/deprecated/let_env.rs
+++ crates/nu-command/src/deprecated/let_env.rs
@@ -1,6 +1,6 @@
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
-use nu_protocol::{Category, PipelineData, ShellError, Signature, Type};
+use nu_protocol::{Category, PipelineData, ShellError, Signature, SyntaxShape, Type};
 
 #[derive(Clone)]
 pub struct LetEnvDeprecated;
@@ -14,6 +14,12 @@ impl Command for LetEnvDeprecated {
         Signature::build(self.name())
             .input_output_types(vec![(Type::Nothing, Type::Nothing)])
             .allow_variants_without_examples(true)
+            .optional("var_name", SyntaxShape::String, "variable name")
+            .optional(
+                "initial_value",
+                SyntaxShape::Keyword(b"=".to_vec(), Box::new(SyntaxShape::MathExpression)),
+                "equals sign followed by value",
+            )
             .category(Category::Deprecated)
     }
 
