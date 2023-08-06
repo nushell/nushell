b8679d9c75159cd9471f0d5833a790bc861ef8ea
Antoine Stevan <44101798+amtoine@users.noreply.github.com>
2023-08-06 13:42:16 +0200

    remove old deprecated commands (#9840)
    # Description
    in this PR i propose to remove the following old deprecated commands
    - `hash base64` -> `encode base64`
    - `math eval` -> `math <sub>`
    - `str to-datetime` -> `into datetime`
    - `str to-decimal` -> `into decimal`
    - `str find-replace` -> `str replace`
    - `str to-int` -> `into int`
    - `keep` -> `take`
    - `match` -> `find`
    - `nth` -> `select`
    - `pivot` -> `transpose`
    - `unalias` -> `hide`
    - `all?` -> `all`
    - `any?` -> `any`
    - `empty?` -> `is-empty`
    - `build-string` -> `str join` / string concatenation with `+`
    - `str lpad` -> `fill`
    - `str rpad` -> `fill`
    - `str collect` -> `str join`
    - `old-alias` -> `alias`
    
    so old i do not remember them at all :open_mouth:
    
    i left the following four commands because they have been moved much
    more recently i think!
    - `fetch` -> `http get`
    - `post` -> `http post`
    - `benchmark` -> `timeit`
    - `let-env`
    - `date format` -> `format date`
    
    # User-Facing Changes
    
    # Tests + Formatting
    - :green_circle: `toolkit fmt`
    - :green_circle: `toolkit clippy`
    - :black_circle: `toolkit test`
    - :black_circle: `toolkit test stdlib`
    
    # After Submitting

--- crates/nu-command/src/deprecated/mod.rs
+++ crates/nu-command/src/deprecated/mod.rs
@@ -1,25 +1,7 @@
-mod collect;
 mod deprecated_commands;
 mod format;
-mod hash_base64;
 mod let_env;
-mod lpad;
-mod math_eval;
-mod rpad;
-mod str_datetime;
-mod str_decimal;
-mod str_find_replace;
-mod str_int;
 
-pub use collect::StrCollectDeprecated;
 pub use deprecated_commands::*;
 pub use format::SubCommand as DateFormat;
-pub use hash_base64::HashBase64;
-pub use let_env::LetEnvDeprecated;
-pub use lpad::LPadDeprecated;
-pub use math_eval::SubCommand as MathEvalDeprecated;
-pub use rpad::RPadDeprecated;
-pub use str_datetime::StrDatetimeDeprecated;
-pub use str_decimal::StrDecimalDeprecated;
-pub use str_find_replace::StrFindReplaceDeprecated;
-pub use str_int::StrIntDeprecated;
+pub use let_env::LetEnv;
