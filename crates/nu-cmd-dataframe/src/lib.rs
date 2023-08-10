nushell/nushell

commit c55b5c0a55896f998d01a0bed6d53fa8a90381c2

move dataframe commands to nu-cmd-dataframe (#9241)
All of the dataframe commands ported over with no issues...

### 11 tests are commented out (for now)

So 100 of the original 111 tests are passing with only 11 tests being
ignored for now..

As per our conversation in the core team meeting on Wednesday
I took @jntrnr  suggestion and just commented out the tests dealing
with
[IntoDatetime](https://github.com/nushell/nushell/blob/main/crates/nu-command/src/conversions/into/mod.rs)

Later on we can move this functionality out of nu-command if we decide
it makes sense...

### The following tests were ignored...

```rust
modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_day.rs
modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_hour.rs
modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_minute.rs

modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_month.rs
modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_nanosecond.rs
modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_ordinal.rs

modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_second.rs
modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_week.rs
modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_weekday.rs

modified:   crates/nu-cmd-dataframe/src/dataframe/series/date/get_year.rs
modified:   crates/nu-cmd-dataframe/src/dataframe/series/string/strftime.rs
```

crates/nu-cmd-dataframe/Cargo.toml
@@ -1,0 +1,68 @@
+[package]
+authors = ["The Nushell Project Developers"]
+description = "Nushell's dataframe commands based on polars."
+edition = "2021"
+license = "MIT"
+name = "nu-cmd-dataframe"
+repository = "https://github.com/nushell/nushell/tree/main/crates/nu-cmd-dataframe"
+version = "0.80.1"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+[lib]
+bench = false
+
+[dependencies]
+nu-cmd-lang = { path = "../nu-cmd-lang", version = "0.80.1" }
+nu-engine = { path = "../nu-engine", version = "0.80.1" }
+nu-parser = { path = "../nu-parser", version = "0.80.1" }
+nu-protocol = { path = "../nu-protocol", version = "0.80.1" }
+
+# Potential dependencies for extras
+chrono = { version = "0.4.23", features = [
+	"std",
+	"unstable-locales",
+], default-features = false }
+fancy-regex = "0.11.0"
+indexmap = { version = "1.7", features = ["serde-1"] }
+num = { version = "0.4.0", optional = true }
+serde = { version = "1.0.123", features = ["derive"] }
+sqlparser = { version = "0.32.0", features = ["serde"], optional = true }
+
+[dependencies.polars]
+features = [
+	"arg_where",
+	"checked_arithmetic",
+	"concat_str",
+	"cross_join",
+	"csv",
+	"cum_agg",
+	"default",
+	"dtype-categorical",
+	"dtype-datetime",
+	"dtype-struct",
+	"dynamic_groupby",
+	"ipc",
+	"is_in",
+	"json",
+	"lazy",
+	"object",
+	"parquet",
+	"random",
+	"rolling_window",
+	"rows",
+	"serde",
+	"serde-lazy",
+	"strings",
+	"strings",
+	"to_dummies",
+]
+optional = true
+version = "0.29.0"
+
+[features]
+dataframe = ["default"]
+default = ["num", "polars", "sqlparser"]
+
+[dev-dependencies]
+nu-test-support = { path = "../nu-test-support", version = "0.80.1" }

crates/nu-cmd-dataframe/src/lib.rs
@@ -1,0 +1,2 @@
+pub mod dataframe;
+pub use dataframe::*;

Cargo.toml
@@ -28,6 +28,7 @@ members = [
 	"crates/nu-parser",
 	"crates/nu-system",
 	"crates/nu-cmd-lang",
+	"crates/nu-cmd-dataframe",
 	"crates/nu-command",
 	"crates/nu-protocol",
 	"crates/nu-plugin",
@@ -45,6 +46,7 @@ members = [
 nu-cli = { path = "./crates/nu-cli", version = "0.80.1" }
 nu-color-config = { path = "./crates/nu-color-config", version = "0.80.1" }
 nu-cmd-lang = { path = "./crates/nu-cmd-lang", version = "0.80.1" }
+nu-cmd-dataframe = { path = "./crates/nu-cmd-dataframe", version = "0.80.1", optional = true }
 nu-command = { path = "./crates/nu-command", version = "0.80.1" }
 nu-engine = { path = "./crates/nu-engine", version = "0.80.1" }
 nu-json = { path = "./crates/nu-json", version = "0.80.1" }

crates/nu-command/Cargo.toml
@@ -14,6 +14,7 @@ bench = false
 
 [dependencies]
 nu-cmd-lang = { path = "../nu-cmd-lang", version = "0.80.1" }
+nu-cmd-dataframe = { path = "../nu-cmd-dataframe", version = "0.80.1", optional = true }
 nu-color-config = { path = "../nu-color-config", version = "0.80.1" }
 nu-engine = { path = "../nu-engine", version = "0.80.1" }
 nu-explore = { path = "../nu-explore", version = "0.80.1" }
@@ -110,43 +111,12 @@ users = "0.11.0"
 optional = true
 version = "3.0.1"
 
-[dependencies.polars]
-features = [
-	"arg_where",
-	"checked_arithmetic",
-	"concat_str",
-	"cross_join",
-	"csv",
-	"cum_agg",
-	"default",
-	"dtype-categorical",
-	"dtype-datetime",
-	"dtype-struct",
-	"dynamic_groupby",
-	"ipc",
-	"is_in",
-	"json",
-	"lazy",
-	"object",
-	"parquet",
-	"random",
-	"rolling_window",
-	"rows",
-	"serde",
-	"serde-lazy",
-	"strings",
-	"strings",
-	"to_dummies",
-]
-optional = true
-version = "0.29.0"
-
 [target.'cfg(windows)'.dependencies.windows]
 features = ["Win32_Foundation", "Win32_Storage_FileSystem", "Win32_System_SystemServices"]
 version = "0.48.0"
 
 [features]
-dataframe = ["num", "polars", "sqlparser"]
+dataframe = ["dep:nu-cmd-dataframe"]
 plugin = ["nu-parser/plugin"]
 sqlite = [
 	"rusqlite",

crates/nu-command/src/lib.rs
@@ -61,12 +61,6 @@ pub use system::*;
 pub use util::*;
 pub use viewers::*;
 
-#[cfg(feature = "dataframe")]
-mod dataframe;
-
-#[cfg(feature = "dataframe")]
-pub use dataframe::*;
-
 #[cfg(feature = "sqlite")]
 mod database;
 

commit d5fa7b8a557ca0b0ca272cf82c66c2cc2e940f39

Put heavy dataframe dependencies behind feature flag (#9971)
Context from Discord:
https://discord.com/channels/601130461678272522/615962413203718156/1138694933545504819

I was working on Nu for the first time in a while and I noticed that
sometimes rust-analyzer takes a really long time to run `cargo check` on
the entire workspace. I dug in and it was checking a bunch of
dataframe-related dependencies even though the `dataframe` feature is
not built by default.

It looks like this is a regression of sorts, introduced by
https://github.com/nushell/nushell/pull/9241. Thankfully the fix is
pretty easy, we can make it so everything important in
`nu-cmd-dataframe` is only used when the `dataframe` feature is enabled.

### Impact on `cargo check --workspace`

Before this PR: 635 crates, 33.59s
After this PR: 498 crates, ~20s

(with the `mold` linker and a `cargo clean` before each run, the
relative difference for incremental checks will likely be much larger)

crates/nu-cmd-dataframe/Cargo.toml
@@ -56,8 +56,8 @@ optional = true
 version = "0.30.0"
 
 [features]
-dataframe = ["default"]
-default = ["num", "polars", "sqlparser"]
+dataframe = ["num", "polars", "sqlparser"]
+default = []
 
 [dev-dependencies]
 nu-cmd-lang = { path = "../nu-cmd-lang", version = "0.83.2" }

crates/nu-cmd-dataframe/src/lib.rs
@@ -1,2 +1,4 @@
+#[cfg(feature = "dataframe")]
 pub mod dataframe;
+#[cfg(feature = "dataframe")]
 pub use dataframe::*;