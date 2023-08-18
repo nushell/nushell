507dd028e9de31217b20dff5252788e934a526eb
JT <547158+jntrnr@users.noreply.github.com>
2023-08-19 07:17:00 +1200

    move 'bytes' back to commands

--- crates/nu-command/src/lib.rs
+++ crates/nu-command/src/lib.rs
@@ -1,3 +1,4 @@
+mod bytes;
 mod charting;
 mod conversions;
 mod date;
@@ -27,6 +28,7 @@ mod strings;
 mod system;
 mod viewers;
 
+pub use bytes::*;
 pub use charting::*;
 pub use conversions::*;
 pub use date::*;
