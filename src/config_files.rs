ed17039dd397d79dfb00242e61c78274bb66d1ce
Jakub Žádník <kubouch@gmail.com>
2023-08-20 18:56:49 +0300

    Remove unused import

--- src/config_files.rs
+++ src/config_files.rs
@@ -4,7 +4,7 @@ use nu_cli::read_plugin_file;
 use nu_cli::{eval_config_contents, eval_source};
 use nu_path::canonicalize_with;
 use nu_protocol::engine::{EngineState, Stack, StateWorkingSet};
-use nu_protocol::{report_error, Span};
+use nu_protocol::report_error;
 use nu_protocol::{ParseError, PipelineData, Spanned};
 use nu_utils::{get_default_config, get_default_env};
 use std::fs::File;
