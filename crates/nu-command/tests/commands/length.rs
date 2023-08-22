7bd81e4eb8f789ac4e7e0a422e77e5dc2b1885e0
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-08-22 14:41:35 -0500

    update tests

--- crates/nu-command/tests/commands/length.rs
+++ crates/nu-command/tests/commands/length.rs
@@ -2,14 +2,14 @@ use nu_test_support::nu;
 
 #[test]
 fn length_columns_in_cal_table() {
-    let actual = nu!("cal | length -c");
+    let actual = nu!("cal | columns | length");
 
     assert_eq!(actual.out, "7");
 }
 
 #[test]
 fn length_columns_no_rows() {
-    let actual = nu!("echo [] | length -c");
+    let actual = nu!("echo [] | length");
 
     assert_eq!(actual.out, "0");
 }
