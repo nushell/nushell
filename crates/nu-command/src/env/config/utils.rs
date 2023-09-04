54dd61d301f21c1a8de2f269e59eb2724d9768d9
Horasal <horsal@horsal.dev>
2023-09-05 06:11:35 +0900

    improve error message

--- crates/nu-command/src/env/config/utils.rs
+++ crates/nu-command/src/env/config/utils.rs
@@ -8,6 +8,44 @@ use nu_protocol::{
 
 use crate::ExternalCommand;
 
+const HELP_MSG: &str = "Nushell's config file can be found with the command: $nu.config-path. \
+For more help: (https://nushell.sh/book/configuration.html#configurations-with-built-in-commands)";
+
+fn get_editor_commandline(value: &Value) -> Result<(String, Vec<String>), ShellError> {
+    match value {
+        Value::String { val, .. } if !val.is_empty() => Ok((val.to_string(), Vec::new())),
+        Value::List { vals, .. } if !vals.is_empty() => {
+            let mut editor_cmd = vals.iter().map(|l| l.as_string());
+            match editor_cmd.next().transpose()? {
+                Some(editor) if !editor.is_empty() => {
+                    let params = editor_cmd.collect::<Result<_, ShellError>>()?;
+                    Ok((editor, params))
+                }
+                _ => Err(ShellError::GenericError(
+                    "Editor's executable is missing".into(),
+                    "Set the first element to an executable".into(),
+                    Some(value.span()),
+                    Some(HELP_MSG.into()),
+                    vec![],
+                )),
+            }
+        }
+        Value::String { .. } | Value::List { .. } => Err(ShellError::GenericError(
+            "Editor can not be empty".into(),
+            "Specify one in $EDITOR or $VISUAL".into(),
+            Some(value.span()),
+            Some(HELP_MSG.into()),
+            vec![],
+        )),
+        x => Err(ShellError::CantConvert {
+            to_type: "string or list<string>".into(),
+            from_type: x.get_type().to_string(),
+            span: value.span(),
+            help: None,
+        }),
+    }
+}
+
 pub(crate) fn get_editor(
     engine_state: &EngineState,
     stack: &mut Stack,
@@ -18,45 +56,13 @@ pub(crate) fn get_editor(
     if !config.buffer_editor.is_empty() {
         Ok((config.buffer_editor.clone(), Vec::new()))
     } else if let Some(value) = env_vars.get("EDITOR").or_else(|| env_vars.get("VISUAL")) {
-        match value {
-            Value::String { val, .. } if !val.is_empty() => Ok((val.to_string(), Vec::new())),
-            Value::List { vals, .. } if !vals.is_empty() => {
-                let mut editor_cmd = vals.iter().map(|l| l.as_string());
-                match editor_cmd.next().transpose()? {
-                    Some(editor) if !editor.is_empty() => {
-                        let params = editor_cmd.collect::<Result<_, ShellError>>()?;
-                        Ok((editor, params))
-                    },
-                    _ => {
-                        Err(ShellError::GenericError(
-                                    "Empty editor configured".into(),
-                                    "$EDITOR or $VISUAL should be an non-empty String or List<String>".into(),
-                                    Some(span),
-                                    Some(
-                                        "Nushell's config file can be found with the command: $nu.config-path. For more help: (https://nushell.sh/book/configuration.html#configurations-with-built-in-commands)"
-                                            .into(),
-                                    ),
-                                    vec![],
-                                ))
-                    }
-                }
-            }
-            x => Err(ShellError::CantConvert {
-                to_type: "string or list<string>".into(),
-                from_type: x.get_type().to_string(),
-                span: value.span(),
-                help: None,
-            }),
-        }
+        get_editor_commandline(value)
     } else {
         Err(ShellError::GenericError(
             "No editor configured".into(),
             "Please specify one via environment variables $EDITOR or $VISUAL".into(),
             Some(span),
-            Some(
-                "Nushell's config file can be found with the command: $nu.config-path. For more help: (https://nushell.sh/book/configuration.html#configurations-with-built-in-commands)"
-                    .into(),
-            ),
+            Some(HELP_MSG.into()),
             vec![],
         ))
     }
