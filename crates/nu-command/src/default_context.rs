d53b0a99d0d870506a81a89794348a27d501540a
Stefan Holderbach <sholderbach@users.noreply.github.com>
2023-09-12 13:03:05 +0200

    Rename `random decimal` to `random float` (#10320)
    # Description
    Similar to #9979
    
    # User-Facing Changes
    `random decimal` will now raise a warning and can be removed in an
    upcoming release.
    
    New command is named `random float`
    
    # Tests + Formatting
    Tests updated and improved.

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -368,6 +368,7 @@ pub fn add_shell_command_context(mut engine_state: EngineState) -> EngineState {
             RandomChars,
             RandomDecimal,
             RandomDice,
+            RandomFloat,
             RandomInteger,
             RandomUuid,
         };
