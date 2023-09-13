b0af83cd90321681cc3a28ebea49b6096525f622
Goncalo Gomes <gomesgoncalo@gmail.com>
2023-09-13 15:25:11 +0100

    fix #10319: allow json request of value type list
    this commit adds the handling of Value::List when BodyType is Json

--- crates/nu-command/tests/commands/network/http/post.rs
+++ crates/nu-command/tests/commands/network/http/post.rs
@@ -94,3 +94,21 @@ fn http_post_json_is_success() {
     mock.assert();
     assert!(actual.out.is_empty())
 }
+
+#[test]
+fn http_post_json_list_is_success() {
+    let mut server = Server::new();
+
+    let mock = server
+        .mock("POST", "/")
+        .match_body(r#"[{"foo":"bar"}]"#)
+        .create();
+
+    let actual = nu!(format!(
+        r#"http post -t 'application/json' {url} [{{foo: "bar"}}]"#,
+        url = server.url()
+    ));
+
+    mock.assert();
+    assert!(actual.out.is_empty())
+}
