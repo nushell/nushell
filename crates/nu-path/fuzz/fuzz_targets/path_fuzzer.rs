e5a51ff4aec79ceab28a1cfb5c7a71eae4509b93
toka <tokazerkje@outlook.com>
2023-09-14 22:12:11 +0200

    fmt

--- crates/nu-path/fuzz/fuzz_targets/path_fuzzer.rs
+++ crates/nu-path/fuzz/fuzz_targets/path_fuzzer.rs
@@ -1,15 +1,15 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use nu_path::{expand_to_real_path, trim_trailing_slash, expand_tilde, expand_path_with};
+use nu_path::{expand_path_with, expand_tilde, expand_to_real_path, trim_trailing_slash};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         let path = std::path::Path::new(s);
-        
+
         // Fuzzing expand_to_real_path function
         let _ = expand_to_real_path(path);
-        
+
         // Fuzzing trim_trailing_slash function
         let _ = trim_trailing_slash(s);
 
@@ -17,7 +17,7 @@ fuzz_target!(|data: &[u8]| {
         let _ = expand_tilde(path);
 
         // Fuzzing expand_path_with function
-        // Here, we're assuming a second path for the "relative to" aspect. 
+        // Here, we're assuming a second path for the "relative to" aspect.
         // For simplicity, we're just using the current directory.
         let current_dir = std::path::Path::new(".");
         let _ = expand_path_with(path, &current_dir);
