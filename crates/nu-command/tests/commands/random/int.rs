468b73266c9f4ea0b3228fa1741fd0f13c316338
amtoine <stevan.antoine@gmail.com>
2023-10-01 16:49:04 +0200

    rename the tests

--- crates/nu-command/tests/commands/random/int.rs
+++ crates/nu-command/tests/commands/random/int.rs
@@ -2,21 +2,21 @@ use nu_test_support::nu;
 
 #[test]
 fn generates_an_integer() {
-    let actual = nu!("random integer 42..43");
+    let actual = nu!("random int 42..43");
 
     assert!(actual.out.contains("42") || actual.out.contains("43"));
 }
 
 #[test]
 fn generates_55() {
-    let actual = nu!("random integer 55..55");
+    let actual = nu!("random int 55..55");
 
     assert!(actual.out.contains("55"));
 }
 
 #[test]
 fn generates_0() {
-    let actual = nu!("random integer ..<1");
+    let actual = nu!("random int ..<1");
 
     assert!(actual.out.contains('0'));
 }
