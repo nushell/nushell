3ebeb724f957d3ee18592fa6bf631d0cff14c26e
Jake Swenson <jake@jakeswenson.com>
2023-10-04 08:42:49 -0700

    Narrow code even more

--- crates/nu-protocol/src/engine/engine_state.rs
+++ crates/nu-protocol/src/engine/engine_state.rs
@@ -269,8 +269,10 @@ impl EngineState {
             }
         }
 
-        if let Some(mut config_record) = stack.get_env_var(self, "config") {
-            let (config, error) = config_record.into_config(self.get_config());
+        // Pull config value from any active overlays
+        if let Some(config_record) = self.get_env_var("config") {
+            let mut config_value = config_record.clone();
+            let (config, error) = config_value.into_config(self.get_config());
             self.config = config;
             if let Some(e) = error {
                 return Err(e);
