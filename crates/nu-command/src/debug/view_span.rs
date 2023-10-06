0dbd014d8bb2e370de86d81b16b8c8bbb4e7f43c
Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>
2023-10-06 18:54:15 +0200

    Use long options for debug (#10621)
    Also add short options for `profile`

--- crates/nu-command/src/debug/view_span.rs
+++ crates/nu-command/src/debug/view_span.rs
@@ -60,8 +60,8 @@ impl Command for ViewSpan {
 
     fn examples(&self) -> Vec<Example> {
         vec![Example {
-            description: "View the source of a span. 1 and 2 are just example values. Use the return of debug -r to get the actual values",
-            example: r#"some | pipeline | or | variable | debug -r; view span 1 2"#,
+            description: "View the source of a span. 1 and 2 are just example values. Use the return of debug --raw to get the actual values",
+            example: r#"some | pipeline | or | variable | debug --raw; view span 1 2"#,
             result: None,
         }]
     }
