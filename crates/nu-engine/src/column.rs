adde327915986d48cb3f23610cc988694fc196c8
Gaëtan <blaxoujunior@gmail.com>
2023-10-16 12:53:37 +0200

    Use index set

--- crates/nu-engine/src/column.rs
+++ crates/nu-engine/src/column.rs
@@ -1,23 +1,19 @@
+use indexmap::IndexSet;
 use nu_protocol::Value;
 use std::collections::HashSet;
 
 pub fn get_columns(input: &[Value]) -> Vec<String> {
-    let mut set: HashSet<&String> = HashSet::new();
-    let mut cols = vec![];
+    let mut set: IndexSet<&String> = IndexSet::new();
 
     for item in input {
         let Value::Record { val, .. } = item else {
             return vec![];
         };
 
-        for col in &val.cols {
-            if set.insert(col) {
-                cols.push(col.to_string());
-            }
-        }
+        set.extend(&val.cols);
     }
 
-    cols
+    set.into_iter().map(|x| x.to_string()).collect()
 }
 
 // If a column doesn't exist in the input, return it.
