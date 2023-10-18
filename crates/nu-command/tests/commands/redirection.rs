a5e26d6f21ea3a7a68cd846cd1a3893b18c71233
WindSoilder <WindSoilder@outlook.com>
2023-10-18 17:59:15 +0800

    add some tests

--- crates/nu-command/tests/commands/redirection.rs
+++ crates/nu-command/tests/commands/redirection.rs
@@ -12,6 +12,14 @@ fn redirect_err() {
         );
 
         assert!(output.out.contains("asdfasdfasdf.txt"));
+
+        // check append mode
+        let output = nu!(
+                cwd: dirs.test(),
+                "cat asdfasdfasdf.txt err>> a.txt; cat a.txt"
+        );
+        let v: Vec<_> = output.out.match_indices("asdfasdfasdf.txt").collect();
+        assert_eq!(v.len(), 2);
     })
 }
 
@@ -24,6 +32,12 @@ fn redirect_err() {
             "vol missingdrive err> a; (open a | size).bytes >= 16"
         );
 
+        assert!(output.out.contains("true"));
+
+        let output = nu!(
+            cwd: dirs.test(),
+            "vol missingdrive err>> a; (open a | size).bytes >= 32"
+        );
         assert!(output.out.contains("true"));
     })
 }
@@ -39,6 +53,10 @@ fn redirect_outerr() {
         let output = nu!(cwd: dirs.test(), "cat a");
 
         assert!(output.out.contains("asdfasdfasdf.txt"));
+
+        let output = nu!(cwd: dirs.test(), "cat asdfasdfasdf.txt o+e>> a; cat a");
+        let v: Vec<_> = output.out.match_indices("asdfasdfasdf.txt").collect();
+        assert_eq!(v.len(), 2);
     })
 }
 
@@ -53,6 +71,13 @@ fn redirect_outerr() {
         let output = nu!(cwd: dirs.test(), "(open a | size).bytes >= 16");
 
         assert!(output.out.contains("true"));
+
+        nu!(
+            cwd: dirs.test(),
+            "vol missingdrive out+err>> a"
+        );
+        let output = nu!(cwd: dirs.test(), "(open a | size).bytes >= 32");
+        assert!(output.out.contains("true"));
     })
 }
 
@@ -65,6 +90,12 @@ fn redirect_out() {
         );
 
         assert!(output.out.contains("hello"));
+
+        let output = nu!(
+            cwd: dirs.test(),
+            "echo 'hello' out>> a; open a"
+        );
+        assert!(output.out.contains("hellohello"));
     })
 }
 
@@ -124,6 +155,34 @@ fn separate_redirection() {
             let expected_err_file = dirs.test().join("err.txt");
             let actual = file_contents(expected_err_file);
             assert!(actual.contains(expect_body));
+
+            // check append mode
+            let expect_body = "messagemessage";
+            #[cfg(not(windows))]
+            {
+                sandbox.with_files(vec![FileWithContent("test.sh", script_body)]);
+                nu!(
+                    cwd: dirs.test(),
+                    "bash test.sh out>> out.txt err>> err.txt"
+                );
+            }
+            #[cfg(windows)]
+            {
+                sandbox.with_files(vec![FileWithContent("test.bat", script_body)]);
+                nu!(
+                    cwd: dirs.test(),
+                    "cmd /D /c test.bat out>> out.txt err>> err.txt"
+                );
+            }
+            // check for stdout redirection file.
+            let expected_out_file = dirs.test().join("out.txt");
+            let actual = file_contents(expected_out_file);
+            assert!(actual.contains(expect_body));
+
+            // check for stderr redirection file.
+            let expected_err_file = dirs.test().join("err.txt");
+            let actual = file_contents(expected_err_file);
+            assert!(actual.contains(expect_body));
         },
     )
 }
@@ -149,6 +208,21 @@ fn same_target_redirection_with_too_much_stderr_not_hang_nushell() {
             ),
         );
 
+        let expected_file = dirs.test().join("another_large_file.txt");
+        let actual = file_contents(expected_file);
+        assert_eq!(actual, format!("{large_file_body}\n"));
+
+        // not hangs in append mode either.
+        let cloned_body = large_file_body.clone();
+        large_file_body.push_str(&format!("\n{cloned_body}"));
+        nu!(
+            cwd: dirs.test(), pipeline(
+                "
+                $env.LARGE = (open --raw a_large_file.txt);
+                nu --testbin echo_env_stderr LARGE out+err>> another_large_file.txt
+                "
+            ),
+        );
         let expected_file = dirs.test().join("another_large_file.txt");
         let actual = file_contents(expected_file);
         assert_eq!(actual, format!("{large_file_body}\n"));
@@ -202,6 +276,16 @@ fn redirection_with_pipeline_works() {
             let expected_out_file = dirs.test().join("out.txt");
             let actual = file_contents(expected_out_file);
             assert!(actual.contains(expect_body));
+
+            // check append mode works
+            nu!(
+                cwd: dirs.test(),
+                "bash test.sh o>> out.txt | describe"
+            );
+            let expected_out_file = dirs.test().join("out.txt");
+            let actual = file_contents(expected_out_file);
+            let v: Vec<_> = actual.match_indices("message").collect();
+            assert_eq!(v.len(), 2);
         },
     )
 }
@@ -222,6 +306,21 @@ fn redirect_support_variable() {
         );
 
         assert!(output.out.contains("hello"));
+
+        // append mode support variable too.
+        let output = nu!(
+            cwd: dirs.test(),
+            "let x = 'tmp_file'; echo 'hello' out>> $x; open tmp_file"
+        );
+        let v: Vec<_> = output.out.match_indices("hello").collect();
+        assert_eq!(v.len(), 2);
+
+        let output = nu!(
+            cwd: dirs.test(),
+            "let x = 'tmp_file'; echo 'hello' out+err>> $x; open tmp_file"
+        );
+        let v: Vec<_> = output.out.match_indices("hello").collect();
+        assert_eq!(v.len(), 2);
     })
 }
 
