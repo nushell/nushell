7162d4d9aa7b961e5fad5cea6f71f11a6c3fab17
Himadri Bhattacharjee <lavafroth@protonmail.com>
2023-10-19 02:32:11 +0530

    Escape path that could be a flag (#10721)
    # Description
    Files that begin with dashes can be ambiguous when passed to commands
    like `ls`. For example if there exists a file `--help`, it might be
    considered a flag if not properly escaped. This PR escapes any file that
    begins with a dash.
    
    # User-Facing Changes
    
    Files beginning with dashes will be escaped.
    
    # Tests + Formatting
    
    Tests are added.

--- crates/nu-cli/tests/completions.rs
+++ crates/nu-cli/tests/completions.rs
@@ -527,6 +527,10 @@ fn file_completion_quoted() {
     let suggestions = completer.complete(target_dir, target_dir.len());
 
     let expected_paths: Vec<String> = vec![
+        "`--help`".to_string(),
+        "`-42`".to_string(),
+        "`-inf`".to_string(),
+        "`4.2`".to_string(),
         "`te st.txt`".to_string(),
         "`te#st.txt`".to_string(),
         "`te'st.txt`".to_string(),
