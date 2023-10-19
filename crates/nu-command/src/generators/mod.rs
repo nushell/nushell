5c6e8ccea067ed57374de6fbafd58ed5f79d011d
Antoine Stevan <44101798+amtoine@users.noreply.github.com>
2023-10-19 19:23:06 +0200

    add `unfold` back with a deprecation warning (#10771)
    related to
    - https://github.com/nushell/nushell/pull/10770
    
    # Description
    because some people look into `unfold` already (myself included lol) and
    there will be 4 weeks with that new command which has a decent section
    in the release note, i fear that
    https://github.com/nushell/nushell/pull/10770 is a bit too brutal,
    removing `unfold` without any warning...
    
    this PR brings `unfold` back to life.
    the `unfold` command will have a deprecation warning and will be removed
    in 0.88.
    
    # User-Facing Changes
    `unfold` is only deprecated, not removed.
    
    # Tests + Formatting
    
    # After Submitting

--- crates/nu-command/src/generators/mod.rs
+++ crates/nu-command/src/generators/mod.rs
@@ -3,9 +3,11 @@ mod generate;
 mod seq;
 mod seq_char;
 mod seq_date;
+mod unfold;
 
 pub use cal::Cal;
 pub use generate::Generate;
 pub use seq::Seq;
 pub use seq_char::SeqChar;
 pub use seq_date::SeqDate;
+pub use unfold::Unfold;
