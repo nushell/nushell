12f02e535c86c2284ef1cee5f2ee627a2b99bf21
amtoine <stevan.antoine@gmail.com>
2023-10-21 19:10:00 +0200

    rename `str size` to `str stats` in the tests
    the command i used
    ```shell
    sd "str size" "str stats" crates/nu-command/tests/commands/**/*.rs
    ```

--- crates/nu-command/tests/commands/random/chars.rs
+++ crates/nu-command/tests/commands/random/chars.rs
@@ -3,7 +3,7 @@ use nu_test_support::nu;
 #[test]
 fn generates_chars_of_specified_length() {
     let actual = nu!(r#"
-        random chars --length 15 | str size | get chars
+        random chars --length 15 | str stats | get chars
         "#);
 
     let result = actual.out;
