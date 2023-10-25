12339715fff3e678d90d9a3513d10514f6ee936a
sholderbach <sholderbach@users.noreply.github.com>
2023-10-25 21:40:02 +0200

    Refactor `move` to use encapsulated record API
    Manually running `.index_of` and then using `.get_index` feels a bit
    pedestrian but should work.

--- crates/nu-command/src/filters/move_.rs
+++ crates/nu-command/src/filters/move_.rs
@@ -197,7 +197,7 @@ fn move_record_columns(
     // Check if before/after column exist
     match &before_or_after.item {
         BeforeOrAfter::After(after) => {
-            if !record.cols.contains(after) {
+            if !record.contains(after) {
                 return Err(ShellError::GenericError(
                     "Cannot move columns".to_string(),
                     "column does not exist".to_string(),
@@ -208,7 +208,7 @@ fn move_record_columns(
             }
         }
         BeforeOrAfter::Before(before) => {
-            if !record.cols.contains(before) {
+            if !record.contains(before) {
                 return Err(ShellError::GenericError(
                     "Cannot move columns".to_string(),
                     "column does not exist".to_string(),
@@ -224,11 +224,7 @@ fn move_record_columns(
     for column in columns.iter() {
         let column_str = column.as_string()?;
 
-        if let Some(idx) = record
-            .cols
-            .iter()
-            .position(|inp_col| &column_str == inp_col)
-        {
+        if let Some(idx) = record.index_of(&column_str) {
             column_idx.push(idx);
         } else {
             return Err(ShellError::GenericError(
@@ -249,7 +245,7 @@ fn move_record_columns(
                 out.push(inp_col.clone(), inp_val.clone());
 
                 for idx in column_idx.iter() {
-                    if let (Some(col), Some(val)) = (record.cols.get(*idx), record.vals.get(*idx)) {
+                    if let Some((col, val)) = record.get_index(*idx) {
                         out.push(col.clone(), val.clone());
                     } else {
                         return Err(ShellError::NushellFailedSpanned {
@@ -262,7 +258,7 @@ fn move_record_columns(
             }
             BeforeOrAfter::Before(before) if before == inp_col => {
                 for idx in column_idx.iter() {
-                    if let (Some(col), Some(val)) = (record.cols.get(*idx), record.vals.get(*idx)) {
+                    if let Some((col, val)) = record.get_index(*idx) {
                         out.push(col.clone(), val.clone());
                     } else {
                         return Err(ShellError::NushellFailedSpanned {
