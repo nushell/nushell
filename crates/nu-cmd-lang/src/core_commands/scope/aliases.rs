780f2cfd089ee022cbdc00c58d48c8b4f99d15bb
Andrej Kolchin <KAAtheWise@protonmail.com>
2023-10-30 20:50:58 +0300

    Change category of scope commands to core

--- crates/nu-cmd-lang/src/core_commands/scope/aliases.rs
+++ crates/nu-cmd-lang/src/core_commands/scope/aliases.rs
@@ -17,7 +17,7 @@ impl Command for ScopeAliases {
         Signature::build("scope aliases")
             .input_output_types(vec![(Type::Nothing, Type::Any)])
             .allow_variants_without_examples(true)
-            .category(Category::Filters)
+            .category(Category::Core)
     }
 
     fn usage(&self) -> &str {
