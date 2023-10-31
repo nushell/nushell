15c22db8f49d42cba97dc5f4bc1de24ac8c4587b
Ian Manske <ian.manske@pm.me>
2023-10-31 18:47:00 +0000

    Make `FromValue` take owned `Value`s (#10900)
    # Description
    Changes `FromValue` to take owned `Value`s instead of borrowed `Value`s.
    This eliminates some unnecessary clones (e.g., in `call_ext.rs`).
    
    # User-Facing Changes
    Breaking API change for `nu_protocol`.

--- crates/nu-command/src/filters/insert.rs
+++ crates/nu-command/src/filters/insert.rs
@@ -122,7 +122,7 @@ fn insert(
 
     // Replace is a block, so set it up and run it instead of using it as the replacement
     if replacement.as_block().is_ok() {
-        let capture_block: Closure = FromValue::from_value(&replacement)?;
+        let capture_block = Closure::from_value(replacement)?;
         let block = engine_state.get_block(capture_block.block_id).clone();
 
         let mut stack = stack.captures_to_stack(&capture_block.captures);
