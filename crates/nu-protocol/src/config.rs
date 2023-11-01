49e8ee378af157b95e3c1aec81ad0e4a7137ff75
sholderbach <sholderbach@users.noreply.github.com>
2023-11-01 19:01:32 +0100

    Lift remaining reconstruction helpers

--- crates/nu-protocol/src/config.rs
+++ crates/nu-protocol/src/config.rs
@@ -522,25 +522,6 @@ impl Value {
                         }
                     }
                     "completions" => {
-                        fn reconstruct_external_completer(config: &Config, span: Span) -> Value {
-                            if let Some(block) = config.external_completer {
-                                Value::block(block, span)
-                            } else {
-                                Value::nothing(span)
-                            }
-                        }
-
-                        fn reconstruct_external(config: &Config, span: Span) -> Value {
-                            Value::record(
-                                record! {
-                                    "max_results" => Value::int(config.max_external_completion_results, span),
-                                    "completer" => reconstruct_external_completer(config, span),
-                                    "enable" => Value::bool(config.enable_external_completion, span),
-                                },
-                                span,
-                            )
-                        }
-
                         if let Value::Record { val, .. } = value {
                             val.retain_mut(|key2, value| {
                                 let span = value.span();
@@ -1296,6 +1277,25 @@ impl Value {
             },
         )
     }
+}
+
+fn reconstruct_external_completer(config: &Config, span: Span) -> Value {
+    if let Some(block) = config.external_completer {
+        Value::block(block, span)
+    } else {
+        Value::nothing(span)
+    }
+}
+
+fn reconstruct_external(config: &Config, span: Span) -> Value {
+    Value::record(
+        record! {
+            "max_results" => Value::int(config.max_external_completion_results, span),
+            "completer" => reconstruct_external_completer(config, span),
+            "enable" => Value::bool(config.enable_external_completion, span),
+        },
+        span,
+    )
 }
 
 fn try_parse_trim_strategy(
