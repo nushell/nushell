692788356121b7190037e1a53d4bea212279ef93
Marika Chlebowska <marika.c@protonmail.com>
2023-11-12 15:39:40 +0100

    also handle string containing special characters

--- crates/nu-parser/src/deparse.rs
+++ crates/nu-parser/src/deparse.rs
@@ -14,38 +14,22 @@ pub fn escape_quote_string(input: &str) -> String {
 }
 
 // Escape rules:
-// input argument contains ' '(like abc def), we will convert it to `abc def`.
-// input argument contains --version='xx yy', we will convert it to --version=`'xx yy'`
-// input argument contains " or \, we will try to escape input.
+// input argument is a flag with =, we will wrap the value of flag in quotes and escape \ and " (--version=1\3 -> --vesion="1\\3")
+// input argument is a flag without =, we will pass it as it is (--version -> --version)
+// input argument is not a flag, we will wrap it in quotes and escape \ and " (some other "argument" -> "some other \"argument\"")
 pub fn escape_for_script_arg(input: &str) -> String {
     // handle for flag, maybe we need to escape the value.
     if input.starts_with("--") {
         if let Some((arg_name, arg_val)) = input.split_once('=') {
             // only want to escape arg_val.
-            let arg_val = if arg_val.contains(' ') {
-                format!("`{arg_val}`")
-            } else if arg_val.contains('"') || arg_val.contains('\\') {
-                escape_quote_string(arg_val)
-            } else if arg_val.is_empty() {
-                // return an empty string
-                "''".to_string()
-            } else {
-                arg_val.into()
-            };
+            let arg_val = escape_quote_string(arg_val);
             return format!("{arg_name}={arg_val}");
+        } else {
+            return input.to_string();
         }
     }
 
-    if input.contains(' ') || (!input.starts_with('$') && input.contains('(')) {
-        format!("`{input}`")
-    } else if input.contains('"') || input.contains('\\') {
-        escape_quote_string(input)
-    } else if input.is_empty() {
-        // return an empty string
-        "''".to_string()
-    } else {
-        input.to_string()
-    }
+    escape_quote_string(input)
 }
 
 #[cfg(test)]
@@ -53,60 +37,55 @@ mod test {
     use super::escape_for_script_arg;
 
     #[test]
-    fn test_not_extra_quote() {
+    fn test_single_character() {
         // check for input arg like this:
         // nu b.nu 8
-        assert_eq!(escape_for_script_arg("8"), "8".to_string());
+        assert_eq!(escape_for_script_arg("8"), r#""8""#.to_string());
+    }
+
+    #[test]
+    fn test_empty_string() {
+        // check for empty string as an argument
+        assert_eq!(escape_for_script_arg(""), r#""""#.to_string());
     }
 
     #[test]
     fn test_arg_with_flag() {
         // check for input arg like this:
         // nu b.nu linux --version=v5.2
-        assert_eq!(escape_for_script_arg("linux"), "linux".to_string());
+        assert_eq!(escape_for_script_arg("linux"), "\"linux\"".to_string());
         assert_eq!(
             escape_for_script_arg("--version=v5.2"),
-            "--version=v5.2".to_string()
+            r#"--version="v5.2""#.to_string()
         );
 
         // check for input arg like this:
         // nu b.nu linux --version v5.2
         assert_eq!(escape_for_script_arg("--version"), "--version".to_string());
-        assert_eq!(escape_for_script_arg("v5.2"), "v5.2".to_string());
+        assert_eq!(escape_for_script_arg("v5.2"), r#""v5.2""#.to_string());
     }
 
     #[test]
     fn test_flag_arg_with_values_contains_space() {
         // check for input arg like this:
-        // nu b.nu test_ver --version='xx yy' --arch=ghi
+        // nu b.nu test_ver --version='xx yy'
         assert_eq!(
             escape_for_script_arg("--version='xx yy'"),
-            "--version=`'xx yy'`".to_string()
-        );
-        assert_eq!(
-            escape_for_script_arg("--arch=ghi"),
-            "--arch=ghi".to_string()
+            r#"--version="'xx yy'""#.to_string()
         );
     }
 
     #[test]
     fn test_escape() {
         // check for input arg like this:
-        // nu b.nu '"'
-        assert_eq!(escape_for_script_arg(r#"""#), r#""\"""#.to_string());
+        // nu b.nu '"\'
+        assert_eq!(escape_for_script_arg(r#""\"#), r#""\"\\""#.to_string());
     }
 
     #[test]
-    fn test_values_with_brackets() {
+    fn test_special_characters() {
         // check for input arg like this:
-        // nu b.nu test_ver some(thing) $something(else)
-        assert_eq!(
-            escape_for_script_arg("some(thing)"),
-            "`some(thing)`".to_string()
-        );
-        assert_eq!(
-            escape_for_script_arg("$something(else)"),
-            "$something(else)".to_string()
-        );
+        // nu b.nu "$(`'"
+        assert_eq!(escape_for_script_arg(r#"$(`'"#), r#""$(`'""#.to_string());
     }
 }
