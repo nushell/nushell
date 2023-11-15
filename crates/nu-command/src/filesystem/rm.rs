8e72a7b39810424f1778f5e24a708d49e45baf1f
Ian Manske <ian.manske@pm.me>
2023-11-15 11:29:16 -0500

    Fix `rm` path handling

--- crates/nu-command/src/filesystem/rm.rs
+++ crates/nu-command/src/filesystem/rm.rs
@@ -250,7 +250,7 @@ fn rm(
             Ok(files) => {
                 for file in files {
                     match file {
-                        Ok(ref f) => {
+                        Ok(f) => {
                             if !target_exists {
                                 target_exists = true;
                             }
@@ -263,7 +263,9 @@ fn rm(
                                 continue;
                             }
 
-                            all_targets.entry(f.clone()).or_insert_with(|| target.span);
+                            all_targets
+                                .entry(currentdir_path.join(f))
+                                .or_insert_with(|| target.span);
                         }
                         Err(e) => {
                             return Err(ShellError::GenericError(
