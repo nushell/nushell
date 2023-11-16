80bee408079df2c1e3e9c2e8a650abbccd3e359b
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-11-16 11:37:46 -0600

    optimize/clean up a few of the `table` changes (#11076)
    # Description
    
    @sholderbach pointed out some places that I could help improve the code
    in the table command changes. This PR tries to implement those.
    
    # User-Facing Changes
    <!-- List of all changes that impact the user experience here. This
    helps us keep track of breaking changes. -->
    
    # Tests + Formatting
    <!--
    Don't forget to add tests that cover your changes.
    
    Make sure you've run and fixed any issues with these commands:
    
    - `cargo fmt --all -- --check` to check standard code formatting (`cargo
    fmt --all` applies these changes)
    - `cargo clippy --workspace -- -D warnings -D clippy::unwrap_used` to
    check that you're using the standard code style
    - `cargo test --workspace` to check that all tests pass (on Windows make
    sure to [enable developer
    mode](https://learn.microsoft.com/en-us/windows/apps/get-started/developer-mode-features-and-debugging))
    - `cargo run -- -c "use std testing; testing run-tests --path
    crates/nu-std"` to run the tests for the standard library
    
    > **Note**
    > from `nushell` you can also use the `toolkit` as follows
    > ```bash
    > use toolkit.nu # or use an `env_change` hook to activate it
    automatically
    > toolkit check pr
    > ```
    -->
    
    # After Submitting
    <!-- If your PR had any user-facing changes, update [the
    documentation](https://github.com/nushell/nushell.github.io) after the
    PR is merged, if necessary. This will help us keep the docs up to date.
    -->

--- crates/nu-command/src/viewers/table.rs
+++ crates/nu-command/src/viewers/table.rs
@@ -321,21 +321,16 @@ fn get_theme_flag(
     state: &EngineState,
     stack: &mut Stack,
 ) -> Result<Option<TableMode>, ShellError> {
-    let theme_name = call.get_flag(state, stack, "theme")?;
-    theme_name
-        .clone()
-        .map(|theme: String| TableMode::from_str(&theme))
-        .transpose()
-        .map_err(|err| ShellError::CantConvert {
-            to_type: String::from("theme"),
-            from_type: String::from("string"),
-            span: call.span(),
-            help: Some(format!(
-                "{}, but found '{}'.",
-                String::from(err),
-                theme_name.unwrap_or("".to_string())
-            )),
+    call.get_flag(state, stack, "theme")?
+        .map(|theme: String| {
+            TableMode::from_str(&theme).map_err(|err| ShellError::CantConvert {
+                to_type: String::from("theme"),
+                from_type: String::from("string"),
+                span: call.span(),
+                help: Some(format!("{}, but found '{}'.", err, theme)),
+            })
         })
+        .transpose()
 }
 
 struct CmdInput<'a> {
