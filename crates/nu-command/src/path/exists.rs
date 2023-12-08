c61e011cd10de8ef27f397794c4c6304582a648b
A. Taha Baki <atahabaki@pm.me>
2023-12-08 15:55:41 +0300

    path exists -n: add test case (do not traverse symlinks...)

--- crates/nu-command/src/path/exists.rs
+++ crates/nu-command/src/path/exists.rs
@@ -170,4 +170,26 @@ mod tests {
 
         test_examples(SubCommand {})
     }
+
+    #[test]
+    fn test_check_symlink_exists() {
+        use nu_test_support::{nu, playground::Playground};
+
+        let symlink_target = "symlink_target";
+        let symlink = "symlink";
+        Playground::setup("rm_test_symlink", |dirs, sandbox| {
+            #[cfg(not(windows))]
+            std::os::unix::fs::symlink(dirs.test().join(symlink_target), dirs.test().join(symlink))
+                .unwrap();
+            #[cfg(windows)]
+            std::os::windows::fs::symlink_file(
+                dirs.test().join(symlink_target),
+                dirs.test().join(symlink),
+            )
+            .unwrap();
+
+            let shell_res = nu!(cwd: sandbox.cwd(), "'symlink' | path exists -n");
+            assert_eq!("true".to_string(), shell_res.out);
+        });
+    }
 }
