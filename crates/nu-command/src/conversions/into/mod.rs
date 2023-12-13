91947a1cb77dbbf79fd3de8f7b48eb8cc0ae8f27
Eric Hodel <drbrain@segment7.net>
2023-12-13 15:51:05 -0800

    Make into cell-path a usable command

--- crates/nu-command/src/conversions/into/mod.rs
+++ crates/nu-command/src/conversions/into/mod.rs
@@ -14,6 +14,7 @@ mod value;
 pub use self::bool::SubCommand as IntoBool;
 pub use self::filesize::SubCommand as IntoFilesize;
 pub use binary::SubCommand as IntoBinary;
+pub use cell_path::IntoCellPath;
 pub use command::Into;
 pub use datetime::SubCommand as IntoDatetime;
 pub use duration::SubCommand as IntoDuration;
