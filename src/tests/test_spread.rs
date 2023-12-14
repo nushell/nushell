9a8b2f781955640c3b08bf913de42ee30fe5eea4
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-12-13 20:53:50 -0500

    Remove unnecessary const tests for spreading lists and records

--- src/tests/test_spread.rs
+++ src/tests/test_spread.rs
@@ -25,30 +25,6 @@ fn spread_in_list() -> TestResult {
 }
 
 #[test]
-fn const_spread_in_list() -> TestResult {
-    run_test(r#"const x = [...[]]; $x | to nuon"#, "[]").unwrap();
-    run_test(
-        r#"const x = [1 2 ...[[3] {x: 1}] 5]; $x | to nuon"#,
-        "[1, 2, [3], {x: 1}, 5]",
-    )
-    .unwrap();
-    run_test(
-        r#"const x = [...([f o o]) 10]; $x | to nuon"#,
-        "[f, o, o, 10]",
-    )
-    .unwrap();
-    run_test(
-        r#"const l = [1, 2, [3]]; const x = [...$l $l]; $x | to nuon"#,
-        "[1, 2, [3], [1, 2, [3]]]",
-    )
-    .unwrap();
-    run_test(
-        r#"[ ...[ ...[ ...[ a ] b ] c ] d ] | to nuon"#,
-        "[a, b, c, d]",
-    )
-}
-
-#[test]
 fn not_spread() -> TestResult {
     run_test(r#"def ... [x] { $x }; ... ..."#, "...").unwrap();
     run_test(
@@ -96,29 +72,10 @@ fn spread_in_record() -> TestResult {
 }
 
 #[test]
-fn const_spread_in_record() -> TestResult {
-    run_test(r#"const x = {...{...{...{}}}}; $x | to nuon"#, "{}").unwrap();
-    run_test(
-        r#"const x = {foo: bar ...{a: {x: 1}} b: 3}; $x | to nuon"#,
-        "{foo: bar, a: {x: 1}, b: 3}",
-    )
-}
-
-#[test]
 fn duplicate_cols() -> TestResult {
     fail_test(r#"{a: 1, ...{a: 3}}"#, "column used twice").unwrap();
     fail_test(r#"{...{a: 4, x: 3}, x: 1}"#, "column used twice").unwrap();
     fail_test(r#"{...{a: 0, x: 2}, ...{x: 5}}"#, "column used twice")
-}
-
-#[test]
-fn const_duplicate_cols() -> TestResult {
-    fail_test(r#"const _ = {a: 1, ...{a: 3}}"#, "column used twice").unwrap();
-    fail_test(r#"const _ = {...{a: 4, x: 3}, x: 1}"#, "column used twice").unwrap();
-    fail_test(
-        r#"const _ = {...{a: 0, x: 2}, ...{x: 5}}"#,
-        "column used twice",
-    )
 }
 
 #[test]
