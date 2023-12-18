c8ad6ec5df5179acecdc48ca7bb0e34b9dc6446f
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-12-18 10:35:26 -0600

    add special emoji handling for debug --raw

--- crates/nu-protocol/src/value/mod.rs
+++ crates/nu-protocol/src/value/mod.rs
@@ -789,7 +789,34 @@ impl Value {
 
     /// Convert Value into a debug string
     pub fn debug_value(&self) -> String {
-        format!("{self:#?}")
+        // Let's do some special handling for emojis
+        const ZERO_WIDTH_JOINER: &str = "\u{200d}";
+        const VARIATION_SELECTOR_16: &str = "\u{fe0f}";
+        const SKIN_TONES: [&str; 5] = [
+            "\u{1f3fb}", // Light Skin Tone
+            "\u{1f3fc}", // Medium-Light Skin Tone
+            "\u{1f3fd}", // Medium Skin Tone
+            "\u{1f3fe}", // Medium-Dark Skin Tone
+            "\u{1f3ff}", // Dark Skin Tone
+        ];
+
+        match self {
+            Value::String { val, .. } => {
+                if val.contains(ZERO_WIDTH_JOINER)
+                    || val.contains(VARIATION_SELECTOR_16)
+                    || SKIN_TONES.iter().any(|skin_tone| val.contains(skin_tone))
+                {
+                    // This has to be an emoji, so let's display the code points that make it up.
+                    format!(
+                        "{:#?}",
+                        Value::test_string(val.escape_unicode().to_string())
+                    )
+                } else {
+                    format!("{self:#?}")
+                }
+            }
+            _ => format!("{self:#?}"),
+        }
     }
 
     /// Convert Value into a parsable string (quote strings)
