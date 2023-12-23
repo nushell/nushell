4f1489b2fc2e0e399b66e8ff957b2c3c4deb20e5
alex <alexwhite1964@icloud.com>
2023-12-23 15:22:45 -0600

    Issue #6844

---
+++ crates/nu-command/src/database/commands/query.rs
@@ -0,0 +1,44 @@
+use nu_engine::get_full_help;
+use nu_protocol::ast::Call;
+use nu_protocol::engine::{Command, EngineState, Stack};
+use nu_protocol::{IntoPipelineData, PipelineData, ShellError, Signature, Value};
+
+#[derive(Clone)]
+pub struct Query;
+
+impl Command for Query {
+    fn name(&self) -> &str {
+        "query"
+    }
+
+    fn signature(&self) -> nu_protocol::Signature {
+        Signature::build(self.name())
+    }
+
+    fn usage(&self) -> &str {
+        "Show all the query commands."
+    }
+
+    fn extra_usage(&self) -> &str {
+        "You must use one of the following subcommands. Using this command as-is will only produce this help message."
+    }
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        Ok(Value::string(
+            get_full_help(
+                &Query.signature(),
+                &Query.examples(),
+                engine_state,
+                stack,
+                self.is_parser_keyword(),
+            ),
+            call.head,
+        )
+        .into_pipeline_data())
+    }
+}
