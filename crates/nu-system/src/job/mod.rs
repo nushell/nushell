1b3e0a9b462ba564252aa5febe15bbdf989aaa1f
Ian Manske <ian.manske@pm.me>
2024-01-12 11:18:08 -0500

    Background jobs internals

--- crates/nu-system/src/job/mod.rs
+++ crates/nu-system/src/job/mod.rs
@@ -1,3 +1,161 @@
+use std::{
+    fmt::Display,
+    io,
+    process::Command,
+    sync::{Arc, Mutex, MutexGuard},
+};
+
+pub type JobId = usize;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum JobStatus {
+    Completed,
+    // Stopped,
+    Running,
+}
+
+impl Display for JobStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                JobStatus::Completed => "done",
+                // JobStatus::Stopped => "stopped",
+                JobStatus::Running => "running",
+            }
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum JobExitStatus {
+    Exited(i32),
+    Signaled { signal: i32, core_dumped: bool },
+    Unknown,
+}
+
+#[derive(Debug, Clone)]
+pub struct JobInfo {
+    pub id: JobId,
+    pub command: String,
+    pub pid: u32,
+    pub exit_status: Option<JobExitStatus>,
+}
+
+impl JobInfo {
+    pub fn status(&self) -> JobStatus {
+        if self.exit_status.is_some() {
+            JobStatus::Completed
+        } else {
+            JobStatus::Running
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Job {
+    id: JobId,
+    command: String,
+    pid: u32,
+    exit_status: Option<JobExitStatus>,
+}
+
+impl Job {
+    fn status(&self) -> JobStatus {
+        if self.exit_status.is_some() {
+            JobStatus::Completed
+        } else {
+            JobStatus::Running
+        }
+    }
+
+    fn info(&self) -> JobInfo {
+        JobInfo {
+            id: self.id,
+            command: self.command.clone(),
+            exit_status: self.exit_status,
+            pid: self.pid,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct JobState {
+    /// All completed and running jobs in ascending order based on JobId.
+    ///
+    /// Completed jobs are removed once `background_jobs` is called.
+    jobs: Vec<Job>,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Jobs(Arc<Mutex<JobState>>);
+
+impl Jobs {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    fn state(&self) -> MutexGuard<JobState> {
+        self.0.lock().expect("unpoisoned")
+    }
+
+    pub fn spawn_background(&self, mut command: Command, interactive: bool) -> io::Result<JobId> {
+        Self::platform_pre_spawn(&mut command, interactive);
+
+        let mut child = command.spawn()?;
+
+        let mut state = self.state();
+        let job = Job {
+            id: state.jobs.last().map(|job| job.id).unwrap_or(0) + 1,
+            command: command.get_program().to_string_lossy().to_string(),
+            pid: child.id(),
+            exit_status: None,
+        };
+        let id = job.id;
+
+        // Other commands/libraries can spawn processes outside of job control,
+        // so we cannot use waitpid(-1) without potentially messing with that.
+        // Instead, we spawn a thread to wait on each background job.
+        let thread = {
+            let jobs = self.clone();
+            std::thread::Builder::new().spawn(move || {
+                let status = child.wait();
+                if let Some(job) = jobs.state().jobs.iter_mut().find(|job| job.id == id) {
+                    debug_assert!(
+                        job.exit_status.is_none(),
+                        "job with id {id} already had its exit status set"
+                    );
+                    job.exit_status = Some(status.map_or(JobExitStatus::Unknown, Into::into));
+                } else {
+                    debug_assert!(false, "did not find job with id {id}")
+                }
+            })
+        };
+
+        if let Err(err) = thread {
+            // TODO: the thread failed to spawn, so the child process is not being waited on.
+            // On unix, this will leave the child as a zombie process until nushell exits.
+            Err(err)
+        } else {
+            // At this point, the job has succesfully launched, so we can add it.
+            state.jobs.push(job);
+            Ok(id)
+        }
+    }
+
+    /// Returns information about each job (runnning and completed).
+    /// Note that any completed jobs are removed from the job list.
+    pub fn info(&self) -> Vec<JobInfo> {
+        let mut state = self.state();
+        let jobs = state.jobs.iter().map(Job::info).collect();
+        state
+            .jobs
+            .retain(|job| job.status() != JobStatus::Completed);
+        jobs
+    }
+}
+
 #[cfg(unix)]
 mod unix;
 #[cfg(unix)]
---
+++ crates/nu-system/src/job/unix.rs
@@ -0,0 +1,73 @@
+use std::{
+    io::{self, IsTerminal},
+    os::unix::process::{CommandExt, ExitStatusExt},
+    process::{Command, ExitStatus},
+};
+
+use nix::{
+    sys::signal::{sigaction, SaFlags, SigAction, SigHandler, SigSet, Signal},
+    unistd::{self, Pid},
+};
+
+use crate::{JobExitStatus, Jobs};
+
+impl From<ExitStatus> for JobExitStatus {
+    fn from(status: ExitStatus) -> Self {
+        match (status.code(), status.signal()) {
+            (Some(code), None) => Self::Exited(code),
+            (None, Some(signal)) => Self::Signaled {
+                signal,
+                core_dumped: status.core_dumped(),
+            },
+            (None, None) => Self::Unknown,
+            (Some(code), Some(signal)) => {
+                // Should be unreachable, as `code()` will be `None` if `signal()` is `Some`
+                // according to the docs for `ExitStatus::code`.
+                debug_assert!(
+                    false,
+                    "ExitStatus cannot have both a code ({code}) and a signal ({signal})"
+                );
+                Self::Unknown
+            }
+        }
+    }
+}
+
+impl Jobs {
+    pub(crate) fn platform_pre_spawn(command: &mut Command, interactive: bool) {
+        if interactive && io::stdin().is_terminal() {
+            prepare_interactive(command);
+        }
+    }
+}
+
+fn prepare_interactive(command: &mut Command) {
+    unsafe {
+        // Safety:
+        // POSIX only allows async-signal-safe functions to be called.
+        // `sigaction`, `getpid`, `setpgid`, and `tcsetpgrp` are async-signal-safe according to:
+        // https://manpages.ubuntu.com/manpages/bionic/man7/signal-safety.7.html
+        command.pre_exec(move || {
+            // When this callback is run, std::process has already:
+            // - reset SIGPIPE to SIG_DFL
+
+            let pid = Pid::this();
+
+            // According to glibc's job control manual:
+            // https://www.gnu.org/software/libc/manual/html_node/Launching-Jobs.html
+            // This has to be done *both* in the parent and here in the child due to race conditions.
+            let _ = unistd::setpgid(pid, pid);
+
+            // Reset signal handlers for child, sync with `terminal.rs`
+            let default = SigAction::new(SigHandler::SigDfl, SaFlags::empty(), SigSet::empty());
+            // SIGINT has special handling
+            sigaction(Signal::SIGQUIT, &default).expect("signal default");
+            sigaction(Signal::SIGTSTP, &default).expect("signal default");
+            sigaction(Signal::SIGTTIN, &default).expect("signal default");
+            sigaction(Signal::SIGTTOU, &default).expect("signal default");
+            sigaction(Signal::SIGTERM, &default).expect("signal default");
+
+            Ok(())
+        });
+    }
+}
