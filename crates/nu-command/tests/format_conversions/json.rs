017ef139f227e09d9a762433ddbf005a8184c967
Ian Manske <ian.manske@pm.me>
2024-01-22 10:15:14 -0500

    Add tests

--- crates/nu-command/tests/format_conversions/json.rs
+++ crates/nu-command/tests/format_conversions/json.rs
@@ -44,6 +44,32 @@ fn from_json_text_to_table() {
 }
 
 #[test]
+fn from_json_text_to_table_strict() {
+    Playground::setup("filter_from_json_test_1_strict", |dirs, sandbox| {
+        sandbox.with_files(vec![FileWithContentToBeTrimmed(
+            "katz.txt",
+            r#"
+                {
+                    "katz": [
+                        {"name":   "Yehuda", "rusty_luck": 1},
+                        {"name": "JT", "rusty_luck": 1},
+                        {"name":   "Andres", "rusty_luck": 1},
+                        {"name":"GorbyPuff", "rusty_luck": 1}
+                    ]
+                }
+            "#,
+        )]);
+
+        let actual = nu!(
+            cwd: dirs.test(),
+            "open katz.txt | from json -s | get katz | get rusty_luck | length "
+        );
+
+        assert_eq!(actual.out, "4");
+    })
+}
+
+#[test]
 fn from_json_text_recognizing_objects_independently_to_table() {
     Playground::setup("filter_from_json_test_2", |dirs, sandbox| {
         sandbox.with_files(vec![FileWithContentToBeTrimmed(
@@ -61,6 +87,33 @@ fn from_json_text_recognizing_objects_independently_to_table() {
             r#"
                 open katz.txt
                 | from json -o
+                | where name == "GorbyPuff"
+                | get rusty_luck.0
+            "#
+        ));
+
+        assert_eq!(actual.out, "3");
+    })
+}
+
+#[test]
+fn from_json_text_recognizing_objects_independently_to_table_strict() {
+    Playground::setup("filter_from_json_test_2_strict", |dirs, sandbox| {
+        sandbox.with_files(vec![FileWithContentToBeTrimmed(
+            "katz.txt",
+            r#"
+                {"name":   "Yehuda", "rusty_luck": 1}
+                {"name": "JT", "rusty_luck": 1}
+                {"name":   "Andres", "rusty_luck": 1}
+                {"name":"GorbyPuff", "rusty_luck": 3}
+            "#,
+        )]);
+
+        let actual = nu!(
+            cwd: dirs.test(), pipeline(
+            r#"
+                open katz.txt
+                | from json -o -s
                 | where name == "GorbyPuff"
                 | get rusty_luck.0
             "#
@@ -100,6 +153,35 @@ fn table_to_json_text() {
 }
 
 #[test]
+fn table_to_json_text_strict() {
+    Playground::setup("filter_to_json_test_strict", |dirs, sandbox| {
+        sandbox.with_files(vec![FileWithContentToBeTrimmed(
+            "sample.txt",
+            r#"
+                JonAndrehudaTZ,3
+                GorbyPuff,100
+            "#,
+        )]);
+
+        let actual = nu!(
+            cwd: dirs.test(), pipeline(
+            r#"
+                open sample.txt
+                | lines
+                | split column "," name luck
+                | select name
+                | to json
+                | from json -s
+                | get 0
+                | get name
+            "#
+        ));
+
+        assert_eq!(actual.out, "JonAndrehudaTZ");
+    })
+}
+
+#[test]
 fn top_level_values_from_json() {
     for (value, type_name) in [("null", "nothing"), ("true", "bool"), ("false", "bool")] {
         let actual = nu!(r#""{}" | from json | to json"#, value);
@@ -107,6 +189,28 @@ fn top_level_values_from_json() {
         let actual = nu!(r#""{}" | from json | describe"#, value);
         assert_eq!(actual.out, type_name);
     }
+}
+
+#[test]
+fn top_level_values_from_json_strict() {
+    for (value, type_name) in [("null", "nothing"), ("true", "bool"), ("false", "bool")] {
+        let actual = nu!(r#""{}" | from json -s | to json"#, value);
+        assert_eq!(actual.out, value);
+        let actual = nu!(r#""{}" | from json -s | describe"#, value);
+        assert_eq!(actual.out, type_name);
+    }
+}
+
+#[test]
+fn strict_parsing_fails_on_comment() {
+    let actual = nu!(r#"'{ "a": 1, /* comment */ "b": 2 }' | from json -s"#);
+    assert!(actual.err.contains("error parsing JSON text"));
+}
+
+#[test]
+fn strict_parsing_fails_on_trailing_comma() {
+    let actual = nu!(r#"'{ "a": 1, "b": 2, }' | from json -s"#);
+    assert!(actual.err.contains("error parsing JSON text"));
 }
 
 #[test]
