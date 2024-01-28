nushell/nushell

commit d6469031611015a7ff3bdfd90ae969f6902672fe

Unify glob behavior on `open`, `rm`, `cp-old`, `mv`, `umv`, `cp` and `du` commands (#11621)
# Description
This pr is a follow up to
[#11569](https://github.com/nushell/nushell/pull/11569#issuecomment-1902279587)
> Revert the logic in https://github.com/nushell/nushell/pull/10694 and
apply the logic in this pr to mv, cp, rv will require a larger change, I
need to think how to achieve the bahavior

And sorry @bobhy for reverting some of your changes.

This pr is going to unify glob behavior on the given commands:
* open
* rm
* cp-old
* mv
* umv
* cp
* du

So they have the same behavior to `ls`, which is:
If given parameter is quoted by single quote(`'`) or double quote(`"`),
don't auto-expand the glob pattern. If not quoted, auto-expand the glob
pattern.

Fixes: #9558  Fixes: #10211 Fixes: #9310 Fixes: #10364 

# TODO
But there is one thing remains: if we give a variable to the command, it
will always auto-expand the glob pattern, e.g:
```nushell
let path = "a[123]b"
rm $path
```
I don't think it's expected. But I also think user might want to
auto-expand the glob pattern in variables.

So I'll introduce a new command called `glob escape`, then if user
doesn't want to auto-expand the glob pattern, he can just do this: `rm
($path | glob escape)`

# User-Facing Changes
<!-- List of all changes that impact the user experience here. This
helps us keep track of breaking changes. -->

# Tests + Formatting
Done

# After Submitting
<!-- If your PR had any user-facing changes, update [the
documentation](https://github.com/nushell/nushell.github.io) after the
PR is merged, if necessary. This will help us keep the docs up to date.
-->

## NOTE
This pr changes the semantic of `GlobPattern`, before this pr, it will
`expand path` after evaluated, this makes `nu_engine::glob_from` have no
chance to glob things right if a path contains glob pattern.

e.g: [#9310

crates/nu-command/src/filesystem/rm.rs
@@ -7,14 +7,15 @@ use std::path::PathBuf;
 
 use super::util::try_interaction;
 
-use nu_cmd_base::arg_glob_leading_dot;
 use nu_engine::env::current_dir;
 use nu_engine::CallExt;
+use nu_glob::MatchOptions;
+use nu_path::expand_path_with;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::{
-    Category, Example, IntoInterruptiblePipelineData, PipelineData, ShellError, Signature, Span,
-    Spanned, SyntaxShape, Type, Value,
+    Category, Example, IntoInterruptiblePipelineData, NuPath, PipelineData, ShellError, Signature,
+    Span, Spanned, SyntaxShape, Type, Value,
 };
 
 const TRASH_SUPPORTED: bool = cfg!(all(
@@ -134,7 +135,7 @@ fn rm(
 
     let ctrlc = engine_state.ctrlc.clone();
 
-    let mut targets: Vec<Spanned<String>> = call.rest(engine_state, stack, 0)?;
+    let mut targets: Vec<Spanned<NuPath>> = call.rest(engine_state, stack, 0)?;
 
     let mut unique_argument_check = None;
 
@@ -157,12 +158,19 @@ fn rm(
 
     for (idx, path) in targets.clone().into_iter().enumerate() {
         if let Some(ref home) = home {
-            if &path.item == home {
+            if expand_path_with(path.item.as_ref(), &currentdir_path)
+                .to_string_lossy()
+                .as_ref()
+                == home.as_str()
+            {
                 unique_argument_check = Some(path.span);
             }
         }
         let corrected_path = Spanned {
-            item: nu_utils::strip_ansi_string_unlikely(path.item),
+            item: match path.item {
+                NuPath::Quoted(s) => NuPath::Quoted(nu_utils::strip_ansi_string_unlikely(s)),
+                NuPath::UnQuoted(s) => NuPath::UnQuoted(nu_utils::strip_ansi_string_unlikely(s)),
+            },
             span: path.span,
         };
         let _ = std::mem::replace(&mut targets[idx], corrected_path);
@@ -233,7 +241,8 @@ fn rm(
     let mut all_targets: HashMap<PathBuf, Span> = HashMap::new();
 
     for target in targets {
-        if currentdir_path.to_string_lossy() == target.item
+        let path = expand_path_with(target.item.as_ref(), &currentdir_path);
+        if currentdir_path.to_string_lossy() == path.to_string_lossy()
             || currentdir_path.starts_with(format!("{}{}", target.item, std::path::MAIN_SEPARATOR))
         {
             return Err(ShellError::GenericError {
@@ -245,10 +254,18 @@ fn rm(
             });
         }
 
-        let path = currentdir_path.join(&target.item);
-        match arg_glob_leading_dot(&target, &currentdir_path) {
+        // let path = currentdir_path.join(target.item.as_ref());
+        match nu_engine::glob_from(
+            &target,
+            &currentdir_path,
+            call.head,
+            Some(MatchOptions {
+                require_literal_leading_dot: true,
+                ..Default::default()
+            }),
+        ) {
             Ok(files) => {
-                for file in files {
+                for file in files.1 {
                     match file {
                         Ok(f) => {
                             if !target_exists {

crates/nu-command/tests/commands/rm.rs
@@ -42,7 +42,7 @@ fn removes_files_with_wildcard() {
 
         nu!(
             cwd: dirs.test(),
-            r#"rm "src/*/*/*.rs""#
+            r#"rm src/*/*/*.rs"#
         );
 
         assert!(!files_exist_at(
@@ -457,5 +457,26 @@ fn rm_prints_filenames_on_error() {
                 actual.err
             );
         }
+    });
+}
+
+#[test]
+fn rm_files_inside_glob_metachars_dir() {
+    Playground::setup("rm_files_inside_glob_metachars_dir", |dirs, sandbox| {
+        let sub_dir = "test[]";
+        sandbox
+            .within(sub_dir)
+            .with_files(vec![EmptyFile("test_file.txt")]);
+
+        let actual = nu!(
+            cwd: dirs.test().join(sub_dir),
+            "rm test_file.txt",
+        );
+
+        assert!(actual.err.is_empty());
+        assert!(!files_exist_at(
+            vec!["test_file.txt"],
+            dirs.test().join(sub_dir)
+        ));
     });
 }

crates/nu-cli/src/syntax_highlight.rs
@@ -336,7 +336,6 @@ fn find_matching_block_end_in_expr(
             Expr::Filepath(_, _) => None,
             Expr::Directory(_, _) => None,
             Expr::GlobPattern(_, _) => None,
-            Expr::LsGlobPattern(_, _) => None,
             Expr::String(_) => None,
             Expr::CellPath(_) => None,
             Expr::ImportPattern(_) => None,

crates/nu-cmd-base/src/arg_glob.rs
@@ -1,205 +1,0 @@
-// utilities for expanding globs in command arguments
-
-use nu_glob::{glob_with_parent, MatchOptions, Paths};
-use nu_protocol::{ShellError, Spanned};
-use std::fs;
-use std::path::{Path, PathBuf};
-
-// standard glob options to use for filesystem command arguments
-
-const GLOB_PARAMS: MatchOptions = MatchOptions {
-    case_sensitive: true,
-    require_literal_separator: false,
-    require_literal_leading_dot: false,
-    recursive_match_hidden_dir: true,
-};
-
-// handle an argument that could be a literal path or a glob.
-// if literal path, return just that (whether user can access it or not).
-// if glob, expand into matching paths, using GLOB_PARAMS options.
-pub fn arg_glob(
-    pattern: &Spanned<String>, // alleged path or glob
-    cwd: &Path,                // current working directory
-) -> Result<Paths, ShellError> {
-    arg_glob_opt(pattern, cwd, GLOB_PARAMS)
-}
-
-// variant of [arg_glob] that requires literal dot prefix in pattern to match dot-prefixed path.
-pub fn arg_glob_leading_dot(pattern: &Spanned<String>, cwd: &Path) -> Result<Paths, ShellError> {
-    arg_glob_opt(
-        pattern,
-        cwd,
-        MatchOptions {
-            require_literal_leading_dot: true,
-            ..GLOB_PARAMS
-        },
-    )
-}
-
-fn arg_glob_opt(
-    pattern: &Spanned<String>,
-    cwd: &Path,
-    options: MatchOptions,
-) -> Result<Paths, ShellError> {
-    // remove ansi coloring (?)
-    let pattern = {
-        Spanned {
-            item: nu_utils::strip_ansi_string_unlikely(pattern.item.clone()),
-            span: pattern.span,
-        }
-    };
-
-    // if there's a file with same path as the pattern, just return that.
-    let pp = cwd.join(&pattern.item);
-    let md = fs::metadata(pp);
-    #[allow(clippy::single_match)]
-    match md {
-        Ok(_metadata) => {
-            return Ok(Paths::single(&PathBuf::from(pattern.item), cwd));
-        }
-        // file not found, but also "invalid chars in file" (e.g * on Windows).  Fall through and glob
-        Err(_) => {}
-    }
-
-    // user wasn't referring to a specific thing in filesystem, try to glob it.
-    match glob_with_parent(&pattern.item, options, cwd) {
-        Ok(p) => Ok(p),
-        Err(pat_err) => Err(ShellError::InvalidGlobPattern {
-            msg: pat_err.msg.into(),
-            span: pattern.span,
-        }),
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use nu_glob::GlobResult;
-    use nu_protocol::{Span, Spanned};
-    use nu_test_support::fs::Stub::EmptyFile;
-    use nu_test_support::playground::Playground;
-    use rstest::rstest;
-
-    fn spanned_string(str: &str) -> Spanned<String> {
-        Spanned {
-            item: str.to_string(),
-            span: Span::test_data(),
-        }
-    }
-
-    #[test]
-    fn does_something() {
-        let act = arg_glob(&spanned_string("*"), &PathBuf::from("."));
-        assert!(act.is_ok());
-        for f in act.expect("checked ok") {
-            match f {
-                Ok(p) => {
-                    assert!(!p.to_str().unwrap().is_empty());
-                }
-                Err(e) => panic!("unexpected error {:?}", e),
-            };
-        }
-    }
-
-    #[test]
-    fn glob_format_error() {
-        let act = arg_glob(&spanned_string(r#"ab]c[def"#), &PathBuf::from("."));
-        assert!(act.is_err());
-    }
-
-    #[rstest]
-    #[case("*", 4, "no dirs")]
-    #[case("**/*", 7, "incl dirs")]
-    fn glob_subdirs(#[case] pat: &str, #[case] exp_count: usize, #[case] case: &str) {
-        Playground::setup("glob_subdirs", |dirs, sandbox| {
-            sandbox.with_files(vec![
-                EmptyFile("yehuda.txt"),
-                EmptyFile("jttxt"),
-                EmptyFile("andres.txt"),
-            ]);
-            sandbox.mkdir(".children");
-            sandbox.within(".children").with_files(vec![
-                EmptyFile("timothy.txt"),
-                EmptyFile("tiffany.txt"),
-                EmptyFile("trish.txt"),
-            ]);
-
-            let p: Vec<GlobResult> = arg_glob(&spanned_string(pat), &dirs.test)
-                .expect("no error")
-                .collect();
-            assert_eq!(
-                exp_count,
-                p.iter().filter(|i| i.is_ok()).count(),
-                " case: {case} ",
-            );
-
-            // expected behavior -- that directories are included in results (if name matches pattern)
-            let t = p
-                .iter()
-                .any(|i| i.as_ref().unwrap().to_string_lossy().contains(".children"));
-            assert!(t, "check for dir, case {case}");
-        })
-    }
-
-    #[rstest]
-    #[case("yehuda.txt", true, 1, "matches literal path")]
-    #[case("*", false, 3, "matches glob")]
-    #[case(r#"bad[glob.foo"#, true, 1, "matches literal, would be bad glob pat")]
-    fn exact_vs_glob(
-        #[case] pat: &str,
-        #[case] exp_matches_input: bool,
-        #[case] exp_count: usize,
-        #[case] case: &str,
-    ) {
-        Playground::setup("exact_vs_glob", |dirs, sandbox| {
-            sandbox.with_files(vec![
-                EmptyFile("yehuda.txt"),
-                EmptyFile("jttxt"),
-                EmptyFile("bad[glob.foo"),
-            ]);
-
-            let res = arg_glob(&spanned_string(pat), &dirs.test)
-                .expect("no error")
-                .collect::<Vec<GlobResult>>();
-
-            eprintln!("res: {:?}", res);
-            if exp_matches_input {
-                assert_eq!(
-                    exp_count,
-                    res.len(),
-                    " case {case}: matches input, but count not 1? "
-                );
-                assert_eq!(
-                    &res[0].as_ref().unwrap().to_string_lossy(),
-                    pat, // todo: is it OK for glob to return relative paths (not to current cwd, but to arg cwd of arg_glob)?
-                );
-            } else {
-                assert_eq!(exp_count, res.len(), " case: {}: matched glob", case);
-            }
-        })
-    }
-
-    #[rstest]
-    #[case(r#"realbad[glob.foo"#, true, 1, "error, bad glob")]
-    fn exact_vs_bad_glob(
-        // if path doesn't exist but pattern is not valid glob, should get error.
-        #[case] pat: &str,
-        #[case] _exp_matches_input: bool,
-        #[case] _exp_count: usize,
-        #[case] _tag: &str,
-    ) {
-        Playground::setup("exact_vs_bad_glob", |dirs, sandbox| {
-            sandbox.with_files(vec![
-                EmptyFile("yehuda.txt"),
-                EmptyFile("jttxt"),
-                EmptyFile("bad[glob.foo"),
-            ]);
-
-            let res = arg_glob(&spanned_string(pat), &dirs.test);
-            assert!(res
-                .expect_err("expected error")
-                .to_string()
-                .contains("Invalid glob pattern"));
-        })
-    }
-}

crates/nu-cmd-base/src/lib.rs
@@ -1,7 +1,4 @@
-mod arg_glob;
 pub mod formats;
 pub mod hook;
 pub mod input_handler;
 pub mod util;
-pub use arg_glob::arg_glob;
-pub use arg_glob::arg_glob_leading_dot;

crates/nu-command/src/filesystem/ls.rs
@@ -41,7 +41,7 @@ impl Command for Ls {
             .input_output_types(vec![(Type::Nothing, Type::Table(vec![]))])
             // LsGlobPattern is similar to string, it won't auto-expand
             // and we use it to track if the user input is quoted.
-            .optional("pattern", SyntaxShape::LsGlobPattern, "The glob pattern to use.")
+            .optional("pattern", SyntaxShape::GlobPattern, "The glob pattern to use.")
             .switch("all", "Show hidden files", Some('a'))
             .switch(
                 "long",
@@ -165,7 +165,8 @@ impl Command for Ls {
         };
 
         let hidden_dir_specified = is_hidden_dir(&path);
-        // when it's quoted, we need to escape our glob pattern
+        // when it's quoted, we need to escape our glob pattern(but without the last extra
+        // start which may be added under given directory)
         // so we can do ls for a file or directory like `a[123]b`
         let path = if quoted {
             let p = path.display().to_string();
@@ -185,7 +186,8 @@ impl Command for Ls {
         };
 
         let glob_path = Spanned {
-            item: path.clone(),
+            // It needs to be un-quoted, the relative logic is handled previously
+            item: NuPath::UnQuoted(path.clone()),
             span: p_tag,
         };
 

crates/nu-command/src/filesystem/mv.rs
@@ -1,14 +1,13 @@
 use std::path::{Path, PathBuf};
 
 use super::util::try_interaction;
-use nu_cmd_base::arg_glob;
 use nu_engine::env::current_dir;
 use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::{
-    Category, Example, IntoInterruptiblePipelineData, PipelineData, ShellError, Signature, Span,
-    Spanned, SyntaxShape, Type, Value,
+    Category, Example, IntoInterruptiblePipelineData, NuPath, PipelineData, ShellError, Signature,
+    Span, Spanned, SyntaxShape, Type, Value,
 };
 
 #[derive(Clone)]
@@ -63,7 +62,8 @@ impl Command for Mv {
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
         // TODO: handle invalid directory or insufficient permissions when moving
-        let spanned_source: Spanned<String> = call.req(engine_state, stack, 0)?;
+        let mut spanned_source: Spanned<NuPath> = call.req(engine_state, stack, 0)?;
+        spanned_source.item = spanned_source.item.strip_ansi_string_unlikely();
         let spanned_destination: Spanned<String> = call.req(engine_state, stack, 1)?;
         let verbose = call.has_flag(engine_state, stack, "verbose")?;
         let interactive = call.has_flag(engine_state, stack, "interactive")?;
@@ -73,11 +73,11 @@ impl Command for Mv {
         let ctrlc = engine_state.ctrlc.clone();
 
         let path = current_dir(engine_state, stack)?;
-        let source = path.join(spanned_source.item.as_str());
         let destination = path.join(spanned_destination.item.as_str());
 
-        let mut sources =
-            arg_glob(&spanned_source, &path).map_or_else(|_| Vec::new(), Iterator::collect);
+        let mut sources = nu_engine::glob_from(&spanned_source, &path, call.head, None)
+            .map(|p| p.1)
+            .map_or_else(|_| Vec::new(), Iterator::collect);
 
         if sources.is_empty() {
             return Err(ShellError::FileNotFound {
@@ -94,7 +94,7 @@ impl Command for Mv {
         //
         // Second, the destination doesn't exist, so we can only rename a single source. Otherwise
         // it's an error.
-
+        let source = path.join(spanned_source.item.as_ref());
         if destination.exists() && !force && !destination.is_dir() && !source.is_dir() {
             return Err(ShellError::GenericError {
                 error: "Destination file already exists".into(),

crates/nu-command/src/filesystem/open.rs
@@ -4,8 +4,8 @@ use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::util::BufferedReader;
 use nu_protocol::{
-    Category, DataSource, Example, IntoInterruptiblePipelineData, PipelineData, PipelineMetadata,
-    RawStream, ShellError, Signature, Spanned, SyntaxShape, Type, Value,
+    Category, DataSource, Example, IntoInterruptiblePipelineData, NuPath, PipelineData,
+    PipelineMetadata, RawStream, ShellError, Signature, Spanned, SyntaxShape, Type, Value,
 };
 use std::io::BufReader;
 
@@ -42,10 +42,10 @@ impl Command for Open {
     fn signature(&self) -> nu_protocol::Signature {
         Signature::build("open")
             .input_output_types(vec![(Type::Nothing, Type::Any), (Type::String, Type::Any)])
-            .optional("filename", SyntaxShape::Filepath, "The filename to use.")
+            .optional("filename", SyntaxShape::GlobPattern, "The filename to use.")
             .rest(
                 "filenames",
-                SyntaxShape::Filepath,
+                SyntaxShape::GlobPattern,
                 "Optional additional files to open.",
             )
             .switch("raw", "open file as raw binary", Some('r'))
@@ -63,8 +63,8 @@ impl Command for Open {
         let call_span = call.head;
         let ctrlc = engine_state.ctrlc.clone();
         let cwd = current_dir(engine_state, stack)?;
-        let req_path = call.opt::<Spanned<String>>(engine_state, stack, 0)?;
-        let mut path_params = call.rest::<Spanned<String>>(engine_state, stack, 1)?;
+        let req_path = call.opt::<Spanned<NuPath>>(engine_state, stack, 0)?;
+        let mut path_params = call.rest::<Spanned<NuPath>>(engine_state, stack, 1)?;
 
         // FIXME: JT: what is this doing here?
 
@@ -87,19 +87,20 @@ impl Command for Open {
                 }
             };
 
-            path_params.insert(0, filename);
+            path_params.insert(
+                0,
+                Spanned {
+                    item: NuPath::UnQuoted(filename.item),
+                    span: filename.span,
+                },
+            );
         }
 
         let mut output = vec![];
 
-        for path in path_params.into_iter() {
+        for mut path in path_params.into_iter() {
             //FIXME: `open` should not have to do this
-            let path = {
-                Spanned {
-                    item: nu_utils::strip_ansi_string_unlikely(path.item),
-                    span: path.span,
-                }
-            };
+            path.item = path.item.strip_ansi_string_unlikely();
 
             let arg_span = path.span;
             // let path_no_whitespace = &path.item.trim_end_matches(|x| matches!(x, '\x09'..='\x0d'));

commit 25b62c2ac3a0e473f559a4f6ed01f0444f11b1da

fix force rm: should suppress error if directory is not found (#11656)
# Description
Fix a breaking change which is introduced by #11621

`rm -f /tmp/aaa` shouldn't return error if `/tmp/aaa/` doesn't exist.

# User-Facing Changes
NaN

# Tests + Formatting
Done

crates/nu-command/src/filesystem/rm.rs
@@ -254,7 +254,6 @@ fn rm(
             });
         }
 
-        // let path = currentdir_path.join(target.item.as_ref());
         match nu_engine::glob_from(
             &target,
             &currentdir_path,
@@ -302,13 +301,11 @@ fn rm(
                 }
             }
             Err(e) => {
-                return Err(ShellError::GenericError {
-                    error: e.to_string(),
-                    msg: e.to_string(),
-                    span: Some(target.span),
-                    help: None,
-                    inner: vec![],
-                })
+                // glob_from may canonicalize path and return `DirectoryNotFound`
+                // nushell should suppress the error if `--force` is used.
+                if !(force && matches!(e, ShellError::DirectoryNotFound { .. })) {
+                    return Err(e);
+                }
             }
         };
     }

crates/nu-command/tests/commands/rm.rs
@@ -480,3 +480,18 @@ fn rm_files_inside_glob_metachars_dir() {
         ));
     });
 }
+
+#[test]
+fn force_rm_suppress_error() {
+    Playground::setup("force_rm_suppress_error", |dirs, sandbox| {
+        sandbox.with_files(vec![EmptyFile("test_file.txt")]);
+
+        // the second rm should suppress error.
+        let actual = nu!(
+            cwd: dirs.test(),
+            "rm test_file.txt; rm -f test_file.txt",
+        );
+
+        assert!(actual.err.is_empty());
+    });
+}