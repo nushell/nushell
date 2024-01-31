c7e17d0695601c2eab9957a562c099e2988c74d6
David Matos <davidmatos06@gmail.com>
2024-01-31 23:36:51 +0100

    wip: working on it

--- crates/nu-command/src/system/uname.rs
+++ crates/nu-command/src/system/uname.rs
@@ -1,4 +1,7 @@
 use nu_engine::CallExt;
+use nu_protocol::record;
+use nu_protocol::Record;
+use nu_protocol::Value;
 use nu_protocol::{
     ast::Call,
     engine::{Command, EngineState, Stack},
@@ -56,6 +59,18 @@ impl Command for UName {
         call: &Call,
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
+        // Linux netname 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 GNU/Linux
+
+        // Value::record(
+        //     record! {
+        //         "name" => Value::string(get_os_name(), span),
+        //         "arch" => Value::string(get_os_arch(), span),
+        //         "family" => Value::string(get_os_family(), span),
+        //         "kernel_version" => Value::string(ver, span),
+        //     },
+        //     span,
+        // )
+        let span = call.head;
         let all = call.has_flag(engine_state, stack, "all")?;
         let kernel_name = call.has_flag(engine_state, stack, "kernel-name")?;
         let nodename = call.has_flag(engine_state, stack, "nodename")?;
@@ -76,16 +91,49 @@ impl Command for UName {
             hardware_platform,
             os,
         };
-        if let Err(error) = uu_uname::uu_uname(&opts) {
-            return Err(ShellError::GenericError {
-                error: format!("{}", error),
-                msg: format!("{}", error),
-                span: None,
-                help: None,
-                inner: Vec::new(),
-            });
-        }
-        Ok(PipelineData::empty())
+        let output = uu_uname::uu_uname(&opts).map_err(|e| ShellError::GenericError {
+            error: format!("{}", e),
+            msg: format!("{}", e),
+            span: None,
+            help: None,
+            inner: Vec::new(),
+        })?;
+        let output = output.trim_end();
+        // Output for now always follows the string as followed
+        // -s -n -r -v -m -o, omitting -p and -i always (uutils)
+        // Linux netname 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 GNU/Linux
+        let output_vec = output.split(' ').collect::<Vec<&str>>();
+
+        // TODO
+        // Check that the length equals how many times i want to divide
+        // That way I can ensure the values are correct.
+        // Can anyone check the format of kernel-version?
+        let mut value = Value::record(
+            record! {
+                "kernel-name" => Value::string(output_vec[0], span),
+                "nodename" => Value::string(output_vec[1], span),
+                "kernel-release" => Value::string(output_vec[2], span),
+                "kernel-version" => Value::string(output_vec[3..11].join(" "), span),
+                "machine" => Value::string(output_vec[11], span),
+                "operating-system" => Value::string(output_vec.last().expect("fix").clone(), span),
+            },
+            span,
+        );
+        // println!("{:?}", record);
+        // let z = record.int;
+
+        // Ok(PipelineData::Value(
+        //     Value::Record {
+        //         val: record,
+        //         internal_span: call.head,
+        //     },
+        //     None,
+        // ))
+        //
+        Ok(PipelineData::Value(value, None))
+        // Ok(Value::record(record, call.head))
+
+        // Ok(PipelineData::empty())
     }
 
     fn examples(&self) -> Vec<Example> {
