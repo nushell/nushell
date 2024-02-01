8c8c5a7c34731af07b283261ed6fffd7dbb28c70
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-02-01 21:05:58 +0200

    Update third example to match old behavior (also because it causes a crash in value/record.rs)

--- crates/nu_plugin_query/src/nu/mod.rs
+++ crates/nu_plugin_query/src/nu/mod.rs
@@ -83,7 +83,7 @@ pub fn web_examples() -> Vec<PluginExample> {
         result: None
     },
     PluginExample {
-        example: "http get https://www.nushell.sh | query web --query 'h2, h2 + p' | group 2 | each {rotate --ccw tagline description} | flatten".into(),
+        example: "http get https://www.nushell.sh | query web --query 'h2, h2 + p' | each {str join} | group 2 | each {rotate --ccw tagline description} | flatten".into(),
         description: "Pass multiple css selectors to extract several elements within single query, group the query results together and rotate them to create a table".into(),
         result: None,
     },
