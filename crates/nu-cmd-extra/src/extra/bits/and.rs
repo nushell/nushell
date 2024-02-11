a8faf411cdc53ccc3acc944aa84f066aa7ba2229
moonlander <astraldraconis@tuta.io>
2024-02-11 14:40:20 -0500

    and: refactor + format

--- crates/nu-cmd-extra/src/extra/bits/and.rs
+++ crates/nu-cmd-extra/src/extra/bits/and.rs
@@ -1,13 +1,25 @@
-use std::iter;
+use nu_cmd_base::input_handler::{operate, CmdArgument};
 use nu_engine::CallExt;
-use nu_protocol::ast::Call;
+use nu_protocol::ast::{Call, CellPath};
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::{
     Category, Example, PipelineData, ShellError, Signature, Span, SyntaxShape, Type, Value,
 };
+use std::iter;
 
 #[derive(Clone)]
 pub struct BitsAnd;
+
+struct Arguments {
+    target: Value,
+    little_endian: bool,
+}
+
+impl CmdArgument for Arguments {
+    fn take_cell_paths(&mut self) -> Option<Vec<CellPath>> {
+        None
+    }
+}
 
 impl Command for BitsAnd {
     fn name(&self) -> &str {
@@ -30,11 +42,9 @@ impl Command for BitsAnd {
             ])
             .required(
                 "target",
-                SyntaxShape::OneOf(vec![
-                    SyntaxShape::Binary,
-                    SyntaxShape::Int
-                ]),
-                "right-hand side of the operation")
+                SyntaxShape::OneOf(vec![SyntaxShape::Binary, SyntaxShape::Int]),
+                "right-hand side of the operation",
+            )
             .named(
                 "endian",
                 SyntaxShape::String,
@@ -62,7 +72,7 @@ impl Command for BitsAnd {
         let head = call.head;
         let target: Value = call.req(engine_state, stack, 0)?;
         let endian = call.get_flag::<Value>(engine_state, stack, "endian")?;
-        
+
         let little_endian = match endian {
             Some(val) => {
                 let span = val.span();
@@ -85,29 +95,16 @@ impl Command for BitsAnd {
             None => cfg!(target_endian = "little"),
         };
 
-        // This doesn't match explicit nulls
         if matches!(input, PipelineData::Empty) {
             return Err(ShellError::PipelineEmpty { dst_span: head });
         }
 
-        match target {
-            Value::Int { val: rhs, ..} => input.map(
-                move |value| and_int(value, rhs, head),
-                engine_state.ctrlc.clone(),
-            ),
-            Value::Binary { val: rhs, ..} => input.map(
-                move |value| and_bytes(value, rhs.clone(), little_endian, head),
-                engine_state.ctrlc.clone(),
-            ),
-            other => Err(
-                ShellError::OnlySupportsThisInputType {
-                    exp_input_type: "int or binary".into(),
-                    wrong_type: other.get_type().to_string(),
-                    dst_span: head,
-                    src_span: other.span(),
-                },
-            )
-        }
+        let args = Arguments {
+            target,
+            little_endian,
+        };
+
+        operate(action, args, input, head, engine_state.ctrlc.clone())
     }
 
     fn examples(&self) -> Vec<Example> {
@@ -120,78 +117,48 @@ impl Command for BitsAnd {
             Example {
                 description: "Apply logical and to two binary values",
                 example: "0x[ab cd] | bits and 0x[99 99]",
-                result: Some(Value::test_binary([137, 137]))
+                result: Some(Value::test_binary([137, 137])),
             },
             Example {
                 description: "Apply logical and to a list of numbers",
                 example: "[4 3 2] | bits and 2",
-                result: Some(Value::test_list(
-                    vec![Value::test_int(0), Value::test_int(2), Value::test_int(2)],
-                )),
+                result: Some(Value::test_list(vec![
+                    Value::test_int(0),
+                    Value::test_int(2),
+                    Value::test_int(2),
+                ])),
             },
             Example {
                 description: "Apply logical and to a list of binary data",
                 example: "[0x[7f ff] 0x[ff f0]] | bits and 0x[99 99]",
-                result: Some(Value::test_list(
-                    vec![Value::test_binary([25, 153]), Value::test_binary([153, 144])],
-                )),
+                result: Some(Value::test_list(vec![
+                    Value::test_binary([25, 153]),
+                    Value::test_binary([153, 144]),
+                ])),
             },
         ]
     }
 }
 
-fn and_int(
-    value: Value,
-    target: i64,
-    head: Span
-) -> Value {
-    let span = value.span();
-    match value {
-        Value::Int { val, .. } => Value::int(val & target, span),
-        Value::Error { .. } => value,
-        Value::Binary { .. } => Value::error(
-            ShellError::PipelineMismatch {
-                exp_input_type: "input, and argument, to be both int or both binary"
-                    .to_string(),
-                dst_span: head,
-                src_span: span,
-            },
-            head
-        ),
-        other => Value::error(
-            ShellError::OnlySupportsThisInputType {
-                exp_input_type: "int or binary".into(),
-                wrong_type: other.get_type().to_string(),
-                dst_span: head,
-                src_span: other.span(),
-            },
-            head,
-        ),
-    }
-}
-    
-fn and_bytes(
-    value: Value,
-    target: Vec<u8>,
-    little_endian: bool,
-    head: Span
-) -> Value {
-    let span = value.span();
-    match value {
-        Value::Binary { val: lhs, .. } => {
-            let rhs = target;
+fn action(input: &Value, args: &Arguments, span: Span) -> Value {
+    let Arguments {
+        target,
+        little_endian,
+    } = args;
+    match (input, target) {
+        (Value::Int { val: lhs, .. }, Value::Int { val: rhs, .. }) => Value::int(lhs & rhs, span),
+        (Value::Binary { val: lhs, .. }, Value::Binary { val: rhs, .. }) => {
             let max_len = lhs.len().max(rhs.len());
             let min_len = lhs.len().min(rhs.len());
 
-            let bytes = lhs.into_iter().zip(rhs);
+            let bytes = lhs.iter().copied().zip(rhs.iter().copied());
 
-            let pad = iter::repeat((0, 0))
-                .take(max_len - min_len);
+            let pad = iter::repeat((0, 0)).take(max_len - min_len);
 
             let mut a;
             let mut b;
 
-            let padded: &mut dyn Iterator<Item = (u8, u8)> = if little_endian {
+            let padded: &mut dyn Iterator<Item = (u8, u8)> = if *little_endian {
                 a = pad.chain(bytes);
                 &mut a
             } else {
@@ -200,27 +167,28 @@ fn and_bytes(
             };
 
             let val: Vec<u8> = padded.map(|(l, r)| l & r).collect();
-    
+
             Value::binary(val, span)
-        },
-        Value::Error { .. } => value,
-        Value::Int { .. } => Value::error(
-            ShellError::PipelineMismatch {
-                exp_input_type: "input, and argument, to be both int or both binary"
-                    .to_string(),
-                dst_span: head,
-                src_span: span,
-            },
-            head
-        ),
-        other => Value::error(
+        }
+        (Value::Binary { .. }, Value::Int { .. }) | (Value::Int { .. }, Value::Binary { .. }) => {
+            Value::error(
+                ShellError::PipelineMismatch {
+                    exp_input_type: "input, and argument, to be both int or both binary"
+                        .to_string(),
+                    dst_span: target.span(),
+                    src_span: span,
+                },
+                span,
+            )
+        }
+        (other, Value::Int { .. } | Value::Binary { .. }) | (_, other) => Value::error(
             ShellError::OnlySupportsThisInputType {
                 exp_input_type: "int or binary".into(),
                 wrong_type: other.get_type().to_string(),
-                dst_span: head,
-                src_span: other.span(),
+                dst_span: other.span(),
+                src_span: span,
             },
-            head,
+            span,
         ),
     }
 }
