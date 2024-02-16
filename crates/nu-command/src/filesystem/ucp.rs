46afcaa17ccfcb04faab03714b13be54c2c5cccd
Yuri Victorovich <yuri@FreeBSD.org>
2024-02-16 11:15:27 -0800

    FreeBSD compatibility patches
    nushell is verified to work on FreeBSD 14 with these patches.
    
    What isn't supported on FreeBSD:
    * the crate 'procfs' doesn't support FreeBSD yet, all functionality
      depending on procfs is disabled
    * several RLIMIT_* values aren't supported on FreeBSD - functions
      related to these are disabled

--- crates/nu-command/src/filesystem/ucp.rs
+++ crates/nu-command/src/filesystem/ucp.rs
@@ -141,9 +141,9 @@ impl Command for UCp {
         } else {
             uu_cp::OverwriteMode::Clobber(uu_cp::ClobberMode::Standard)
         };
-        #[cfg(any(target_os = "linux", target_os = "android", target_os = "macos"))]
+        #[cfg(any(target_os = "linux", target_os = "freebsd" , target_os = "android", target_os = "macos"))]
         let reflink_mode = uu_cp::ReflinkMode::Auto;
-        #[cfg(not(any(target_os = "linux", target_os = "android", target_os = "macos")))]
+        #[cfg(not(any(target_os = "linux", target_os = "freebsd" , target_os = "android", target_os = "macos")))]
         let reflink_mode = uu_cp::ReflinkMode::Never;
         let mut paths: Vec<Spanned<NuPath>> = call.rest(engine_state, stack, 0)?;
         if paths.is_empty() {
@@ -283,6 +283,7 @@ fn make_attributes(preserve: Option<Value>) -> Result<uu_cp::Attributes, ShellEr
         let mut attributes = uu_cp::Attributes {
             #[cfg(any(
                 target_os = "linux",
+                target_os = "freebsd",
                 target_os = "android",
                 target_os = "macos",
                 target_os = "netbsd"
@@ -303,6 +304,7 @@ fn make_attributes(preserve: Option<Value>) -> Result<uu_cp::Attributes, ShellEr
             mode: ATTR_SET,
             #[cfg(any(
                 target_os = "linux",
+                target_os = "freebsd",
                 target_os = "android",
                 target_os = "macos",
                 target_os = "netbsd"
@@ -344,6 +346,7 @@ fn parse_and_set_attribute(
                 "mode" => &mut attribute.mode,
                 #[cfg(any(
                     target_os = "linux",
+                    target_os = "freebsd",
                     target_os = "android",
                     target_os = "macos",
                     target_os = "netbsd"
