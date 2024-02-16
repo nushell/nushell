21540540c0ac7991b9fdc36433d695105d306998
Ian Manske <ian.manske@pm.me>
2024-02-16 15:49:53 -0500

    Cleanup tests

--- crates/nu-parser/tests/test_parser.rs
+++ crates/nu-parser/tests/test_parser.rs
@@ -718,9 +718,8 @@ fn test_nothing_comparison_eq() {
 #[case(b"let a = 1 out> /dev/null", "RedirectionInLetMut")]
 #[case(b"mut a = 1 err> /dev/null", "RedirectionInLetMut")]
 #[case(b"mut a = 1 out> /dev/null", "RedirectionInLetMut")]
-// This two cases cause AssignInPipeline instead of RedirectionInLetMut
-#[case(b"let a = 1 out+err> /dev/null", "AssignInPipeline")]
-#[case(b"mut a = 1 out+err> /dev/null", "AssignInPipeline")]
+#[case(b"let a = 1 out+err> /dev/null", "RedirectionInLetMut")]
+#[case(b"mut a = 1 out+err> /dev/null", "RedirectionInLetMut")]
 fn test_redirection_with_letmut(#[case] phase: &[u8], #[case] expected: &str) {
     let engine_state = EngineState::new();
     let mut working_set = StateWorkingSet::new(&engine_state);
