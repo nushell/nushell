b2bcc309ff74c2647efc4c3ca15e62274fe2bc6e
sholderbach <sholderbach@users.noreply.github.com>
2024-02-21 13:27:36 +0100

    Move error-related files in `nu-protocol` subdir

--- crates/nu-protocol/src/lib.rs
+++ crates/nu-protocol/src/lib.rs
@@ -1,22 +1,19 @@
 mod alias;
 pub mod ast;
-pub mod cli_error;
 pub mod config;
 pub mod debugger;
 mod did_you_mean;
 pub mod engine;
+mod errors;
 pub mod eval_base;
 pub mod eval_const;
 mod example;
 mod id;
 mod lev_distance;
 mod module;
-mod parse_error;
-mod parse_warning;
 mod pipeline_data;
 #[cfg(feature = "plugin")]
 mod plugin;
-mod shell_error;
 mod signature;
 pub mod span;
 mod syntax_shape;
@@ -26,20 +23,17 @@ mod value;
 
 pub use alias::*;
 pub use ast::Unit;
-pub use cli_error::*;
 pub use config::*;
 pub use did_you_mean::did_you_mean;
 pub use engine::{ENV_VARIABLE_ID, IN_VARIABLE_ID, NU_VARIABLE_ID};
+pub use errors::*;
 pub use example::*;
 pub use id::*;
 pub use lev_distance::levenshtein_distance;
 pub use module::*;
-pub use parse_error::{DidYouMean, ParseError};
-pub use parse_warning::ParseWarning;
 pub use pipeline_data::*;
 #[cfg(feature = "plugin")]
 pub use plugin::*;
-pub use shell_error::*;
 pub use signature::*;
 pub use span::*;
 pub use syntax_shape::*;
