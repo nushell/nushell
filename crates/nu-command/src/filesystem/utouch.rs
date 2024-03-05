7c27b376513e584d36d022304babddae534c4572
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-03-05 02:00:20 -0500

    Give all arguments to uu_touch at once

--- crates/nu-command/src/filesystem/utouch.rs
+++ crates/nu-command/src/filesystem/utouch.rs
@@ -1,14 +1,16 @@
-use std::path::{Path, PathBuf};
+use std::io::ErrorKind;
+use std::path::PathBuf;
 
-use chrono::{DateTime, FixedOffset, Local};
+use chrono::{DateTime, FixedOffset};
 
 use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::{
-    Category, Example, PipelineData, ShellError, Signature, Spanned, SyntaxShape, Type,
+    Category, Example, PipelineData, ShellError, Signature, Span, Spanned, SyntaxShape, Type,
 };
-use uu_touch::{datetime_to_filetime, stat, InputFile, Options};
+use uu_touch::error::{TouchError, TouchFileError};
+use uu_touch::{ChangeTimes, InputFile, Options, Source};
 
 #[derive(Clone)]
 pub struct UTouch;
@@ -23,7 +25,7 @@ impl Command for UTouch {
     }
 
     fn signature(&self) -> Signature {
-        Signature::build("touch")
+        Signature::build("utouch")
             .input_output_types(vec![ (Type::Nothing, Type::Nothing) ])
             .required(
                 "filename",
@@ -84,68 +86,101 @@ impl Command for UTouch {
         let target: Spanned<String> = call.req(engine_state, stack, 0)?;
         let rest: Vec<Spanned<String>> = call.rest(engine_state, stack, 1)?;
 
-        let reference: Option<Spanned<PathBuf>> =
-            call.get_flag(engine_state, stack, "reference")?;
+        let (reference_file, reference_span) = if let Some(reference) =
+            call.get_flag::<Spanned<PathBuf>>(engine_state, stack, "reference")?
+        {
+            (Some(reference.item), Some(reference.span))
+        } else {
+            (None, None)
+        };
         let timestamp: Option<Spanned<DateTime<FixedOffset>>> =
             call.get_flag(engine_state, stack, "timestamp")?;
 
-        let (atime, mtime) = if let Some(timestamp) = timestamp {
-            if let Some(reference) = reference {
+        let source = if let Some(timestamp) = timestamp {
+            if let Some(reference_span) = reference_span {
                 return Err(ShellError::IncompatibleParameters {
                     left_message: "timestamp given".to_string(),
                     left_span: timestamp.span,
                     right_message: "reference given".to_string(),
-                    right_span: reference.span,
+                    right_span: reference_span,
                 });
             }
-            let filetime = datetime_to_filetime(&timestamp.item);
-            (filetime, filetime)
-        } else if let Some(reference) = reference {
-            let reference_path = Path::new(&reference.item);
-            if !reference_path.exists() {
-                return Err(ShellError::TypeMismatch {
-                    err_message: format!("path provided is invalid: {}", reference_path.display()),
-                    span: reference.span,
-                });
-            }
-            stat(reference_path, !no_deref).map_err(|e| ShellError::GenericError {
-                error: "couldn't get metadata".to_string(),
-                msg: format!("{}", e),
-                span: Some(reference.span),
-                help: None,
-                inner: Vec::new(),
-            })?
+            Source::Timestamp(timestamp.item.into())
+        } else if let Some(reference_file) = reference_file {
+            Source::Reference(reference_file)
         } else {
-            let now = datetime_to_filetime(&Local::now());
-            (now, now)
+            Source::Now
         };
 
-        for file in vec![target].into_iter().chain(rest) {
-            if let Err(err) = uu_touch::touch(
-                &InputFile::Path(PathBuf::from(file.item)),
-                &Options {
-                    no_create,
-                    no_deref,
-                    atime: if !change_mtime || change_atime {
-                        Some(atime)
+        let change_times = if change_atime && !change_mtime {
+            ChangeTimes::AtimeOnly
+        } else if change_mtime && !change_atime {
+            ChangeTimes::MtimeOnly
+        } else {
+            ChangeTimes::Both
+        };
+
+        let mut files = vec![InputFile::Path(PathBuf::from(target.item))];
+        let mut file_spans = vec![target.span];
+        for file in rest {
+            files.push(InputFile::Path(PathBuf::from(file.item)));
+            file_spans.push(file.span);
+        }
+
+        if let Err(err) = uu_touch::touch(
+            &files,
+            &Options {
+                no_create,
+                no_deref,
+                source,
+                date: None,
+                change_times,
+                strict: true,
+            },
+        ) {
+            let nu_err = match err {
+                TouchError::ReferenceFileInaccessible(reference_path, io_err) => {
+                    let span = reference_span.expect("utouch was given a reference file");
+                    if io_err.kind() == ErrorKind::NotFound {
+                        // todo merge main into this to say which file not found
+                        ShellError::FileNotFound { span }
                     } else {
-                        None
-                    },
-                    mtime: if !change_atime || change_mtime {
-                        Some(mtime)
-                    } else {
-                        None
-                    },
-                },
-            ) {
-                return Err(ShellError::GenericError {
-                    error: "utouch failed".to_string(),
+                        io_to_nu_err(
+                            io_err,
+                            format!("Failed to read metadata of {}", reference_path.display()),
+                            span,
+                        )
+                    }
+                }
+                TouchError::TouchFileError { path, index, error } => {
+                    let span = file_spans[index];
+                    match error {
+                        TouchFileError::CannotCreate(_) => ShellError::CreateNotPossible {
+                            msg: format!("Cannot create {}", path.display()),
+                            span,
+                        },
+                        TouchFileError::CannotReadTimes(io_err) => io_to_nu_err(
+                            io_err,
+                            format!("Cannot read times for {}", path.display()),
+                            span,
+                        ),
+                        TouchFileError::CannotSetTimes(io_err) => io_to_nu_err(
+                            io_err,
+                            format!("Cannot set times for {}", path.display()),
+                            span,
+                        ),
+                        TouchFileError::TargetFileNotFound => ShellError::FileNotFound { span },
+                    }
+                }
+                _ => ShellError::GenericError {
+                    error: err.to_string(),
                     msg: err.to_string(),
-                    span: Some(file.span),
+                    span: Some(call.head),
                     help: None,
                     inner: Vec::new(),
-                });
-            }
+                },
+            };
+            return Err(nu_err);
         }
 
         Ok(PipelineData::empty())
@@ -179,10 +214,24 @@ impl Command for UTouch {
                 result: None,
             },
             Example {
-                description: r#"Changes the last accessed time of "fixture.json" to a date"#,
-                example: r#"utouch -a -d "August 24, 2019; 12:30:30" fixture.json"#,
+                description: r#"Changes the last accessed time of "fixture.json" to a datetime"#,
+                example: r#"utouch -a -t "August 24, 2019; 12:30:30" fixture.json"#,
                 result: None,
             },
         ]
+    }
+}
+
+fn io_to_nu_err(err: std::io::Error, msg: String, span: Span) -> ShellError {
+    if err.kind() == ErrorKind::PermissionDenied {
+        ShellError::PermissionDeniedError { msg, span }
+    } else {
+        ShellError::GenericError {
+            error: err.to_string(),
+            msg,
+            span: Some(span),
+            help: None,
+            inner: Vec::new(),
+        }
     }
 }
