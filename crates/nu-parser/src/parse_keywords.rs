nushell/nushell

commit bc19be25b1ed4b5dad45ccd8410be4d5ab84c116

Keep plugins persistently running in the background (#12064)
# Description
This PR uses the new plugin protocol to intelligently keep plugin
processes running in the background for further plugin calls.

Running plugins can be seen by running the new `plugin list` command,
and stopped by running the new `plugin stop` command.

This is an enhancement for the performance of plugins, as starting new
plugin processes has overhead, especially for plugins in languages that
take a significant amount of time on startup. It also enables plugins
that have persistent state between commands, making the migration of
features like dataframes and `stor` to plugins possible.

Plugins are automatically stopped by the new plugin garbage collector,
configurable with `$env.config.plugin_gc`:

```nushell
  $env.config.plugin_gc = {
      # Configuration for plugin garbage collection
      default: {
          enabled: true # true to enable stopping of inactive plugins
          stop_after: 10sec # how long to wait after a plugin is inactive to stop it
      }
      plugins: {
          # alternate configuration for specific plugins, by name, for example:
          #
          # gstat: {
          #     enabled: false
          # }
      }
  }
```

If garbage collection is enabled, plugins will be stopped after
`stop_after` passes after they were last active. Plugins are counted as
inactive if they have no running plugin calls. Reading the stream from
the response of a plugin call is still considered to be activity, but if
a plugin holds on to a stream but the call ends without an active
streaming response, it is not counted as active even if it is reading
it. Plugins can explicitly disable the GC as appropriate with
`engine.set_gc_disabled(true)`.

The `version` command now lists plugin names rather than plugin
commands. The list of plugin commands is accessible via `plugin list`.

crates/nu-parser/src/parse_keywords.rs
@@ -13,8 +13,8 @@ use nu_protocol::{
     },
     engine::{StateWorkingSet, DEFAULT_OVERLAY_NAME},
     eval_const::eval_constant,
-    span, Alias, BlockId, DeclId, Exportable, Module, ModuleId, ParseError, PositionalArg,
-    ResolvedImportPattern, Span, Spanned, SyntaxShape, Type, Value, VarId,
+    span, Alias, BlockId, DeclId, Exportable, IntoSpanned, Module, ModuleId, ParseError,
+    PositionalArg, ResolvedImportPattern, Span, Spanned, SyntaxShape, Type, Value, VarId,
 };
 use std::collections::{HashMap, HashSet};
 use std::path::{Path, PathBuf};
@@ -3543,8 +3543,10 @@ pub fn parse_where(working_set: &mut StateWorkingSet, spans: &[Span]) -> Pipelin
 
 #[cfg(feature = "plugin")]
 pub fn parse_register(working_set: &mut StateWorkingSet, spans: &[Span]) -> Pipeline {
-    use nu_plugin::{get_signature, PluginDeclaration};
-    use nu_protocol::{engine::Stack, PluginSignature};
+    use std::sync::Arc;
+
+    use nu_plugin::{get_signature, PersistentPlugin, PluginDeclaration};
+    use nu_protocol::{engine::Stack, PluginIdentity, PluginSignature, RegisteredPlugin};
 
     let cwd = working_set.get_cwd();
 
@@ -3671,35 +3673,61 @@ pub fn parse_register(working_set: &mut StateWorkingSet, spans: &[Span]) -> Pipe
 
     // We need the current environment variables for `python` based plugins
     // Or we'll likely have a problem when a plugin is implemented in a virtual Python environment.
-    let stack = Stack::new();
-    let current_envs =
-        nu_engine::env::env_to_strings(working_set.permanent_state, &stack).unwrap_or_default();
+    let get_envs = || {
+        let stack = Stack::new();
+        nu_engine::env::env_to_strings(working_set.permanent_state, &stack)
+    };
 
     let error = arguments.and_then(|(path, path_span)| {
         let path = path.path_buf();
-        // restrict plugin file name starts with `nu_plugin_`
-        let valid_plugin_name = path
-            .file_name()
-            .map(|s| s.to_string_lossy().starts_with("nu_plugin_"));
 
-        let Some(true) = valid_plugin_name else {
-            return Err(ParseError::LabeledError(
-                "Register plugin failed".into(),
-                "plugin name must start with nu_plugin_".into(),
-                path_span,
-            ));
-        };
+        // Create the plugin identity. This validates that the plugin name starts with `nu_plugin_`
+        let identity =
+            PluginIdentity::new(path, shell).map_err(|err| err.into_spanned(path_span))?;
+
+        // Find garbage collection config
+        let gc_config = working_set
+            .get_config()
+            .plugin_gc
+            .get(identity.name())
+            .clone();
+
+        // Add it to the working set
+        let plugin = working_set.find_or_create_plugin(&identity, || {
+            Arc::new(PersistentPlugin::new(identity.clone(), gc_config))
+        });
+
+        // Downcast the plugin to `PersistentPlugin` - we generally expect this to succeed. The
+        // trait object only exists so that nu-protocol can contain plugins without knowing anything
+        // about their implementation, but we only use `PersistentPlugin` in practice.
+        let plugin: Arc<PersistentPlugin> = plugin.as_any().downcast().map_err(|_| {
+            ParseError::InternalError(
+                "encountered unexpected RegisteredPlugin type".into(),
+                spans[0],
+            )
+        })?;
 
         let signatures = signature.map_or_else(
             || {
-                let signatures =
-                    get_signature(&path, shell.as_deref(), &current_envs).map_err(|err| {
-                        ParseError::LabeledError(
-                            "Error getting signatures".into(),
-                            err.to_string(),
-                            spans[0],
-                        )
-                    });
+                // It's important that the plugin is restarted if we're going to get signatures
+                //
+                // The user would expect that `register` would always run the binary to get new
+                // signatures, in case it was replaced with an updated binary
+                plugin.stop().map_err(|err| {
+                    ParseError::LabeledError(
+                        "Failed to restart plugin to get new signatures".into(),
+                        err.to_string(),
+                        spans[0],
+                    )
+                })?;
+
+                let signatures = get_signature(plugin.clone(), get_envs).map_err(|err| {
+                    ParseError::LabeledError(
+                        "Error getting signatures".into(),
+                        err.to_string(),
+                        spans[0],
+                    )
+                });
 
                 if signatures.is_ok() {
                     // mark plugins file as dirty only when the user is registering plugins
@@ -3715,7 +3743,7 @@ pub fn parse_register(working_set: &mut StateWorkingSet, spans: &[Span]) -> Pipe
         for signature in signatures {
             // create plugin command declaration (need struct impl Command)
             // store declaration in working set
-            let plugin_decl = PluginDeclaration::new(path.clone(), signature, shell.clone());
+            let plugin_decl = PluginDeclaration::new(&plugin, signature);
 
             working_set.add_decl(Box::new(plugin_decl));
         }

crates/nu-cmd-lang/src/core_commands/mod.rs
@@ -71,8 +71,13 @@ pub use try_::Try;
 pub use use_::Use;
 pub use version::Version;
 pub use while_::While;
-//#[cfg(feature = "plugin")]
+
+mod plugin;
+mod plugin_list;
+mod plugin_stop;
 mod register;
 
-//#[cfg(feature = "plugin")]
+pub use plugin::PluginCommand;
+pub use plugin_list::PluginList;
+pub use plugin_stop::PluginStop;
 pub use register::Register;

crates/nu-cmd-lang/src/core_commands/plugin.rs
@@ -1,0 +1,64 @@
+use nu_engine::get_full_help;
+use nu_protocol::{
+    ast::Call,
+    engine::{Command, EngineState, Stack},
+    Category, Example, IntoPipelineData, PipelineData, ShellError, Signature, Type, Value,
+};
+
+#[derive(Clone)]
+pub struct PluginCommand;
+
+impl Command for PluginCommand {
+    fn name(&self) -> &str {
+        "plugin"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build("plugin")
+            .input_output_types(vec![(Type::Nothing, Type::Nothing)])
+            .category(Category::Core)
+    }
+
+    fn usage(&self) -> &str {
+        "Commands for managing plugins."
+    }
+
+    fn extra_usage(&self) -> &str {
+        "To load a plugin, see `register`."
+    }
+
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        Ok(Value::string(
+            get_full_help(
+                &PluginCommand.signature(),
+                &PluginCommand.examples(),
+                engine_state,
+                stack,
+                self.is_parser_keyword(),
+            ),
+            call.head,
+        )
+        .into_pipeline_data())
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![
+            Example {
+                example: "plugin list",
+                description: "List installed plugins",
+                result: None,
+            },
+            Example {
+                example: "plugin stop inc",
+                description: "Stop the plugin named `inc`.",
+                result: None,
+            },
+        ]
+    }
+}

crates/nu-cmd-lang/src/core_commands/plugin_list.rs
@@ -1,0 +1,101 @@
+use itertools::Itertools;
+use nu_protocol::{
+    ast::Call,
+    engine::{Command, EngineState, Stack},
+    record, Category, Example, IntoInterruptiblePipelineData, PipelineData, ShellError, Signature,
+    Type, Value,
+};
+
+#[derive(Clone)]
+pub struct PluginList;
+
+impl Command for PluginList {
+    fn name(&self) -> &str {
+        "plugin list"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build("plugin list")
+            .input_output_type(
+                Type::Nothing,
+                Type::Table(vec![
+                    ("name".into(), Type::String),
+                    ("is_running".into(), Type::Bool),
+                    ("pid".into(), Type::Int),
+                    ("filename".into(), Type::String),
+                    ("shell".into(), Type::String),
+                    ("commands".into(), Type::List(Type::String.into())),
+                ]),
+            )
+            .category(Category::Core)
+    }
+
+    fn usage(&self) -> &str {
+        "List installed plugins."
+    }
+
+    fn examples(&self) -> Vec<nu_protocol::Example> {
+        vec![
+            Example {
+                example: "plugin list",
+                description: "List installed plugins.",
+                result: Some(Value::test_list(vec![Value::test_record(record! {
+                    "name" => Value::test_string("inc"),
+                    "is_running" => Value::test_bool(true),
+                    "pid" => Value::test_int(106480),
+                    "filename" => if cfg!(windows) {
+                        Value::test_string(r"C:\nu\plugins\nu_plugin_inc.exe")
+                    } else {
+                        Value::test_string("/opt/nu/plugins/nu_plugin_inc")
+                    },
+                    "shell" => Value::test_nothing(),
+                    "commands" => Value::test_list(vec![Value::test_string("inc")]),
+                })])),
+            },
+            Example {
+                example: "ps | where pid in (plugin list).pid",
+                description: "Get process information for running plugins.",
+                result: None,
+            },
+        ]
+    }
+
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        _stack: &mut Stack,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        let span = call.span();
+        // Group plugin decls by plugin identity
+        let decls = engine_state.plugin_decls().into_group_map_by(|decl| {
+            decl.plugin_identity()
+                .expect("plugin decl should have identity")
+        });
+        // Build plugins list
+        let list = engine_state.plugins().iter().map(|plugin| {
+            // Find commands that belong to the plugin
+            let commands = decls.get(plugin.identity())
+                .into_iter()
+                .flat_map(|decls| {
+                    decls.iter().map(|decl| Value::string(decl.name(), span))
+                })
+                .collect();
+
+            Value::record(record! {
+                "name" => Value::string(plugin.identity().name(), span),
+                "is_running" => Value::bool(plugin.is_running(), span),
+                "pid" => plugin.pid()
+                    .map(|p| Value::int(p as i64, span))
+                    .unwrap_or(Value::nothing(span)),
+                "filename" => Value::string(plugin.identity().filename().to_string_lossy(), span),
+                "shell" => plugin.identity().shell()
+                    .map(|s| Value::string(s.to_string_lossy(), span))
+                    .unwrap_or(Value::nothing(span)),
+                "commands" => Value::list(commands, span),
+            }, span)
+        }).collect::<Vec<Value>>();
+        Ok(list.into_pipeline_data(engine_state.ctrlc.clone()))
+    }
+}

crates/nu-cmd-lang/src/core_commands/plugin_stop.rs
@@ -1,0 +1,75 @@
+use nu_engine::CallExt;
+use nu_protocol::{
+    ast::Call,
+    engine::{Command, EngineState, Stack},
+    Category, Example, PipelineData, ShellError, Signature, Spanned, SyntaxShape, Type,
+};
+
+#[derive(Clone)]
+pub struct PluginStop;
+
+impl Command for PluginStop {
+    fn name(&self) -> &str {
+        "plugin stop"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build("plugin stop")
+            .input_output_type(Type::Nothing, Type::Nothing)
+            .required(
+                "name",
+                SyntaxShape::String,
+                "The name of the plugin to stop.",
+            )
+            .category(Category::Core)
+    }
+
+    fn usage(&self) -> &str {
+        "Stop an installed plugin if it was running."
+    }
+
+    fn examples(&self) -> Vec<nu_protocol::Example> {
+        vec![
+            Example {
+                example: "plugin stop inc",
+                description: "Stop the plugin named `inc`.",
+                result: None,
+            },
+            Example {
+                example: "plugin list | each { |p| plugin stop $p.name }",
+                description: "Stop all plugins.",
+                result: None,
+            },
+        ]
+    }
+
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        let name: Spanned<String> = call.req(engine_state, stack, 0)?;
+
+        let mut found = false;
+        for plugin in engine_state.plugins() {
+            if plugin.identity().name() == name.item {
+                plugin.stop()?;
+                found = true;
+            }
+        }
+
+        if found {
+            Ok(PipelineData::Empty)
+        } else {
+            Err(ShellError::GenericError {
+                error: format!("Failed to stop the `{}` plugin", name.item),
+                msg: "couldn't find a plugin with this name".into(),
+                span: Some(name.span),
+                help: Some("you may need to `register` the plugin first".into()),
+                inner: vec![],
+            })
+        }
+    }
+}

crates/nu-cmd-lang/src/core_commands/version.rs
@@ -130,11 +130,11 @@ pub fn version(engine_state: &EngineState, call: &Call) -> Result<PipelineData, 
         Value::string(features_enabled().join(", "), call.head),
     );
 
-    // Get a list of command names and check for plugins
+    // Get a list of plugin names
     let installed_plugins = engine_state
-        .plugin_decls()
-        .filter(|x| x.is_plugin().is_some())
-        .map(|x| x.name())
+        .plugins()
+        .iter()
+        .map(|x| x.identity().name())
         .collect::<Vec<_>>();
 
     record.push(

crates/nu-cmd-lang/src/default_context.rs
@@ -65,7 +65,7 @@ pub fn create_default_context() -> EngineState {
         };
 
         //#[cfg(feature = "plugin")]
-        bind_command!(Register);
+        bind_command!(PluginCommand, PluginList, PluginStop, Register,);
 
         working_set.render()
     };

crates/nu-engine/src/scope.rs
@@ -115,7 +115,7 @@ impl<'e, 's> ScopeData<'e, 's> {
                     // we can only be a is_builtin or is_custom, not both
                     "is_builtin" => Value::bool(!decl.is_custom_command(), span),
                     "is_sub" => Value::bool(decl.is_sub(), span),
-                    "is_plugin" => Value::bool(decl.is_plugin().is_some(), span),
+                    "is_plugin" => Value::bool(decl.is_plugin(), span),
                     "is_custom" => Value::bool(decl.is_custom_command(), span),
                     "is_keyword" => Value::bool(decl.is_parser_keyword(), span),
                     "is_extern" => Value::bool(decl.is_known_external(), span),

commit 1d14d294087b93c4269fe0762fbd02ecba677c2d

Fix unused IntoSpanned warning in nu_parser::parse_keywords when 'plugin' feature not enabled (#12144)
# Description

There is a warning about unused `IntoSpanned` currently when running
`cargo check -p nu-parser`, introduced accidentally by #12064. This
fixes that.

# User-Facing Changes
None

# Tests + Formatting
- :green_circle: `toolkit fmt`
- :green_circle: `toolkit clippy`
- :green_circle: `toolkit test`
- :green_circle: `toolkit test stdlib`

crates/nu-parser/src/parse_keywords.rs
@@ -13,8 +13,8 @@ use nu_protocol::{
     },
     engine::{StateWorkingSet, DEFAULT_OVERLAY_NAME},
     eval_const::eval_constant,
-    span, Alias, BlockId, DeclId, Exportable, IntoSpanned, Module, ModuleId, ParseError,
-    PositionalArg, ResolvedImportPattern, Span, Spanned, SyntaxShape, Type, Value, VarId,
+    span, Alias, BlockId, DeclId, Exportable, Module, ModuleId, ParseError, PositionalArg,
+    ResolvedImportPattern, Span, Spanned, SyntaxShape, Type, Value, VarId,
 };
 use std::collections::{HashMap, HashSet};
 use std::path::{Path, PathBuf};
@@ -3546,7 +3546,9 @@ pub fn parse_register(working_set: &mut StateWorkingSet, spans: &[Span]) -> Pipe
     use std::sync::Arc;
 
     use nu_plugin::{get_signature, PersistentPlugin, PluginDeclaration};
-    use nu_protocol::{engine::Stack, PluginIdentity, PluginSignature, RegisteredPlugin};
+    use nu_protocol::{
+        engine::Stack, IntoSpanned, PluginIdentity, PluginSignature, RegisteredPlugin,
+    };
 
     let cwd = working_set.get_cwd();
 