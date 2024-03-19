89e27b1fbf8719b5cfeafaf68f5e67ba289f7446
Devyn Cairns <devyn.cairns@gmail.com>
2024-03-19 06:34:34 -0700

    export ErrorLabel

--- crates/nu-protocol/src/errors/mod.rs
+++ crates/nu-protocol/src/errors/mod.rs
@@ -5,7 +5,7 @@ mod parse_warning;
 mod shell_error;
 
 pub use cli_error::{format_error, report_error, report_error_new};
-pub use labeled_error::LabeledError;
+pub use labeled_error::{ErrorLabel, LabeledError};
 pub use parse_error::{DidYouMean, ParseError};
 pub use parse_warning::ParseWarning;
 pub use shell_error::*;
