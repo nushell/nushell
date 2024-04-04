nushell/nushell

commit 87c5f6e45533af47ab0f83ce10ab78812f817c1d

ls, rm, cp, open, touch, mkdir: Don't expand tilde if input path is quoted string or a variable. (#12232)
# Description
Fixes:  #11887
Fixes: #11626

This pr unify the tilde expand behavior over several filesystem relative
commands. It follows the same rule with glob expansion:
|  command  |  result |
| ----------- |  ------ |
| ls ~/aaa  | expand tilde
| ls "~/aaa"  | don't expand tilde
| let f = "~/aaa"; ls $f | don't expand tilde, if you want to: use `ls
($f \| path expand)`
| let f: glob = "~/aaa"; ls $f | expand tilde, they don't expand on
`mkdir`, `touch` comamnd.

Actually I'm not sure for 4th item, currently it's expanding is just
because it followes the same rule with glob expansion.

### About the change
It changes `expand_path_with` to accept a new argument called
`expand_tilde`, if it's true, expand it, if not, just keep it as `~`
itself.

# User-Facing Changes
After this change, `ls "~/aaa"` won't expand tilde.

# Tests + Formatting
Done

crates/nu-command/src/filesystem/umkdir.rs
@@ -1,8 +1,8 @@
-use nu_engine::env::current_dir;
 use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::{Category, Example, PipelineData, ShellError, Signature, SyntaxShape, Type};
+use std::path::PathBuf;
 
 use uu_mkdir::mkdir;
 #[cfg(not(windows))]
@@ -60,11 +60,10 @@ impl Command for UMkdir {
         call: &Call,
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let cwd = current_dir(engine_state, stack)?;
         let mut directories = call
             .rest::<String>(engine_state, stack, 0)?
             .into_iter()
-            .map(|dir| nu_path::expand_path_with(dir, &cwd))
+            .map(PathBuf::from)
             .peekable();
 
         let is_verbose = call.has_flag(engine_state, stack, "verbose")?;

crates/nu-command/tests/commands/touch.rs
@@ -1,7 +1,8 @@
 use chrono::{DateTime, Local};
-use nu_test_support::fs::Stub;
+use nu_test_support::fs::{files_exist_at, Stub};
 use nu_test_support::nu;
 use nu_test_support::playground::Playground;
+use std::path::Path;
 
 // Use 1 instead of 0 because 0 has a special meaning in Windows
 const TIME_ONE: filetime::FileTime = filetime::FileTime::from_unix_time(1, 0);
@@ -485,5 +486,19 @@ fn change_dir_atime_to_reference() {
             target_original_mtime,
             target.metadata().unwrap().modified().unwrap()
         );
+    })
+}
+
+#[test]
+fn create_a_file_with_tilde() {
+    Playground::setup("touch with tilde", |dirs, _| {
+        let actual = nu!(cwd: dirs.test(), "touch '~tilde'");
+        assert!(actual.err.is_empty());
+        assert!(files_exist_at(vec![Path::new("~tilde")], dirs.test()));
+
+        // pass variable
+        let actual = nu!(cwd: dirs.test(), "let f = '~tilde2'; touch $f");
+        assert!(actual.err.is_empty());
+        assert!(files_exist_at(vec![Path::new("~tilde2")], dirs.test()));
     })
 }

crates/nu-command/tests/commands/umkdir.rs
@@ -145,3 +145,17 @@ fn mkdir_umask_permission() {
         );
     })
 }
+
+#[test]
+fn mkdir_with_tilde() {
+    Playground::setup("mkdir with tilde", |dirs, _| {
+        let actual = nu!(cwd: dirs.test(), "mkdir '~tilde'");
+        assert!(actual.err.is_empty());
+        assert!(files_exist_at(vec![Path::new("~tilde")], dirs.test()));
+
+        // pass variable
+        let actual = nu!(cwd: dirs.test(), "let f = '~tilde2'; mkdir $f");
+        assert!(actual.err.is_empty());
+        assert!(files_exist_at(vec![Path::new("~tilde2")], dirs.test()));
+    })
+}

crates/nu-cli/src/repl.rs
@@ -734,7 +734,7 @@ fn parse_operation(
         orig = trim_quotes_str(&orig).to_string()
     }
 
-    let path = nu_path::expand_path_with(&orig, &cwd);
+    let path = nu_path::expand_path_with(&orig, &cwd, true);
     if looks_like_path(&orig) && path.is_dir() && tokens.0.len() == 1 {
         Ok(ReplOperation::AutoCd {
             cwd,

crates/nu-command/src/env/load_env.rs
@@ -70,7 +70,7 @@ impl Command for LoadEnv {
                         if env_var == "PWD" {
                             let cwd = current_dir(engine_state, stack)?;
                             let rhs = rhs.coerce_into_string()?;
-                            let rhs = nu_path::expand_path_with(rhs, cwd);
+                            let rhs = nu_path::expand_path_with(rhs, cwd, true);
                             stack.add_env_var(
                                 env_var,
                                 Value::string(rhs.to_string_lossy(), call.head),

crates/nu-command/src/filesystem/ls.rs
@@ -118,12 +118,14 @@ impl Command for Ls {
         let (path, p_tag, absolute_path, quoted) = match pattern_arg {
             Some(pat) => {
                 let p_tag = pat.span;
-                let p = expand_to_real_path(pat.item.as_ref());
-
-                let expanded = nu_path::expand_path_with(&p, &cwd);
+                let expanded = nu_path::expand_path_with(
+                    pat.item.as_ref(),
+                    &cwd,
+                    matches!(pat.item, NuGlob::Expand(..)),
+                );
                 // Avoid checking and pushing "*" to the path when directory (do not show contents) flag is true
                 if !directory && expanded.is_dir() {
-                    if permission_denied(&p) {
+                    if permission_denied(&expanded) {
                         #[cfg(unix)]
                         let error_msg = format!(
                             "The permissions of {:o} do not allow access for this user",
@@ -151,9 +153,17 @@ impl Command for Ls {
                     }
                     extra_star_under_given_directory = true;
                 }
-                let absolute_path = p.is_absolute();
+
+                // it's absolute path if:
+                // 1. pattern is absolute.
+                // 2. pattern can be expanded, and after expands to real_path, it's absolute.
+                //    here `expand_to_real_path` call is required, because `~/aaa` should be absolute
+                //    path.
+                let absolute_path = Path::new(pat.item.as_ref()).is_absolute()
+                    || (pat.item.is_expand()
+                        && expand_to_real_path(pat.item.as_ref()).is_absolute());
                 (
-                    p,
+                    expanded,
                     p_tag,
                     absolute_path,
                     matches!(pat.item, NuGlob::DoNotExpand(_)),

crates/nu-command/src/filesystem/open.rs
@@ -1,6 +1,5 @@
 use super::util::get_rest_for_glob_pattern;
 use nu_engine::{current_dir, get_eval_block, CallExt};
-use nu_path::expand_to_real_path;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::util::BufferedReader;
@@ -153,7 +152,6 @@ impl Command for Open {
                     };
 
                     let buf_reader = BufReader::new(file);
-                    let real_path = expand_to_real_path(path);
 
                     let file_contents = PipelineData::ExternalStream {
                         stdout: Some(RawStream::new(
@@ -166,7 +164,7 @@ impl Command for Open {
                         exit_code: None,
                         span: call_span,
                         metadata: Some(PipelineMetadata {
-                            data_source: DataSource::FilePath(real_path),
+                            data_source: DataSource::FilePath(path.to_path_buf()),
                         }),
                         trim_end_newline: false,
                     };

crates/nu-command/src/filesystem/rm.rs
@@ -157,7 +157,7 @@ fn rm(
 
     for (idx, path) in paths.clone().into_iter().enumerate() {
         if let Some(ref home) = home {
-            if expand_path_with(path.item.as_ref(), &currentdir_path)
+            if expand_path_with(path.item.as_ref(), &currentdir_path, path.item.is_expand())
                 .to_string_lossy()
                 .as_ref()
                 == home.as_str()
@@ -242,7 +242,11 @@ fn rm(
     let mut all_targets: HashMap<PathBuf, Span> = HashMap::new();
 
     for target in paths {
-        let path = expand_path_with(target.item.as_ref(), &currentdir_path);
+        let path = expand_path_with(
+            target.item.as_ref(),
+            &currentdir_path,
+            target.item.is_expand(),
+        );
         if currentdir_path.to_string_lossy() == path.to_string_lossy()
             || currentdir_path.starts_with(format!("{}{}", target.item, std::path::MAIN_SEPARATOR))
         {
@@ -281,7 +285,11 @@ fn rm(
                             }
 
                             all_targets
-                                .entry(nu_path::expand_path_with(f, &currentdir_path))
+                                .entry(nu_path::expand_path_with(
+                                    f,
+                                    &currentdir_path,
+                                    target.item.is_expand(),
+                                ))
                                 .or_insert_with(|| target.span);
                         }
                         Err(e) => {

commit 8b0024553085118fe5b67f4e393417c020a783c7

Fix #12391: mkdir uses process startup directory instead of current script directory
nushell/nushell@87c5f6e455 accidentally introduced a bug where the path
was not being properly expanded according to the cwd. This makes both
'touch' and 'mkdir' use globs just like the rest of the commands to
preserve tilde behavior while still expanding the paths properly.

crates/nu-command/src/filesystem/touch.rs
@@ -1,7 +1,11 @@
 use filetime::FileTime;
-use nu_engine::command_prelude::*;
+use nu_engine::{command_prelude::*, current_dir};
+use nu_path::expand_path_with;
+use nu_protocol::NuGlob;
 
 use std::{fs::OpenOptions, path::Path, time::SystemTime};
+
+use super::util::get_rest_for_glob_pattern;
 
 #[derive(Clone)]
 pub struct Touch;
@@ -18,7 +22,11 @@ impl Command for Touch {
     fn signature(&self) -> Signature {
         Signature::build("touch")
             .input_output_types(vec![(Type::Nothing, Type::Nothing)])
-            .rest("files", SyntaxShape::Filepath, "The file(s) to create.")
+            .rest(
+                "files",
+                SyntaxShape::OneOf(vec![SyntaxShape::GlobPattern, SyntaxShape::Filepath]),
+                "The file(s) to create."
+            )
             .named(
                 "reference",
                 SyntaxShape::String,
@@ -58,7 +66,7 @@ impl Command for Touch {
         let mut change_atime: bool = call.has_flag(engine_state, stack, "access")?;
         let reference: Option<Spanned<String>> = call.get_flag(engine_state, stack, "reference")?;
         let no_create: bool = call.has_flag(engine_state, stack, "no-create")?;
-        let files: Vec<String> = call.rest(engine_state, stack, 0)?;
+        let files: Vec<Spanned<NuGlob>> = get_rest_for_glob_pattern(engine_state, stack, call, 0)?;
 
         if files.is_empty() {
             return Err(ShellError::MissingParameter {
@@ -105,8 +113,10 @@ impl Command for Touch {
                 })?;
         }
 
-        for (index, item) in files.into_iter().enumerate() {
-            let path = Path::new(&item);
+        let cwd = current_dir(engine_state, stack)?;
+
+        for (index, glob) in files.into_iter().enumerate() {
+            let path = expand_path_with(glob.item.as_ref(), &cwd, glob.item.is_expand());
 
             // If --no-create is passed and the file/dir does not exist there's nothing to do
             if no_create && !path.exists() {
@@ -119,7 +129,7 @@ impl Command for Touch {
                     .write(true)
                     .create(true)
                     .truncate(false)
-                    .open(path)
+                    .open(&path)
                 {
                     return Err(ShellError::CreateNotPossible {
                         msg: format!("Failed to create file: {err}"),
@@ -132,7 +142,7 @@ impl Command for Touch {
             }
 
             if change_mtime {
-                if let Err(err) = filetime::set_file_mtime(&item, FileTime::from_system_time(mtime))
+                if let Err(err) = filetime::set_file_mtime(&path, FileTime::from_system_time(mtime))
                 {
                     return Err(ShellError::ChangeModifiedTimeNotPossible {
                         msg: format!("Failed to change the modified time: {err}"),
@@ -145,7 +155,7 @@ impl Command for Touch {
             }
 
             if change_atime {
-                if let Err(err) = filetime::set_file_atime(&item, FileTime::from_system_time(atime))
+                if let Err(err) = filetime::set_file_atime(&path, FileTime::from_system_time(atime))
                 {
                     return Err(ShellError::ChangeAccessTimeNotPossible {
                         msg: format!("Failed to change the access time: {err}"),

crates/nu-command/src/filesystem/umkdir.rs
@@ -1,9 +1,10 @@
-use nu_engine::command_prelude::*;
+use nu_engine::{command_prelude::*, current_dir};
 
-use std::path::PathBuf;
 use uu_mkdir::mkdir;
 #[cfg(not(windows))]
 use uucore::mode;
+
+use super::util::get_rest_for_glob_pattern;
 
 #[derive(Clone)]
 pub struct UMkdir;
@@ -39,7 +40,7 @@ impl Command for UMkdir {
             .input_output_types(vec![(Type::Nothing, Type::Nothing)])
             .rest(
                 "rest",
-                SyntaxShape::Directory,
+                SyntaxShape::OneOf(vec![SyntaxShape::GlobPattern, SyntaxShape::Directory]),
                 "The name(s) of the path(s) to create.",
             )
             .switch(
@@ -57,10 +58,10 @@ impl Command for UMkdir {
         call: &Call,
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let mut directories = call
-            .rest::<String>(engine_state, stack, 0)?
+        let cwd = current_dir(engine_state, stack)?;
+        let mut directories = get_rest_for_glob_pattern(engine_state, stack, call, 0)?
             .into_iter()
-            .map(PathBuf::from)
+            .map(|dir| nu_path::expand_path_with(dir.item.as_ref(), &cwd, dir.item.is_expand()))
             .peekable();
 
         let is_verbose = call.has_flag(engine_state, stack, "verbose")?;

crates/nu-command/tests/commands/touch.rs
@@ -502,3 +502,16 @@ fn create_a_file_with_tilde() {
         assert!(files_exist_at(vec![Path::new("~tilde2")], dirs.test()));
     })
 }
+
+#[test]
+fn respects_cwd() {
+    Playground::setup("touch_respects_cwd", |dirs, _sandbox| {
+        nu!(
+            cwd: dirs.test(),
+            "mkdir dir; cd dir; touch i_will_be_created.txt"
+        );
+
+        let path = dirs.test().join("dir/i_will_be_created.txt");
+        assert!(path.exists());
+    })
+}

crates/nu-command/tests/commands/umkdir.rs
@@ -123,6 +123,20 @@ fn creates_directory_three_dots_quotation_marks() {
     })
 }
 
+#[test]
+fn respects_cwd() {
+    Playground::setup("mkdir_respects_cwd", |dirs, _| {
+        nu!(
+            cwd: dirs.test(),
+            "mkdir some_folder; cd some_folder; mkdir another/deeper_one"
+        );
+
+        let expected = dirs.test().join("some_folder/another/deeper_one");
+
+        assert!(expected.exists());
+    })
+}
+
 #[cfg(not(windows))]
 #[test]
 fn mkdir_umask_permission() {