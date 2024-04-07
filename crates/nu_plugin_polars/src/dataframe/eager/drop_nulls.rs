8b008304f4646a1133d5175502feef1943605513
Jack Wright <jack.wright@disqo.com>
2024-04-06 18:18:19 -0700

    enabling polars DropNulls command

--- crates/nu_plugin_polars/src/dataframe/eager/drop_nulls.rs
+++ crates/nu_plugin_polars/src/dataframe/eager/drop_nulls.rs
@@ -65,8 +65,7 @@ impl PluginCommand for DropNulls {
                         None,
                     )
                     .expect("simple df for test should not fail")
-                    .base_value(Span::test_data())
-                    .expect("rendering base value should not fail"),
+                    .into_value(Span::test_data()),
                 ),
             },
             Example {
@@ -87,8 +86,7 @@ impl PluginCommand for DropNulls {
                         None,
                     )
                     .expect("simple df for test should not fail")
-                    .base_value(Span::test_data())
-                    .expect("rendering base value should not fail"),
+                    .into_value(Span::test_data()),
                 ),
             },
         ]
@@ -139,15 +137,13 @@ fn command(
     to_pipeline_data(plugin, engine, call.head, df)
 }
 
-// todo - fix tests
-// #[cfg(test)]
-// mod test {
-//     use super::super::super::test_dataframe::test_dataframe;
-//     use super::super::WithColumn;
-//     use super::*;
-//
-//     #[test]
-//     fn test_examples() {
-//         test_dataframe(vec![Box::new(DropNulls {}), Box::new(WithColumn {})])
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::test_polars_plugin_command;
+
+    #[test]
+    fn test_examples() -> Result<(), ShellError> {
+        test_polars_plugin_command(&DropNulls)
+    }
+}
