e6c5f348819607b926dda6846169c9a197a5f46a
Jack Wright <jack.wright@disqo.com>
2024-04-08 14:33:36 -0700

    "fixing polars arg-sort tests"

--- crates/nu_plugin_polars/src/dataframe/series/indexes/arg_sort.rs
+++ crates/nu_plugin_polars/src/dataframe/series/indexes/arg_sort.rs
@@ -65,8 +65,7 @@ impl PluginCommand for ArgSort {
                         None,
                     )
                     .expect("simple df for test should not fail")
-                    .base_value(Span::test_data())
-                    .expect("rendering base value should not fail"),
+                    .into_value(Span::test_data()),
                 ),
             },
             Example {
@@ -87,8 +86,7 @@ impl PluginCommand for ArgSort {
                         None,
                     )
                     .expect("simple df for test should not fail")
-                    .base_value(Span::test_data())
-                    .expect("rendering base value should not fail"),
+                    .into_value(Span::test_data()),
                 ),
             },
         ]
@@ -130,14 +128,13 @@ fn command(
     to_pipeline_data(plugin, engine, call.head, df)
 }
 
-// todo: fix tests
-// #[cfg(test)]
-// mod test {
-//     use super::super::super::super::test_dataframe::test_dataframe;
-//     use super::*;
-//
-//     #[test]
-//     fn test_examples() {
-//         test_dataframe(vec![Box::new(ArgSort {})])
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::test_polars_plugin_command;
+
+    #[test]
+    fn test_examples() -> Result<(), ShellError> {
+        test_polars_plugin_command(&ArgSort)
+    }
+}
