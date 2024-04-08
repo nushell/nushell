27c47bcb4357e06181d6068885cef707400fa790
Jack Wright <jack.wright@disqo.com>
2024-04-08 14:48:17 -0700

    "fixing polars arg-unique tests"

--- crates/nu_plugin_polars/src/dataframe/series/indexes/arg_unique.rs
+++ crates/nu_plugin_polars/src/dataframe/series/indexes/arg_unique.rs
@@ -51,8 +51,7 @@ impl PluginCommand for ArgUnique {
                     None,
                 )
                 .expect("simple df for test should not fail")
-                .base_value(Span::test_data())
-                .expect("rendering base value should not fail"),
+                .into_value(Span::test_data()),
             ),
         }]
     }
@@ -93,14 +92,13 @@ fn command(
     to_pipeline_data(plugin, engine, call.head, df)
 }
 
-// todo: fix tests
-// #[cfg(test)]
-// mod test {
-//     use super::super::super::super::test_dataframe::test_dataframe;
-//     use super::*;
-//
-//     #[test]
-//     fn test_examples() {
-//         test_dataframe(vec![Box::new(ArgUnique {})])
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::test_polars_plugin_command;
+
+    #[test]
+    fn test_examples() -> Result<(), ShellError> {
+        test_polars_plugin_command(&ArgUnique)
+    }
+}
