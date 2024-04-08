e8f432fc07790752596843c27c15c96006edb971
Jack Wright <jack.wright@disqo.com>
2024-04-08 14:57:46 -0700

    "fixing polars set tests"

--- crates/nu_plugin_polars/src/dataframe/series/masks/set.rs
+++ crates/nu_plugin_polars/src/dataframe/series/masks/set.rs
@@ -64,8 +64,7 @@ impl PluginCommand for SetSeries {
                     None,
                 )
                 .expect("simple df for test should not fail")
-                .base_value(Span::test_data())
-                .expect("rendering base value should not fail"),
+                .into_value(Span::test_data()),
             ),
         }]
     }
@@ -199,19 +198,13 @@ fn command(
     to_pipeline_data(plugin, engine, call.head, res)
 }
 
-// todo: fix tests
-// #[cfg(test)]
-// mod test {
-//     use super::super::super::super::test_dataframe::test_dataframe;
-//     use super::super::super::{IsNull, Shift};
-//     use super::*;
-//
-//     #[test]
-//     fn test_examples() {
-//         test_dataframe(vec![
-//             Box::new(SetSeries {}),
-//             Box::new(IsNull {}),
-//             Box::new(Shift {}),
-//         ])
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::test_polars_plugin_command;
+
+    #[test]
+    fn test_examples() -> Result<(), ShellError> {
+        test_polars_plugin_command(&SetSeries)
+    }
+}
