b9a917d76b03bdb49f8a71cc224fa9878cbcc43d
Jack Wright <jack.wright@disqo.com>
2024-04-08 16:12:46 -0700

    "WIP: get origina tests. requires into command"

--- crates/nu_plugin_polars/src/dataframe/series/date/get_ordinal.rs
+++ crates/nu_plugin_polars/src/dataframe/series/date/get_ordinal.rs
@@ -49,8 +49,7 @@ impl PluginCommand for GetOrdinal {
                     None,
                 )
                 .expect("simple df for test should not fail")
-                .base_value(Span::test_data())
-                .expect("rendering base value should not fail"),
+                .into_value(Span::test_data()),
             ),
         }]
     }
@@ -89,15 +88,14 @@ fn command(
     to_pipeline_data(plugin, engine, call.head, df)
 }
 
-// todo: fix tests
-// #[cfg(test)]
-// mod test {
-//     use super::super::super::super::super::IntoDatetime;
-//     use super::super::super::super::test_dataframe::test_dataframe;
-//     use super::*;
-//
-//     #[test]
-//     fn test_examples() {
-//         test_dataframe(vec![Box::new(GetOrdinal {}), Box::new(IntoDatetime {})])
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::test_polars_plugin_command;
+
+    #[test]
+    #[ignore = "need the into command to run"]
+    fn test_examples() -> Result<(), ShellError> {
+        test_polars_plugin_command(&GetOrdinal)
+    }
+}
