29099e1187eb830ffef8e95fa24b611c39df3b91
Jack Wright <jack.wright@disqo.com>
2024-04-09 10:56:23 -0700

    "fixing polars unique tests."

--- crates/nu_plugin_polars/src/dataframe/series/unique.rs
+++ crates/nu_plugin_polars/src/dataframe/series/unique.rs
@@ -66,8 +66,7 @@ impl PluginCommand for Unique {
                         None,
                     )
                     .expect("simple df for test should not fail")
-                    .base_value(Span::test_data())
-                    .expect("rendering base value should not fail"),
+                    .into_value(Span::test_data()),
                 ),
             },
             Example {
@@ -152,14 +151,13 @@ fn command_lazy(
     to_pipeline_data(plugin, engine, call.head, lazy)
 }
 
-// todo: fix tests
-// #[cfg(test)]
-// mod test {
-//     use super::super::super::test_dataframe::test_dataframe;
-//     use super::*;
-//
-//     #[test]
-//     fn test_examples() {
-//         test_dataframe(vec![Box::new(Unique {})])
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::test_polars_plugin_command;
+
+    #[test]
+    fn test_examples() -> Result<(), ShellError> {
+        test_polars_plugin_command(&Unique)
+    }
+}
