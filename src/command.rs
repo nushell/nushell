a4d67a9f4ca4cf3da7db266ffc908e34a9814b0d
Devyn Cairns <devyn.cairns@gmail.com>
2024-04-09 07:04:00 -0700

    Add --no-newline option to `nu` (#12410)
    I have `nu` set as my shell in my editor, which allows me to easily pipe
    selections of text to things like `str pascal-case` or even more complex
    string operation pipelines, which I find super handy. However, the only
    annoying thing is that I pretty much always have to add `| print -n` at
    the end, because `nu` adds a newline when it prints the resulting value.
    
    This adds a `--no-newline` option to stop that from happening, and then
    you don't need to pipe to `print -n` anymore, you can just have your
    shell command for your editor contain that flag.
    
    - Add `--no-newline` command line option
    
    - :green_circle: `toolkit fmt`
    - :green_circle: `toolkit clippy`
    - :green_circle: `toolkit test`
    - :green_circle: `toolkit test stdlib`

--- src/command.rs
+++ src/command.rs
@@ -97,6 +97,7 @@ pub(crate) fn parse_commandline_args(
             let execute = call.get_flag_expr("execute");
             let table_mode: Option<Value> =
                 call.get_flag(engine_state, &mut stack, "table-mode")?;
+            let no_newline = call.get_named_arg("no-newline");
 
             // ide flags
             let lsp = call.has_flag(engine_state, &mut stack, "lsp")?;
@@ -190,6 +191,7 @@ pub(crate) fn parse_commandline_args(
                 ide_check,
                 ide_ast,
                 table_mode,
+                no_newline,
             });
         }
     }
@@ -224,6 +226,7 @@ pub(crate) struct NushellCliArgs {
     pub(crate) log_target: Option<Spanned<String>>,
     pub(crate) execute: Option<Spanned<String>>,
     pub(crate) table_mode: Option<Value>,
+    pub(crate) no_newline: Option<Spanned<String>>,
     pub(crate) include_path: Option<Spanned<String>>,
     pub(crate) lsp: bool,
     pub(crate) ide_goto_def: Option<Value>,
@@ -270,6 +273,7 @@ impl Command for Nu {
                 "the table mode to use. rounded is default.",
                 Some('m'),
             )
+            .switch("no-newline", "print the result for --commands(-c) without a newline", None)
             .switch(
                 "no-config-file",
                 "start with no config file and no env file",
