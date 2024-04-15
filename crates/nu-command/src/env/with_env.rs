6387fd99f11edf0ab5e602a8175abaf672e640b1
sholderbach <sholderbach@users.noreply.github.com>
2024-04-15 18:20:32 +0200

    Remove examples for deprecated input

--- crates/nu-command/src/env/with_env.rs
+++ crates/nu-command/src/env/with_env.rs
@@ -43,21 +43,6 @@ impl Command for WithEnv {
     fn examples(&self) -> Vec<Example> {
         vec![
             Example {
-                description: "Set the MYENV environment variable",
-                example: r#"with-env [MYENV "my env value"] { $env.MYENV }"#,
-                result: Some(Value::test_string("my env value")),
-            },
-            Example {
-                description: "Set by primitive value list",
-                example: r#"with-env [X Y W Z] { $env.X }"#,
-                result: Some(Value::test_string("Y")),
-            },
-            Example {
-                description: "Set by single row table",
-                example: r#"with-env [[X W]; [Y Z]] { $env.W }"#,
-                result: Some(Value::test_string("Z")),
-            },
-            Example {
                 description: "Set by key-value record",
                 example: r#"with-env {X: "Y", W: "Z"} { [$env.X $env.W] }"#,
                 result: Some(Value::list(
