8c9474a8c6f0313b60369a51e6432535a099cca5
amtoine <stevan.antoine@gmail.com>
2024-04-17 16:24:23 +0200

    copy the code as-is from `from nuon` and `to nuon`
    this commit just keeps the `run` implementation of the two commands and
    the helper functions.

--- crates/nu-nuon/src/lib.rs
+++ crates/nu-nuon/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod from;
+mod to;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use from::from_nuon;
+pub use to::to_nuon;
