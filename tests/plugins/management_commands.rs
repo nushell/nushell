df80c9331c3ebbfddac22bc5b8dd53946a6efc53
Devyn Cairns <devyn.cairns@gmail.com>
2024-04-19 16:27:17 -0700

    try a different strategy for getting the plugin path

--- tests/plugins/management_commands.rs
+++ tests/plugins/management_commands.rs
@@ -2,20 +2,23 @@ use nu_test_support::nu_with_plugins;
 
 #[test]
 fn plugin_add_then_restart_nu() {
+    let bins_path = nu_test_support::fs::binaries();
+    let example_plugin_path = nu_path::canonicalize_with(
+        if cfg!(windows) {
+            "nu_plugin_example.exe"
+        } else {
+            "nu_plugin_example"
+        },
+        bins_path,
+    )
+    .expect("nu_plugin_example not found");
     let result = nu_with_plugins!(
         cwd: ".",
         plugins: [],
-        if cfg!(windows) {
-            r#"
-                plugin add target\debug\nu_plugin_example.exe
-                ^$nu.current-exe --config $nu.config-path --env-config $nu.env-path --plugin-config $nu.plugin-path --commands 'plugin list | get name | to json --raw'
-            "#
-        } else {
-            r#"
-                plugin add target/debug/nu_plugin_example
-                ^$nu.current-exe --config $nu.config-path --env-config $nu.env-path --plugin-config $nu.plugin-path --commands 'plugin list | get name | to json --raw'
-            "#
-        }
+        &format!("
+            plugin add '{}'
+            ^$nu.current-exe --config $nu.config-path --env-config $nu.env-path --plugin-config $nu.plugin-path --commands 'plugin list | get name | to json --raw'
+        ", example_plugin_path.display())
     );
     assert!(result.status.success());
     assert_eq!(r#"["example"]"#, result.out);
