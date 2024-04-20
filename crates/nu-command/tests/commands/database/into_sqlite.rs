2b3eb9668cc185a9fade07cacae2aae75ec93a98
amtoine <stevan.antoine@gmail.com>
2024-04-20 10:56:41 +0200

    format

--- crates/nu-command/tests/commands/database/into_sqlite.rs
+++ crates/nu-command/tests/commands/database/into_sqlite.rs
@@ -330,8 +330,7 @@ fn into_sqlite_big_insert() {
                 )
                 .unwrap();
 
-            let nuon = nuon::to_nuon(&value, nuon::ToStyle::Raw, Some(Span::unknown()))
-                .unwrap()
+            let nuon = nuon::to_nuon(&value, nuon::ToStyle::Raw, Some(Span::unknown())).unwrap()
                 + &line_ending();
 
             nuon_file.write_all(nuon.as_bytes()).unwrap();
