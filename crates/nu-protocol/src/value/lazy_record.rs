a60975c1b69f013c749fccb4c9e1cc9a14831846
Ian Manske <ian.manske@pm.me>
2024-04-21 23:27:30 -0400

    Rename and add methods to `LazyRecord`

--- crates/nu-protocol/src/value/lazy_record.rs
+++ crates/nu-protocol/src/value/lazy_record.rs
@@ -1,27 +1,32 @@
 use crate::{Record, ShellError, Span, Value};
 use std::fmt;
 
-// Trait definition for a lazy record (where columns are evaluated on-demand)
-// typetag is needed to make this implement Serialize+Deserialize... even though we should never actually serialize a LazyRecord.
-// To serialize a LazyRecord, collect it into a Value::Record with collect() first.
+/// Trait definition for a lazy record (where columns are evaluated on-demand)
+/// To serialize a LazyRecord, convert it into a [`Value`] with [`to_value`](LazyRecord::to_value)
+/// or into a [`Record`] with [`to_record`](LazyRecord::to_record).
 pub trait LazyRecord<'a>: fmt::Debug + Send + Sync {
-    // All column names
+    /// All column names
     fn column_names(&'a self) -> Vec<&'a str>;
 
-    // Get 1 specific column value
+    /// Get the value for a specific column
     fn get_column_value(&self, column: &str) -> Result<Value, ShellError>;
 
     fn span(&self) -> Span;
 
-    // Convert the lazy record into a regular Value::Record by collecting all its columns
-    fn collect(&'a self) -> Result<Value, ShellError> {
+    /// Convert this [`LazyRecord`] into a [`Record`] by evaluating all of its columns
+    fn to_record(&'a self) -> Result<Record, ShellError> {
         self.column_names()
             .into_iter()
             .map(|col| {
                 let val = self.get_column_value(col)?;
                 Ok((col.to_owned(), val))
             })
-            .collect::<Result<Record, _>>()
+            .collect()
+    }
+
+    /// Convert this [`LazyRecord`] into a [`Value`] by evaluating all of its columns
+    fn to_value(&'a self) -> Result<Value, ShellError> {
+        self.to_record()
             .map(|record| Value::record(record, self.span()))
     }
 
