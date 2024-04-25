d16adc25f42138e6d03e557f32442314ba8506c4
YizhePKU <yizhe@pku.edu.cn>
2024-04-25 14:02:45 +0800

    Implement `nu_path::components()`

--- crates/nu-path/src/lib.rs
+++ crates/nu-path/src/lib.rs
@@ -1,9 +1,11 @@
 mod assert_path_eq;
+mod components;
 pub mod dots;
 mod expansions;
 mod helpers;
 mod tilde;
 
+pub use components::components;
 pub use expansions::{canonicalize_with, expand_path_with, expand_to_real_path, locate_in_dirs};
 pub use helpers::{config_dir, config_dir_old, home_dir};
 pub use tilde::expand_tilde;
