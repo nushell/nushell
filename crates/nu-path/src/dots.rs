450d0c84e7de1a839816f66599d1c686729c0503
YizhePKU <yizhe@pku.edu.cn>
2024-04-25 15:35:52 +0800

    Rework tests for `expand_dots` and `expand_ndots`
    This also fixes the problem of `assert_eq!` not correctly testing path equality.

--- crates/nu-path/src/dots.rs
+++ crates/nu-path/src/dots.rs
@@ -67,200 +67,129 @@ pub fn expand_dots(path: impl AsRef<Path>) -> PathBuf {
 }
 
 #[cfg(test)]
-mod tests {
+mod test_expand_ndots {
     use super::*;
+    use crate::assert_path_eq;
 
     #[test]
-    fn expand_two_dots() {
-        let path = Path::new("/foo/bar/..");
-
-        assert_eq!(
-            PathBuf::from("/foo"), // missing path
-            expand_dots(path)
-        );
+    fn empty_path() {
+        let path = Path::new("");
+        assert_path_eq!(expand_ndots(path), "");
     }
 
     #[test]
-    fn expand_dots_with_curdir() {
-        let path = Path::new("/foo/./bar/./baz");
-
-        assert_eq!(PathBuf::from("/foo/bar/baz"), expand_dots(path));
-    }
-
-    // track_caller refers, in the panic-message, to the line of the function call and not
-    // inside of the function, which is nice for a test-helper-function
-    #[track_caller]
-    fn check_ndots_expansion(expected: &str, s: &str) {
-        let expanded = expand_ndots(Path::new(s));
-        assert_eq!(Path::new(expected), &expanded);
-    }
-
-    // common tests
-    #[test]
-    fn string_without_ndots() {
-        check_ndots_expansion("../hola", "../hola");
+    fn root_dir() {
+        let path = Path::new("/");
+        let expected = if cfg!(windows) { "\\" } else { "/" };
+        assert_path_eq!(expand_ndots(path), expected);
     }
 
     #[test]
-    fn string_with_three_ndots_and_chars() {
-        check_ndots_expansion("a...b", "a...b");
+    fn two_dots() {
+        let path = Path::new("..");
+        assert_path_eq!(expand_ndots(path), "..");
     }
 
     #[test]
-    fn string_with_two_ndots_and_chars() {
-        check_ndots_expansion("a..b", "a..b");
+    fn three_dots() {
+        let path = Path::new("...");
+        let expected = if cfg!(windows) { r"..\.." } else { "../.." };
+        assert_path_eq!(expand_ndots(path), expected);
     }
 
     #[test]
-    fn string_with_one_dot_and_chars() {
-        check_ndots_expansion("a.b", "a.b");
+    fn five_dots() {
+        let path = Path::new(".....");
+        let expected = if cfg!(windows) {
+            r"..\..\..\.."
+        } else {
+            "../../../.."
+        };
+        assert_path_eq!(expand_ndots(path), expected);
     }
 
     #[test]
-    fn string_starts_with_dots() {
-        check_ndots_expansion(".file", ".file");
-        check_ndots_expansion("..file", "..file");
-        check_ndots_expansion("...file", "...file");
-        check_ndots_expansion("....file", "....file");
-        check_ndots_expansion(".....file", ".....file");
+    fn three_dots_with_trailing_slash() {
+        let path = Path::new("/tmp/.../");
+        let expected = if cfg!(windows) {
+            r"\tmp\..\..\"
+        } else {
+            "/tmp/../../"
+        };
+        assert_path_eq!(expand_ndots(path), expected);
     }
 
     #[test]
-    fn string_ends_with_dots() {
-        check_ndots_expansion("file.", "file.");
-        check_ndots_expansion("file..", "file..");
-        check_ndots_expansion("file...", "file...");
-        check_ndots_expansion("file....", "file....");
-        check_ndots_expansion("file.....", "file.....");
+    fn filenames_with_dots() {
+        let path = Path::new("...foo.../");
+        let expected = if cfg!(windows) {
+            r"...foo...\"
+        } else {
+            "...foo.../"
+        };
+        assert_path_eq!(expand_ndots(path), expected);
     }
 
     #[test]
-    fn string_starts_and_ends_with_dots() {
-        check_ndots_expansion(".file.", ".file.");
-        check_ndots_expansion("..file..", "..file..");
-        check_ndots_expansion("...file...", "...file...");
-        check_ndots_expansion("....file....", "....file....");
-        check_ndots_expansion(".....file.....", ".....file.....");
+    fn multiple_ndots() {
+        let path = Path::new("..././...");
+        let expected = if cfg!(windows) {
+            r"..\..\..\.."
+        } else {
+            "../../../.."
+        };
+        assert_path_eq!(expand_ndots(path), expected);
     }
-    #[test]
-    fn expand_multiple_dots() {
-        check_ndots_expansion("../..", "...");
-        check_ndots_expansion("../../..", "....");
-        check_ndots_expansion("../../../..", ".....");
-        check_ndots_expansion("../../../../", ".../...");
-        check_ndots_expansion("../../file name/../../", ".../file name/...");
-        check_ndots_expansion("../../../file name/../../../", "..../file name/....");
-    }
+}
+
+#[cfg(test)]
+mod test_expand_dots {
+    use super::*;
+    use crate::assert_path_eq;
 
     #[test]
-    fn expand_dots_double_dots_no_change() {
-        // Can't resolve this as we don't know our parent dir
-        assert_eq!(Path::new(".."), expand_dots(Path::new("..")));
-    }
-
-    #[test]
-    fn expand_dots_single_dot_no_change() {
-        // Can't resolve this as we don't know our current dir
-        assert_eq!(Path::new("."), expand_dots(Path::new(".")));
+    fn empty_path() {
+        let path = Path::new("");
+        assert_path_eq!(expand_dots(path), "");
     }
 
     #[test]
-    fn expand_dots_multi_single_dots_no_change() {
-        assert_eq!(Path::new("././."), expand_dots(Path::new("././.")));
+    fn single_dot() {
+        let path = Path::new("./");
+        let expected = if cfg!(windows) { r".\" } else { "./" };
+        assert_path_eq!(expand_dots(path), expected);
     }
 
     #[test]
-    fn expand_multi_double_dots_no_change() {
-        assert_eq!(Path::new("../../../"), expand_dots(Path::new("../../../")));
+    fn double_dots() {
+        let path = Path::new("../../..");
+        let expected = if cfg!(windows) {
+            r"..\..\.."
+        } else {
+            "../../.."
+        };
+        assert_path_eq!(expand_dots(path), expected);
     }
 
     #[test]
-    fn expand_dots_no_change_with_dirs() {
-        // Can't resolve this as we don't know our parent dir
-        assert_eq!(
-            Path::new("../../../dir1/dir2/"),
-            expand_dots(Path::new("../../../dir1/dir2"))
-        );
+    fn backtrack_once() {
+        let path = Path::new("/foo/bar/../baz/");
+        let expected = if cfg!(windows) {
+            r"\foo\baz\"
+        } else {
+            "/foo/baz/"
+        };
+        assert_path_eq!(expand_dots(path), expected);
     }
 
     #[test]
-    fn expand_dots_simple() {
-        assert_eq!(Path::new("/foo"), expand_dots(Path::new("/foo/bar/..")));
-    }
-
-    #[test]
-    fn expand_dots_complex() {
-        assert_eq!(
-            Path::new("/test"),
-            expand_dots(Path::new("/foo/./bar/../../test/././test2/../"))
-        );
-    }
-
-    #[cfg(windows)]
-    mod windows {
-        use super::*;
-
-        #[test]
-        fn string_with_three_ndots() {
-            check_ndots_expansion(r"..\..", "...");
-        }
-
-        #[test]
-        fn string_with_mixed_ndots_and_chars() {
-            check_ndots_expansion(
-                r"a...b/./c..d/../e.f/..\..\..//.",
-                "a...b/./c..d/../e.f/....//.",
-            );
-        }
-
-        #[test]
-        fn string_with_three_ndots_and_final_slash() {
-            check_ndots_expansion(r"..\../", ".../");
-        }
-
-        #[test]
-        fn string_with_three_ndots_and_garbage() {
-            check_ndots_expansion(r"not_a_cmd.../ garbage.*[", "not_a_cmd.../ garbage.*[");
-        }
-    }
-
-    #[cfg(not(windows))]
-    mod non_windows {
-        use super::*;
-        #[test]
-        fn string_with_three_ndots() {
-            check_ndots_expansion(r"../..", "...");
-        }
-
-        #[test]
-        fn string_with_mixed_ndots_and_chars() {
-            check_ndots_expansion(
-                "a...b/./c..d/../e.f/../../..//.",
-                "a...b/./c..d/../e.f/....//.",
-            );
-        }
-
-        #[test]
-        fn string_with_three_ndots_and_final_slash() {
-            check_ndots_expansion("../../", ".../");
-        }
-
-        #[test]
-        fn string_with_three_ndots_and_garbage() {
-            // filenames can contain spaces, in these cases the ... .... etc.
-            // that are part of a filepath should not be expanded
-            check_ndots_expansion("not_a_cmd.../ garbage.*[", "not_a_cmd.../ garbage.*[");
-            check_ndots_expansion("/not_a_cmd.../ garbage.*[", "/not_a_cmd.../ garbage.*[");
-            check_ndots_expansion("./not_a_cmd.../ garbage.*[", "./not_a_cmd.../ garbage.*[");
-            check_ndots_expansion(
-                "../../not a cmd.../ garbage.*[",
-                ".../not a cmd.../ garbage.*[",
-            );
-            check_ndots_expansion(
-                "../../not a cmd.../ garbage.*[...",
-                ".../not a cmd.../ garbage.*[...",
-            );
-            check_ndots_expansion("../../ not a cmd garbage.*[", ".../ not a cmd garbage.*[");
-        }
+    fn backtrack_to_root() {
+        let path = Path::new("/foo/bar/../../../../baz");
+        let expected = if cfg!(windows) {
+            r"\..\..\baz"
+        } else {
+            "/../../baz"
+        };
+        assert_path_eq!(expand_dots(path), expected);
     }
 }
