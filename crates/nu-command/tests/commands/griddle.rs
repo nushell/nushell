7b7c63d0098d1e3b1d5619df41291a714a3d785a
gabriel <poonsters6@gmail.com>
2024-04-26 15:26:11 +0800

    fix dumb code

--- crates/nu-command/tests/commands/griddle.rs
+++ crates/nu-command/tests/commands/griddle.rs
@@ -4,5 +4,5 @@ use nu_test_support::nu;
 fn grid_errors_with_few_columns() {
     let actual = nu!("[1 2 3 4 5] | grid --width 5");
 
-    assert_eq!(actual.err.contains("Couldn't fit grid into 5 columns"));
+    assert!(actual.err.contains("Couldn't fit grid into 5 columns"));
 }
