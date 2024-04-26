c42e6b57b04c4e6fcbc8281266396b2940dd0ceb
gabriel <poonsters6@gmail.com>
2024-04-26 15:18:50 +0800

    fmt

--- crates/nu-command/tests/commands/griddle.rs
+++ crates/nu-command/tests/commands/griddle.rs
@@ -2,9 +2,7 @@ use nu_test_support::{nu, pipeline};
 
 #[test]
 fn grid_errors_with_few_columns() {
-    let actual = nu!(
-        "[1 2 3 4 5] | grid --width 5"
-    );
+    let actual = nu!("[1 2 3 4 5] | grid --width 5");
 
     assert_eq!(actual.err.contains("Couldn't fit grid into 5 columns"));
 }
