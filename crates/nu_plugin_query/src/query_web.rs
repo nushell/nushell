nushell/nushell

commit efe25e3f58548dc4b776ded181cff558edcc2b07

Better generic errors for plugins (and perhaps scripts) (#12236)
# Description
This makes `LabeledError` much more capable of representing close to
everything a `miette::Diagnostic` can, including `ShellError`, and
allows plugins to generate multiple error spans, codes, help, etc.

`LabeledError` is now embeddable within `ShellError` as a transparent
variant.

This could also be used to improve `error make` and `try/catch` to
reflect `LabeledError` exactly in the future.

Also cleaned up some errors in existing plugins.

# User-Facing Changes
Breaking change for plugins. Nicer errors for users.

crates/nu_plugin_query/src/query_web.rs
@@ -1,6 +1,9 @@
 use crate::{web_tables::WebTable, Query};
-use nu_plugin::{EngineInterface, EvaluatedCall, LabeledError, SimplePluginCommand};
-use nu_protocol::{Category, PluginExample, PluginSignature, Record, Span, SyntaxShape, Value};
+use nu_plugin::{EngineInterface, EvaluatedCall, SimplePluginCommand};
+use nu_protocol::{
+    Category, LabeledError, PluginExample, PluginSignature, Record, Span, Spanned, SyntaxShape,
+    Value,
+};
 use scraper::{Html, Selector as ScraperSelector};
 
 pub struct QueryWeb;
@@ -99,10 +102,7 @@ impl Default for Selector {
 
 pub fn parse_selector_params(call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
     let head = call.head;
-    let query: String = match call.get_flag("query")? {
-        Some(q2) => q2,
-        None => "".to_string(),
-    };
+    let query: Option<Spanned<String>> = call.get_flag("query")?;
     let as_html = call.has_flag("as-html")?;
     let attribute = call.get_flag("attribute")?.unwrap_or_default();
     let as_table: Value = call
@@ -111,16 +111,20 @@ pub fn parse_selector_params(call: &EvaluatedCall, input: &Value) -> Result<Valu
 
     let inspect = call.has_flag("inspect")?;
 
-    if !&query.is_empty() && ScraperSelector::parse(&query).is_err() {
-        return Err(LabeledError {
-            msg: "Cannot parse this query as a valid css selector".to_string(),
-            label: "Parse error".to_string(),
-            span: Some(head),
-        });
+    if let Some(query) = &query {
+        if let Err(err) = ScraperSelector::parse(&query.item) {
+            return Err(LabeledError::new("CSS query parse error")
+                .with_label(err.to_string(), query.span)
+                .with_help("cannot parse this query as a valid CSS selector"));
+        }
+    } else {
+        return Err(
+            LabeledError::new("Missing query argument").with_label("add --query here", call.head)
+        );
     }
 
     let selector = Selector {
-        query,
+        query: query.map(|q| q.item).unwrap_or_default(),
         as_html,
         attribute,
         as_table,
@@ -130,11 +134,8 @@ pub fn parse_selector_params(call: &EvaluatedCall, input: &Value) -> Result<Valu
     let span = input.span();
     match input {
         Value::String { val, .. } => Ok(begin_selector_query(val.to_string(), selector, span)),
-        _ => Err(LabeledError {
-            label: "requires text input".to_string(),
-            msg: "Expected text from pipeline".to_string(),
-            span: Some(span),
-        }),
+        _ => Err(LabeledError::new("Requires text input")
+            .with_label("expected text from pipeline", span)),
     }
 }
 

crates/nu-cmd-lang/src/core_commands/error_make.rs
@@ -2,7 +2,8 @@ use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::{
-    Category, Example, PipelineData, Record, ShellError, Signature, Span, SyntaxShape, Type, Value,
+    Category, Example, LabeledError, PipelineData, Record, ShellError, Signature, Span,
+    SyntaxShape, Type, Value,
 };
 
 #[derive(Clone)]
@@ -258,13 +259,10 @@ fn make_other_error(value: &Value, throw_span: Option<Span>) -> ShellError {
     }
 
     // correct return: everything present
-    ShellError::GenericError {
-        error: msg,
-        msg: text,
-        span: Some(Span::new(span_start as usize, span_end as usize)),
-        help,
-        inner: vec![],
-    }
+    let mut error =
+        LabeledError::new(msg).with_label(text, Span::new(span_start as usize, span_end as usize));
+    error.help = help;
+    error.into()
 }
 
 fn get_span_sides(span: &Record, span_span: Span, side: &str) -> Result<i64, ShellError> {

crates/nu-plugin/src/lib.rs
@@ -16,9 +16,9 @@
 //! invoked by Nushell.
 //!
 //! ```rust,no_run
-//! use nu_plugin::{EvaluatedCall, LabeledError, MsgPackSerializer, serve_plugin};
+//! use nu_plugin::{EvaluatedCall, MsgPackSerializer, serve_plugin};
 //! use nu_plugin::{Plugin, PluginCommand, SimplePluginCommand, EngineInterface};
-//! use nu_protocol::{PluginSignature, Value};
+//! use nu_protocol::{PluginSignature, LabeledError, Value};
 //!
 //! struct MyPlugin;
 //! struct MyCommand;
@@ -64,7 +64,7 @@ mod util;
 pub use plugin::{
     serve_plugin, EngineInterface, Plugin, PluginCommand, PluginEncoder, SimplePluginCommand,
 };
-pub use protocol::{EvaluatedCall, LabeledError};
+pub use protocol::EvaluatedCall;
 pub use serializers::{json::JsonSerializer, msgpack::MsgPackSerializer};
 
 // Used by other nu crates.

crates/nu-plugin/src/plugin/command.rs
@@ -1,6 +1,6 @@
-use nu_protocol::{PipelineData, PluginSignature, Value};
+use nu_protocol::{LabeledError, PipelineData, PluginSignature, Value};
 
-use crate::{EngineInterface, EvaluatedCall, LabeledError, Plugin};
+use crate::{EngineInterface, EvaluatedCall, Plugin};
 
 /// The API for a Nushell plugin command
 ///
@@ -18,7 +18,7 @@ use crate::{EngineInterface, EvaluatedCall, LabeledError, Plugin};
 /// Basic usage:
 /// ```
 /// # use nu_plugin::*;
-/// # use nu_protocol::{PluginSignature, PipelineData, Type, Value};
+/// # use nu_protocol::{PluginSignature, PipelineData, Type, Value, LabeledError};
 /// struct LowercasePlugin;
 /// struct Lowercase;
 ///
@@ -108,7 +108,7 @@ pub trait PluginCommand: Sync {
 /// Basic usage:
 /// ```
 /// # use nu_plugin::*;
-/// # use nu_protocol::{PluginSignature, Type, Value};
+/// # use nu_protocol::{PluginSignature, Type, Value, LabeledError};
 /// struct HelloPlugin;
 /// struct Hello;
 ///

crates/nu-plugin/src/plugin/interface/engine.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 use nu_protocol::{
-    engine::Closure, Config, IntoInterruptiblePipelineData, ListStream, PipelineData,
+    engine::Closure, Config, IntoInterruptiblePipelineData, LabeledError, ListStream, PipelineData,
     PluginSignature, ShellError, Spanned, Value,
 };
 
@@ -16,7 +16,7 @@ use crate::{
         PluginCall, PluginCallId, PluginCallResponse, PluginCustomValue, PluginInput, PluginOption,
         ProtocolInfo,
     },
-    LabeledError, PluginOutput,
+    PluginOutput,
 };
 
 use super::{

crates/nu-plugin/src/plugin/interface/engine/tests.rs
@@ -4,8 +4,8 @@ use std::{
 };
 
 use nu_protocol::{
-    engine::Closure, Config, CustomValue, IntoInterruptiblePipelineData, PipelineData,
-    PluginExample, PluginSignature, ShellError, Span, Spanned, Value,
+    engine::Closure, Config, CustomValue, IntoInterruptiblePipelineData, LabeledError,
+    PipelineData, PluginExample, PluginSignature, ShellError, Span, Spanned, Value,
 };
 
 use crate::{
@@ -16,7 +16,7 @@ use crate::{
         ListStreamInfo, PipelineDataHeader, PluginCall, PluginCustomValue, PluginInput, Protocol,
         ProtocolInfo, RawStreamInfo, StreamData, StreamMessage,
     },
-    EvaluatedCall, LabeledError, PluginCallResponse, PluginOutput,
+    EvaluatedCall, PluginCallResponse, PluginOutput,
 };
 
 use super::{EngineInterfaceManager, ReceivedPluginCall};
@@ -738,11 +738,7 @@ fn interface_write_response_with_stream() -> Result<(), ShellError> {
 fn interface_write_response_with_error() -> Result<(), ShellError> {
     let test = TestCase::new();
     let interface = test.engine().interface_for_context(35);
-    let labeled_error = LabeledError {
-        label: "this is an error".into(),
-        msg: "a test error".into(),
-        span: None,
-    };
+    let labeled_error = LabeledError::new("this is an error").with_help("a test error");
     interface
         .write_response(Err(labeled_error.clone()))?
         .write()?;

crates/nu-plugin/src/plugin/mod.rs
@@ -1,4 +1,5 @@
 use nu_engine::documentation::get_flags_section;
+use nu_protocol::LabeledError;
 
 use std::cmp::Ordering;
 use std::collections::HashMap;
@@ -13,9 +14,7 @@ use std::sync::mpsc::TrySendError;
 use std::sync::{mpsc, Arc, Mutex};
 
 use crate::plugin::interface::{EngineInterfaceManager, ReceivedPluginCall};
-use crate::protocol::{
-    CallInfo, CustomValueOp, LabeledError, PluginCustomValue, PluginInput, PluginOutput,
-};
+use crate::protocol::{CallInfo, CustomValueOp, PluginCustomValue, PluginInput, PluginOutput};
 use crate::EncodingType;
 
 #[cfg(unix)]
@@ -230,7 +229,7 @@ where
 /// Basic usage:
 /// ```
 /// # use nu_plugin::*;
-/// # use nu_protocol::{PluginSignature, Type, Value};
+/// # use nu_protocol::{PluginSignature, LabeledError, Type, Value};
 /// struct HelloPlugin;
 /// struct Hello;
 ///
@@ -537,11 +536,12 @@ pub fn serve_plugin(plugin: &impl Plugin, encoder: impl PluginEncoder + 'static)
             let result = if let Some(command) = commands.get(&name) {
                 command.run(plugin, &engine, &call, input)
             } else {
-                Err(LabeledError {
-                    label: format!("Plugin command not found: `{name}`"),
-                    msg: format!("plugin `{plugin_name}` doesn't have this command"),
-                    span: Some(call.head),
-                })
+                Err(
+                    LabeledError::new(format!("Plugin command not found: `{name}`")).with_label(
+                        format!("plugin `{plugin_name}` doesn't have this command"),
+                        call.head,
+                    ),
+                )
             };
             let write_result = engine
                 .write_response(result)

crates/nu-plugin/src/protocol/mod.rs
@@ -12,8 +12,8 @@ use std::collections::HashMap;
 
 pub use evaluated_call::EvaluatedCall;
 use nu_protocol::{
-    ast::Operator, engine::Closure, Config, PipelineData, PluginSignature, RawStream, ShellError,
-    Span, Spanned, Value,
+    ast::Operator, engine::Closure, Config, LabeledError, PipelineData, PluginSignature, RawStream,
+    ShellError, Span, Spanned, Value,
 };
 pub use plugin_custom_value::PluginCustomValue;
 #[cfg(test)]
@@ -287,73 +287,6 @@ pub enum StreamMessage {
     /// Acknowledge that a message has been consumed. This is used to implement flow control by
     /// the stream producer. Sent by the stream consumer.
     Ack(StreamId),
-}
-
-/// An error message with debugging information that can be passed to Nushell from the plugin
-///
-/// The `LabeledError` struct is a structured error message that can be returned from
-/// a [Plugin](crate::Plugin)'s [`run`](crate::Plugin::run()) method. It contains
-/// the error message along with optional [Span] data to support highlighting in the
-/// shell.
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
-pub struct LabeledError {
-    /// The name of the error
-    pub label: String,
-    /// A detailed error description
-    pub msg: String,
-    /// The [Span] in which the error occurred
-    pub span: Option<Span>,
-}
-
-impl From<LabeledError> for ShellError {
-    fn from(error: LabeledError) -> Self {
-        if error.span.is_some() {
-            ShellError::GenericError {
-                error: error.label,
-                msg: error.msg,
-                span: error.span,
-                help: None,
-                inner: vec![],
-            }
-        } else {
-            ShellError::GenericError {
-                error: error.label,
-                msg: "".into(),
-                span: None,
-                help: (!error.msg.is_empty()).then_some(error.msg),
-                inner: vec![],
-            }
-        }
-    }
-}
-
-impl From<ShellError> for LabeledError {
-    fn from(error: ShellError) -> Self {
-        use miette::Diagnostic;
-        // This is not perfect - we can only take the first labeled span as that's all we have
-        // space for.
-        if let Some(labeled_span) = error.labels().and_then(|mut iter| iter.nth(0)) {
-            let offset = labeled_span.offset();
-            let span = Span::new(offset, offset + labeled_span.len());
-            LabeledError {
-                label: error.to_string(),
-                msg: labeled_span
-                    .label()
-                    .map(|label| label.to_owned())
-                    .unwrap_or_else(|| "".into()),
-                span: Some(span),
-            }
-        } else {
-            LabeledError {
-                label: error.to_string(),
-                msg: error
-                    .help()
-                    .map(|help| help.to_string())
-                    .unwrap_or_else(|| "".into()),
-                span: None,
-            }
-        }
-    }
 }
 
 /// Response to a [`PluginCall`]. The type parameter determines the output type for pipeline data.

commit 365f5954ee412a184173113cb5f72df7bc092a51

restore `query web --as-table` to working order (#12693)
# Description

This PR fixes a problem introduced with PR
https://github.com/nushell/nushell/pull/12236. That PR accidentally
stopped `--as-table` from working.

Closes https://github.com/nushell/nushell/issues/12689

It works again.

![image](https://github.com/nushell/nushell/assets/343840/b517507f-6b92-4e39-a389-5c69907d77c0)

# User-Facing Changes
<!-- List of all changes that impact the user experience here. This
helps us keep track of breaking changes. -->

# Tests + Formatting
<!--
Don't forget to add tests that cover your changes.

Make sure you've run and fixed any issues with these commands:

- `cargo fmt --all -- --check` to check standard code formatting (`cargo
fmt --all` applies these changes)
- `cargo clippy --workspace -- -D warnings -D clippy::unwrap_used` to
check that you're using the standard code style
- `cargo test --workspace` to check that all tests pass (on Windows make
sure to [enable developer
mode](https://learn.microsoft.com/en-us/windows/apps/get-started/developer-mode-features-and-debugging))
- `cargo run -- -c "use std testing; testing run-tests --path
crates/nu-std"` to run the tests for the standard library

> **Note**
> from `nushell` you can also use the `toolkit` as follows
> ```bash
> use toolkit.nu # or use an `env_change` hook to activate it
automatically
> toolkit check pr
> ```
-->

# After Submitting
<!-- If your PR had any user-facing changes, update [the
documentation](https://github.com/nushell/nushell.github.io) after the
PR is merged, if necessary. This will help us keep the docs up to date.
-->

crates/nu_plugin_query/src/query_web.rs
@@ -126,10 +126,6 @@ pub fn parse_selector_params(call: &EvaluatedCall, input: &Value) -> Result<Valu
                 .with_label(err.to_string(), query.span)
                 .with_help("cannot parse this query as a valid CSS selector"));
         }
-    } else {
-        return Err(
-            LabeledError::new("Missing query argument").with_label("add --query here", call.head)
-        );
     }
 
     let selector = Selector {