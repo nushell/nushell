b9cb03cc13ff50cb29848bcf6434a54385a985aa
amtoine <stevan.antoine@gmail.com>
2024-05-01 10:04:40 +0200

    move the doc to README

---
+++ crates/nuon/README.md
@@ -0,0 +1,6 @@
+Support for the NUON format.
+
+The NUON format is a superset of JSON designed to fit the feel of Nushell.
+Some of its extra features are
+- trailing commas are allowed
+- quotes are not required around keys
--- crates/nuon/src/lib.rs
+++ crates/nuon/src/lib.rs
@@ -1,9 +1,4 @@
-//! Support for the NUON format.
-//!
-//! The NUON format is a superset of JSON designed to fit the feel of Nushell.
-//! Some of its extra features are
-//! - trailing commas are allowed
-//! - quotes are not required around keys
+#![doc = include_str!("../README.md")]
 mod from;
 mod to;
 
