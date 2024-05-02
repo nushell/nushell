1cda5e12ee01d7320f81f81ce4db30330d7ab086
Maxim Uvarov <maxim-uvarov@users.noreply.github.com>
2024-05-02 13:55:33 +0000

    polars: `shift --fill` add test

--- crates/nu_plugin_polars/src/dataframe/series/shift.rs
+++ crates/nu_plugin_polars/src/dataframe/series/shift.rs
@@ -45,21 +45,38 @@ impl PluginCommand for Shift {
     }
 
     fn examples(&self) -> Vec<Example> {
-        vec![Example {
-            description: "Shifts the values by a given period",
-            example: "[1 2 2 3 3] | polars into-df | polars shift 2 | polars drop-nulls",
-            result: Some(
-                NuDataFrame::try_from_columns(
-                    vec![Column::new(
-                        "0".to_string(),
-                        vec![Value::test_int(1), Value::test_int(2), Value::test_int(2)],
-                    )],
-                    None,
-                )
-                .expect("simple df for test should not fail")
-                .into_value(Span::test_data()),
-            ),
-        }]
+        vec![
+            Example {
+                description: "Shifts the values by a given period",
+                example: "[1 2 2 3 3] | polars into-df | polars shift 2 | polars drop-nulls",
+                result: Some(
+                    NuDataFrame::try_from_columns(
+                        vec![Column::new(
+                            "0".to_string(),
+                            vec![Value::test_int(1), Value::test_int(2), Value::test_int(2)],
+                        )],
+                        None,
+                    )
+                    .expect("simple df for test should not fail")
+                    .into_value(Span::test_data()),
+                ),
+            },
+            Example {
+                description: "Shifts the values by a given period",
+                example: "[1 2 2 3 3] | polars into-lazy | polars shift 2 --fill 0 | polars collect",
+                result: Some(
+                    NuDataFrame::try_from_columns(
+                        vec![Column::new(
+                            "0".to_string(),
+                            vec![Value::test_int(0), Value::test_int(0), Value::test_int(1), Value::test_int(2), Value::test_int(2)],
+                        )],
+                        None,
+                    )
+                    .expect("simple df for test should not fail")
+                    .into_value(Span::test_data()),
+                ),
+            },
+        ]
     }
 
     fn run(
