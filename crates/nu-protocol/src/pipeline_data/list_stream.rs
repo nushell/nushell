fef5cbcaf1c5e53d5dcd559fd7b9a5c0cf1d47a2
Ian Manske <ian.manske@pm.me>
2024-05-02 19:44:18 -0400

    Add doc comments

--- crates/nu-protocol/src/pipeline_data/list_stream.rs
+++ crates/nu-protocol/src/pipeline_data/list_stream.rs
@@ -6,50 +6,56 @@ use std::{
 
 pub type ValueIterator = Box<dyn Iterator<Item = Value> + Send + 'static>;
 
-/// A potentially infinite stream of values, optionally with a mean to send a Ctrl-C signal to stop
-/// the stream from continuing.
+/// A potentially infinite, interruptible stream of [`Value`]s.
 ///
-/// In practice, a "stream" here means anything which can be iterated and produce Values as it iterates.
-/// Like other iterators in Rust, observing values from this stream will drain the items as you view them
-/// and the stream cannot be replayed.
+/// In practice, a "stream" here means anything which can be iterated and produces Values.
+/// Like other iterators in Rust, observing values from this stream will drain the items
+/// as you view them and the stream cannot be replayed.
 pub struct ListStream {
     stream: ValueIterator,
     span: Span,
 }
 
 impl ListStream {
+    /// Create a new [`ListStream`] from a [`Value`] `Iterator`.
     pub fn new(
-        stream: impl Iterator<Item = Value> + Send + 'static,
+        iter: impl Iterator<Item = Value> + Send + 'static,
         span: Span,
         interrupt: Option<Arc<AtomicBool>>,
     ) -> Self {
         Self {
-            stream: Box::new(Interrupt::new(stream, interrupt)),
+            stream: Box::new(Interrupt::new(iter, interrupt)),
             span,
         }
     }
 
+    /// Returns the [`Span`] associated with this [`ListStream`].
     pub fn span(&self) -> Span {
         self.span
     }
 
+    /// Convert a [`ListStream`] into its inner [`Value`] `Iterator`.
     pub fn into_inner(self) -> ValueIterator {
         self.stream
     }
 
+    /// Converts each value in a [`ListStream`] into a string and then joins the strings together
+    /// using the given separator.
     pub fn into_string(self, separator: &str, config: &Config) -> String {
         self.into_iter()
-            .map(|x: Value| x.to_expanded_string(", ", config))
+            .map(|val| val.to_expanded_string(", ", config))
             .collect::<Vec<String>>()
             .join(separator)
     }
 
+    /// Collect the values of a [`ListStream`] into a list [`Value`].
     pub fn into_value(self) -> Value {
         Value::list(self.stream.collect(), self.span)
     }
 
+    /// Consume all values in the stream, returning an error if any of the values is a `Value::Error`.
     pub fn drain(self) -> Result<(), ShellError> {
-        for next in self.into_iter() {
+        for next in self {
             if let Value::Error { error, .. } = next {
                 return Err(*error);
             }
@@ -57,13 +63,18 @@ impl ListStream {
         Ok(())
     }
 
-    pub fn map(self, mapping: impl FnMut(Value) -> Value + Send + 'static) -> Self {
-        Self {
-            stream: Box::new(self.stream.map(mapping)),
-            span: self.span,
-        }
-    }
-
+    /// Modify the inner iterator of a [`ListStream`] using a function.
+    ///
+    /// This can be used to call any number of standard iterator functions on the [`ListStream`].
+    /// E.g., `take`, `filter`, `step_by`, and more.
+    ///
+    /// ```
+    /// use nu_protocol::{ListStream, Span, Value};
+    ///
+    /// let span = Span::unknown();
+    /// let stream = ListStream::new(std::iter::repeat(Value::int(0, span)), span, None);
+    /// let new_stream = stream.modify(|iter| iter.take(100));
+    /// ```
     pub fn modify<I>(self, f: impl FnOnce(ValueIterator) -> I) -> Self
     where
         I: Iterator<Item = Value> + Send + 'static,
@@ -72,6 +83,12 @@ impl ListStream {
             stream: Box::new(f(self.stream)),
             span: self.span,
         }
+    }
+
+    /// Create a new [`ListStream`] whose values are the results of applying the given function
+    /// to each of the values in the original [`ListStream`].
+    pub fn map(self, mapping: impl FnMut(Value) -> Value + Send + 'static) -> Self {
+        self.modify(|iter| iter.map(mapping))
     }
 }
 
