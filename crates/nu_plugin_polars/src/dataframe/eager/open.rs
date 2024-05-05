ea2aa29ccbd53c93543ffa2004192ced43b09e7d
Jack Wright <jack.wright@disqo.com>
2024-05-05 10:59:55 -0700

    "made polars open as lazy as possible"

--- crates/nu_plugin_polars/src/dataframe/eager/open.rs
+++ crates/nu_plugin_polars/src/dataframe/eager/open.rs
@@ -4,6 +4,7 @@ use crate::{
     PolarsPlugin,
 };
 use nu_path::expand_path_with;
+use polars_lazy::frame::LazyJsonLineReader;
 
 use super::super::values::NuDataFrame;
 use nu_plugin::PluginCommand;
@@ -19,8 +20,8 @@ use std::{
 };
 
 use polars::prelude::{
-    CsvEncoding, CsvReader, IpcReader, JsonFormat, JsonReader, LazyCsvReader, LazyFileListReader,
-    LazyFrame, ParquetReader, ScanArgsIpc, ScanArgsParquet, SerReader,
+    col, Expr, JsonReader, LazyCsvReader, LazyFileListReader, LazyFrame, ScanArgsIpc,
+    ScanArgsParquet, SerReader,
 };
 
 use polars_io::{avro::AvroReader, prelude::ParallelStrategy, HiveOptions};
@@ -46,7 +47,6 @@ impl PluginCommand for OpenDataFrame {
                 SyntaxShape::Filepath,
                 "file path to load values from",
             )
-            .switch("lazy", "creates a lazy dataframe", Some('l'))
             .named(
                 "type",
                 SyntaxShape::String,
@@ -161,63 +161,39 @@ fn from_parquet(
     engine: &nu_plugin::EngineInterface,
     call: &nu_plugin::EvaluatedCall,
     file_path: &Path,
-    file_span: Span,
+    _file_span: Span,
 ) -> Result<Value, ShellError> {
-    if call.has_flag("lazy")? {
-        let file: String = call.req(0)?;
-        let args = ScanArgsParquet {
-            n_rows: None,
-            cache: true,
-            parallel: ParallelStrategy::Auto,
-            rechunk: false,
-            row_index: None,
-            low_memory: false,
-            cloud_options: None,
-            use_statistics: false,
-            hive_options: HiveOptions::default(),
-        };
+    let args = ScanArgsParquet {
+        n_rows: None,
+        cache: true,
+        parallel: ParallelStrategy::Auto,
+        rechunk: false,
+        row_index: None,
+        low_memory: false,
+        cloud_options: None,
+        use_statistics: false,
+        hive_options: HiveOptions::default(),
+    };
 
-        let df: NuLazyFrame = LazyFrame::scan_parquet(file, args)
-            .map_err(|e| ShellError::GenericError {
-                error: "Parquet reader error".into(),
-                msg: format!("{e:?}"),
-                span: Some(call.head),
-                help: None,
-                inner: vec![],
-            })?
-            .into();
+    let maybe_columns: Option<Vec<Expr>> = call
+        .get_flag::<Vec<String>>("columns")?
+        .map(|cols| cols.iter().map(|s| col(s)).collect());
 
-        df.cache_and_to_value(plugin, engine, call.head)
-    } else {
-        let columns: Option<Vec<String>> = call.get_flag("columns")?;
-
-        let r = File::open(file_path).map_err(|e| ShellError::GenericError {
-            error: "Error opening file".into(),
-            msg: e.to_string(),
-            span: Some(file_span),
+    let mut polars_df =
+        LazyFrame::scan_parquet(file_path, args).map_err(|e| ShellError::GenericError {
+            error: "Parquet reader error".into(),
+            msg: format!("{e:?}"),
+            span: Some(call.head),
             help: None,
             inner: vec![],
         })?;
-        let reader = ParquetReader::new(r);
 
-        let reader = match columns {
-            None => reader,
-            Some(columns) => reader.with_columns(Some(columns)),
-        };
-
-        let df: NuDataFrame = reader
-            .finish()
-            .map_err(|e| ShellError::GenericError {
-                error: "Parquet reader error".into(),
-                msg: format!("{e:?}"),
-                span: Some(call.head),
-                help: None,
-                inner: vec![],
-            })?
-            .into();
-
-        df.cache_and_to_value(plugin, engine, call.head)
+    if let Some(columns) = maybe_columns {
+        polars_df = polars_df.select(columns);
     }
+
+    let df: NuLazyFrame = polars_df.into();
+    df.cache_and_to_value(plugin, engine, call.head)
 }
 
 fn from_avro(
@@ -262,60 +238,36 @@ fn from_ipc(
     engine: &nu_plugin::EngineInterface,
     call: &nu_plugin::EvaluatedCall,
     file_path: &Path,
-    file_span: Span,
+    _file_span: Span,
 ) -> Result<Value, ShellError> {
-    if call.has_flag("lazy")? {
-        let file: String = call.req(0)?;
-        let args = ScanArgsIpc {
-            n_rows: None,
-            cache: true,
-            rechunk: false,
-            row_index: None,
-            memory_map: true,
-            cloud_options: None,
-        };
+    let args = ScanArgsIpc {
+        n_rows: None,
+        cache: true,
+        rechunk: false,
+        row_index: None,
+        memory_map: true,
+        cloud_options: None,
+    };
 
-        let df: NuLazyFrame = LazyFrame::scan_ipc(file, args)
-            .map_err(|e| ShellError::GenericError {
-                error: "IPC reader error".into(),
-                msg: format!("{e:?}"),
-                span: Some(call.head),
-                help: None,
-                inner: vec![],
-            })?
-            .into();
+    let maybe_columns: Option<Vec<Expr>> = call
+        .get_flag::<Vec<String>>("columns")?
+        .map(|cols| cols.iter().map(|s| col(s)).collect());
 
-        df.cache_and_to_value(plugin, engine, call.head)
-    } else {
-        let columns: Option<Vec<String>> = call.get_flag("columns")?;
-
-        let r = File::open(file_path).map_err(|e| ShellError::GenericError {
-            error: "Error opening file".into(),
-            msg: e.to_string(),
-            span: Some(file_span),
+    let mut polars_df =
+        LazyFrame::scan_ipc(file_path, args).map_err(|e| ShellError::GenericError {
+            error: "IPC reader error".into(),
+            msg: format!("{e:?}"),
+            span: Some(call.head),
             help: None,
             inner: vec![],
         })?;
-        let reader = IpcReader::new(r);
 
-        let reader = match columns {
-            None => reader,
-            Some(columns) => reader.with_columns(Some(columns)),
-        };
-
-        let df: NuDataFrame = reader
-            .finish()
-            .map_err(|e| ShellError::GenericError {
-                error: "IPC reader error".into(),
-                msg: format!("{e:?}"),
-                span: Some(call.head),
-                help: None,
-                inner: vec![],
-            })?
-            .into();
-
-        df.cache_and_to_value(plugin, engine, call.head)
+    if let Some(columns) = maybe_columns {
+        polars_df = polars_df.select(columns);
     }
+
+    let df: NuLazyFrame = polars_df.into();
+    df.cache_and_to_value(plugin, engine, call.head)
 }
 
 fn from_json(
@@ -364,32 +316,21 @@ fn from_jsonl(
     engine: &nu_plugin::EngineInterface,
     call: &nu_plugin::EvaluatedCall,
     file_path: &Path,
-    file_span: Span,
+    _file_span: Span,
 ) -> Result<Value, ShellError> {
     let infer_schema: Option<usize> = call.get_flag("infer-schema")?;
     let maybe_schema = call
         .get_flag("schema")?
         .map(|schema| NuSchema::try_from(&schema))
         .transpose()?;
-    let file = File::open(file_path).map_err(|e| ShellError::GenericError {
-        error: "Error opening file".into(),
-        msg: e.to_string(),
-        span: Some(file_span),
-        help: None,
-        inner: vec![],
-    })?;
 
-    let buf_reader = BufReader::new(file);
-    let reader = JsonReader::new(buf_reader)
-        .with_json_format(JsonFormat::JsonLines)
-        .infer_schema_len(infer_schema);
+    let maybe_columns: Option<Vec<Expr>> = call
+        .get_flag::<Vec<String>>("columns")?
+        .map(|cols| cols.iter().map(|s| col(s)).collect());
 
-    let reader = match maybe_schema {
-        Some(schema) => reader.with_schema(schema.into()),
-        None => reader,
-    };
-
-    let df: NuDataFrame = reader
+    let mut polars_df = LazyJsonLineReader::new(file_path)
+        .with_infer_schema_length(infer_schema)
+        .with_schema(maybe_schema.map(|s| s.into()))
         .finish()
         .map_err(|e| ShellError::GenericError {
             error: "Json lines reader error".into(),
@@ -397,9 +338,13 @@ fn from_jsonl(
             span: Some(call.head),
             help: None,
             inner: vec![],
-        })?
-        .into();
+        })?;
 
+    if let Some(columns) = maybe_columns {
+        polars_df = polars_df.select(columns);
+    }
+
+    let df: NuLazyFrame = polars_df.into();
     df.cache_and_to_value(plugin, engine, call.head)
 }
 
@@ -408,137 +353,73 @@ fn from_csv(
     engine: &nu_plugin::EngineInterface,
     call: &nu_plugin::EvaluatedCall,
     file_path: &Path,
-    file_span: Span,
+    _file_span: Span,
 ) -> Result<Value, ShellError> {
     let delimiter: Option<Spanned<String>> = call.get_flag("delimiter")?;
     let no_header: bool = call.has_flag("no-header")?;
     let infer_schema: Option<usize> = call.get_flag("infer-schema")?;
     let skip_rows: Option<usize> = call.get_flag("skip-rows")?;
-    let columns: Option<Vec<String>> = call.get_flag("columns")?;
+    let maybe_columns: Option<Vec<Expr>> = call
+        .get_flag::<Vec<String>>("columns")?
+        .map(|cols| cols.iter().map(|s| col(s)).collect());
 
     let maybe_schema = call
         .get_flag("schema")?
         .map(|schema| NuSchema::try_from(&schema))
         .transpose()?;
 
-    if call.has_flag("lazy")? {
-        let csv_reader = LazyCsvReader::new(file_path);
+    let csv_reader = LazyCsvReader::new(file_path);
 
-        let csv_reader = match delimiter {
-            None => csv_reader,
-            Some(d) => {
-                if d.item.len() != 1 {
-                    return Err(ShellError::GenericError {
-                        error: "Incorrect delimiter".into(),
-                        msg: "Delimiter has to be one character".into(),
-                        span: Some(d.span),
-                        help: None,
-                        inner: vec![],
-                    });
-                } else {
-                    let delimiter = match d.item.chars().next() {
-                        Some(d) => d as u8,
-                        None => unreachable!(),
-                    };
-                    csv_reader.with_separator(delimiter)
-                }
+    let csv_reader = match delimiter {
+        None => csv_reader,
+        Some(d) => {
+            if d.item.len() != 1 {
+                return Err(ShellError::GenericError {
+                    error: "Incorrect delimiter".into(),
+                    msg: "Delimiter has to be one character".into(),
+                    span: Some(d.span),
+                    help: None,
+                    inner: vec![],
+                });
+            } else {
+                let delimiter = match d.item.chars().next() {
+                    Some(d) => d as u8,
+                    None => unreachable!(),
+                };
+                csv_reader.with_separator(delimiter)
             }
-        };
+        }
+    };
 
-        let csv_reader = csv_reader.has_header(!no_header);
+    let csv_reader = csv_reader.has_header(!no_header);
 
-        let csv_reader = match maybe_schema {
-            Some(schema) => csv_reader.with_schema(Some(schema.into())),
-            None => csv_reader,
-        };
+    let csv_reader = match maybe_schema {
+        Some(schema) => csv_reader.with_schema(Some(schema.into())),
+        None => csv_reader,
+    };
 
-        let csv_reader = match infer_schema {
-            None => csv_reader,
-            Some(r) => csv_reader.with_infer_schema_length(Some(r)),
-        };
+    let csv_reader = match infer_schema {
+        None => csv_reader,
+        Some(r) => csv_reader.with_infer_schema_length(Some(r)),
+    };
 
-        let csv_reader = match skip_rows {
-            None => csv_reader,
-            Some(r) => csv_reader.with_skip_rows(r),
-        };
+    let csv_reader = match skip_rows {
+        None => csv_reader,
+        Some(r) => csv_reader.with_skip_rows(r),
+    };
 
-        let df: NuLazyFrame = csv_reader
-            .finish()
-            .map_err(|e| ShellError::GenericError {
-                error: "Parquet reader error".into(),
-                msg: format!("{e:?}"),
-                span: Some(call.head),
-                help: None,
-                inner: vec![],
-            })?
-            .into();
+    let mut polars_df = csv_reader.finish().map_err(|e| ShellError::GenericError {
+        error: "CSV reader error".into(),
+        msg: format!("{e:?}"),
+        span: Some(call.head),
+        help: None,
+        inner: vec![],
+    })?;
 
-        df.cache_and_to_value(plugin, engine, call.head)
-    } else {
-        let csv_reader = CsvReader::from_path(file_path)
-            .map_err(|e| ShellError::GenericError {
-                error: "Error creating CSV reader".into(),
-                msg: e.to_string(),
-                span: Some(file_span),
-                help: None,
-                inner: vec![],
-            })?
-            .with_encoding(CsvEncoding::LossyUtf8);
-
-        let csv_reader = match delimiter {
-            None => csv_reader,
-            Some(d) => {
-                if d.item.len() != 1 {
-                    return Err(ShellError::GenericError {
-                        error: "Incorrect delimiter".into(),
-                        msg: "Delimiter has to be one character".into(),
-                        span: Some(d.span),
-                        help: None,
-                        inner: vec![],
-                    });
-                } else {
-                    let delimiter = match d.item.chars().next() {
-                        Some(d) => d as u8,
-                        None => unreachable!(),
-                    };
-                    csv_reader.with_separator(delimiter)
-                }
-            }
-        };
-
-        let csv_reader = csv_reader.has_header(!no_header);
-
-        let csv_reader = match maybe_schema {
-            Some(schema) => csv_reader.with_schema(Some(schema.into())),
-            None => csv_reader,
-        };
-
-        let csv_reader = match infer_schema {
-            None => csv_reader,
-            Some(r) => csv_reader.infer_schema(Some(r)),
-        };
-
-        let csv_reader = match skip_rows {
-            None => csv_reader,
-            Some(r) => csv_reader.with_skip_rows(r),
-        };
-
-        let csv_reader = match columns {
-            None => csv_reader,
-            Some(columns) => csv_reader.with_columns(Some(columns)),
-        };
-
-        let df: NuDataFrame = csv_reader
-            .finish()
-            .map_err(|e| ShellError::GenericError {
-                error: "Parquet reader error".into(),
-                msg: format!("{e:?}"),
-                span: Some(call.head),
-                help: None,
-                inner: vec![],
-            })?
-            .into();
-
-        df.cache_and_to_value(plugin, engine, call.head)
+    if let Some(columns) = maybe_columns {
+        polars_df = polars_df.select(columns);
     }
+
+    let df: NuLazyFrame = polars_df.into();
+    df.cache_and_to_value(plugin, engine, call.head)
 }
