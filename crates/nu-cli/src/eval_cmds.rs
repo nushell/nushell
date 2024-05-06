6ed98ef9823bd9601fc5e5d56e98028afdfc1a4c
Ian Manske <ian.manske@pm.me>
2024-05-06 15:52:08 -0400

    Simplify printing

--- crates/nu-cli/src/eval_cmds.rs
+++ crates/nu-cli/src/eval_cmds.rs
@@ -7,7 +7,7 @@ use nu_protocol::{
     engine::{EngineState, Stack, StateWorkingSet},
     report_error, PipelineData, ShellError, Spanned, Value,
 };
-use std::{io::Write, sync::Arc};
+use std::sync::Arc;
 
 /// Run a command (or commands) given to us by the user
 pub fn evaluate_commands(
@@ -50,69 +50,22 @@ pub fn evaluate_commands(
     // Run the block
     let data = eval_block::<WithoutDebug>(engine_state, stack, &block, input)?;
 
+    if let PipelineData::Value(Value::Error { error, .. }, ..) = data {
+        return Err(*error);
+    }
+
     if let Some(t_mode) = table_mode {
         Arc::make_mut(&mut engine_state.config).table_mode =
             t_mode.coerce_str()?.parse().unwrap_or_default();
     }
-    let exit_code = print_table_or_error(engine_state, stack, data, no_newline);
+
+    // We don't need to do anything special to print a table because print() handles it
+    let exit_code = data.print(engine_state, stack, no_newline, false)?;
+    if exit_code != 0 {
+        std::process::exit(exit_code as i32);
+    }
 
     info!("evaluate {}:{}:{}", file!(), line!(), column!());
 
-    match exit_code {
-        None | Some(0) => Ok(()),
-        Some(exit_code) => std::process::exit(exit_code as i32),
-    }
-}
-
-pub(crate) fn print_table_or_error(
-    engine_state: &mut EngineState,
-    stack: &mut Stack,
-    mut pipeline_data: PipelineData,
-    no_newline: bool,
-) -> Option<i64> {
-    let exit_code = match &mut pipeline_data {
-        PipelineData::ExternalStream { exit_code, .. } => exit_code.take(),
-        _ => None,
-    };
-
-    if let PipelineData::Value(Value::Error { error, .. }, ..) = &pipeline_data {
-        let working_set = StateWorkingSet::new(engine_state);
-        report_error(&working_set, &**error);
-        std::process::exit(1);
-    }
-
-    // We don't need to do anything special to print a table because print() handles it
-    print_or_exit(pipeline_data, engine_state, stack, no_newline);
-
-    // Make sure everything has finished
-    if let Some(exit_code) = exit_code {
-        let mut exit_code: Vec<_> = exit_code.into_iter().collect();
-        exit_code
-            .pop()
-            .and_then(|last_exit_code| match last_exit_code {
-                Value::Int { val: code, .. } => Some(code),
-                _ => None,
-            })
-    } else {
-        None
-    }
-}
-
-fn print_or_exit(
-    pipeline_data: PipelineData,
-    engine_state: &EngineState,
-    stack: &mut Stack,
-    no_newline: bool,
-) {
-    let result = pipeline_data.print(engine_state, stack, no_newline, false);
-
-    let _ = std::io::stdout().flush();
-    let _ = std::io::stderr().flush();
-
-    if let Err(error) = result {
-        let working_set = StateWorkingSet::new(engine_state);
-        report_error(&working_set, &error);
-        let _ = std::io::stderr().flush();
-        std::process::exit(1);
-    }
+    Ok(())
 }
