aaec16306146ede692fdfaf583f602af1a79bdec
WindSoilder <WindSoilder@outlook.com>
2024-05-17 07:06:58 +0800

    revert for easy merge

--- crates/nu-cmd-extra/tests/commands/bytes/starts_with.rs
+++ crates/nu-cmd-extra/tests/commands/bytes/starts_with.rs
@@ -59,7 +59,7 @@ fn long_stream_binary() {
 fn long_stream_binary_overflow() {
     // .. ranges are inclusive..inclusive, so we don't need to +1 to check for an overflow
     let actual = nu!(r#"
-            nu --testbin repeater (0x[01]) 32768 | bytes starts-with (0..=32768 | each {|| 0x[01] } | bytes collect)
+            nu --testbin repeater (0x[01]) 32768 | bytes starts-with (0..32768 | each {|| 0x[01] } | bytes collect)
         "#);
 
     assert_eq!(actual.out, "false");
@@ -69,7 +69,7 @@ fn long_stream_binary_overflow() {
 fn long_stream_binary_exact() {
     // ranges are inclusive..inclusive, so we don't need to +1 to check for an overflow
     let actual = nu!(r#"
-            nu --testbin repeater (0x[01020304]) 8192 | bytes starts-with (0..8192 | each {|| 0x[01020304] } | bytes collect)
+            nu --testbin repeater (0x[01020304]) 8192 | bytes starts-with (0..<8192 | each {|| 0x[01020304] } | bytes collect)
         "#);
 
     assert_eq!(actual.out, "true");
@@ -79,7 +79,7 @@ fn long_stream_binary_exact() {
 fn long_stream_string_exact() {
     // ranges are inclusive..inclusive, so we don't need to +1 to check for an overflow
     let actual = nu!(r#"
-            nu --testbin repeater hell 8192 | bytes starts-with (0..8192 | each {|| "hell" | into binary } | bytes collect)
+            nu --testbin repeater hell 8192 | bytes starts-with (0..<8192 | each {|| "hell" | into binary } | bytes collect)
         "#);
 
     assert_eq!(actual.out, "true");
@@ -89,8 +89,8 @@ fn long_stream_string_exact() {
 fn long_stream_mixed_exact() {
     // ranges are inclusive..inclusive, so we don't need to +1 to check for an overflow
     let actual = nu!(r#"
-            let binseg = (0..2048 | each {|| 0x[003d9fbf] } | bytes collect)
-            let strseg = (0..2048 | each {|| "hell" | into binary } | bytes collect)
+            let binseg = (0..<2048 | each {|| 0x[003d9fbf] } | bytes collect)
+            let strseg = (0..<2048 | each {|| "hell" | into binary } | bytes collect)
 
             nu --testbin repeat_bytes 003d9fbf 2048 68656c6c 2048 | bytes starts-with (bytes build $binseg $strseg)
         "#);
@@ -106,8 +106,8 @@ fn long_stream_mixed_exact() {
 fn long_stream_mixed_overflow() {
     // ranges are inclusive..inclusive, so we don't need to +1 to check for an overflow
     let actual = nu!(r#"
-            let binseg = (0..2048 | each {|| 0x[003d9fbf] } | bytes collect)
-            let strseg = (0..2048 | each {|| "hell" | into binary } | bytes collect)
+            let binseg = (0..<2048 | each {|| 0x[003d9fbf] } | bytes collect)
+            let strseg = (0..<2048 | each {|| "hell" | into binary } | bytes collect)
 
             nu --testbin repeat_bytes 003d9fbf 2048 68656c6c 2048 | bytes starts-with (bytes build $binseg $strseg 0x[01])
         "#);
