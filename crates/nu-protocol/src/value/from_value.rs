5965fbc93cfaaf8880d65f0528a0d67bffda3a4a
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2024-05-23 12:17:44 +0200

    replace manual FromValue impls for Spanned with a blanket impl

--- crates/nu-protocol/src/value/from_value.rs
+++ crates/nu-protocol/src/value/from_value.rs
@@ -23,21 +23,13 @@ impl FromValue for Value {
     }
 }
 
-impl FromValue for Spanned<i64> {
+impl<T> FromValue for Spanned<T> where T: FromValue {
     fn from_value(v: Value) -> Result<Self, ShellError> {
         let span = v.span();
-        match v {
-            Value::Int { val, .. } => Ok(Spanned { item: val, span }),
-            Value::Filesize { val, .. } => Ok(Spanned { item: val, span }),
-            Value::Duration { val, .. } => Ok(Spanned { item: val, span }),
-
-            v => Err(ShellError::CantConvert {
-                to_type: "int".into(),
-                from_type: v.get_type().to_string(),
-                span: v.span(),
-                help: None,
-            }),
-        }
+        Ok(Spanned {
+            item: T::from_value(v)?,
+            span
+        })
     }
 }
 
@@ -58,26 +50,6 @@ impl FromValue for i64 {
     }
 }
 
-impl FromValue for Spanned<f64> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        match v {
-            Value::Int { val, .. } => Ok(Spanned {
-                item: val as f64,
-                span,
-            }),
-            Value::Float { val, .. } => Ok(Spanned { item: val, span }),
-
-            v => Err(ShellError::CantConvert {
-                to_type: "float".into(),
-                from_type: v.get_type().to_string(),
-                span: v.span(),
-                help: None,
-            }),
-        }
-    }
-}
-
 impl FromValue for f64 {
     fn from_value(v: Value) -> Result<Self, ShellError> {
         match v {
@@ -85,51 +57,6 @@ impl FromValue for f64 {
             Value::Int { val, .. } => Ok(val as f64),
             v => Err(ShellError::CantConvert {
                 to_type: "float".into(),
-                from_type: v.get_type().to_string(),
-                span: v.span(),
-                help: None,
-            }),
-        }
-    }
-}
-
-impl FromValue for Spanned<usize> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        match v {
-            Value::Int { val, .. } => {
-                if val.is_negative() {
-                    Err(ShellError::NeedsPositiveValue { span })
-                } else {
-                    Ok(Spanned {
-                        item: val as usize,
-                        span,
-                    })
-                }
-            }
-            Value::Filesize { val, .. } => {
-                if val.is_negative() {
-                    Err(ShellError::NeedsPositiveValue { span })
-                } else {
-                    Ok(Spanned {
-                        item: val as usize,
-                        span,
-                    })
-                }
-            }
-            Value::Duration { val, .. } => {
-                if val.is_negative() {
-                    Err(ShellError::NeedsPositiveValue { span })
-                } else {
-                    Ok(Spanned {
-                        item: val as usize,
-                        span,
-                    })
-                }
-            }
-
-            v => Err(ShellError::CantConvert {
-                to_type: "non-negative int".into(),
                 from_type: v.get_type().to_string(),
                 span: v.span(),
                 help: None,
@@ -190,27 +117,6 @@ impl FromValue for String {
     }
 }
 
-impl FromValue for Spanned<String> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        Ok(Spanned {
-            item: match v {
-                Value::CellPath { val, .. } => val.to_string(),
-                Value::String { val, .. } => val,
-                v => {
-                    return Err(ShellError::CantConvert {
-                        to_type: "string".into(),
-                        from_type: v.get_type().to_string(),
-                        span: v.span(),
-                        help: None,
-                    })
-                }
-            },
-            span,
-        })
-    }
-}
-
 impl FromValue for NuGlob {
     fn from_value(v: Value) -> Result<Self, ShellError> {
         // FIXME: we may want to fail a little nicer here
@@ -235,38 +141,6 @@ impl FromValue for NuGlob {
                 help: None,
             }),
         }
-    }
-}
-
-impl FromValue for Spanned<NuGlob> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        Ok(Spanned {
-            item: match v {
-                Value::CellPath { val, .. } => NuGlob::Expand(val.to_string()),
-                Value::String { val, .. } => NuGlob::DoNotExpand(val),
-                Value::Glob {
-                    val,
-                    no_expand: quoted,
-                    ..
-                } => {
-                    if quoted {
-                        NuGlob::DoNotExpand(val)
-                    } else {
-                        NuGlob::Expand(val)
-                    }
-                }
-                v => {
-                    return Err(ShellError::CantConvert {
-                        to_type: "string".into(),
-                        from_type: v.get_type().to_string(),
-                        span: v.span(),
-                        help: None,
-                    })
-                }
-            },
-            span,
-        })
     }
 }
 
@@ -402,40 +276,10 @@ impl FromValue for bool {
     }
 }
 
-impl FromValue for Spanned<bool> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        match v {
-            Value::Bool { val, .. } => Ok(Spanned { item: val, span }),
-            v => Err(ShellError::CantConvert {
-                to_type: "bool".into(),
-                from_type: v.get_type().to_string(),
-                span: v.span(),
-                help: None,
-            }),
-        }
-    }
-}
-
 impl FromValue for DateTime<FixedOffset> {
     fn from_value(v: Value) -> Result<Self, ShellError> {
         match v {
             Value::Date { val, .. } => Ok(val),
-            v => Err(ShellError::CantConvert {
-                to_type: "date".into(),
-                from_type: v.get_type().to_string(),
-                span: v.span(),
-                help: None,
-            }),
-        }
-    }
-}
-
-impl FromValue for Spanned<DateTime<FixedOffset>> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        match v {
-            Value::Date { val, .. } => Ok(Spanned { item: val, span }),
             v => Err(ShellError::CantConvert {
                 to_type: "date".into(),
                 from_type: v.get_type().to_string(),
@@ -460,45 +304,11 @@ impl FromValue for Range {
     }
 }
 
-impl FromValue for Spanned<Range> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        match v {
-            Value::Range { val, .. } => Ok(Spanned { item: *val, span }),
-            v => Err(ShellError::CantConvert {
-                to_type: "range".into(),
-                from_type: v.get_type().to_string(),
-                span: v.span(),
-                help: None,
-            }),
-        }
-    }
-}
-
 impl FromValue for Vec<u8> {
     fn from_value(v: Value) -> Result<Self, ShellError> {
         match v {
             Value::Binary { val, .. } => Ok(val),
             Value::String { val, .. } => Ok(val.into_bytes()),
-            v => Err(ShellError::CantConvert {
-                to_type: "binary data".into(),
-                from_type: v.get_type().to_string(),
-                span: v.span(),
-                help: None,
-            }),
-        }
-    }
-}
-
-impl FromValue for Spanned<Vec<u8>> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        match v {
-            Value::Binary { val, .. } => Ok(Spanned { item: val, span }),
-            Value::String { val, .. } => Ok(Spanned {
-                item: val.into_bytes(),
-                span,
-            }),
             v => Err(ShellError::CantConvert {
                 to_type: "binary data".into(),
                 from_type: v.get_type().to_string(),
@@ -560,21 +370,6 @@ impl FromValue for Closure {
     fn from_value(v: Value) -> Result<Self, ShellError> {
         match v {
             Value::Closure { val, .. } => Ok(*val),
-            v => Err(ShellError::CantConvert {
-                to_type: "Closure".into(),
-                from_type: v.get_type().to_string(),
-                span: v.span(),
-                help: None,
-            }),
-        }
-    }
-}
-
-impl FromValue for Spanned<Closure> {
-    fn from_value(v: Value) -> Result<Self, ShellError> {
-        let span = v.span();
-        match v {
-            Value::Closure { val, .. } => Ok(Spanned { item: *val, span }),
             v => Err(ShellError::CantConvert {
                 to_type: "Closure".into(),
                 from_type: v.get_type().to_string(),
