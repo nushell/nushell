607f5fc94748f4196343d9660e1d235b0304b605
Ian Manske <ian.manske@pm.me>
2024-06-01 15:31:31 -0400

    Add docs for path forms

--- crates/nu-path/src/form.rs
+++ crates/nu-path/src/form.rs
@@ -1,42 +1,32 @@
 mod private {
+    // This trait should not be extended by external crates in order to uphold safety guarantees.
+    // As such, this trait is put inside a private module to prevent external impls.
+    // This ensures that all possible [`PathForm`]s can only be defined here and will:
+    // - be zero sized
+    // - have a no-op [`Drop`] implementation
     pub trait Sealed {}
 }
 
+/// A marker trait for the different kinds of path forms.
+/// Each form has its own invariants that are guaranteed be upheld.
+/// The list of path forms are:
+/// - [`Any`]: a path with no invariants. It may be a relative or an absolute path.
+/// - [`Relative`]: a strictly relative path.
+/// - [`Absolute`]: a strictly absolute path.
+/// - [`Canonical`]: a path that must be in canonicalized form.
 pub trait PathForm: private::Sealed {}
-
-pub trait MaybeRelative: PathForm {}
-
-pub trait MaybeAbsolute: PathForm {}
-
-pub trait IsAbsolute: PathForm {}
-
-pub trait PathCast<Form: PathForm>: PathForm {}
-
-impl<Form: PathForm> PathCast<Form> for Form {}
-
-pub trait PathJoin: PathForm {
-    type Output: PathForm;
-}
-
-pub trait PathPush: PathForm {}
-
-pub trait PathMut: PathForm {}
+impl PathForm for Any {}
+impl PathForm for Relative {}
+impl PathForm for Absolute {}
+impl PathForm for Canonical {}
 
 /// A path whose form is unknown. It could be a relative, absolute, or canonical path.
 ///
-///
+/// The path is not guaranteed to be normalized. It may contain unresolved symlinks,
+/// trailing slashes, dot components (`..` or `.`), and duplicate path separators.
 pub struct Any;
 
 impl private::Sealed for Any {}
-
-impl PathForm for Any {}
-impl MaybeRelative for Any {}
-impl MaybeAbsolute for Any {}
-impl PathJoin for Any {
-    type Output = Self;
-}
-impl PathPush for Any {}
-impl PathMut for Any {}
 
 /// A strictly relative path.
 ///
@@ -45,13 +35,6 @@ impl PathMut for Any {}
 pub struct Relative;
 
 impl private::Sealed for Relative {}
-impl PathForm for Relative {}
-impl PathCast<Any> for Relative {}
-impl MaybeRelative for Relative {}
-impl PathJoin for Relative {
-    type Output = Any;
-}
-impl PathMut for Relative {}
 
 /// An absolute path.
 ///
@@ -60,15 +43,6 @@ impl PathMut for Relative {}
 pub struct Absolute;
 
 impl private::Sealed for Absolute {}
-impl PathForm for Absolute {}
-impl PathCast<Any> for Absolute {}
-impl MaybeAbsolute for Absolute {}
-impl IsAbsolute for Absolute {}
-impl PathJoin for Absolute {
-    type Output = Self;
-}
-impl PathPush for Absolute {}
-impl PathMut for Absolute {}
 
 // A canonical path.
 //
@@ -76,11 +50,90 @@ impl PathMut for Absolute {}
 pub struct Canonical;
 
 impl private::Sealed for Canonical {}
-impl PathForm for Canonical {}
+
+/// A marker trait for [`PathForm`]s that may be relative paths.
+/// This includes only the [`Any`] and [`Relative`] path forms.
+///
+/// [`push`](crate::PathBuf::push) and [`join`](crate::Path::join)
+/// operations only support [`MaybeRelative`] path forms as input.
+pub trait MaybeRelative: PathForm {}
+impl MaybeRelative for Any {}
+impl MaybeRelative for Relative {}
+
+/// A marker trait for [`PathForm`]s that may be absolute paths.
+/// This includes the [`Any`], [`Absolute`], and [`Canonical`] path forms.
+pub trait MaybeAbsolute: PathForm {}
+impl MaybeAbsolute for Any {}
+impl MaybeAbsolute for Absolute {}
+impl MaybeAbsolute for Canonical {}
+
+/// A marker trait for [`PathForm`]s that are absolute paths.
+/// This includes only the [`Absolute`] and [`Canonical`] path forms.
+///
+/// Only [`PathForm`]s that implement this trait can be easily converted to [`std::path::Path`]
+/// or [`std::path::PathBuf`]. This is to encourage/force other Nushell crates to account for
+/// the emulated current working directory, instead of using the [`std::env::current_dir`].
+pub trait IsAbsolute: PathForm {}
+impl IsAbsolute for Absolute {}
+impl IsAbsolute for Canonical {}
+
+/// A marker trait that signifies one [`PathForm`] can be used as or trivially converted to
+/// another [`PathForm`].
+///
+/// The list of possible conversions are:
+/// - [`Relative`], [`Absolute`], or [`Canonical`] into [`Any`].
+/// - [`Canonical`] into [`Absolute`].
+/// - Any form into itself.
+pub trait PathCast<Form: PathForm>: PathForm {}
+impl<Form: PathForm> PathCast<Form> for Form {}
+impl PathCast<Any> for Relative {}
+impl PathCast<Any> for Absolute {}
 impl PathCast<Any> for Canonical {}
 impl PathCast<Absolute> for Canonical {}
-impl MaybeAbsolute for Canonical {}
-impl IsAbsolute for Canonical {}
+
+/// A trait used to specify the output [`PathForm`] of a path join operation.
+///
+/// The output path forms based on the left hand side path form are as follows:
+///
+/// | Left hand side | Output form  |
+/// | --------------:|:------------ |
+/// | [`Any`]        | [`Any`]      |
+/// | [`Relative`]   | [`Any`]      |
+/// | [`Absolute`]   | [`Absolute`] |
+/// | [`Canonical`]  | [`Absolute`] |
+pub trait PathJoin: PathForm {
+    type Output: PathForm;
+}
+impl PathJoin for Any {
+    type Output = Self;
+}
+impl PathJoin for Relative {
+    type Output = Any;
+}
+impl PathJoin for Absolute {
+    type Output = Self;
+}
 impl PathJoin for Canonical {
     type Output = Absolute;
 }
+
+/// A marker trait for [`PathForm`]s that support mutation.
+///
+/// This includes the [`Any`], [`Relative`], and [`Absolute`] path forms.
+/// [`Canonical`] paths do not support mutation,
+/// since unguarded mutation can cause them to no longer be canonical.
+pub trait PathMut: PathForm {}
+impl PathMut for Any {}
+impl PathMut for Relative {}
+impl PathMut for Absolute {}
+
+/// A marker trait for [`PathForm`]s that support pushing [`MaybeRelative`] paths.
+///
+/// This includes only [`Any`] and [`Absolute`] path forms.
+/// Pushing onto a [`Relative`] path could cause it to become [`Absolute`],
+/// which is why they do not support pushing.
+/// In the future, a `push_rel` and/or a `try_push` method could be added as an alternative.
+/// Similarly, [`Canonical`] paths may become uncanonical if a non-canonical path is pushed onto it.
+pub trait PathPush: PathMut {}
+impl PathPush for Any {}
+impl PathPush for Absolute {}
