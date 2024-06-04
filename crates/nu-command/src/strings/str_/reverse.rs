f4df4e080acc9ccf1066bddbd0c9b34fa5518c19
Embers-of-the-Fire <stellarishs@163.com>
2024-06-04 19:40:20 +0800

    add: make `str reverse` const

--- crates/nu-command/src/strings/str_/reverse.rs
+++ crates/nu-command/src/strings/str_/reverse.rs
@@ -1,5 +1,6 @@
 use nu_cmd_base::input_handler::{operate, CellPathOnlyArgs};
 use nu_engine::command_prelude::*;
+use nu_protocol::engine::StateWorkingSet;
 
 #[derive(Clone)]
 pub struct SubCommand;
@@ -37,6 +38,10 @@ impl Command for SubCommand {
         vec!["convert", "inverse", "flip"]
     }
 
+    fn is_const(&self) -> bool {
+        true
+    }
+
     fn run(
         &self,
         engine_state: &EngineState,
@@ -47,6 +52,23 @@ impl Command for SubCommand {
         let cell_paths: Vec<CellPath> = call.rest(engine_state, stack, 0)?;
         let args = CellPathOnlyArgs::from(cell_paths);
         operate(action, args, input, call.head, engine_state.ctrlc.clone())
+    }
+
+    fn run_const(
+        &self,
+        working_set: &StateWorkingSet,
+        call: &Call,
+        input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        let cell_paths: Vec<CellPath> = call.rest_const(working_set, 0)?;
+        let args = CellPathOnlyArgs::from(cell_paths);
+        operate(
+            action,
+            args,
+            input,
+            call.head,
+            working_set.permanent().ctrlc.clone(),
+        )
     }
 
     fn examples(&self) -> Vec<Example> {
