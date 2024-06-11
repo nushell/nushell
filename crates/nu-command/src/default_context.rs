87feee719b7079969e1e4056be5b3477123e80a5
Ian Manske <ian.manske@pm.me>
2024-06-10 22:14:56 -0700

    Remove `which-support` feature (now always enabled)

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -172,7 +172,6 @@ pub fn add_shell_command_context(mut engine_state: EngineState) -> EngineState {
         ))]
         bind_command! { Ps };
 
-        #[cfg(feature = "which-support")]
         bind_command! { Which };
 
         // Strings
