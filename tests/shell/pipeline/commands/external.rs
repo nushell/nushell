634361b2d12e5cc3fecfcfec1f71532914c72d77
Ian Manske <ian.manske@pm.me>
2024-06-13 01:04:12 +0000

    Make `which-support` feature non-optional (#13125)
    # Description
    Removes the `which-support` cargo feature and makes all of its
    feature-gated code enabled by default in all builds. I'm not sure why
    this one command is gated behind a feature. It seems to be a relic of
    older code where we had features for what seems like every command.

--- tests/shell/pipeline/commands/external.rs
+++ tests/shell/pipeline/commands/external.rs
@@ -3,7 +3,6 @@ use nu_test_support::nu;
 use nu_test_support::playground::Playground;
 use pretty_assertions::assert_eq;
 
-#[cfg(feature = "which-support")]
 #[test]
 fn shows_error_for_command_not_found() {
     let actual = nu!("ferris_is_not_here.exe");
@@ -11,7 +10,6 @@ fn shows_error_for_command_not_found() {
     assert!(!actual.err.is_empty());
 }
 
-#[cfg(feature = "which-support")]
 #[test]
 fn shows_error_for_command_not_found_in_pipeline() {
     let actual = nu!("ferris_is_not_here.exe | echo done");
@@ -20,7 +18,6 @@ fn shows_error_for_command_not_found_in_pipeline() {
 }
 
 #[ignore] // jt: we can't test this using the -c workaround currently
-#[cfg(feature = "which-support")]
 #[test]
 fn automatically_change_directory() {
     use nu_test_support::playground::Playground;
