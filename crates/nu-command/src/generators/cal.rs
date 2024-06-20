818082d139a2cbd12b861626b77fd3a4e122d285
NotTheDr01ds <32344964+NotTheDr01ds@users.noreply.github.com>
2024-06-20 16:27:01 -0400

    Fixed 'typo' identified in CI

--- crates/nu-command/src/generators/cal.rs
+++ crates/nu-command/src/generators/cal.rs
@@ -86,7 +86,7 @@ impl Command for Cal {
                 result: None,
             },
             Example {
-                description: "How many Friday the 13ths occurred in 2015",
+                description: "How many 'Friday the Thirteenths' occurred in 2015?",
                 example: "cal --as-table --full-year 2015 | where fr == 13 | length",
                 result: None,
             },
