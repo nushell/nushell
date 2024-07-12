1dc47f3b9c5e0e31128c6517f60e0c0cba52db34
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-07-12 18:20:32 -0400

    Remove duplicate UTouch

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -294,7 +294,6 @@ pub fn add_shell_command_context(mut engine_state: EngineState) -> EngineState {
             ToTsv,
             Touch,
             Upsert,
-            UTouch,
             Where,
             ToXml,
             ToYaml,
