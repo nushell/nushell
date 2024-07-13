c0a101eb840089e917199a4388ff3378156107be
Yash Thakur <45539777+ysthakur@users.noreply.github.com>
2024-07-13 10:06:32 -0400

    Don't add touch command to default context twice
    Touch was added to the shell command context twice, once with the other filesystem commands and once with the format commands. I removed that second occurrence of touch, because I'm assuming it was only added there because "Touch" starts with "To"

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -289,7 +289,6 @@ pub fn add_shell_command_context(mut engine_state: EngineState) -> EngineState {
             ToText,
             ToToml,
             ToTsv,
-            Touch,
             Upsert,
             Where,
             ToXml,
