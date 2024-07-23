29ce58e65e7f383cdafa7e8fdb7f1d1db05bbf11
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-07-23 21:23:20 +0300

    Finish Base64

--- crates/nu-command/src/strings/base/base64.rs
+++ crates/nu-command/src/strings/base/base64.rs
@@ -1,4 +1,33 @@
+use data_encoding::Encoding;
+
 use nu_engine::command_prelude::*;
+
+fn get_encoding_from_flags(url: bool, nopad: bool) -> Encoding {
+    match (url, nopad) {
+        (false, false) => data_encoding::BASE64,
+        (false, true) => data_encoding::BASE64_NOPAD,
+        (true, false) => data_encoding::BASE64URL,
+        (true, true) => data_encoding::BASE64URL_NOPAD,
+    }
+}
+
+fn get_encoding(
+    engine_state: &EngineState,
+    stack: &mut Stack,
+    call: &Call,
+) -> Result<Encoding, ShellError> {
+    let url = call.has_flag(engine_state, stack, "url")?;
+    let nopad = call.has_flag(engine_state, stack, "nopad")?;
+
+    Ok(get_encoding_from_flags(url, nopad))
+}
+
+fn get_encoding_const(working_set: &StateWorkingSet, call: &Call) -> Result<Encoding, ShellError> {
+    let url = call.has_flag_const(working_set, "url")?;
+    let nopad = call.has_flag_const(working_set, "nopad")?;
+
+    Ok(get_encoding_from_flags(url, nopad))
+}
 
 #[derive(Clone)]
 pub struct DecodeBase64;
@@ -12,12 +41,13 @@ impl Command for DecodeBase64 {
         Signature::build("decode base64")
             .input_output_types(vec![(Type::String, Type::Binary)])
             .allow_variants_without_examples(true)
-            .required("encoding", SyntaxShape::String, "encoding to use")
+            .switch("url", "Decode the URL-safe Base64 version.", None)
+            .switch("nopad", "Reject padding.", None)
             .category(Category::Formats)
     }
 
     fn usage(&self) -> &str {
-        "Decode a value."
+        "TODO"
     }
 
     fn extra_usage(&self) -> &str {
@@ -39,7 +69,8 @@ impl Command for DecodeBase64 {
         call: &Call,
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        todo!()
+        let encoding = get_encoding(engine_state, stack, call)?;
+        super::decode(encoding, call.head, input)
     }
 
     fn run_const(
@@ -48,7 +79,8 @@ impl Command for DecodeBase64 {
         call: &Call,
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        todo!()
+        let encoding = get_encoding_const(working_set, call)?;
+        super::decode(encoding, call.head, input)
     }
 }
 
@@ -72,7 +104,7 @@ impl Command for EncodeBase64 {
     }
 
     fn usage(&self) -> &str {
-        "Encode a value."
+        "TODO"
     }
 
     fn extra_usage(&self) -> &str {
@@ -94,7 +126,8 @@ impl Command for EncodeBase64 {
         call: &Call,
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        todo!()
+        let encoding = get_encoding(engine_state, stack, call)?;
+        super::encode(encoding, call.head, input)
     }
 
     fn run_const(
@@ -103,7 +136,8 @@ impl Command for EncodeBase64 {
         call: &Call,
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        todo!()
+        let encoding = get_encoding_const(working_set, call)?;
+        super::encode(encoding, call.head, input)
     }
 }
 
