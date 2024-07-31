8637a2f1a2280ef50df0a166ca94ea8baf37fb02
Gwendolyn <me@gwendolyn.dev>
2024-07-31 11:13:52 +0200

    extract `get_signature()` for working sets analogous to the same function on the engine state

--- crates/nu-parser/src/parser.rs
+++ crates/nu-parser/src/parser.rs
@@ -954,12 +954,7 @@ pub fn parse_internal_call(
     let _ = working_set.add_span(call.head);
 
     let decl = working_set.get_decl(decl_id);
-    let signature = if let Some(block_id) = decl.block_id() {
-        let block = working_set.get_block(block_id);
-        *block.signature.clone()
-    } else {
-        decl.signature()
-    };
+    let signature = working_set.get_signature(decl);
     let output = signature.get_output_type();
 
     // storing the var ID for later due to borrowing issues
