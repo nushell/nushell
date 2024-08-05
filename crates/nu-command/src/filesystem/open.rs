50ecdbbe15e509abd15ee6ab34a6113e6fa3108c
Jack Wright <jack.wright@disqo.com>
2024-08-05 13:53:41 -0700

    content-type testing

--- crates/nu-command/src/filesystem/open.rs
+++ crates/nu-command/src/filesystem/open.rs
@@ -147,9 +147,9 @@ impl Command for Open {
                     };
 
                     let content_type = if raw {
-                        mime_guess::from_path(path)
-                            .first()
-                            .map(|mime| mime.to_string())
+                        path.extension()
+                            .map(|ext| ext.to_string_lossy().to_string())
+                            .and_then(|ref s| detect_content_type(s))
                     } else {
                         None
                     };
@@ -275,4 +275,23 @@ fn extract_extensions(filename: &str) -> Vec<String> {
     extensions.reverse();
 
     extensions
+}
+
+fn detect_content_type(extension: &str) -> Option<String> {
+    // This will allow the overriding of metadata to be consistent with
+    // the content type
+    match extension {
+        // Per RFC-9512, application/yaml should be used
+        "yaml" | "yml" => Some("application/yaml".to_string()),
+        _ => mime_guess::from_ext(extension)
+            .first()
+            .map(|mime| mime.to_string()),
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_content_type() {}
 }
