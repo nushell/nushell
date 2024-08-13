d529974edf73bc75c6791bef669f2adccef89b4c
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2024-08-13 10:46:36 -0500

    add more helpful error with text/xml

--- crates/nu-command/src/network/http/client.rs
+++ crates/nu-command/src/network/http/client.rs
@@ -4,9 +4,10 @@ use base64::{
     engine::{general_purpose::PAD, GeneralPurpose},
     Engine,
 };
+use fancy_regex::Regex;
 use multipart_rs::MultipartWriter;
 use nu_engine::command_prelude::*;
-use nu_protocol::{ByteStream, Signals};
+use nu_protocol::{ByteStream, LabeledError, Signals};
 use std::{
     collections::HashMap,
     io::Cursor,
@@ -560,23 +561,28 @@ fn transform_response_using_content_type(
     resp: Response,
     content_type: &str,
 ) -> Result<PipelineData, ShellError> {
-    let content_type =
-        mime::Mime::from_str(content_type).map_err(|_| ShellError::GenericError {
-            error: format!("MIME type unknown: {content_type}"),
-            msg: "".into(),
-            span: None,
-            help: Some("given unknown MIME type".into()),
-            inner: vec![],
-        })?;
+    let regex = Regex::new("\"").expect("Failed to create regex");
+    let content_type_trim = regex.replace_all(content_type, "").to_string();
+
+    let content_type = mime::Mime::from_str(&content_type_trim).map_err(|err| {
+        LabeledError::new(err.to_string())
+            .with_help("given unknown MIME type, or error parsing MIME type")
+            .with_label(
+                format!("MIME type unknown: {content_type_trim}"),
+                Span::unknown(),
+            )
+    })?;
+
     let ext = match (content_type.type_(), content_type.subtype()) {
         (mime::TEXT, mime::PLAIN) => {
             let path_extension = url::Url::parse(requested_url)
-                .map_err(|_| ShellError::GenericError {
-                    error: format!("Cannot parse URL: {requested_url}"),
-                    msg: "".into(),
-                    span: None,
-                    help: Some("cannot parse".into()),
-                    inner: vec![],
+                .map_err(|err| {
+                    LabeledError::new(err.to_string())
+                        .with_help("cannot parse")
+                        .with_label(
+                            format!("Cannot parse URL: {requested_url}"),
+                            Span::unknown(),
+                        )
                 })?
                 .path_segments()
                 .and_then(|segments| segments.last())
