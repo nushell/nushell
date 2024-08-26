6d0a0a5692681c8c63550d07c2c2fb4bdf27f4f9
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-08-26 01:11:09 -0400

    Make NuMatcher struct

--- crates/nu-cli/src/completions/completion_options.rs
+++ crates/nu-cli/src/completions/completion_options.rs
@@ -1,7 +1,10 @@
 use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use nu_parser::trim_quotes_str;
 use nu_protocol::{CompletionAlgorithm, CompletionSort};
-use std::fmt::Display;
+use nu_utils::IgnoreCaseExt;
+use std::{borrow::Cow, fmt::Display};
+
+use super::SemanticSuggestion;
 
 /// Describes how suggestions should be matched.
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -19,32 +22,131 @@ pub enum MatchAlgorithm {
     Fuzzy,
 }
 
-impl MatchAlgorithm {
-    /// Returns whether the `needle` search text matches the given `haystack`.
-    pub fn matches_str(&self, haystack: &str, needle: &str) -> bool {
-        let haystack = trim_quotes_str(haystack);
-        let needle = trim_quotes_str(needle);
-        match *self {
-            MatchAlgorithm::Prefix => haystack.starts_with(needle),
-            MatchAlgorithm::Fuzzy => {
-                let matcher = SkimMatcherV2::default();
-                matcher.fuzzy_match(haystack, needle).is_some()
+pub struct NuMatcher<T> {
+    options: CompletionOptions,
+    needle: String,
+    state: State<T>,
+}
+
+enum State<T> {
+    Prefix {
+        /// Holds (haystack, item)
+        items: Vec<(String, T)>,
+    },
+    Fuzzy {
+        /// Holds (haystack, item, score)
+        items: Vec<(String, T, i64)>,
+    },
+}
+
+/// Filters and sorts suggestions
+impl<T> NuMatcher<T> {
+    /// # Arguments
+    ///
+    /// * `needle` - The text to search for
+    pub fn new(needle: impl AsRef<str>, options: CompletionOptions) -> NuMatcher<T> {
+        let orig_needle = trim_quotes_str(needle.as_ref());
+        let lowercase_needle = if options.case_sensitive {
+            orig_needle.to_owned()
+        } else {
+            orig_needle.to_folded_case()
+        };
+        match options.match_algorithm {
+            MatchAlgorithm::Prefix => NuMatcher {
+                options,
+                needle: lowercase_needle,
+                state: State::Prefix { items: Vec::new() },
+            },
+            MatchAlgorithm::Fuzzy => NuMatcher {
+                options,
+                needle: orig_needle.to_owned(),
+                state: State::Fuzzy { items: Vec::new() },
+            },
+        }
+    }
+
+    /// Add the given item if the given haystack matches.
+    ///
+    /// Returns whether the item was added.
+    pub fn add(&mut self, haystack: String, item: T) -> bool {
+        match &mut self.state {
+            State::Prefix { items } => {
+                let haystack = trim_quotes_str(&haystack).to_owned();
+                let haystack_lowercased = if self.options.case_sensitive {
+                    Cow::Borrowed(&haystack)
+                } else {
+                    Cow::Owned(haystack.to_folded_case())
+                };
+                if haystack_lowercased.starts_with(self.needle.as_str()) {
+                    items.push((haystack, item));
+                    true
+                } else {
+                    false
+                }
+            }
+            State::Fuzzy { items } => {
+                let mut matcher = SkimMatcherV2::default();
+                if self.options.case_sensitive {
+                    matcher = matcher.respect_case();
+                } else {
+                    matcher = matcher.ignore_case();
+                };
+                let Some(score) = matcher.fuzzy_match(&haystack, &self.needle) else {
+                    return false;
+                };
+                items.push((haystack, item, score));
+                true
             }
         }
     }
 
-    /// Returns whether the `needle` search text matches the given `haystack`.
-    pub fn matches_u8(&self, haystack: &[u8], needle: &[u8]) -> bool {
-        match *self {
-            MatchAlgorithm::Prefix => haystack.starts_with(needle),
-            MatchAlgorithm::Fuzzy => {
-                let haystack_str = String::from_utf8_lossy(haystack);
-                let needle_str = String::from_utf8_lossy(needle);
-
-                let matcher = SkimMatcherV2::default();
-                matcher.fuzzy_match(&haystack_str, &needle_str).is_some()
+    /// Remove the last added item. This is useful if you want to filter matched
+    /// completions by some expensive condition. You can call `add`, run the expensive condition,
+    /// then call `remove_last` if the expensive condition is false.
+    pub fn remove_last(&mut self) {
+        match &mut self.state {
+            State::Prefix { items } => {
+                items.pop();
+            }
+            State::Fuzzy { items } => {
+                items.pop();
             }
         }
+    }
+
+    /// Get all the items that matched (sorted)
+    pub fn results(self) -> Vec<T> {
+        match self.state {
+            State::Prefix { mut items, .. } => {
+                items.sort_by(|(haystack1, _), (haystack2, _)| haystack1.cmp(haystack2));
+                items.into_iter().map(|(_, item)| item).collect::<Vec<_>>()
+            }
+            State::Fuzzy { mut items, .. } => {
+                match self.options.sort {
+                    CompletionSort::Alphabetical => {
+                        items.sort_by(|(haystack1, _, _), (haystack2, _, _)| {
+                            haystack1.cmp(haystack2)
+                        });
+                    }
+                    CompletionSort::Smart => {
+                        items.sort_by(|(haystack1, _, score1), (haystack2, _, score2)| {
+                            score2.cmp(score1).then(haystack1.cmp(haystack2))
+                        });
+                    }
+                }
+                items
+                    .into_iter()
+                    .map(|(_, item, _)| item)
+                    .collect::<Vec<_>>()
+            }
+        }
+    }
+}
+
+impl NuMatcher<SemanticSuggestion> {
+    pub fn add_semantic_suggestion(&mut self, sugg: SemanticSuggestion) -> bool {
+        let value = sugg.suggestion.value.to_string();
+        self.add(value, sugg)
     }
 }
 
@@ -105,35 +207,48 @@ impl Default for CompletionOptions {
 
 #[cfg(test)]
 mod test {
-    use super::MatchAlgorithm;
+    use rstest::rstest;
 
-    #[test]
-    fn match_algorithm_prefix() {
-        let algorithm = MatchAlgorithm::Prefix;
+    use super::{CompletionOptions, MatchAlgorithm, NuMatcher};
 
-        assert!(algorithm.matches_str("example text", ""));
-        assert!(algorithm.matches_str("example text", "examp"));
-        assert!(!algorithm.matches_str("example text", "text"));
-
-        assert!(algorithm.matches_u8(&[1, 2, 3], &[]));
-        assert!(algorithm.matches_u8(&[1, 2, 3], &[1, 2]));
-        assert!(!algorithm.matches_u8(&[1, 2, 3], &[2, 3]));
+    #[rstest]
+    #[case(MatchAlgorithm::Prefix, "example text", "", true)]
+    #[case(MatchAlgorithm::Prefix, "example text", "examp", true)]
+    #[case(MatchAlgorithm::Prefix, "example text", "text", false)]
+    #[case(MatchAlgorithm::Fuzzy, "example text", "", true)]
+    #[case(MatchAlgorithm::Fuzzy, "example text", "examp", true)]
+    #[case(MatchAlgorithm::Fuzzy, "example text", "ext", true)]
+    #[case(MatchAlgorithm::Fuzzy, "example text", "mplxt", true)]
+    #[case(MatchAlgorithm::Fuzzy, "example text", "mpp", false)]
+    fn match_algorithm_simple(
+        #[case] match_algorithm: MatchAlgorithm,
+        #[case] haystack: &str,
+        #[case] needle: &str,
+        #[case] should_match: bool,
+    ) {
+        let options = CompletionOptions {
+            match_algorithm,
+            ..Default::default()
+        };
+        let mut matcher = NuMatcher::new(needle, options);
+        matcher.add(haystack.to_string(), haystack);
+        if should_match {
+            assert_eq!(vec![haystack], matcher.results());
+        } else {
+            assert_ne!(vec![haystack], matcher.results());
+        }
     }
 
     #[test]
-    fn match_algorithm_fuzzy() {
-        let algorithm = MatchAlgorithm::Fuzzy;
-
-        assert!(algorithm.matches_str("example text", ""));
-        assert!(algorithm.matches_str("example text", "examp"));
-        assert!(algorithm.matches_str("example text", "ext"));
-        assert!(algorithm.matches_str("example text", "mplxt"));
-        assert!(!algorithm.matches_str("example text", "mpp"));
-
-        assert!(algorithm.matches_u8(&[1, 2, 3], &[]));
-        assert!(algorithm.matches_u8(&[1, 2, 3], &[1, 2]));
-        assert!(algorithm.matches_u8(&[1, 2, 3], &[2, 3]));
-        assert!(algorithm.matches_u8(&[1, 2, 3], &[1, 3]));
-        assert!(!algorithm.matches_u8(&[1, 2, 3], &[2, 2]));
+    fn match_algorithm_fuzzy_sort_score() {
+        let options = CompletionOptions {
+            match_algorithm: MatchAlgorithm::Fuzzy,
+            ..Default::default()
+        };
+        let mut matcher = NuMatcher::new("foo bar", options);
+        for item in ["foo/bar", "bar/foo", "foobar"] {
+            matcher.add(item.to_string(), item);
+        }
+        assert_eq!(vec!["bar/foo", "foo/bar", "foobar"], matcher.results());
     }
 }
--- crates/nu-cli/src/completions/dotnu_completions.rs
+++ crates/nu-cli/src/completions/dotnu_completions.rs
@@ -6,7 +6,7 @@ use nu_protocol::{
 use reedline::Suggestion;
 use std::path::{is_separator, Path, MAIN_SEPARATOR as SEP, MAIN_SEPARATOR_STR};
 
-use super::{completion_common::sort_suggestions, SemanticSuggestion};
+use super::SemanticSuggestion;
 
 #[derive(Clone, Default)]
 pub struct DotNuCompletion {}
@@ -87,49 +87,44 @@ impl Completer for DotNuCompletion {
 
         // Fetch the files filtering the ones that ends with .nu
         // and transform them into suggestions
-        let output: Vec<SemanticSuggestion> = search_dirs
-            .into_iter()
-            .flat_map(|search_dir| {
-                let completions = file_path_completion(
-                    span,
-                    &partial,
-                    &search_dir,
-                    options,
-                    working_set.permanent_state,
-                    stack,
-                );
-                completions
-                    .into_iter()
-                    .filter(move |it| {
-                        // Different base dir, so we list the .nu files or folders
-                        if !is_current_folder {
-                            it.1.ends_with(".nu") || it.1.ends_with(SEP)
-                        } else {
-                            // Lib dirs, so we filter only the .nu files or directory modules
-                            if it.1.ends_with(SEP) {
-                                Path::new(&search_dir).join(&it.1).join("mod.nu").exists()
-                            } else {
-                                it.1.ends_with(".nu")
-                            }
-                        }
-                    })
-                    .map(move |x| SemanticSuggestion {
-                        suggestion: Suggestion {
-                            value: x.1,
-                            style: x.2,
-                            span: reedline::Span {
-                                start: x.0.start - offset,
-                                end: x.0.end - offset,
-                            },
-                            append_whitespace: true,
-                            ..Suggestion::default()
-                        },
-                        // TODO????
-                        kind: None,
-                    })
-            })
-            .collect();
 
-        sort_suggestions(&prefix_str, output, options)
+        let completions = file_path_completion(
+            span,
+            &partial,
+            &search_dirs.iter().map(|d| d.as_str()).collect::<Vec<_>>(),
+            options,
+            working_set.permanent_state,
+            stack,
+        );
+        completions
+            .into_iter()
+            .filter(move |it| {
+                // Different base dir, so we list the .nu files or folders
+                if !is_current_folder {
+                    it.path.ends_with(".nu") || it.path.ends_with(SEP)
+                } else {
+                    // Lib dirs, so we filter only the .nu files or directory modules
+                    if it.path.ends_with(SEP) {
+                        Path::new(&it.cwd).join(&it.path).join("mod.nu").exists()
+                    } else {
+                        it.path.ends_with(".nu")
+                    }
+                }
+            })
+            .map(move |x| SemanticSuggestion {
+                suggestion: Suggestion {
+                    value: x.path,
+                    style: x.style,
+                    span: reedline::Span {
+                        start: x.span.start - offset,
+                        end: x.span.end - offset,
+                    },
+                    append_whitespace: true,
+                    ..Suggestion::default()
+                },
+                // TODO????
+                kind: None,
+            })
+            .collect::<Vec<_>>()
     }
 }
