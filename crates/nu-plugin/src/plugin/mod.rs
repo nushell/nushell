d28260a29bb124a47af08dbf0ddb64aaf89bdb92
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2024-09-03 11:38:44 -0500

    move version to the top

--- crates/nu-plugin/src/plugin/mod.rs
+++ crates/nu-plugin/src/plugin/mod.rs
@@ -655,6 +655,7 @@ fn print_help(plugin: &impl Plugin, encoder: impl PluginEncoder) {
 
     println!("Nushell Plugin");
     println!("Encoder: {}", encoder.name());
+    println!("Version: {}", plugin.version());
 
     let mut help = String::new();
     let help_style = HelpStyle::default();
@@ -662,7 +663,6 @@ fn print_help(plugin: &impl Plugin, encoder: impl PluginEncoder) {
     plugin.commands().into_iter().for_each(|command| {
         let signature = command.signature();
         let res = write!(help, "\nCommand: {}", command.name())
-            .and_then(|_| writeln!(help, "\nVersion: {}", plugin.version()))
             .and_then(|_| writeln!(help, "\nDescription:\n > {}", command.description()))
             .and_then(|_| {
                 if !command.extra_description().is_empty() {
