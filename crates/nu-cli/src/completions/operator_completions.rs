2daa86cbbfe657471d2cab8d564f8b88a53e25cb
Vivek Kethineni <vickey2183@gmail.com>
2024-09-05 22:00:43 -0500

    Added basic completions for strings and ints

--- crates/nu-cli/src/completions/operator_completions.rs
+++ crates/nu-cli/src/completions/operator_completions.rs
@@ -1,6 +1,6 @@
 use crate::completions::{
     completion_common::{adjust_if_intermediate, complete_item, AdjustView},
-    Completer, CompletionOptions,
+    Completer, CompletionOptions, SuggestionKind, MatchAlgorithm
 };
 use nu_ansi_term::Style;
 use nu_protocol::{
@@ -33,13 +33,61 @@ impl OperatorCompletion {
 
 
 impl OperatorCompletion {
-    pub fn fetch_int_completions(&self) -> Vec<SemanticSuggestion> {
-        println!("int operators");
-        vec![]
+    pub fn fetch_int_completions(&self, span: Span, offset: usize, partial: &str) -> Vec<SemanticSuggestion> {
+        let int_ops = vec![
+            ("mod", "Modulo"),
+            ("bit-or", "bitwise or"), 
+            ("bit-xor", "bitwise exclusive or"),
+            ("bit-and", "bitwise and"),
+            ("bit-shl", "bitwise shift left"),
+            ("bit-shr", "bitwise shift right")
+        ];
+
+        let match_algorithm = MatchAlgorithm::Fuzzy;
+
+        let input_fuzzy_search = |(operator, _): &(&str, &str)| match_algorithm.matches_str(operator, partial);
+
+        int_ops
+            .into_iter()
+            .filter(input_fuzzy_search)
+            .map(move |x| SemanticSuggestion {
+                suggestion: Suggestion {
+                    value: x.0.to_string(),
+                    description: Some(x.1.to_string()),
+                    span: reedline::Span::new(span.start - offset, span.end - offset),
+                    append_whitespace: true,
+                    ..Suggestion::default()
+                },
+                kind: Some(SuggestionKind::Command(nu_protocol::engine::CommandType::Builtin)),
+            })
+            .collect()
+
     }
-    pub fn fetch_str_completions(&self) -> Vec<SemanticSuggestion> {
-        println!("str operators");
-        vec![]
+    pub fn fetch_str_completions(&self, span: Span, offset: usize, partial: &str) -> Vec<SemanticSuggestion> {
+        let str_ops = vec![
+            ("in" , "In / Contains (doesn't use regex)"),
+            ("not-in" , "Not In / Not Contains (doesn't use regex"),
+            ("starts-with" , "Starts With"),
+            ("ends-with", "Ends With")
+        ];
+
+        let match_algorithm = MatchAlgorithm::Fuzzy;
+
+        let input_fuzzy_search = |(operator, _): &(&str, &str)| match_algorithm.matches_str(operator, partial);
+
+        str_ops
+            .into_iter()
+            .map(move |x| SemanticSuggestion {
+                suggestion: Suggestion {
+                    value: x.0.to_string(),
+                    description: Some(x.1.to_string()),
+                    span: reedline::Span::new(span.start - offset, span.end - offset),
+                    append_whitespace: true,
+                    ..Suggestion::default()
+                },
+                kind: Some(SuggestionKind::Command(nu_protocol::engine::CommandType::Builtin)),
+            })
+            .collect()
     }
     
 }
@@ -55,8 +103,19 @@ impl Completer for OperatorCompletion {
         _pos: usize,
         options: &CompletionOptions,
     ) -> Vec<SemanticSuggestion> {
-        println!("{:?}", self.previous_expr);
-        vec![]
+        //Check if int, numeric, or string
+        let partial = std::str::from_utf8(working_set.get_span_contents(span)).unwrap_or("");
+        match &self.previous_expr.expr {
+            Expr::BinaryOp(x, _, _) => {
+                match x.expr {
+                    Expr::Int(_) => self.fetch_int_completions(span, offset, partial), 
+                    Expr::String(_) => self.fetch_str_completions(span, offset, partial), 
+                    _ => vec![]
+                }
+            },
+            _ => vec![]
+            
+        }
     }
 }
 
