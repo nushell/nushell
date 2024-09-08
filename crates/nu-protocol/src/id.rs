3f123a52515caaa460ab67cef52b750fee056a0d
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2024-09-09 00:12:25 +0200

    designed a simple newtype pattern for ids

--- crates/nu-protocol/src/id.rs
+++ crates/nu-protocol/src/id.rs
@@ -1,12 +1,112 @@
+use std::any;
+use std::cmp::Ordering;
+use std::fmt::{Debug, Error, Formatter};
+use std::marker::PhantomData;
+
 use serde::{Deserialize, Serialize};
 
-pub type VarId = usize;
-pub type DeclId = usize;
-pub type BlockId = usize;
-pub type ModuleId = usize;
-pub type OverlayId = usize;
-pub type FileId = usize;
-pub type VirtualPathId = usize;
+pub struct Id<T> {
+    inner: usize,
+    _phantom: PhantomData<T>,
+}
+
+impl<T> Id<T> {
+    /// Creates a new `Id`.
+    ///
+    /// Using a distinct type like `Id` instead of `usize` helps us avoid mixing plain integers
+    /// with identifiers.
+    pub fn new(inner: usize) -> Self {
+        Self {
+            inner,
+            _phantom: PhantomData::default(),
+        }
+    }
+
+    /// Returns the inner `usize` value.
+    ///
+    /// This requires an explicit call, ensuring we only use the raw value when intended.
+    pub fn get(self) -> usize {
+        self.inner
+    }
+}
+
+impl<T> Debug for Id<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        let marker = any::type_name::<T>().split("::").last().expect("not empty");
+        write!(f, "{marker}Id({})", self.inner)
+    }
+}
+
+impl<T> Clone for Id<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+            _phantom: self._phantom.clone(),
+        }
+    }
+}
+
+impl<T> Copy for Id<T> {}
+
+impl<T> PartialEq for Id<T> {
+    fn eq(&self, other: &self) -> bool {
+        self.inner == other.inner
+    }
+}
+
+impl<T> Eq for Id<T> {}
+
+impl<T> Ord for Id<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.inner.cmp(&other.inner)
+    }
+}
+
+impl<T> PartialOrd for Id<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T> Serialize for Id<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.inner.serialize(serializer)
+    }
+}
+
+impl<'de, T> Deserialize for Id<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let inner = usize::deserialize(deserializer)?;
+        Ok(Self {
+            inner,
+            _phantom: PhantomData::default(),
+        })
+    }
+}
+
+pub mod marker {
+    pub struct Var;
+    pub struct Decl;
+    pub struct Block;
+    pub struct Module;
+    pub struct Overlay;
+    pub struct File;
+    pub struct VirtualPath;
+}
+
+pub type VarId = Id<marker::Var>;
+pub type DeclId = Id<marker::Decl>;
+pub type BlockId = Id<marker::Block>;
+pub type ModuleId = Id<marker::Module>;
+pub type OverlayId = Id<marker::Overlay>;
+pub type FileId = Id<marker::File>;
+pub type VirtualPathId = Id<marker::VirtualPath>;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct SpanId(pub usize); // more robust ID style used in the new parser
