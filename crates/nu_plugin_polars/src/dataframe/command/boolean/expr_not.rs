6113399fed3522893811e3744a27fad1c02379ec
Jack Wright <jack.wright@disqo.com>
2024-09-10 18:42:50 -0700

    refactoring more macro commmands

--- crates/nu_plugin_polars/src/dataframe/command/boolean/expr_not.rs
+++ crates/nu_plugin_polars/src/dataframe/command/boolean/expr_not.rs
@@ -1,21 +1,72 @@
-use crate::expr_command;
-use crate::values::CustomValueSupport;
-use crate::values::NuExpression;
+use crate::dataframe::values::NuExpression;
+use crate::values::{cant_convert_err, CustomValueSupport, PolarsPluginObject, PolarsPluginType};
 use crate::PolarsPlugin;
 use nu_plugin::{EngineInterface, EvaluatedCall, PluginCommand};
-use nu_protocol::{Category, Example, LabeledError, PipelineData, Signature, Type};
+use nu_protocol::{Category, Example, LabeledError, PipelineData, ShellError, Signature, Type};
 
-// ExprNot command
-// Expands to a command definition for a not expression
-expr_command!(
-    ExprNot,
-    "polars expr-not",
-    "Creates a not expression.",
-    vec![Example {
-        description: "Creates a not expression",
-        example: "(polars col a) > 2) | polars expr-not",
-        result: None,
-    },],
-    not,
-    test_not
-);
+pub struct ExprNot;
+
+impl PluginCommand for ExprNot {
+    type Plugin = PolarsPlugin;
+
+    fn name(&self) -> &str {
+        "polars expr-not"
+    }
+
+    fn description(&self) -> &str {
+        "Creates a not expression."
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build(self.name())
+            .input_output_types(vec![(
+                Type::Custom("expression".into()),
+                Type::Custom("expression".into()),
+            )])
+            .category(Category::Custom("dataframe".into()))
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![Example {
+            description: "Creates a not expression",
+            example: "(polars col a) > 2) | polars expr-not",
+            result: None,
+        }]
+    }
+
+    fn run(
+        &self,
+        plugin: &Self::Plugin,
+        engine: &EngineInterface,
+        call: &EvaluatedCall,
+        input: PipelineData,
+    ) -> Result<PipelineData, LabeledError> {
+        let value = input.into_value(call.head)?;
+        match PolarsPluginObject::try_from_value(plugin, &value)? {
+            PolarsPluginObject::NuExpression(expr) => command_expr(plugin, engine, call, expr),
+            _ => Err(cant_convert_err(&value, &[PolarsPluginType::NuExpression])),
+        }
+        .map_err(LabeledError::from)
+    }
+}
+
+fn command_expr(
+    plugin: &PolarsPlugin,
+    engine: &EngineInterface,
+    call: &EvaluatedCall,
+    expr: NuExpression,
+) -> Result<PipelineData, ShellError> {
+    NuExpression::from(expr.into_polars().not()).to_pipeline_data(plugin, engine, call.head)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::test_polars_plugin_command;
+    use nu_protocol::ShellError;
+
+    #[test]
+    fn test_examples() -> Result<(), ShellError> {
+        test_polars_plugin_command(&ExprNot)
+    }
+}
