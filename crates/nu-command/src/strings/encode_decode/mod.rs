f6feb8a3770ed26a65b2f1111ef2f13ce56bd62b
hustcer <hustcer@outlook.com>
2024-10-07 09:59:03 +0800

    Remove the old encode base64 and decode base64 replace with new-base64 commands

--- crates/nu-command/src/strings/encode_decode/mod.rs
+++ crates/nu-command/src/strings/encode_decode/mod.rs
@@ -1,11 +1,7 @@
 mod base64;
 mod decode;
-mod decode_base64;
 mod encode;
-mod encode_base64;
 mod encoding;
 
 pub use self::decode::Decode;
-pub use self::decode_base64::DecodeBase64Old;
 pub use self::encode::Encode;
-pub use self::encode_base64::EncodeBase64Old;
