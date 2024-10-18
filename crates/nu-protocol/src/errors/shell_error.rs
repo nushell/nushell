e735bd475f53b62e30a3e4a041e21462db63ac47
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2024-10-17 20:16:38 -0500

    add rendered and json error messages in try/catch (#14082)
    # Description
    
    This PR adds a couple more options for dealing with try/catch errors. It
    adds a `json` version of the error and a `rendered` version of the
    error. It also respects the error_style configuration point.
    
    ![image](https://github.com/user-attachments/assets/32574f07-f511-40c0-8b57-de5f6f13a9c4)
    
    
    # User-Facing Changes
    <!-- List of all changes that impact the user experience here. This
    helps us keep track of breaking changes. -->
    
    # Tests + Formatting
    <!--
    Don't forget to add tests that cover your changes.
    
    Make sure you've run and fixed any issues with these commands:
    
    - `cargo fmt --all -- --check` to check standard code formatting (`cargo
    fmt --all` applies these changes)
    - `cargo clippy --workspace -- -D warnings -D clippy::unwrap_used` to
    check that you're using the standard code style
    - `cargo test --workspace` to check that all tests pass (on Windows make
    sure to [enable developer
    mode](https://learn.microsoft.com/en-us/windows/apps/get-started/developer-mode-features-and-debugging))
    - `cargo run -- -c "use toolkit.nu; toolkit test stdlib"` to run the
    tests for the standard library
    
    > **Note**
    > from `nushell` you can also use the `toolkit` as follows
    > ```bash
    > use toolkit.nu # or use an `env_change` hook to activate it
    automatically
    > toolkit check pr
    > ```
    -->
    
    # After Submitting
    <!-- If your PR had any user-facing changes, update [the
    documentation](https://github.com/nushell/nushell.github.io) after the
    PR is merged, if necessary. This will help us keep the docs up to date.
    -->

--- crates/nu-protocol/src/errors/shell_error.rs
+++ crates/nu-protocol/src/errors/shell_error.rs
@@ -1474,13 +1474,16 @@ impl ShellError {
         self.external_exit_code().map(|e| e.item).unwrap_or(1)
     }
 
-    pub fn into_value(self, span: Span) -> Value {
+    pub fn into_value(self, span: Span, fancy_errors: bool) -> Value {
         let exit_code = self.external_exit_code();
 
         let mut record = record! {
             "msg" => Value::string(self.to_string(), span),
             "debug" => Value::string(format!("{self:?}"), span),
-            "raw" => Value::error(self, span),
+            "raw" => Value::error(self.clone(), span),
+            // "labeled_error" => Value::string(LabeledError::from_diagnostic_and_render(self.clone()), span),
+            "rendered" => Value::string(ShellError::render_error_to_string(self.clone(), fancy_errors), span),
+            "json" => Value::string(serde_json::to_string(&self).expect("Could not serialize error"), span),
         };
 
         if let Some(code) = exit_code {
@@ -1498,6 +1501,21 @@ impl ShellError {
             "Encountered error during parse-time evaluation".into(),
             span,
         )
+    }
+    pub fn render_error_to_string(diag: impl miette::Diagnostic, fancy_errors: bool) -> String {
+        let theme = if fancy_errors {
+            miette::GraphicalTheme::unicode()
+        } else {
+            miette::GraphicalTheme::none()
+        };
+        let mut out = String::new();
+        miette::GraphicalReportHandler::new()
+            .with_width(80)
+            .with_theme(theme)
+            .render_report(&mut out, &diag)
+            .unwrap_or_default();
+
+        out
     }
 }
 
