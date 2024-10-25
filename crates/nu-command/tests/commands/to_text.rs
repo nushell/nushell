564b2061e2c52045e245e63df1059a633930aa50
Ian Manske <ian.manske@pm.me>
2024-10-24 20:20:51 -0700

    Add tests

--- crates/nu-command/tests/commands/to_text.rs
+++ crates/nu-command/tests/commands/to_text.rs
@@ -2,18 +2,52 @@ use nu_test_support::nu;
 
 #[test]
 fn list_to_text() {
-    let actual = nu!(r#"["foo" "bar" "baz"] | to text"#);
+    // Using `str length` since nu! strips newlines, grr
+    let actual = nu!(r#"[] | to text | str length"#);
+    assert_eq!(actual.out, "0");
 
-    // these actually have newlines between them in the real world but nu! strips newlines, grr
-    assert_eq!(actual.out, "foobarbaz");
+    let actual = nu!(r#"[a] | to text | str length"#);
+    assert_eq!(actual.out, "2");
+
+    let actual = nu!(r#"[a b] | to text | str length"#);
+    assert_eq!(actual.out, "4");
 }
 
-// the output should be the same when `to text` gets a ListStream instead of a Value::List
+// The output should be the same when `to text` gets a ListStream instead of a Value::List.
 #[test]
 fn list_stream_to_text() {
-    // use `each` to convert the list to a ListStream
-    let actual = nu!(r#"["foo" "bar" "baz"] | each {|i| $i} | to text"#);
+    let actual = nu!(r#"[] | each {} | to text | str length"#);
+    assert_eq!(actual.out, "0");
 
-    // these actually have newlines between them in the real world but nu! strips newlines, grr
-    assert_eq!(actual.out, "foobarbaz");
+    let actual = nu!(r#"[a] | each {} | to text | str length"#);
+    assert_eq!(actual.out, "2");
+
+    let actual = nu!(r#"[a b] | each {} | to text | str length"#);
+    assert_eq!(actual.out, "4");
+}
+
+#[test]
+fn list_to_text_no_newline() {
+    // Using `str length` since nu! strips newlines, grr
+    let actual = nu!(r#"[] | to text -n | str length"#);
+    assert_eq!(actual.out, "0");
+
+    let actual = nu!(r#"[a] | to text -n | str length"#);
+    assert_eq!(actual.out, "1");
+
+    let actual = nu!(r#"[a b] | to text -n | str length"#);
+    assert_eq!(actual.out, "3");
+}
+
+// The output should be the same when `to text` gets a ListStream instead of a Value::List.
+#[test]
+fn list_stream_to_text_no_newline() {
+    let actual = nu!(r#"[] | each {} | to text -n | str length"#);
+    assert_eq!(actual.out, "0");
+
+    let actual = nu!(r#"[a] | each {} | to text -n | str length"#);
+    assert_eq!(actual.out, "1");
+
+    let actual = nu!(r#"[a b] | each {} | to text -n | str length"#);
+    assert_eq!(actual.out, "3");
 }
