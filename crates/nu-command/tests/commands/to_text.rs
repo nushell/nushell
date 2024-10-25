b1c976628c8c403841590cf81394312a67dbc786
Ian Manske <ian.manske@pm.me>
2024-10-24 20:42:33 -0700

    Fix tests

--- crates/nu-command/tests/commands/to_text.rs
+++ crates/nu-command/tests/commands/to_text.rs
@@ -1,5 +1,7 @@
 use nu_test_support::nu;
 
+const LINE_LEN: usize = if cfg!(target_os = "windows") { 2 } else { 1 };
+
 #[test]
 fn list() {
     // Using `str length` since nu! strips newlines, grr
@@ -7,10 +9,10 @@ fn list() {
     assert_eq!(actual.out, "0");
 
     let actual = nu!(r#"[a] | to text | str length"#);
-    assert_eq!(actual.out, "2");
+    assert_eq!(actual.out, (1 + LINE_LEN).to_string());
 
     let actual = nu!(r#"[a b] | to text | str length"#);
-    assert_eq!(actual.out, "4");
+    assert_eq!(actual.out, (2 * (1 + LINE_LEN)).to_string());
 }
 
 // The output should be the same when `to text` gets a ListStream instead of a Value::List.
@@ -20,10 +22,10 @@ fn list_stream() {
     assert_eq!(actual.out, "0");
 
     let actual = nu!(r#"[a] | each {} | to text | str length"#);
-    assert_eq!(actual.out, "2");
+    assert_eq!(actual.out, (1 + LINE_LEN).to_string());
 
     let actual = nu!(r#"[a b] | each {} | to text | str length"#);
-    assert_eq!(actual.out, "4");
+    assert_eq!(actual.out, (2 * (1 + LINE_LEN)).to_string());
 }
 
 #[test]
@@ -35,7 +37,7 @@ fn list_no_newline() {
     assert_eq!(actual.out, "1");
 
     let actual = nu!(r#"[a b] | to text -n | str length"#);
-    assert_eq!(actual.out, "3");
+    assert_eq!(actual.out, (2 + LINE_LEN).to_string());
 }
 
 // The output should be the same when `to text` gets a ListStream instead of a Value::List.
@@ -48,5 +50,5 @@ fn list_stream_no_newline() {
     assert_eq!(actual.out, "1");
 
     let actual = nu!(r#"[a b] | each {} | to text -n | str length"#);
-    assert_eq!(actual.out, "3");
+    assert_eq!(actual.out, (2 + LINE_LEN).to_string());
 }
