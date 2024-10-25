ead44a14cf390ee581d4e8cf905bc4f7071c3adb
Ian Manske <ian.manske@pm.me>
2024-10-24 20:24:42 -0700

    Cleanup

--- crates/nu-command/tests/commands/to_text.rs
+++ crates/nu-command/tests/commands/to_text.rs
@@ -1,7 +1,7 @@
 use nu_test_support::nu;
 
 #[test]
-fn list_to_text() {
+fn list() {
     // Using `str length` since nu! strips newlines, grr
     let actual = nu!(r#"[] | to text | str length"#);
     assert_eq!(actual.out, "0");
@@ -15,7 +15,7 @@ fn list_to_text() {
 
 // The output should be the same when `to text` gets a ListStream instead of a Value::List.
 #[test]
-fn list_stream_to_text() {
+fn list_stream() {
     let actual = nu!(r#"[] | each {} | to text | str length"#);
     assert_eq!(actual.out, "0");
 
@@ -27,8 +27,7 @@ fn list_stream_to_text() {
 }
 
 #[test]
-fn list_to_text_no_newline() {
-    // Using `str length` since nu! strips newlines, grr
+fn list_no_newline() {
     let actual = nu!(r#"[] | to text -n | str length"#);
     assert_eq!(actual.out, "0");
 
@@ -41,7 +40,7 @@ fn list_to_text_no_newline() {
 
 // The output should be the same when `to text` gets a ListStream instead of a Value::List.
 #[test]
-fn list_stream_to_text_no_newline() {
+fn list_stream_no_newline() {
     let actual = nu!(r#"[] | each {} | to text -n | str length"#);
     assert_eq!(actual.out, "0");
 
