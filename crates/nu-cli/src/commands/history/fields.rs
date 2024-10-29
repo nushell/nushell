f8d4adfb7af1bf08f4ece1d8b58fad8cac1e63ba
Piotr Kufel <qfel@users.noreply.github.com>
2024-10-29 04:34:48 -0700

    Add the history import command (again) (#14083)
    # Description
    
    This is mainly https://github.com/nushell/nushell/pull/13450 (which got
    reverted). Additionally:
     - always clear IDs on import, disallow specifying IDs when piping
     - added extra tests
     - create backup of the history
    
    # User-Facing Changes
    
    New command: `history import`
    
    # Tests + Formatting
    
    Added mostly integration tests and a few smaller unit tests.

---
+++ crates/nu-cli/src/commands/history/fields.rs
@@ -0,0 +1,9 @@
+// Each const is named after a HistoryItem field, and the value is the field name to be displayed to
+// the user (or accept during import).
+pub const COMMAND_LINE: &str = "command";
+pub const START_TIMESTAMP: &str = "start_timestamp";
+pub const HOSTNAME: &str = "hostname";
+pub const CWD: &str = "cwd";
+pub const EXIT_STATUS: &str = "exit_status";
+pub const DURATION: &str = "duration";
+pub const SESSION_ID: &str = "session_id";
