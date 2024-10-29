b50a688d70dba59db928bffc915c7885b29d8969
NotTheDr01ds <32344964+NotTheDr01ds@users.noreply.github.com>
2024-10-29 07:43:12 -0400

    Restored original name with new output code; added new scrollback code

--- crates/nu-command/tests/commands/platform/ansi_.rs
+++ crates/nu-command/tests/commands/platform/ansi_.rs
@@ -11,5 +11,19 @@ fn test_ansi_shows_error_on_escape() {
 fn test_ansi_list_outputs_table() {
     let actual = nu!("ansi --list | length");
 
-    assert_eq!(actual.out, "424");
+    assert_eq!(actual.out, "425");
+}
+
+#[test]
+fn test_ansi_codes() {
+    let actual = nu!("$'(ansi clear_scrollback_buffer)'");
+    assert_eq!(actual.out, "\x1b[3J");
+
+    // Currently, bg is placed before fg in the results
+    // It's okay if something internally changes this, but
+    // if so, the test case will need to be updated to:
+    // assert_eq!(actual.out, "\x1b[31;48;2;0;255;0mHello\x1b[0m");
+
+    let actual = nu!("$'(ansi { fg: red, bg: \"#00ff00\" })Hello(ansi reset)'");
+    assert_eq!(actual.out, "\x1b[48;2;0;255;0;31mHello\x1b[0m");
 }
