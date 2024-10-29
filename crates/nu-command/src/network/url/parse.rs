b8ecf27377b5a30bc891c38da103494fb2fb6147
Bahex <bahey1999@gmail.com>
2024-10-29 06:42:47 +0300

    url parse: switch to `url split-query` impl for query string
    also reduce the nesting in the code

--- crates/nu-command/src/network/url/parse.rs
+++ crates/nu-command/src/network/url/parse.rs
@@ -2,6 +2,8 @@ use nu_engine::command_prelude::*;
 use nu_protocol::Config;
 use url::Url;
 
+use super::query::query_string_to_table;
+
 #[derive(Clone)]
 pub struct SubCommand;
 
@@ -80,54 +82,41 @@ fn get_url_string(value: &Value, config: &Config) -> String {
 fn parse(value: Value, head: Span, config: &Config) -> Result<PipelineData, ShellError> {
     let url_string = get_url_string(&value, config);
 
-    let result_url = Url::parse(url_string.as_str());
-
     // This is the span of the original string, not the call head.
     let span = value.span();
 
-    match result_url {
-        Ok(url) => {
-            let params =
-                serde_urlencoded::from_str::<Vec<(String, String)>>(url.query().unwrap_or(""));
-            match params {
-                Ok(result) => {
-                    let params = result
-                        .into_iter()
-                        .map(|(k, v)| (k, Value::string(v, head)))
-                        .collect();
+    let url = Url::parse(url_string.as_str()).map_err(|_| ShellError::UnsupportedInput {
+        msg: "Incomplete or incorrect URL. Expected a full URL, e.g., https://www.example.com"
+            .to_string(),
+        input: "value originates from here".into(),
+        msg_span: head,
+        input_span: span,
+    })?;
 
-                    let port = url.port().map(|p| p.to_string()).unwrap_or_default();
-
-                    let record = record! {
-                        "scheme" => Value::string(url.scheme(), head),
-                        "username" => Value::string(url.username(), head),
-                        "password" => Value::string(url.password().unwrap_or(""), head),
-                        "host" => Value::string(url.host_str().unwrap_or(""), head),
-                        "port" => Value::string(port, head),
-                        "path" => Value::string(url.path(), head),
-                        "query" => Value::string(url.query().unwrap_or(""), head),
-                        "fragment" => Value::string(url.fragment().unwrap_or(""), head),
-                        "params" => Value::record(params, head),
-                    };
-
-                    Ok(PipelineData::Value(Value::record(record, head), None))
-                }
-                _ => Err(ShellError::UnsupportedInput {
-                    msg: "String not compatible with url-encoding".to_string(),
-                    input: "value originates from here".into(),
-                    msg_span: head,
-                    input_span: span,
-                }),
-            }
-        }
-        Err(_e) => Err(ShellError::UnsupportedInput {
-            msg: "Incomplete or incorrect URL. Expected a full URL, e.g., https://www.example.com"
-                .to_string(),
+    let params = query_string_to_table(url.query().unwrap_or(""), head, span).map_err(|_| {
+        ShellError::UnsupportedInput {
+            msg: "String not compatible with url-encoding".to_string(),
             input: "value originates from here".into(),
             msg_span: head,
             input_span: span,
-        }),
-    }
+        }
+    })?;
+
+    let port = url.port().map(|p| p.to_string()).unwrap_or_default();
+
+    let record = record! {
+        "scheme" => Value::string(url.scheme(), head),
+        "username" => Value::string(url.username(), head),
+        "password" => Value::string(url.password().unwrap_or(""), head),
+        "host" => Value::string(url.host_str().unwrap_or(""), head),
+        "port" => Value::string(port, head),
+        "path" => Value::string(url.path(), head),
+        "query" => Value::string(url.query().unwrap_or(""), head),
+        "fragment" => Value::string(url.fragment().unwrap_or(""), head),
+        "params" => params,
+    };
+
+    Ok(PipelineData::Value(Value::record(record, head), None))
 }
 
 #[cfg(test)]
