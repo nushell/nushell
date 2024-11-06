3182adb6a0a2bee1d5e17118697ef71cb8841acb
Bahex <Bahex@users.noreply.github.com>
2024-11-06 16:35:37 +0300

    Url split query (#14211)
    Addresses the following points from #14162
    
    > - There is no built-in counterpart to url build-query for splitting a
    query string
    
    There is `from url`, which, due to naming, is a little hard to discover
    and suffers from the following point
    
    > - url parse can create records with duplicate keys
    > - url parse's params should either:
    >   - ~group the same keys into a list.~
    > - instead of a record, be a key-value table. (table<key: string,
    value: string>)
    
    # Description
    
    ## `url split-query`
    
    Counterpart to `url build-query`, splits a url encoded query string to
    key value pairs, represented as `table<key: string, value: string>`
    
    ```
    > "a=one&a=two&b=three" | url split-query
    ╭───┬─────┬───────╮
    │ # │ key │ value │
    ├───┼─────┼───────┤
    │ 0 │ a   │ one   │
    │ 1 │ a   │ two   │
    │ 2 │ b   │ three │
    ╰───┴─────┴───────╯
    ```
    
    ## `url parse`
    
    The output's `param` field is now a table as well, mirroring the new
    `url split-query`
    
    ```
    > 'http://localhost?a=one&a=two&b=three' | url parse
    ╭──────────┬─────────────────────╮
    │ scheme   │ http                │
    │ username │                     │
    │ password │                     │
    │ host     │ localhost           │
    │ port     │                     │
    │ path     │ /                   │
    │ query    │ a=one&a=two&b=three │
    │ fragment │                     │
    │          │ ╭───┬─────┬───────╮ │
    │ params   │ │ # │ key │ value │ │
    │          │ ├───┼─────┼───────┤ │
    │          │ │ 0 │ a   │ one   │ │
    │          │ │ 1 │ a   │ two   │ │
    │          │ │ 2 │ b   │ three │ │
    │          │ ╰───┴─────┴───────╯ │
    ╰──────────┴─────────────────────╯
    ```
    
    # User-Facing Changes
    
    - `url parse`'s output has the mentioned change, which is backwards
    incompatible.

--- crates/nu-command/src/network/url/query.rs
+++ crates/nu-command/src/network/url/query.rs
@@ -1,4 +1,4 @@
-use nu_protocol::{Record, ShellError, Span, Type, Value};
+use nu_protocol::{IntoValue, Record, ShellError, Span, Type, Value};
 
 pub fn record_to_query_string(
     record: &Record,
@@ -41,4 +41,27 @@ pub fn record_to_query_string(
         span: head,
         help: None,
     })
+}
+
+pub fn query_string_to_table(query: &str, head: Span, span: Span) -> Result<Value, ShellError> {
+    let params = serde_urlencoded::from_str::<Vec<(String, String)>>(query)
+        .map_err(|_| ShellError::UnsupportedInput {
+            msg: "String not compatible with url-encoding".to_string(),
+            input: "value originates from here".into(),
+            msg_span: head,
+            input_span: span,
+        })?
+        .into_iter()
+        .map(|(key, value)| {
+            Value::record(
+                nu_protocol::record! {
+                    "key" => key.into_value(head),
+                    "value" => value.into_value(head)
+                },
+                head,
+            )
+        })
+        .collect::<Vec<_>>();
+
+    Ok(Value::list(params, head))
 }
