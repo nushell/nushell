a6126185139a0ef5e7b1f3d4a96f27793dc27efe
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-11-22 01:58:51 -0500

    Make find_commands_by_predicate take FnMut

--- crates/nu-protocol/src/engine/state_working_set.rs
+++ crates/nu-protocol/src/engine/state_working_set.rs
@@ -724,7 +724,7 @@ impl<'a> StateWorkingSet<'a> {
 
     pub fn find_commands_by_predicate(
         &self,
-        predicate: impl Fn(&[u8]) -> bool,
+        mut predicate: impl FnMut(&[u8]) -> bool,
         ignore_deprecated: bool,
     ) -> Vec<(Vec<u8>, Option<String>, CommandType)> {
         let mut output = vec![];
