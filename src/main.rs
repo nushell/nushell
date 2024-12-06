89d06db335ea30e7d37fd692552d55225df232fd
shray sharma <sharma.shray92@Gmail.com>
2024-12-06 20:11:26 +0100

    upd

--- src/main.rs
+++ src/main.rs
@@ -45,11 +45,10 @@ fn get_engine_state() -> EngineState {
 
 /// Get the directory where the Nushell executable is located.
 fn current_exe_directory() -> PathBuf {
-    let path = std::env::current_exe().unwrap_or_else(|err| {
+    std::env::current_exe().unwrap_or_else(|err| {
         eprintln!("Failed to get current executable path: {}", err);
         PathBuf::new() // Return a sensible default
-    });
-    path
+    })
 }
 
 /// Get the current working directory from the environment.
