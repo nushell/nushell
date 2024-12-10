3515e3ee28129c8ec939c31eb4c74b8dae69debe
132ikl <132@ikl.sh>
2024-12-10 07:36:27 -0500

    Remove grid icons deprecation warning (#14526)
    <!--
    if this PR closes one or more issues, you can automatically link the PR
    with
    them by using one of the [*linking
    keywords*](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword),
    e.g.
    - this PR should close #xxxx
    - fixes #xxxx
    
    you can also mention related issues, PRs or discussions!
    -->
    
    # Description
    <!--
    Thank you for improving Nushell. Please, check our [contributing
    guide](../CONTRIBUTING.md) and talk to the core team before making major
    changes.
    
    Description of your pull request goes here. **Provide examples and/or
    screenshots** if your changes affect the user experience.
    -->
    
    Noticed this TODO, so I did as it said.
    
    # User-Facing Changes
    <!-- List of all changes that impact the user experience here. This
    helps us keep track of breaking changes. -->
    N/A (the functionality was already removed)
    
    # Tests + Formatting
    <!--
    Don't forget to add tests that cover your changes.
    
    Make sure you've run and fixed any issues with these commands:
    
    - `cargo fmt --all -- --check` to check standard code formatting (`cargo
    fmt --all` applies these changes)
    - `cargo clippy --workspace -- -D warnings -D clippy::unwrap_used` to
    check that you're using the standard code style
    - `cargo test --workspace` to check that all tests pass (on Windows make
    sure to [enable developer
    mode](https://learn.microsoft.com/en-us/windows/apps/get-started/developer-mode-features-and-debugging))
    - `cargo run -- -c "use toolkit.nu; toolkit test stdlib"` to run the
    tests for the standard library
    
    > **Note**
    > from `nushell` you can also use the `toolkit` as follows
    > ```bash
    > use toolkit.nu # or use an `env_change` hook to activate it
    automatically
    > toolkit check pr
    > ```
    -->
    N/A
    
    # After Submitting
    <!-- If your PR had any user-facing changes, update [the
    documentation](https://github.com/nushell/nushell.github.io) after the
    PR is merged, if necessary. This will help us keep the docs up to date.
    -->
    N/A
    
    ---------
    
    Co-authored-by: Darren Schroeder <343840+fdncred@users.noreply.github.com>

--- crates/nu-protocol/src/config/mod.rs
+++ crates/nu-protocol/src/config/mod.rs
@@ -156,10 +156,6 @@ impl UpdateFromValue for Config {
                 "filesize" => self.filesize.update(val, path, errors),
                 "explore" => self.explore.update(val, path, errors),
                 "color_config" => self.color_config.update(val, path, errors),
-                "use_grid_icons" => {
-                    // TODO: delete it after 0.99
-                    errors.deprecated_option(path, "use `grid -i`", val.span());
-                }
                 "footer_mode" => self.footer_mode.update(val, path, errors),
                 "float_precision" => self.float_precision.update(val, path, errors),
                 "use_ansi_coloring" => self.use_ansi_coloring.update(val, path, errors),
