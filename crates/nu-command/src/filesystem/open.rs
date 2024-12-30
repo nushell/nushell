363793ec23616cfb0bb945a8ce6b9d3d23dcea7c
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2024-12-30 23:43:43 +0100

    add a small comment

--- crates/nu-command/src/filesystem/open.rs
+++ crates/nu-command/src/filesystem/open.rs
@@ -91,6 +91,8 @@ impl Command for Open {
                         file: path.item.to_string(),
                         span,
                     },
+                    // that particular error in `nu_engine::glob_from` doesn't have a span attached 
+                    // to it, so let's add it
                     ShellError::GenericError {
                         error,
                         msg,
