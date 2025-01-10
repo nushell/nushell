dbe9943cc7917c1176cddf684250d07f53e32262
Zhenping Zhao <pegasus.cadence@gmail.com>
2025-01-09 23:07:14 -0800

    rm test

--- crates/nu-command/tests/commands/filesystem/rm.rs
+++ crates/nu-command/tests/commands/filesystem/rm.rs
@@ -568,3 +568,37 @@ fn rm_with_tilde() {
         assert!(!files_exist_at(&["~tilde"], dirs.test()));
     })
 }
+
+#[cfg(windows)]
+#[test]
+fn test_pwd_per_drive() {
+    Playground::setup("test_rm_pwd_per_drive", |dirs, sandbox| {
+        sandbox.mkdir("test_folder");
+        let _actual = nu!(
+            cwd: dirs.test(),
+            r#"
+                subst X: /D | touch out
+                subst X: test_folder
+                x:
+                mkdir test_folder_on_x
+                cd -
+                x:test_folder_on_x\
+                touch test_file_on_x.txt
+                cd -
+            "#
+        );
+        assert!(_actual.err.is_empty());
+        let expected_file = dirs
+            .test()
+            .join("test_folder\\test_folder_on_x\\test_file_on_x.txt");
+        assert!(expected_file.exists());
+        let _actual = nu!(
+            cwd: dirs.test(),
+            r#"
+                rm x:test_folder_on_x\test_file_on_x.txt
+                subst X: /D | touch out
+            "#
+        );
+        assert!(!expected_file.exists());
+    })
+}
