nushell/nushell

commit 3f8dd1b7059de684b53d59622287ac854ab47c00

feat(lsp): document_symbols and workspace_symbols (#14770)
<!--
if this PR closes one or more issues, you can automatically link the PR
with
them by using one of the [*linking
keywords*](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword),
e.g.
- this PR should close #xxxx
- fixes #xxxx

you can also mention related issues, PRs or discussions!
-->

# Description
<!--
Thank you for improving Nushell. Please, check our [contributing
guide](../CONTRIBUTING.md) and talk to the core team before making major
changes.

Description of your pull request goes here. **Provide examples and/or
screenshots** if your changes affect the user experience.
-->

This PR adds symbols related features to lsp
<img width="940" alt="image"
src="https://github.com/user-attachments/assets/aeaed338-133c-430a-b966-58a9bc445211"
/>

Notice that symbols of type variable may got filtered by client side
plugins

<img width="906" alt="image"
src="https://github.com/user-attachments/assets/e031b3dc-443a-486f-8a35-4415c07196d0"
/>


# User-Facing Changes
<!-- List of all changes that impact the user experience here. This
helps us keep track of breaking changes. -->

# Tests + Formatting
<!--
Don't forget to add tests that cover your changes.

Make sure you've run and fixed any issues with these commands:

- `cargo fmt --all -- --check` to check standard code formatting (`cargo
fmt --all` applies these changes)
- `cargo clippy --workspace -- -D warnings -D clippy::unwrap_used` to
check that you're using the standard code style
- `cargo test --workspace` to check that all tests pass (on Windows make
sure to [enable developer
mode](https://learn.microsoft.com/en-us/windows/apps/get-started/developer-mode-features-and-debugging))
- `cargo run -- -c "use toolkit.nu; toolkit test stdlib"` to run the
tests for the standard library

> **Note**
> from `nushell` you can also use the `toolkit` as follows

crates/nu-lsp/src/diagnostics.rs
@@ -1,21 +1,16 @@
-use crate::LanguageServer;
+use crate::{span_to_range, LanguageServer};
 use lsp_types::{
     notification::{Notification, PublishDiagnostics},
     Diagnostic, DiagnosticSeverity, PublishDiagnosticsParams, Uri,
 };
 use miette::{IntoDiagnostic, Result};
-use nu_protocol::Value;
 
 impl LanguageServer {
     pub(crate) fn publish_diagnostics_for_file(&mut self, uri: Uri) -> Result<()> {
-        let mut engine_state = self.engine_state.clone();
-        let cwd = std::env::current_dir().expect("Could not get current working directory.");
-        engine_state.add_env_var("PWD".into(), Value::test_string(cwd.to_string_lossy()));
+        let mut engine_state = self.new_engine_state();
         engine_state.generate_nu_constant();
 
-        let Some((_, offset, working_set, file)) =
-            self.update_engine_state(&mut engine_state, &uri)
-        else {
+        let Some((_, offset, working_set, file)) = self.parse_file(&mut engine_state, &uri) else {
             return Ok(());
         };
 
@@ -29,7 +24,7 @@ impl LanguageServer {
             let message = err.to_string();
 
             diagnostics.diagnostics.push(Diagnostic {
-                range: Self::span_to_range(&err.span(), file, offset),
+                range: span_to_range(&err.span(), file, offset),
                 severity: Some(DiagnosticSeverity::ERROR),
                 message,
                 ..Default::default()

crates/nu-lsp/src/lib.rs
@@ -2,7 +2,10 @@
 use lsp_server::{Connection, IoThreads, Message, Response, ResponseError};
 use lsp_textdocument::{FullTextDocument, TextDocuments};
 use lsp_types::{
-    request::{Completion, GotoDefinition, HoverRequest, Request},
+    request::{
+        Completion, DocumentSymbolRequest, GotoDefinition, HoverRequest, Request,
+        WorkspaceSymbolRequest,
+    },
     CompletionItem, CompletionItemKind, CompletionParams, CompletionResponse, CompletionTextEdit,
     GotoDefinitionParams, GotoDefinitionResponse, Hover, HoverContents, HoverParams, Location,
     MarkupContent, MarkupKind, OneOf, Range, ServerCapabilities, TextDocumentSyncKind, TextEdit,
@@ -13,8 +16,8 @@ use nu_cli::{NuCompleter, SuggestionKind};
 use nu_parser::{flatten_block, parse, FlatShape};
 use nu_protocol::{
     ast::Block,
-    engine::{EngineState, Stack, StateWorkingSet},
-    DeclId, Span, Value, VarId,
+    engine::{CachedFile, EngineState, Stack, StateWorkingSet},
+    DeclId, ModuleId, Span, Value, VarId,
 };
 use std::{
     path::{Path, PathBuf},
@@ -22,16 +25,19 @@ use std::{
     sync::Arc,
     time::Duration,
 };
+use symbols::SymbolCache;
 use url::Url;
 
 mod diagnostics;
 mod notification;
+mod symbols;
 
 #[derive(Debug)]
 enum Id {
     Variable(VarId),
     Declaration(DeclId),
     Value(FlatShape),
+    Module(ModuleId),
 }
 
 pub struct LanguageServer {
@@ -39,12 +45,10 @@ pub struct LanguageServer {
     io_threads: Option<IoThreads>,
     docs: TextDocuments,
     engine_state: EngineState,
+    symbol_cache: SymbolCache,
 }
 
-pub fn path_to_uri<P>(path: P) -> Uri
-where
-    P: AsRef<Path>,
-{
+pub fn path_to_uri(path: impl AsRef<Path>) -> Uri {
     Uri::from_str(
         Url::from_file_path(path)
             .expect("Failed to convert path to Url")
@@ -58,6 +62,12 @@ pub fn uri_to_path(uri: &Uri) -> PathBuf {
         .expect("Failed to convert Uri to Url")
         .to_file_path()
         .expect("Failed to convert Url to path")
+}
+
+pub fn span_to_range(span: &Span, file: &FullTextDocument, offset: usize) -> Range {
+    let start = file.position_at(span.start.saturating_sub(offset) as u32);
+    let end = file.position_at(span.end.saturating_sub(offset) as u32);
+    Range { start, end }
 }
 
 impl LanguageServer {
@@ -76,6 +86,7 @@ impl LanguageServer {
             io_threads,
             docs: TextDocuments::new(),
             engine_state,
+            symbol_cache: SymbolCache::new(),
         })
     }
 
@@ -87,6 +98,8 @@ impl LanguageServer {
             definition_provider: Some(OneOf::Left(true)),
             hover_provider: Some(lsp_types::HoverProviderCapability::Simple(true)),
             completion_provider: Some(lsp_types::CompletionOptions::default()),
+            document_symbol_provider: Some(OneOf::Left(true)),
+            workspace_symbol_provider: Some(OneOf::Left(true)),
             ..Default::default()
         })
         .expect("Must be serializable");
@@ -131,6 +144,14 @@ impl LanguageServer {
                         Completion::METHOD => {
                             Self::handle_lsp_request(request, |params| self.complete(params))
                         }
+                        DocumentSymbolRequest::METHOD => {
+                            Self::handle_lsp_request(request, |params| self.document_symbol(params))
+                        }
+                        WorkspaceSymbolRequest::METHOD => {
+                            Self::handle_lsp_request(request, |params| {
+                                self.workspace_symbol(params)
+                            })
+                        }
                         _ => {
                             continue;
                         }
@@ -144,6 +165,7 @@ impl LanguageServer {
                 Message::Response(_) => {}
                 Message::Notification(notification) => {
                     if let Some(updated_file) = self.handle_lsp_notification(notification) {
+                        self.symbol_cache.mark_dirty(updated_file.clone(), true);
                         self.publish_diagnostics_for_file(updated_file)?;
                     }
                 }
@@ -157,7 +179,13 @@ impl LanguageServer {
         Ok(())
     }
 
-    pub fn update_engine_state<'a>(
+    pub fn new_engine_state(&self) -> EngineState {
+        let mut engine_state = self.engine_state.clone();
+        engine_state.add_env_var("PWD".into(), Value::test_string("."));
+        engine_state
+    }
+
+    pub fn parse_file<'a>(
         &mut self,
         engine_state: &'a mut EngineState,
         uri: &Uri,
@@ -176,6 +204,40 @@ impl LanguageServer {
         // TODO: merge delta back to engine_state?
         // self.engine_state.merge_delta(working_set.render());
         Some((block, offset, working_set, file))
+    }
+
+    fn get_location_by_span<'a>(
+        &self,
+        files: impl Iterator<Item = &'a CachedFile>,
+        span: &Span,
+    ) -> Option<Location> {
+        for cached_file in files.into_iter() {
+            if cached_file.covered_span.contains(span.start) {
+                let path = Path::new(&*cached_file.name);
+                if !(path.exists() && path.is_file()) {
+                    return None;
+                }
+                let target_uri = path_to_uri(path);
+                if let Some(doc) = self.docs.get_document(&target_uri) {
+                    return Some(Location {
+                        uri: target_uri,
+                        range: span_to_range(span, doc, cached_file.covered_span.start),
+                    });
+                } else {
+                    // in case where the document is not opened yet, typically included by `nu -I`
+                    let temp_doc = FullTextDocument::new(
+                        "nu".to_string(),
+                        0,
+                        String::from_utf8((*cached_file.content).to_vec()).expect("Invalid UTF-8"),
+                    );
+                    return Some(Location {
+                        uri: target_uri,
+                        range: span_to_range(span, &temp_doc, cached_file.covered_span.start),
+                    });
+                }
+            }
+        }
+        None
     }
 
     fn handle_lsp_request<P, H, R>(req: lsp_server::Request, mut param_handler: H) -> Response
@@ -207,12 +269,6 @@ impl LanguageServer {
         }
     }
 
-    fn span_to_range(span: &Span, file: &FullTextDocument, offset: usize) -> Range {
-        let start = file.position_at(span.start.saturating_sub(offset) as u32);
-        let end = file.position_at(span.end.saturating_sub(offset) as u32);
-        Range { start, end }
-    }
-
     fn find_id(
         flattened: Vec<(Span, FlatShape)>,
         location: usize,
@@ -226,7 +282,7 @@ impl LanguageServer {
                     FlatShape::Variable(var_id) | FlatShape::VarDecl(var_id) => {
                         return Some((Id::Variable(*var_id), offset, span));
                     }
-                    FlatShape::InternalCall(decl_id) => {
+                    FlatShape::InternalCall(decl_id) | FlatShape::Custom(decl_id) => {
                         return Some((Id::Declaration(*decl_id), offset, span));
                     }
                     _ => return Some((Id::Value(shape), offset, span)),
@@ -236,40 +292,8 @@ impl LanguageServer {
         None
     }
 
-    fn get_location_by_span(&self, working_set: &StateWorkingSet, span: &Span) -> Option<Location> {
-        for cached_file in working_set.files() {
-            if cached_file.covered_span.contains(span.start) {
-                let path = Path::new(&*cached_file.name);
-                if !(path.exists() && path.is_file()) {
-                    return None;
-                }
-                let target_uri = path_to_uri(path);
-                if let Some(doc) = self.docs.get_document(&target_uri) {
-                    return Some(Location {
-                        uri: target_uri,
-                        range: Self::span_to_range(span, doc, cached_file.covered_span.start),
-                    });
-                } else {
-                    // in case where the document is not opened yet, typically included by `nu -I`
-                    let temp_doc = FullTextDocument::new(
-                        "Unk".to_string(),
-                        0,
-                        String::from_utf8((*cached_file.content).to_vec()).expect("Invalid UTF-8"),
-                    );
-                    return Some(Location {
-                        uri: target_uri,
-                        range: Self::span_to_range(span, &temp_doc, cached_file.covered_span.start),
-                    });
-                }
-            }
-        }
-        None
-    }
-
     fn goto_definition(&mut self, params: &GotoDefinitionParams) -> Option<GotoDefinitionResponse> {
-        let mut engine_state = self.engine_state.clone();
-        let cwd = std::env::current_dir().expect("Could not get current working directory.");
-        engine_state.add_env_var("PWD".into(), Value::test_string(cwd.to_string_lossy()));
+        let mut engine_state = self.new_engine_state();
 
         let path_uri = params
             .text_document_position_params
@@ -277,7 +301,7 @@ impl LanguageServer {
             .uri
             .to_owned();
         let (block, file_offset, working_set, file) =
-            self.update_engine_state(&mut engine_state, &path_uri)?;
+            self.parse_file(&mut engine_state, &path_uri)?;
         let flattened = flatten_block(&working_set, &block);
         let (id, _, _) = Self::find_id(
             flattened,
@@ -294,17 +318,19 @@ impl LanguageServer {
                 let var = working_set.get_variable(var_id);
                 Some(var.declaration_span)
             }
+            Id::Module(module_id) => {
+                let module = working_set.get_module(module_id);
+                module.span
+            }
             _ => None,
         }?;
         Some(GotoDefinitionResponse::Scalar(
-            self.get_location_by_span(&working_set, &span)?,
+            self.get_location_by_span(working_set.files(), &span)?,
         ))
     }
 
     fn hover(&mut self, params: &HoverParams) -> Option<Hover> {
-        let mut engine_state = self.engine_state.clone();
-        let cwd = std::env::current_dir().expect("Could not get current working directory.");
-        engine_state.add_env_var("PWD".into(), Value::test_string(cwd.to_string_lossy()));
+        let mut engine_state = self.new_engine_state();
 
         let path_uri = params
             .text_document_position_params
@@ -312,7 +338,7 @@ impl LanguageServer {
             .uri
             .to_owned();
         let (block, file_offset, working_set, file) =
-            self.update_engine_state(&mut engine_state, &path_uri)?;
+            self.parse_file(&mut engine_state, &path_uri)?;
         let flattened = flatten_block(&working_set, &block);
         let (id, _, _) = Self::find_id(
             flattened,
@@ -515,6 +541,7 @@ impl LanguageServer {
                     range: None,
                 })
             }
+            _ => None,
         }
     }
 

crates/nu-lsp/Cargo.toml
@@ -22,6 +22,7 @@ miette = { workspace = true }
 serde = { workspace = true }
 serde_json = { workspace = true }
 url = { workspace = true }
+fuzzy-matcher = { workspace = true }
 
 [dev-dependencies]
 nu-cmd-lang = { path = "../nu-cmd-lang", version = "0.101.1" }

crates/nu-lsp/src/notification.rs
@@ -1,6 +1,10 @@
 use lsp_types::{
-    notification::{DidChangeTextDocument, DidOpenTextDocument, DidSaveTextDocument, Notification},
-    DidChangeTextDocumentParams, DidOpenTextDocumentParams, DidSaveTextDocumentParams, Uri,
+    notification::{
+        DidChangeTextDocument, DidCloseTextDocument, DidOpenTextDocument, DidSaveTextDocument,
+        Notification,
+    },
+    DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams,
+    DidSaveTextDocumentParams, Uri,
 };
 
 use crate::LanguageServer;
@@ -30,6 +34,13 @@ impl LanguageServer {
                     serde_json::from_value(notification.params.clone())
                         .expect("Expect receive DidChangeTextDocumentParams");
                 Some(params.text_document.uri)
+            }
+            DidCloseTextDocument::METHOD => {
+                let params: DidCloseTextDocumentParams =
+                    serde_json::from_value(notification.params.clone())
+                        .expect("Expect receive DidCloseTextDocumentParams");
+                self.symbol_cache.drop(&params.text_document.uri);
+                None
             }
             _ => None,
         }

commit 0b71eb201c15464c7fd9583c2525bcce73e7faf9

fix(lsp): PWD env_var (#14805)
<!--
if this PR closes one or more issues, you can automatically link the PR
with
them by using one of the [*linking
keywords*](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword),
e.g.
- this PR should close #xxxx
- fixes #xxxx

you can also mention related issues, PRs or discussions!
-->

# Description
<!--
Thank you for improving Nushell. Please, check our [contributing
guide](../CONTRIBUTING.md) and talk to the core team before making major
changes.

Description of your pull request goes here. **Provide examples and/or
screenshots** if your changes affect the user experience.
-->

This PR fixes an issue introduced by #14770 , as shown in
https://github.com/nushell/nushell/pull/14802#issuecomment-2585270161

# User-Facing Changes
<!-- List of all changes that impact the user experience here. This
helps us keep track of breaking changes. -->

# Tests + Formatting
<!--
Don't forget to add tests that cover your changes.

Make sure you've run and fixed any issues with these commands:

- `cargo fmt --all -- --check` to check standard code formatting (`cargo
fmt --all` applies these changes)
- `cargo clippy --workspace -- -D warnings -D clippy::unwrap_used` to
check that you're using the standard code style
- `cargo test --workspace` to check that all tests pass (on Windows make
sure to [enable developer
mode](https://learn.microsoft.com/en-us/windows/apps/get-started/developer-mode-features-and-debugging))
- `cargo run -- -c "use toolkit.nu; toolkit test stdlib"` to run the
tests for the standard library

> **Note**
> from `nushell` you can also use the `toolkit` as follows
> ```bash
> use toolkit.nu # or use an `env_change` hook to activate it
automatically
> toolkit check pr
> ```
-->

# After Submitting
<!-- If your PR had any user-facing changes, update [the
documentation](https://github.com/nushell/nushell.github.io) after the

crates/nu-lsp/src/diagnostics.rs
@@ -118,4 +118,28 @@ mod tests {
             })
         );
     }
+
+    #[test]
+    fn publish_diagnostics_none() {
+        let (client_connection, _recv) = initialize_language_server();
+
+        let mut script = fixtures();
+        script.push("lsp");
+        script.push("diagnostics");
+        script.push("pwd.nu");
+        let script = path_to_uri(&script);
+
+        let notification = open_unchecked(&client_connection, script.clone());
+
+        assert_json_eq!(
+            notification,
+            serde_json::json!({
+                "method": "textDocument/publishDiagnostics",
+                "params": {
+                    "uri": script,
+                    "diagnostics": []
+                }
+            })
+        );
+    }
 }

crates/nu-lsp/src/lib.rs
@@ -189,7 +189,8 @@ impl LanguageServer {
 
     pub fn new_engine_state(&self) -> EngineState {
         let mut engine_state = self.engine_state.clone();
-        engine_state.add_env_var("PWD".into(), Value::test_string("."));
+        let cwd = std::env::current_dir().expect("Could not get current working directory.");
+        engine_state.add_env_var("PWD".into(), Value::test_string(cwd.to_string_lossy()));
         engine_state
     }
 

tests/fixtures/lsp/diagnostics/pwd.nu
@@ -1,0 +1,1 @@
+const toolkit_dir = path self .