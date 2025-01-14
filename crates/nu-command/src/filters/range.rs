a60e61c7af96a88a74a8cd9667ecf95eee1f8ddb
sholderbach <sholderbach@users.noreply.github.com>
2025-01-14 15:12:03 +0100

    Rewrite `range` with `slice` internals

--- crates/nu-command/src/filters/range.rs
+++ crates/nu-command/src/filters/range.rs
@@ -1,6 +1,4 @@
 use nu_engine::command_prelude::*;
-use nu_protocol::Range as NumRange;
-use std::ops::Bound;
 
 #[derive(Clone)]
 pub struct Range;
@@ -64,70 +62,7 @@ impl Command for Range {
         call: &Call,
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let head = call.head;
-        let metadata = input.metadata();
-        let rows: Spanned<NumRange> = call.req(engine_state, stack, 0)?;
-
-        match rows.item {
-            NumRange::IntRange(range) => {
-                let start = range.start();
-                let end = match range.end() {
-                    Bound::Included(end) => end,
-                    Bound::Excluded(end) => end - 1,
-                    Bound::Unbounded => {
-                        if range.step() < 0 {
-                            i64::MIN
-                        } else {
-                            i64::MAX
-                        }
-                    }
-                };
-
-                // only collect the input if we have any negative indices
-                if start < 0 || end < 0 {
-                    let v: Vec<_> = input.into_iter().collect();
-                    let vlen: i64 = v.len() as i64;
-
-                    let from = if start < 0 {
-                        (vlen + start) as usize
-                    } else {
-                        start as usize
-                    };
-
-                    let to = if end < 0 {
-                        (vlen + end) as usize
-                    } else if end > v.len() as i64 {
-                        v.len()
-                    } else {
-                        end as usize
-                    };
-
-                    if from > to {
-                        Ok(PipelineData::Value(Value::nothing(head), None))
-                    } else {
-                        let iter = v.into_iter().skip(from).take(to - from + 1);
-                        Ok(iter.into_pipeline_data(head, engine_state.signals().clone()))
-                    }
-                } else {
-                    let from = start as usize;
-                    let to = end as usize;
-
-                    if from > to {
-                        Ok(PipelineData::Value(Value::nothing(head), None))
-                    } else {
-                        let iter = input.into_iter().skip(from).take(to - from + 1);
-                        Ok(iter.into_pipeline_data(head, engine_state.signals().clone()))
-                    }
-                }
-                .map(|x| x.set_metadata(metadata))
-            }
-            NumRange::FloatRange(_) => Err(ShellError::UnsupportedInput {
-                msg: "float range".into(),
-                input: "value originates from here".into(),
-                msg_span: call.head,
-                input_span: rows.span,
-            }),
-        }
+        super::Slice::run(&super::Slice, engine_state, stack, call, input)
     }
 }
 
