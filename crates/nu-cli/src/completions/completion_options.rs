nushell/nushell

commit 75105033b25a03b70b839dd707c951eb978f1082

Use nucleo instead of skim for completions (#14846)
# Description

This PR replaces `SkimMatcherV2` from the
[fuzzy-matcher](https://docs.rs/fuzzy-matcher/latest/fuzzy_matcher/)
crate with the
[nucleo-matcher](https://docs.rs/nucleo-matcher/latest/nucleo_matcher/)
crate for doing fuzzy matching. This touches both our completion code in
`nu-cli` and symbol filtering in `nu-lsp`.

Nucleo should give us better performance than Skim. In the event that we
decide to use the Nucleo frontend ([crate
docs](https://docs.rs/nucleo/latest/nucleo/)) too, it also works on
Windows, unlike [Skim](https://github.com/skim-rs/skim), which appears
to only support Linux and MacOS.

Unfortunately, we still have an indirect dependency on `fuzzy-matcher`,
because the [`dialoguer`](https://github.com/console-rs/dialoguer) crate
uses it.

# User-Facing Changes

No breaking changes. Suggestions will be sorted differently, because
Nucleo uses a different algorithm from Skim for matching/scoring.
Hopefully, the new sorting will generally make more sense.

# Tests + Formatting

In `nu-cli`, modified an existing test, but didn't test performance. I
haven't tested `nu-lsp` manually, but existing tests pass.

I did manually do `ls /nix/store/<TAB>`, `ls /nix/store/d<TAB>`, etc.,
but didn't notice Nucleo being faster (my `/nix/store` folder has 34136
items at the time of writing).

crates/nu-cli/src/completions/completion_options.rs
@@ -1,7 +1,10 @@
-use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use nu_parser::trim_quotes_str;
 use nu_protocol::{CompletionAlgorithm, CompletionSort};
 use nu_utils::IgnoreCaseExt;
+use nucleo_matcher::{
+    pattern::{AtomKind, CaseMatching, Normalization, Pattern},
+    Config, Matcher, Utf32Str,
+};
 use std::{borrow::Cow, fmt::Display};
 
 use super::SemanticSuggestion;
@@ -34,9 +37,10 @@ enum State<T> {
         items: Vec<(String, T)>,
     },
     Fuzzy {
-        matcher: Box<SkimMatcherV2>,
+        matcher: Matcher,
+        pat: Pattern,
         /// Holds (haystack, item, score)
-        items: Vec<(String, T, i64)>,
+        items: Vec<(String, T, u32)>,
     },
 }
 
@@ -46,30 +50,37 @@ impl<T> NuMatcher<T> {
     ///
     /// * `needle` - The text to search for
     pub fn new(needle: impl AsRef<str>, options: CompletionOptions) -> NuMatcher<T> {
-        let orig_needle = trim_quotes_str(needle.as_ref());
-        let lowercase_needle = if options.case_sensitive {
-            orig_needle.to_owned()
-        } else {
-            orig_needle.to_folded_case()
-        };
+        let needle = trim_quotes_str(needle.as_ref());
         match options.match_algorithm {
-            MatchAlgorithm::Prefix => NuMatcher {
-                options,
-                needle: lowercase_needle,
-                state: State::Prefix { items: Vec::new() },
-            },
-            MatchAlgorithm::Fuzzy => {
-                let mut matcher = SkimMatcherV2::default();
-                if options.case_sensitive {
-                    matcher = matcher.respect_case();
+            MatchAlgorithm::Prefix => {
+                let lowercase_needle = if options.case_sensitive {
+                    needle.to_owned()
                 } else {
-                    matcher = matcher.ignore_case();
+                    needle.to_folded_case()
                 };
                 NuMatcher {
                     options,
-                    needle: orig_needle.to_owned(),
+                    needle: lowercase_needle,
+                    state: State::Prefix { items: Vec::new() },
+                }
+            }
+            MatchAlgorithm::Fuzzy => {
+                let pat = Pattern::new(
+                    needle,
+                    if options.case_sensitive {
+                        CaseMatching::Respect
+                    } else {
+                        CaseMatching::Ignore
+                    },
+                    Normalization::Smart,
+                    AtomKind::Fuzzy,
+                );
+                NuMatcher {
+                    options,
+                    needle: needle.to_owned(),
                     state: State::Fuzzy {
-                        matcher: Box::new(matcher),
+                        matcher: Matcher::new(Config::DEFAULT),
+                        pat,
                         items: Vec::new(),
                     },
                 }
@@ -102,8 +113,15 @@ impl<T> NuMatcher<T> {
                 }
                 matches
             }
-            State::Fuzzy { items, matcher } => {
-                let Some(score) = matcher.fuzzy_match(haystack, &self.needle) else {
+            State::Fuzzy {
+                matcher,
+                pat,
+                items,
+            } => {
+                let mut haystack_buf = Vec::new();
+                let haystack_utf32 = Utf32Str::new(trim_quotes_str(haystack), &mut haystack_buf);
+                let mut indices = Vec::new();
+                let Some(score) = pat.indices(haystack_utf32, matcher, &mut indices) else {
                     return false;
                 };
                 if let Some(item) = item {

crates/nu-cli/tests/completions/mod.rs
@@ -111,25 +111,6 @@ fn custom_completer() -> NuCompleter {
     NuCompleter::new(Arc::new(engine), Arc::new(stack))
 }
 
-#[fixture]
-fn subcommand_completer() -> NuCompleter {
-    // Create a new engine
-    let (_, _, mut engine, mut stack) = new_engine();
-
-    let commands = r#"
-            $env.config.completions.algorithm = "fuzzy"
-            def foo [] {}
-            def "foo bar" [] {}
-            def "foo abaz" [] {}
-            def "foo aabcrr" [] {}
-            def food [] {}
-        "#;
-    assert!(support::merge_input(commands.as_bytes(), &mut engine, &mut stack).is_ok());
-
-    // Instantiate a new completer
-    NuCompleter::new(Arc::new(engine), Arc::new(stack))
-}
-
 /// Use fuzzy completions but sort in alphabetical order
 #[fixture]
 fn fuzzy_alpha_sort_completer() -> NuCompleter {
@@ -1040,24 +1021,32 @@ fn command_watch_with_filecompletion() {
 }
 
 #[rstest]
-fn subcommand_completions(mut subcommand_completer: NuCompleter) {
-    let prefix = "foo br";
-    let suggestions = subcommand_completer.complete(prefix, prefix.len());
-    match_suggestions(
-        &vec!["foo bar".to_string(), "foo aabcrr".to_string()],
-        &suggestions,
-    );
+fn subcommand_completions() {
+    let (_, _, mut engine, mut stack) = new_engine();
+    let commands = r#"
+            $env.config.completions.algorithm = "fuzzy"
+            def foo-test-command [] {}
+            def "foo-test-command bar" [] {}
+            def "foo-test-command aagap bcr" [] {}
+            def "food bar" [] {}
+        "#;
+    assert!(support::merge_input(commands.as_bytes(), &mut engine, &mut stack).is_ok());
+    let mut subcommand_completer = NuCompleter::new(Arc::new(engine), Arc::new(stack));
 
-    let prefix = "foo b";
+    let prefix = "fod br";
     let suggestions = subcommand_completer.complete(prefix, prefix.len());
     match_suggestions(
         &vec![
-            "foo bar".to_string(),
-            "foo abaz".to_string(),
-            "foo aabcrr".to_string(),
+            "food bar".to_string(),
+            "foo-test-command aagap bcr".to_string(),
+            "foo-test-command bar".to_string(),
         ],
         &suggestions,
     );
+
+    let prefix = "foot bar";
+    let suggestions = subcommand_completer.complete(prefix, prefix.len());
+    match_suggestions(&vec!["foo-test-command bar".to_string()], &suggestions);
 }
 
 #[test]

Cargo.toml
@@ -90,7 +90,6 @@ encoding_rs = "0.8"
 fancy-regex = "0.14"
 filesize = "0.2"
 filetime = "0.2"
-fuzzy-matcher = "0.3"
 heck = "0.5.0"
 human-date-parser = "0.2.0"
 indexmap = "2.7"
@@ -117,6 +116,7 @@ native-tls = "0.2"
 nix = { version = "0.29", default-features = false }
 notify-debouncer-full = { version = "0.3", default-features = false }
 nu-ansi-term = "0.50.1"
+nucleo-matcher = "0.3"
 num-format = "0.4"
 num-traits = "0.2"
 oem_cp = "2.0.0"

crates/nu-cli/Cargo.toml
@@ -33,11 +33,11 @@ reedline = { workspace = true, features = ["bashisms", "sqlite"] }
 chrono = { default-features = false, features = ["std"], workspace = true }
 crossterm = { workspace = true }
 fancy-regex = { workspace = true }
-fuzzy-matcher = { workspace = true }
 is_executable = { workspace = true }
 log = { workspace = true }
-miette = { workspace = true, features = ["fancy-no-backtrace"] }
 lscolors = { workspace = true, default-features = false, features = ["nu-ansi-term"] }
+miette = { workspace = true, features = ["fancy-no-backtrace"] }
+nucleo-matcher = { workspace = true }
 percent-encoding = { workspace = true }
 sysinfo = { workspace = true }
 unicode-segmentation = { workspace = true }

crates/nu-lsp/Cargo.toml
@@ -14,11 +14,11 @@ nu-parser = { path = "../nu-parser", version = "0.101.1" }
 nu-protocol = { path = "../nu-protocol", version = "0.101.1" }
 
 crossbeam-channel = { workspace = true }
-fuzzy-matcher = { workspace = true }
 lsp-server = { workspace = true }
 lsp-textdocument = { workspace = true }
 lsp-types = { workspace = true }
 miette = { workspace = true }
+nucleo-matcher = { workspace = true }
 serde = { workspace = true }
 serde_json = { workspace = true }
 url = { workspace = true }

crates/nu-lsp/src/symbols.rs
@@ -2,7 +2,6 @@ use std::collections::{BTreeMap, HashSet};
 use std::hash::{Hash, Hasher};
 
 use crate::{path_to_uri, span_to_range, uri_to_path, Id, LanguageServer};
-use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use lsp_textdocument::{FullTextDocument, TextDocuments};
 use lsp_types::{
     DocumentSymbolParams, DocumentSymbolResponse, Location, Range, SymbolInformation, SymbolKind,
@@ -14,6 +13,8 @@ use nu_protocol::{
     engine::{CachedFile, EngineState, StateWorkingSet},
     DeclId, Span, VarId,
 };
+use nucleo_matcher::pattern::{AtomKind, CaseMatching, Normalization, Pattern};
+use nucleo_matcher::{Config, Matcher, Utf32Str};
 use std::{cmp::Ordering, path::Path};
 
 /// Struct stored in cache, uri not included
@@ -70,7 +71,7 @@ impl Symbol {
 /// Cache symbols for each opened file to avoid repeated parsing
 pub struct SymbolCache {
     /// Fuzzy matcher for symbol names
-    matcher: SkimMatcherV2,
+    matcher: Matcher,
     /// File Uri --> Symbols
     cache: BTreeMap<Uri, Vec<Symbol>>,
     /// If marked as dirty, parse on next request
@@ -80,7 +81,7 @@ pub struct SymbolCache {
 impl SymbolCache {
     pub fn new() -> Self {
         SymbolCache {
-            matcher: SkimMatcherV2::default(),
+            matcher: Matcher::new(Config::DEFAULT),
             cache: BTreeMap::new(),
             dirty_flags: BTreeMap::new(),
         }
@@ -240,12 +241,20 @@ impl SymbolCache {
         )
     }
 
-    pub fn get_fuzzy_matched_symbols(&self, query: &str) -> Vec<SymbolInformation> {
+    pub fn get_fuzzy_matched_symbols(&mut self, query: &str) -> Vec<SymbolInformation> {
+        let pat = Pattern::new(
+            query,
+            CaseMatching::Smart,
+            Normalization::Smart,
+            AtomKind::Fuzzy,
+        );
         self.cache
             .iter()
             .flat_map(|(uri, symbols)| symbols.iter().map(|s| s.clone().to_symbol_information(uri)))
             .filter_map(|s| {
-                self.matcher.fuzzy_match(&s.name, query)?;
+                let mut buf = Vec::new();
+                let name = Utf32Str::new(&s.name, &mut buf);
+                pat.score(name, &mut self.matcher)?;
                 Some(s)
             })
             .collect()

commit 22a01d7e7616913c0ccee38b798ab9a25b4b468d

Use single atom for fuzzy matching (fix #14904) (#14913)
# Description

Closes #14904. The bug there was introduced by #14846, which replaced
skim with Nucleo. It turns out that Nucleo's `Pattern::new` function
doesn't treat the needle as a single atom - it splits on spaces and
makes each word its own atom. This PR fixes the problem by creating a
single `Atom` for the whole needle rather than creating a `Pattern`.

Because of the bug, when you typed `lines <TAB>` (with a space at the
end), the suggestion `lines` was also matched. This suggestion was
shorter than the original typed needle, which would cause an
out-of-bounds error.

This also meant that if you typed `foo bar<TAB>`, `foo aaaaa bar` would
be shown before `foo bar aaa`. At the time, I didn't realize that it was
more intuitive to have `foo bar aaa` be put first.

# User-Facing Changes

Typing something like `lines <TAB>` should no longer cause a panic.

# Tests + Formatting

- Added a test to ensure spaces are respected when fuzzy matching
- Updated a test with the changed sort order for subcommand suggestions

# After Submitting

No need to update docs.

crates/nu-cli/src/completions/completion_options.rs
@@ -2,7 +2,7 @@ use nu_parser::trim_quotes_str;
 use nu_protocol::{CompletionAlgorithm, CompletionSort};
 use nu_utils::IgnoreCaseExt;
 use nucleo_matcher::{
-    pattern::{AtomKind, CaseMatching, Normalization, Pattern},
+    pattern::{Atom, AtomKind, CaseMatching, Normalization},
     Config, Matcher, Utf32Str,
 };
 use std::{borrow::Cow, fmt::Display};
@@ -38,9 +38,9 @@ enum State<T> {
     },
     Fuzzy {
         matcher: Matcher,
-        pat: Pattern,
+        atom: Atom,
         /// Holds (haystack, item, score)
-        items: Vec<(String, T, u32)>,
+        items: Vec<(String, T, u16)>,
     },
 }
 
@@ -65,7 +65,7 @@ impl<T> NuMatcher<T> {
                 }
             }
             MatchAlgorithm::Fuzzy => {
-                let pat = Pattern::new(
+                let atom = Atom::new(
                     needle,
                     if options.case_sensitive {
                         CaseMatching::Respect
@@ -74,13 +74,14 @@ impl<T> NuMatcher<T> {
                     },
                     Normalization::Smart,
                     AtomKind::Fuzzy,
+                    false,
                 );
                 NuMatcher {
                     options,
                     needle: needle.to_owned(),
                     state: State::Fuzzy {
                         matcher: Matcher::new(Config::DEFAULT),
-                        pat,
+                        atom,
                         items: Vec::new(),
                     },
                 }
@@ -115,13 +116,13 @@ impl<T> NuMatcher<T> {
             }
             State::Fuzzy {
                 matcher,
-                pat,
+                atom,
                 items,
             } => {
                 let mut haystack_buf = Vec::new();
                 let haystack_utf32 = Utf32Str::new(trim_quotes_str(haystack), &mut haystack_buf);
                 let mut indices = Vec::new();
-                let Some(score) = pat.indices(haystack_utf32, matcher, &mut indices) else {
+                let Some(score) = atom.indices(haystack_utf32, matcher, &mut indices) else {
                     return false;
                 };
                 if let Some(item) = item {
@@ -291,5 +292,23 @@ mod test {
         }
         // Sort by score, then in alphabetical order
         assert_eq!(vec!["fob", "foo bar", "foo/bar"], matcher.results());
+    }
+
+    #[test]
+    fn match_algorithm_fuzzy_sort_strip() {
+        let options = CompletionOptions {
+            match_algorithm: MatchAlgorithm::Fuzzy,
+            ..Default::default()
+        };
+        let mut matcher = NuMatcher::new("'love spaces' ", options);
+        for item in [
+            "'i love spaces'",
+            "'i love spaces' so much",
+            "'lovespaces' ",
+        ] {
+            matcher.add(item, item);
+        }
+        // Make sure the spaces are respected
+        assert_eq!(vec!["'i love spaces' so much"], matcher.results());
     }
 }

crates/nu-cli/tests/completions/mod.rs
@@ -1038,8 +1038,8 @@ fn subcommand_completions() {
     match_suggestions(
         &vec![
             "food bar".to_string(),
-            "foo-test-command aagap bcr".to_string(),
             "foo-test-command bar".to_string(),
+            "foo-test-command aagap bcr".to_string(),
         ],
         &suggestions,
     );