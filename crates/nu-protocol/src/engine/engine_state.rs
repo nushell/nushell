eb255ba0a07351db7fc3af057608b5fbb77f4ace
cosineblast <55855728+cosineblast@users.noreply.github.com>
2025-01-26 18:37:47 -0300

    Use contains_key instead of is_some

--- crates/nu-protocol/src/engine/engine_state.rs
+++ crates/nu-protocol/src/engine/engine_state.rs
@@ -144,7 +144,7 @@ impl Jobs {
 
     // this is useful when you want to remove a job form the list and add it back later
     pub fn add_job_with_id(&mut self, id: JobId, job: Job) -> Result<(), &'static str> {
-        if self.jobs.get(&id).is_some() {
+        if self.jobs.contains_key(&id) {
             Err("job already exists")
         } else {
             self.jobs.insert(id, job);
