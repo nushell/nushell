1e5d2c37bd25acb65595a2470948c2c942234817
Bahex <bahey1999@gmail.com>
2025-02-03 17:27:58 +0300

    Add Attribute and AttributeBlock

---
+++ crates/nu-protocol/src/ast/attribute.rs
@@ -0,0 +1,14 @@
+use super::Expression;
+use crate::Span;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Attribute {
+    pub expr: Expression,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct AttributeBlock {
+    pub attributes: Vec<Attribute>,
+    pub item: Box<Expression>,
+}
