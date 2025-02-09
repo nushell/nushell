758da65caa1989e4784a7e7ddc48f8eb0295e38e
NotTheDr01ds <32344964+NotTheDr01ds@users.noreply.github.com>
2025-02-09 07:09:09 -0500

    Add test for (char eol) and synonyms

--- crates/nu-command/tests/commands/platform/char_.rs
+++ crates/nu-command/tests/commands/platform/char_.rs
@@ -10,3 +10,13 @@ fn test_char_list_outputs_table() {
 
     assert_eq!(actual.out, "113");
 }
+
+#[test]
+fn test_char_eol() {
+    let actual = nu!(r#"
+        let expected = if ($nu.os-info.name == 'windows') { "\r\n" } else { "\n" }
+        ((char lsep) == $expected) and ((char line_sep) == $expected) and ((char eol) == $expected)
+    "#);
+
+    assert_eq!(actual.out, "true");
+}
