59d0879a0cf6835f9777005c19ba6810c8804ee6
Bahex <bahey1999@gmail.com>
2025-02-10 03:50:28 +0300

    remove `attr env` and `attr wrapped`, subject to further discussion

--- crates/nu-cmd-lang/src/core_commands/attr/mod.rs
+++ crates/nu-cmd-lang/src/core_commands/attr/mod.rs
@@ -1,9 +1,5 @@
-mod env;
 mod example;
 mod search_terms;
-mod wrapped;
 
-pub use env::AttrEnv;
 pub use example::AttrExample;
 pub use search_terms::AttrSearchTerms;
-pub use wrapped::AttrWrapped;
