1d713be69da47609579bb1bfe614a448a45ad394
Ian Manske <ian.manske@pm.me>
2025-02-22 16:39:34 -0800

    Shrink `Value` size assertion

--- crates/nu-protocol/src/value/mod.rs
+++ crates/nu-protocol/src/value/mod.rs
@@ -244,7 +244,7 @@ impl Clone for Value {
 // This is to document/enforce the size of `Value` in bytes.
 // We should try to avoid increasing the size of `Value`,
 // and PRs that do so will have to change the number below so that it's noted in review.
-const _: () = assert!(std::mem::size_of::<Value>() <= 56);
+const _: () = assert!(std::mem::size_of::<Value>() <= 48);
 
 impl Value {
     fn cant_convert_to<T>(&self, typ: &str) -> Result<T, ShellError> {
