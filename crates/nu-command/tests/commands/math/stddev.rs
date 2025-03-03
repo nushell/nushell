4785f5f252cac223e66fc27b31d9638f0490431e
Loïc Riegel <loic.riegel@outlook.fr>
2025-03-03 19:17:26 +0100

    bugfix: math commands now return error with infinite range [#15135]

--- crates/nu-command/tests/commands/math/stddev.rs
+++ crates/nu-command/tests/commands/math/stddev.rs
@@ -5,3 +5,10 @@ fn const_avg() {
     let actual = nu!("const SDEV = [1 2] | math stddev; $SDEV");
     assert_eq!(actual.out, "0.5");
 }
+
+#[test]
+fn cannot_stddev_range() {
+    let actual = nu!("0..5 | math stddev");
+
+    assert!(actual.err.contains("nu::parser::input_type_mismatch"));
+}
--- crates/nu-command/tests/commands/math/variance.rs
+++ crates/nu-command/tests/commands/math/variance.rs
@@ -5,3 +5,10 @@ fn const_variance() {
     let actual = nu!("const VAR = [1 2 3 4 5] | math variance; $VAR");
     assert_eq!(actual.out, "2");
 }
+
+#[test]
+fn cannot_variance_range() {
+    let actual = nu!("0..5 | math variance");
+
+    assert!(actual.err.contains("nu::parser::input_type_mismatch"));
+}
