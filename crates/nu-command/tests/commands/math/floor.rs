9318042aebfb06207e749df79a31038423e2b2ed
Loïc Riegel <loic.riegel@outlook.fr>
2025-03-03 19:17:26 +0100

    bugfix: math commands now return error with infinite range [#15135]

--- crates/nu-command/tests/commands/math/floor.rs
+++ crates/nu-command/tests/commands/math/floor.rs
@@ -5,3 +5,10 @@ fn const_floor() {
     let actual = nu!("const FLOOR = 15.5 | math floor; $FLOOR");
     assert_eq!(actual.out, "15");
 }
+
+#[test]
+fn cannot_floor_range() {
+    let actual = nu!("0.. | math floor");
+
+    assert!(actual.err.contains("nu::parser::input_type_mismatch"));
+}
