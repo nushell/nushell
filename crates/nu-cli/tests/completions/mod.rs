e716bdfee3e8c11952b6116166cbca3f21ab9863
blindfs <blindfs19@gmail.com>
2025-03-08 21:46:36 +0800

    fix: typo check

--- crates/nu-cli/tests/completions/mod.rs
+++ crates/nu-cli/tests/completions/mod.rs
@@ -594,7 +594,7 @@ fn exportable_completions() {
     let suggestions = completer.complete(completion_str, completion_str.len());
     match_suggestions(&vec!["TAU"], &suggestions);
 
-    let completion_str = "use 🤔🐘 'fo";
+    let completion_str = "use 🤔🐘 'foo";
     let suggestions = completer.complete(completion_str, completion_str.len());
     match_suggestions(&vec!["foo"], &suggestions);
 }
