32c3f054f59e0a776b76b5a70be29abcc110fd6e
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2025-03-14 21:09:09 -0500

    remove mimalloc allocator

--- crates/nu_plugin_polars/src/main.rs
+++ crates/nu_plugin_polars/src/main.rs
@@ -1,9 +1,6 @@
 use nu_plugin::{serve_plugin, MsgPackSerializer};
 use nu_plugin_polars::PolarsPlugin;
 
-#[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
 fn main() {
     env_logger::init();
 
