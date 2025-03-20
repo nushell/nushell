bd48da9eb7d117b4ae26efba57db286dacb0f601
Florent Vilmart <florent@flovilmart.com>
2025-03-20 16:47:51 -0400

    feat: Use reedline for basic input implementation

--- crates/nu-command/src/platform/input/input_.rs
+++ crates/nu-command/src/platform/input/input_.rs
@@ -1,18 +1,15 @@
-use crossterm::{
-    cursor,
-    event::{Event, KeyCode, KeyEventKind, KeyModifiers},
-    execute,
-    style::Print,
-    terminal::{self, ClearType},
-};
-use itertools::Itertools;
+use crate::platform::input::legacy_input::LegacyInput;
+use crate::platform::input::reedline_prompt::ReedlinePrompt;
 use nu_engine::command_prelude::*;
 use nu_protocol::shell_error::io::IoError;
+use reedline::{Reedline, Signal};
 
-use std::{io::Write, time::Duration};
+use std::io::Write;
 
 #[derive(Clone)]
 pub struct Input;
+
+impl LegacyInput for Input {}
 
 impl Command for Input {
     fn name(&self) -> &str {
@@ -61,11 +58,11 @@ impl Command for Input {
         call: &Call,
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let prompt: Option<String> = call.opt(engine_state, stack, 0)?;
+        let prompt_str: Option<String> = call.opt(engine_state, stack, 0)?;
         let bytes_until: Option<String> = call.get_flag(engine_state, stack, "bytes-until-any")?;
         let suppress_output = call.has_flag(engine_state, stack, "suppress-output")?;
-        let numchar: Option<Spanned<i64>> = call.get_flag(engine_state, stack, "numchar")?;
-        let numchar: Spanned<i64> = numchar.unwrap_or(Spanned {
+        let numchar_flag: Option<Spanned<i64>> = call.get_flag(engine_state, stack, "numchar")?;
+        let numchar: Spanned<i64> = numchar_flag.unwrap_or(Spanned {
             item: i64::MAX,
             span: call.head,
         });
@@ -81,8 +78,15 @@ impl Command for Input {
             });
         }
 
+        // Those 2 options are not supported by reedline, default to the legacy
+        // implementation
+        if suppress_output || bytes_until.is_some() || numchar_flag.is_some() {
+            return self.legacy_input(engine_state, stack, call, _input);
+        }
+
         let default_val: Option<String> = call.get_flag(engine_state, stack, "default")?;
-        if let Some(prompt) = &prompt {
+
+        if let Some(prompt) = &prompt_str {
             match &default_val {
                 None => print!("{prompt}"),
                 Some(val) => print!("{prompt} (default: {val})"),
@@ -91,82 +95,35 @@ impl Command for Input {
         }
 
         let mut buf = String::new();
-
-        crossterm::terminal::enable_raw_mode().map_err(&from_io_error)?;
-        // clear terminal events
-        while crossterm::event::poll(Duration::from_secs(0)).map_err(&from_io_error)? {
-            // If there's an event, read it to remove it from the queue
-            let _ = crossterm::event::read().map_err(&from_io_error)?;
-        }
+        let prompt = ReedlinePrompt {
+            left_prompt: prompt_str.unwrap_or("".to_string()),
+            indicator: "".to_string(), // TODO: Add support for custom prompt indicators
+                                       // for now, and backwards compat, we just use the  empty
+                                       // string
+        };
+        let mut line_editor = Reedline::create();
+        // TODO handle options
 
         loop {
             if i64::try_from(buf.len()).unwrap_or(0) >= numchar.item {
                 break;
             }
-            match crossterm::event::read() {
-                Ok(Event::Key(k)) => match k.kind {
-                    KeyEventKind::Press | KeyEventKind::Repeat => {
-                        match k.code {
-                            // TODO: maintain keycode parity with existing command
-                            KeyCode::Char(c) => {
-                                if k.modifiers == KeyModifiers::ALT
-                                    || k.modifiers == KeyModifiers::CONTROL
-                                {
-                                    if k.modifiers == KeyModifiers::CONTROL && c == 'c' {
-                                        crossterm::terminal::disable_raw_mode()
-                                            .map_err(&from_io_error)?;
-                                        return Err(IoError::new(
-                                            std::io::ErrorKind::Interrupted,
-                                            call.head,
-                                            None,
-                                        )
-                                        .into());
-                                    }
-                                    continue;
-                                }
-
-                                if let Some(bytes_until) = bytes_until.as_ref() {
-                                    if bytes_until.bytes().contains(&(c as u8)) {
-                                        break;
-                                    }
-                                }
-                                buf.push(c);
-                            }
-                            KeyCode::Backspace => {
-                                let _ = buf.pop();
-                            }
-                            KeyCode::Enter => {
-                                break;
-                            }
-                            _ => continue,
-                        }
-                    }
-                    _ => continue,
-                },
+            match line_editor.read_line(&prompt) {
+                Ok(Signal::Success(buffer)) => {
+                    buf.push_str(&buffer);
+                    break;
+                }
+                Ok(Signal::CtrlC) => {
+                    return Err(
+                        IoError::new(std::io::ErrorKind::Interrupted, call.head, None).into(),
+                    );
+                }
                 Ok(_) => continue,
                 Err(event_error) => {
                     crossterm::terminal::disable_raw_mode().map_err(&from_io_error)?;
                     return Err(from_io_error(event_error).into());
                 }
             }
-            if !suppress_output {
-                // clear the current line and print the current buffer
-                execute!(
-                    std::io::stdout(),
-                    terminal::Clear(ClearType::CurrentLine),
-                    cursor::MoveToColumn(0),
-                )
-                .map_err(|err| IoError::new(err.kind(), call.head, None))?;
-                if let Some(prompt) = &prompt {
-                    execute!(std::io::stdout(), Print(prompt.to_string()))
-                        .map_err(&from_io_error)?;
-                }
-                execute!(std::io::stdout(), Print(buf.to_string())).map_err(&from_io_error)?;
-            }
-        }
-        crossterm::terminal::disable_raw_mode().map_err(&from_io_error)?;
-        if !suppress_output {
-            std::io::stdout().write_all(b"\n").map_err(&from_io_error)?;
         }
         match default_val {
             Some(val) if buf.is_empty() => Ok(Value::string(val, call.head).into_pipeline_data()),
