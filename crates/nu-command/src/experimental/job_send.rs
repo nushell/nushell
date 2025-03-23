4c716cef756f619eba7f3d168b9ddcf143e65765
cosineblast <55855728+cosineblast@users.noreply.github.com>
2025-03-22 23:25:27 -0300

    Improve job send documentation
    It now mentions job id 0 is the root nushell job

--- crates/nu-command/src/experimental/job_send.rs
+++ crates/nu-command/src/experimental/job_send.rs
@@ -17,7 +17,8 @@ impl Command for JobSend {
         r#"
 This command sends a message to a background job, which can then read sent messages
 in a first-in-first-out fashion with `job recv`. When it does so, it may additionally specify a numeric filter tag,
-in which case it will only read messages sent with the exact same filter tag.
+in which case it will only read messages sent with the exact same filter tag. 
+In particular, the id 0 refers to the main/initial nushell thread.
 
 A message can be any nushell value, and streams are always collected before being sent.
 
