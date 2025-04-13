76664c7ff871bdffb3cf82e14c2ae4fc7ccea4f9
kidrigger <rex16saddler@gmail.com>
2025-04-13 16:57:52 +0200

    Fix examples about RFC3339 format in `date now` and `format date`.
    Replace example on `date now | debug` with `date now | format date "%+"`.
    Add RFC3339 "%+" format string example on `format date`.
    
    Users can now find how to format date-time to RFC3339.
    
    FIXES: #15168

--- crates/nu-command/src/date/now.rs
+++ crates/nu-command/src/date/now.rs
@@ -43,6 +43,11 @@ impl Command for DateNow {
                 result: None,
             },
             Example {
+                description: "Get the current date and display it in RFC3339 format string.",
+                example: r#"date now | format date "%+""#,
+                result: None,
+            },
+            Example {
                 description: "Get the time duration since 2019-04-30.",
                 example: r#"(date now) - 2019-05-01"#,
                 result: None,
@@ -50,11 +55,6 @@ impl Command for DateNow {
             Example {
                 description: "Get the time duration since a more specific time.",
                 example: r#"(date now) - 2019-05-01T04:12:05.20+08:00"#,
-                result: None,
-            },
-            Example {
-                description: "Get current time in full RFC 3339 format with time zone.",
-                example: r#"date now | debug"#,
                 result: None,
             },
         ]
