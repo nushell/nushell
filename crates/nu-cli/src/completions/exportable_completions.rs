nushell/nushell

commit 56d7e4bb89b127731ed8296d5abc898d72e7bc73

refactor(completion, lsp): include decl_id in suggetion_kind for later usage (#15536)
# Description

Should be more performant, calling for `find_decl` by name for all
entries is generally a heavy op.

# User-Facing Changes

NA

# Tests + Formatting

# After Submitting

crates/nu-cli/src/completions/exportable_completions.rs
@@ -69,7 +69,7 @@ impl Completer for ExportableCompletion<'_> {
                     wrapped_name(name),
                     Some(cmd.description().to_string()),
                     None,
-                    SuggestionKind::Command(cmd.command_type()),
+                    SuggestionKind::Command(cmd.command_type(), Some(*decl_id)),
                 );
             }
         }

crates/nu-lsp/src/completion.rs
@@ -62,17 +62,12 @@ impl LanguageServer {
         suggestion: SemanticSuggestion,
         range: Range,
     ) -> CompletionItem {
-        let decl_id = suggestion.kind.as_ref().and_then(|kind| {
-            matches!(kind, SuggestionKind::Command(_))
-                .then_some(engine_state.find_decl(suggestion.suggestion.value.as_bytes(), &[])?)
-        });
-
         let mut snippet_text = suggestion.suggestion.value.clone();
         let mut doc_string = suggestion.suggestion.extra.map(|ex| ex.join("\n"));
         let mut insert_text_format = None;
         let mut idx = 0;
         // use snippet as `insert_text_format` for command argument completion
-        if let Some(decl_id) = decl_id {
+        if let Some(SuggestionKind::Command(_, Some(decl_id))) = suggestion.kind {
             let cmd = engine_state.get_decl(decl_id);
             doc_string = Some(Self::get_decl_description(cmd, true));
             insert_text_format = Some(InsertTextFormat::SNIPPET);
@@ -138,7 +133,7 @@ impl LanguageServer {
                 .as_ref()
                 .map(|kind| match kind {
                     SuggestionKind::Value(t) => t.to_string(),
-                    SuggestionKind::Command(cmd) => cmd.to_string(),
+                    SuggestionKind::Command(cmd, _) => cmd.to_string(),
                     SuggestionKind::Module => "module".to_string(),
                     SuggestionKind::Operator => "operator".to_string(),
                     SuggestionKind::Variable => "variable".to_string(),
@@ -172,7 +167,7 @@ impl LanguageServer {
                 _ => None,
             },
             SuggestionKind::CellPath => Some(CompletionItemKind::PROPERTY),
-            SuggestionKind::Command(c) => match c {
+            SuggestionKind::Command(c, _) => match c {
                 CommandType::Keyword => Some(CompletionItemKind::KEYWORD),
                 CommandType::Builtin => Some(CompletionItemKind::FUNCTION),
                 CommandType::Alias => Some(CompletionItemKind::REFERENCE),

crates/nu-cli/src/completions/attribute_completions.rs
@@ -27,7 +27,7 @@ impl Completer for AttributeCompletion {
         let attr_commands =
             working_set.find_commands_by_predicate(|s| s.starts_with(b"attr "), true);
 
-        for (name, desc, ty) in attr_commands {
+        for (decl_id, name, desc, ty) in attr_commands {
             let name = name.strip_prefix(b"attr ").unwrap_or(&name);
             matcher.add_semantic_suggestion(SemanticSuggestion {
                 suggestion: Suggestion {
@@ -41,7 +41,7 @@ impl Completer for AttributeCompletion {
                     },
                     append_whitespace: false,
                 },
-                kind: Some(SuggestionKind::Command(ty)),
+                kind: Some(SuggestionKind::Command(ty, Some(decl_id))),
             });
         }
 
@@ -78,7 +78,7 @@ impl Completer for AttributableCompletion {
                     },
                     append_whitespace: false,
                 },
-                kind: Some(SuggestionKind::Command(cmd.command_type())),
+                kind: Some(SuggestionKind::Command(cmd.command_type(), None)),
             });
         }
 

crates/nu-cli/src/completions/base.rs
@@ -1,7 +1,7 @@
 use crate::completions::CompletionOptions;
 use nu_protocol::{
     engine::{Stack, StateWorkingSet},
-    Span,
+    DeclId, Span,
 };
 use reedline::Suggestion;
 
@@ -28,7 +28,7 @@ pub struct SemanticSuggestion {
 // TODO: think about name: maybe suggestion context?
 #[derive(Clone, Debug, PartialEq)]
 pub enum SuggestionKind {
-    Command(nu_protocol::engine::CommandType),
+    Command(nu_protocol::engine::CommandType, Option<DeclId>),
     Value(nu_protocol::Type),
     CellPath,
     Directory,

crates/nu-cli/src/completions/command_completions.rs
@@ -75,7 +75,10 @@ impl CommandCompletion {
                                             append_whitespace: true,
                                             ..Default::default()
                                         },
-                                        kind: Some(SuggestionKind::Command(CommandType::External)),
+                                        kind: Some(SuggestionKind::Command(
+                                            CommandType::External,
+                                            None,
+                                        )),
                                     },
                                 );
                             }
@@ -112,7 +115,7 @@ impl Completer for CommandCompletion {
                 },
                 true,
             );
-            for (name, description, typ) in filtered_commands {
+            for (decl_id, name, description, typ) in filtered_commands {
                 let name = String::from_utf8_lossy(&name);
                 internal_suggs.insert(
                     name.to_string(),
@@ -124,7 +127,7 @@ impl Completer for CommandCompletion {
                             append_whitespace: true,
                             ..Suggestion::default()
                         },
-                        kind: Some(SuggestionKind::Command(typ)),
+                        kind: Some(SuggestionKind::Command(typ, Some(decl_id))),
                     },
                 );
             }

crates/nu-protocol/src/engine/engine_state.rs
@@ -731,18 +731,19 @@ impl EngineState {
         &self,
         mut predicate: impl FnMut(&[u8]) -> bool,
         ignore_deprecated: bool,
-    ) -> Vec<(Vec<u8>, Option<String>, CommandType)> {
+    ) -> Vec<(DeclId, Vec<u8>, Option<String>, CommandType)> {
         let mut output = vec![];
 
         for overlay_frame in self.active_overlays(&[]).rev() {
-            for decl in &overlay_frame.decls {
-                if overlay_frame.visibility.is_decl_id_visible(decl.1) && predicate(decl.0) {
-                    let command = self.get_decl(*decl.1);
+            for (name, decl_id) in &overlay_frame.decls {
+                if overlay_frame.visibility.is_decl_id_visible(decl_id) && predicate(name) {
+                    let command = self.get_decl(*decl_id);
                     if ignore_deprecated && command.signature().category == Category::Removed {
                         continue;
                     }
                     output.push((
-                        decl.0.clone(),
+                        *decl_id,
+                        name.clone(),
                         Some(command.description().to_string()),
                         command.command_type(),
                     ));

crates/nu-protocol/src/engine/state_working_set.rs
@@ -780,21 +780,22 @@ impl<'a> StateWorkingSet<'a> {
         &self,
         mut predicate: impl FnMut(&[u8]) -> bool,
         ignore_deprecated: bool,
-    ) -> Vec<(Vec<u8>, Option<String>, CommandType)> {
+    ) -> Vec<(DeclId, Vec<u8>, Option<String>, CommandType)> {
         let mut output = vec![];
 
         for scope_frame in self.delta.scope.iter().rev() {
             for overlay_id in scope_frame.active_overlays.iter().rev() {
                 let overlay_frame = scope_frame.get_overlay(*overlay_id);
 
-                for decl in &overlay_frame.decls {
-                    if overlay_frame.visibility.is_decl_id_visible(decl.1) && predicate(decl.0) {
-                        let command = self.get_decl(*decl.1);
+                for (name, decl_id) in &overlay_frame.decls {
+                    if overlay_frame.visibility.is_decl_id_visible(decl_id) && predicate(name) {
+                        let command = self.get_decl(*decl_id);
                         if ignore_deprecated && command.signature().category == Category::Removed {
                             continue;
                         }
                         output.push((
-                            decl.0.clone(),
+                            *decl_id,
+                            name.clone(),
                             Some(command.description().to_string()),
                             command.command_type(),
                         ));

commit cd4560e97ab2c08b966f43523b30dd59cf6ace2f

fix(lsp): a panic caused by completion with decl_id out of range (#15576)
Fixes a bug caused by #15536 
Sorry about that, @fdncred 

# Description

I've made the panic reproducible in the test case.

TLDR: completer will sometimes return new decl_ids outside of the range
of the engine_state passed in.

# User-Facing Changes

bug fix

# Tests + Formatting

+1

# After Submitting

crates/nu-cli/src/completions/exportable_completions.rs
@@ -69,7 +69,8 @@ impl Completer for ExportableCompletion<'_> {
                     wrapped_name(name),
                     Some(cmd.description().to_string()),
                     None,
-                    SuggestionKind::Command(cmd.command_type(), Some(*decl_id)),
+                    // `None` here avoids arguments being expanded by snippet edit style for lsp
+                    SuggestionKind::Command(cmd.command_type(), None),
                 );
             }
         }

crates/nu-lsp/src/completion.rs
@@ -68,52 +68,60 @@ impl LanguageServer {
         let mut idx = 0;
         // use snippet as `insert_text_format` for command argument completion
         if let Some(SuggestionKind::Command(_, Some(decl_id))) = suggestion.kind {
-            let cmd = engine_state.get_decl(decl_id);
-            doc_string = Some(Self::get_decl_description(cmd, true));
-            insert_text_format = Some(InsertTextFormat::SNIPPET);
-            let signature = cmd.signature();
-            // add curly brackets around block arguments
-            // and keywords, e.g. `=` in `alias foo = bar`
-            let mut arg_wrapper = |arg: &PositionalArg, text: String, optional: bool| -> String {
-                idx += 1;
-                match &arg.shape {
-                    SyntaxShape::Block | SyntaxShape::MatchBlock => {
-                        format!("{{ ${{{}:{}}} }}", idx, text)
-                    }
-                    SyntaxShape::Keyword(kwd, _) => {
-                        // NOTE: If optional, the keyword should also be in a placeholder so that it can be removed easily.
-                        // Here we choose to use nested placeholders. Note that some editors don't fully support this format,
-                        // but usually they will simply ignore the inner ones, so it should be fine.
-                        if optional {
-                            idx += 1;
-                            format!(
-                                "${{{}:{} ${{{}:{}}}}}",
-                                idx - 1,
-                                String::from_utf8_lossy(kwd),
-                                idx,
-                                text
-                            )
-                        } else {
-                            format!("{} ${{{}:{}}}", String::from_utf8_lossy(kwd), idx, text)
+            // NOTE: for new commands defined in current context,
+            // which are not present in the engine state, skip the documentation and snippet.
+            if engine_state.num_decls() > decl_id.get() {
+                let cmd = engine_state.get_decl(decl_id);
+                doc_string = Some(Self::get_decl_description(cmd, true));
+                insert_text_format = Some(InsertTextFormat::SNIPPET);
+                let signature = cmd.signature();
+                // add curly brackets around block arguments
+                // and keywords, e.g. `=` in `alias foo = bar`
+                let mut arg_wrapper = |arg: &PositionalArg,
+                                       text: String,
+                                       optional: bool|
+                 -> String {
+                    idx += 1;
+                    match &arg.shape {
+                        SyntaxShape::Block | SyntaxShape::MatchBlock => {
+                            format!("{{ ${{{}:{}}} }}", idx, text)
                         }
+                        SyntaxShape::Keyword(kwd, _) => {
+                            // NOTE: If optional, the keyword should also be in a placeholder so that it can be removed easily.
+                            // Here we choose to use nested placeholders. Note that some editors don't fully support this format,
+                            // but usually they will simply ignore the inner ones, so it should be fine.
+                            if optional {
+                                idx += 1;
+                                format!(
+                                    "${{{}:{} ${{{}:{}}}}}",
+                                    idx - 1,
+                                    String::from_utf8_lossy(kwd),
+                                    idx,
+                                    text
+                                )
+                            } else {
+                                format!("{} ${{{}:{}}}", String::from_utf8_lossy(kwd), idx, text)
+                            }
+                        }
+                        _ => format!("${{{}:{}}}", idx, text),
                     }
-                    _ => format!("${{{}:{}}}", idx, text),
-                }
-            };
+                };
 
-            for required in signature.required_positional {
-                snippet_text.push(' ');
-                snippet_text
-                    .push_str(arg_wrapper(&required, required.name.clone(), false).as_str());
-            }
-            for optional in signature.optional_positional {
-                snippet_text.push(' ');
-                snippet_text
-                    .push_str(arg_wrapper(&optional, format!("{}?", optional.name), true).as_str());
-            }
-            if let Some(rest) = signature.rest_positional {
-                idx += 1;
-                snippet_text.push_str(format!(" ${{{}:...{}}}", idx, rest.name).as_str());
+                for required in signature.required_positional {
+                    snippet_text.push(' ');
+                    snippet_text
+                        .push_str(arg_wrapper(&required, required.name.clone(), false).as_str());
+                }
+                for optional in signature.optional_positional {
+                    snippet_text.push(' ');
+                    snippet_text.push_str(
+                        arg_wrapper(&optional, format!("{}?", optional.name), true).as_str(),
+                    );
+                }
+                if let Some(rest) = signature.rest_positional {
+                    idx += 1;
+                    snippet_text.push_str(format!(" ${{{}:...{}}}", idx, rest.name).as_str());
+                }
             }
         }
         // no extra space for a command with args expanded in the snippet
@@ -328,6 +336,17 @@ mod tests {
                 "kind": 17
             })
         ));
+
+        // fallback completion on a newly defined command,
+        // the decl_id is missing in the engine state, this shouldn't cause any panic.
+        let resp = send_complete_request(&client_connection, script.clone(), 13, 9);
+        assert_json_include!(
+            actual: result_from_message(resp),
+            expected: serde_json::json!([
+                // defined before the cursor
+                { "label": "config n foo bar", "detail": detail_str, "kind": 2 },
+            ])
+        );
 
         // inside parenthesis
         let resp = send_complete_request(&client_connection, script, 10, 34);

tests/fixtures/lsp/completion/command.nu
@@ -10,3 +10,5 @@ def "config n foo bar" [
   echo "🤔🐘"
   | str substring (str substring -)
 }
+
+config n # don't panic!