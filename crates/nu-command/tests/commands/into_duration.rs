dbae48a9a51a57df38d92132241e9a1ddb58ceb3
Loïc Riegel <loic.riegel@outlook.fr>
2025-04-18 20:43:39 +0200

    tests about error paths and invalid usage

--- crates/nu-command/tests/commands/into_duration.rs
+++ crates/nu-command/tests/commands/into_duration.rs
@@ -50,3 +50,46 @@ fn into_duration_table_column() {
 
     assert_eq!(actual.out, expected.out);
 }
+
+// Tests error paths
+
+#[test]
+fn into_duration_from_record_fails_with_wrong_type() {
+    let actual = nu!(r#"{week: '10'} | into duration"#);
+
+    assert!(actual
+        .err
+        .contains("nu::shell::only_supports_this_input_type"));
+}
+
+#[test]
+fn into_duration_from_record_fails_with_invalid_date_time_values() {
+    let actual = nu!(r#"{week: -10} | into duration"#);
+
+    assert!(actual.err.contains("nu::shell::incorrect_value"));
+}
+
+#[test]
+fn into_duration_from_record_fails_with_invalid_sign() {
+    let actual = nu!(r#"{week: 10, sign: 'x'} | into duration"#);
+
+    assert!(actual.err.contains("nu::shell::incorrect_value"));
+}
+
+// Tests invalid usage
+
+#[test]
+fn into_duration_from_record_fails_with_unknown_key() {
+    let actual = nu!(r#"{week: 10, unknown: 1} | into duration"#);
+
+    assert!(actual.err.contains("nu::shell::unsupported_input"));
+}
+
+#[test]
+fn into_duration_from_record_incompatible_with_unit_flag() {
+    let actual = nu!(
+        r#"{week: 10, day: 1, hour: 2, minute: 3, second: 4, sign: '-'} | into duration --unit sec"#
+    );
+
+    assert!(actual.err.contains("nu::shell::incompatible_parameters"));
+}
