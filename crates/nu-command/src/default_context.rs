a1497716f1333b70e0c82b21c8a773116f43e405
Renan Ribeiro <55855728+cosineblast@users.noreply.github.com>
2025-04-21 09:08:00 -0300

    Add job tags (#15555)
    # Description
    
    This PR implements job tagging through the usage of a new `job tag`
    command and a `--tag` for `job spawn`
    
    Closes #15354
    
    # User-Facing Changes
    
    - New `job tag` command
    - Job list may now have an additional `tag` column for the tag of jobs
    (rows representing jobs without tags do not have this column filled)
    - New `--tag` flag for `job spawn`
    
    # Tests + Formatting
    
    Integration tests are provided to test the newly implemented features
    
    # After Submitting
    
    Possibly document job tagging in the jobs documentation

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -452,6 +452,7 @@ pub fn add_shell_command_context(mut engine_state: EngineState) -> EngineState {
             JobSpawn,
             JobList,
             JobKill,
+            JobTag,
             Job,
         };
 
