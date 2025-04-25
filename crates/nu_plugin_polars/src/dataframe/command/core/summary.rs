7eafe4b73128d1d33b8690c476f66cb23293391b
Julian Amarilla <julian.amarilla@compass.com>
2025-04-25 02:01:35 -0300

    make polars commands propagate metadata

--- crates/nu_plugin_polars/src/dataframe/command/core/summary.rs
+++ crates/nu_plugin_polars/src/dataframe/command/core/summary.rs
@@ -112,6 +112,20 @@ impl PluginCommand for Summary {
         call: &EvaluatedCall,
         input: PipelineData,
     ) -> Result<PipelineData, LabeledError> {
+        let metadata = input.metadata();
+        self.run_inner(plugin, engine, call, input)
+            .map(|pd| pd.set_metadata(metadata))
+    }
+}
+
+impl Summary {
+    fn run_inner(
+        &self,
+        plugin: &PolarsPlugin,
+        engine: &EngineInterface,
+        call: &EvaluatedCall,
+        input: PipelineData,
+    ) -> Result<PipelineData, LabeledError> {
         command(plugin, engine, call, input).map_err(LabeledError::from)
     }
 }
--- crates/nu_plugin_polars/src/dataframe/command/data/len.rs
+++ crates/nu_plugin_polars/src/dataframe/command/data/len.rs
@@ -53,6 +53,20 @@ impl PluginCommand for ExprLen {
         plugin: &Self::Plugin,
         engine: &EngineInterface,
         call: &EvaluatedCall,
+        input: PipelineData,
+    ) -> Result<PipelineData, LabeledError> {
+        let metadata = input.metadata();
+        self.run_inner(plugin, engine, call, input)
+            .map(|pd| pd.set_metadata(metadata))
+    }
+}
+
+impl ExprLen {
+    fn run_inner(
+        &self,
+        plugin: &PolarsPlugin,
+        engine: &EngineInterface,
+        call: &EvaluatedCall,
         _input: PipelineData,
     ) -> Result<PipelineData, LabeledError> {
         let res: NuExpression = polars::prelude::len().into();
