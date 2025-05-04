9a809c966c83121812b2515bdec60db520801016
hustcer <hustcer@outlook.com>
2025-05-04 20:59:57 +0800

    chore: Update Copyright

--- scripts/build.rs
+++ scripts/build.rs
@@ -3,7 +3,7 @@ fn main() {
     let mut res = winresource::WindowsResource::new();
     res.set("ProductName", "Nushell");
     res.set("FileDescription", "Nushell");
-    res.set("LegalCopyright", "Copyright (C) 2022");
+    res.set("LegalCopyright", "Copyright (C) 2025");
     res.set_icon("assets/nu_logo.ico");
     res.compile()
         .expect("Failed to run the Windows resource compiler (rc.exe)");
