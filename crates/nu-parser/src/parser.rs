759e6a8fa289d2ac8d2244615a9074a2bc510459
Bahex <17417311+Bahex@users.noreply.github.com>
2025-05-11 16:58:36 +0300

    fix: apply the fix for external command arguments too

--- crates/nu-parser/src/parser.rs
+++ crates/nu-parser/src/parser.rs
@@ -455,8 +455,10 @@ fn parse_external_arg(working_set: &mut StateWorkingSet, span: Span) -> External
 fn parse_regular_external_arg(working_set: &mut StateWorkingSet, span: Span) -> Expression {
     let contents = working_set.get_span_contents(span);
 
-    if contents.starts_with(b"$") || contents.starts_with(b"(") {
+    if contents.starts_with(b"$") {
         parse_dollar_expr(working_set, span)
+    } else if contents.starts_with(b"(") {
+        parse_paren_expr(working_set, span, &SyntaxShape::Any)
     } else if contents.starts_with(b"[") {
         parse_list_expression(working_set, span, &SyntaxShape::Any)
     } else {
