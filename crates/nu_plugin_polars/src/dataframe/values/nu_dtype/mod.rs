75e05a4fbf7b50e485bd8f9051d7f93a56fa3b1b
Jack Wright <ayax79@gmail.com>
2025-05-12 08:17:53 -0700

    More enum work

--- crates/nu_plugin_polars/src/dataframe/values/nu_dtype/mod.rs
+++ crates/nu_plugin_polars/src/dataframe/values/nu_dtype/mod.rs
@@ -330,6 +330,28 @@ fn str_to_time_unit(ts_string: &str, span: Span) -> Result<TimeUnit, ShellError>
 pub(crate) fn dtype_to_value(dtype: &DataType, span: Span) -> Value {
     match dtype {
         DataType::Struct(fields) => fields_to_value(fields.iter().cloned(), span),
+        DataType::Enum(_, _) => Value::list(
+            get_categories(dtype)
+                .unwrap_or_default()
+                .iter()
+                .map(|s| Value::string(s, span))
+                .collect(),
+            span,
+        ),
         _ => Value::string(dtype.to_string().replace('[', "<").replace(']', ">"), span),
+    }
+}
+
+pub(super) fn get_categories(dtype: &DataType) -> Option<Vec<String>> {
+    if let DataType::Enum(Some(rev_mapping), _) = dtype {
+        Some(
+            rev_mapping
+                .get_categories()
+                .iter()
+                .filter_map(|v| v.map(ToString::to_string))
+                .collect::<Vec<String>>(),
+        )
+    } else {
+        None
     }
 }
