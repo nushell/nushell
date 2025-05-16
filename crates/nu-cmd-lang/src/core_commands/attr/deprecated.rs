d552f85afb1f56d3fa8fd8475c8e696d9f54ea56
132ikl <132@ikl.sh>
2025-05-16 17:10:18 -0400

    Start deprecated attribute

---
+++ crates/nu-cmd-lang/src/core_commands/attr/deprecated.rs
@@ -0,0 +1,83 @@
+use nu_engine::command_prelude::*;
+
+#[derive(Clone)]
+pub struct AttrDeprecated;
+
+impl Command for AttrDeprecated {
+    fn name(&self) -> &str {
+        "attr category"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build("attr category")
+            .input_output_types(vec![
+                (Type::Nothing, Type::Nothing),
+                (Type::Nothing, Type::String),
+            ])
+            .optional(
+                "message",
+                SyntaxShape::String,
+                "Message to include with deprecation warning.",
+            )
+            .category(Category::Core)
+    }
+
+    fn description(&self) -> &str {
+        "Attribute for marking a command as deprecated."
+    }
+
+    fn extra_description(&self) -> &str {
+        "Also consider setting the category to deprecated."
+    }
+
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        let message: Option<Value> = call.opt(engine_state, stack, 0)?;
+        match message {
+            Some(message) => Ok(message.into_pipeline_data()),
+            None => Ok(PipelineData::Empty),
+        }
+    }
+
+    fn run_const(
+        &self,
+        working_set: &StateWorkingSet,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        let message: Option<Value> = call.opt_const(working_set, 0)?;
+        match message {
+            Some(message) => Ok(message.into_pipeline_data()),
+            None => Ok(PipelineData::Empty),
+        }
+    }
+
+    fn is_const(&self) -> bool {
+        true
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![
+            Example {
+                description: "Add a deprecation warning to a custom command",
+                example: r###"@deprecated
+    def outdated [] {}"###,
+                result: Some(Value::nothing(Span::test_data())),
+            },
+            Example {
+                description: "Add a deprecation warning with a custom message",
+                example: r###"@deprecated "Use my-new-command instead."
+    def my-old-command [] {}"###,
+                result: Some(Value::string(
+                    "Use my-new-command instead.",
+                    Span::test_data(),
+                )),
+            },
+        ]
+    }
+}
--- crates/nu-cmd-lang/src/core_commands/attr/mod.rs
+++ crates/nu-cmd-lang/src/core_commands/attr/mod.rs
@@ -1,7 +1,9 @@
 mod category;
+mod deprecated;
 mod example;
 mod search_terms;
 
 pub use category::AttrCategory;
+pub use deprecated::AttrDeprecated;
 pub use example::AttrExample;
 pub use search_terms::AttrSearchTerms;
