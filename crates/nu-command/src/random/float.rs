f6a89dda73a75f66c8efc1f6dccaf36a6c07ba4c
Ritik Ranjan <e02948@ritik.ranjan@hsc.com>
2025-05-24 22:55:22 +0530

    [FIX] #15813 non-finite error

--- crates/nu-command/src/random/float.rs
+++ crates/nu-command/src/random/float.rs
@@ -90,7 +90,7 @@ fn float(
             let value = match range.end() {
                 Bound::Included(end) => random_range(range.start()..=end),
                 Bound::Excluded(end) => random_range(range.start()..end),
-                Bound::Unbounded => random_range(range.start()..f64::INFINITY),
+                Bound::Unbounded => random_range(range.start()..i64::MAX as f64),
             };
 
             Ok(PipelineData::Value(Value::float(value, span), None))
