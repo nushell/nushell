87239e3a460ec66e100d0198e03a0afad44aea50
Florent Vilmart <364568+flovilmart@users.noreply.github.com>
2025-05-28 13:30:01 -0400

    Update crates/nu-command/src/platform/input/input_.rs
    Co-authored-by: Darren Schroeder <343840+fdncred@users.noreply.github.com>

--- crates/nu-command/src/platform/input/input_.rs
+++ crates/nu-command/src/platform/input/input_.rs
@@ -50,7 +50,7 @@ impl Command for Input {
             .named(
                 "history-file",
                 SyntaxShape::Filepath,
-                "path to a file to read and write command history. This is a text file, and will be created if it doesn't exist. Can specific to your input prompt.",
+                "Path to a file to read and write command history. This is a text file and will be created if it doesn't exist. Will be used as the selection list.",
                 None,
             )
             .named(
