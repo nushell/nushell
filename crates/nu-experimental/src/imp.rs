1b203098aa5ba3bc3bd2340e215607649775cc59
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-06-21 17:18:26 +0200

    impl Debug for ExperimentalOption

--- crates/nu-experimental/src/imp.rs
+++ crates/nu-experimental/src/imp.rs
@@ -1,5 +1,5 @@
-use std::fmt::Debug;
 use crate::*;
+use std::fmt::Debug;
 
 pub(crate) trait DynExperimentalOptionMarker {
     fn identifier(&self) -> &'static str;
@@ -22,7 +22,9 @@ impl<M: options::ExperimentalOptionMarker> DynExperimentalOptionMarker for M {
 }
 
 impl ExperimentalOption {
-    pub(crate) const fn new(marker: &'static (dyn DynExperimentalOptionMarker + Send + Sync)) -> Self {
+    pub(crate) const fn new(
+        marker: &'static (dyn DynExperimentalOptionMarker + Send + Sync),
+    ) -> Self {
         Self {
             value: OnceLock::new(),
             marker,
@@ -42,11 +44,28 @@ impl ExperimentalOption {
     }
 
     pub fn get(&self) -> bool {
-        self.value.get().copied().unwrap_or_else(|| match self.marker.stability() {
-            Stability::Unstable => false,
-            Stability::Stable => false,
-            Stability::StableDefault => true,
-            Stability::Deprecated => false,
-        })
+        self.value
+            .get()
+            .copied()
+            .unwrap_or_else(|| match self.marker.stability() {
+                Stability::Unstable => false,
+                Stability::Stable => false,
+                Stability::StableDefault => true,
+                Stability::Deprecated => false,
+            })
+    }
+}
+
+impl Debug for ExperimentalOption {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let add_description = f.sign_plus();
+        let mut debug_struct = f.debug_struct("ExperimentalOption");
+        debug_struct.field("identifier", &self.identifier());
+        debug_struct.field("value", &self.value.get());
+        debug_struct.field("stability", &self.stability());
+        if add_description {
+            debug_struct.field("description", &self.description());
+        }
+        debug_struct.finish()
     }
 }
--- crates/nu-experimental/src/options/example.rs
+++ crates/nu-experimental/src/options/example.rs
@@ -1,10 +1,10 @@
 use crate::*;
 
 /// Example experimental option.
-/// 
+///
 /// This is an example how experimental options should be implemented and documented.
-/// Just from reading the documentation about this static value should make it clear what 
-/// experimental feature is supposed to do and it should be used in the rest of our codebase. 
+/// Just from reading the documentation about this static value should make it clear what
+/// experimental feature is supposed to do and it should be used in the rest of our codebase.
 pub static EXAMPLE: ExperimentalOption = ExperimentalOption::new(&Example);
 
 // We don't need to document this type as it is not public.
