8cd46d54a4687c7e303424d2d5969d42313d8135
132ikl <132@ikl.sh>
2025-07-08 13:05:09 -0400

    Avoid hot-path span dereference in eval_instruction

--- crates/nu-engine/src/eval_ir.rs
+++ crates/nu-engine/src/eval_ir.rs
@@ -292,6 +292,9 @@ fn eval_instruction<D: DebugContext>(
 ) -> Result<InstructionResult, ShellError> {
     use self::InstructionResult::*;
 
+    // Check for ctrl-c
+    ctx.engine_state.signals().check(&span)?;
+
     // See the docs for `Instruction` for more information on what these instructions are supposed
     // to do.
     match instruction {
