575a2dba21584a3d818a027b4bbf0ba375c92b33
Luca Scherzer <luca.scherzer@de.clara.net>
2025-07-17 17:43:10 +0200

    fix: merge

--- crates/nu-command/src/filesystem/watch.rs
+++ crates/nu-command/src/filesystem/watch.rs
@@ -6,11 +6,7 @@ use notify_debouncer_full::{
     },
 };
 use nu_engine::{ClosureEval, command_prelude::*};
-use nu_protocol::{
-    engine::{Closure, StateWorkingSet},
-    format_cli_error, report_shell_error,
-    shell_error::io::IoError,
-};
+use nu_protocol::{engine::Closure, report_shell_error, shell_error::io::IoError};
 
 use std::{
     path::PathBuf,
