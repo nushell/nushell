36595494d46af6f3d85663e34d9f8f8552195663
Bahex <17417311+Bahex@users.noreply.github.com>
2025-07-22 14:15:20 +0300

    fix(group-by): --to-table should return an empty list on empty input

--- crates/nu-command/src/filters/group_by.rs
+++ crates/nu-command/src/filters/group_by.rs
@@ -231,7 +231,12 @@ pub fn group_by(
 
     let values: Vec<Value> = input.into_iter().collect();
     if values.is_empty() {
-        return Ok(Value::record(Record::new(), head).into_pipeline_data());
+        let val = if to_table {
+            Value::list(Vec::new(), head)
+        } else {
+            Value::record(Record::new(), head)
+        };
+        return Ok(val.into_pipeline_data());
     }
 
     let grouped = match &groupers[..] {
