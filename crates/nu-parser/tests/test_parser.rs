nushell/nushell

commit 30c38b8c496e1cc5ebeefe8254724dd9bafd6014

fix(parser): repeated `(` / parenthesis / opened sub-expressions causes memory leak (#16204)
- fixes #16186

# Description

Don't attempt further parsing when there is no complete sub-expression.

---------

Co-authored-by: Bahex <17417311+Bahex@users.noreply.github.com>

crates/nu-parser/src/parser.rs
@@ -2003,15 +2003,11 @@ pub fn parse_paren_expr(
     let fcp_error_count = working_set.parse_errors.len();
     if fcp_error_count > starting_error_count {
         let malformed_subexpr = working_set.parse_errors[starting_error_count..]
-            .iter()
-            .any(|e| match e {
-                ParseError::Unclosed(right, _) if right == ")" => true,
-                ParseError::Unbalanced(left, right, _) if left == "(" && right == ")" => true,
-                _ => false,
-            });
+            .first()
+            .is_some_and(|e| matches!(e, ParseError::Unclosed(right, _) if right == ")" ));
         if malformed_subexpr {
             working_set.parse_errors.truncate(starting_error_count);
-            parse_string(working_set, span)
+            parse_string_interpolation(working_set, span)
         } else {
             fcp_expr
         }
@@ -2250,8 +2246,10 @@ pub fn parse_string_interpolation(working_set: &mut StateWorkingSet, span: Span)
             if token_start < end {
                 let span = Span::new(token_start, end);
 
-                let expr = parse_full_cell_path(working_set, None, span);
-                output.push(expr);
+                if delimiter_stack.is_empty() {
+                    let expr = parse_full_cell_path(working_set, None, span);
+                    output.push(expr);
+                }
             }
         }
     }

commit f3d92e3fa1ee8d42fe1ef79225247fd849afb1cc

fix bare interpolation regression (#16235)
Regression from #16204 

Before:

![](https://raw.githubusercontent.com/gist/Bahex/d9b654c119625a43fbf5fa05939648bb/raw/f1995bc71f2f5c51068df8dad72823379799443a/before.svg)

After:

![](https://raw.githubusercontent.com/gist/Bahex/d9b654c119625a43fbf5fa05939648bb/raw/f1995bc71f2f5c51068df8dad72823379799443a/after.svg)

# Tests + Formatting
+1

---------

Co-authored-by: Bahex <17417311+Bahex@users.noreply.github.com>

crates/nu-parser/src/parser.rs
@@ -2004,7 +2004,11 @@ pub fn parse_paren_expr(
     if fcp_error_count > starting_error_count {
         let malformed_subexpr = working_set.parse_errors[starting_error_count..]
             .first()
-            .is_some_and(|e| matches!(e, ParseError::Unclosed(right, _) if right == ")" ));
+            .is_some_and(|e| match e {
+                ParseError::Unclosed(right, _) if (right == ")") => true,
+                ParseError::Unbalanced(left, right, _) if left == "(" && right == ")" => true,
+                _ => false,
+            });
         if malformed_subexpr {
             working_set.parse_errors.truncate(starting_error_count);
             parse_string_interpolation(working_set, span)

crates/nu-parser/tests/test_parser.rs
@@ -1636,6 +1636,46 @@ mod string {
             assert_eq!(subexprs[3], &Expr::String("bar".to_string()));
         }
 
+        /// PR with summary of the issue: https://github.com/nushell/nushell/pull/16235
+        /// Release Notes Mention: https://www.nushell.sh/blog/2025-07-23-nushell_0_106_0.html#regression-bare-word-interpolation-on-both-sides-does-not-work-toc
+        #[test]
+        pub fn parse_string_interpolation_bare_starting_and_ending_subexpr() {
+            let engine_state = EngineState::new();
+            let mut working_set = StateWorkingSet::new(&engine_state);
+
+            let block = parse(
+                &mut working_set,
+                None,
+                b"(100 + 20 + 3)/bar/(300 + 20 + 1)",
+                true,
+            );
+
+            assert!(working_set.parse_errors.is_empty(),);
+
+            let [pipeline] = block.pipelines.as_slice() else {
+                panic!("expected 1 pipeline")
+            };
+            let [element] = pipeline.elements.as_slice() else {
+                panic!("expected 1 pipeline element")
+            };
+            assert!(element.redirection.is_none());
+
+            let Expr::StringInterpolation(expressions) = &element.expr.expr else {
+                panic!("Expected an `Expr::StringInterpolation`")
+            };
+            let subexprs: Vec<_> = expressions.iter().map(|e| &e.expr).collect();
+
+            let [
+                Expr::FullCellPath(..),
+                Expr::String(s),
+                Expr::FullCellPath(..),
+            ] = subexprs.as_slice()
+            else {
+                panic!("AST does not have the expected structure")
+            };
+            assert_eq!(s, "/bar/");
+        }
+
         #[test]
         pub fn parse_string_interpolation_bare_starting_subexpr_external_arg() {
             let engine_state = EngineState::new();