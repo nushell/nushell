1ced2dc736292e3434e79afdd3bae9e48c65fbf6
Jack Wright <jack.wright@nike.com>
2025-07-30 13:57:25 -0700

    working on getting help on a specific command

--- crates/nu-mcp/src/server.rs
+++ crates/nu-mcp/src/server.rs
@@ -1,15 +1,18 @@
 use std::{borrow::Cow, sync::Arc};
 
 use nu_protocol::{
-    PipelineData, Span,
+    PipelineData, Span, Value,
     engine::{Call, EngineState, Stack},
+    write_all_and_flush,
 };
 use rmcp::{
     ErrorData as McpError, ServerHandler,
-    handler::server::tool::ToolRouter,
-    model::{ServerCapabilities, ServerInfo},
+    handler::server::tool::{Parameters, ToolRouter},
+    model::{Annotated, CallToolResult, Content, RawContent, ServerCapabilities, ServerInfo},
     tool, tool_handler, tool_router,
 };
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
 pub struct NushellMcpServer {
     engine_state: Arc<EngineState>,
@@ -26,35 +29,36 @@ impl NushellMcpServer {
     }
 
     #[tool(description = "List available commands")]
-    async fn list_commands(&self) -> Result<String, McpError> {
-        if let (Some(cmds_decl_id), Some(csv_decl_id)) = (
-            self.engine_state.find_decl(b"help commands", &[]),
-            self.engine_state.find_decl(b"to csv", &[]),
-        ) {
-            let mut stack = Stack::new();
-            let span = Span::unknown();
-            let call = Call::new(span);
-            let pipeline = PipelineData::empty();
-            let pipeline = self
-                .engine_state
-                .get_decl(cmds_decl_id)
-                .run(self.engine_state.as_ref(), &mut stack, &call, pipeline)
-                .map_err(shell_error_to_mcp_error)?;
-
-            let (output, _span, _metadata) = self
-                .engine_state
-                .get_decl(csv_decl_id)
-                .run(self.engine_state.as_ref(), &mut stack, &call, pipeline)
-                .and_then(|data| data.collect_string_strict(span))
-                .map_err(shell_error_to_mcp_error)?;
-            Ok(output)
-        } else {
-            return Err(McpError::resource_not_found(
-                Cow::from("Either nushell command 'help commands' or 'to csv' not available"),
-                None,
-            ));
-        }
+    async fn list_commands(&self) -> Result<CallToolResult, McpError> {
+        execute_cmd(
+            self.engine_state.as_ref(),
+            PipelineData::empty(),
+            b"help commands",
+        )
+        .and_then(|pipeline| execute_cmd(self.engine_state.as_ref(), pipeline, b"to json"))
+        .and_then(|pipeline| pipeline_to_content(pipeline, self.engine_state.as_ref()))
+        .map(|content| CallToolResult::success(vec![content]))
     }
+
+    #[tool(description = "Get help for a specific command")]
+    async fn command_help(
+        &self,
+        Parameters(CommandNameRequest { name }): Parameters<CommandNameRequest>,
+    ) -> Result<CallToolResult, McpError> {
+        let cmd = format!("help {name}");
+        execute_cmd(
+            self.engine_state.as_ref(),
+            PipelineData::empty(),
+            cmd.as_bytes(),
+        )
+        .and_then(|pipeline| pipeline_to_content(pipeline, self.engine_state.as_ref()))
+        .map(|content| CallToolResult::success(vec![content]))
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, JsonSchema)]
+struct CommandNameRequest {
+    name: String,
 }
 
 #[tool_handler]
@@ -68,7 +72,62 @@ impl ServerHandler for NushellMcpServer {
     }
 }
 
+fn execute_cmd(
+    engine_state: &EngineState,
+    pipeline_data: PipelineData,
+    name: &[u8],
+) -> Result<PipelineData, McpError> {
+    let mut stack = Stack::new();
+    let span = Span::unknown();
+    let call = Call::new(span);
+    if let Some(decl_id) = engine_state.find_decl(name, &[]) {
+        engine_state
+            .get_decl(decl_id)
+            .run(engine_state, &mut stack, &call, pipeline_data)
+            .map_err(shell_error_to_mcp_error)
+    } else {
+        Err(McpError::resource_not_found(
+            Cow::from("Command not found"),
+            None,
+        ))
+    }
+}
+
 fn shell_error_to_mcp_error(error: nu_protocol::ShellError) -> McpError {
     //todo - make this more sophisticated   :w
-    McpError::internal_error(format!("{error}"), None)
+    McpError::internal_error(format!("ShellError: {error}"), None)
+}
+
+fn pipeline_to_content(
+    pipeline_data: PipelineData,
+    engine_state: &EngineState,
+) -> Result<Content, McpError> {
+    let span = pipeline_data.span();
+    // todo - this bystream use case won't work
+    if let PipelineData::ByteStream(_stream, ..) = pipeline_data {
+        // Copy ByteStreams directly
+        // stream.print(false)
+        Err(McpError::internal_error(
+            Cow::from("ByteStream output is not supported"),
+            None,
+        ))
+    } else {
+        let mut buffer: Vec<u8> = Vec::new();
+        let config = engine_state.get_config();
+        for item in pipeline_data {
+            let out = if let Value::Error { error, .. } = item {
+                return Err(shell_error_to_mcp_error(*error));
+            } else {
+                item.to_expanded_string("\n", config)
+            };
+
+            write_all_and_flush(out, &mut buffer, "mcp_output", span, engine_state.signals())
+                .map_err(shell_error_to_mcp_error)?;
+        }
+        let content =
+            RawContent::text(String::from_utf8(buffer).map_err(|e| {
+                McpError::internal_error(format!("Invalid UTF-8 output: {e}"), None)
+            })?);
+        Ok(Annotated::new(content, None))
+    }
 }
