abfc56fba098191ab3479a0a09a1de78d4216dd0
sholderbach <sholderbach@users.noreply.github.com>
2025-08-02 18:16:44 +0200

    Add `send: vichangemode` to reedline config
    Allows custom bindings (non-chord) to send a `edit_mode: vi` mode change
    via the new `ReedlineEvent::ViChangeMode`
    Takes https://github.com/nushell/reedline/pull/932

--- crates/nu-cli/src/reedline_config.rs
+++ crates/nu-cli/src/reedline_config.rs
@@ -1047,6 +1047,10 @@ fn event_from_record(
             ReedlineEvent::ExecuteHostCommand(cmd.to_expanded_string("", config))
         }
         "openeditor" => ReedlineEvent::OpenEditor,
+        "vichangemode" => {
+            let mode = extract_value("mode", record, span)?;
+            ReedlineEvent::ViChangeMode(mode.as_str()?.to_owned())
+        }
         str => {
             return Err(ShellError::InvalidValue {
                 valid: "a reedline event".into(),
