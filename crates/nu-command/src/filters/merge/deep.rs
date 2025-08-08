0967e8eba56254252afd2e292183f92a4fcb162c
Bahex <17417311+Bahex@users.noreply.github.com>
2025-08-08 14:28:09 +0300

    feat(merge deep): add completions for --strategy flag

--- crates/nu-command/src/filters/merge/deep.rs
+++ crates/nu-command/src/filters/merge/deep.rs
@@ -42,7 +42,21 @@ The way lists and tables are merged is controlled by the `--strategy` flag:
                 "The new value to merge with.",
             )
             .category(Category::Filters)
-            .named("strategy", SyntaxShape::String, "The list merging strategy to use. One of: table (default), overwrite, append, prepend", Some('s'))
+            .param(
+                Flag::new("strategy")
+                    .short('s')
+                    .arg(SyntaxShape::String)
+                    .desc(
+                        "The list merging strategy to use. One of: table (default), overwrite, \
+                         append, prepend",
+                    )
+                    .completion(Completion::new_list(&[
+                        "table",
+                        "overwrite",
+                        "append",
+                        "prepend",
+                    ])),
+            )
     }
 
     fn examples(&self) -> Vec<Example> {
