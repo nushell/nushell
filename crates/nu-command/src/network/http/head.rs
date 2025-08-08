f0c2c3bdb1c5434db85bfedc86d877acd1153a90
Bahex <17417311+Bahex@users.noreply.github.com>
2025-08-08 15:26:54 +0300

    feat(http*): add completions for --redirect-mode flag

--- crates/nu-command/src/network/http/head.rs
+++ crates/nu-command/src/network/http/head.rs
@@ -6,6 +6,8 @@ use crate::network::http::client::{
 use nu_engine::command_prelude::*;
 use nu_protocol::Signals;
 
+use super::client::RedirectMode;
+
 #[derive(Clone)]
 pub struct HttpHead;
 
@@ -51,11 +53,16 @@ impl Command for HttpHead {
                 "insecure",
                 "allow insecure server connections when using SSL",
                 Some('k'),
-            ).named(
-                "redirect-mode",
-                SyntaxShape::String,
-                "What to do when encountering redirects. Default: 'follow'. Valid options: 'follow' ('f'), 'manual' ('m'), 'error' ('e').",
-                Some('R')
+            )
+            .param(
+                Flag::new("redirect-mode")
+                    .short('R')
+                    .arg(SyntaxShape::String)
+                    .desc(
+                        "What to do when encountering redirects. Default: 'follow'. Valid \
+                         options: 'follow' ('f'), 'manual' ('m'), 'error' ('e').",
+                    )
+                    .completion(Completion::new_list(RedirectMode::MODES)),
             )
             .filter()
             .category(Category::Network)
