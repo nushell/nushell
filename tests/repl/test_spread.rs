ef447afe41a02038c8587c01c54d6b5a1882fa41
Bahex <17417311+Bahex@users.noreply.github.com>
2025-08-09 17:16:45 +0300

    tests: add tests to check `null` can be spread to rest arguments

--- tests/repl/test_spread.rs
+++ tests/repl/test_spread.rs
@@ -203,6 +203,26 @@ fn respect_shape() -> TestResult {
 
 #[test]
 fn spread_null() -> TestResult {
+    // Spread in list
     run_test(r#"[1, 2, ...(null)] | to nuon --raw"#, r#"[1,2]"#)?;
-    run_test(r#"{a: 1, b: 2, ...(null)} | to nuon --raw"#, r#"{a:1,b:2}"#)
+
+    // Spread in record
+    run_test(r#"{a: 1, b: 2, ...(null)} | to nuon --raw"#, r#"{a:1,b:2}"#)?;
+
+    // Spread to built-in command's ...rest
+    run_test(r#"echo 1 2 ...(null) | to nuon --raw"#, r#"[1,2]"#)?;
+
+    // Spread to custom command's ...rest
+    run_test(
+        r#"
+            def foo [...rest] { $rest }
+            foo ...(null) 1 2 ...(null) 3 | to nuon --raw
+        "#,
+        r#"[1,2,3]"#,
+    )?;
+
+    // Spread to external command's arguments
+    assert_eq!(nu!(r#"nu --testbin cococo 1 ...(null) 2"#).out, "1 2");
+
+    Ok(())
 }
