4245c67ce3ea3f1065ead977e681373a22f2517b
Ecorous <ecorous@outlook.com>
2025-08-11 14:31:24 +0100

    Add `--raw` flag to `to html` (#16373)
    Works towards #16347 however more work may be required first
    
    # Description
    Adds a `--raw` flag to `to html`. This stops the resulting html content
    being escaped
    # User-Facing Changes
    
    # Tests + Formatting
    # After Submitting

--- crates/nu-cmd-extra/src/example_test.rs
+++ crates/nu-cmd-extra/src/example_test.rs
@@ -3,7 +3,12 @@ use nu_protocol::engine::Command;
 
 #[cfg(test)]
 pub fn test_examples(cmd: impl Command + 'static) {
-    test_examples::test_examples(cmd);
+    test_examples::test_examples(cmd, &[]);
+}
+
+#[cfg(test)]
+pub fn test_examples_with_commands(cmd: impl Command + 'static, commands: &[&dyn Command]) {
+    test_examples::test_examples(cmd, commands);
 }
 
 #[cfg(test)]
@@ -21,10 +26,10 @@ mod test_examples {
     };
     use std::collections::HashSet;
 
-    pub fn test_examples(cmd: impl Command + 'static) {
+    pub fn test_examples(cmd: impl Command + 'static, commands: &[&dyn Command]) {
         let examples = cmd.examples();
         let signature = cmd.signature();
-        let mut engine_state = make_engine_state(cmd.clone_box());
+        let mut engine_state = make_engine_state(cmd.clone_box(), commands);
 
         let cwd = std::env::current_dir().expect("Could not get current working directory.");
 
@@ -38,7 +43,7 @@ mod test_examples {
                 check_example_input_and_output_types_match_command_signature(
                     &example,
                     &cwd,
-                    &mut make_engine_state(cmd.clone_box()),
+                    &mut make_engine_state(cmd.clone_box(), commands),
                     &signature.input_output_types,
                     signature.operates_on_cell_paths(),
                 ),
@@ -57,7 +62,7 @@ mod test_examples {
         );
     }
 
-    fn make_engine_state(cmd: Box<dyn Command>) -> Box<EngineState> {
+    fn make_engine_state(cmd: Box<dyn Command>, commands: &[&dyn Command]) -> Box<EngineState> {
         let mut engine_state = Box::new(EngineState::new());
 
         let delta = {
@@ -68,6 +73,10 @@ mod test_examples {
             working_set.add_decl(Box::new(nu_command::Enumerate));
             working_set.add_decl(Box::new(nu_cmd_lang::If));
             working_set.add_decl(Box::new(nu_command::MathRound));
+
+            for command in commands {
+                working_set.add_decl(command.clone_box());
+            }
 
             // Adding the command that is being tested to the working set
             working_set.add_decl(cmd);
