01255d090624df73b1a5b57aaaf98cce9608f048
blindfs <blindfs19@gmail.com>
2025-08-25 19:30:59 +0800

    fix(completion): missing local variables

--- crates/nu-cli/src/completions/variable_completions.rs
+++ crates/nu-cli/src/completions/variable_completions.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 use crate::completions::{Completer, CompletionOptions, SemanticSuggestion, SuggestionKind};
 use nu_protocol::{
-    Span, VarId,
+    Span, Type, VarId,
     engine::{Stack, StateWorkingSet},
 };
 use reedline::Suggestion;
@@ -39,39 +41,52 @@ impl Completer for VariableCompletion {
             });
         }
 
-        let mut add_candidate = |name, var_id: &VarId| {
+        let mut add_candidate = |name, ty: &Type| {
+            let mut value = String::from_utf8_lossy(name).to_string();
+            if !value.starts_with('$') {
+                value.insert(0, '$');
+            }
             matcher.add_semantic_suggestion(SemanticSuggestion {
                 suggestion: Suggestion {
-                    value: String::from_utf8_lossy(name).to_string(),
+                    value,
                     span: current_span,
-                    description: Some(working_set.get_variable(*var_id).ty.to_string()),
+                    description: Some(ty.to_string()),
                     ..Suggestion::default()
                 },
                 kind: Some(SuggestionKind::Variable),
             })
         };
 
-        // TODO: The following can be refactored (see find_commands_by_predicate() used in
-        // command_completions).
-        let mut removed_overlays = vec![];
-        // Working set scope vars
-        for scope_frame in working_set.delta.scope.iter().rev() {
-            for overlay_frame in scope_frame.active_overlays(&mut removed_overlays).rev() {
-                for (name, var_id) in &overlay_frame.vars {
-                    add_candidate(name, var_id);
-                }
+        // TODO: smarter scope-aware variable completion
+        let mut variables_defined_before_cursor = HashMap::new();
+        // Permanent state vars
+        for overlay_frame in working_set.permanent_state.active_overlays(&[]).rev() {
+            for (name, var_id) in &overlay_frame.vars {
+                let var = working_set.get_variable(*var_id);
+                add_candidate(name, &var.ty);
             }
         }
-        // Permanent state vars
-        // for scope in &self.engine_state.scope {
-        for overlay_frame in working_set
-            .permanent_state
-            .active_overlays(&removed_overlays)
-            .rev()
-        {
-            for (name, var_id) in &overlay_frame.vars {
-                add_candidate(name, var_id);
+
+        // A superset of valid variables in current scope,
+        // A workaround for https://github.com/nushell/nushell/issues/15291
+        let working_span = working_set
+            .files()
+            .last()
+            .map(|f| f.covered_span)
+            .unwrap_or(Span::unknown());
+        for id in 0..working_set.num_vars() {
+            let var_id = VarId::new(id);
+            let var = working_set.get_variable(var_id);
+            if working_span.contains_span(var.declaration_span)
+                && var.declaration_span.start < span.end
+            {
+                let var_name = working_set.get_span_contents(var.declaration_span);
+                variables_defined_before_cursor.insert(var_name, &var.ty);
             }
+        }
+
+        for (name, ty) in variables_defined_before_cursor {
+            add_candidate(name, ty);
         }
 
         matcher.results()
--- crates/nu-cli/tests/completions/mod.rs
+++ crates/nu-cli/tests/completions/mod.rs
@@ -1981,6 +1981,19 @@ fn variables_completions() {
 }
 
 #[test]
+fn local_variable_completion() {
+    let (_, _, engine, stack) = new_engine();
+    let mut completer = NuCompleter::new(Arc::new(engine), Arc::new(stack));
+
+    let completion_str = "def test [foo: string] { let fooo = 1; $fo";
+    let suggestions = completer.complete(completion_str, completion_str.len());
+
+    // https://github.com/nushell/nushell/issues/15291
+    let expected: Vec<_> = vec!["$foo", "$fooo"];
+    match_suggestions(&expected, &suggestions);
+}
+
+#[test]
 fn record_cell_path_completions() {
     let (_, _, mut engine, mut stack) = new_engine();
     let command = r#"let foo = {a: [1 {a: 2}]}; const bar = {a: [1 {a: 2}]}"#;
