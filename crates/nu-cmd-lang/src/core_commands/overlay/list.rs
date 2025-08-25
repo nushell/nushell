ca0e9614ada4d5699167be8214a58bcb5f2f4b25
hardfault <hardfau1t@proton.me>
2025-08-25 19:15:47 +0530

    feat(overlay): add active column to `overlay list` (#16125)
    # Description
    
    This adds a column to overlay list which indicates whether given overlay
    is active or not
    ```nu
    overlay new spam
    overlay  hide spam
    overlay list 
     0   spam
    ```
    
    
    # User-Facing Changes
    ## Release notes summary
    `overlay list` now returns table instead of list of overlays. Before,
    only active overlays were included in `overlay list`. Now, hidden
    overlays will be included as well, and there is a column indicating
    whether a given overlay is hidden or not.
    
    > TODO(release-notes): make sure this call out is formatted correctly
    for the blog
    
    > [!TIP]
    > The ordering of `overlay list` is still preserved. If you run `overlay
    list | last`, you will still get the most recently activated overlay.
    >
    > For migrating to the new behavior, you can update any usages of
    `overlay list | last` to `overlay list | last | get name`.
    
    # Tests + Formatting
    
    # After Submitting

--- crates/nu-cmd-lang/src/core_commands/overlay/list.rs
+++ crates/nu-cmd-lang/src/core_commands/overlay/list.rs
@@ -9,42 +9,96 @@ impl Command for OverlayList {
     }
 
     fn description(&self) -> &str {
-        "List all active overlays."
+        "List all overlays with their active status."
     }
 
     fn signature(&self) -> nu_protocol::Signature {
         Signature::build("overlay list")
             .category(Category::Core)
-            .input_output_types(vec![(Type::Nothing, Type::List(Box::new(Type::String)))])
+            .input_output_types(vec![(
+                Type::Nothing,
+                Type::Table(
+                    vec![
+                        ("name".to_string(), Type::String),
+                        ("active".to_string(), Type::Bool),
+                    ]
+                    .into(),
+                ),
+            )])
     }
 
     fn extra_description(&self) -> &str {
-        "The overlays are listed in the order they were activated."
+        "The overlays are listed in the order they were activated. Hidden overlays are listed first, followed by active overlays listed in the order that they were activated. `last` command will always give the top active overlay"
     }
 
     fn run(
         &self,
-        _engine_state: &EngineState,
+        engine_state: &EngineState,
         stack: &mut Stack,
         call: &Call,
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let active_overlays_engine: Vec<Value> = stack
+        // get active overlay iterator
+        let active_overlays = stack
             .active_overlays
             .iter()
-            .map(|s| Value::string(s, call.head))
+            .map(|overlay| (overlay.clone(), true));
+
+        // Get all overlay names from engine state
+        let output_rows: Vec<Value> = engine_state
+            .scope
+            .overlays
+            .iter()
+            .filter_map(|(name, _)| {
+                let name = String::from_utf8_lossy(name).to_string();
+                if stack
+                    .active_overlays
+                    .iter()
+                    .any(|active_name| active_name == &name)
+                {
+                    None
+                } else {
+                    Some((name, false))
+                }
+            })
+            .chain(active_overlays)
+            .map(|(name, active)| {
+                Value::record(
+                    record! {
+                        "name" => Value::string(name.to_owned(), call.head),
+                        "active" => Value::bool(active, call.head),
+                    },
+                    call.head,
+                )
+            })
             .collect();
 
-        Ok(Value::list(active_overlays_engine, call.head).into_pipeline_data())
+        Ok(Value::list(output_rows, call.head).into_pipeline_data())
     }
 
     fn examples(&self) -> Vec<Example> {
-        vec![Example {
-            description: "Get the last activated overlay",
-            example: r#"module spam { export def foo [] { "foo" } }
+        vec![
+            Example {
+                description: "List all overlays with their active status",
+                example: r#"module spam { export def foo [] { "foo" } }
     overlay use spam
-    overlay list | last"#,
-            result: Some(Value::test_string("spam")),
-        }]
+    overlay list"#,
+                result: Some(Value::test_list(vec![Value::test_record(record! {
+                    "name" => Value::test_string("spam"),
+                    "active" => Value::test_bool(true),
+                })])),
+            },
+            Example {
+                description: "Get overlay status after hiding",
+                example: r#"module spam { export def foo [] { "foo" } }
+    overlay use spam
+    overlay hide spam
+    overlay list | where name == "spam""#,
+                result: Some(Value::test_list(vec![Value::test_record(record! {
+                    "name" => Value::test_string("spam"),
+                    "active" => Value::test_bool(false),
+                })])),
+            },
+        ]
     }
 }
