6aa0e82f1ce595c570a549b733597639cdfc71aa
blindfs <blindfs19@gmail.com>
2025-09-10 21:03:49 +0800

    fix: test

--- tests/repl/test_parser.rs
+++ tests/repl/test_parser.rs
@@ -1046,10 +1046,7 @@ fn quote_escape_but_not_env_shorthand() -> TestResult {
 // https://github.com/nushell/nushell/issues/16586
 #[test]
 fn redefine_def_should_not_panic() -> TestResult {
-    fail_test(
-        r#"def def (=a|s)>"#,
-        "Missing required positional argument.",
-    )
+    fail_test(r#"def def (=a|s)>"#, "Unclosed delimiter")
 }
 
 #[test]
