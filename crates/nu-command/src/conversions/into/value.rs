5fca8c287aae458e18343b9df90ca0edf9dcba37
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-09-16 23:56:39 +0200

    pass `into value` to `detect types` with warning

--- crates/nu-command/src/conversions/into/value.rs
+++ crates/nu-command/src/conversions/into/value.rs
@@ -1,4 +1,7 @@
 use nu_engine::command_prelude::*;
+use nu_protocol::{DeprecationEntry, DeprecationType, ReportMode, report_shell_warning};
+
+use crate::DetectTypes;
 
 #[derive(Clone)]
 pub struct IntoValue;
@@ -14,6 +17,38 @@ impl Command for IntoValue {
             .extra_description(self.extra_description())
             .input_output_type(Type::Any, Type::Any)
             .category(Category::Conversions)
+            // TODO: remove these after deprecation phase
+            .named(
+                "columns",
+                SyntaxShape::List(Box::new(SyntaxShape::Any)),
+                "list of columns to update",
+                Some('c'),
+            )
+            .switch(
+                "prefer-filesizes",
+                "For ints display them as human-readable file sizes",
+                Some('f'),
+            )
+            .allow_variants_without_examples(true)
+    }
+
+    fn deprecation_info(&self) -> Vec<DeprecationEntry> {
+        vec![
+            DeprecationEntry {
+                ty: DeprecationType::Flag("columns".to_string()),
+                report_mode: ReportMode::EveryUse,
+                since: Some("0.108.0".into()),
+                expected_removal: Some("0.109.0".into()),
+                help: Some("Use this flag on `detect types`.".into()),
+            },
+            DeprecationEntry {
+                ty: DeprecationType::Flag("prefer-filesizes".to_string()),
+                report_mode: ReportMode::EveryUse,
+                since: Some("0.108.0".into()),
+                expected_removal: Some("0.109.0".into()),
+                help: Some("Use this flag on `detect types`.".into()),
+            },
+        ]
     }
 
     fn description(&self) -> &str {
@@ -36,9 +71,9 @@ impl Command for IntoValue {
 
     fn run(
         &self,
-        _: &EngineState,
-        _: &mut Stack,
-        _: &Call,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
         input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
         if let PipelineData::Value(Value::Custom { val, internal_span }, metadata) = input {
@@ -48,6 +83,19 @@ impl Command for IntoValue {
             ));
         }
 
-        Ok(input)
+        report_shell_warning(
+            engine_state,
+            &ShellWarning::Deprecated {
+                dep_type: "Moved Command".into(),
+                label: "Detecting types of tables is moved to `detect types`.".into(),
+                span: call.head,
+                help: Some("Use `detect types` instead. In the future this will be a no-op for nushell native values.".into()),
+                report_mode: ReportMode::EveryUse,
+            },
+        );
+        DetectTypes.run(engine_state, stack, call, input)
+
+        // After deprecation period, this rest will be a noop.
+        // Ok(input)
     }
 }
