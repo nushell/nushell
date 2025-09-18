nushell/nushell

commit e1ce67e3c1e88f3c8c42e39d478f30fcfa0b2532

fix(lsp): malfunctioning on unsaved files, empty `workspaceFolder` parameter, overlay use with prefixes (#16568)
Fixes some minor issues of nu-lsp, including #16559 and #16539, as well
as most of the key takeaways of #16540

## Release notes summary - What our users need to know

## Tasks after submitting

- [ ] add test cases, I'll leave them to future PRs where I'm going to
refactor the verbose testing code of nu-lsp.

crates/nu-lsp/src/goto.rs
@@ -15,9 +15,6 @@ impl LanguageServer {
         for cached_file in files.into_iter() {
             if cached_file.covered_span.contains(span.start) {
                 let path = Path::new(&*cached_file.name);
-                if !path.is_file() {
-                    return None;
-                }
                 let target_uri = path_to_uri(path);
                 if let Some(file) = self.docs.lock().ok()?.get_document(&target_uri) {
                     return Some(Location {
@@ -25,6 +22,9 @@ impl LanguageServer {
                         range: span_to_range(span, file, cached_file.covered_span.start),
                     });
                 } else {
+                    if !path.is_file() {
+                        return None;
+                    }
                     // in case where the document is not opened yet,
                     // typically included by the `use/source` command
                     let temp_doc = FullTextDocument::new(
@@ -77,16 +77,12 @@ impl LanguageServer {
         &mut self,
         params: &GotoDefinitionParams,
     ) -> Option<GotoDefinitionResponse> {
-        let path_uri = params
-            .text_document_position_params
-            .text_document
-            .uri
-            .to_owned();
-        let mut engine_state = self.new_engine_state(Some(&path_uri));
+        let path_uri = &params.text_document_position_params.text_document.uri;
+        let mut engine_state = self.new_engine_state(Some(path_uri));
         let (working_set, id, _, _) = self
             .parse_and_find(
                 &mut engine_state,
-                &path_uri,
+                path_uri,
                 params.text_document_position_params.position,
             )
             .ok()?;

crates/nu-lsp/src/hover.rs
@@ -117,16 +117,12 @@ impl LanguageServer {
     }
 
     pub(crate) fn hover(&mut self, params: &HoverParams) -> Option<Hover> {
-        let path_uri = params
-            .text_document_position_params
-            .text_document
-            .uri
-            .to_owned();
-        let mut engine_state = self.new_engine_state(Some(&path_uri));
+        let path_uri = &params.text_document_position_params.text_document.uri;
+        let mut engine_state = self.new_engine_state(Some(path_uri));
         let (working_set, id, _, _) = self
             .parse_and_find(
                 &mut engine_state,
-                &path_uri,
+                path_uri,
                 params.text_document_position_params.position,
             )
             .ok()?;

crates/nu-lsp/src/lib.rs
@@ -234,6 +234,14 @@ impl LanguageServer {
                             })
                         }
                         request::Rename::METHOD => {
+                            if self.channels.is_some() {
+                                self.send_error_message(
+                                    request.id.clone(),
+                                    3,
+                                    "Please wait for renaming preparation to complete.".into(),
+                                )?;
+                                continue;
+                            }
                             Self::handle_lsp_request(request, |params| self.rename(params))
                         }
                         request::SemanticTokensFullRequest::METHOD => {
@@ -393,6 +401,7 @@ impl LanguageServer {
         let file_path = uri_to_path(uri);
         let file_path_str = file_path.to_str()?;
         let contents = file.get_content(None).as_bytes();
+        // For `const foo = path self .`
         let _ = working_set.files.push(file_path.clone(), Span::unknown());
         let block = nu_parser::parse(&mut working_set, Some(file_path_str), contents, false);
         let span = working_set.get_span_for_filename(file_path_str)?;

commit d1a824951cce57d565e81c6edd1249d8d8c2955d

fix(lsp): panic on goto std files (#16694)
Fixes a new issue introduced by #16568 

```nushell
use std/assert
```

Goto def on `assert` will cause panic, since std files are cached with
relative paths as names.

This PR simply skips those files.
Perhaps a better way would be to find them in the vendor std-lib
directory, which I'm not sure how to get correctly.

## Release notes summary - What our users need to know

## Tasks after submitting

crates/nu-lsp/src/goto.rs
@@ -15,6 +15,17 @@ impl LanguageServer {
         for cached_file in files.into_iter() {
             if cached_file.covered_span.contains(span.start) {
                 let path = Path::new(&*cached_file.name);
+                // skip nu-std files
+                // TODO: maybe find them in vendor directories?
+                if path.is_relative() {
+                    let _ = self.send_log_message(
+                        lsp_types::MessageType::WARNING,
+                        format!(
+                            "Location found in file {path:?}, but absolute path is expected. Skipping..."
+                        ),
+                    );
+                    continue;
+                }
                 let target_uri = path_to_uri(path);
                 if let Some(file) = self.docs.lock().ok()?.get_document(&target_uri) {
                     return Some(Location {
@@ -99,7 +110,7 @@ mod tests {
     use crate::path_to_uri;
     use crate::tests::{initialize_language_server, open, open_unchecked, result_from_message};
     use assert_json_diff::assert_json_eq;
-    use lsp_server::{Connection, Message};
+    use lsp_server::{Connection, Message, Notification};
     use lsp_types::{
         GotoDefinitionParams, PartialResultParams, Position, TextDocumentIdentifier,
         TextDocumentPositionParams, Uri, WorkDoneProgressParams,
@@ -146,7 +157,7 @@ mod tests {
         let script = path_to_uri(&none_existent_path);
         let resp = send_goto_definition_request(&client_connection, script, 0, 0);
 
-        assert_json_eq!(result_from_message(resp), serde_json::json!(null));
+        assert_json_eq!(result_from_message(resp), serde_json::Value::Null);
     }
 
     #[rstest]
@@ -189,24 +200,21 @@ mod tests {
         if let Some(name) = expected_file {
             target_uri = target_uri.replace(filename, name);
         }
-        assert_json_eq!(
-            result.pointer("/uri").unwrap(),
-            serde_json::json!(target_uri)
-        );
+        assert_json_eq!(result["uri"], serde_json::json!(target_uri));
         let (line, character) = expected_start;
         assert_json_eq!(
-            result.pointer("/range/start").unwrap(),
+            result["range"]["start"],
             serde_json::json!({ "line": line, "character": character })
         );
         if let Some((line, character)) = expected_end {
             assert_json_eq!(
-                result.pointer("/range/end").unwrap(),
+                result["range"]["end"],
                 serde_json::json!({ "line": line, "character": character })
             );
         }
     }
 
-    #[rstest]
+    #[test]
     // https://github.com/nushell/nushell/issues/16539
     fn goto_definition_in_new_file() {
         let (client_connection, _recv) = initialize_language_server(None, None);
@@ -239,5 +247,27 @@ mod tests {
                 }
             })
         );
+    }
+
+    #[test]
+    fn goto_definition_on_stdlib_should_not_panic() {
+        let (client_connection, _recv) = initialize_language_server(None, None);
+
+        let mut script = fixtures();
+        script.push("lsp/goto/use_module.nu");
+        let script = path_to_uri(&script);
+
+        open_unchecked(&client_connection, script.clone());
+        let resp = send_goto_definition_request(&client_connection, script, 7, 19);
+        match resp {
+            Message::Notification(Notification { params, .. }) => {
+                assert!(
+                    params["message"]
+                        .to_string()
+                        .contains("absolute path is expected")
+                );
+            }
+            _ => panic!("Unexpected message!"),
+        }
     }
 }

crates/nu-lsp/src/hover.rs
@@ -263,9 +263,7 @@ mod hover_tests {
         let resp = send_hover_request(&client_connection, script, line, character);
 
         assert_json_eq!(
-            result_from_message(resp)
-                .pointer("/contents/value")
-                .unwrap(),
+            result_from_message(resp)["contents"]["value"],
             serde_json::json!(expected)
         );
     }
@@ -281,12 +279,7 @@ mod hover_tests {
         open_unchecked(&client_connection, script.clone());
         let resp = send_hover_request(&client_connection, script, 6, 2);
 
-        let hover_text = result_from_message(resp)
-            .pointer("/contents/value")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .to_string();
+        let hover_text = result_from_message(resp)["contents"]["value"].to_string();
 
         #[cfg(not(windows))]
         assert!(hover_text.contains("SLEEP"));
@@ -321,11 +314,7 @@ mod hover_tests {
         let resp = send_hover_request(&client_connection, script_uri, line, character);
         let result = result_from_message(resp);
 
-        let actual = result
-            .pointer("/contents/value")
-            .unwrap()
-            .to_string()
-            .replace("\\r", "");
+        let actual = result["contents"]["value"].to_string().replace("\\r", "");
 
         assert!(actual.starts_with(expected_prefix));
     }

crates/nu-lsp/src/lib.rs
@@ -2,8 +2,8 @@
 use lsp_server::{Connection, IoThreads, Message, Response, ResponseError};
 use lsp_textdocument::{FullTextDocument, TextDocuments};
 use lsp_types::{
-    InlayHint, OneOf, Position, Range, ReferencesOptions, RenameOptions, SemanticToken,
-    SemanticTokenType, SemanticTokensLegend, SemanticTokensOptions,
+    InlayHint, MessageType, OneOf, Position, Range, ReferencesOptions, RenameOptions,
+    SemanticToken, SemanticTokenType, SemanticTokensLegend, SemanticTokensOptions,
     SemanticTokensServerCapabilities, ServerCapabilities, SignatureHelpOptions,
     TextDocumentSyncKind, Uri, WorkDoneProgressOptions, WorkspaceFolder,
     WorkspaceFoldersServerCapabilities, WorkspaceServerCapabilities,
@@ -291,6 +291,10 @@ impl LanguageServer {
     pub(crate) fn cancel_background_thread(&mut self) {
         if let Some((sender, _)) = &self.channels {
             sender.send(true).ok();
+            let _ = self.send_log_message(
+                MessageType::WARNING,
+                "Workspace-wide search took too long!".into(),
+            );
         }
     }
 
@@ -510,7 +514,7 @@ mod tests {
 
         let _initialize_response = client_connection
             .receiver
-            .recv_timeout(Duration::from_secs(2))
+            .recv_timeout(Duration::from_secs(5))
             .unwrap();
 
         (client_connection, recv)

crates/nu-lsp/src/notification.rs
@@ -1,8 +1,9 @@
 use crate::LanguageServer;
 use lsp_types::{
     DidChangeTextDocumentParams, DidChangeWorkspaceFoldersParams, DidCloseTextDocumentParams,
-    DidOpenTextDocumentParams, ProgressParams, ProgressParamsValue, ProgressToken, Uri,
-    WorkDoneProgress, WorkDoneProgressBegin, WorkDoneProgressEnd, WorkDoneProgressReport,
+    DidOpenTextDocumentParams, LogMessageParams, MessageType, ProgressParams, ProgressParamsValue,
+    ProgressToken, Uri, WorkDoneProgress, WorkDoneProgressBegin, WorkDoneProgressEnd,
+    WorkDoneProgressReport,
     notification::{
         DidChangeTextDocument, DidChangeWorkspaceFolders, DidCloseTextDocument,
         DidOpenTextDocument, Notification, Progress,
@@ -65,6 +66,18 @@ impl LanguageServer {
         };
         let notification =
             lsp_server::Notification::new(Progress::METHOD.to_string(), progress_params);
+        self.connection
+            .sender
+            .send(lsp_server::Message::Notification(notification))
+            .into_diagnostic()
+    }
+
+    pub(crate) fn send_log_message(&self, typ: MessageType, message: String) -> Result<()> {
+        let log_params = LogMessageParams { typ, message };
+        let notification = lsp_server::Notification::new(
+            lsp_types::notification::LogMessage::METHOD.to_string(),
+            log_params,
+        );
         self.connection
             .sender
             .send(lsp_server::Message::Notification(notification))

crates/nu-lsp/src/workspace.rs
@@ -544,6 +544,7 @@ mod tests {
     use crate::tests::{initialize_language_server, open, open_unchecked, send_hover_request};
     use assert_json_diff::assert_json_eq;
     use lsp_server::{Connection, Message};
+    use lsp_types::notification::{LogMessage, Notification, Progress};
     use lsp_types::{
         DocumentHighlightParams, InitializeParams, PartialResultParams, Position, ReferenceContext,
         ReferenceParams, RenameParams, TextDocumentIdentifier, TextDocumentPositionParams, Uri,
@@ -832,7 +833,7 @@ mod tests {
         let mut has_response = false;
         for message in messages {
             match message {
-                Message::Notification(n) => assert_eq!(n.method, "$/progress"),
+                Message::Notification(n) => assert_eq!(n.method, Progress::METHOD),
                 Message::Response(r) => {
                     has_response = true;
                     let result = r.result.unwrap();
@@ -921,7 +922,7 @@ mod tests {
         let mut has_response = false;
         for message in messages {
             match message {
-                Message::Notification(n) => assert_eq!(n.method, "$/progress"),
+                Message::Notification(n) => assert_eq!(n.method, Progress::METHOD),
                 Message::Response(r) => {
                     has_response = true;
                     assert_json_eq!(r.result, expected_prepare)
@@ -979,7 +980,7 @@ mod tests {
 
         open_unchecked(&client_connection, script.clone());
 
-        let message_num = 3;
+        let message_num = 4;
         let messages = send_rename_prepare_request(
             &client_connection,
             script.clone(),
@@ -1000,7 +1001,13 @@ mod tests {
         let mut has_response = false;
         for message in messages {
             match message {
-                Message::Notification(n) => assert_eq!(n.method, "$/progress"),
+                Message::Notification(n) => {
+                    if n.method == LogMessage::METHOD {
+                        assert_json_eq!(n.params["message"], "Workspace-wide search took too long!")
+                    } else {
+                        assert_eq!(n.method, Progress::METHOD)
+                    };
+                }
                 // the response of the preempting hover request
                 Message::Response(r) => {
                     has_response = true;

tests/fixtures/lsp/goto/use_module.nu
@@ -2,3 +2,7 @@ use module.nu "module name"
 overlay use module.nu as new_name
 overlay hide --keep-env [PWD] new_name
 hide "module name"
+
+# stdlib files are loaded virtually
+# goto_def on them should just fail without panic
+use std/log log-level