05e7c1d3c4f910c60a67d4c8b82aecb875334bdb
synerr <imposter@amogus.sex>
2025-09-19 14:59:10 +0300

    Fixed the issue with the error message

--- crates/nu-command/src/bytes/collect.rs
+++ crates/nu-command/src/bytes/collect.rs
@@ -11,7 +11,7 @@ impl Command for BytesCollect {
 
     fn signature(&self) -> Signature {
         Signature::build("bytes collect")
-            .input_output_types(vec![(Type::List(Box::new(Type::Binary)), Type::Binary)])
+            .input_output_types(vec![(Type::List(Box::new(Type::Binary)), Type::Binary), (Type::table(), Type::Binary)])
             .optional(
                 "separator",
                 SyntaxShape::Binary,
