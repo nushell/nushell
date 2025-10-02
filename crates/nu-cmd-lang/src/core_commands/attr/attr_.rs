bc2a5cfbf8fd6a4b3e79ab6a760e7127edba6fba
Bahex <17417311+Bahex@users.noreply.github.com>
2025-10-02 18:15:32 +0300

    fix `attr *` command signatures (#16774)
    Also wrapped some string literals that were over `rustfmt`'s max line
    length.

--- crates/nu-cmd-lang/src/core_commands/attr/attr_.rs
+++ crates/nu-cmd-lang/src/core_commands/attr/attr_.rs
@@ -19,7 +19,10 @@ impl Command for Attr {
     }
 
     fn extra_description(&self) -> &str {
-        "You must use one of the following subcommands. Using this command as-is will only produce this help message."
+        "\
+            You must use one of the following subcommands. \
+            Using this command as-is will only produce this help message.\
+        "
     }
 
     fn run(
