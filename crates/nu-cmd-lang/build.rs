1896a0892ed49f4c9d2bc1d231394f4eb3783ac5
Kai Sforza <kai@kaictl.me>
2025-10-29 21:39:06 +0000

    Allow setting the NU_COMMIT_HASH as an env variable (#16947)

--- crates/nu-cmd-lang/build.rs
+++ crates/nu-cmd-lang/build.rs
@@ -3,7 +3,12 @@ use std::process::Command;
 fn main() {
     // Look up the current Git commit ourselves instead of relying on shadow_rs,
     // because shadow_rs does it in a really slow-to-compile way (it builds libgit2)
-    let hash = get_git_hash().unwrap_or_default();
+    // Allow overriding it with `NU_COMMIT_HASH` from outside, such as with nix.
+    let hash = get_git_hash().unwrap_or(
+        option_env!("NU_COMMIT_HASH")
+            .unwrap_or_default()
+            .to_string(),
+    );
     println!("cargo:rustc-env=NU_COMMIT_HASH={hash}");
     shadow_rs::ShadowBuilder::builder()
         .build()
