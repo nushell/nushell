7df03293ef2cb4663a6f83522066f15fc71c34fc
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2025-11-01 07:04:33 -0500

    oops

--- crates/nu-plugin/src/plugin/interface/mod.rs
+++ crates/nu-plugin/src/plugin/interface/mod.rs
@@ -1107,6 +1107,6 @@ fn set_pgrp_from_enter_foreground(pgrp: i64) -> Result<(), ShellError> {
 #[cfg(not(unix))]
 fn set_pgrp_from_enter_foreground(_pgrp: i64) -> Result<(), ShellError> {
     Err(ShellError::NushellFailed {
-        msg: "EnterForeground asked plugin to join process group, but this is not supported on non UNIX platforms.",
+        msg: "EnterForeground asked plugin to join process group, but this is not supported on non UNIX platforms.".to_string(),
     })
 }
