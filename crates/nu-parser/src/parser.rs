28ddcf1934517de084b0d38709b3d0c549503dbd
blackhat-hemsworth <114207848+blackhat-hemsworth@users.noreply.github.com>
2025-12-04 11:59:15 -0700

    fixed parsing issue with multiarm match + guard (#17115)
    <!--
    Thank you for improving Nushell!
    Please, read our contributing guide:
    https://github.com/nushell/nushell/blob/main/CONTRIBUTING.md
    -->
    
    ## Release notes summary - What our users need to know
    <!--
    This section will be included as part of our release notes. See the
    contributing guide for more details.
    Please include only details relevant for users here. Motivation and
    technical details can be added above or below this section.
    
    You may leave this section blank until your PR is finalized. Ask a core
    team member if you need help filling this section.
    -->
    
    match statements will now work with multiple arms _and_ a guard
    expression.
    
    Updated ﻿parse_﻿match_block_expression such that in `match 3 {1 | 2 | 3
    if true => 'test'}` the logic will now add "if true" guard to "1 | 2 |
    3" match pattern and evaluate per existing logic. Previously the guard
    check would be skipped for multi-arm expressions, which would always
    lead to a parse error (logic expecting "=>")
    
    fixes #16131 
    
    ## Tasks after submitting
    <!-- Remove any tasks which aren't relevant for your PR, or add your own
    -->
    - [ ] Update the
    [documentation](https://github.com/nushell/nushell.github.io)
    
    ---------
    
    Co-authored-by: zc he <blindfs19@gmail.com>

--- crates/nu-parser/src/parser.rs
+++ crates/nu-parser/src/parser.rs
@@ -5088,8 +5088,9 @@ pub fn parse_match_block_expression(working_set: &mut StateWorkingSet, span: Spa
                 guard: None,
                 span: Span::new(start, end),
             }
+        }
         // A match guard
-        } else if connector == b"if" {
+        if connector == b"if" {
             let if_end = {
                 let end = output[position].span.end;
                 Span::new(end, end)
