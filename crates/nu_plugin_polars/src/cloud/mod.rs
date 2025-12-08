3d62b8ecbaf87de279e8a669cfe8e5ff7fd8f4a6
Jack Wright <56345+ayax79@users.noreply.github.com>
2025-12-08 10:00:40 -0800

    Azure blob storage support for polars (#17116)
    ## Release notes summary - What our users need to know
    
    Introduces Azure support to the polars plugin. 
    
    The following environment variables will are supported:
    - AZURE_STORAGE_ACCOUNT_NAME: storage account name
    - AZURE_STORAGE_ACCOUNT_KEY: storage account master key
    - AZURE_STORAGE_ACCESS_KEY: alias for AZURE_STORAGE_ACCOUNT_KEY
    - AZURE_STORAGE_CLIENT_ID: client id for service principal authorization
    - AZURE_STORAGE_CLIENT_SECRET: client secret for service principal
    authorization
    - AZURE_STORAGE_TENANT_ID: tenant id used in oauth flows
    
    Example Usage:
    ```nu
    $env.AZURE_STORAGE_ACCOUNT_KEY = "<your access key value>"
    $env.AZURE_STORAGE_ACCOUNT_NAME = "<your account name>"
    ps | polars into-df | polars save azure://<your account name>/ps.parquet
    polars open azure://<your account name>/ps.parquet | polars first 5 | polars collect
    ```
    
    Co-authored-by: Jack Wright <jack.wright@nike.com>

--- crates/nu_plugin_polars/src/cloud/mod.rs
+++ crates/nu_plugin_polars/src/cloud/mod.rs
@@ -1,29 +1,23 @@
 use nu_protocol::ShellError;
 use polars::prelude::PlPath;
-use polars_io::cloud::CloudOptions;
-use polars_utils::plpath::CloudScheme;
+use polars_io::cloud::{CloudOptions, CloudType};
 
 use crate::PolarsPlugin;
 
 mod aws;
-
-enum CloudType {
-    Aws,
-}
-
-fn determine_cloud_type(plpath: &PlPath) -> Option<CloudType> {
-    match plpath.cloud_scheme() {
-        Some(CloudScheme::S3) | Some(CloudScheme::S3a) => Some(CloudType::Aws),
-        _ => None,
-    }
-}
+mod azure;
 
 pub(crate) fn build_cloud_options(
     plugin: &PolarsPlugin,
     path: &PlPath,
 ) -> Result<Option<CloudOptions>, ShellError> {
-    match determine_cloud_type(path) {
+    match path
+        .cloud_scheme()
+        .map(|ref scheme| CloudType::from_cloud_scheme(scheme))
+    {
         Some(CloudType::Aws) => aws::build_cloud_options(plugin).map(Some),
+        Some(CloudType::Azure) => azure::build_cloud_options().map(Some),
+
         _ => Ok(None),
     }
 }
