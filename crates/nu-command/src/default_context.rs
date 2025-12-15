d628c7e84e025341dd25bc77f754e20f46343b0a
Wind <WindSoilder@outlook.com>
2025-12-16 06:32:14 +0800

    add http pool cmd (#17177)
    I forgot adding `http pool` cmd in #17157. Which is reported by @Tyarel8
    .
    
    ## Release notes summary - What our users need to know
    NaN
    
    ## Tasks after submitting

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -408,6 +408,7 @@ pub fn add_shell_command_context(mut engine_state: EngineState) -> EngineState {
             HttpPost,
             HttpPut,
             HttpOptions,
+            HttpPool,
             Port,
             VersionCheck,
         }
