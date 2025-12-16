nushell/nushell

commit a1f54c5637a26286cd1807a7fa1861b6ba3ecacd

Fix up the env.rs tests (#17085)
Just uses `ends-with` and a prefix to skip the control characters at the
start of the strings

This fixes the issues that came up in #17038 for being run in terminals.
This doesn't fix the underlying issue, however it does allow tests to be
run in certain terminals and still pass.

This also fixes a problem with some of the tests that load `std/log`. If
`$env.NU_LOG_FORMAT` is set in the parent shell, then these tests will
incorrectly fail. Clearing the env out before running makes these work.

<!--
Thank you for improving Nushell!
Please, read our contributing guide:
https://github.com/nushell/nushell/blob/main/CONTRIBUTING.md
-->

## Release notes summary - What our users need to know
<!--
This section will be included as part of our release notes. See the
contributing guide for more details.
Please include only details relevant for users here. Motivation and
technical details can be added above or below this section.

You may leave this section blank until your PR is finalized. Ask a core
team member if you need help filling this section.
-->

## Tasks after submitting
<!-- Remove any tasks which aren't relevant for your PR, or add your own
-->
- [ ] Update the
[documentation](https://github.com/nushell/nushell.github.io)

crates/nu-test-support/src/macros.rs
@@ -427,9 +427,23 @@ fn setup_command(executable_path: &AbsolutePath, target_cwd: &AbsolutePath) -> C
     let mut command = Command::new(executable_path);
 
     command
+        .env_clear()
         .current_dir(target_cwd)
         .env_remove("FILE_PWD")
         .env("PWD", target_cwd); // setting PWD is enough to set cwd;
+
+    // Need these extra environments from before the environment is cleared.
+    #[cfg(windows)]
+    {
+        let envs: std::collections::HashMap<String, String> = std::env::vars()
+            .filter(|(n, _)| {
+                n.starts_with("System") // System variables for disks, paths, etc.
+                    || n == "NUSHELL_CARGO_PROFILE" // Variable for crate::fs::binaries()
+                    || n == "PATHEXT" // Needed for Windows translate `nu` to `.../nu.exe`
+            })
+            .collect();
+        command.envs(envs);
+    };
 
     command
 }

crates/nu-command/tests/commands/ucp.rs
@@ -1209,11 +1209,14 @@ fn test_cp_inside_glob_metachars_dir() {
 #[test]
 fn test_cp_to_customized_home_directory() {
     Playground::setup("cp_to_home", |dirs, sandbox| {
-        unsafe {
-            std::env::set_var("HOME", dirs.test());
-        }
         sandbox.with_files(&[EmptyFile("test_file.txt")]);
-        let actual = nu!(cwd: dirs.test(), "mkdir test; cp test_file.txt ~/test/");
+        let actual = nu!(
+            cwd: dirs.test(),
+            envs: vec![
+                ("HOME".into(), dirs.test().to_string_lossy().into())
+            ],
+            "mkdir test; cp test_file.txt ~/test/"
+        );
 
         assert!(actual.err.is_empty());
         assert!(files_exist_at(&["test_file.txt"], dirs.test().join("test")));

tests/shell/environment/env.rs
@@ -253,12 +253,12 @@ fn env_shlvl_commandstring_does_not_increment() {
 #[ignore = "Causing hangs when both tests overlap"]
 #[test]
 fn env_shlvl_in_repl() {
-    let actual = nu!("
+    let actual = nu!(r#"
         $env.SHLVL = 5
-        nu --no-std-lib -n -e 'print $env.SHLVL; exit'
-    ");
+        nu --no-std-lib -n -e 'print $"SHLVL:($env.SHLVL)"; exit'
+    "#);
 
-    assert_eq!(actual.out, "6");
+    assert!(actual.out.ends_with("SHLVL:6"));
 }
 
 #[ignore = "Causing hangs when both tests overlap"]
@@ -266,19 +266,19 @@ fn env_shlvl_in_repl() {
 fn env_shlvl_in_exec_repl() {
     let actual = nu!(r#"
         $env.SHLVL = 29
-        nu -c "exec nu --no-std-lib -n -e 'print $env.SHLVL; exit'"
+        nu -c 'exec nu --no-std-lib -n -e `print $"SHLVL:($env.SHLVL)"; exit`'
     "#);
 
-    assert_eq!(actual.out, "30");
+    assert!(actual.out.ends_with("SHLVL:30"));
 }
 
 #[test]
 fn path_is_a_list_in_repl() {
     let actual = nu!(r#"
-        nu -c "exec nu --no-std-lib -n -e 'print ($env.pATh | describe); exit'"
+        nu -c "exec nu --no-std-lib -n -e `print $'path:($env.pATh | describe)'; exit`"
     "#);
 
-    assert_eq!(actual.out, "list<string>");
+    assert!(actual.out.ends_with("path:list<string>"));
 }
 
 #[test]

commit d959aaef52a44d1913364400dda15ef175eea0c7

Added CARGO_ and RUSTUP_ environment variables back to test environment (#17186)
#17185 # Release notes summary - What our users need to know

Fixes an issue caused by #17085. In that PR, the environment is reset
before running tests. That's a good thing, but it breaks tests entirely
if your Cargo environment is customized using environment variables.
This adds back in `CARGO_*` and `RUSTUP_*` environment variables to help
ensure a proper test run.

## Tasks after submitting

N/A

crates/nu-test-support/src/macros.rs
@@ -433,17 +433,16 @@ fn setup_command(executable_path: &AbsolutePath, target_cwd: &AbsolutePath) -> C
         .env("PWD", target_cwd); // setting PWD is enough to set cwd;
 
     // Need these extra environments from before the environment is cleared.
-    #[cfg(windows)]
-    {
-        let envs: std::collections::HashMap<String, String> = std::env::vars()
-            .filter(|(n, _)| {
-                n.starts_with("System") // System variables for disks, paths, etc.
-                    || n == "NUSHELL_CARGO_PROFILE" // Variable for crate::fs::binaries()
-                    || n == "PATHEXT" // Needed for Windows translate `nu` to `.../nu.exe`
-            })
-            .collect();
-        command.envs(envs);
-    };
+    let envs: std::collections::HashMap<String, String> = std::env::vars()
+        .filter(|(n, _)| {
+            n.starts_with("System") // System variables for disks, paths, etc.
+                || n == "NUSHELL_CARGO_PROFILE" // Variable for crate::fs::binaries()
+                || n == "PATHEXT" // Needed for Windows translate `nu` to `.../nu.exe`
+                || n.starts_with("CARGO_")
+                || n.starts_with("RUSTUP_")
+        })
+        .collect();
+    command.envs(envs);
 
     command
 }