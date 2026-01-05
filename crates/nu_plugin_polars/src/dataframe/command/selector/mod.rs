4a6dc5edd697dda4fae13edd0d56528bd674d87e
Jack Wright <jack.wright@nike.com>
2026-01-05 13:18:16 -0800

    Stub command for `polars selector`

--- crates/nu_plugin_polars/src/dataframe/command/selector/mod.rs
+++ crates/nu_plugin_polars/src/dataframe/command/selector/mod.rs
@@ -3,6 +3,7 @@ mod selector_by_dtype;
 mod selector_by_name;
 mod selector_first;
 mod selector_last;
+mod selector_stub;
 
 use nu_plugin::PluginCommand;
 
@@ -13,9 +14,11 @@ pub use selector_by_dtype::SelectorByDtype;
 pub use selector_by_name::SelectorByName;
 pub use selector_first::SelectorFirst;
 pub use selector_last::SelectorLast;
+pub use selector_stub::SelectorCmd;
 
 pub(crate) fn selector_commands() -> Vec<Box<dyn PluginCommand<Plugin = PolarsPlugin>>> {
     vec![
+        Box::new(SelectorCmd),
         Box::new(SelectorAll),
         Box::new(SelectorByDtype),
         Box::new(SelectorByName),
