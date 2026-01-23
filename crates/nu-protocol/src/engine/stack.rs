75379da683068cb5cd112e9097af62f6cd56d63e
nekluu <mustafaxuglen@gmail.com>
2026-01-23 04:18:29 +0300

    refactor(nu-protocol): replace panic with exit(1) in engine/stack.rs

--- crates/nu-protocol/src/engine/stack.rs
+++ crates/nu-protocol/src/engine/stack.rs
@@ -278,8 +278,8 @@ impl Stack {
                 ));
             }
         } else {
-            // TODO: Remove panic
-            panic!("internal error: no active overlay");
+            eprint!("internal error: no active overlay");
+            std::process::exit(1);
         }
     }
 
