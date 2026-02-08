8203f6d6550022a575126596bff1beed50013281
innocentzero <1nn0c3n7z3r0@proton.me>
2026-02-08 23:09:22 +0530

    POC for skip columns
    Signed-off-by: innocentzero <1nn0c3n7z3r0@proton.me>

--- crates/nu-command/src/filters/skip/mod.rs
+++ crates/nu-command/src/filters/skip/mod.rs
@@ -1,7 +1,9 @@
 mod skip_;
 mod skip_until;
 mod skip_while;
+mod skip_column;
 
 pub use skip_::Skip;
 pub use skip_until::SkipUntil;
 pub use skip_while::SkipWhile;
+pub use skip_column::SkipColumn;
