0c129fd0656f9ccb6bb85a7be6282821339d8f12
innocentzero <1nn0c3n7z3r0@proton.me>
2026-02-09 07:29:58 +0530

    cargo fmt
    Signed-off-by: innocentzero <1nn0c3n7z3r0@proton.me>

--- crates/nu-command/src/filters/skip/mod.rs
+++ crates/nu-command/src/filters/skip/mod.rs
@@ -1,9 +1,9 @@
 mod skip_;
+mod skip_column;
 mod skip_until;
 mod skip_while;
-mod skip_column;
 
 pub use skip_::Skip;
+pub use skip_column::SkipColumn;
 pub use skip_until::SkipUntil;
 pub use skip_while::SkipWhile;
-pub use skip_column::SkipColumn;
