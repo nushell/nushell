a5cb824b723d0af026dd910c700d0e93d24d8ab9
Jack Wright <jack.wright@nike.com>
2026-02-17 11:13:28 -0800

    Allows conversion of NuExpression to NuSelector

--- crates/nu_plugin_polars/src/dataframe/values/nu_selector/mod.rs
+++ crates/nu_plugin_polars/src/dataframe/values/nu_selector/mod.rs
@@ -8,7 +8,10 @@ use uuid::Uuid;
 pub use self::custom_value::NuSelectorCustomValue;
 
 use super::{CustomValueSupport, NuExpression, PolarsPluginObject, PolarsPluginType};
-use crate::Cacheable;
+use crate::{
+    Cacheable, PolarsPlugin,
+    values::{NuExpressionCustomValue, PolarsPluginCustomValue},
+};
 
 #[derive(Default, Clone, Debug)]
 pub struct NuSelector {
@@ -110,5 +113,49 @@ impl CustomValueSupport for NuSelector {
 
     fn base_value(self, span: Span) -> Result<Value, ShellError> {
         self.to_value(span)
+    }
+
+    fn try_from_value(plugin: &PolarsPlugin, value: &Value) -> Result<Self, ShellError> {
+        match value {
+            Value::String { val, .. } => {
+                let selector = Selector::ByName {
+                    names: [val.into()].into(),
+                    strict: true,
+                };
+                Ok(NuSelector::new(Some(selector)))
+            }
+            Value::Custom {
+                val, internal_span, ..
+            } => {
+                if let Some(cv) = val.as_any().downcast_ref::<Self::CV>() {
+                    Self::try_from_custom_value(plugin, cv)
+                } else if let Some(cv) = val.as_any().downcast_ref::<NuExpressionCustomValue>() {
+                    let expr = NuExpression::try_from_custom_value(plugin, cv)?;
+                    let selector = expr.into_polars().try_into_selector().map_err(|e| {
+                        ShellError::GenericError {
+                            error: e.to_string(),
+                            msg: "".into(),
+                            span: Some(*internal_span),
+                            help: None,
+                            inner: vec![],
+                        }
+                    })?;
+                    Ok(NuSelector::new(Some(selector)))
+                } else {
+                    Err(ShellError::CantConvert {
+                        to_type: Self::get_type_static().to_string(),
+                        from_type: value.get_type().to_string(),
+                        span: value.span(),
+                        help: None,
+                    })
+                }
+            }
+            _ => Err(ShellError::CantConvert {
+                to_type: Self::get_type_static().to_string(),
+                from_type: value.get_type().to_string(),
+                span: value.span(),
+                help: None,
+            }),
+        }
     }
 }
