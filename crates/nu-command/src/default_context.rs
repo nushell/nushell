81f9b280690de72aaf8f25f5bb3e1f0455487535
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-02-27 15:13:32 +0100

    move native clip commands behind experimental option

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -256,11 +256,13 @@ pub fn add_shell_command_context(mut engine_state: EngineState) -> EngineState {
 
         // Platform
         #[cfg(all(feature = "os", not(target_arch = "wasm32")))]
-        bind_command! {
-            ClipCommand,
-            ClipCopy,
-            ClipPaste,
-        };
+        if nu_experimental::NATIVE_CLIP.get() {
+            bind_command! {
+                ClipCommand,
+                ClipCopy,
+                ClipPaste,
+            };
+        }
 
         #[cfg(feature = "os")]
         bind_command! {
