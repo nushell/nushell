c3025e58b25e28f2117807c2e6f408b4f03ca391
blindfs <blindfs19@gmail.com>
2026-03-04 21:28:10 +0800

    fix typo

--- crates/nu-experimental/src/options/cell_path_types.rs
+++ crates/nu-experimental/src/options/cell_path_types.rs
@@ -1,7 +1,7 @@
 use crate::*;
 
 /// Enable type inferencing on "full cell path" expressions which are all typed as `any` if
-/// disabled. Inferred types are stored on internal AST, which are handful for some downstream
+/// disabled. Inferred types are stored on internal AST, which are helpful for some downstream
 /// tasks such as nu-lsp inlay hints.
 /// As a side effect, some previously allowed operations may get rejected with type-mismatch parsing errors.
 pub static CELL_PATH_TYPES: ExperimentalOption = ExperimentalOption::new(&CellPathTypes);
