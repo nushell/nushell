ea1a4b8679ac52ab18ce007c5263e023cc1e3336
Jack Wright <jack.wright@nike.com>
2026-03-04 16:26:29 -0800

    Polars: implementing selectors for numeric types

---
+++ crates/nu_plugin_polars/src/dataframe/command/selector/selector_integer.rs
@@ -0,0 +1,90 @@
+use crate::values::NuDataFrame;
+use crate::{
+    PolarsPlugin,
+    dataframe::values::NuSelector,
+    values::{CustomValueSupport, PolarsPluginType},
+};
+use nu_plugin::{EngineInterface, EvaluatedCall, PluginCommand};
+use nu_protocol::{Category, Example, LabeledError, PipelineData, Signature, Span, Type};
+use polars::df;
+use polars::prelude::{DataTypeSelector, Selector};
+
+#[derive(Clone)]
+pub struct SelectorInteger;
+
+impl PluginCommand for SelectorInteger {
+    type Plugin = PolarsPlugin;
+
+    fn name(&self) -> &str {
+        "polars selector integer"
+    }
+
+    fn description(&self) -> &str {
+        "Select all integer columns."
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build(self.name())
+            .input_output_type(Type::Any, PolarsPluginType::NuSelector.into())
+            .category(Category::Custom("expression".into()))
+    }
+
+    fn examples(&self) -> Vec<Example<'_>> {
+        vec![Example {
+            example: r#"{
+        "foo": ["x", "y"],
+        "bar": [123, 456],
+        "baz": [2.0, 5.5],
+        "zap": [0, 0],
+    } |
+    polars into-df --as-columns |
+    polars select (polars selector integer) |
+    polars sort-by bar zap |
+    polars collect"#,
+            description: "Select all integer columns",
+            result: Some(
+                NuDataFrame::from(
+                    df!(
+                        "bar" => [123, 456],
+                        "zap" => [0, 0],
+                    )
+                    .expect("simple df for test should not fail"),
+                )
+                .into_value(Span::test_data()),
+            ),
+        }]
+    }
+
+    fn search_terms(&self) -> Vec<&str> {
+        vec!["columns", "select", "integer", "int"]
+    }
+
+    fn run(
+        &self,
+        plugin: &Self::Plugin,
+        engine: &EngineInterface,
+        call: &EvaluatedCall,
+        input: PipelineData,
+    ) -> Result<PipelineData, LabeledError> {
+        let metadata = input.metadata();
+
+        let selector = Selector::ByDType(DataTypeSelector::Integer);
+        let nu_selector = NuSelector::from(selector);
+
+        nu_selector
+            .to_pipeline_data(plugin, engine, call.head)
+            .map_err(LabeledError::from)
+            .map(|pd| pd.set_metadata(metadata))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::test_polars_plugin_command;
+
+    #[test]
+    fn test_examples() -> Result<(), nu_protocol::ShellError> {
+        test_polars_plugin_command(&SelectorInteger)
+    }
+}
