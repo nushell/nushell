e9fa0530f4eb3c952bbafff242f3829ad284119a
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-09 17:53:28 +0100

    refactor format dir

--- crates/nu-command/tests/commands/date/format.rs
+++ crates/nu-command/tests/commands/date/format.rs
@@ -1,117 +1,116 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn formatter_not_valid() {
-    let actual = nu!(r#"
+fn formatter_not_valid() -> Result {
+    let code = r#"
         date now | format date '%N'
-        "#);
+        "#;
 
-    assert!(actual.err.contains("invalid format"));
+    let err = test().run(code).expect_shell_error()?;
+    assert!(matches!(err, ShellError::TypeMismatch { .. }));
+    Ok(())
 }
 
 #[test]
-fn test_j_q_format_specifiers() {
-    let actual = nu!(r#"
+fn test_j_q_format_specifiers() -> Result {
+    let code = r#"
         "2021-10-22 20:00:12 +01:00" | format date '%J_%Q'
-        "#);
+        "#;
 
-    assert_eq!(actual.out, "20211022_200012");
+    test().run(code).expect_value_eq("20211022_200012")
 }
 
 #[test]
-fn test_j_q_format_specifiers_current_time() {
-    let actual = nu!(r#"
+fn test_j_q_format_specifiers_current_time() -> Result {
+    let code = r#"
         date now | format date '%J_%Q' | str length
-        "#);
+        "#;
 
     // Should be exactly 15 characters: YYYYMMDD_HHMMSS
-    assert_eq!(actual.out, "15");
+    test().run(code).expect_value_eq(15)
 }
 
 #[test]
-fn test_j_format_specifier_date_only() {
-    let actual = nu!(r#"
+fn test_j_format_specifier_date_only() -> Result {
+    let code = r#"
         "2021-10-22 20:00:12 +01:00" | format date '%J'
-        "#);
+        "#;
 
-    assert_eq!(actual.out, "20211022");
+    test().run(code).expect_value_eq("20211022")
 }
 
 #[test]
-fn test_q_format_specifier_time_only() {
-    let actual = nu!(r#"
+fn test_q_format_specifier_time_only() -> Result {
+    let code = r#"
         "2021-10-22 20:00:12 +01:00" | format date '%Q'
-        "#);
+        "#;
 
-    assert_eq!(actual.out, "200012");
+    test().run(code).expect_value_eq("200012")
 }
 
 #[test]
-fn fails_without_input() {
-    let actual = nu!(r#"
+fn fails_without_input() -> Result {
+    let code = r#"
         format date "%c"
-        "#);
+        "#;
 
-    assert!(actual.err.contains("Pipeline empty"));
+    let err = test().run(code).expect_shell_error()?;
+    assert!(matches!(err, ShellError::PipelineEmpty { .. }));
+    Ok(())
 }
 
 #[test]
-fn locale_format_respect_different_locale() {
-    let actual = nu!(locale: "en_US", r#"
+fn locale_format_respect_different_locale() -> Result {
+    let code = r#"
     "2021-10-22 20:00:12 +01:00" | format date "%c"
-    "#);
-    assert!(actual.out.contains("Fri 22 Oct 2021 08:00:12 PM +01:00"));
+    "#;
+    let actual: String = test().locale("en_US").run(code)?;
+    assert_contains("Fri 22 Oct 2021 08:00:12 PM +01:00", actual);
 
-    let actual = nu!(locale: "en_GB", r#"
-    "2021-10-22 20:00:12 +01:00" | format date "%c"
-    "#);
-    assert!(actual.out.contains("Fri 22 Oct 2021 20:00:12 +01:00"));
+    let actual: String = test().locale("en_GB").run(code)?;
+    assert_contains("Fri 22 Oct 2021 20:00:12 +01:00", actual);
 
-    let actual = nu!(locale: "de_DE", r#"
-    "2021-10-22 20:00:12 +01:00" | format date "%c"
-    "#);
-    assert!(actual.out.contains("Fr 22 Okt 2021 20:00:12 +01:00"));
+    let actual: String = test().locale("de_DE").run(code)?;
+    assert_contains("Fr 22 Okt 2021 20:00:12 +01:00", actual);
 
-    let actual = nu!(locale: "zh_CN", r#"
-    "2021-10-22 20:00:12 +01:00" | format date "%c"
-    "#);
-    assert!(actual.out.contains("2021年10月22日 星期五 20时00分12秒"));
+    let actual: String = test().locale("zh_CN").run(code)?;
+    assert_contains("2021年10月22日 星期五 20时00分12秒", actual);
 
-    let actual = nu!(locale: "ja_JP", r#"
-    "2021-10-22 20:00:12 +01:00" | format date "%c"
-    "#);
-    assert!(actual.out.contains("2021年10月22日 20時00分12秒"));
+    let actual: String = test().locale("ja_JP").run(code)?;
+    assert_contains("2021年10月22日 20時00分12秒", actual);
 
-    let actual = nu!(locale: "fr_FR", r#"
-    "2021-10-22 20:00:12 +01:00" | format date "%c"
-    "#);
-    assert!(actual.out.contains("ven. 22 oct. 2021 20:00:12 +01:00"));
+    let actual: String = test().locale("fr_FR").run(code)?;
+    assert_contains("ven. 22 oct. 2021 20:00:12 +01:00", actual);
+    Ok(())
 }
 
 #[test]
-fn format_respects_locale_from_with_env() {
+fn format_respects_locale_from_with_env() -> Result {
     // Tests for https://github.com/nushell/nushell/issues/17321
 
-    let actual = nu!(r#"
+    let code = r#"
     "2021-10-22 20:00:12 +01:00" | with-env { NU_TEST_LOCALE_OVERRIDE: ko_KR.UTF-8 } { format date "%x %X" }
-    "#);
-    assert!(actual.out.contains("2021년 10월 22일 20시 00분 12초"));
+    "#;
+    let actual: String = test().run(code)?;
+    assert_contains("2021년 10월 22일 20시 00분 12초", actual);
 
-    let actual = nu!(r#"
+    let code = r#"
     "2021-10-22 20:00:12 +01:00" | with-env { NU_TEST_LOCALE_OVERRIDE: en_US.UTF-8 } { format date "%x %X" }
-    "#);
-    assert!(actual.out.contains("10/22/2021 08:00:12 PM"));
+    "#;
+    let actual: String = test().run(code)?;
+    assert_contains("10/22/2021 08:00:12 PM", actual);
+    Ok(())
 }
 
 #[test]
-fn locale_with_different_format_specifiers() {
-    let actual = nu!(locale: "en_US", r#"
+fn locale_with_different_format_specifiers() -> Result {
+    let code = r#"
         "Thu, 26 Oct 2023 22:52:14 +0200" | format date "%x %X"
-        "#);
-    assert!(actual.out.contains("10/26/2023 10:52:14 PM"));
+        "#;
+    let actual: String = test().locale("en_US").run(code)?;
+    assert_contains("10/26/2023 10:52:14 PM", actual);
 
-    let actual = nu!(locale: "nl_NL", r#"
-        "Thu, 26 Oct 2023 22:52:14 +0200" | format date "%x %X"
-        "#);
-    assert!(actual.out.contains("26-10-23 22:52:14"));
+    let actual: String = test().locale("nl_NL").run(code)?;
+    assert_contains("26-10-23 22:52:14", actual);
+    Ok(())
 }
