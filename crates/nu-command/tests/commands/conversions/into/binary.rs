c374fcd6f8c8b3dee26a12ee6c88e06a602b4c25
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-09 17:53:30 +0100

    fmt

--- crates/nu-command/tests/commands/conversions/into/binary.rs
+++ crates/nu-command/tests/commands/conversions/into/binary.rs
@@ -9,5 +9,8 @@ fn sets_stream_from_internal_command_as_binary() -> Result {
 #[test]
 fn sets_stream_from_external_command_as_binary() -> Result {
     let code = "^nu --testbin cococo | into binary | describe";
-    test().add_nu_to_path().run(code).expect_value_eq("binary (stream)")
+    test()
+        .add_nu_to_path()
+        .run(code)
+        .expect_value_eq("binary (stream)")
 }
