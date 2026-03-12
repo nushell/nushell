7b94039133d34c8a5fa228f54e94eadec809c55e
Juhan280 <azurarobo280@gmail.com>
2026-03-12 21:08:12 +0600

    replace todo! macro with todo comments

--- crates/nu-cli/src/repl.rs
+++ crates/nu-cli/src/repl.rs
@@ -737,7 +737,8 @@ fn loop_iteration(ctx: LoopContext) -> (bool, Stack, Reedline) {
             // if cleanup_exit didn't exit, we should keep running
             return (true, stack, line_editor);
         }
-        Ok(_) => todo!(),
+        // TODO: handle other signals
+        Ok(_) => (),
         Err(err) => {
             let message = err.to_string();
             if !message.contains("duration") {
--- crates/nu-command/src/platform/input/input_.rs
+++ crates/nu-command/src/platform/input/input_.rs
@@ -198,7 +198,8 @@ impl Command for Input {
                 // Do nothing on ctrl-d
                 return Ok(Value::nothing(call.head).into_pipeline_data());
             }
-            Ok(_) => todo!(),
+            // TODO: handle other signals
+            Ok(_) => (),
             Err(event_error) => {
                 let from_io_error = IoError::factory(call.head, None);
                 return Err(from_io_error(event_error).into());
