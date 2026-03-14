ab591e36e551401928d76ab4cf016371d3de4c95
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-14 13:44:01 +0100

    fix clippy for WASM

--- crates/nu-utils/src/net.rs
+++ crates/nu-utils/src/net.rs
@@ -6,6 +6,8 @@ use std::{
 pub fn reserve_local_addr() -> io::Result<SocketAddr> {
     let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 0))?;
     let addr = listener.local_addr()?;
+    // The `TcpListener` impl for WASM does not implement `Drop` for some reason.
+    #[cfg_attr(target_arch = "wasm32", expect(clippy::drop_non_drop))]
     drop(listener);
     Ok(addr)
 }
