2a37f8ab238a8fd392bf62ec35c41a53716990c4
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-16 20:00:41 +0100

    refactor config_env_default module

--- crates/nu-command/tests/commands/config_env_default.rs
+++ crates/nu-command/tests/commands/config_env_default.rs
@@ -1,13 +1,8 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn print_config_env_default_to_stdout() {
-    let actual = nu!("config env --default");
-    assert_eq!(
-        actual.out,
-        nu_utils::ConfigFileKind::Env
-            .default()
-            .replace(['\n', '\r'], "")
-    );
-    assert!(actual.err.is_empty());
+fn print_config_env_default_to_stdout() -> Result {
+    test()
+        .run("config env --default")
+        .expect_value_eq(nu_utils::ConfigFileKind::Env.default())
 }
