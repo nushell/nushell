46c4ff8b4f56536103b0f6314f1faf1abc3f8595
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-16 16:39:16 +0100

    refactor chunk_by module

--- crates/nu-command/tests/commands/chunk_by.rs
+++ crates/nu-command/tests/commands/chunk_by.rs
@@ -1,58 +1,65 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn chunk_by_on_empty_input_returns_empty_list() {
-    let actual = nu!("[] | chunk-by {|it| $it} | to nuon");
-    assert!(actual.err.is_empty());
-    assert_eq!(actual.out, "[]");
+fn chunk_by_on_empty_input_returns_empty_list() -> Result {
+    test()
+        .run("[] | chunk-by {|it| $it} | to nuon")
+        .expect_value_eq("[]")
+}
+
+#[rustfmt::skip]
+#[test]
+fn chunk_by_strings_works() -> Result {
+    let code = "
+        [a a a b b b c c c a a a]
+        | chunk-by {|it| $it}
+    ";
+
+    test().run(code).expect_value_eq([
+        ["a", "a", "a"],
+        ["b", "b", "b"],
+        ["c", "c", "c"],
+        ["a", "a", "a"],
+    ])
 }
 
 #[test]
-fn chunk_by_strings_works() {
-    let sample = r#"[a a a b b b c c c a a a]"#;
+fn chunk_by_field_works() -> Result {
+    #[derive(Debug, IntoValue)]
+    struct Sample {
+        name: &'static str,
+        age: u32,
+        cool: bool,
+    }
 
-    let actual = nu!(format!(
-        r#"
-            {sample}
-            | chunk-by {{|it| $it}}
-            | to nuon
-        "#
-    ));
+    let data = [
+        Sample {
+            name: "bob",
+            age: 20,
+            cool: false,
+        },
+        Sample {
+            name: "jane",
+            age: 30,
+            cool: false,
+        },
+        Sample {
+            name: "marie",
+            age: 19,
+            cool: true,
+        },
+        Sample {
+            name: "carl",
+            age: 36,
+            cool: true,
+        },
+    ];
 
-    assert_eq!(actual.out, "[[a, a, a], [b, b, b], [c, c, c], [a, a, a]]");
-}
+    let code = "
+        $in
+        | chunk-by {|it| $it.cool}
+        | length
+    ";
 
-#[test]
-fn chunk_by_field_works() {
-    let sample = r#"[
-    {
-        name: bob,
-        age: 20,
-        cool: false
-    },
-    {
-        name: jane,
-        age: 30,
-        cool: false
-    },
-    {
-        name: marie,
-        age: 19,
-        cool: true
-    },
-    {
-        name: carl,
-        age: 36,
-        cool: true
-    } ]"#;
-
-    let actual = nu!(format!(
-        r#"
-            {sample}
-            | chunk-by {{|it| $it.cool}}
-            | length
-        "#
-    ));
-
-    assert_eq!(actual.out, "2");
+    test().run_with_data(code, data).expect_value_eq(2)
 }
