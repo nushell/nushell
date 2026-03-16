7cfbdb45f949288a25236ec43cf98fc30aa899d4
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-16 15:20:26 +0100

    refactor window module

--- crates/nu-command/tests/commands/window.rs
+++ crates/nu-command/tests/commands/window.rs
@@ -1,105 +1,136 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn window_size_negative() {
-    let actual = nu!("[0 1 2] | window -1");
-    assert!(actual.err.contains("positive"));
+fn window_size_negative() -> Result {
+    let err = test().run("[0 1 2] | window -1").expect_shell_error()?;
+    assert!(matches!(err, ShellError::NeedsPositiveValue { .. }));
+    Ok(())
 }
 
 #[test]
-fn window_size_zero() {
-    let actual = nu!("[0 1 2] | window 0");
-    assert!(actual.err.contains("zero"));
+fn window_size_zero() -> Result {
+    let err = test().run("[0 1 2] | window 0").expect_shell_error()?;
+    match err {
+        ShellError::IncorrectValue { msg, .. } => {
+            assert_eq!(msg, "`window_size` cannot be zero");
+            Ok(())
+        }
+        err => Err(err.into()),
+    }
 }
 
 #[test]
-fn window_size_not_int() {
-    let actual = nu!("[0 1 2] | window (if true { 1sec })");
-    assert!(actual.err.contains("can't convert"));
+fn window_size_not_int() -> Result {
+    let err = test()
+        .run("[0 1 2] | window (if true { 1sec })")
+        .expect_shell_error()?;
+    assert!(matches!(err, ShellError::CantConvert { .. }));
+    Ok(())
 }
 
 #[test]
-fn stride_negative() {
-    let actual = nu!("[0 1 2] | window 1 -s -1");
-    assert!(actual.err.contains("positive"));
+fn stride_negative() -> Result {
+    let err = test()
+        .run("[0 1 2] | window 1 -s -1")
+        .expect_shell_error()?;
+    assert!(matches!(err, ShellError::NeedsPositiveValue { .. }));
+    Ok(())
 }
 
 #[test]
-fn stride_zero() {
-    let actual = nu!("[0 1 2] | window 1 -s 0");
-    assert!(actual.err.contains("zero"));
+fn stride_zero() -> Result {
+    let err = test().run("[0 1 2] | window 1 -s 0").expect_shell_error()?;
+    match err {
+        ShellError::IncorrectValue { msg, .. } => {
+            assert_eq!(msg, "`stride` cannot be zero");
+            Ok(())
+        }
+        err => Err(err.into()),
+    }
 }
 
 #[test]
-fn stride_not_int() {
-    let actual = nu!("[0 1 2] | window 1 -s (if true { 1sec })");
-    assert!(actual.err.contains("can't convert"));
+fn stride_not_int() -> Result {
+    let err = test()
+        .run("[0 1 2] | window 1 -s (if true { 1sec })")
+        .expect_shell_error()?;
+    assert!(matches!(err, ShellError::CantConvert { .. }));
+    Ok(())
 }
 
 #[test]
-fn empty() {
-    let actual = nu!("[] | window 2 | is-empty");
-    assert_eq!(actual.out, "true");
+fn empty() -> Result {
+    test().run("[] | window 2 | is-empty").expect_value_eq(true)
 }
 
 #[test]
-fn list_stream() {
-    let actual = nu!("([0 1 2] | every 1 | window 2) == ([0 1 2] | window 2)");
-    assert_eq!(actual.out, "true");
+fn list_stream() -> Result {
+    test()
+        .run("([0 1 2] | every 1 | window 2) == ([0 1 2] | window 2)")
+        .expect_value_eq(true)
 }
 
 #[test]
-fn table_stream() {
-    let actual = nu!(
-        "([[foo bar]; [0 1] [2 3] [4 5]] | every 1 | window 2) == ([[foo bar]; [0 1] [2 3] [4 5]] | window 2)"
-    );
-    assert_eq!(actual.out, "true");
+fn table_stream() -> Result {
+    test()
+        .run(
+            "([[foo bar]; [0 1] [2 3] [4 5]] | every 1 | window 2) == ([[foo bar]; [0 1] [2 3] [4 5]] | window 2)",
+        )
+        .expect_value_eq(true)
 }
 
 #[test]
-fn no_empty_chunks() {
-    let actual = nu!("([0 1 2 3 4 5] | window 3 -s 3 -r | length) == 2");
-    assert_eq!(actual.out, "true");
+fn no_empty_chunks() -> Result {
+    test()
+        .run("([0 1 2 3 4 5] | window 3 -s 3 -r | length) == 2")
+        .expect_value_eq(true)
 }
 
 #[test]
-fn same_as_chunks() {
-    let actual = nu!("([0 1 2 3 4] | window 2 -s 2 -r) == ([0 1 2 3 4 ] | chunks 2)");
-    assert_eq!(actual.out, "true");
+fn same_as_chunks() -> Result {
+    test()
+        .run("([0 1 2 3 4] | window 2 -s 2 -r) == ([0 1 2 3 4 ] | chunks 2)")
+        .expect_value_eq(true)
 }
 
 #[test]
-fn stride_equal_to_window_size() {
-    let actual = nu!("([0 1 2 3] | window 2 -s 2 | flatten) == [0 1 2 3]");
-    assert_eq!(actual.out, "true");
+fn stride_equal_to_window_size() -> Result {
+    test()
+        .run("([0 1 2 3] | window 2 -s 2 | flatten) == [0 1 2 3]")
+        .expect_value_eq(true)
 }
 
 #[test]
-fn stride_greater_than_window_size() {
-    let actual = nu!("([0 1 2 3 4] | window 2 -s 3 | flatten) == [0 1 3 4]");
-    assert_eq!(actual.out, "true");
+fn stride_greater_than_window_size() -> Result {
+    test()
+        .run("([0 1 2 3 4] | window 2 -s 3 | flatten) == [0 1 3 4]")
+        .expect_value_eq(true)
 }
 
 #[test]
-fn stride_less_than_window_size() {
-    let actual = nu!("([0 1 2 3 4 5] | window 3 -s 2 | length) == 2");
-    assert_eq!(actual.out, "true");
+fn stride_less_than_window_size() -> Result {
+    test()
+        .run("([0 1 2 3 4 5] | window 3 -s 2 | length) == 2")
+        .expect_value_eq(true)
 }
 
 #[test]
-fn stride_equal_to_window_size_remainder() {
-    let actual = nu!("([0 1 2 3 4] | window 2 -s 2 -r | flatten) == [0 1 2 3 4]");
-    assert_eq!(actual.out, "true");
+fn stride_equal_to_window_size_remainder() -> Result {
+    test()
+        .run("([0 1 2 3 4] | window 2 -s 2 -r | flatten) == [0 1 2 3 4]")
+        .expect_value_eq(true)
 }
 
 #[test]
-fn stride_greater_than_window_size_remainder() {
-    let actual = nu!("([0 1 2 3 4] | window 2 -s 3 -r | flatten) == [0 1 3 4]");
-    assert_eq!(actual.out, "true");
+fn stride_greater_than_window_size_remainder() -> Result {
+    test()
+        .run("([0 1 2 3 4] | window 2 -s 3 -r | flatten) == [0 1 3 4]")
+        .expect_value_eq(true)
 }
 
 #[test]
-fn stride_less_than_window_size_remainder() {
-    let actual = nu!("([0 1 2 3 4 5] | window 3 -s 2 -r | length) == 3");
-    assert_eq!(actual.out, "true");
+fn stride_less_than_window_size_remainder() -> Result {
+    test()
+        .run("([0 1 2 3 4 5] | window 3 -s 2 -r | length) == 3")
+        .expect_value_eq(true)
 }
