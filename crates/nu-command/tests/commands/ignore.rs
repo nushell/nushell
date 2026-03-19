f4407b3018be5fdacb465b3d6a80db18ea309f07
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-20 00:47:46 +0100

    refactor ignore module

--- crates/nu-command/tests/commands/ignore.rs
+++ crates/nu-command/tests/commands/ignore.rs
@@ -1,7 +1,18 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
+use std::fs;
 
 #[test]
-fn ignore_still_causes_stream_to_be_consumed_fully() {
-    let result = nu!("[foo bar] | each { |val| print $val; $val } | ignore");
-    assert_eq!("foobar", result.out);
+fn ignore_still_causes_stream_to_be_consumed_fully() -> Result {
+    Playground::setup("ignore_consumes_stream", |dirs, _| {
+        let code = "
+            [foo bar]
+            | each {|val| $val | save --append output.txt; $val}
+            | ignore
+        ";
+
+        let () = test().cwd(dirs.test()).run(code)?;
+        let file_content = fs::read_to_string(dirs.test().join("output.txt")).unwrap();
+        assert_eq!(file_content, "foobar");
+        Ok(())
+    })
 }
