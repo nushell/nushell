2d16d94b3c22e5135538abac4e38c7a5a24effa6
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-20 00:05:08 +0100

    refactor par_each module

--- crates/nu-command/tests/commands/par_each.rs
+++ crates/nu-command/tests/commands/par_each.rs
@@ -1,9 +1,9 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn par_each_does_not_flatten_nested_structures() {
+fn par_each_does_not_flatten_nested_structures() -> Result {
     // This is a regression test for issue #8497
-    let actual = nu!("[1 2 3] | par-each { |it| [$it, $it] } | sort | to json --raw");
+    let code = "[1 2 3] | par-each { |it| [$it, $it] } | sort";
 
-    assert_eq!(actual.out, "[[1,1],[2,2],[3,3]]");
+    test().run(code).expect_value_eq([[1,1],[2,2],[3,3]])
 }
