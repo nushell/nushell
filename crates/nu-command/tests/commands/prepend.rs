558b2702a637d53c7dbfc91818f200cd637d7df4
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-20 00:55:03 +0100

    refactor prepend module

--- crates/nu-command/tests/commands/prepend.rs
+++ crates/nu-command/tests/commands/prepend.rs
@@ -1,26 +1,10 @@
-use nu_test_support::fs::Stub::FileWithContentToBeTrimmed;
-use nu_test_support::nu;
-use nu_test_support::playground::Playground;
+use nu_test_support::prelude::*;
 
 #[test]
-fn adds_a_row_to_the_beginning() {
-    Playground::setup("prepend_test_1", |dirs, sandbox| {
-        sandbox.with_files(&[FileWithContentToBeTrimmed(
-            "los_tres_caballeros.txt",
-            "
-                Andrés N. Robalino
-                JT Turner
-                Yehuda Katz
-            ",
-        )]);
-
-        let actual = nu!(cwd: dirs.test(), r#"
-            open los_tres_caballeros.txt
-            | lines
-            | prepend "pollo loco"
-            | get 0
-            "#);
-
-        assert_eq!(actual.out, "pollo loco");
-    })
+fn adds_a_row_to_the_beginning() -> Result {
+    let input = ["Andrés N. Robalino", "JT Turner", "Yehuda Katz"];
+    let code = r#"$in | prepend "pollo loco" | get 0"#;
+    test()
+        .run_with_data(code, input)
+        .expect_value_eq("pollo loco")
 }
