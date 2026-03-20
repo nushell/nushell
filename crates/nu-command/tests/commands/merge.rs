8f6a99ee622390c2c22459b1bcd0c2e2ca68415a
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-20 21:18:00 +0100

    refactor merge module

--- crates/nu-command/tests/commands/merge.rs
+++ crates/nu-command/tests/commands/merge.rs
@@ -1,7 +1,7 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn row() {
+fn row() -> Result {
     let left_sample = "[[name, country, luck];
         [Andrés, Ecuador, 0],
         [JT, USA, 0],
@@ -12,7 +12,7 @@ fn row() {
          ["Andrés Robalino", "Guayaquil Ecuador", 1],
          ["JT Turner", "New Zealand", 1]]"#;
 
-    let actual = nu!(format!(
+    let code = format!(
         r#"
             ({left_sample})
             | merge ({right_sample})
@@ -20,73 +20,49 @@ fn row() {
             | get luck
             | math sum
         "#
-    ));
+    );
 
-    assert_eq!(actual.out, "2");
+    test().run(code).expect_value_eq(2)
 }
 
 #[test]
-fn single_record_no_overwrite() {
-    assert_eq!(
-        nu!("
-            {a: 1, b: 5} | merge {c: 2} | to nuon
-            ")
-        .out,
-        "{a: 1, b: 5, c: 2}"
-    );
+fn single_record_no_overwrite() -> Result {
+    test()
+        .run("{a: 1, b: 5} | merge {c: 2} | to nuon")
+        .expect_value_eq("{a: 1, b: 5, c: 2}")
 }
 
 #[test]
-fn single_record_overwrite() {
-    assert_eq!(
-        nu!("
-            {a: 1, b: 2} | merge {a: 2} | to nuon
-            ")
-        .out,
-        "{a: 2, b: 2}"
-    );
+fn single_record_overwrite() -> Result {
+    test()
+        .run("{a: 1, b: 2} | merge {a: 2} | to nuon")
+        .expect_value_eq("{a: 2, b: 2}")
 }
 
 #[test]
-fn single_row_table_overwrite() {
-    assert_eq!(
-        nu!("
-            [[a b]; [1 4]] | merge [[a b]; [2 4]] | to nuon
-            ")
-        .out,
-        "[[a, b]; [2, 4]]"
-    );
+fn single_row_table_overwrite() -> Result {
+    test()
+        .run("[[a b]; [1 4]] | merge [[a b]; [2 4]] | to nuon")
+        .expect_value_eq("[[a, b]; [2, 4]]")
 }
 
 #[test]
-fn single_row_table_no_overwrite() {
-    assert_eq!(
-        nu!("
-            [[a b]; [1 4]] | merge [[c d]; [2 4]] | to nuon
-            ")
-        .out,
-        "[[a, b, c, d]; [1, 4, 2, 4]]"
-    );
+fn single_row_table_no_overwrite() -> Result {
+    test()
+        .run("[[a b]; [1 4]] | merge [[c d]; [2 4]] | to nuon")
+        .expect_value_eq("[[a, b, c, d]; [1, 4, 2, 4]]")
 }
 
 #[test]
-fn multi_row_table_no_overwrite() {
-    assert_eq!(
-        nu!("
-            [[a b]; [1 4] [8 9] [9 9]] | merge [[c d]; [2 4]]  | to nuon
-            ")
-        .out,
-        "[{a: 1, b: 4, c: 2, d: 4}, {a: 8, b: 9}, {a: 9, b: 9}]"
-    );
+fn multi_row_table_no_overwrite() -> Result {
+    test()
+        .run("[[a b]; [1 4] [8 9] [9 9]] | merge [[c d]; [2 4]]  | to nuon")
+        .expect_value_eq("[{a: 1, b: 4, c: 2, d: 4}, {a: 8, b: 9}, {a: 9, b: 9}]")
 }
 
 #[test]
-fn multi_row_table_overwrite() {
-    assert_eq!(
-        nu!("
-            [[a b]; [1 4] [8 9] [9 9]] | merge  [[a b]; [7 7]]  | to nuon
-            ")
-        .out,
-        "[[a, b]; [7, 7], [8, 9], [9, 9]]"
-    );
+fn multi_row_table_overwrite() -> Result {
+    test()
+        .run("[[a b]; [1 4] [8 9] [9 9]] | merge  [[a b]; [7 7]]  | to nuon")
+        .expect_value_eq("[[a, b]; [7, 7], [8, 9], [9, 9]]")
 }
