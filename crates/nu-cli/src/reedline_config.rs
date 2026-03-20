c799283dde1f12afd51a028e89c631f40acbf749
Juhan280 <azurarobo280@gmail.com>
2026-03-21 03:17:50 +0600

    Update `keybindings list` to be more clear

--- crates/nu-cli/src/reedline_config.rs
+++ crates/nu-cli/src/reedline_config.rs
@@ -1104,7 +1104,7 @@ pub(crate) fn display_reedline_event(event: ReedlineEventDiscriminants) -> Optio
         RED::SearchHistory => "SearchHistory",
         RED::Multiple => "Multiple[ {{ ReedLineEvents, }} ]",
         RED::UntilFound => "UntilFound [ {{ ReedLineEvents, }} ]",
-        RED::Menu => "Menu Name: <string>",
+        RED::Menu => "Menu name: <string>",
         RED::MenuNext => "MenuNext",
         RED::MenuPrevious => "MenuPrevious",
         RED::MenuUp => "MenuUp",
@@ -1113,7 +1113,7 @@ pub(crate) fn display_reedline_event(event: ReedlineEventDiscriminants) -> Optio
         RED::MenuRight => "MenuRight",
         RED::MenuPageNext => "MenuPageNext",
         RED::MenuPagePrevious => "MenuPagePrevious",
-        RED::ExecuteHostCommand => "ExecuteHostCommand",
+        RED::ExecuteHostCommand => "ExecuteHostCommand cmd: <string>",
         RED::OpenEditor => "OpenEditor",
         RED::ViChangeMode => "ViChangeMode mode: <string>",
         RED::Mouse | RED::Resize => return None,
@@ -1245,14 +1245,14 @@ fn edit_from_record(
         Ok(ECD::CutCurrentLine) => EditCommand::CutCurrentLine,
         Ok(ECD::CutFromStart) => EditCommand::CutFromStart,
         Ok(ECD::CutFromStartLinewise) => EditCommand::CutFromStartLinewise {
-            leave_blank_line: extract_value("value", record, span)
+            leave_blank_line: extract_value("keep_line", record, span)
                 .and_then(|value| value.as_bool())?,
         },
         Ok(ECD::CutFromLineStart) => EditCommand::CutFromLineStart,
         Ok(ECD::CutFromLineNonBlankStart) => EditCommand::CutFromLineNonBlankStart,
         Ok(ECD::CutToEnd) => EditCommand::CutToEnd,
         Ok(ECD::CutToEndLinewise) => EditCommand::CutToEndLinewise {
-            leave_blank_line: extract_value("value", record, span)
+            leave_blank_line: extract_value("keep_line", record, span)
                 .and_then(|value| value.as_bool())?,
         },
         Ok(ECD::CutToLineEnd) => EditCommand::CutToLineEnd,
@@ -1440,11 +1440,11 @@ pub(crate) fn display_edit_command(edit: EditCommandDiscriminants) -> Option<&'s
         ECD::MoveBigWordRightEnd => "MoveBigWordRightEnd Optional[select: <bool>]",
         ECD::MoveWordRightStart => "MoveWordRightStart Optional[select: <bool>]",
         ECD::MoveBigWordRightStart => "MoveBigWordRightStart Optional[select: <bool>]",
-        ECD::MoveToPosition => "MoveToPosition  Value: <int>, Optional[select: <bool>]",
-        ECD::MoveLeftUntil => "MoveLeftUntil Value: <char>, Optional[select: <bool>]",
-        ECD::MoveLeftBefore => "MoveLeftBefore Value: <char>, Optional[select: <bool>]",
-        ECD::InsertChar => "InsertChar  Value: <char>",
-        ECD::InsertString => "InsertString Value: <string>",
+        ECD::MoveToPosition => "MoveToPosition value: <int>, Optional[select: <bool>]",
+        ECD::MoveLeftUntil => "MoveLeftUntil value: <char>, Optional[select: <bool>]",
+        ECD::MoveLeftBefore => "MoveLeftBefore value: <char>, Optional[select: <bool>]",
+        ECD::InsertChar => "InsertChar value: <char>",
+        ECD::InsertString => "InsertString value: <string>",
         ECD::InsertNewline => "InsertNewline",
         ECD::ReplaceChar => "ReplaceChar <char>",
         ECD::Backspace => "Backspace",
@@ -1457,11 +1457,11 @@ pub(crate) fn display_edit_command(edit: EditCommandDiscriminants) -> Option<&'s
         ECD::Complete => "Complete",
         ECD::CutCurrentLine => "CutCurrentLine",
         ECD::CutFromStart => "CutFromStart",
-        ECD::CutFromStartLinewise => "CutFromStartLinewise Value: <bool>",
+        ECD::CutFromStartLinewise => "CutFromStartLinewise keep_line: <bool>",
         ECD::CutFromLineStart => "CutFromLineStart",
         ECD::CutFromLineNonBlankStart => "CutFromLineNonBlankStart",
         ECD::CutToEnd => "CutToEnd",
-        ECD::CutToEndLinewise => "CutToEndLinewise Value: <bool>",
+        ECD::CutToEndLinewise => "CutToEndLinewise keep_line: <bool>",
         ECD::CutToLineEnd => "CutToLineEnd",
         ECD::KillLine => "KillLine",
         ECD::CutWordLeft => "CutWordLeft",
@@ -1480,12 +1480,12 @@ pub(crate) fn display_edit_command(edit: EditCommandDiscriminants) -> Option<&'s
         ECD::SwapGraphemes => "SwapGraphemes",
         ECD::Undo => "Undo",
         ECD::Redo => "Redo",
-        ECD::CutRightUntil => "CutRightUntil Value: <char>",
-        ECD::CutRightBefore => "CutRightBefore Value: <char>",
-        ECD::MoveRightUntil => "MoveRightUntil Value: <char>",
-        ECD::MoveRightBefore => "MoveRightBefore Value: <char>",
-        ECD::CutLeftUntil => "CutLeftUntil Value: <char>",
-        ECD::CutLeftBefore => "CutLeftBefore Value: <char>",
+        ECD::CutRightUntil => "CutRightUntil value: <char>",
+        ECD::CutRightBefore => "CutRightBefore value: <char>",
+        ECD::MoveRightUntil => "MoveRightUntil value: <char>",
+        ECD::MoveRightBefore => "MoveRightBefore value: <char>",
+        ECD::CutLeftUntil => "CutLeftUntil value: <char>",
+        ECD::CutLeftBefore => "CutLeftBefore value: <char>",
         ECD::SelectAll => "SelectAll",
         ECD::CutSelection => "CutSelection",
         ECD::CopySelection => "CopySelection",
@@ -1506,10 +1506,10 @@ pub(crate) fn display_edit_command(edit: EditCommandDiscriminants) -> Option<&'s
         ECD::CopyBigWordRightToNext => "CopyBigWordRightToNext",
         ECD::CopyLeft => "CopyLeft",
         ECD::CopyRight => "CopyRight",
-        ECD::CopyRightUntil => "CopyRightUntil Value: <char>",
-        ECD::CopyRightBefore => "CopyRightBefore Value: <char>",
-        ECD::CopyLeftUntil => "CopyLeftUntil Value: <char>",
-        ECD::CopyLeftBefore => "CopyLeftBefore Value: <char>",
+        ECD::CopyRightUntil => "CopyRightUntil value: <char>",
+        ECD::CopyRightBefore => "CopyRightBefore value: <char>",
+        ECD::CopyLeftUntil => "CopyLeftUntil value: <char>",
+        ECD::CopyLeftBefore => "CopyLeftBefore value: <char>",
         ECD::SwapCursorAndAnchor => "SwapCursorAndAnchor",
         #[cfg(feature = "system-clipboard")]
         ECD::CutSelectionSystem => "CutSelectionSystem",
@@ -1517,12 +1517,16 @@ pub(crate) fn display_edit_command(edit: EditCommandDiscriminants) -> Option<&'s
         ECD::CopySelectionSystem => "CopySelectionSystem",
         #[cfg(feature = "system-clipboard")]
         ECD::PasteSystem => "PasteSystem",
-        ECD::CutInsidePair => "CutInsidePair Value: <char> <char>",
-        ECD::CopyInsidePair => "CopyInsidePair Value: <char> <char>",
-        ECD::CutAroundPair => "CutAroundPair Value: <char> <char>",
-        ECD::CopyAroundPair => "CopyAroundPair Value: <char> <char>",
-        ECD::CutTextObject => "CutTextObject Value: <TextObject>",
-        ECD::CopyTextObject => "CopyTextObject Value: <TextObject>",
+        ECD::CutInsidePair => "CutInsidePair left: <char>, right <char>",
+        ECD::CopyInsidePair => "CopyInsidePair left: <char>, right <char>",
+        ECD::CutAroundPair => "CutAroundPair left: <char>, right <char>",
+        ECD::CopyAroundPair => "CopyAroundPair left: <char>, right <char>",
+        ECD::CutTextObject => {
+            "CutTextObject scope: <inner|around>, object_type: <word|bigword|bracket|quote>"
+        }
+        ECD::CopyTextObject => {
+            "CopyTextObject scope: <inner|around>, object_type: <word|bigword|bracket|quote>"
+        }
         ECD::ReplaceChars => return None,
     })
 }
