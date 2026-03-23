df2b9bc71459eccf6438a8ae19ea0b105706bf07
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-03-23 15:28:04 +0100

    mark ShellError::GenericError as deprecated

--- crates/nu-protocol/src/errors/shell_error/mod.rs
+++ crates/nu-protocol/src/errors/shell_error/mod.rs
@@ -1041,6 +1041,7 @@ pub enum ShellError {
     /// This is a generic error type used for different situations.
     #[error("{error}")]
     #[diagnostic(code(nu::shell::error))]
+    #[deprecated(note = "use `ShellError::Generic` instead")]
     GenericError {
         error: String,
         msg: String,
