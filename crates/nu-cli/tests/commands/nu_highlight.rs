5b19641678952562dbca8d1fb3678d525b82ed6f
pickx <pickyx@protonmail.com>
2026-03-28 01:27:20 +0300

    add tests

--- crates/nu-cli/tests/commands/nu_highlight.rs
+++ crates/nu-cli/tests/commands/nu_highlight.rs
@@ -1,4 +1,27 @@
 use nu_test_support::nu;
+use rstest::rstest;
+
+/// checks that garbage is highlighted as error
+#[rstest]
+#[case::internal("ps out>| $in")]
+#[case::internal_glob_arg("glob out>| $in")]
+#[case::internal_rest_args("reject a b c d out>| $in")]
+#[case::external("^foobar out>| $in")]
+#[case::single_invalid("&&")]
+fn nu_highlight_garbage_detection(#[case] cmd: &str) {
+    use std::fmt::Write;
+    let color = "#112233";
+
+    let mut buf = String::new();
+    writeln!(&mut buf, "let color = '{color}'");
+    writeln!(&mut buf, "$env.config.color_config.shape_garbage = $color");
+    writeln!(&mut buf, "let highlight = '{cmd}' | nu-highlight");
+    write!(&mut buf, "$highlight has (ansi $color)");
+
+    let outcome = nu!(buf);
+
+    assert_eq!(outcome.out, "true");
+}
 
 #[test]
 fn nu_highlight_not_expr() {
