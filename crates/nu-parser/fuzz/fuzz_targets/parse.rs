9494653a04522fa3d9d23080a85d0a539aebefed
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-04-01 15:51:29 +0200

    fix linter warnings

--- crates/nu-parser/fuzz/fuzz_targets/parse.rs
+++ crates/nu-parser/fuzz/fuzz_targets/parse.rs
@@ -9,5 +9,5 @@ fuzz_target!(|data: &[u8]| {
     let engine_state = EngineState::new();
     let mut working_set = StateWorkingSet::new(&engine_state);
 
-    let _block = parse(&mut working_set, None, &data, true);
+    let _block = parse(&mut working_set, None, data, true);
 });
