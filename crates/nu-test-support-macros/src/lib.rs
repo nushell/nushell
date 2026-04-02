e458a59ecdfada478012f005e422902ec2374e7f
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-04-02 22:10:03 +0200

    add comment that docs live in nu-test-support

--- crates/nu-test-support-macros/src/lib.rs
+++ crates/nu-test-support-macros/src/lib.rs
@@ -13,6 +13,7 @@ pub fn test(
     test::test(item_fn).into()
 }
 
+// For docs to this, check out the re-export in `nu_test_support::test_cell_path!()`.
 #[proc_macro]
 pub fn test_cell_path(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
     test_cell_path::test_cell_path(tokens.into())
