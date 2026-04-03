fe0c3390fca273d354f2547f223a6688fd3fc0a9
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-04-03 21:09:17 +0200

    move converting nu_json::Value into nu_protocol::Value into nu-json

--- crates/nu-command/src/formats/from/json.rs
+++ crates/nu-command/src/formats/from/json.rs
@@ -2,7 +2,7 @@ use std::io::{BufRead, Cursor};
 
 use nu_engine::command_prelude::*;
 use nu_protocol::{
-    ListStream, Signals, shell_error::generic::GenericError, shell_error::io::IoError,
+    ListStream, Signals, TryIntoValue, shell_error::{generic::GenericError, io::IoError}
 };
 
 #[derive(Clone)]
@@ -154,48 +154,9 @@ fn read_json_lines(
     ListStream::new(iter, span, signals)
 }
 
-fn convert_nujson_to_value(value: nu_json::Value, span: Span) -> Value {
-    match value {
-        nu_json::Value::Array(array) => Value::list(
-            array
-                .into_iter()
-                .map(|x| convert_nujson_to_value(x, span))
-                .collect(),
-            span,
-        ),
-        nu_json::Value::Bool(b) => Value::bool(b, span),
-        nu_json::Value::F64(f) => Value::float(f, span),
-        nu_json::Value::I64(i) => Value::int(i, span),
-        nu_json::Value::Null => Value::nothing(span),
-        nu_json::Value::Object(k) => Value::record(
-            k.into_iter()
-                .map(|(k, v)| (k, convert_nujson_to_value(v, span)))
-                .collect(),
-            span,
-        ),
-        nu_json::Value::U64(u) => {
-            if u > i64::MAX as u64 {
-                Value::error(
-                    ShellError::CantConvert {
-                        to_type: "i64 sized integer".into(),
-                        from_type: "value larger than i64".into(),
-                        span,
-                        help: None,
-                    },
-                    span,
-                )
-            } else {
-                Value::int(u as i64, span)
-            }
-        }
-        nu_json::Value::String(s) => Value::string(s, span),
-    }
-}
-
 pub fn convert_string_to_value(string_input: &str, span: Span) -> Result<Value, ShellError> {
-    match nu_json::from_str(string_input) {
-        Ok(value) => Ok(convert_nujson_to_value(value, span)),
-
+    match nu_json::from_str::<nu_json::Value>(string_input) {
+        Ok(value) => Ok(value.try_into_value(span)?),
         Err(x) => match x {
             nu_json::Error::Syntax(_, row, col) => {
                 let label = x.to_string();
@@ -225,8 +186,8 @@ pub fn convert_string_to_value(string_input: &str, span: Span) -> Result<Value, 
 }
 
 fn convert_string_to_value_strict(string_input: &str, span: Span) -> Result<Value, ShellError> {
-    match serde_json::from_str(string_input) {
-        Ok(value) => Ok(convert_nujson_to_value(value, span)),
+    match serde_json::from_str::<nu_json::Value>(string_input) {
+        Ok(value) => Ok(value.try_into_value(span)?),
         Err(err) => Err(if err.is_syntax() {
             let label = err.to_string();
             let label_span = Span::from_row_column(err.line(), err.column(), string_input);
---
+++ crates/nu-json/src/nu_value.rs
@@ -0,0 +1,40 @@
+use nu_protocol::{ShellError, Span, TryIntoValue};
+
+use crate::Value as JsonValue;
+use nu_protocol::Value as NuValue;
+
+impl TryIntoValue for JsonValue {
+    fn try_into_value(self, span: Span) -> Result<NuValue, ShellError> {
+        Ok(match self {
+            JsonValue::String(s) => NuValue::string(s, span),
+            JsonValue::Bool(b) => NuValue::bool(b, span),
+            JsonValue::F64(f) => NuValue::float(f, span),
+            JsonValue::I64(i) => NuValue::int(i, span),
+            JsonValue::Null => NuValue::nothing(span),
+            JsonValue::U64(u) => match i64::try_from(u) {
+                Ok(i) => NuValue::int(i, span),
+                Err(_) => {
+                    return Err(ShellError::CantConvert {
+                        to_type: "i64 sized integer".into(),
+                        from_type: "value larger than i64".into(),
+                        span,
+                        help: None,
+                    });
+                }
+            },
+            JsonValue::Array(array) => NuValue::list(
+                array
+                    .into_iter()
+                    .map(|x| x.try_into_value(span))
+                    .collect::<Result<_,_>>()?,
+                span,
+            ),
+            JsonValue::Object(k) => NuValue::record(
+                k.into_iter()
+                    .map(|(k, v)| v.try_into_value(span).map(|v| (k, v)))
+                    .collect::<Result<_,_>>()?,
+                span,
+            ),
+        })
+    }
+}
