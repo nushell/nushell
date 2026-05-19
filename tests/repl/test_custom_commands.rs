b555a3ec3a37556ad19a6b9271b2a99351f9c4af
Tyarel8 <98483313+Tyarel8@users.noreply.github.com>
2026-05-19 17:01:12 +0200

    add tests

--- tests/repl/test_custom_commands.rs
+++ tests/repl/test_custom_commands.rs
@@ -305,3 +305,43 @@ fn allow_pass_negative_float() -> TestResult {
     run_test("def spam [val: float] { $val }; spam -1.4", "-1.4")?;
     run_test("def spam [val: float] { $val }; spam -2", "-2.0")
 }
+
+#[test]
+fn glob_bare_word_with_interpolation() -> TestResult {
+    run_test(
+        "def spam [foo: glob] { $foo | describe }; let var = 'val'; spam ~/($var)/test",
+        "glob",
+    )?;
+    run_test(
+        "def spam [--foo: glob] { $foo | describe }; let var = 'val'; spam --foo ~/($var)",
+        "glob",
+    )?;
+    run_test(
+        "def spam [foo: glob] { $foo | describe }; let var = 'val'; spam ($var)/test",
+        "glob",
+    )
+}
+
+#[test]
+fn glob_string_interpolation() -> TestResult {
+    run_test(
+        "def spam [--foo: glob] { $foo | describe }; let var = 'val'; spam --foo $\"/path/($var)\"",
+        "glob",
+    )
+}
+
+#[test]
+fn glob_no_interpolation() -> TestResult {
+    run_test(
+        "def spam [foo: glob] { $foo | describe }; spam *.nu",
+        "glob",
+    )?;
+    run_test(
+        "def spam [--foo: glob] { $foo | describe }; spam --foo '*.nu'",
+        "glob",
+    )?;
+    run_test(
+        "def spam [foo: glob] { $foo | describe }; spam `*.nu`",
+        "glob",
+    )
+}
