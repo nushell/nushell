c27422f7334d0f2943658ba3fac6b033cbcc154d
abdelkadous <development.abdelkadous@gmail.com>
2026-05-20 20:07:09 +0100

    improve 🔥 (format::to::kdl): add the `--serialize` flag and stringify the closures when passed. refactor 🔨 (format::to::kdl): improve error handling. doc 📚 (format::to::kdl): add example for the `--serialize` flag.

--- crates/nu-command/src/formats/to/kdl.rs
+++ crates/nu-command/src/formats/to/kdl.rs
@@ -17,6 +17,11 @@ impl Command for ToKdl {
     fn signature(&self) -> Signature {
         Signature::build("to kdl")
             .input_output_types(vec![(Type::Any, Type::String)])
+            .switch(
+                "serialize",
+                "Serialize nushell types that cannot be deserialized.",
+                Some('s'),
+            )
             .category(Category::Formats)
     }
 
@@ -26,32 +31,50 @@ impl Command for ToKdl {
 
     fn run(
         &self,
-        _: &EngineState,
-        _: &mut Stack,
+        engine_state: &EngineState,
+        stack: &mut Stack,
         call: &Call,
         mut input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let span = input.span().unwrap_or(call.head);
+        let call_span = input.span().unwrap_or(call.head);
         let metadata = input.take_metadata().map(|md| md.with_content_type(None));
 
-        let output_string = match &input.into_value(span)? {
-            Value::Record { val, .. } => {
-                convert_record_into_formatted_kdl_document_recursively(val, span)?.to_string()
-            }
+        // get args
+        let serialize_types = call.has_flag(engine_state, stack, "serialize")?;
+
+        let output_string = match &input.into_value(call_span)? {
+            Value::Record { val, .. } => convert_record_into_formatted_kdl_document_recursively(
+                engine_state,
+                val,
+                serialize_types,
+                call_span,
+            )?
+            .to_string(),
             Value::List { vals, .. } => {
                 let mut kdl_document = KdlDocument::new();
                 let mut node = KdlNode::new("root");
-                convert_list_into_entries_of_kdl_node_recursively(&mut node, vals, span)?;
+                convert_list_into_entries_of_kdl_node_recursively(
+                    engine_state,
+                    &mut node,
+                    vals,
+                    serialize_types,
+                    call_span,
+                )?;
 
                 kdl_document.nodes_mut().push(node);
                 kdl_document.autoformat();
                 kdl_document.to_string()
             }
-            val => convert_nu_value_to_kdl_value(val).to_string(),
+            val => {
+                if val.as_closure().is_ok() && !serialize_types {
+                    return Err(errors::should_use_serialize(call_span, val.span()));
+                }
+                convert_nu_value_to_kdl_value(engine_state, call_span, val)?.to_string()
+            }
         };
 
         Ok(output_string
-            .into_value(span)
+            .into_value(call_span)
             .into_pipeline_data_with_metadata(metadata))
     }
 
@@ -76,13 +99,20 @@ impl Command for ToKdl {
                 example: "[1 2 3] | to kdl",
                 result: Some(Value::test_string("root 1 2 3\n")),
             },
+            Example {
+                description: "Convert nu closure to kdl string",
+                example: "{2: {|| 1 + 1} } | to kdl --serialize",
+                result: Some(Value::test_string("\"2\" \"{|| 1 + 1}\"\n")),
+            },
         ]
     }
 }
 
 fn convert_record_into_formatted_kdl_document_recursively(
+    engine_state: &EngineState,
     record: &Record,
-    span: Span,
+    serialize_types: bool,
+    call_span: Span,
 ) -> Result<KdlDocument, ShellError> {
     let mut kdl_document = KdlDocument::new();
 
@@ -101,21 +131,40 @@ fn convert_record_into_formatted_kdl_document_recursively(
                         KdlIdentifier::parse(k).map_err(|_| ShellError::NushellFailed {
                             msg: "failed to make an identifier for a kdl node".to_owned(),
                         })?;
-                    let value = convert_nu_value_to_kdl_value(v);
+
+                    if v.as_closure().is_ok() && !serialize_types {
+                        return Err(errors::should_use_serialize(call_span, v.span()));
+                    }
+
+                    let value = convert_nu_value_to_kdl_value(engine_state, call_span, v)?;
 
                     let entry = KdlEntry::new_prop(identifier, value);
                     node.push(entry);
                 } else {
                     let _ = node.children_mut().insert(
-                        convert_record_into_formatted_kdl_document_recursively(val, span)?,
+                        convert_record_into_formatted_kdl_document_recursively(
+                            engine_state,
+                            val,
+                            serialize_types,
+                            call_span,
+                        )?,
                     );
                 }
             }
             Value::List { vals, .. } => {
-                convert_list_into_entries_of_kdl_node_recursively(&mut node, vals, span)?;
+                convert_list_into_entries_of_kdl_node_recursively(
+                    engine_state,
+                    &mut node,
+                    vals,
+                    serialize_types,
+                    call_span,
+                )?;
             }
             val => {
-                node.push(convert_nu_value_to_kdl_value(val));
+                if val.as_closure().is_ok() && !serialize_types {
+                    return Err(errors::should_use_serialize(call_span, val.span()));
+                }
+                node.push(convert_nu_value_to_kdl_value(engine_state, call_span, val)?);
             }
         };
 
@@ -128,16 +177,22 @@ fn convert_record_into_formatted_kdl_document_recursively(
 }
 
 fn convert_list_into_entries_of_kdl_node_recursively(
+    engine_state: &EngineState,
     node: &mut KdlNode,
     list: &[Value],
-    span: Span,
+    serialize_types: bool,
+    call_span: Span,
 ) -> Result<(), ShellError> {
     let children = list
         .iter()
         .filter(|val| val.as_record().is_ok_and(|it| it.len() > 1));
 
     if children.clone().count() > 1 {
-        return Err(ShellError::UnsupportedInput { msg: "can't have more than one child for each node in kdl, make sure input don't contain a node has a value of multiple records and more then one of these records has more then one item".to_owned(), input: format!("issue in node: '{}'", node.name().value()), msg_span: span, input_span: node.into_spanned(span).span });
+        return Err(errors::cant_have_more_than_one_child_for_each_node_in_kdl(
+            node,
+            call_span,
+            list.first().expect("").span(),
+        ));
     };
 
     for value in list {
@@ -149,24 +204,42 @@ fn convert_list_into_entries_of_kdl_node_recursively(
                     && val_vec[0].1.as_list().is_err()
                 {
                     let (k, v) = val_vec[0];
-                    let identifier =
-                        KdlIdentifier::parse(k).map_err(|_| ShellError::NushellFailed {
-                            msg: "failed to make an identifier for a kdl node".to_owned(),
-                        })?;
-                    let value = convert_nu_value_to_kdl_value(v);
+                    let identifier = KdlIdentifier::parse(k).map_err(|_| {
+                        errors::nushell_failed("failed to make an identifier for a kdl node")
+                    })?;
+
+                    if v.as_closure().is_ok() && !serialize_types {
+                        return Err(errors::should_use_serialize(call_span, v.span()));
+                    }
+
+                    let value = convert_nu_value_to_kdl_value(engine_state, call_span, v)?;
                     let entry = KdlEntry::new_prop(identifier, value);
                     node.push(entry);
                 } else {
                     let _ = node.children_mut().insert(
-                        convert_record_into_formatted_kdl_document_recursively(val, span)?,
+                        convert_record_into_formatted_kdl_document_recursively(
+                            engine_state,
+                            val,
+                            serialize_types,
+                            call_span,
+                        )?,
                     );
                 }
             }
             Value::List { vals, .. } => {
-                convert_list_into_entries_of_kdl_node_recursively(node, vals, span)?;
+                convert_list_into_entries_of_kdl_node_recursively(
+                    engine_state,
+                    node,
+                    vals,
+                    serialize_types,
+                    call_span,
+                )?;
             }
             val => {
-                node.push(convert_nu_value_to_kdl_value(val));
+                if val.as_closure().is_ok() && !serialize_types {
+                    return Err(errors::should_use_serialize(call_span, val.span()));
+                }
+                node.push(convert_nu_value_to_kdl_value(engine_state, call_span, val)?);
             }
         };
     }
@@ -174,27 +247,58 @@ fn convert_list_into_entries_of_kdl_node_recursively(
     Ok(())
 }
 
-fn convert_nu_value_to_kdl_value(value: &Value) -> KdlValue {
+fn convert_nu_value_to_kdl_value(
+    engine_state: &EngineState,
+    span: Span,
+    value: &Value,
+) -> Result<KdlValue, ShellError> {
     match value {
-        Value::Bool { val, .. } => KdlValue::Bool(*val),
-        Value::Int { val, .. } => KdlValue::Integer(*val as i128),
-        Value::Float { val, .. } => KdlValue::Float(*val),
-        Value::Filesize { val, .. } => KdlValue::String(val.to_string()),
-        Value::Duration { val, .. } => KdlValue::String(val.to_string()),
-        Value::Date { val, .. } => KdlValue::String(val.to_string()),
-        Value::Range { val, .. } => KdlValue::String(val.to_string()),
-        Value::String { val, .. } => KdlValue::String(val.clone()),
-        Value::Glob { val, .. } => KdlValue::String(val.clone()),
-        Value::Closure { val, .. } => KdlValue::String(format!("closure_{}", val.block_id.get())),
-        Value::Nothing { .. } => KdlValue::Null,
-        Value::Error { error, .. } => KdlValue::String(format!("{error:?}")),
-        Value::Binary { val, .. } => KdlValue::String(format!("{val:?}")),
-        Value::CellPath { val, .. } => KdlValue::String(val.to_string()),
-        // If we fail to collapse the custom value, just print <{type_name}> - failure is not
-        // that critical here
-        Value::Custom { val, .. } => KdlValue::String(format!("<{}>", val.type_name())),
+        Value::Bool { val, .. } => Ok(KdlValue::Bool(*val)),
+        Value::Int { val, .. } => Ok(KdlValue::Integer(*val as i128)),
+        Value::Float { val, .. } => Ok(KdlValue::Float(*val)),
+        Value::Filesize { val, .. } => Ok(KdlValue::String(val.to_string())),
+        Value::Duration { val, .. } => Ok(KdlValue::String(val.to_string())),
+        Value::Date { val, .. } => Ok(KdlValue::String(val.to_string())),
+        Value::Range { val, .. } => Ok(KdlValue::String(val.to_string())),
+        Value::String { val, .. } => Ok(KdlValue::String(val.clone())),
+        Value::Glob { val, .. } => Ok(KdlValue::String(val.clone())),
+        Value::Closure { val, .. } => Ok(KdlValue::String(
+            val.coerce_into_string(engine_state, span)?.to_string(),
+        )),
+        Value::Nothing { .. } => Ok(KdlValue::Null),
+        Value::Error { error, .. } => Ok(KdlValue::String(format!("{error:?}"))),
+        Value::Binary { val, .. } => Ok(KdlValue::String(format!("{val:?}"))),
+        Value::CellPath { val, .. } => Ok(KdlValue::String(val.to_string())),
+        Value::Custom { val, .. } => Ok(KdlValue::String(format!("<{}>", val.type_name()))),
         // UNSAFE: i struct the code above to ensure insha'Allah that this is never reached but it's still danger because this project has many people work on it so this can be change.
         _ => unreachable!("can't convert record and list values to kdl"),
+    }
+}
+
+mod errors {
+    use super::*;
+
+    pub fn should_use_serialize(call_span: Span, value_span: Span) -> ShellError {
+        ShellError::UnsupportedInput {
+            msg: "closures are currently not deserializable (use --serialize to serialize as a string)".into(),
+            input: "value originates from here".into(),
+            msg_span: call_span,
+            input_span: value_span,
+        }
+    }
+
+    pub fn cant_have_more_than_one_child_for_each_node_in_kdl(
+        node: &KdlNode,
+        call_span: Span,
+        first_span: Span,
+    ) -> ShellError {
+        ShellError::UnsupportedInput { msg: "can't have more than one child for each node in kdl, make sure input don't contain a node has a value of multiple records and more then one of these records has more then one item".to_owned(), input: format!("issue in node: '{}'", node.name().value()), msg_span: call_span, input_span: first_span }
+    }
+
+    pub fn nushell_failed(msg: &str) -> ShellError {
+        ShellError::NushellFailed {
+            msg: msg.to_owned(),
+        }
     }
 }
 
