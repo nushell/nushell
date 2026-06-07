2c66d71516a50f8a9ed223ba7cbfd77c1af943eb
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-06-07 22:54:00 +0200

    print simple json for validation

--- crates/nu-heavy-utils/src/main.rs
+++ crates/nu-heavy-utils/src/main.rs
@@ -1,4 +1,4 @@
-use nu_protocol::{IntoSpanned, Span};
+use nu_protocol::{FromValue, IntoSpanned, Span};
 
 const FIXTURE: &str = include_str!("../../../tests/fixtures/formats/sample.yaml");
 
@@ -7,5 +7,7 @@ fn main() {
     let options = nu_heavy_utils::yaml::ParseOptions::default();
     let yaml = FIXTURE.into_spanned(span);
     let parsed = nu_heavy_utils::yaml::parse(yaml, span, &options).unwrap();
-    dbg!(parsed);
+    let serializable = nu_json::Value::from_value(parsed).unwrap();
+    let json = serde_json::to_string_pretty(&serializable).unwrap();
+    println!("{json}");
 }
