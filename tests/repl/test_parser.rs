nushell/nightly

commit d271a058df9e08b8cb2a22f5fd3a7400529d59f3

fix(parser): an edge case of parse_unit_value (#17923)

crates/nu-parser/src/parser.rs
@@ -2969,7 +2969,9 @@ pub fn parse_unit_value<'res>(
     transform: fn(String) -> String,
 ) -> Option<ParseUnitResult<'res>> {
     if bytes.len() < 2
-        || !(bytes[0].is_ascii_digit() || (bytes[0] == b'-' && bytes[1].is_ascii_digit()))
+        || !(bytes[0].is_ascii_digit()
+            || bytes[0] == b'.'
+            || (bytes[0] == b'-' && bytes[1].is_ascii_digit()))
     {
         return None;
     }

tests/repl/test_parser.rs
@@ -812,6 +812,11 @@ fn extern_errors_with_no_space_between_params_and_name_2() -> TestResult {
 }
 
 #[test]
+fn duration_with_float_number() -> TestResult {
+    run_test(".6min", "36sec")
+}
+
+#[test]
 fn duration_with_underscores_1() -> TestResult {
     run_test("420_min", "7hr")
 }

nushell/nushell

commit e70202b0223bafd8c9ea08f56e84e6409c8ac4b8

fix(parser): a regression of parse_unit_value where `[.foons]` not return early (#18363)
## Description

Fixes a regression introduced by #17923

## User-facing changes (Release notes)

Fixed a regression where lists like `[.foons]` will cause parser error.

crates/nu-parser/src/parser.rs
@@ -3230,7 +3230,7 @@ pub fn parse_unit_value<'res>(
 ) -> Option<ParseUnitResult<'res>> {
     if bytes.len() < 2
         || !(bytes[0].is_ascii_digit()
-            || bytes[0] == b'.'
+            || (bytes[0] == b'.' && bytes[1].is_ascii_digit())
             || (bytes[0] == b'-' && bytes[1].is_ascii_digit()))
     {
         return None;

tests/repl/test_parser.rs
@@ -340,6 +340,11 @@ fn list_quotes_with_equals() -> TestResult {
 }
 
 #[test]
+fn list_raw_string_unit_value_like() -> TestResult {
+    run_test("[.foons] | get 0", ".foons")
+}
+
+#[test]
 fn record_quotes_with_equals() -> TestResult {
     run_test(r#"{"a=":b} | get a="#, "b")?;
     run_test(r#"{"=a":b} | get =a"#, "b")?;