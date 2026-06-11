bc94fc372c070e881f08af3d371eeccb1ffa2005
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-06-11 09:22:46 +0200

    added basic serialization

--- crates/nu-heavy-utils/src/main.rs
+++ crates/nu-heavy-utils/src/main.rs
@@ -1,4 +1,4 @@
-use nu_protocol::{FromValue, IntoSpanned, Span};
+use nu_protocol::{IntoSpanned, Span};
 
 const FIXTURE: &str = include_str!("../../../tests/fixtures/formats/sample.yaml");
 
@@ -7,7 +7,9 @@ fn main() {
     let options = nu_heavy_utils::yaml::ParseOptions::default();
     let yaml = FIXTURE.into_spanned(span);
     let parsed = nu_heavy_utils::yaml::parse(yaml, span, &options).unwrap();
-    let serializable = nu_json::Value::from_value(parsed).unwrap();
-    let json = serde_json::to_string_pretty(&serializable).unwrap();
-    println!("{json}");
+    let parsed = dbg!(parsed);
+
+    let options = nu_heavy_utils::yaml::SerializeOptions::default();
+    let serialized = nu_heavy_utils::yaml::serialize(&parsed, span, &options).unwrap();
+    println!("{serialized}");
 }
--- crates/nu-heavy-utils/src/yaml/serialize.rs
+++ crates/nu-heavy-utils/src/yaml/serialize.rs
@@ -1,6 +1,8 @@
 use crate::yaml::Spec;
 use derive_setters::Setters;
-use nu_protocol::{ShellError, Span, Spanned, Value};
+use nu_protocol::{ShellError, Span, Value};
+use serde::{Serialize, ser::SerializeMap};
+use serde_saphyr::{FlowMap, FlowSeq, FoldStr, LitStr};
 
 #[non_exhaustive]
 #[derive(Debug, Clone, Default, Setters)]
@@ -8,6 +10,61 @@ pub struct SerializeOptions {
     spec: Spec,
 }
 
-pub fn serialize(value: &Value, options: &SerializeOptions) -> Result<String, ShellError> {
-    todo!()
+pub fn serialize(value: &Value, span: Span, options: &SerializeOptions) -> Result<String, ShellError> {
+    let value = YamlValue::from(value);
+    serde_saphyr::to_string(&value).map_err(|_err| todo!())
+}
+
+#[derive(Serialize)]
+#[serde(untagged)]
+enum YamlValue<'v> {
+    Bool(bool),
+    Int(i64),
+    Float(f64),
+    Str(&'v str),
+    FoldStr(FoldStr<'v>),
+    LitStr(LitStr<'v>),
+    Map(YamlMap<'v>),
+    FlowMap(FlowMap<Vec<(&'v str, YamlValue<'v>)>>),
+    Seq(Vec<YamlValue<'v>>),
+    FlowSeq(FlowSeq<Vec<YamlValue<'v>>>), 
+    Null,   
+}
+
+struct YamlMap<'v>(Vec<(&'v str, YamlValue<'v>)>);
+
+impl Serialize for YamlMap<'_> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer {
+        let mut map = serializer.serialize_map(self.0.len().into())?;
+        for (key, value) in self.0.iter() {
+            map.serialize_entry(key, value)?;
+        }
+        map.end()
+    }
+}
+
+impl<'v> From<&'v Value> for YamlValue<'v> {
+    fn from(value: &'v Value) -> Self {
+        match value {
+            Value::Bool { val, .. } => YamlValue::Bool(*val),
+            Value::Int { val, .. } => YamlValue::Int(*val),
+            Value::Float { val, .. } => YamlValue::Float(*val),
+            Value::String { val, .. } => YamlValue::Str(val.as_str()),
+            Value::Glob { val, .. } => todo!(),
+            Value::Filesize { val, .. } => todo!(),
+            Value::Duration { val, .. } => todo!(),
+            Value::Date { val, .. } => todo!(),
+            Value::Range { val, .. } => todo!(),
+            Value::Record { val, .. } => YamlValue::Map(YamlMap(Vec::from_iter(val.iter().map(|(k, v)| (k.as_str(), v.into()))))),
+            Value::List { vals, .. } => YamlValue::Seq(Vec::from_iter(vals.iter().map(|v| v.into()))),
+            Value::Closure { val, .. } => todo!(),
+            Value::Error { error, .. } => todo!(),
+            Value::Binary { val, .. } => todo!(),
+            Value::CellPath { val, .. } => todo!(),
+            Value::Custom { val, .. } => todo!(),
+            Value::Nothing { .. } => YamlValue::Null,
+        }
+    }
 }
