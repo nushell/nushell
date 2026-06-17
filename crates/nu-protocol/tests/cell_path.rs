44fb48ccfa45db37cadeb27830019c9cf72f9c4b
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-06-17 18:04:00 +0200

    add a lot of test cases

--- crates/nu-protocol/tests/cell_path.rs
+++ crates/nu-protocol/tests/cell_path.rs
@@ -1,5 +1,5 @@
 use nu_protocol::ast::CellPath;
-use nu_test_support::prelude::*;
+use nu_test_support::{prelude::*, test_cell_path};
 use rstest::rstest;
 
 #[rstest]
@@ -8,6 +8,9 @@ use rstest::rstest;
 #[case("`two words`!?.name")]
 #[case("`two words`.name")]
 #[case("$.")]
+#[case("$.")]
+#[case("$.34")]
+#[case("$.abc")]
 #[case("$.abc")]
 #[case("0!?")]
 #[case("0!")]
@@ -34,6 +37,40 @@ fn engine_eq_from_str(#[case] input: &str) -> Result {
     let via_from_str: CellPath = input.parse().expect("cell path parses");
     assert_eq!(via_engine, via_from_str);
     Ok(())
+}
+
+#[rstest]
+#[case(test_cell_path!("double quoted"!?.name))]
+#[case(test_cell_path!("double quoted".name))]
+#[case(test_cell_path!("quoted member"?!.name))]
+#[case(test_cell_path!("quoted member".name))]
+#[case(test_cell_path!("two words"!?.name))]
+#[case(test_cell_path!("two words".name))]
+#[case(test_cell_path!())]
+#[case(test_cell_path!())]
+#[case(test_cell_path!(0!?))]
+#[case(test_cell_path!(0!))]
+#[case(test_cell_path!(0?!))]
+#[case(test_cell_path!(0?))]
+#[case(test_cell_path!(0.abc))]
+#[case(test_cell_path!(0))]
+#[case(test_cell_path!(34))]
+#[case(test_cell_path!(abc!?.def))]
+#[case(test_cell_path!(abc!.def))]
+#[case(test_cell_path!(abc?!.def))]
+#[case(test_cell_path!(abc.0?.def?))]
+#[case(test_cell_path!(abc.0))]
+#[case(test_cell_path!(abc))]
+#[case(test_cell_path!(abc))]
+#[case(test_cell_path!(abc))]
+#[case(test_cell_path!(items.0 .1))]
+#[case(test_cell_path!(items.0b1010))]
+#[case(test_cell_path!(items.0o12))]
+#[case(test_cell_path!(items.1_000))]
+fn roundtrip(#[case] input: CellPath) {
+    let serialized = serde_json::to_string(&input).unwrap();
+    let deserialized = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(input, deserialized);
 }
 
 #[rstest]
