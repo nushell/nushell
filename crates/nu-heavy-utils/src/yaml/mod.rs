31b710fa5c9e0156d3a7c60de329e18b0232cef5
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-06-25 19:44:26 +0200

    add doc to KnownTag type

--- crates/nu-heavy-utils/src/yaml/mod.rs
+++ crates/nu-heavy-utils/src/yaml/mod.rs
@@ -8,97 +8,190 @@ pub use parse::*;
 mod serialize;
 pub use serialize::*;
 
+/// YAML spec version to comply to.
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, Default, FromValue, PartialEq, Eq)]
 pub enum Spec {
+    /// YAML spec v1.1
+    ///
+    /// This is a more chaotic version for the YAML specification.
+    /// But it is more widely used and many configuration files depend on that version, so this is
+    /// available to use.
+    ///
+    /// <https://yaml.org/spec/1.1
     #[nu_value(rename = "1.1")]
     V1_1,
 
+    /// YAML spec v1.2
+    ///
+    /// This is the more reasonable and newer specification for YAML.
+    /// By default we comply to this version.
+    ///
+    /// <https://yaml.org/spec/1.2>
     #[default]
     #[nu_value(rename = "1.2")]
     V1_2,
 }
 
+/// Known supported YAML tags.
+///
+/// This includes the YAML 1.1 language-independent types from
+/// <https://yaml.org/type/>, plus Nushell-specific extension tags that map to
+/// selected [`nu_protocol::Value`] variants.
 #[derive(strum::Display, strum::EnumString, Debug, Clone, Copy)]
 #[strum(parse_err_ty = UnknownTagError, parse_err_fn = UnknownTagError::new)]
 enum KnownTag {
     // YAML known tags
+
+    /// Unordered mapping language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/map.html>
     #[strum(to_string = "!!map", serialize = "tag:yaml.org,2002:map")]
     Map,
 
+    /// Sequence language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/seq.html>
     #[strum(to_string = "!!seq", serialize = "tag:yaml.org,2002:seq")]
     Seq,
 
+    /// String language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/str.html>
     #[strum(to_string = "!!str", serialize = "tag:yaml.org,2002:str")]
     Str,
 
+    /// Null language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/null.html>
     #[strum(to_string = "!!null", serialize = "tag:yaml.org,2002:null")]
     Null,
 
+    /// Boolean language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/bool.html>
     #[strum(to_string = "!!bool", serialize = "tag:yaml.org,2002:bool")]
     Bool,
 
+    /// Integer language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/int.html>
     #[strum(to_string = "!!int", serialize = "tag:yaml.org,2002:int")]
     Int,
 
+    /// Floating-point language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/float.html>
     #[strum(to_string = "!!float", serialize = "tag:yaml.org,2002:float")]
     Float,
 
+    /// Binary data language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/binary.html>
     #[strum(to_string = "!!binary", serialize = "tag:yaml.org,2002:binary")]
     Binary,
 
+    /// Ordered mapping language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/omap.html>
     #[strum(to_string = "!!omap", serialize = "tag:yaml.org,2002:omap")]
     OMap,
 
+    /// Pairs language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/pairs.html>
     #[strum(to_string = "!!pairs", serialize = "tag:yaml.org,2002:pairs")]
     Pairs,
 
+    /// Set language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/set.html>
     #[strum(to_string = "!!set", serialize = "tag:yaml.org,2002:set")]
     Set,
 
+    /// Merge key language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/merge.html>
     #[strum(to_string = "!!merge", serialize = "tag:yaml.org,2002:merge")]
     Merge,
 
+    /// Timestamp language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/timestamp.html>
     #[strum(to_string = "!!timestamp", serialize = "tag:yaml.org,2002:timestamp")]
     Timestamp,
 
+    /// Value key language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/value.html>
     #[strum(to_string = "!!value", serialize = "tag:yaml.org,2002:value")]
     Value, // not really relevant, just for completeness
 
+    /// YAML encoding keys language-independent type for YAML, version 1.1
+    ///
+    /// Reference: <https://yaml.org/type/yaml.html>
     #[strum(to_string = "!!yaml", serialize = "tag:yaml.org,2002:yaml")]
     Yaml,
 
     // Nushell custom tags
+
+    /// Nushell glob value.
+    ///
+    /// Reference: [`nu_protocol::Value::Glob`]
     #[strum(to_string = "!glob", serialize = "tag:nushell.sh,2026:glob")]
     Glob,
 
+    /// Nushell filesize value.
+    ///
+    /// Reference: [`nu_protocol::Value::Filesize`]
     #[strum(to_string = "!filesize", serialize = "tag:nushell.sh,2026:filesize")]
     Filesize,
 
+    /// Nushell duration value.
+    ///
+    /// Reference: [`nu_protocol::Value::Duration`]
     #[strum(to_string = "!duration", serialize = "tag:nushell.sh,2026:duration")]
     Duration,
 
+    /// Nushell date value.
+    ///
+    /// Reference: [`nu_protocol::Value::Date`]
     // TODO: handle date via timestamp tag instead
     #[strum(to_string = "!date", serialize = "tag:nushell.sh,2026:date")]
     Date,
 
+    /// Nushell range value.
+    ///
+    /// Reference: [`nu_protocol::Value::Range`]
     #[strum(to_string = "!range", serialize = "tag:nushell.sh,2026:range")]
     Range,
 
+    /// Nushell closure value.
+    ///
+    /// Reference: [`nu_protocol::Value::Closure`]
     #[strum(to_string = "!closure", serialize = "tag:nushell.sh,2026:closure")]
     Closure,
 
+    /// Nushell error value.
+    ///
+    /// Reference: [`nu_protocol::Value::Error`]
     #[strum(to_string = "!error", serialize = "tag:nushell.sh,2026:error")]
     Error,
 
+    /// Nushell cell path value.
+    ///
+    /// Reference: [`nu_protocol::Value::CellPath`]
     #[strum(to_string = "!cell-path", serialize = "tag:nushell.sh,2026:cell-path")]
     CellPath,
 }
 
+
 impl KnownTag {
+    /// Nushell custom tag prefix.
     pub const NUSHELL_PREFIX: &str = "tag:nushell.sh,2026:";
 }
 
+/// Error if YAML tag is unknown.
 struct UnknownTagError(String);
 
 impl UnknownTagError {
