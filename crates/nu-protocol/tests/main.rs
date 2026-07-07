55efab4854a597a6bec9fca3df750b430bb1f587
Tim 'Piepmatz'  Hesse <git+github@cptpiepmatz.de>
2026-07-07 08:29:43 +0200

    remove showcase

--- crates/nu-protocol/tests/main.rs
+++ crates/nu-protocol/tests/main.rs
@@ -12,15 +12,3 @@ mod test_value;
 #[macro_use]
 extern crate nu_test_support;
 use nu_test_support::harness::main;
-
-use nu_test_support::prelude::*;
-use rstest::*;
-
-#[rstest]
-#[case::no_need()]
-#[deps(NU)]
-#[case::needs_nu()]
-#[deps(NU_PLUGIN_EXAMPLE)]
-#[case::needs_plugin()]
-#[nu_test_support::test]
-fn showcase() {}
