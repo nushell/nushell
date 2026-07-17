ef72147a31c7c2592cf288b254c96ca101ec0276
Bahex <17417311+Bahex@users.noreply.github.com>
2026-07-17 09:04:28 +0300

    Revert "feat(date): add new commands `date floor` and `date ceil` (#18620)"
    This reverts commit 57e82097838ddf3fbf8a0c56b8ff84f37e2aaab6.

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -314,8 +314,6 @@ pub fn add_shell_command_context(mut engine_state: EngineState) -> EngineState {
         // Date
         bind_command! {
             Date,
-            DateCeil,
-            DateFloor,
             DateFromHuman,
             DateHumanize,
             DateListTimezones,
