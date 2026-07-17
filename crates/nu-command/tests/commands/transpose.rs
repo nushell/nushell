9a9ee218cd4e33e05c05b87ea82c1cf1538a943e
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2026-07-17 09:10:11 +0900

    Migrate transpose tests to the new harness

--- crates/nu-command/tests/commands/transpose.rs
+++ crates/nu-command/tests/commands/transpose.rs
@@ -1,42 +1,58 @@
-use nu_test_support::nu;
+use nu_protocol::test_record;
+use nu_test_support::prelude::*;
 
 #[test]
-fn row() {
-    let actual = nu!("[[key value]; [foo 1] [foo 2]] | transpose -r | debug");
-
-    assert!(actual.out.contains("foo: 1"));
+fn row() -> Result {
+    test()
+        .run("[[key value]; [foo 1] [foo 2]] | transpose -r")
+        .expect_value_eq([test_record! { "foo" => 1 }])
 }
 
 #[test]
-fn row_but_last() {
-    let actual = nu!("[[key value]; [foo 1] [foo 2]] | transpose -r -l | debug");
-
-    assert!(actual.out.contains("foo: 2"));
+fn row_but_last() -> Result {
+    test()
+        .run("[[key value]; [foo 1] [foo 2]] | transpose -r -l")
+        .expect_value_eq([test_record! { "foo" => 2 }])
 }
 
 #[test]
-fn row_but_all() {
-    let actual = nu!("[[key value]; [foo 1] [foo 2]] | transpose -r -a | debug");
-
-    assert!(actual.out.contains("foo: [1, 2]"));
+fn row_but_all() -> Result {
+    test()
+        .run("[[key value]; [foo 1] [foo 2]] | transpose -r -a")
+        .expect_value_eq([test_record! { "foo" => [1, 2] }])
 }
 
 #[test]
-fn throw_inner_error() {
+fn throw_inner_error() -> Result {
     let error_msg = "This message should show up";
     let error = format!("(error make {{ msg: \"{error_msg}\" }})");
-    let actual = nu!(format!(
-        "[[key value]; [foo 1] [foo 2] [{} 3]] | transpose",
-        error
-    ));
+    let err = test()
+        .run(format!(
+            "[[key value]; [foo 1] [foo 2] [{} 3]] | transpose",
+            error
+        ))
+        .expect_shell_error()?;
 
-    assert!(actual.err.contains(error.as_str()));
+    assert_eq!(err.into_labeled()?.msg, error_msg);
+    Ok(())
 }
 
 #[test]
-fn rejects_non_table_stream_input() {
-    let actual = nu!("[1 2 3] | each { |it| ($it * 2) } | transpose | to nuon");
+fn rejects_non_table_stream_input() -> Result {
+    let err = test()
+        .run("[1 2 3] | each { |it| ($it * 2) } | transpose | to nuon")
+        .expect_shell_error()?;
 
-    assert!(actual.out.is_empty());
-    assert!(actual.err.contains("only table"));
+    match err {
+        ShellError::OnlySupportsThisInputType {
+            exp_input_type,
+            wrong_type,
+            ..
+        } => {
+            assert_eq!(exp_input_type, "table or record");
+            assert_eq!(wrong_type, "list<any>");
+            Ok(())
+        }
+        err => Err(err.into()),
+    }
 }
