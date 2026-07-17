94145241b044d40c8e25982f84aaeb460cea980c
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-07-18 01:55:43 +0200

    inherit PATHEXT on windows in the tester

--- crates/nu-test-support/src/tester/mod.rs
+++ crates/nu-test-support/src/tester/mod.rs
@@ -59,6 +59,12 @@ static INITIAL_ENGINE_STATES: KeyedLazyLock<GroupKey, EngineState> = KeyedLazyLo
     .into_iter()
     .for_each(|(key, val)| engine_state.add_env_var(key.into(), val));
 
+    // Should this be inherited or do we want tighter testing?
+    #[cfg(windows)]
+    if let Ok(path_ext) = env::var("PATHEXT") {
+        engine_state.add_env_var("PATHEXT".into(), Value::test_string(path_ext));
+    }
+
     nu_std::load_standard_library(&mut engine_state).expect("could not load standard library");
 
     engine_state
