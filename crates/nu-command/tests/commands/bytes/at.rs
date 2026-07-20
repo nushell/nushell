f3e64741e83840b0d75484282d578c45229c4e69
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-07-20 16:07:53 +0200

    update `bytes at` tests

--- crates/nu-command/tests/commands/bytes/at.rs
+++ crates/nu-command/tests/commands/bytes/at.rs
@@ -2,9 +2,9 @@ use nu_protocol::{IntoPipelineData, Span};
 use nu_test_support::prelude::*;
 
 #[test]
-#[deps(NU)]
+#[deps(TESTBIN_IECHO)]
 pub fn returns_error_for_relative_range_on_infinite_stream() -> Result {
-    let code = "nu --testbin iecho 3 | bytes at ..-3";
+    let code = "iecho 3 | bytes at ..-3";
     let err = test().run(code).expect_shell_error()?;
     assert!(matches!(
         err,
@@ -14,16 +14,16 @@ pub fn returns_error_for_relative_range_on_infinite_stream() -> Result {
 }
 
 #[test]
-#[deps(NU)]
+#[deps(TESTBIN_IECHO)]
 pub fn returns_bytes_for_fixed_range_on_infinite_stream_including_end() -> Result {
-    let code = "nu --testbin iecho 3 | bytes at ..10 | decode";
+    let code = "iecho 3 | bytes at ..10 | decode";
     test().run(code).expect_value_eq("3\n3\n3\n3\n3\n3")
 }
 
 #[test]
-#[deps(NU)]
+#[deps(TESTBIN_IECHO)]
 pub fn returns_bytes_for_fixed_range_on_infinite_stream_excluding_end() -> Result {
-    let code = "nu --testbin iecho 3 | bytes at ..<9 | decode";
+    let code = "iecho 3 | bytes at ..<9 | decode";
     test().run(code).expect_value_eq("3\n3\n3\n3\n3")
 }
 
