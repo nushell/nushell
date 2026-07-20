f2bda85c3211a09ef896fad30a37a28d8e620861
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-07-20 13:43:28 +0200

    update `loop` tests

--- crates/nu-command/tests/commands/loop_.rs
+++ crates/nu-command/tests/commands/loop_.rs
@@ -1,52 +1,60 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn loop_doesnt_auto_print_in_each_iteration() {
-    let actual = nu!("
-        mut total = 0;
+fn loop_doesnt_auto_print_in_each_iteration() -> Result {
+    let code = "
+        mut total = 0
         loop {
             if $total == 3 {
-                break;
+                break
             } else {
-                $total += 1;
+                $total += 1
             }
             1
-        }");
-    // Make sure we don't see any of these values in the output
-    // As we do not auto-print loops anymore
-    assert!(!actual.out.contains('1'));
-}
-
-#[test]
-fn loop_break_on_external_failed() {
-    let actual = nu!("
-        mut total = 0;
-        loop {
-            if $total == 3 {
-                break;
-            } else {
-                $total += 1;
-            }
-            print 1;
-            nu --testbin fail;
-        }");
-    // Note: nu! macro auto replace "\n" and "\r\n" with ""
-    // so our output will be `1`.
-    assert_eq!(actual.out, "1");
-}
-
-#[test]
-fn failed_loop_should_break_running() {
-    let actual = nu!("
-        mut total = 0;
-        loop {
-            if $total == 3 {
-                break;
-            } else {
-                $total += 1;
-            }
-            nu --testbin fail;
         }
-        print 3");
-    assert!(!actual.out.contains('3'));
+    ";
+
+    test().run(code).expect_value_eq(())
+}
+
+#[test]
+#[deps(TESTBIN_FAIL)]
+fn loop_break_on_external_failed() -> Result {
+    let code = "
+        mut total = 0
+        loop {
+            if $total == 3 {
+                break
+            } else {
+                $total += 1
+            }
+            print 1
+            fail
+        }
+    ";
+
+    test()
+        .run(code)
+        .expect_error_code_eq("nu::shell::external_command")
+}
+
+#[test]
+#[deps(TESTBIN_FAIL)]
+fn failed_loop_should_break_running() -> Result {
+    let code = "
+        mut total = 0
+        loop {
+            if $total == 3 {
+                break
+            } else {
+                $total += 1
+            }
+            fail
+        }
+        3
+    ";
+
+    test()
+        .run(code)
+        .expect_error_code_eq("nu::shell::non_zero_exit_code")
 }
