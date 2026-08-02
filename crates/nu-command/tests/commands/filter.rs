5da31fd4403404d791f6d844f5be51b05d301df1
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-08-02 23:03:50 +0200

    refactor `filter` tests

--- crates/nu-command/tests/commands/filter.rs
+++ crates/nu-command/tests/commands/filter.rs
@@ -1,16 +1,20 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn filter_with_return_in_closure() {
-    let actual = nu!("
-    1..10 | filter { |it|
-        if $it mod 2 == 0 {
-            return true
-        };
-        return false;
-    } | to nuon
-    ");
+#[deps(NU)]
+fn filter_with_return_in_closure() -> Result {
+    let result: CompleteResult = test().run(
+        "nu -n -c '
+            1..10 | filter { |it|
+                if $it mod 2 == 0 {
+                    return true
+                };
+                return false;
+            } | to nuon
+        ' | complete",
+    )?;
 
-    assert_eq!(actual.out, "[2, 4, 6, 8, 10]");
-    assert!(actual.err.contains("deprecated"));
+    assert_eq!(result.stdout.trim(), "[2, 4, 6, 8, 10]");
+    assert_contains("deprecated", result.stderr);
+    Ok(())
 }
