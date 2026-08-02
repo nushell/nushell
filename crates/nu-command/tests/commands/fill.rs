8e8b1df9e27b2576b700ed333b8dd280d480bff2
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-08-02 23:03:36 +0200

    refactor `fill` tests

--- crates/nu-command/tests/commands/fill.rs
+++ crates/nu-command/tests/commands/fill.rs
@@ -1,21 +1,20 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn string_fill_plain() {
-    let actual = nu!(r#""abc" | fill --alignment center --character "+" --width 5"#);
-
-    assert_eq!(actual.out, "+abc+");
+fn string_fill_plain() -> Result {
+    test()
+        .run(r#""abc" | fill --alignment center --character "+" --width 5"#)
+        .expect_value_eq("+abc+")
 }
 
 #[test]
-fn string_fill_fancy() {
-    let actual = nu!(r#"
-        $"(ansi red)a(ansi green)\u{65}\u{308}(ansi cyan)c(ansi reset)" 
+fn string_fill_fancy() -> Result {
+    let code = r#"
+        $"(ansi red)a(ansi green)\u{65}\u{308}(ansi cyan)c(ansi reset)"
         | fill --alignment center --character "+" --width 5
-        "#);
+    "#;
 
-    assert_eq!(
-        actual.out,
-        "+\u{1b}[31ma\u{1b}[32me\u{308}\u{1b}[36mc\u{1b}[0m+"
-    );
+    test()
+        .run(code)
+        .expect_value_eq("+\u{1b}[31ma\u{1b}[32me\u{308}\u{1b}[36mc\u{1b}[0m+")
 }
