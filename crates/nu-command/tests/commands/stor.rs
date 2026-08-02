1bdfa333f9504c40a0105cbc44a814e4e0836b41
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-08-02 22:54:12 +0200

    refactor `stor` tests

--- crates/nu-command/tests/commands/stor.rs
+++ crates/nu-command/tests/commands/stor.rs
@@ -1,33 +1,37 @@
-use nu_test_support::nu;
+use nu_test_support::prelude::*;
 
 #[test]
-fn stor_insert() {
-    let actual = nu!(r#"
-        stor create --table-name test_table --columns { id: int, value: str };
-        stor insert -t test_table --data-record {
-            id: 1
-            value: "'Initial value'"
-        };
-        stor open | query db 'select value from test_table' | get 0.value
-    "#);
-
-    assert_eq!(actual.out, "'Initial value'");
+fn stor_insert() -> Result {
+    test()
+        .run(
+            r#"
+                stor create --table-name stor_insert_table --columns { id: int, value: str };
+                stor insert -t stor_insert_table --data-record {
+                    id: 1
+                    value: "'Initial value'"
+                };
+                stor open | query db 'select value from stor_insert_table' | get 0.value
+            "#,
+        )
+        .expect_value_eq("'Initial value'")
 }
 
 #[test]
-fn stor_update_with_quote() {
-    let actual = nu!(r#"
-        stor create --table-name test_table --columns { id: int, value: str };
-        stor insert -t test_table --data-record {
-            id: 1
-            value: "'Initial value'"
-        };
-        stor update -t test_table --where-clause 'id = 1' --update-record {
-            id: 1
-            value: "This didn't work, but should now."
-        };
-        stor open | query db 'select value from test_table' | get 0.value
-    "#);
-
-    assert_eq!(actual.out, "This didn't work, but should now.");
+fn stor_update_with_quote() -> Result {
+    test()
+        .run(
+            r#"
+                stor create --table-name stor_update_table --columns { id: int, value: str };
+                stor insert -t stor_update_table --data-record {
+                    id: 1
+                    value: "'Initial value'"
+                };
+                stor update -t stor_update_table --where-clause 'id = 1' --update-record {
+                    id: 1
+                    value: "This didn't work, but should now."
+                };
+                stor open | query db 'select value from stor_update_table' | get 0.value
+            "#,
+        )
+        .expect_value_eq("This didn't work, but should now.")
 }
