2e32c646b5f46b9b5edb7d261f599899c0382818
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-08-03 00:07:01 +0200

    refactor `mktemp` tests

--- crates/nu-command/tests/commands/mktemp.rs
+++ crates/nu-command/tests/commands/mktemp.rs
@@ -1,57 +1,49 @@
 use nu_path::AbsolutePath;
-use nu_test_support::nu;
 use nu_test_support::playground::Playground;
+use nu_test_support::prelude::*;
 
 #[test]
-fn creates_temp_file() {
+fn creates_temp_file() -> Result {
     Playground::setup("mktemp_test_1", |dirs, _| {
-        let output = nu!(
-            cwd: dirs.test(),
-            "mktemp"
-        );
-        let loc = AbsolutePath::try_new(&output.out).unwrap();
-        println!("{loc:?}");
+        let output: String = test().cwd(dirs.test()).run("mktemp")?;
+        let loc = AbsolutePath::try_new(&output).unwrap();
         assert!(loc.exists());
+        Ok(())
     })
 }
 
 #[test]
-fn creates_temp_file_with_suffix() {
+fn creates_temp_file_with_suffix() -> Result {
     Playground::setup("mktemp_test_2", |dirs, _| {
-        let output = nu!(
-            cwd: dirs.test(),
-            "mktemp --suffix .txt tempfileXXX"
-        );
-        let loc = AbsolutePath::try_new(&output.out).unwrap();
+        let output: String = test()
+            .cwd(dirs.test())
+            .run("mktemp --suffix .txt tempfileXXX")?;
+        let loc = AbsolutePath::try_new(&output).unwrap();
         assert!(loc.exists());
         assert!(loc.is_file());
-        assert!(output.out.ends_with(".txt"));
-        assert!(output.out.starts_with(dirs.test().to_str().unwrap()));
+        assert!(output.ends_with(".txt"));
+        assert!(output.starts_with(dirs.test().to_str().unwrap()));
+        Ok(())
     })
 }
 
 #[test]
-fn creates_temp_directory() {
+fn creates_temp_directory() -> Result {
     Playground::setup("mktemp_test_3", |dirs, _| {
-        let output = nu!(
-            cwd: dirs.test(),
-            "mktemp -d"
-        );
-        let loc = AbsolutePath::try_new(&output.out).unwrap();
+        let output: String = test().cwd(dirs.test()).run("mktemp -d")?;
+        let loc = AbsolutePath::try_new(&output).unwrap();
         assert!(loc.exists());
         assert!(loc.is_dir());
+        Ok(())
     })
 }
 
 #[test]
-fn doesnt_create_temp_file() {
+fn doesnt_create_temp_file() -> Result {
     Playground::setup("mktemp_test_1", |dirs, _| {
-        let output = nu!(
-            cwd: dirs.test(),
-            "mktemp --dry"
-        );
-        let loc = AbsolutePath::try_new(&output.out).unwrap();
-        println!("{loc:?}");
+        let output: String = test().cwd(dirs.test()).run("mktemp --dry")?;
+        let loc = AbsolutePath::try_new(&output).unwrap();
         assert!(!loc.exists());
+        Ok(())
     })
 }
