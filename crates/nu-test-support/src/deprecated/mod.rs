f2a4623ebed56d7b9ff5fe893fd8306c49f4de3c
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-08-03 00:14:00 +0200

    remove `nu!`

--- crates/nu-test-support/src/deprecated/mod.rs
+++ crates/nu-test-support/src/deprecated/mod.rs
@@ -4,30 +4,4 @@
 //! However they are not yet marked as `#[deprecated]` to avoid a massive amount of warnings during
 //! compilation.
 
-use std::process::ExitStatus;
-
 pub mod locale_override;
-pub mod macros;
-
-#[derive(Debug)]
-pub struct Outcome {
-    pub out: String,
-    pub err: String,
-    pub status: ExitStatus,
-}
-
-impl Outcome {
-    pub fn new(out: String, err: String, status: ExitStatus) -> Outcome {
-        Outcome { out, err, status }
-    }
-}
-
-pub fn shell_os_paths() -> Vec<std::path::PathBuf> {
-    let mut original_paths = vec![];
-
-    if let Some(paths) = std::env::var_os(nu_utils::consts::NATIVE_PATH_ENV_VAR) {
-        original_paths = std::env::split_paths(&paths).collect::<Vec<_>>();
-    }
-
-    original_paths
-}
