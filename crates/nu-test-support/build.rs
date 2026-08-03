4ceee9ac99da147e9fee2c73a42498cd43e50d67
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-08-03 11:15:54 +0200

    also detect target in `nu-test-support` build script

--- crates/nu-test-support/build.rs
+++ crates/nu-test-support/build.rs
@@ -1,13 +1,27 @@
-use std::{env, path::PathBuf, sync::LazyLock};
+use std::{env, path::PathBuf, process::Command, sync::LazyLock};
 
 fn main() {
-    extract_build_profile();
+    extract_build_profile_and_target();
 }
 
 static OUT_DIR: LazyLock<PathBuf> =
     LazyLock::new(|| PathBuf::from(env::var("OUT_DIR").expect("set by cargo")));
 
-fn extract_build_profile() {
+static TARGET_LIST: LazyLock<Vec<String>> = LazyLock::new(|| {
+    let result = Command::new("rustc")
+        .args(["--print", "target-list"])
+        .output()
+        .expect("calling rustc should work");
+    assert!(result.status.success());
+    String::from_utf8(result.stdout)
+        .expect("valid utf8")
+        .trim()
+        .lines()
+        .map(ToString::to_string)
+        .collect()
+});
+
+fn extract_build_profile_and_target() {
     let mut components = OUT_DIR.components().rev();
     let _out = components.next().expect("has `out` dir");
     let _crate = components.next().expect("has dir for crate");
@@ -15,6 +29,11 @@ fn extract_build_profile() {
     let profile = components.next().expect("has profile dir");
     let profile = profile.as_os_str().to_string_lossy();
     println!("cargo::rustc-env=BUILD_PROFILE={profile}");
-}
 
-// TODO: maybe figure out target too
+    if let Some(maybe_target) = components.next() {
+        let maybe_target = maybe_target.as_os_str().to_string_lossy();
+        if TARGET_LIST.iter().any(|target| target.as_str() == maybe_target.as_ref()) {
+            println!("cargo::rustc-env=BUILD_TARGET={maybe_target}");
+        }
+    }
+}
