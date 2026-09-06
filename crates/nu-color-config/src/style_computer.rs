04fbf0b9fb3b3b3ca6138d7779781efb33073fe3
Samuel Willows <samueltwillows@hotmail.com>
2026-09-05 23:51:07 -0700

    update(style_computer.rs): fixed unessesary string allocations

--- crates/nu-color-config/src/style_computer.rs
+++ crates/nu-color-config/src/style_computer.rs
@@ -104,7 +104,7 @@ impl<'a> StyleComputer<'a> {
     // Used only by the `table` command.
     pub fn style_primitive(&self, value: &Value) -> TextStyle {
         use Alignment::*;
-        let s = self.compute(&value.get_type().get_non_specified_string(), value);
+        let s = self.compute(&value.get_type_shallow().get_non_specified_string(), value);
         match *value {
             Value::Bool { .. } => TextStyle::with_style(Left, s),
             Value::Int { .. } => TextStyle::with_style(Right, s),
