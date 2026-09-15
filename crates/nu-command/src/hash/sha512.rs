8fe372eace6f6eeff9825ed6a30e09c961005563
Sebastian Lenzlinger <s.m.lenzlinger@student.vu.nl>
2026-09-15 16:20:11 +0200

    Add correct sha512 digest values for examples and tests

--- crates/nu-command/src/hash/sha512.rs
+++ crates/nu-command/src/hash/sha512.rs
@@ -15,7 +15,7 @@ impl HashDigest for Sha512 {
                 description: "Return the sha512 hash of a string, hex-encoded",
                 example: "'abcdefghijklmnopqrstuvwxyz' | hash sha512",
                 result: Some(Value::string(
-                    "71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73".to_owned(),
+                    "4dbff86cc2ca1bae1e16468a05cb9881c97f1753bce3619034898faa1aabe429955a1bf8ec483d7421fe3c1646613a59ed5441fb0f321389f77f48a879c7b1f1".to_owned(),
                     Span::test_data(),
                 )),
             },
@@ -24,10 +24,12 @@ impl HashDigest for Sha512 {
                 example: "'abcdefghijklmnopqrstuvwxyz' | hash sha512 --binary",
                 result: Some(Value::binary(
                     vec![
-                        0x71, 0xc4, 0x80, 0xdf, 0x93, 0xd6, 0xae, 0x2f, 0x1e, 0xfa, 0xd1, 0x44,
-                        0x7c, 0x66, 0xc9, 0x52, 0x5e, 0x31, 0x62, 0x18, 0xcf, 0x51, 0xfc, 0x8d,
-                        0x9e, 0xd8, 0x32, 0xf2, 0xda, 0xf1, 0x8b, 0x73,
-                    ],
+                        0x4d, 0xbf, 0xf8, 0x6c, 0xc2, 0xca, 0x1b, 0xae, 0x1e, 0x16, 0x46, 0x8a,
+                        0x05, 0xcb, 0x98, 0x81, 0xc9, 0x7f, 0x17, 0x53, 0xbc, 0xe3, 0x61, 0x90,
+                        0x34, 0x89, 0x8f, 0xaa, 0x1a, 0xab, 0xe4, 0x29, 0x95, 0x5a, 0x1b, 0xf8,
+                        0xec, 0x48, 0x3d, 0x74, 0x21, 0xfe, 0x3c, 0x16, 0x46, 0x61, 0x3a, 0x59,
+                        0xed, 0x54, 0x41, 0xfb, 0x0f, 0x32, 0x13, 0x89, 0xf7, 0x7f, 0x48, 0xa8,
+                        0x79, 0xc7, 0xb1, 0xf1],
                     Span::test_data(),
                 )),
             },
@@ -47,17 +49,17 @@ impl HashDigest for Sha512 {
                 result: Some(Value::list(
                     vec![
                         Value::string(
-                            "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
+                            "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f"
                                 .to_owned(),
                             Span::test_data(),
                         ),
                         Value::string(
-                            "cb8379ac2098aa165029e3938a51da0bcecfc008fd6795f401178647f96c5b34"
+                            "40a855bf0a93c1019d75dd5b59cd8157608811dd75c5977e07f3bc4be0cad98b22dde4db9ddb429fc2ad3cf9ca379fedf6c1dc4d4bb8829f10c2f0ee04a66663"
                                 .to_owned(),
                             Span::test_data(),
                         ),
                         Value::string(
-                            "50ae61e841fac4e8f9e40baf2ad36ec868922ea48368c18f9535e47db56dd7fb"
+                            "366aead3bed29b6d1de2b8d211e791e5dc7a9611b3d4c61c9323128d746e670a69e9690ce5620efc3b36f6d1b655ce36a72a2fbed4927448b668f1e3f341c0d9"
                                 .to_owned(),
                             Span::test_data(),
                         ),
@@ -83,7 +85,7 @@ mod tests {
     fn hash_string() {
         let binary = Value::string("abcdefghijklmnopqrstuvwxyz".to_owned(), Span::test_data());
         let expected = Value::string(
-            "71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73".to_owned(),
+            "4dbff86cc2ca1bae1e16468a05cb9881c97f1753bce3619034898faa1aabe429955a1bf8ec483d7421fe3c1646613a59ed5441fb0f321389f77f48a879c7b1f1".to_owned(),
             Span::test_data(),
         );
         let actual = generic_digest::action::<Sha512>(
@@ -101,7 +103,7 @@ mod tests {
     fn hash_bytes() {
         let binary = Value::binary(vec![0xC0, 0xFF, 0xEE], Span::test_data());
         let expected = Value::string(
-            "c47a10dc272b1221f0380a2ae0f7d7fa830b3e378f2f5309bbf13f61ad211913".to_owned(),
+            "d6f3d166b443b394f2505c48a5c6904c682d5a6fbe360d6c337a98f7ea6675f195157b33f599b600e39783c72024f91b4718651b4cfd08afcf6c06b9cdb6508c".to_owned(),
             Span::test_data(),
         );
         let actual = generic_digest::action::<Sha512>(
